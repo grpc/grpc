1ef8175bc5e300538a248aef88f4ed9ca033cdf1
ctiller <ctiller@google.com>
2014-12-09 15:45:55 -0800

    Automated g4 rollback of changelist 81719124.
    *** Reason for rollback ***
    
    Breaks interop tests
    
    *** Original change description ***
    
    Advertise h2-16, h2-15, h2-14, and accept any of them.
    
    ***
    	Change on 2014/12/09 by ctiller <ctiller@google.com>
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=81723283

--- src/core/transport/chttp2/alpn.c
+++ src/core/transport/chttp2/alpn.c
@@ -32,25 +32,14 @@
  */
 
 #include "src/core/transport/chttp2/alpn.h"
-#include <grpc/support/log.h>
-#include <grpc/support/useful.h>
 
-/* in order of preference */
-static const char *const supported_versions[] = {"h2-16", "h2-15", "h2-14"};
+static const char *const supported_versions[] = {GRPC_CHTTP2_ALPN_VERSION,
+                                                 "h2-15", "h2-14"};
 
 int grpc_chttp2_is_alpn_version_supported(const char *version, size_t size) {
   size_t i;
-  for (i = 0; i < GPR_ARRAY_SIZE(supported_versions); i++) {
+  for (i = 0; i < sizeof(supported_versions) / sizeof(const char *); i++) {
     if (!strncmp(version, supported_versions[i], size)) return 1;
   }
   return 0;
-}
-
-size_t grpc_chttp2_num_alpn_versions() {
-  return GPR_ARRAY_SIZE(supported_versions);
-}
-
-const char *grpc_chttp2_get_alpn_version_index(size_t i) {
-  GPR_ASSERT(i < GPR_ARRAY_SIZE(supported_versions));
-  return supported_versions[i];
 }
