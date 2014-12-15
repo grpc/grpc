028ddde5d0616705251563222c8d5e5a409d9378
nnoble <nnoble@google.com>
2014-12-15 13:50:08 -0800

    Adding build files for OpenSSL.
    -) Adding a hand-massaged NMake script generated from OpenSSL's perl scripts.
    -) Adding a batch file to invoke it, and only compile the libraries we're interested in.
    	Change on 2014/12/15 by nnoble <nnoble@google.com>
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=82172630

---
+++ vsprojects/vs2013/build_openssl_x86.bat
@@ -0,0 +1,6 @@
+@echo Building OpenSSL 32bits using Visual Studio 2013.
+
+@call "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat" x86
+
+cd ..\..\third_party\openssl
+nmake /F ..\..\vsprojects\third_party\openssl\OpenSSL.mak out32\ssleay32.lib out32\libeay32.lib
