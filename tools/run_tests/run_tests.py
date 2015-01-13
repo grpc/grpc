7c1d7f7a9a11ff1032bee4e145344a3fc8fe2719
Craig Tiller <ctiller@google.com>
2015-01-12 17:08:33 -0800

    Run C++ tests and C tests

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -13,6 +13,7 @@ import watch_dirs
 
 # flags required for make for each configuration
 _CONFIGS = ['dbg', 'opt', 'tsan', 'msan', 'asan']
+_MAKE_TEST_TARGETS = ['buildtests_c', 'buildtests_cxx']
 
 # parse command line
 argp = argparse.ArgumentParser(description='Run grpc tests.')
@@ -44,9 +45,11 @@ def _build_and_run(check_cancelled):
   if not jobset.run(
       (['make',
         '-j', '%d' % (multiprocessing.cpu_count() + 1),
-        'buildtests_c',
+        target,
         'CONFIG=%s' % cfg]
-       for cfg in configs), check_cancelled, maxjobs=1):
+       for cfg in configs
+       for target in _MAKE_TEST_TARGETS),
+      check_cancelled, maxjobs=1):
     sys.exit(1)
 
   # run all the tests
