044db7422a83368aa44eb0bbdfbffef19b61a800
Nicolas Noble <nnoble@google.com>
2015-01-14 16:57:24 -0800

    Various cosmetic improvements to run_tests.py
      -) Adding a new command line flag to be able to see which tests are passing.
      -) Adding more status when the tool is idle in forever mode.
      -) Adding a last status when the tool is finished so to avoid leaving the console in a weird state.
      -) Adding a status message in the forever mode if the previous run failed.
      -) Swapped the message and its explanation, so you don't have to scroll up to see which test failed.
      -) Fixed a race condition in the watch_dir.py code if a file is deleted during the loop.

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -56,6 +56,10 @@ argp.add_argument('-f', '--forever',
                   default=False,
                   action='store_const',
                   const=True)
+argp.add_argument('--newline_on_success',
+                  default=False,
+                  action='store_const',
+                  const=True)
 args = argp.parse_args()
 
 # grab config
@@ -69,7 +73,7 @@ runs_per_test = args.runs_per_test
 forever = args.forever
 
 
-def _build_and_run(check_cancelled):
+def _build_and_run(check_cancelled, newline_on_success, forever=False):
   """Do one pass of building & running tests."""
   # build latest, sharing cpu between the various makes
   if not jobset.run(
@@ -88,20 +92,29 @@ def _build_and_run(check_cancelled):
       for x in itertools.chain.from_iterable(itertools.repeat(
           glob.glob('bins/%s/%s_test' % (
               config.build_config, filt)),
-          runs_per_test))), check_cancelled):
+          runs_per_test))), check_cancelled, newline_on_success=newline_on_success):
+    if not forever:
+      jobset.message('FAILED', 'Some tests failed', do_newline=True)
     return 2
 
+  if not forever:
+    jobset.message('SUCCESS', 'All tests passed', do_newline=True)
   return 0
 
 
 if forever:
+  success = True
   while True:
     dw = watch_dirs.DirWatcher(['src', 'include', 'test'])
     initial_time = dw.most_recent_change()
     have_files_changed = lambda: dw.most_recent_change() != initial_time
-    _build_and_run(have_files_changed)
+    previous_success = success
+    success = _build_and_run(have_files_changed, newline_on_success=False, forever=True) == 0
+    if not previous_success and success:
+      jobset.message('SUCCESS', 'All tests are now passing properly', do_newline=True)
+    jobset.message('IDLE', 'No change detected')
     while not have_files_changed():
       time.sleep(1)
 else:
-  sys.exit(_build_and_run(lambda: False))
+  sys.exit(_build_and_run(lambda: False, newline_on_success=args.newline_on_success))
 
