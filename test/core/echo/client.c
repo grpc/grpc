240d02c0760cdc67345c972b18d43aa1bfea9ccf
Craig Tiller <ctiller@google.com>
2015-01-14 16:16:52 -0800

    Echo test uses grpc_call_invoke

--- test/core/echo/client.c
+++ test/core/echo/client.c
@@ -79,11 +79,8 @@ int main(int argc, char **argv) {
   GPR_ASSERT(argc == 2);
   channel = grpc_channel_create(argv[1], NULL);
   call = grpc_channel_create_call(channel, "/foo", "localhost", gpr_inf_future);
-  GPR_ASSERT(grpc_call_start_invoke(call, cq, (void *)1, (void *)1, (void *)1,
+  GPR_ASSERT(grpc_call_invoke(call, cq, (void *)1, (void *)1,
                                     0) == GRPC_CALL_OK);
-  ev = grpc_completion_queue_next(cq, gpr_inf_future);
-  GPR_ASSERT(ev->data.invoke_accepted == GRPC_OP_OK);
-  grpc_event_finish(ev);
 
   start_write_next_slice(call, bytes_written, WRITE_SLICE_LENGTH);
   bytes_written += WRITE_SLICE_LENGTH;
