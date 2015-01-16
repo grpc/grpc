62b03e2d9f9930217a289392c2af84dccaf46927
Craig Tiller <ctiller@google.com>
2015-01-16 13:19:16 -0800

    Sym-link in system extensions

--- src/php/bin/run_tests.sh
+++ src/php/bin/run_tests.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 # Loads the local shared library, and runs all of the test cases in tests/
 # against it
+set -ex
 cd $(dirname $0)
-php -d extension_dir=../ext/grpc/modules/ -d extension=grpc.so \
+default_extension_dir=`php -i | grep extension_dir | sed 's/.*=> //g'`
+
+# sym-link in system supplied extensions
+for f in $default_extension_dir/*.so
+do
+  ln -s $f ../ext/grpc/modules/$(basename $f) || true
+done
+
+php \
+  -d extension_dir=../ext/grpc/modules/ \
+  -d extension=grpc.so \
   phpunit -v --debug --strict ../tests/unit_tests
