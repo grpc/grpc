1b3a881f8fbf00a7b278cf60cc37b892c751c10a
Craig Tiller <ctiller@google.com>
2015-01-16 13:02:33 -0800

    Dont hardcode phpunit director

--- src/php/bin/run_tests.sh
+++ src/php/bin/run_tests.sh
@@ -3,4 +3,4 @@
 # against it
 cd $(dirname $0)
 php -d extension_dir=../ext/grpc/modules/ -d extension=grpc.so \
-  /usr/local/bin/phpunit -v --debug --strict ../tests/unit_tests
+  phpunit -v --debug --strict ../tests/unit_tests
