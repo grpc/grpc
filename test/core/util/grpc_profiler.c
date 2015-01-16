7132d51b2e833964416e28659e1d77cbe606b668
Craig Tiller <ctiller@google.com>
2015-01-16 09:00:12 -0800

    Include some useful help text

--- test/core/util/grpc_profiler.c
+++ test/core/util/grpc_profiler.c
@@ -40,8 +40,15 @@ void grpc_profiler_start(const char *filename) { ProfilerStart(filename); }
 
 void grpc_profiler_stop() { ProfilerStop(); }
 #else
+#include <grpc/support/log.h>
 
-void grpc_profiler_start(const char *filename) {}
+void grpc_profiler_start(const char *filename) {
+  gpr_log(GPR_DEBUG,
+          "You do not have google-perftools installed, profiling is disabled");
+  gpr_log(GPR_DEBUG,
+          "To install on ubuntu: sudo apt-get install google-perftools "
+          "libgoogle-perftools-dev");
+}
 
 void grpc_profiler_stop(void) {}
 #endif
