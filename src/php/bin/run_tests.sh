c2e80bfbd0ee80772a4b36e482761e97292e82a4
Craig Tiller <ctiller@google.com>
2015-01-16 14:04:29 -0800

    Pickup wherever phpunit is installed

--- src/php/bin/run_tests.sh
+++ src/php/bin/run_tests.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # Loads the local shared library, and runs all of the test cases in tests/
 # against it
-set -ex
+set -e
 cd $(dirname $0)
 default_extension_dir=`php -i | grep extension_dir | sed 's/.*=> //g'`
 
 # sym-link in system supplied extensions
 for f in $default_extension_dir/*.so
 do
-  ln -s $f ../ext/grpc/modules/$(basename $f) || true
+  ln -s $f ../ext/grpc/modules/$(basename $f) &> /dev/null || true
 done
 
 php \
   -d extension_dir=../ext/grpc/modules/ \
   -d extension=grpc.so \
-  phpunit -v --debug --strict ../tests/unit_tests
+  `which phpunit` -v --debug --strict ../tests/unit_tests
