8f3bda2ad28676f0638ad11c26c95af1db144a6b
Craig Tiller <craig.tiller@gmail.com>
2015-01-23 11:27:42 -0800

    Remove use of sprintf

--- src/core/channel/client_channel.c
+++ src/core/channel/client_channel.c
@@ -425,7 +425,7 @@ static void init_channel_elem(grpc_channel_element *elem,
   chand->transport_setup_initiated = 0;
   chand->args = grpc_channel_args_copy(args);
 
-  sprintf(temp, "%d", GRPC_STATUS_CANCELLED);
+  gpr_ltoa(GRPC_STATUS_CANCELLED, temp);
   chand->cancel_status =
       grpc_mdelem_from_strings(metadata_context, "grpc-status", temp);
 }
