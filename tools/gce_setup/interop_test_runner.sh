0eaafca3b2fdd0fc03bbeffcd61569f80b4b04c0
Donna Dionne <donnadionne@google.com>
2015-01-26 16:38:10 -0800

    Adding commands to run cloud to prod tests Adding 2 runners to run the test matrix

---
+++ tools/gce_setup/interop_test_runner.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+main() {
+  source grpc_docker.sh
+  test_cases=(large_unary empty unary ping_pong client_streaming server_streaming)
+  clients=(cxx java go ruby)
+  servers=(cxx java go ruby)
+  for test_case in "${test_cases[@]}"
+  do
+    for client in "${clients[@]}"
+    do
+      for server in "${servers[@]}"
+      do
+        if grpc_interop_test $test_case grpc-docker-testclients $client grpc-docker-server $server
+        then
+          echo "$test_case $client $server passed" >> /tmp/interop_result.txt
+        else
+          echo "$test_case $client $server failed" >> /tmp/interop_result.txt
+        fi
+      done
+    done
+  done
+  gsutil cp /tmp/interop_result.txt gs://stoked-keyword-656-output/interop_result.txt
+  rm /tmp/interop_result.txt
+}
+
+set -x
+main "$@"
