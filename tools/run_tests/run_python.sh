b8b0adfbf9399856fa40945b38a292bccebf370a
Nathaniel Manista <nathaniel@google.com>
2015-01-29 00:30:51 +0000

    Refactoring of the Python build.
    The source code is moved from src/python to
    src/python/src. A setup.py is added at
    src/python. The build_python.sh and
    run_python.sh scripts are updated to build
    and run the Python tests by building a
    package and installing it in the developer's
    Python 2.7 virtual environment.

--- tools/run_tests/run_python.sh
+++ tools/run_tests/run_python.sh
@@ -6,6 +6,12 @@ set -ex
 cd $(dirname $0)/../..
 
 root=`pwd`
-PYTHONPATH=third_party/protobuf/python python2.7_virtual_environment/bin/python2.7 -B -m unittest discover -s src/python -p '*.py'
-# TODO(nathaniel): Get this working again (requires 3.X-friendly protobuf)
+# TODO(issue 215): Properly itemize these in run_tests.py so that they can be parallelized.
+python2.7_virtual_environment/bin/python2.7 -B -m _framework.base.packets.implementations_test
+python2.7_virtual_environment/bin/python2.7 -B -m _framework.face.blocking_invocation_inline_service_test
+python2.7_virtual_environment/bin/python2.7 -B -m _framework.face.event_invocation_synchronous_event_service_test
+python2.7_virtual_environment/bin/python2.7 -B -m _framework.face.future_invocation_asynchronous_event_service_test
+python2.7_virtual_environment/bin/python2.7 -B -m _framework.foundation._later_test
+python2.7_virtual_environment/bin/python2.7 -B -m _framework.foundation._logging_pool_test
+# TODO(nathaniel): Get tests working under 3.4 (requires 3.X-friendly protobuf)
 # python3.4 -B -m unittest discover -s src/python -p '*.py'
