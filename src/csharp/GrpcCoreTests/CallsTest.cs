d68325838ed7c384e93036a1b71a495fd3c82d04
Jan Tattermusch <jtattermusch@google.com>
2015-01-29 12:22:59 -0800

    refactoring

---
+++ src/csharp/GrpcCoreTests/CallsTest.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+
+namespace Google.GRPC.Core.Tests
+{
+    public class CallsTest
+    {
+
+        [Test]
+        public void Test1() {
+        }
+
+
+    }
+}
+
