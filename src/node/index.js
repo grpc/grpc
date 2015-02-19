bdc8baf4fc76baef38fb572e51755bd4e60aa349
murgatroid99 <mlumish@google.com>
2015-02-18 17:06:34 -0800

    Added comment about where Google credentials come from

--- src/node/index.js
+++ src/node/index.js
@@ -75,7 +75,8 @@ function load(filename) {
 
 /**
  * Get a function that a client can use to update metadata with authentication
- * information from a Google Auth credential object.
+ * information from a Google Auth credential object, which comes from the
+ * googleauth library.
  * @param {Object} credential The credential object to use
  * @return {function(Object, callback)} Metadata updater function
  */
