babfd91e4ae71a5601827cebcafc73094c3d8588
Jan Tattermusch <jtattermusch@google.com>
2015-02-20 09:34:34 -0800

    Rename Grpc.Api to Grpc.Examples

--- src/csharp/Grpc.Examples.Tests/Properties/AssemblyInfo.cs
+++ src/csharp/Grpc.Examples.Tests/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle("Grpc.Api.Tests")]
+[assembly: AssemblyTitle("Grpc.Examples.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
--- src/csharp/Grpc.Examples/Properties/AssemblyInfo.cs
+++ src/csharp/Grpc.Examples/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("Grpc.Api")]
+[assembly: AssemblyTitle ("Grpc.Examples")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
