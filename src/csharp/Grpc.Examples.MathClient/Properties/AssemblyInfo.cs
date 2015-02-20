92aa11dff6059f708bd8d5f531fd86e7535bb1a8
Jan Tattermusch <jtattermusch@google.com>
2015-02-20 09:38:54 -0800

    renamed MathClient -> Grpc.Examples.MathClient

--- src/csharp/Grpc.Examples.MathClient/Properties/AssemblyInfo.cs
+++ src/csharp/Grpc.Examples.MathClient/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("MathClient")]
+[assembly: AssemblyTitle ("Grpc.Examples.MathClient")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
