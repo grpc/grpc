255b73b5b198f593b14d878842dd2a0bfec87fe9
nmittler <nathanmittler@google.com>
2015-02-25 11:02:03 -0800

    Removing java examples from grpc-common

--- java/run_greeter_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-TARGET='Greeter Server'
-TARGET_CLASS='ex.grpc.GreeterServer'
-
-cd "$(dirname "$0")"
-mvn -q -nsu -am package -Dcheckstyle.skip=true -DskipTests
-. target/bootclasspath.properties
-echo "[INFO] Running: $TARGET ($TARGET_CLASS)"
-exec java "$bootclasspath" -cp "$jar" "$TARGET_CLASS"
