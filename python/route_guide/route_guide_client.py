grpc/grpc-experiments

python route guide - just the code

python/route_guide/route_guide_client.py
@@ -1,0 +1,148 @@
+# Copyright 2015, Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of Google Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""The Python implementation of the gRPC route guide client."""
+
+import route_guide_pb2
+
+import json
+import random
+import time
+
+_TIMEOUT_SECONDS = 30
+
+
+def read_route_guide_db():
+  """Reads the route guide"""
+  db = []
+  with open("route_guide_db.json") as f:
+    for item in json.load(f):
+      feature = route_guide_pb2.Feature(name = item["name"])
+      feature.location.longitude = item["location"]["longitude"]
+      feature.location.latitude = item["location"]["latitude"]
+      db.append(feature)
+  return db
+
+
+def make_point(latitude, longitude):
+  return route_guide_pb2.Point(latitude = latitude, longitude = longitude)
+
+
+def make_route_note(message, latitude, longitude):
+  route_note = route_guide_pb2.RouteNote(message = message)
+  route_note.location.latitude = latitude
+  route_note.location.longitude = longitude
+  return route_note
+
+
+def guide_get_one_feature(stub, point):
+  feature = stub.GetFeature(point, _TIMEOUT_SECONDS)
+  if not feature.location:
+    print "Server returned incomplete feature"
+    return
+
+  if feature.name:
+    print "Feature called %s at %s" % (feature.name, feature.location)
+  else:
+    print "Found no feature at %s" % feature.location
+
+
+def guide_get_feature(stub):
+  guide_get_one_feature(stub, make_point(409146138, -746188906))
+  guide_get_one_feature(stub, make_point(0, 0))
+
+
+def guide_list_features(stub):
+  rect = route_guide_pb2.Rectangle()
+  rect.lo.latitude = 400000000
+  rect.lo.longitude = -750000000
+  rect.hi.latitude = 420000000
+  rect.hi.longitude = -730000000
+  print "Looking for features between 40, -75 and 42, -73"
+
+  features = stub.ListFeatures(rect, _TIMEOUT_SECONDS)
+
+  for feature in features:
+    print "Feature called %s at %s" % (feature.name, feature.location)
+
+
+def generate_route(db):
+  for index in range(0, 10):
+    random_feature = db[random.randint(0, len(db) - 1)]
+    print "Visiting point %s" % random_feature.location
+    yield random_feature.location
+    time.sleep(random.uniform(0.5, 1.5))
+
+
+def guide_record_route(stub):
+  feature_db = read_route_guide_db()
+
+  route_iter = generate_route(feature_db)
+  route_summary = stub.RecordRoute(route_iter, _TIMEOUT_SECONDS)
+  print "Finished trip with %s points " % route_summary.point_count
+  print "Passed %s features " % route_summary.feature_count
+  print "Travelled %s meters " % route_summary.distance
+  print "It took %s seconds " % route_summary.elapsed_time
+
+
+def generate_messages():
+  messages = [
+              make_route_note("First message", 0, 0),
+              make_route_note("Second message", 0, 1),
+              make_route_note("Third message", 1, 0),
+              make_route_note("Fourth message", 0, 0),
+              make_route_note("Fifth message", 1, 0),
+  ]
+  for msg in messages:
+    print "Sending %s at %s" % (msg.message, msg.location)
+    yield msg
+    time.sleep(random.uniform(0.5, 1.0))
+
+
+def guide_route_chat(stub):
+  responses = stub.RouteChat(generate_messages(), _TIMEOUT_SECONDS)
+  for response in responses:
+    print "Received message %s at %s" % (response.message, response.location)
+
+
+def run():
+  with route_guide_pb2.early_adopter_create_RouteGuide_stub('localhost', 50051) as stub:
+
+    print "-------------- GetFeature --------------"
+    guide_get_feature(stub);
+    print "-------------- ListFeatures --------------"
+    guide_list_features(stub);
+    print "-------------- RecordRoute --------------"
+    guide_record_route(stub);
+    print "-------------- RouteChat --------------"
+    guide_route_chat(stub);
+
+
+if __name__ == '__main__':
+  run()

grpc/grpc

python/route_guide/route_guide_client.py
@@ -1,0 +1,143 @@
+# Copyright 2015, Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of Google Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""The Python implementation of the gRPC route guide client."""
+
+import route_guide_pb2
+
+import json
+import random
+import time
+
+_TIMEOUT_SECONDS = 30
+
+
+def read_route_guide_db():
+  """Reads the route guide"""
+  feature_list = []
+  with open("route_guide_db.json") as route_guide_db_file:
+    for item in json.load(route_guide_db_file):
+      feature = route_guide_pb2.Feature(name=item["name"])
+      feature.location.longitude = item["location"]["longitude"]
+      feature.location.latitude = item["location"]["latitude"]
+      feature_list.append(feature)
+  return feature_list
+
+
+def make_route_note(message, latitude, longitude):
+  route_note = route_guide_pb2.RouteNote(message=message)
+  route_note.location.latitude = latitude
+  route_note.location.longitude = longitude
+  return route_note
+
+
+def guide_get_one_feature(stub, point):
+  feature = stub.GetFeature(point, _TIMEOUT_SECONDS)
+  if not feature.location:
+    print "Server returned incomplete feature"
+    return
+
+  if feature.name:
+    print "Feature called %s at %s" % (feature.name, feature.location)
+  else:
+    print "Found no feature at %s" % feature.location
+
+
+def guide_get_feature(stub):
+  guide_get_one_feature(stub, route_guide_pb2.Point(latitude=409146138, longitude=-746188906))
+  guide_get_one_feature(stub, route_guide_pb2.Point(latitude=0, longitude=0))
+
+
+def guide_list_features(stub):
+  rect = route_guide_pb2.Rectangle()
+  rect.lo.latitude = 400000000
+  rect.lo.longitude = -750000000
+  rect.hi.latitude = 420000000
+  rect.hi.longitude = -730000000
+  print "Looking for features between 40, -75 and 42, -73"
+
+  features = stub.ListFeatures(rect, _TIMEOUT_SECONDS)
+
+  for feature in features:
+    print "Feature called %s at %s" % (feature.name, feature.location)
+
+
+def generate_route(feature_list):
+  for index in range(0, 10):
+    random_feature = feature_list[random.randint(0, len(feature_list) - 1)]
+    print "Visiting point %s" % random_feature.location
+    yield random_feature.location
+    time.sleep(random.uniform(0.5, 1.5))
+
+
+def guide_record_route(stub):
+  feature_list = read_route_guide_db()
+
+  route_iter = generate_route(feature_list)
+  route_summary = stub.RecordRoute(route_iter, _TIMEOUT_SECONDS)
+  print "Finished trip with %s points " % route_summary.point_count
+  print "Passed %s features " % route_summary.feature_count
+  print "Travelled %s meters " % route_summary.distance
+  print "It took %s seconds " % route_summary.elapsed_time
+
+
+def generate_messages():
+  messages = [
+              make_route_note("First message", 0, 0),
+              make_route_note("Second message", 0, 1),
+              make_route_note("Third message", 1, 0),
+              make_route_note("Fourth message", 0, 0),
+              make_route_note("Fifth message", 1, 0),
+  ]
+  for msg in messages:
+    print "Sending %s at %s" % (msg.message, msg.location)
+    yield msg
+    time.sleep(random.uniform(0.5, 1.0))
+
+
+def guide_route_chat(stub):
+  responses = stub.RouteChat(generate_messages(), _TIMEOUT_SECONDS)
+  for response in responses:
+    print "Received message %s at %s" % (response.message, response.location)
+
+
+def run():
+  with route_guide_pb2.early_adopter_create_RouteGuide_stub('localhost', 50051) as stub:
+    print "-------------- GetFeature --------------"
+    guide_get_feature(stub);
+    print "-------------- ListFeatures --------------"
+    guide_list_features(stub);
+    print "-------------- RecordRoute --------------"
+    guide_record_route(stub);
+    print "-------------- RouteChat --------------"
+    guide_route_chat(stub);
+
+
+if __name__ == '__main__':
+  run()