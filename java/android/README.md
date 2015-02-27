aea18013934ad1c301f97f27390f9e85b3b27513
Takeshi Hagikura <thagikura@google.com>
2015-02-27 23:59:54 +0900

    Build the Android tutorial app by gradle

--- java/android/README.md
+++ java/android/README.md
@@ -27,49 +27,15 @@ $ cd grpc-java
 $ # follow the instructions in 'How to Build'
 ```
 
-**3 [Create an Android project](https://developer.android.com/training/basics/firstapp/creating-project.html) under your working directory.**
-- Set Application name to "Helloworld Example" and set Company Domain to "grpc.io". Make sure your package name is "io.grpc.helloworldexample"
-- Choose appropriate minimum SDK
-- Use Blank Activity
-- Set Activity Name to HelloworldActivity
-- Set Layout Name to activity_helloworld
-
-**4 Prepare the app**
+**3 Prepare the app**
 - Clone this git repo
 ```sh
 $ git clone https://github.com/grpc/grpc-common
 
 ```
-- Replace the generated HelloworldActivity.java and activity_helloworld.xml with the two files in this repo
-- Copy GreeterGrpc.java and Helloworld.java under your_app_dir/app/src/main/java/io/grpc/examples/
-- In your AndroidManifest.xml, make sure you have
-```sh
-<uses-permission android:name="android.permission.INTERNET" />
-```
-added outside your appplication tag
 
-**5 Add dependencies. gRPC Java on Android depends on grpc-java, protobuf nano, okhttp**
-- Copy grpc-java .jar files to your_app_dir/app/libs
+**4 Install the app**
 ```sh
-$ cp grpc-java/core/build/libs/*.jar your_app_dir/app/libs/
-$ cp grpc-java/stub/build/libs/*.jar your_app_dir/app/libs/
-$ cp grpc-java/nano/build/libs/*.jar your_app_dir/app/libs/
-$ cp grpc-java/okhttp/build/libs/*.jar your_app_dir/app/libs/
+$ cd grpc-common
+$ ./gradlew installDebug
 ```
-- Copy or download other dependencies to your_app_dir/app/libs/
-  - [Guava 18](http://search.maven.org/remotecontent?filepath=com/google/guava/guava/18.0/guava-18.0.jar)
-  - [okhttp 2.2.0](http://repo1.maven.org/maven2/com/squareup/okhttp/okhttp/2.2.0/okhttp-2.2.0.jar)
-  - [okio](http://search.maven.org/remotecontent?filepath=com/squareup/okio/okio/1.2.0/okio-1.2.0.jar)
-  - [jsr305](http://search.maven.org/remotecontent?filepath=com/google/code/findbugs/jsr305/3.0.0/jsr305-3.0.0.jar)
-  - protobuf nano:
-```sh
-$ cp ~/.m2/repository/com/google/protobuf/nano/protobuf-javanano/3.0.0-alpha-2/protobuf-javanano-3.0.0-alpha-2.jar your_app_dir/app/libs/
-```
-- Make sure your_app_dir/app/build.gradle contains:
-```sh
-dependencies {
-    compile fileTree(dir: 'libs', include: ['*.jar'])
-}
-```
-
-**6 [Run your Helloworld Example app](https://developer.android.com/training/basics/firstapp/running-app.html)**
---
+++ java/android/app/src/main/java/io/grpc/helloworldexample/GreeterGrpc.java
@@ -0,0 +1,179 @@
+package io.grpc.helloworldexample;
+
+import java.io.IOException;
+
+import static io.grpc.stub.Calls.asyncUnaryCall;
+import static io.grpc.stub.Calls.blockingUnaryCall;
+import static io.grpc.stub.Calls.createMethodDescriptor;
+import static io.grpc.stub.Calls.unaryFutureCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryRequestCall;
+import static io.grpc.stub.ServerCalls.createMethodDefinition;
+
+public class GreeterGrpc {
+
+    private static final io.grpc.stub.Method<Helloworld.HelloRequest,
+            Helloworld.HelloReply> METHOD_SAY_HELLO =
+            io.grpc.stub.Method.create(
+                    io.grpc.MethodType.UNARY, "SayHello",
+                    io.grpc.nano.NanoUtils.<Helloworld.HelloRequest>marshaller(
+                            new io.grpc.nano.Parser<Helloworld.HelloRequest>() {
+                                @Override
+                                public Helloworld.HelloRequest parse(com.google.protobuf.nano.CodedInputByteBufferNano input) throws IOException {
+                                    return Helloworld.HelloRequest.parseFrom(input);
+                                }
+                            }),
+                    io.grpc.nano.NanoUtils.<Helloworld.HelloReply>marshaller(
+                            new io.grpc.nano.Parser<Helloworld.HelloReply>() {
+                                @Override
+                                public Helloworld.HelloReply parse(com.google.protobuf.nano.CodedInputByteBufferNano input) throws IOException {
+                                    return Helloworld.HelloReply.parseFrom(input);
+                                }
+                            }));
+
+    public static GreeterStub newStub(io.grpc.Channel channel) {
+        return new GreeterStub(channel, CONFIG);
+    }
+
+    public static GreeterBlockingStub newBlockingStub(
+            io.grpc.Channel channel) {
+        return new GreeterBlockingStub(channel, CONFIG);
+    }
+
+    public static GreeterFutureStub newFutureStub(
+            io.grpc.Channel channel) {
+        return new GreeterFutureStub(channel, CONFIG);
+    }
+
+    public static final GreeterServiceDescriptor CONFIG =
+            new GreeterServiceDescriptor();
+
+    public static class GreeterServiceDescriptor extends
+            io.grpc.stub.AbstractServiceDescriptor<GreeterServiceDescriptor> {
+        public final io.grpc.MethodDescriptor<Helloworld.HelloRequest,
+                Helloworld.HelloReply> sayHello;
+
+        private GreeterServiceDescriptor() {
+            sayHello = createMethodDescriptor(
+                    "helloworld.Greeter", METHOD_SAY_HELLO);
+        }
+
+        private GreeterServiceDescriptor(
+                java.util.Map<java.lang.String, io.grpc.MethodDescriptor<?, ?>> methodMap) {
+            sayHello = (io.grpc.MethodDescriptor<Helloworld.HelloRequest,
+                    Helloworld.HelloReply>) methodMap.get(
+                    CONFIG.sayHello.getName());
+        }
+
+        @java.lang.Override
+        protected GreeterServiceDescriptor build(
+                java.util.Map<java.lang.String, io.grpc.MethodDescriptor<?, ?>> methodMap) {
+            return new GreeterServiceDescriptor(methodMap);
+        }
+
+        @java.lang.Override
+        public com.google.common.collect.ImmutableList<io.grpc.MethodDescriptor<?, ?>> methods() {
+            return com.google.common.collect.ImmutableList.<io.grpc.MethodDescriptor<?, ?>>of(
+                    sayHello);
+        }
+    }
+
+    public static interface Greeter {
+
+        public void sayHello(Helloworld.HelloRequest request,
+                io.grpc.stub.StreamObserver<Helloworld.HelloReply> responseObserver);
+    }
+
+    public static interface GreeterBlockingClient {
+
+        public Helloworld.HelloReply sayHello(Helloworld.HelloRequest request);
+    }
+
+    public static interface GreeterFutureClient {
+
+        public com.google.common.util.concurrent.ListenableFuture<Helloworld.HelloReply> sayHello(
+                Helloworld.HelloRequest request);
+    }
+
+    public static class GreeterStub extends
+            io.grpc.stub.AbstractStub<GreeterStub, GreeterServiceDescriptor>
+            implements Greeter {
+        private GreeterStub(io.grpc.Channel channel,
+                GreeterServiceDescriptor config) {
+            super(channel, config);
+        }
+
+        @java.lang.Override
+        protected GreeterStub build(io.grpc.Channel channel,
+                GreeterServiceDescriptor config) {
+            return new GreeterStub(channel, config);
+        }
+
+        @java.lang.Override
+        public void sayHello(Helloworld.HelloRequest request,
+                io.grpc.stub.StreamObserver<Helloworld.HelloReply> responseObserver) {
+            asyncUnaryCall(
+                    channel.newCall(config.sayHello), request, responseObserver);
+        }
+    }
+
+    public static class GreeterBlockingStub extends
+            io.grpc.stub.AbstractStub<GreeterBlockingStub, GreeterServiceDescriptor>
+            implements GreeterBlockingClient {
+        private GreeterBlockingStub(io.grpc.Channel channel,
+                GreeterServiceDescriptor config) {
+            super(channel, config);
+        }
+
+        @java.lang.Override
+        protected GreeterBlockingStub build(io.grpc.Channel channel,
+                GreeterServiceDescriptor config) {
+            return new GreeterBlockingStub(channel, config);
+        }
+
+        @java.lang.Override
+        public Helloworld.HelloReply sayHello(Helloworld.HelloRequest request) {
+            return blockingUnaryCall(
+                    channel.newCall(config.sayHello), request);
+        }
+    }
+
+    public static class GreeterFutureStub extends
+            io.grpc.stub.AbstractStub<GreeterFutureStub, GreeterServiceDescriptor>
+            implements GreeterFutureClient {
+        private GreeterFutureStub(io.grpc.Channel channel,
+                GreeterServiceDescriptor config) {
+            super(channel, config);
+        }
+
+        @java.lang.Override
+        protected GreeterFutureStub build(io.grpc.Channel channel,
+                GreeterServiceDescriptor config) {
+            return new GreeterFutureStub(channel, config);
+        }
+
+        @java.lang.Override
+        public com.google.common.util.concurrent.ListenableFuture<Helloworld.HelloReply> sayHello(
+                Helloworld.HelloRequest request) {
+            return unaryFutureCall(
+                    channel.newCall(config.sayHello), request);
+        }
+    }
+
+    public static io.grpc.ServerServiceDefinition bindService(
+            final Greeter serviceImpl) {
+        return io.grpc.ServerServiceDefinition.builder("helloworld.Greeter")
+                .addMethod(createMethodDefinition(
+                        METHOD_SAY_HELLO,
+                        asyncUnaryRequestCall(
+                                new io.grpc.stub.ServerCalls.UnaryRequestMethod<
+                                        Helloworld.HelloRequest,
+                                        Helloworld.HelloReply>() {
+                                    @java.lang.Override
+                                    public void invoke(
+                                            Helloworld.HelloRequest request,
+                                            io.grpc.stub.StreamObserver<Helloworld.HelloReply> responseObserver) {
+                                        serviceImpl.sayHello(request, responseObserver);
+                                    }
+                                }))).build();
+    }
+}
