730623074cdca05f8cdddff192818f0be579c9bc
murgatroid99 <mlumish@google.com>
2015-03-09 17:58:55 -0700

    Updated coveralls command line parameters

--- tools/run_tests/send_coveralls.sh
+++ tools/run_tests/send_coveralls.sh
@@ -32,6 +32,8 @@ set -ex
 
 export root=$(dirname $0)/../..
 
-coveralls --exclude third_party --exclude gens -b$root --gcov-options '\-p' --dump $root/core_coveralls.txt
-coveralls --exclude third_party --exclude gens -b$root/src/node/build --gcov-options '\-p' --dump $root/node_coveralls.txt
+cd $root
+
+coveralls --exclude third_party --exclude gens -r. -b. --gcov-options '\-p' --dump ./core_coveralls.txt
+coveralls --include src/node/ -r. -bsrc/node/build --gcov-options '\-p' --dump ./node_coveralls.txt
 node ./src/node/coveralls_multi.js $root/core_coveralls.txt $root/node_coveralls.txt -- $SCRIPT_LCOV_FILE
