4d059dd0be263f4fed06a53623b66048add090cf
wilson <wilson.wen.chn@gmail.com>
2015-03-26 17:49:03 +0800

    Fix python helloworld proto problem
    update readme, delete unused files

--- python/helloworld/run_codegen.sh
+++ python/helloworld/run_codegen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Runs the protoc with gRPC plugin to generate protocol messages and gRPC stubs.
-protoc -I . --python_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_python_plugin` helloworld.proto
+protoc -I ../../protos --python_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_python_plugin` ../../protos/helloworld.proto
