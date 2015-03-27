772ec655373ae56566248c5e1dc5fc1e99074687
Jorge Canizales <jcanizales@google.com>
2015-03-26 17:59:40 -0700

    Idem

---
+++ src/objective-c/examples/Sample/Pods/Target Support Files/Pods-Sample/Pods-Sample-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// OpenSSL
+#define COCOAPODS_POD_AVAILABLE_OpenSSL
+#define COCOAPODS_VERSION_MAJOR_OpenSSL 1
+#define COCOAPODS_VERSION_MINOR_OpenSSL 0
+#define COCOAPODS_VERSION_PATCH_OpenSSL 201
+
+// RxLibrary
+#define COCOAPODS_POD_AVAILABLE_RxLibrary
+#define COCOAPODS_VERSION_MAJOR_RxLibrary 0
+#define COCOAPODS_VERSION_MINOR_RxLibrary 0
+#define COCOAPODS_VERSION_PATCH_RxLibrary 1
+
+// gRPC
+#define COCOAPODS_POD_AVAILABLE_gRPC
+#define COCOAPODS_VERSION_MAJOR_gRPC 0
+#define COCOAPODS_VERSION_MINOR_gRPC 0
+#define COCOAPODS_VERSION_PATCH_gRPC 2
+
+// gRPC/C-Core
+#define COCOAPODS_POD_AVAILABLE_gRPC_C_Core
+#define COCOAPODS_VERSION_MAJOR_gRPC_C_Core 0
+#define COCOAPODS_VERSION_MINOR_gRPC_C_Core 0
+#define COCOAPODS_VERSION_PATCH_gRPC_C_Core 2
+
