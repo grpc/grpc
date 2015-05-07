b8a5f866f252df574342c3d64d3887cda4cddcaf
Yang Gao <yangg@google.com>
2015-05-07 16:26:33 -0700

    do not request another call if server is shutdown

--- src/cpp/server/server.cc
+++ src/cpp/server/server.cc
@@ -446,8 +446,12 @@ void Server::RunRpc() {
     ScheduleCallback();
     if (ok) {
       SyncRequest::CallData cd(this, mrd);
-      mrd->Request(server_);
-
+      {
+        grpc::unique_lock<grpc::mutex> lock(mu_);
+        if (!shutdown_) {
+          mrd->Request(server_);
+        }
+      }
       cd.Run();
     }
   }
