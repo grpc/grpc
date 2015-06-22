493542f4446efe4b87d187d22901078a5de06fe8
Craig Tiller <ctiller@google.com>
2015-06-22 16:00:36 -0700

    Remove return status from maybe_work.
    Since alarm checks may mutate work deadlines for pollsets, the value
    returned from maybe_work is meaningless. Instead, maybe pollset_work
    always return 1 if maybe_work is invoked, and then redo the deadline
    check _on the next call_ to pollset_work.

--- src/core/iomgr/pollset_posix.h
+++ src/core/iomgr/pollset_posix.h
@@ -68,8 +68,8 @@ typedef struct grpc_pollset {
 struct grpc_pollset_vtable {
   void (*add_fd)(grpc_pollset *pollset, struct grpc_fd *fd);
   void (*del_fd)(grpc_pollset *pollset, struct grpc_fd *fd);
-  int (*maybe_work)(grpc_pollset *pollset, gpr_timespec deadline,
-                    gpr_timespec now, int allow_synchronous_callback);
+  void (*maybe_work)(grpc_pollset *pollset, gpr_timespec deadline,
+                     gpr_timespec now, int allow_synchronous_callback);
   void (*kick)(grpc_pollset *pollset);
   void (*finish_shutdown)(grpc_pollset *pollset);
   void (*destroy)(grpc_pollset *pollset);
