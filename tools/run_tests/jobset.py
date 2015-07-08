a29d2d7c33c4dce9a566f1acd190bbea56a1ad13
vjpai <vpai@google.com>
2015-07-08 10:31:15 -0700

    Merge non-tty path with Windows path since they do the same thing

--- tools/run_tests/jobset.py
+++ tools/run_tests/jobset.py
@@ -95,28 +95,21 @@ def message(tag, msg, explanatory_text=None, do_newline=False):
     return
   message.old_tag = tag
   message.old_msg = msg
-  if platform.system() == 'Windows':
+  if platform.system() == 'Windows' or not sys.stdout.isatty():
     if explanatory_text:
       print explanatory_text
     print '%s: %s' % (tag, msg)
     return
   try:
-    if sys.stdout.isatty():
-        sys.stdout.write('%s%s%s\x1b[%d;%dm%s\x1b[0m: %s%s' % (
-            _BEGINNING_OF_LINE,
-            _CLEAR_LINE,
-            '\n%s' % explanatory_text if explanatory_text is not None else '',
-            _COLORS[_TAG_COLOR[tag]][1],
-            _COLORS[_TAG_COLOR[tag]][0],
-            tag,
-            msg,
-            '\n' if do_newline or explanatory_text is not None else ''))
-    else:
-        sys.stdout.write('%s%s: %s%s' % (
-            '\n%s' % explanatory_text if explanatory_text is not None else '',
-            tag,
-            msg,
-            '\n'))
+    sys.stdout.write('%s%s%s\x1b[%d;%dm%s\x1b[0m: %s%s' % (
+        _BEGINNING_OF_LINE,
+        _CLEAR_LINE,
+        '\n%s' % explanatory_text if explanatory_text is not None else '',
+        _COLORS[_TAG_COLOR[tag]][1],
+        _COLORS[_TAG_COLOR[tag]][0],
+        tag,
+        msg,
+        '\n' if do_newline or explanatory_text is not None else ''))
     sys.stdout.flush()
   except:
     pass
