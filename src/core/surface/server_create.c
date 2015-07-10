711db76a2c4154b93f17e68f554d3ad0724aa05a
David Garcia Quintas <dgq@google.com>
2015-07-09 22:36:45 -0700

    Reverted wrong set of changes to server_create

--- src/core/surface/server_create.c
+++ src/core/surface/server_create.c
@@ -32,12 +32,9 @@
  */
 
 #include <grpc/grpc.h>
-
-#include "src/core/channel/compress_filter.h"
 #include "src/core/surface/completion_queue.h"
 #include "src/core/surface/server.h"
 
 grpc_server *grpc_server_create(const grpc_channel_args *args) {
-  const grpc_channel_filter *filters[] = {&grpc_compress_filter};
-  return grpc_server_create_from_filters(filters, 0, args);
+  return grpc_server_create_from_filters(NULL, 0, args);
 }
