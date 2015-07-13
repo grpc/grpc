6a3cf974f7f4cbc60f25a314bdde2687a4bbafa2
David Garcia Quintas <dgq@google.com>
2015-07-13 13:38:18 -0700

    Added WriteOptions for per-message flags and modified CallOps to use it.

--- test/cpp/end2end/mock_test.cc
+++ test/cpp/end2end/mock_test.cc
@@ -86,7 +86,9 @@ class MockClientReaderWriter<EchoRequest, EchoResponse> GRPC_FINAL
     msg->set_message(last_message_);
     return true;
   }
-  bool Write(const EchoRequest& msg) GRPC_OVERRIDE {
+
+  bool Write(const EchoRequest& msg,
+             const WriteOptions& options) GRPC_OVERRIDE {
     gpr_log(GPR_INFO, "mock recv msg %s", msg.message().c_str());
     last_message_ = msg.message();
     return true;
