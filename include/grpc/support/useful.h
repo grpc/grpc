cfb21ce66809f4aff85b5b3cb7c824178071bffa
David Garcia Quintas <dgq@google.com>
2015-07-15 17:50:41 -0700

    Added missing "static"

--- include/grpc/support/useful.h
+++ include/grpc/support/useful.h
@@ -64,7 +64,7 @@
 #define GPR_BITGET(i, n) (((i) & (1u << n)) != 0)
 
 /** Returns number of bits set in bitset \a i */
-int gpr_bitcount(gpr_uint32 i) {
+static int gpr_bitcount(gpr_uint32 i) {
   i = i - ((i >> 1) & 0x55555555);
   i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
   return (((i + (i >> 4)) & 0xF0F0F0F) * 0x1010101) >> 24;
