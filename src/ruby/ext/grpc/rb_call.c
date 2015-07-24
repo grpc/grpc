a5fea60e8d8c7bdf0cc82ac04a5c086b900d7531
Marcin Wyszynski <marcin.pixie@gmail.com>
2015-07-24 10:24:32 +0200

    array_length and it's counter types should match

--- src/ruby/ext/grpc/rb_call.c
+++ src/ruby/ext/grpc/rb_call.c
@@ -236,7 +236,7 @@ static VALUE grpc_rb_call_set_metadata(VALUE self, VALUE metadata) {
 static int grpc_rb_md_ary_fill_hash_cb(VALUE key, VALUE val, VALUE md_ary_obj) {
   grpc_metadata_array *md_ary = NULL;
   long array_length;
-  int i;
+  long i;
 
   /* Construct a metadata object from key and value and add it */
   TypedData_Get_Struct(md_ary_obj, grpc_metadata_array,
