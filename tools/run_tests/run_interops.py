6b7d9c1698bfb4284889545385be34e66fd4eac6
Donna Dionne <donnadionne@google.com>
2015-07-25 15:30:11 -0700

    adding interop running with flags.

--- tools/run_tests/run_interops.py
+++ tools/run_tests/run_interops.py
@@ -1,13 +1,34 @@
+import argparse
 import xml.etree.cElementTree as ET
 import jobset
 
-cxx_steps = 'tools/run_tests/run_interops.sh'
+argp = argparse.ArgumentParser(description='Run interop tests.')
+argp.add_argument('-l', '--language',
+                  choices=['build_only', 'cxx'],
+                  nargs='+',
+                  default=['build_only'])
+args = argp.parse_args()
 
-cxx_job = jobset.JobSpec(cmdline=cxx_steps, shortname='interop')
+_TESTS = ['large_unary', 'empty_unary', 'ping_pong']
+
+jobs = []
+jobNumber = 0
+
+build_job = jobset.JobSpec(cmdline='tools/run_tests/run_interops.sh', shortname='interop')
+jobs.append(build_job)
+jobNumber+=1
+
+if args.language == 'cxx':
+  testCommand = 'sudo docker run grpc/cxx /var/local/git/grpc/bins/opt/interop_client --enable_ssl --use_prod_roots --server_host_override=grpc-test.sandbox.google.com --server_host=grpc-test.sandbox.google.com --server_port=443 --test_case='
+  for test in _TESTS:
+    job = jobset.JobSpec(cmdline=testCommand+test, shortname=test)
+    jobs.append(job)
+    jobNumber+=1
+
 root = ET.Element('testsuites')
 testsuite = ET.SubElement(root, 'testsuite', id='1', package='grpc', name='tests')
 
-jobset.run([cxx_job], maxjobs=3, xml_report=testsuite)
+jobset.run(jobs, maxjobs=jobNumber, xml_report=testsuite)
 
 tree = ET.ElementTree(root)
 tree.write('report.xml', encoding='UTF-8')
