5e1dcf7b56af611200fa96a592041f1b7709c96f
Donna Dionne <donnadionne@google.com>
2015-07-26 00:29:52 -0700

    small error

--- tools/run_tests/run_interops.py
+++ tools/run_tests/run_interops.py
@@ -2,8 +2,7 @@ import xml.etree.cElementTree as ET
 import jobset
 
 #cxx_steps = 'tools/run_tests/run_interops.sh'
-cxx_steps = 'sudo docker run grpc/cxx /var/local/git/grpc/bins/opt/interop_client --enable_ssl --use_prod_roots --server_host_override
-=grpc-test.sandbox.google.com --server_host=grpc-test.sandbox.google.com --server_port=443 --test_case=large_unary'
+cxx_steps = 'sudo docker run grpc/cxx /var/local/git/grpc/bins/opt/interop_client --enable_ssl --use_prod_roots --server_host_override=grpc-test.sandbox.google.com --server_host=grpc-test.sandbox.google.com --server_port=443 --test_case=large_unary'
 
 cxx_job = jobset.JobSpec(cmdline=cxx_steps, shortname='interop')
 root = ET.Element('testsuites')
