6ab9e28f53712aa55a4d9545a9f144a7e1e0fc92
Donna Dionne <donnadionne@google.com>
2015-07-27 11:09:29 -0700

    adding cleanup step

--- tools/run_tests/run_interops.py
+++ tools/run_tests/run_interops.py
@@ -2,16 +2,18 @@ import xml.etree.cElementTree as ET
 import jobset
 
 build_steps = 'tools/run_tests/run_interops_build.sh'
+cleanup_steps = 'tools/run_tests/run_interops_cleanup.sh'
 
 _TESTS = ['large_unary', 'empty_unary', 'ping_pong', 'client_streaming', 'server_streaming']
 
 build_job = jobset.JobSpec(cmdline=build_steps, shortname='build')
+cleanup_job = jobset.JobSpec(cmdline=cleanup_steps, shortname='cleanup')
 
 jobs = []
 jobNumber = 0
 
 for test in _TESTS:
-  test_job = jobset.JobSpec(cmdline=['tools/run_tests/run_interops_test.sh', '%s' % test], shortname='cxx')
+  test_job = jobset.JobSpec(cmdline=['tools/run_tests/run_interops_test.sh', '%s' % test], shortname=test)
   jobs.append(test_job)
   jobNumber+=1
 
@@ -20,6 +22,7 @@ testsuite = ET.SubElement(root, 'testsuite', id='1', package='grpc', name='tests
 
 jobset.run([build_job], maxjobs=1, xml_report=testsuite)
 jobset.run(jobs, maxjobs=jobNumber, xml_report=testsuite)
+jobset.run([cleanup_job], maxjobs=1, xml_report=testsuite)
 
 tree = ET.ElementTree(root)
 tree.write('report.xml', encoding='UTF-8')
