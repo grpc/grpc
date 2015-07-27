843d45bb90baec812b7a190657c481e57cb91baf
Donna Dionne <donnadionne@google.com>
2015-07-27 08:57:47 -0700

    small fix

--- tools/run_tests/run_interops.py
+++ tools/run_tests/run_interops.py
@@ -4,12 +4,13 @@ import jobset
 buid_steps = 'tools/run_tests/run_interops_build.sh'
 cxx_steps = 'tools/run_tests/run_interops_test.sh'
 
-build_job = jobset.JobSpec(cmdline=cxx_steps, shortname='build')
+build_job = jobset.JobSpec(cmdline=build_steps, shortname='build')
 cxx_job = jobset.JobSpec(cmdline=cxx_steps, shortname='cxx')
 root = ET.Element('testsuites')
 testsuite = ET.SubElement(root, 'testsuite', id='1', package='grpc', name='tests')
 
-jobset.run([build_job, cxx_job], maxjobs=2, xml_report=testsuite)
+jobset.run([build_job], maxjobs=1, xml_report=testsuite)
+jobset.run([cxx_job], maxjobs=1, xml_report=testsuite)
 
 tree = ET.ElementTree(root)
 tree.write('report.xml', encoding='UTF-8')
