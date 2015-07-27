1a9266c91725853b6453d44a6dcd054f1786d0b5
Donna Dionne <donnadionne@google.com>
2015-07-27 09:47:14 -0700

    do tests in a loop now

--- tools/run_tests/run_interops.py
+++ tools/run_tests/run_interops.py
@@ -2,15 +2,24 @@ import xml.etree.cElementTree as ET
 import jobset
 
 build_steps = 'tools/run_tests/run_interops_build.sh'
-cxx_steps = 'tools/run_tests/run_interops_test.sh'
+
+_TESTS = ['large_unary', 'empty_unary', 'ping_pong']
 
 build_job = jobset.JobSpec(cmdline=build_steps, shortname='build')
-cxx_job = jobset.JobSpec(cmdline=['tools/run_tests/run_interops_test.sh', 'large_unary'], shortname='cxx')
+
+jobs = []
+jobNumber = 0
+
+for test in _TESTS:
+  test_job = jobset.JobSpec(cmdline=['tools/run_tests/run_interops_test.sh', '%s' % test], shortname='cxx')
+  jobs.append(test_job)
+  jobNumber+=1
+
 root = ET.Element('testsuites')
 testsuite = ET.SubElement(root, 'testsuite', id='1', package='grpc', name='tests')
 
 jobset.run([build_job], maxjobs=1, xml_report=testsuite)
-jobset.run([cxx_job], maxjobs=1, xml_report=testsuite)
+jobset.run(jobs, maxjobs=jobNumber, xml_report=testsuite)
 
 tree = ET.ElementTree(root)
 tree.write('report.xml', encoding='UTF-8')
