54ac89a3cef1901fdc500835c9f014c13c7d355b
Donna Dionne <donnadionne@google.com>
2015-07-27 11:50:42 -0700

    moving cleanup to first.

--- tools/run_tests/run_interops.py
+++ tools/run_tests/run_interops.py
@@ -2,12 +2,10 @@ import xml.etree.cElementTree as ET
 import jobset
 
 build_steps = 'tools/run_tests/run_interops_build.sh'
-cleanup_steps = 'tools/run_tests/run_interops_cleanup.sh'
 
 _TESTS = ['large_unary', 'empty_unary', 'ping_pong', 'client_streaming', 'server_streaming']
 
 build_job = jobset.JobSpec(cmdline=build_steps, shortname='build')
-cleanup_job = jobset.JobSpec(cmdline=cleanup_steps, shortname='cleanup')
 
 jobs = []
 jobNumber = 0
@@ -22,7 +20,6 @@ testsuite = ET.SubElement(root, 'testsuite', id='1', package='grpc', name='tests
 
 jobset.run([build_job], maxjobs=1, xml_report=testsuite)
 jobset.run(jobs, maxjobs=jobNumber, xml_report=testsuite)
-jobset.run([cleanup_job], maxjobs=1, xml_report=testsuite)
 
 tree = ET.ElementTree(root)
 tree.write('report.xml', encoding='UTF-8')
