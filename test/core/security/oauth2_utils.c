c5eccd45f4b5cc0926a2060fa4b2ec8942c66a98
yang-g <yangg@google.com>
2015-07-28 00:23:50 -0700

    Fix clock type mismatch

--- test/core/security/oauth2_utils.c
+++ test/core/security/oauth2_utils.c
@@ -84,7 +84,7 @@ char *grpc_test_fetch_oauth2_token_with_credentials(grpc_credentials *creds) {
 
   gpr_mu_lock(GRPC_POLLSET_MU(&request.pollset));
   while (!request.is_done)
-    grpc_pollset_work(&request.pollset, gpr_inf_future(GPR_CLOCK_REALTIME));
+    grpc_pollset_work(&request.pollset, gpr_inf_future(GPR_CLOCK_MONOTONIC));
   gpr_mu_unlock(GRPC_POLLSET_MU(&request.pollset));
 
   grpc_pollset_shutdown(&request.pollset, do_nothing, NULL);
