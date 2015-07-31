d65b34e71702b4fceb9ae9598faeb6c1c7db491d
Jan Tattermusch <jtattermusch@google.com>
2015-07-30 17:22:10 -0700

    bump C# version to 0.6.1 and core version to 0.10.1

--- src/csharp/Grpc.Core/Version.cs
+++ src/csharp/Grpc.Core/Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // The current version of gRPC C#.
-[assembly: AssemblyVersion("0.6.0.*")]
+[assembly: AssemblyVersion("0.6.1.*")]
 
