98c50b074dbe1d544ade44621506e19b5cfeafc4
yang-g <yangg@google.com>
2015-08-03 16:02:14 -0700

    cq_ seems not used

--- src/cpp/client/client_context.cc
+++ src/cpp/client/client_context.cc
@@ -46,21 +46,12 @@ namespace grpc {
 ClientContext::ClientContext()
     : initial_metadata_received_(false),
       call_(nullptr),
-      cq_(nullptr),
       deadline_(gpr_inf_future(GPR_CLOCK_REALTIME)),
       state_(CREATED) {}
 
 ClientContext::~ClientContext() {
   if (call_) {
     grpc_call_destroy(call_);
-  }
-  if (cq_) {
-    // Drain cq_.
-    grpc_completion_queue_shutdown(cq_);
-    while (grpc_completion_queue_next(cq_, gpr_inf_future(GPR_CLOCK_REALTIME))
-               .type != GRPC_QUEUE_SHUTDOWN)
-      ;
-    grpc_completion_queue_destroy(cq_);
   }
 }
 
