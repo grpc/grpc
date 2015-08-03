e8d953557dd9254b68f9b39e936585a7524b0388
Jorge Canizales <jcanizales@google.com>
2015-08-03 15:41:11 -0700

    Formatting and documentation

--- src/objective-c/tests/build_tests.sh
+++ src/objective-c/tests/build_tests.sh
@@ -28,12 +28,18 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# Don't run this script standalone. Instead, run from the repository root:
+# ./tools/run_tests/run_tests.py -l objc
+
 set -e
 
 cd $(dirname $0)
 
 hash pod 2>/dev/null || { echo >&2 "Cocoapods needs to be installed."; exit 1; }
-hash xcodebuild 2>/dev/null || { echo >&2 "XCode command-line tools need to be installed."; exit 1; }
+hash xcodebuild 2>/dev/null || {
+    echo >&2 "XCode command-line tools need to be installed."
+    exit 1
+}
 
 BINDIR=../../../bins/$CONFIG
 
