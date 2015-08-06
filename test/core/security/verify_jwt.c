038d26acd80878366c9df40a36d91db7c684aa7f
Craig Tiller <ctiller@google.com>
2015-08-06 08:57:54 -0700

    Update tools

--- test/core/security/verify_jwt.c
+++ test/core/security/verify_jwt.c
@@ -111,8 +111,8 @@ int main(int argc, char **argv) {
   gpr_mu_lock(GRPC_POLLSET_MU(&sync.pollset));
   while (!sync.is_done) {
     grpc_pollset_worker worker;
-    grpc_pollset_work(&sync.pollset, &worker,
-                      gpr_inf_future(GPR_CLOCK_REALTIME));
+    grpc_pollset_work(&sync.pollset, &worker, gpr_now(GPR_CLOCK_MONOTONIC),
+                      gpr_inf_future(GPR_CLOCK_MONOTONIC));
   }
   gpr_mu_unlock(GRPC_POLLSET_MU(&sync.pollset));
 
