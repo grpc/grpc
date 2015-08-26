03a463a994f0dcd3d2beb9122b7613d32e40766a
Craig Tiller <ctiller@google.com>
2015-08-26 14:14:26 -0700

    Give interop tests more time to build

--- tools/run_tests/run_interops.py
+++ tools/run_tests/run_interops.py
@@ -8,7 +8,9 @@ argp.add_argument('-l', '--language',
 args = argp.parse_args()
 
 # build job
-build_job = jobset.JobSpec(cmdline=['tools/run_tests/run_interops_build.sh', '%s' % args.language], shortname='build')
+build_job = jobset.JobSpec(cmdline=['tools/run_tests/run_interops_build.sh', '%s' % args.language],
+                           shortname='build',
+                           timeout_seconds=30*60)
 
 # test jobs, each test is a separate job to run in parallel
 _TESTS = ['large_unary', 'empty_unary', 'ping_pong', 'client_streaming', 'server_streaming']
