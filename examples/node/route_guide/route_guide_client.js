c919228e0ae7982c60e40d59a946ac0288ae8ce5
murgatroid99 <mlumish@google.com>
2015-08-28 15:24:07 -0700

    Updated Node examples to be compatible with master

--- examples/node/route_guide/route_guide_client.js
+++ examples/node/route_guide/route_guide_client.js
@@ -34,7 +34,8 @@ var path = require('path');
 var _ = require('underscore');
 var grpc = require('grpc');
 var examples = grpc.load(__dirname + '/route_guide.proto').examples;
-var client = new examples.RouteGuide('localhost:50051');
+var client = new examples.RouteGuide('localhost:50051',
+                                     grpc.Credentials.createInsecure());
 
 var COORD_FACTOR = 1e7;
 
