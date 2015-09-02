b86367d60d40aa117e64458bb01285786f911e30
Jan Tattermusch <jtattermusch@google.com>
2015-09-02 14:12:49 -0700

    Make examples depend on the right nuget package version

--- examples/csharp/route_guide/generate_protos.bat
+++ examples/csharp/route_guide/generate_protos.bat
@@ -5,6 +5,6 @@ setlocal
 @rem enter this directory
 cd /d %~dp0
 
-packages\Google.Protobuf.3.0.0-alpha4\tools\protoc.exe -I../../protos --csharp_out RouteGuide  ../../protos/route_guide.proto --grpc_out RouteGuide --plugin=protoc-gen-grpc=packages\Grpc.Tools.0.7.0-preview20150827\tools\grpc_csharp_plugin.exe
+packages\Google.Protobuf.3.0.0-alpha4\tools\protoc.exe -I../../protos --csharp_out RouteGuide  ../../protos/route_guide.proto --grpc_out RouteGuide --plugin=protoc-gen-grpc=packages\Grpc.Tools.0.7.0\tools\grpc_csharp_plugin.exe
 
 endlocal
