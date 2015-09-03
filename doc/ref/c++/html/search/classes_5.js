7afa666d2f0bff9e87ab36d2d6945236eabed363
David Garcia Quintas <dgq@google.com>
2015-09-03 09:45:14 -0700

    Regenerated doxygen after #3227

--- doc/ref/c++/html/search/classes_5.js
+++ doc/ref/c++/html/search/classes_5.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['genericservercontext',['GenericServerContext',['../classgrpc_1_1_generic_server_context.html',1,'grpc']]],
-  ['genericstub',['GenericStub',['../classgrpc_1_1_generic_stub.html',1,'grpc']]],
-  ['grpclibrary',['GrpcLibrary',['../classgrpc_1_1_grpc_library.html',1,'grpc']]]
+  ['handlerparameter',['HandlerParameter',['../structgrpc_1_1_method_handler_1_1_handler_parameter.html',1,'grpc::MethodHandler']]]
 ];
