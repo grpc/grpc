0938400040e01f098bcc9934f833fbf22eaa7e49
Tim Emiola <temiola@google.com>
2015-09-09 18:08:49 -0700

    Revert upstream changes that add reference docs.
    Revert "Regenerated doxygen after #3227"
    
    - This reverts commit 7afa666d2f0bff9e87ab36d2d6945236eabed363.
    
    Revert "regenerate C# docs"
    
    - This reverts commit 3c99f09ef321e3b876ea843a3689dda33b89e104.
    
    Revert "added generated docs"
    
    - This reverts commit f209c587033d333705fe40ab349dc5ddda18df3a.
    
    Revert "Initial Doxygen import for core, c++"
    
    - This reverts commit 68f4e0f6abc6bb99ff334542d5b5003edc3e651e.

--- doc/ref/c++/html/search/variables_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['call',['call',['../structgrpc_1_1_method_handler_1_1_handler_parameter.html#aebd5b34a7b5bc6e04ef5ab5df42e7e72',1,'grpc::MethodHandler::HandlerParameter']]],
-  ['cancelled',['CANCELLED',['../classgrpc_1_1_status.html#a9994ffe95a0495915d82481c2ec594ab',1,'grpc::Status']]],
-  ['cert_5fchain',['cert_chain',['../structgrpc_1_1_ssl_server_credentials_options_1_1_pem_key_cert_pair.html#ad58d2824d15cc6a1d646b8bef0b8352e',1,'grpc::SslServerCredentialsOptions::PemKeyCertPair']]]
-];
--- doc/ref/core.internal/html/search/all_18.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['zombied',['ZOMBIED',['../server_8c.html#a6ca2e6dbf0562417ab7c66e8907a56b1acfa05d5a2f18cf6a5976178d9949c512',1,'server.c']]]
-];
