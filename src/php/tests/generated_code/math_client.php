dc8e5b0873cd973e4377543d6c5fdc2e6d8242e7
Stanley Cheung <stanleycheung@google.com>
2015-09-22 07:53:56 -0700

    minor fixes

--- src/php/tests/generated_code/math_client.php
+++ src/php/tests/generated_code/math_client.php
@@ -1,4 +1,36 @@
 <?php
+/*
+ *
+ * Copyright 2015, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 # Fix the following two lines to point to your installation
 include 'vendor/autoload.php';
@@ -8,7 +40,7 @@ function p($line) {
   print("$line<br/>\n");
 }
 
-h$host = "localhost:50051";
+$host = "localhost:50051";
 p("Connecting to host: $host");
 $client = new math\MathClient($host, []);
 p("Client class: ".get_class($client));
@@ -36,7 +68,7 @@ $call = $client->Fib($fib_arg);
 $result_array = iterator_to_array($call->responses());
 $result = '';
 foreach ($result_array as $num) {
-  $result .= ' ' . $num->getNum();
+  $result .= ' '.$num->getNum();
 }
 p("The first $limit Fibonacci numbers are:".$result);
 p('');
@@ -50,7 +82,7 @@ for ($i = 0; $i <= $limit; $i++) {
 }
 list($response, $status) = $call->wait();
 p(sprintf("The first %d positive integers sum to: %d",
-                   $limit, $response->getNum()));
+          $limit, $response->getNum()));
 p('');
 
 p("Running bidi-streaming test:");
@@ -58,13 +90,13 @@ $call = $client->DivMany();
 for ($i = 0; $i < 7; $i++) {
   $div_arg = new math\DivArgs();
   $dividend = 2 * $i + 1;
-  $div_arg->setDividend($dividend);
   $divisor = 3;
+  $div_arg->setDividend($dividend);
   $div_arg->setDivisor($divisor);
   $call->write($div_arg);
   p("client writing: $dividend / $divisor");
   $response = $call->read();
   p(sprintf("server writing: quotient = %d, remainder = %d",
-                     $response->getQuotient(), $response->getRemainder()));
+            $response->getQuotient(), $response->getRemainder()));
 }
 $call->writesDone();
