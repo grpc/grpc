9375895a4de10effecbbd9ed64f027bf5c4475e5
murgatroid99 <mlumish@google.com>
2015-10-14 11:51:05 -0700

    Moved Node pre-build steps to a separate shell script

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -202,9 +202,7 @@ class NodeLanguage(object):
 
   def pre_build_steps(self):
     # Default to 1 week cache expiration
-    return [['npm', 'update', '--cache-min', '604800'],
-            ['npm', 'install', '-g', 'node-gyp-install'],
-            ['node-gyp-install']]
+    return [['tools/run_tests/pre_build_node.sh']]
 
   def make_targets(self):
     return []
