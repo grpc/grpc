75457ff7e15263fd659049d7b09a382dde706aea
Stanley Cheung <stanleycheung@google.com>
2015-10-20 16:27:55 -0700

    php: add more unit test for code coverage analysis

--- src/php/tests/generated_code/GeneratedCodeTest.php
+++ src/php/tests/generated_code/GeneratedCodeTest.php
@@ -34,7 +34,7 @@
 require_once dirname(__FILE__) . '/AbstractGeneratedCodeTest.php';
 
 class GeneratedCodeTest extends AbstractGeneratedCodeTest {
-  public static function setUpBeforeClass() {
+  public function setUp() {
     self::$client = new math\MathClient(
         getenv('GRPC_TEST_HOST'), []);
   }
