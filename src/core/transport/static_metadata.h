15c4922ce60a2692d37a2819ed4908a11463bffc
Craig Tiller <ctiller@google.com>
2015-11-19 07:55:03 -0800

    Add commentary

--- src/core/transport/static_metadata.h
+++ src/core/transport/static_metadata.h
@@ -35,6 +35,10 @@
  * To make changes to this file, change
  * tools/codegen/core/gen_static_metadata.py,
  * and then re-run it.
+ *
+ * See metadata.h for an explanation of the interface here, and metadata.c for
+ * an
+ * explanation of what's going on.
  */
 
 #ifndef GRPC_INTERNAL_CORE_TRANSPORT_STATIC_METADATA_H
