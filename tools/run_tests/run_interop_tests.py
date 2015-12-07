eaa7d203b51aba558eb141ca50ee95a8f7790617
Masood Malekghassemi <soltanmm@users.noreply.github.com>
2015-12-07 14:38:53 -0800

    Fix interop Python test invocation
    The ideal fix would be to have a tox test command for running the
    interop server and client.

--- tools/run_tests/run_interop_tests.py
+++ tools/run_tests/run_interop_tests.py
@@ -267,7 +267,7 @@ class PythonLanguage:
 
   def client_cmd(self, args):
     return [
-        'python2.7_virtual_environment/bin/python',
+        'src/python/grpcio/.tox/py27/bin/python',
         'src/python/grpcio/setup.py',
         'run_interop',
         '--client',
@@ -279,7 +279,7 @@ class PythonLanguage:
 
   def server_cmd(self, args):
     return [
-        'python2.7_virtual_environment/bin/python',
+        'src/python/grpcio/.tox/py27/bin/python',
         'src/python/grpcio/setup.py',
         'run_interop',
         '--server',
