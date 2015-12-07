0a9bac929e1cafd0596611c178ed52a9b9a6dde9
murgatroid99 <mlumish@google.com>
2015-12-07 14:59:31 -0800

    Reverted changes to single_run_localhost.sh

--- test/cpp/qps/single_run_localhost.sh
+++ test/cpp/qps/single_run_localhost.sh
@@ -34,15 +34,17 @@ set -ex
 
 cd $(dirname $0)/../../..
 
+killall qps_worker || true
+
 config=opt
 
 NUMCPUS=`python2.7 -c 'import multiprocessing; print multiprocessing.cpu_count()'`
 
-make CONFIG=$config qps_driver -j$NUMCPUS
+make CONFIG=$config qps_worker qps_driver -j$NUMCPUS
 
-node src/node/performance/worker_server.js --driver_port=10000 &
+bins/$config/qps_worker -driver_port 10000 &
 PID1=$!
-node src/node/performance/worker_server.js --driver_port=10010 &
+bins/$config/qps_worker -driver_port 10010 &
 PID2=$!
 
 export QPS_WORKERS="localhost:10000,localhost:10010"
@@ -51,3 +53,4 @@ bins/$config/qps_driver $*
 
 kill -2 $PID1 $PID2
 wait
+
