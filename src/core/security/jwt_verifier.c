a4794e57a45b303280b5d859a355e22f684a29ae
Craig Tiller <ctiller@google.com>
2015-12-22 15:03:40 -0800

    Update copyrights

--- src/core/security/jwt_verifier.c
+++ src/core/security/jwt_verifier.c
@@ -8,9 +8,9 @@
  * met:
  *
  *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimser.
+ * notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimser
+ * copyright notice, this list of conditions and the following disclaimer
  * in the documentation and/or other materials provided with the
  * distribution.
  *     * Neither the name of Google Inc. nor the names of its
