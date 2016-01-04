15eb60850ad96f4e2f45715a7f1b69823960fc6f
Craig Tiller <ctiller@google.com>
2016-01-04 15:05:35 -0800

    Attempt to get docker clang-format working

--- tools/distrib/clang_format_code.sh
+++ tools/distrib/clang_format_code.sh
@@ -37,5 +37,5 @@ cd $(dirname $0)/../..
 docker build -t grpc_clang_format tools/dockerfile/grpc_clang_format
 
 # run clang-format against the checked out codebase
-docker run -e TEST=$TEST --rm=true -v `pwd`:/local-code -t grpc_clang_format /clang_format_all_the_things.sh
+docker run -e TEST=$TEST --rm=true -v ${HOST_GIT_ROOT:-`pwd`}:/local-code -t grpc_clang_format /clang_format_all_the_things.sh
 
