1508410c5eb5cfbb5775b46d7733baff374a84b7
murgatroid99 <mlumish@google.com>
2016-01-05 15:13:08 -0800

    Made ruby per-rpc creds interop use auth plugin API

--- src/ruby/lib/grpc.rb
+++ src/ruby/lib/grpc.rb
@@ -1,4 +1,4 @@
-# Copyright 2015, Google Inc.
+# Copyright 2015-2016, Google Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -27,7 +27,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-ssl_roots_path = File.expand_path('../../../etc/roots.pem', __FILE__)
+ssl_roots_path = File.expand_path('../../../../etc/roots.pem', __FILE__)
 unless ENV['GRPC_DEFAULT_SSL_ROOTS_FILE_PATH']
   ENV['GRPC_DEFAULT_SSL_ROOTS_FILE_PATH'] = ssl_roots_path
 end
