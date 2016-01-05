387e116fd6e3afd2413bd2d01b25eb19decd842e
Masood Malekghassemi <soltanmm@users.noreply.github.com>
2016-01-05 10:16:12 -0800

    Use BoringSSL for Python gRPC

--- tools/run_tests/build_python.sh
+++ tools/run_tests/build_python.sh
@@ -37,8 +37,8 @@ ROOT=`pwd`
 export LD_LIBRARY_PATH=$ROOT/libs/$CONFIG
 export DYLD_LIBRARY_PATH=$ROOT/libs/$CONFIG
 export PATH=$ROOT/bins/$CONFIG:$ROOT/bins/$CONFIG/protobuf:$PATH
-export CFLAGS="-I$ROOT/include -std=c89"
-export LDFLAGS="-L$ROOT/libs/$CONFIG -L$ROOT/libs/$CONFIG/openssl"
+export CFLAGS="-I$ROOT/include -std=gnu99"
+export LDFLAGS="-L$ROOT/libs/$CONFIG"
 export GRPC_PYTHON_BUILD_WITH_CYTHON=1
 export GRPC_PYTHON_ENABLE_CYTHON_TRACING=1
 
