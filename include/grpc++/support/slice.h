fa8b7d3a61f0a3400097e608e073ba320b49fb92
Vijay Pai <vpai@google.com>
2016-01-07 12:12:40 -0800

    Fix one use of grpc instead of gpr

--- include/grpc++/support/slice.h
+++ include/grpc++/support/slice.h
@@ -39,7 +39,7 @@
 
 namespace grpc {
 
-/// A wrapper around \a grpc_slice.
+/// A wrapper around \a gpr_slice.
 ///
 /// A slice represents a contiguous reference counted array of bytes.
 /// It is cheap to take references to a slice, and it is cheap to create a
