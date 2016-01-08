756a36cdacbe057eada8febf73878c0d06af2cd4
David Garcia Quintas <dgq@google.com>
2016-01-08 15:56:21 -0800

    Srcs and hdrs sanity checks skip third_party

--- tools/codegen/core/gen_load_balancing_proto.sh
+++ tools/codegen/core/gen_load_balancing_proto.sh
@@ -2,7 +2,7 @@
 #
 # Example usage:
 #   tools/codegen/core/gen_load_balancing_proto.sh \
-#     src/core/client_config/lb_policies/proto/load_balancer.proto
+#     src/proto/grpc/lb/v0/load_balancer.proto
 
 if [ $# -eq 0 ]; then
   echo "Usage: $0 <load_balancer.proto>"
@@ -26,7 +26,13 @@ pushd "$(dirname $1)" > /dev/null
 
 protoc \
 --plugin=protoc-gen-nanopb="$GRPC_ROOT/third_party/nanopb/generator/protoc-gen-nanopb" \
---nanopb_out='-L#include\ \"third_party/nanopb/pb.h\"'":$GRPC_ROOT/src/core/proto" \
+--nanopb_out='-L#include\ \"third_party/nanopb/pb.h\"'":$GRPC_ROOT/src/core/proto/grpc/lb/v0" \
 "$(basename $1)"
+
+readonly PROTO_BASENAME=$(basename $1 .proto)
+sed -i "s:$PROTO_BASENAME.pb.h:src/core/proto/grpc/lb/v0/$PROTO_BASENAME.pb.h:g" \
+    "$GRPC_ROOT/src/core/proto/grpc/lb/v0/$PROTO_BASENAME.pb.c"
+clang-format -i "$GRPC_ROOT/src/core/proto/grpc/lb/v0/$PROTO_BASENAME.pb.c"
+clang-format -i "$GRPC_ROOT/src/core/proto/grpc/lb/v0/$PROTO_BASENAME.pb.h"
 
 popd > /dev/null
