82a0aabb7a6a4f0852bb02cef3a5f079d386fbac
David Garcia Quintas <dgq@google.com>
2016-01-11 15:51:03 -0800

    added sanity scripts and checks

--- tools/codegen/core/gen_load_balancing_proto.sh
+++ tools/codegen/core/gen_load_balancing_proto.sh
@@ -4,35 +4,47 @@
 #   tools/codegen/core/gen_load_balancing_proto.sh \
 #     src/proto/grpc/lb/v0/load_balancer.proto
 
+set -ex
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 <load_balancer.proto>"
+  echo "Usage: $0 <load_balancer.proto> [output dir]"
   exit 1
+fi
+
+readonly GRPC_ROOT=$PWD
+
+OUTPUT_DIR="$GRPC_ROOT/src/core/proto/grpc/lb/v0"
+if [ $# -eq 2 ]; then
+  mkdir -p "$2"
+  if [ $? != 0 ]; then
+    echo "Error creating output directory $2"
+    exit 2
+  fi
+  OUTPUT_DIR="$2"
 fi
 
 readonly EXPECTED_OPTIONS_FILE_PATH="${1%.*}.options"
 
 if [[ ! -f "$1" ]]; then
   echo "Input proto file '$1' doesn't exist."
-  exit 2
+  exit 3
 fi
 if [[ ! -f "${EXPECTED_OPTIONS_FILE_PATH}" ]]; then
   echo "Expected nanopb options file '${EXPECTED_OPTIONS_FILE_PATH}' missing"
-  exit 3
+  exit 4
 fi
-
-readonly GRPC_ROOT=$PWD
 
 pushd "$(dirname $1)" > /dev/null
 
 protoc \
 --plugin=protoc-gen-nanopb="$GRPC_ROOT/third_party/nanopb/generator/protoc-gen-nanopb" \
---nanopb_out='-L#include\ \"third_party/nanopb/pb.h\"'":$GRPC_ROOT/src/core/proto/grpc/lb/v0" \
+--nanopb_out='-T -L#include\ \"third_party/nanopb/pb.h\"'":$OUTPUT_DIR" \
 "$(basename $1)"
 
 readonly PROTO_BASENAME=$(basename $1 .proto)
 sed -i "s:$PROTO_BASENAME.pb.h:src/core/proto/grpc/lb/v0/$PROTO_BASENAME.pb.h:g" \
-    "$GRPC_ROOT/src/core/proto/grpc/lb/v0/$PROTO_BASENAME.pb.c"
-clang-format -i "$GRPC_ROOT/src/core/proto/grpc/lb/v0/$PROTO_BASENAME.pb.c"
-clang-format -i "$GRPC_ROOT/src/core/proto/grpc/lb/v0/$PROTO_BASENAME.pb.h"
+    "$OUTPUT_DIR/$PROTO_BASENAME.pb.c"
+
+clang-format -style="{BasedOnStyle: Google, Language: Cpp}" -i "$OUTPUT_DIR/$PROTO_BASENAME.pb.c"
+clang-format -style="{BasedOnStyle: Google, Language: Cpp}" -i "$OUTPUT_DIR/$PROTO_BASENAME.pb.h"
 
 popd > /dev/null
