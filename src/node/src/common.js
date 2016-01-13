4cfb01d7bf6e334f7b852cc499f0c5f14289d4a5
murgatroid99 <mlumish@google.com>
2016-01-13 10:05:00 -0800

    Don't modify proto method names in service paths in Node library

--- src/node/src/common.js
+++ src/node/src/common.js
@@ -125,7 +125,7 @@ exports.getProtobufServiceAttrs = function getProtobufServiceAttrs(service) {
   var prefix = '/' + fullyQualifiedName(service) + '/';
   return _.object(_.map(service.children, function(method) {
     return [_.camelCase(method.name), {
-      path: prefix + _.capitalize(method.name),
+      path: prefix + method.name,
       requestStream: method.requestStream,
       responseStream: method.responseStream,
       requestSerialize: serializeCls(method.resolvedRequestType.build()),
