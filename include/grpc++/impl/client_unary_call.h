98c391d84f6cefa08ae109db1a6644d7ef94d9ec
David Garcia Quintas <dgq@google.com>
2016-01-13 16:02:00 -0800

    WIP. Added ChannelInterface

--- include/grpc++/impl/client_unary_call.h
+++ include/grpc++/impl/client_unary_call.h
@@ -35,6 +35,7 @@
 #define GRPCXX_IMPL_CLIENT_UNARY_CALL_H
 
 #include <grpc++/impl/call.h>
+#include <grpc++/impl/codegen/channel_interface.h>
 #include <grpc++/support/config.h>
 #include <grpc++/support/status.h>
 
@@ -47,7 +48,7 @@ class RpcMethod;
 
 // Wrapper that performs a blocking unary call
 template <class InputMessage, class OutputMessage>
-Status BlockingUnaryCall(Channel* channel, const RpcMethod& method,
+Status BlockingUnaryCall(ChannelInterface* channel, const RpcMethod& method,
                          ClientContext* context, const InputMessage& request,
                          OutputMessage* result) {
   CompletionQueue cq;
