269b8be406a447e6544ff3a2a14c23940984bccc
yang-g <yangg@google.com>
2016-01-15 10:46:26 -0800

    add generic test

--- include/grpc++/generic/async_generic_service.h
+++ include/grpc++/generic/async_generic_service.h
@@ -58,9 +58,6 @@ class GenericServerContext GRPC_FINAL : public ServerContext {
 
 class AsyncGenericService GRPC_FINAL {
  public:
-  // TODO(yangg) Once we can add multiple completion queues to the server
-  // in c core, add a CompletionQueue* argument to the ctor here.
-  // TODO(yangg) support methods list.
   AsyncGenericService() : server_(nullptr) {}
   AsyncGenericService(const grpc::string& methods) : server_(nullptr) {}
 
