22fd6b886288fc87531af311acbf3e159a1ec9b7
Craig Tiller <ctiller@google.com>
2016-01-19 13:03:46 -0800

    Eliminate jobset timeouts in some circumstances
    There are cases where we truly don't care how long something takes, and
    some external timeout will kill truly stuck processes, so let's
    eliminate timeouts in those cases

--- tools/buildgen/generate_projects.py
+++ tools/buildgen/generate_projects.py
@@ -85,7 +85,7 @@ for template in templates:
       os.close(tf[0])
       cmd.append(test[out])
     cmd.append(root + '/' + f)
-    jobs.append(jobset.JobSpec(cmd, shortname=out))
+    jobs.append(jobset.JobSpec(cmd, shortname=out, timeout_seconds=None))
 
 jobset.run(jobs, maxjobs=multiprocessing.cpu_count())
 
