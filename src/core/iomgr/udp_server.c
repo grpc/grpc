c4fc0fdabdcfb3bb9eb4cc65f0458337aec40041
Robbie Shade <rjshade@google.com>
2016-01-28 11:05:06 -0500

    Remove unused grpc_udp_server_write method

--- src/core/iomgr/udp_server.c
+++ src/core/iomgr/udp_server.c
@@ -425,15 +425,5 @@ void grpc_udp_server_start(grpc_exec_ctx *exec_ctx, grpc_udp_server *s,
   gpr_mu_unlock(&s->mu);
 }
 
-/* TODO(rjshade): Add a test for this method. */
-void grpc_udp_server_write(server_port *sp, const char *buffer, size_t buf_len,
-                           const struct sockaddr *peer_address) {
-  ssize_t rc;
-  rc = sendto(sp->fd, buffer, buf_len, 0, peer_address, sizeof(peer_address));
-  if (rc < 0) {
-    gpr_log(GPR_ERROR, "Unable to send data: %s", strerror(errno));
-  }
-}
-
 #endif
 #endif
