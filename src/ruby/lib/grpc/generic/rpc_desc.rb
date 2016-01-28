1fafa7c0e6983c24c3cb10f4fc866da1b6a4b481
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2016-01-28 20:45:39 +0100

    Few more fixes to the Ruby installation.
    -) properly depending on libraries.
    -) adding an independent 'loader' class to use instead of require 'grpc/grpc'.

--- src/ruby/lib/grpc/generic/rpc_desc.rb
+++ src/ruby/lib/grpc/generic/rpc_desc.rb
@@ -27,7 +27,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-require 'grpc/grpc'
+require 'grpc/loader'
 
 # GRPC contains the General RPC module.
 module GRPC
