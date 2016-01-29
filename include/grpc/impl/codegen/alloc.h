67d01c85e68ea37cdbf57ea36fed33e4260b3e48
Craig Tiller <craig.tiller@gmail.com>
2016-01-29 07:58:22 -0800

    Add {GPR,GRPC,CENSUS}_API tags to public functions

--- include/grpc/impl/codegen/alloc.h
+++ include/grpc/impl/codegen/alloc.h
@@ -36,6 +36,8 @@
 
 #include <stddef.h>
 
+#include <grpc/impl/codegen/port_platform.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -47,23 +49,23 @@ typedef struct gpr_allocation_functions {
 } gpr_allocation_functions;
 
 /* malloc, never returns NULL */
-void *gpr_malloc(size_t size);
+GPR_API void *gpr_malloc(size_t size);
 /* free */
-void gpr_free(void *ptr);
+GPR_API void gpr_free(void *ptr);
 /* realloc, never returns NULL */
-void *gpr_realloc(void *p, size_t size);
+GPR_API void *gpr_realloc(void *p, size_t size);
 /* aligned malloc, never returns NULL, will align to 1 << alignment_log */
-void *gpr_malloc_aligned(size_t size, size_t alignment_log);
+GPR_API void *gpr_malloc_aligned(size_t size, size_t alignment_log);
 /* free memory allocated by gpr_malloc_aligned */
-void gpr_free_aligned(void *ptr);
+GPR_API void gpr_free_aligned(void *ptr);
 
 /** Request the family of allocation functions in \a functions be used. NOTE
  * that this request will be honored in a *best effort* basis and that no
  * guarantees are made about the default functions (eg, malloc) being called. */
-void gpr_set_allocation_functions(gpr_allocation_functions functions);
+GPR_API void gpr_set_allocation_functions(gpr_allocation_functions functions);
 
 /** Return the family of allocation functions currently in effect. */
-gpr_allocation_functions gpr_get_allocation_functions();
+GPR_API gpr_allocation_functions gpr_get_allocation_functions();
 
 #ifdef __cplusplus
 }
