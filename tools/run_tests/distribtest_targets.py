b5f7397b08de46e5241f7bcf74a4662917a42c0d
Jan Tattermusch <jtattermusch@google.com>
2016-02-05 13:53:47 -0800

    cleanup distribtest Dockerimages

--- tools/run_tests/distribtest_targets.py
+++ tools/run_tests/distribtest_targets.py
@@ -38,13 +38,13 @@ def create_docker_jobspec(name, dockerfile_dir, shell_command, environ={},
   """Creates jobspec for a task running under docker."""
   environ = environ.copy()
   environ['RUN_COMMAND'] = shell_command
+  environ['RELATIVE_COPY_PATH'] = 'test/distrib'
 
   docker_args=[]
   for k,v in environ.iteritems():
     docker_args += ['-e', '%s=%s' % (k, v)]
   docker_env = {'DOCKERFILE_DIR': dockerfile_dir,
-                'DOCKER_RUN_SCRIPT': 'tools/jenkins/docker_run.sh',
-                'RELATIVE_COPY_PATH': 'test/distrib'}
+                'DOCKER_RUN_SCRIPT': 'tools/jenkins/docker_run.sh'}
   jobspec = jobset.JobSpec(
           cmdline=['tools/jenkins/build_and_run_docker.sh'] + docker_args,
           environ=docker_env,
