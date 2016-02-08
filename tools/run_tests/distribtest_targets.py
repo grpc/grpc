513d5c1f6a5197b571905a7ef95a51d0dd375bd4
murgatroid99 <mlumish@google.com>
2016-02-08 14:52:30 -0800

    Removed macos from the docker_suffix list

--- tools/run_tests/distribtest_targets.py
+++ tools/run_tests/distribtest_targets.py
@@ -244,6 +244,6 @@ def targets():
           ] + [
             NodeDistribTest('linux', 'x64', os, version)
             for os in ('wheezy', 'jessie', 'ubuntu1204', 'ubuntu1404',
-                       'ubuntu1504', 'ubuntu1510', 'ubuntu1604', 'macos')
+                       'ubuntu1504', 'ubuntu1510', 'ubuntu1604')
             for version in ('0.10', '0.12', '3', '4', '5')
           ]
