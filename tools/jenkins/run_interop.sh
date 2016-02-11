bf1c8c3810d92d71c5efd2c93777c9ffd42175eb
Adele Zhou <adelez@google.com>
2016-02-11 10:09:31 -0800

    Add one more server.

--- tools/jenkins/run_interop.sh
+++ tools/jenkins/run_interop.sh
@@ -34,4 +34,4 @@ set -ex
 # Enter the gRPC repo root
 cd $(dirname $0)/../..
 
-tools/run_tests/run_interop_tests.py -l all -s all --cloud_to_prod --cloud_to_prod_auth --prod_servers default cloud_gateway gateway_v2 cloud_gateway_v2 --use_docker --http2_interop -t -j 12 $@ || true
+tools/run_tests/run_interop_tests.py -l all -s all --cloud_to_prod --cloud_to_prod_auth --prod_servers default cloud_gateway gateway_v2 cloud_gateway_v2 gateway_v4 --use_docker --http2_interop -t -j 12 $@ || true
