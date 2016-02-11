dbca72fb01f5600de3648a16198001d3c085d2ff
vjpai <vpai@google.com>
2016-02-11 00:18:16 -0800

    init test

--- test/cpp/common/alarm_cpp_test.cc
+++ test/cpp/common/alarm_cpp_test.cc
@@ -75,6 +75,7 @@ TEST(AlarmTest, Cancellation) {
 }  // namespace grpc
 
 int main(int argc, char** argv) {
+  grpc_test_init(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
