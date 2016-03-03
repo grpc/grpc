1dcb1959ceec35aae9ef017a3842f8c6053afe30
Jan Tattermusch <jtattermusch@google.com>
2016-03-03 12:35:08 -0800

    run build_ext in build_python step

--- tools/run_tests/build_python.sh
+++ tools/run_tests/build_python.sh
@@ -46,4 +46,5 @@ tox --notest
 
 $ROOT/.tox/py27/bin/python $ROOT/setup.py build
 $ROOT/.tox/py27/bin/python $ROOT/setup.py build_py
+$ROOT/.tox/py27/bin/python $ROOT/setup.py build_ext --inplace
 $ROOT/.tox/py27/bin/python $ROOT/setup.py gather --test
