0e65fb36f24bceb05b51a9970068d6ba6b1f409d
murgatroid99 <mlumish@google.com>
2016-03-07 17:49:37 -0800

    Node: propagate read errors back down to core

--- src/node/src/client.js
+++ src/node/src/client.js
@@ -149,6 +149,9 @@ function _readsDone(status) {
   if (!status) {
     status = {code: grpc.status.OK, details: 'OK'};
   }
+  if (status.code !== grpc.status.OK) {
+    this.call.cancelWithStatus(status.code, status.details);
+  }
   this.finished = true;
   this.read_status = status;
   this._emitStatusIfDone();
