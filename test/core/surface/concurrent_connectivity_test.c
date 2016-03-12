f09962b0f17dbfddac5ff5f75455e1475c35c164
Craig Tiller <ctiller@google.com>
2016-03-11 20:12:45 -0800

    If we cant check timers due to contention, at least make sure we do a follow up check soon

--- test/core/surface/concurrent_connectivity_test.c
+++ test/core/surface/concurrent_connectivity_test.c
@@ -1,3 +1,36 @@
+/*
+*
+* Copyright 2015-2016, Google Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*
+*     * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above
+* copyright notice, this list of conditions and the following disclaimer
+* in the documentation and/or other materials provided with the
+* distribution.
+*     * Neither the name of Google Inc. nor the names of its
+* contributors may be used to endorse or promote products derived from
+* this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
 #include <stdio.h>
 
 #include <grpc/grpc.h>
@@ -24,9 +57,9 @@ void create_loop_destroy(void* actually_an_int) {
       grpc_connectivity_state state =
           grpc_channel_check_connectivity_state(chan, 1);
       grpc_channel_watch_connectivity_state(chan, state, later_time, cq, NULL);
-      GPR_ASSERT(grpc_completion_queue_next(
-                     cq, GRPC_TIMEOUT_SECONDS_TO_DEADLINE(3), NULL)
-                     .type == GRPC_OP_COMPLETE);
+      GPR_ASSERT(grpc_completion_queue_next(cq,
+                                            GRPC_TIMEOUT_SECONDS_TO_DEADLINE(3),
+                                            NULL).type == GRPC_OP_COMPLETE);
     }
     grpc_channel_destroy(channels[thread_index]);
     grpc_completion_queue_destroy(queues[thread_index]);
