5183553b11b35553a93f3da65110494019025df0
Jan Tattermusch <jtattermusch@google.com>
2016-03-30 11:12:35 -0700

    dont break the mac build

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -478,7 +478,8 @@ class CSharpLanguage(object):
       _check_compiler(self.args.compiler, ['default'])
       if self.platform == 'mac':
         # On Mac, official distribution of mono is 32bit.
-        self._make_options = ['EMBED_OPENSSL=true', 'EMBED_ZLIB=true',
+        # TODO(jtattermusch): EMBED_ZLIB=true currently breaks the mac build
+        self._make_options = ['EMBED_OPENSSL=true'
                               'CFLAGS=-arch i386', 'LDFLAGS=-arch i386']
       else:
         self._make_options = ['EMBED_OPENSSL=true', 'EMBED_ZLIB=true']
