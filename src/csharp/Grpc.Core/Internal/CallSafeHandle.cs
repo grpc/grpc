97a95e462ae0704148197c391a2359cadb165579
Craig Tiller <ctiller@google.com>
2016-03-31 07:46:18 -0700

    Update copyrights

--- src/csharp/Grpc.Core/Internal/CallSafeHandle.cs
+++ src/csharp/Grpc.Core/Internal/CallSafeHandle.cs
@@ -1,5 +1,5 @@
 #region Copyright notice and license
-// Copyright 2015-2016, Google Inc.
+// Copyright 2015, Google Inc.
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without
