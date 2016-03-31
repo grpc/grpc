a8cd6dc55ad9a104a78ea63cf48f0d4e5f9cec0b
murgatroid99 <mlumish@google.com>
2016-03-30 17:05:13 -0700

    Fixed minor bug in metrics server, added metrics client

--- src/node/stress/metrics_client.js
+++ src/node/stress/metrics_client.js
@@ -33,55 +33,29 @@
 
 'use strict';
 
-var _ = require('lodash');
-
 var grpc = require('../../..');
 
 var proto = grpc.load(__dirname + '/../../proto/grpc/testing/metrics.proto');
 var metrics = proto.grpc.testing;
 
-function getGauge(call, callback) {
-  /* jshint validthis: true */
-  // Should be bound to a MetricsServer object
-  var name = call.request.name;
-  if (this.gauges.hasOwnProperty(name)) {
-    callback(null, _.assign({name: name}, this.gauges[name]()));
+function main() {
+  var parseArgs = require('minimist');
+  var argv = parseArgs(process.argv, {
+    string: 'metrics_server_address',
+    boolean: 'total_only'
+  });
+  var client = new metrics.MetricsService(argv.metrics_server_address,
+                                          grpc.credentials.createInsecure());
+  if (argv.total_only) {
+    client.getGauge({name: 'qps'}, function(err, data) {
+      console.log(data.name + ':', data.long_value);
+    });
   } else {
-    callback({code: grpc.status.NOT_FOUND,
-              details: 'No such gauge: ' + name});
+    var call = client.getAllGauges({});
+    call.on('data', function(data) {
+      console.log(data.name + ':', data.long_value);
+    });
   }
 }
 
-function getAllGauges(call) {
-  /* jshint validthis: true */
-  // Should be bound to a MetricsServer object
-  _.each(this.gauges, function(getter, name) {
-    call.write(_.assign({name: name}, getter()));
-  });
-  call.end();
-}
-
-function MetricsServer(port) {
-  var server = new grpc.Server();
-  server.addProtoService(metrics.MetricsService.service, {
-    getGauge: _.bind(getGauge, this),
-    getAllGauges: _.bind(getAllGauges, this)
-  });
-  server.bind('localhost:' + port, grpc.ServerCredentials.createInsecure());
-  this.server = server;
-  this.gauges = {};
-}
-
-MetricsServer.prototype.start = function() {
-  this.server.start();
-}
-
-MetricsServer.prototype.registerGauge = function(name, getter) {
-  this.gauges[name] = getter;
-};
-
-MetricsServer.prototype.shutdown = function() {
-  this.server.forceShutdown();
-};
-
-module.exports = MetricsServer;
+main();
