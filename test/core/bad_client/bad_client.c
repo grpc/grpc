2839edd222d980c3735d1ae1fa5c1baef1446d6c
Craig Tiller <ctiller@google.com>
2016-04-04 08:37:59 -0700

    Fix registration in test

--- test/core/bad_client/bad_client.c
+++ test/core/bad_client/bad_client.c
@@ -107,9 +107,10 @@ void grpc_run_bad_client_test(grpc_bad_client_server_side_validator validator,
   gpr_event_init(&a.done_write);
   a.validator = validator;
   grpc_server_register_completion_queue(a.server, a.cq, NULL);
-  a.registered_method = grpc_server_register_method(
-      a.server, GRPC_BAD_CLIENT_REGISTERED_METHOD,
-      GRPC_BAD_CLIENT_REGISTERED_HOST, GRPC_SRM_PAYLOAD_NONE, 0);
+  a.registered_method =
+      grpc_server_register_method(a.server, GRPC_BAD_CLIENT_REGISTERED_METHOD,
+                                  GRPC_BAD_CLIENT_REGISTERED_HOST,
+                                  GRPC_SRM_PAYLOAD_READ_INITIAL_BYTE_BUFFER, 0);
   grpc_server_start(a.server);
   transport = grpc_create_chttp2_transport(&exec_ctx, NULL, sfd.server, 0);
   server_setup_transport(&a, transport);
