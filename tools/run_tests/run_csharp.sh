a8ef9c39254c4b37f9b7bdf9c4d03766f1d8818e
Jan Tattermusch <jtattermusch@google.com>
2016-04-08 17:48:40 -0700

    use correct non-overwrite flag for cp

--- tools/run_tests/run_csharp.sh
+++ tools/run_tests/run_csharp.sh
@@ -34,7 +34,7 @@ CONFIG=${CONFIG:-opt}
 NUNIT_CONSOLE="mono packages/NUnit.ConsoleRunner.3.2.0/tools/nunit3-console.exe"
 
 # nunit3-console fails if nunit.framework.dll is not found.
-cp -u src/csharp/packages/NUnit.3.2.0/lib/net45/nunit.framework.dll src/csharp/packages/NUnit.ConsoleRunner.3.2.0/tools/
+cp -n src/csharp/packages/NUnit.3.2.0/lib/net45/nunit.framework.dll src/csharp/packages/NUnit.ConsoleRunner.3.2.0/tools/
 
 # change to gRPC repo root
 cd $(dirname $0)/../..
