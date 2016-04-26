0507f677d679e4d2d1b8fd918156a7d65fb94e27
Rinat <nutsgamedev@gmail.com>
2016-04-26 22:45:38 +0500

    improve nodejs example

--- examples/node/greeter_server.js
+++ examples/node/greeter_server.js
@@ -31,16 +31,18 @@
  *
  */
 
-var PROTO_PATH = __dirname + '/../protos/helloworld.proto';
+const PROTO_PATH = __dirname + '/../protos/helloworld.proto';
+const SERVER_ADDRESS = '0.0.0.0:50051';
 
-var grpc = require('grpc');
-var hello_proto = grpc.load(PROTO_PATH).helloworld;
+const grpc = require('grpc');
+const hello_proto = grpc.load(PROTO_PATH).helloworld;
 
 /**
  * Implements the SayHello RPC method.
  */
 function sayHello(call, callback) {
   callback(null, {message: 'Hello ' + call.request.name});
+  console.log(`Invoke sayHello() with request.name: ${call.request.name}`);
 }
 
 /**
@@ -48,10 +50,11 @@ function sayHello(call, callback) {
  * sample server port
  */
 function main() {
-  var server = new grpc.Server();
+  const server = new grpc.Server();
   server.addProtoService(hello_proto.Greeter.service, {sayHello: sayHello});
-  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+  server.bind(SERVER_ADDRESS, grpc.ServerCredentials.createInsecure());
   server.start();
+  console.log(`Server start on ${SERVER_ADDRESS}`);
 }
 
 main();
