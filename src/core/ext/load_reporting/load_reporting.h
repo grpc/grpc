b41363e88eb78123926e760e45a981add977140f
David Garcia Quintas <dgq@google.com>
2016-04-29 00:41:21 -0700

    all good! missing end2end test for new lr filter

--- src/core/ext/load_reporting/load_reporting.h
+++ src/core/ext/load_reporting/load_reporting.h
@@ -31,35 +31,34 @@
  *
  */
 
-#ifndef GRPC_CORE_LIB_LOAD_REPORTING_LOAD_REPORTING_H
-#define GRPC_CORE_LIB_LOAD_REPORTING_LOAD_REPORTING_H
+#ifndef GRPC_CORE_EXT_LOAD_REPORTING_LOAD_REPORTING_H
+#define GRPC_CORE_EXT_LOAD_REPORTING_LOAD_REPORTING_H
 
 #include "src/core/lib/iomgr/closure.h"
 #include "src/core/lib/surface/call.h"
+
+typedef struct grpc_load_reporting_data grpc_load_reporting_data;
 
 /** Custom function to be called by the load reporting filter.
  *
  * The \a data pointer is the same as the one passed to \a
  * grpc_load_reporting_init. \a stats are the final per-call statistics gathered
  * by the gRPC runtime. */
-typedef void (*load_reporting_fn)(void *data, const grpc_call_stats *stats);
+typedef void (*grpc_load_reporting_fn)(void *data,
+                                       const grpc_call_stats *stats);
 
 /** Register \a fn as the function to be invoked by the load reporting filter,
  * passing \a data as its namesake argument. To be called only from a plugin
  * init function. */
-void grpc_load_reporting_init(load_reporting_fn fn, void *data);
+grpc_load_reporting_data *grpc_load_reporting_create(grpc_load_reporting_fn fn,
+                                                     void *data);
 
-/** Takes care of freeing the memory allocated for \a data (see \a
- * grpc_load_reporting_init), if any. To be called only from a plugin destroy
- * function. */
-void grpc_load_reporting_destroy();
+// XXX
+void grpc_load_reporting_destroy(grpc_load_reporting_data *lrd);
 
 /** Invoke the function registered by \a grpc_load_reporting_init, passing it \a
  * stats as one of the arguments (see \a load_reporting_fn). */
-void grpc_load_reporting_call(const grpc_call_stats *stats);
+void grpc_load_reporting_call(grpc_load_reporting_data *lrd,
+                              const grpc_call_stats *stats);
 
-/** Returns the custom load reporting data, as registered in \a
- * grpc_load_reporting_init. */
-void *grpc_load_reporting_data();
-
-#endif /* GRPC_CORE_LIB_LOAD_REPORTING_LOAD_REPORTING_H */
+#endif /* GRPC_CORE_EXT_LOAD_REPORTING_LOAD_REPORTING_H */
