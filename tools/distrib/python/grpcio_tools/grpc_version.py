955887928f1ac6d9b39599d14b6b7a522db026d9
Masood Malekghassemi <atash@google.com>
2016-05-03 12:05:13 -0700

    Keep grpcio_tools version in sync with rest of project

--- tools/distrib/python/grpcio_tools/grpc_version.py
+++ tools/distrib/python/grpcio_tools/grpc_version.py
@@ -1,4 +1,4 @@
-# Copyright 2016, Google Inc.
+# Copyright 2015, Google Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -27,57 +27,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from distutils import extension
-import os
-import os.path
-import sys
+# AUTO-GENERATED FROM `$REPO_ROOT/templates/tools/distrib/python/grpcio_tools/grpc_version.py.template`!!!
 
-import setuptools
-from setuptools.command import build_ext
-
-# TODO(atash) add flag to disable Cython use
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.abspath('.'))
-
-import protoc_lib_deps
-import grpc_version
-
-def protoc_ext_module():
-  plugin_sources = [
-      'grpc/protoc/main.cc',
-      'grpc_root/src/compiler/python_generator.cc'] + [
-      os.path.join('third_party/protobuf/src', cc_file)
-      for cc_file in protoc_lib_deps.CC_FILES]
-  plugin_ext = extension.Extension(
-      name='grpc.protoc.protoc_compiler',
-      sources=['grpc/protoc/protoc_compiler.pyx'] + plugin_sources,
-      include_dirs=[
-          '.',
-          'grpc_root',
-          'grpc_root/include',
-          'third_party/protobuf/src',
-      ],
-      language='c++',
-      define_macros=[('HAVE_PTHREAD', 1)],
-      extra_compile_args=['-lpthread', '-frtti', '-std=c++11'],
-  )
-  return plugin_ext
-
-def maybe_cythonize(exts):
-  from Cython import Build
-  return Build.cythonize(exts)
-
-setuptools.setup(
-  name='grpcio_tools',
-  version=grpc_version.VERSION,
-  license='',
-  ext_modules=maybe_cythonize([
-      protoc_ext_module(),
-  ]),
-  packages=setuptools.find_packages('.'),
-  namespace_packages=['grpc'],
-  install_requires=[
-    'protobuf>=3.0.0a3',
-  ],
-)
+VERSION='0.14.0.dev0'
