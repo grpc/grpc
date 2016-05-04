c84ed6813e24b6d64d2eea7e39188ddae11528c6
Yuchen Zeng <zyc@google.com>
2016-05-04 16:30:11 -0700

    Proto server reflection

---
+++ extensions/reflection/proto_server_reflection.h
@@ -0,0 +1,92 @@
+/*
+ *
+ * Copyright 2015, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef GRPC_EXTENSIONS_REFLECTION_PROTO_SERVER_REFLECTION_H
+#define GRPC_EXTENSIONS_REFLECTION_PROTO_SERVER_REFLECTION_H
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <grpc++/grpc++.h>
+
+#include <grpc++/impl/reflection.grpc.pb.h>
+
+namespace grpc {
+
+class ProtoServerReflection GRPC_FINAL
+    : public reflection::v1alpha::ServerReflection::Service {
+ public:
+  ProtoServerReflection();
+
+  ProtoServerReflection(const Server* server);
+
+  void SetServiceList(const std::vector<grpc::string>* services);
+
+  Status ListService(
+      ServerContext* context, const reflection::v1alpha::EmptyRequest* request,
+      reflection::v1alpha::ListServiceResponse* response) GRPC_OVERRIDE;
+
+  Status GetFileByName(
+      ServerContext* context,
+      const reflection::v1alpha::FileNameRequest* request,
+      reflection::v1alpha::FileDescriptorProtoResponse* response) GRPC_OVERRIDE;
+
+  Status GetFileContainingSymbol(
+      ServerContext* context, const reflection::v1alpha::SymbolRequest* request,
+      reflection::v1alpha::FileDescriptorProtoResponse* response) GRPC_OVERRIDE;
+
+  Status GetFileContainingExtension(
+      ServerContext* context,
+      const reflection::v1alpha::ExtensionRequest* request,
+      reflection::v1alpha::FileDescriptorProtoResponse* response) GRPC_OVERRIDE;
+
+  Status GetAllExtensionNumbers(
+      ServerContext* context, const reflection::v1alpha::TypeRequest* request,
+      reflection::v1alpha::ExtensionNumberResponse* response) GRPC_OVERRIDE;
+
+ private:
+  void FillFileDescriptorProtoResponse(
+      const google::protobuf::FileDescriptor* file_desc,
+      reflection::v1alpha::FileDescriptorProtoResponse* response);
+
+  const google::protobuf::DescriptorPool* descriptor_pool_;
+  const std::vector<string>* services_;
+};
+
+}  // namespace grpc
+
+#endif  // GRPC_EXTENSIONS_REFLECTION_PROTO_SERVER_REFLECTION_H
