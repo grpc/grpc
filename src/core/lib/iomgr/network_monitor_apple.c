3629b0213d5f3343c1de24ccd3570142fab1990b
Yuchen Zeng <zyc@google.com>
2016-05-10 17:01:42 -0700

    Network monitor added

---
+++ src/core/lib/iomgr/network_monitor_apple.c
@@ -0,0 +1,157 @@
+/*
+ *
+ * Copyright 2016, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <grpc/support/port_platform.h>
+
+#ifdef GPR_APPLE_CONFIG
+
+#include "src/core/lib/iomgr/network_monitor.h"
+
+#include <SystemConfiguration/SystemConfiguration.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <grpc/support/sync.h>
+
+struct grpc_connectivity_monitor {
+  gpr_mu mu;
+  dispatch_queue_t dispatch_queue;
+  SCNetworkReachabilityRef reachability_ref;
+  void (*loss_connection_handler)(void);
+};
+
+static struct grpc_connectivity_monitor monitor;
+
+static bool is_host_reachable(SCNetworkReachabilityFlags flags) {
+  return !!(flags & kSCNetworkReachabilityFlagsReachable) &&
+  !(flags & kSCNetworkReachabilityFlagsInterventionRequired) &&
+  !(flags & kSCNetworkReachabilityFlagsConnectionOnDemand);
+}
+
+static void reachability_callback(SCNetworkReachabilityRef target,
+                                        SCNetworkReachabilityFlags flags,
+                                        void* info) {
+  struct grpc_connectivity_monitor* monitor = (struct grpc_connectivity_monitor *)info;
+  if (!is_host_reachable(flags)) {
+    monitor->loss_connection_handler();
+  }
+}
+
+static bool is_monitor_initialized(struct grpc_connectivity_monitor* monitor) {
+  return monitor != NULL && monitor->reachability_ref != NULL &&
+                     monitor->dispatch_queue != NULL &&
+                     monitor->loss_connection_handler != NULL;
+}
+
+static bool start_monitor(struct grpc_connectivity_monitor* monitor) {
+  if (monitor == NULL) {
+    return false;
+  }
+  SCNetworkReachabilityContext context = {
+    .version = 0,
+    .info = (void*)monitor,
+  };
+  gpr_mu_lock(&monitor->mu);
+  bool result = is_monitor_initialized(monitor) &&
+         SCNetworkReachabilitySetCallback(monitor->reachability_ref, reachability_callback, &context) &&
+         SCNetworkReachabilitySetDispatchQueue(monitor->reachability_ref, monitor->dispatch_queue);
+  gpr_mu_unlock(&monitor->mu);
+  return result;
+}
+
+static bool stop_monitor(struct grpc_connectivity_monitor* monitor) {
+  if (monitor == NULL) {
+    return false;
+  }
+  gpr_mu_lock(&monitor->mu);
+  bool result =  is_monitor_initialized(monitor) &&
+         SCNetworkReachabilitySetCallback(monitor->reachability_ref, NULL, NULL) &&
+         SCNetworkReachabilitySetDispatchQueue(monitor->reachability_ref, NULL);
+  gpr_mu_unlock(&monitor->mu);
+  return result;
+}
+
+static bool init_connectivity_monitor(struct grpc_connectivity_monitor* monitor, const char* addr, void (*handler)(void)) {
+  gpr_mu_lock(&monitor->mu);
+  if (monitor == NULL || monitor->reachability_ref != NULL) {
+    gpr_mu_unlock(&monitor->mu);
+    return false;
+  }
+  if (!(monitor->dispatch_queue = dispatch_get_main_queue())) {
+    gpr_mu_unlock(&monitor->mu);
+    return false;
+  }
+  if (!(monitor->reachability_ref = SCNetworkReachabilityCreateWithName(NULL, addr))) {
+    monitor->dispatch_queue = NULL;
+    gpr_mu_lock(&monitor->mu);
+    return false;
+  }
+  monitor->loss_connection_handler = handler;
+  gpr_mu_unlock(&monitor->mu);
+  return true;
+}
+
+static void destory_connectivity_monitor(struct grpc_connectivity_monitor* monitor) {
+  gpr_mu_lock(&monitor->mu);
+  if (monitor == NULL) {
+    gpr_mu_unlock(&monitor->mu);
+    return;
+  }
+  if (monitor->reachability_ref != NULL) {
+    CFRelease(monitor->reachability_ref);
+  }
+  monitor->reachability_ref = NULL;
+  monitor->dispatch_queue = NULL;
+  monitor->loss_connection_handler = NULL;
+  gpr_mu_unlock(&monitor->mu);
+}
+
+bool grpc_start_connectivity_monitor(const char* addr, void (*handler)(void)) {
+  init_connectivity_monitor(&monitor, addr, handler);
+  SCNetworkReachabilityFlags flags;
+  if (SCNetworkReachabilityGetFlags(monitor.reachability_ref, &flags))
+    reachability_callback(monitor.reachability_ref, flags, &monitor);
+  if (!start_monitor(&monitor)) {
+    destory_connectivity_monitor(&monitor);
+    return false;
+  }
+  return true;
+}
+
+bool grpc_stop_connectivity_monitor() {
+  if (!stop_monitor(&monitor)) {
+    return false;
+  }
+  destory_connectivity_monitor(&monitor);
+  return true;
+}
+
+#endif /* GPR_APPLE_CONFIG */
