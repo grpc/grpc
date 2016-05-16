fcbe0b64b9d175ecbf88b9ea366fd84db29f18ca
David Klempner <klempner@google.com>
2016-05-16 16:43:39 -0700

    Run clang-format against port_posix.c and port_windows.c

--- test/core/util/port_windows.c
+++ test/core/util/port_windows.c
@@ -240,8 +240,6 @@ int grpc_pick_unused_port_or_die(void) {
   return port;
 }
 
-void grpc_recycle_unused_port(int port) {
-  GPR_ASSERT(free_chosen_port(port));
-}
+void grpc_recycle_unused_port(int port) { GPR_ASSERT(free_chosen_port(port)); }
 
 #endif /* GPR_WINSOCK_SOCKET && GRPC_TEST_PICK_PORT */
