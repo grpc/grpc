4a3ac3121fce8817025173afd9990e3f7f56c123
thinkerou <thinkerou@gmail.com>
2016-05-19 00:13:43 +0800

    revert

--- src/core/lib/security/secure_endpoint.c
+++ src/core/lib/security/secure_endpoint.c
@@ -157,12 +157,8 @@ static void on_read(grpc_exec_ctx *exec_ctx, void *user_data, bool success) {
 
   /* TODO(yangg) check error, maybe bail out early */
   for (i = 0; i < ep->source_buffer.count; i++) {
-    if (ep->protector == NULL || cur == NULL) break;
- 
     gpr_slice encrypted = ep->source_buffer.slices[i];
     uint8_t *message_bytes = GPR_SLICE_START_PTR(encrypted);
-    if (message_bytes == NULL) break;
-
     size_t message_size = GPR_SLICE_LENGTH(encrypted);
 
     while (message_size > 0 || keep_looping) {
@@ -181,7 +177,6 @@ static void on_read(grpc_exec_ctx *exec_ctx, void *user_data, bool success) {
       message_bytes += processed_message_size;
       message_size -= processed_message_size;
       cur += unprotected_buffer_size_written;
-      if (message_bytes == NULL || cur == NULL) break;
 
       if (cur == end) {
         flush_read_staging_buffer(ep, &cur, &end);
