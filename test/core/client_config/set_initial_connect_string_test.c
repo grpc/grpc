1f24af879c507927203f11301e6ff2439bdbeff8
Yuchen Zeng <zyc@google.com>
2016-06-10 13:15:07 -0700

    Restricted the check to GUNC

--- test/core/client_config/set_initial_connect_string_test.c
+++ test/core/client_config/set_initial_connect_string_test.c
@@ -173,7 +173,7 @@ static void actually_poll_server(void *arg) {
     bool done = gpr_atm_acq_load(&state.done_atm) != 0;
     gpr_timespec time_left =
         gpr_time_sub(deadline, gpr_now(GPR_CLOCK_REALTIME));
-    gpr_log(GPR_DEBUG, "done=%d, time_left=%" PRIuPTR ".%09d", done,
+    gpr_log(GPR_DEBUG, "done=%d, time_left=%" PRId64 ".%09" PRId32, done,
             time_left.tv_sec, time_left.tv_nsec);
     if (done || gpr_time_cmp(time_left, gpr_time_0(GPR_TIMESPAN)) < 0) {
       break;
