69dc5a90c82484a2b799fcb687e5ae1a1534a116
Craig Tiller <ctiller@google.com>
2016-06-16 10:31:32 -0700

    Add file extension

--- vsprojects/build_protos.bat
+++ vsprojects/build_protos.bat
@@ -30,29 +30,32 @@
 @rem enter repo root
 cd /d %~dp0\..
 
-%PROTOC% src/proto/grpc/lb/v1/load_balancer --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/lb/v1/load_balancer --cpp_out=./
-%PROTOC% src/proto/grpc/testing/compiler_test --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/compiler_test --cpp_out=./
-%PROTOC% src/proto/grpc/testing/control --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/control --cpp_out=./
-%PROTOC% src/proto/grpc/testing/duplicate/echo_duplicate --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/duplicate/echo_duplicate --cpp_out=./
-%PROTOC% src/proto/grpc/testing/echo --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/echo --cpp_out=./
-%PROTOC% src/proto/grpc/testing/echo_messages --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/echo_messages --cpp_out=./
-%PROTOC% src/proto/grpc/testing/empty --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/empty --cpp_out=./
-%PROTOC% src/proto/grpc/testing/messages --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/messages --cpp_out=./
-%PROTOC% src/proto/grpc/testing/metrics --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/metrics --cpp_out=./
-%PROTOC% src/proto/grpc/testing/payloads --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/payloads --cpp_out=./
-%PROTOC% src/proto/grpc/testing/services --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/services --cpp_out=./
-%PROTOC% src/proto/grpc/testing/stats --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/stats --cpp_out=./
-%PROTOC% src/proto/grpc/testing/test --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
-%PROTOC% src/proto/grpc/testing/test --cpp_out=./
+set PROTOC=protoc
+set GRPC_CPP_PLUGIN_PATH=grpc_cpp_plugin
+
+%PROTOC% src/proto/grpc/lb/v1/load_balancer.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/lb/v1/load_balancer.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/compiler_test.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/compiler_test.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/control.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/control.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/duplicate/echo_duplicate.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/duplicate/echo_duplicate.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/echo.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/echo.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/echo_messages.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/echo_messages.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/empty.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/empty.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/messages.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/messages.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/metrics.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/metrics.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/payloads.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/payloads.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/services.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/services.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/stats.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/stats.proto --cpp_out=./
+%PROTOC% src/proto/grpc/testing/test.proto --grpc_out=./ --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN_PATH%
+%PROTOC% src/proto/grpc/testing/test.proto --cpp_out=./
