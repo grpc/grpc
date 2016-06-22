82393efe0c930f7756e9d0e427d2e9ae61e0a88e
Yuchen Zeng <zyc@google.com>
2016-06-22 10:29:17 -0700

    Fix run_test.py

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -604,7 +604,7 @@ class ObjCLanguage(object):
     return [self.config.job_spec(['src/objective-c/tests/run_tests.sh'], None,
                                   environ=_FORCE_ENVIRON_FOR_WRAPPERS),
             self.config.job_spec(['src/objective-c/tests/build_example_test.sh'],
-                                 timeout_seconds=15*60, None,
+                                 None, timeout_seconds=15*60,
                                  environ=_FORCE_ENVIRON_FOR_WRAPPERS)]
 
   def pre_build_steps(self):
