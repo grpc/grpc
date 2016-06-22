3131c269c14f97294ebf8b6e3d1a235d4acf3317
Sree Kuchibhotla <sreek@google.com>
2016-06-21 17:28:28 -0700

    Integrate with unified error reporting

--- test/core/iomgr/ev_epoll_linux_test.c
+++ test/core/iomgr/ev_epoll_linux_test.c
@@ -88,7 +88,8 @@ static void test_pollset_init(test_pollset *pollsets, int num_pollsets) {
   }
 }
 
-static void destroy_pollset(grpc_exec_ctx *exec_ctx, void *p, bool success) {
+static void destroy_pollset(grpc_exec_ctx *exec_ctx, void *p,
+                            grpc_error *error) {
   grpc_pollset_destroy(p);
 }
 
