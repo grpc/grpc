f95a4898abd5888c34b31354cdbe9a909958aa08
Yuchen Zeng <zyc@google.com>
2016-06-21 23:27:46 -0700

    Fixed format issues

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -603,8 +603,8 @@ class ObjCLanguage(object):
   def test_specs(self):
     return [self.config.job_spec(['src/objective-c/tests/run_tests.sh'], None,
                                   environ=_FORCE_ENVIRON_FOR_WRAPPERS),
-            self.config.job_spec(['src/objective-c/tests/build_example_test.sh'], None,
-                                  environ=_FORCE_ENVIRON_FOR_WRAPPERS)]
+            self.config.job_spec(['src/objective-c/tests/build_example_test.sh'],
+                                 None, environ=_FORCE_ENVIRON_FOR_WRAPPERS)]
 
   def pre_build_steps(self):
     return []
