378f46ef4073dc408ac30fa70c9587d322541b21
thinkerou <thinkerou@gmail.com>
2016-06-29 23:05:58 +0800

    Fix spell rpc_error_call error

--- src/python/grpcio/grpc/beta/_client_adaptations.py
+++ src/python/grpcio/grpc/beta/_client_adaptations.py
@@ -67,7 +67,7 @@ def _abortion(rpc_error_call):
   error_kind = face.Abortion.Kind.LOCAL_FAILURE if pair is None else pair[0]
   return face.Abortion(
       error_kind, rpc_error_call.initial_metadata(),
-      rpc_error_call.trailing_metadata(), code, rpc_error_code.details())
+      rpc_error_call.trailing_metadata(), code, rpc_error_call.details())
 
 
 def _abortion_error(rpc_error_call):
@@ -523,7 +523,7 @@ class _DynamicStub(face.DynamicStub):
   def __init__(self, generic_stub, group, cardinalities):
     self._generic_stub = generic_stub
     self._group = group
-    self._cardinalities = cardinalities
+    self._cardinalities = cardinalities or {}
 
   def __getattr__(self, attr):
     method_cardinality = self._cardinalities.get(attr)
