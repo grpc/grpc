72376da8ba85cefadd552d3213318f789a38d6ed
Craig Tiller <ctiller@google.com>
2016-07-12 17:14:37 -0700

    Get json_run_localhost working again

--- test/cpp/qps/gen_build_yaml.py
+++ test/cpp/qps/gen_build_yaml.py
@@ -47,7 +47,8 @@ def _scenario_json_string(scenario_json):
   # tweak parameters to get fast test times
   scenario_json['warmup_seconds'] = 1
   scenario_json['benchmark_seconds'] = 10
-  return json.dumps(scenario_config.remove_nonproto_fields(scenario_json))
+  scenarios_json = {'scenarios': [scenario_config.remove_nonproto_fields(scenario_json)]}
+  return json.dumps(scenarios_json)
 
 def threads_of_type(scenario_json, path):
   d = scenario_json
@@ -72,7 +73,7 @@ print yaml.dump({
     {
       'name': 'json_run_localhost',
       'shortname': 'json_run_localhost:%s' % scenario_json['name'],
-      'args': ['--scenario_json',
+      'args': ['--scenarios_json',
                pipes.quote(_scenario_json_string(scenario_json))],
       'ci_platforms': ['linux', 'mac', 'posix', 'windows'],
       'platforms': ['linux', 'mac', 'posix', 'windows'],
