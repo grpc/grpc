d1d528dab1dc25f7b7fbf2a6ea8e4d4e4ed656f7
thinkerou <thinkerou@gmail.com>
2016-07-15 17:02:30 +0800

    unify the version of six and delete repeated six string

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ PACKAGE_DIRECTORIES = {
 }
 
 INSTALL_REQUIRES = (
-    'six>=1.5.2',
+    'six>=1.10',
     'enum34>=1.0.4',
     'futures>=2.2.0',
     # TODO(atash): eventually split the grpcio package into a metapackage
@@ -188,7 +188,6 @@ INSTALL_REQUIRES = (
 SETUP_REQUIRES = INSTALL_REQUIRES + (
     'sphinx>=1.3',
     'sphinx_rtd_theme>=0.1.8',
-    'six>=1.10',
 )
 
 COMMAND_CLASS = {
