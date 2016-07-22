a002c9e9a34913a01b0498ec1072a403a853d3ea
Jan Tattermusch <jtattermusch@google.com>
2016-07-21 18:42:43 -0700

    update protobuf to v3.0.0-beta4

--- tools/run_tests/sanity/check_submodules.sh
+++ tools/run_tests/sanity/check_submodules.sh
@@ -45,7 +45,7 @@ cat << EOF | awk '{ print $1 }' | sort > $want_submodules
  05b155ff59114735ec8cd089f669c4c3d8f59029 third_party/gflags (v2.1.0-45-g05b155f)
  c99458533a9b4c743ed51537e25989ea55944908 third_party/googletest (release-1.7.0)
  f8ac463766281625ad710900479130c7fcb4d63b third_party/nanopb (nanopb-0.3.4-29-gf8ac463)
- bdeb215cab2985195325fcd5e70c3fa751f46e0f third_party/protobuf (v3.0.0-beta-3.3)
+ 56855f6f002eeee8cf03021eaf2ece2adff2a297 third_party/protobuf (v3.0.0-beta-4)
  50893291621658f355bc5b4d450a8d06a563053d third_party/zlib (v1.2.8)
 EOF
 
