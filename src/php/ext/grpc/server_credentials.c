5efb9f27bfeab63e425839b9160e49149e2e9c28
thinkerou <thinkerou@gmail.com>
2016-07-29 14:28:42 +0800

    update the comments as param and type

--- src/php/ext/grpc/server_credentials.c
+++ src/php/ext/grpc/server_credentials.c
@@ -86,9 +86,9 @@ zval *grpc_php_wrap_server_credentials(grpc_server_credentials
 
 /**
  * Create SSL credentials.
- * @param string pem_root_certs PEM encoding of the server root certificates
- * @param string pem_private_key PEM encoding of the client's private key
- * @param string pem_cert_chain PEM encoding of the client's certificate chain
+ * @param string $pem_root_certs PEM encoding of the server root certificates
+ * @param array $array The array of PEM encoding of the client's certificates
+ * @param bool $force_client_auth If request the client certificate (optional)
  * @return Credentials The new SSL credentials object
  */
 PHP_METHOD(ServerCredentials, createSsl) {
