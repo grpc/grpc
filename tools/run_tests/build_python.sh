f32e71eaca170d182f104b83c761ea22b6783d61
Ken Payson <kpayson@google.com>
2016-08-02 11:11:23 -0700

    Downgrade docker setuptools

--- tools/run_tests/build_python.sh
+++ tools/run_tests/build_python.sh
@@ -168,7 +168,9 @@ pip_install_dir() {
   cd $PWD
 }
 
-$VENV_PYTHON -m pip install --upgrade pip setuptools
+$VENV_PYTHON -m pip install --upgrade pip
+# TODO(https://github.com/pypa/setuptools/issues/709) get the latest setuptools
+$VENV_PYTHON -m pip install setuptools==25.1.1
 $VENV_PYTHON -m pip install cython
 pip_install_dir $ROOT
 $VENV_PYTHON $ROOT/tools/distrib/python/make_grpcio_tools.py
