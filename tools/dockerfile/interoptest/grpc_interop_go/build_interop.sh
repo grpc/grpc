45fa6323dbcfd806183eb8380251768406652d4c
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2016-08-05 20:19:24 +0200

    That won't work properly. Falling back on fixing the list of dependencies.

--- tools/dockerfile/interoptest/grpc_interop_go/build_interop.sh
+++ tools/dockerfile/interoptest/grpc_interop_go/build_interop.sh
@@ -31,12 +31,21 @@
 # Builds Go interop server and client in a base image.
 set -e
 
-go get google.golang.org/grpc/interop
 
 # Clone just the grpc-go source code without any dependencies.
 # We are cloning from a local git repo that contains the right revision
 # to test instead of using "go get" to download from Github directly.
 git clone --recursive /var/local/jenkins/grpc-go src/google.golang.org/grpc
+
+# Get dependencies from GitHub
+# NOTE: once grpc-go dependencies change, this needs to be updated manually
+# but we don't expect this to happen any time soon.
+go get github.com/golang/protobuf/proto
+go get golang.org/x/net/context
+go get golang.org/x/net/trace
+go get golang.org/x/oauth2
+go get golang.org/x/oauth2/google
+go get google.golang.org/cloud
 
 # copy service account keys if available
 cp -r /var/local/jenkins/service_account $HOME || true
