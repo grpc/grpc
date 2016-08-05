43b4bfd02b16e78030cf5c91ae310f1d964d56fb
Yuchen Zeng <zyc@google.com>
2016-08-05 10:47:10 -0700

    Revert unnecessary changes

--- examples/cpp/helloworld/greeter_client.cc
+++ examples/cpp/helloworld/greeter_client.cc
@@ -88,7 +88,7 @@ int main(int argc, char** argv) {
   // localhost at port 50051). We indicate that the channel isn't authenticated
   // (use of InsecureChannelCredentials()).
   GreeterClient greeter(grpc::CreateChannel(
-      "ip6-localhost:50051", grpc::InsecureChannelCredentials()));
+      "localhost:50051", grpc::InsecureChannelCredentials()));
   std::string user("world");
   std::string reply = greeter.SayHello(user);
   std::cout << "Greeter received: " << reply << std::endl;
