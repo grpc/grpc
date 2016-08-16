9bf906234e6e1245e54185a84f132e1fe462757e
Yuchen Zeng <zyc@google.com>
2016-08-15 17:28:13 -0700

    Clang-format

--- test/cpp/util/cli_credentials.h
+++ test/cpp/util/cli_credentials.h
@@ -42,7 +42,7 @@ namespace testing {
 
 class CliCredentials {
  public:
-  virtual ~CliCredentials() {};
+  virtual ~CliCredentials(){};
   virtual std::shared_ptr<grpc::ChannelCredentials> GetCredentials() const;
   virtual const grpc::string GetCredentialUsage() const;
 };
