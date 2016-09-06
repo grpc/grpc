08a261a2cce88974e8fd600012bb15aa64b5fb35
Vizerai <jsking@google.com>
2016-09-06 11:29:03 -0700

    update

--- test/core/census/trace_context_test.c
+++ test/core/census/trace_context_test.c
@@ -47,20 +47,14 @@
 #include "third_party/nanopb/pb_decode.h"
 #include "src/core/ext/census/gen/trace_context.pb.h"
 
-bool validate_context(uint8_t* buffer, size_t buf_size) {
-  google_trace_TraceContext ctxt1 = google_trace_TraceContext_init_zero;
+#define BUF_SIZE 512
+
+bool validate_encode_decode_context(google_trace_TraceContext *ctxt1,
+                                    uint8_t* buffer, size_t buf_size) {
   google_trace_TraceContext ctxt2 = google_trace_TraceContext_init_zero;
   size_t msg_length;
 
-  ctxt1.has_trace_id = true;
-  ctxt1.trace_id.has_hi = true;
-  ctxt1.trace_id.has_lo = true;
-  ctxt1.trace_id.lo = 1;
-  ctxt1.trace_id.hi = 2;
-  ctxt1.has_span_id = true;
-  ctxt1.span_id = 3;
-
-  if (!encode_trace_context(&ctxt1, buffer, buf_size, &msg_length)) {
+  if (!encode_trace_context(ctxt1, buffer, buf_size, &msg_length)) {
     return false;
   }
 
@@ -72,16 +66,109 @@ bool validate_context(uint8_t* buffer, size_t buf_size) {
     return false;
   }
 
-  fprintf(stderr, "encoding and decoding successful...\n");
-  fprintf(stderr, "trace_id: (%"PRIu64": %"PRIu64")  span_id: %"PRIu64"\n",
-          ctxt2.trace_id.hi, ctxt2.trace_id.lo, ctxt2.span_id);
+  GPR_ASSERT(ctxt1->trace_id.hi == ctxt2.trace_id.hi &&
+             ctxt1->trace_id.lo == ctxt2.trace_id.lo &&
+             ctxt1->span_id == ctxt2.span_id);
 
   return true;
 }
 
+bool validate_decode_context(uint8_t* buffer, size_t msg_length) {
+  google_trace_TraceContext ctxt = google_trace_TraceContext_init_zero;
+
+  //validate the decoding of a context written to buffer
+  if (!decode_trace_context(&ctxt, buffer, msg_length)) {
+    return false;
+  }
+
+  if (!ctxt.has_trace_id || !ctxt.has_span_id) {
+    return false;
+  }
+
+  return true;
+}
+
+// Given a file name, read raw proto and define the resource included within.
+// Returns resource id from census_define_resource().
+static void read_context_from_file(const char *file, const bool succeed) {
+  uint8_t buffer[BUF_SIZE];
+  FILE *input = fopen(file, "rb");
+  GPR_ASSERT(input != NULL);
+  size_t nbytes = fread(buffer, 1, BUF_SIZE, input);
+  GPR_ASSERT(nbytes < BUF_SIZE && feof(input) && !ferror(input));
+  bool res = validate_decode_context(buffer, nbytes);
+  GPR_ASSERT(res == succeed);
+  GPR_ASSERT(fclose(input) == 0);
+}
+
+static void test_full() {
+  read_context_from_file("test/core/census/data/context_full.pb", true);
+}
+
+static void test_empty() {
+  read_context_from_file("test/core/census/data/context_empty.pb", false);
+}
+
+static void test_trace_only() {
+  read_context_from_file(
+      "test/core/census/data/context_trace_only.pb", false);
+}
+
+static void test_span_only() {
+  read_context_from_file(
+      "test/core/census/data/context_span_only.pb", false);
+}
+
+static void test_encode_decode() {
+  uint8_t buffer[BUF_SIZE] = {0};
+
+  google_trace_TraceContext ctxt1 = google_trace_TraceContext_init_zero;
+  ctxt1.has_trace_id = true;
+  ctxt1.trace_id.has_hi = true;
+  ctxt1.trace_id.has_lo = true;
+  ctxt1.trace_id.lo = 1;
+  ctxt1.trace_id.hi = 2;
+  ctxt1.has_span_id = true;
+  ctxt1.span_id = 3;
+  validate_encode_decode_context(&ctxt1, buffer, sizeof(buffer));
+
+  google_trace_TraceContext ctxt2 = google_trace_TraceContext_init_zero;
+  ctxt2.has_trace_id = true;
+  ctxt2.trace_id.has_hi = false;
+  ctxt2.trace_id.has_lo = false;
+  ctxt2.has_span_id = true;
+  validate_encode_decode_context(&ctxt2, buffer, sizeof(buffer));
+}
+
+static void test_corrupt() {
+  uint8_t buffer[BUF_SIZE] = {0};
+  google_trace_TraceContext ctxt1 = google_trace_TraceContext_init_zero;
+  size_t msg_length;
+
+  ctxt1.has_trace_id = true;
+  ctxt1.trace_id.has_hi = true;
+  ctxt1.trace_id.has_lo = true;
+  ctxt1.trace_id.lo = 1;
+  ctxt1.trace_id.hi = 2;
+  ctxt1.has_span_id = true;
+  ctxt1.span_id = 3;
+  encode_trace_context(&ctxt1, buffer, sizeof(buffer), &msg_length);
+
+  //corrupt some bytes
+  buffer[1] = 255;
+
+  bool res = validate_decode_context(buffer, msg_length);
+  GPR_ASSERT(res == false);
+}
+
 int main(int argc, char **argv) {
-  uint8_t buffer[256] = {0};
-  validate_context(buffer, sizeof(buffer));
+  grpc_test_init(argc, argv);
+  test_full();
+  test_empty();
+  test_trace_only();
+  test_span_only();
+  test_encode_decode();
+  test_corrupt();
 
   return 0;
 }
