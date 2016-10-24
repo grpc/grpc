2826934769615bef0c3b17dff2a3c459958f4e23
murgatroid99 <mlumish@google.com>
2016-10-24 09:55:44 -0700

    Added import order comments, resolved other minor issues

--- src/core/ext/lb_policy/grpclb/grpclb.c
+++ src/core/ext/lb_policy/grpclb/grpclb.c
@@ -96,6 +96,10 @@
  * - Implement LB service forwarding (point 2c. in the doc's diagram).
  */
 
+/* With the addition of a libuv endpoint, sockaddr.h now includes uv.h when
+   using that endpoint. Because of various transitive includes in uv.h,
+   including windows.h on Windows, uv.h must be included before other system
+   headers. Therefore, sockaddr.h must always be included first */
 #include "src/core/lib/iomgr/sockaddr.h"
 
 #include <errno.h>
