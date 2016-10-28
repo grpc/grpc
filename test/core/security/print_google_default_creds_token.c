25b83f0d1360a081078895bd1d1f877cb29e6c04
Craig Tiller <craig.tiller@gmail.com>
2016-10-28 09:05:27 -0700

    Add missing include

--- test/core/security/print_google_default_creds_token.c
+++ test/core/security/print_google_default_creds_token.c
@@ -45,6 +45,7 @@
 #include "src/core/lib/security/credentials/composite/composite_credentials.h"
 #include "src/core/lib/security/credentials/credentials.h"
 #include "src/core/lib/support/string.h"
+#include "src/core/lib/slice/slice_string_helpers.h"
 
 typedef struct {
   gpr_mu *mu;
