69259d425111696dd2df5a243e8b714db3dc3dac
murgatroid99 <mlumish@google.com>
2016-10-31 14:34:10 -0700

    Add resource quota support to uv TCP code

--- src/core/lib/iomgr/endpoint_pair_uv.c
+++ src/core/lib/iomgr/endpoint_pair_uv.c
@@ -41,8 +41,9 @@
 
 #include "src/core/lib/iomgr/endpoint_pair.h"
 
-grpc_endpoint_pair grpc_iomgr_create_endpoint_pair(const char *name,
-                                                   size_t read_slice_size) {
+grpc_endpoint_pair grpc_iomgr_create_endpoint_pair(
+    const char *name, grpc_resource_quota *resource_quota,
+    size_t read_slice_size) {
   grpc_endpoint_pair endpoint_pair;
   // TODO(mlumish): implement this properly under libuv
   GPR_ASSERT(false &&
