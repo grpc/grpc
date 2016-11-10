3fde39913ceaa03597eb86420fb041d1210ae0e3
David Garcia Quintas <dgq@google.com>
2016-11-10 14:20:17 -0800

    Fixed wrong NULLing in chttp2 transport

--- src/core/ext/transport/chttp2/transport/chttp2_transport.c
+++ src/core/ext/transport/chttp2/transport/chttp2_transport.c
@@ -1037,7 +1037,7 @@ static void perform_stream_op_locked(grpc_exec_ctx *exec_ctx, void *stream_op,
                                       "op.send_initial_metadata");
         }
       } else {
-        s->send_trailing_metadata = NULL;
+        s->send_initial_metadata = NULL;
         grpc_chttp2_complete_closure_step(
             exec_ctx, t, s, &s->send_initial_metadata_finished,
             GRPC_ERROR_CREATE(
