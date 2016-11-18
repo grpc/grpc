f62706f2060a029ff9eaf178fc32946f7feda3c3
Yuchen Zeng <zyc@google.com>
2016-11-17 20:59:48 -0800

    Add pollset_set in grpc_resolver

--- src/core/ext/client_channel/resolver.c
+++ src/core/ext/client_channel/resolver.c
@@ -36,6 +36,7 @@
 void grpc_resolver_init(grpc_resolver *resolver,
                         const grpc_resolver_vtable *vtable) {
   resolver->vtable = vtable;
+  resolver->pollset_set = grpc_pollset_set_create();
   gpr_ref_init(&resolver->refs, 1);
 }
 
@@ -62,6 +63,7 @@ void grpc_resolver_unref(grpc_resolver *resolver,
 void grpc_resolver_unref(grpc_exec_ctx *exec_ctx, grpc_resolver *resolver) {
 #endif
   if (gpr_unref(&resolver->refs)) {
+    grpc_pollset_set_destroy(resolver->pollset_set);
     resolver->vtable->destroy(exec_ctx, resolver);
   }
 }
