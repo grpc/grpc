8fd64fb03848696f851cc6a025e4c570d71a06a3
Craig Tiller <ctiller@google.com>
2016-11-23 11:38:43 -0800

    Fix slice refcounting

--- src/core/lib/surface/channel.c
+++ src/core/lib/surface/channel.c
@@ -126,7 +126,7 @@ grpc_channel *grpc_channel_create(grpc_exec_ctx *exec_ctx, const char *target,
           channel->default_authority = grpc_mdelem_from_slices(
               exec_ctx, GRPC_MDSTR_AUTHORITY,
               grpc_slice_intern(
-                  grpc_slice_from_copied_string(args->args[i].value.string)));
+                  grpc_slice_from_static_string(args->args[i].value.string)));
         }
       } else if (0 ==
                  strcmp(args->args[i].key, GRPC_SSL_TARGET_NAME_OVERRIDE_ARG)) {
@@ -143,7 +143,7 @@ grpc_channel *grpc_channel_create(grpc_exec_ctx *exec_ctx, const char *target,
             channel->default_authority = grpc_mdelem_from_slices(
                 exec_ctx, GRPC_MDSTR_AUTHORITY,
                 grpc_slice_intern(
-                    grpc_slice_from_copied_string(args->args[i].value.string)));
+                    grpc_slice_from_static_string(args->args[i].value.string)));
           }
         }
       } else if (0 == strcmp(args->args[i].key,
