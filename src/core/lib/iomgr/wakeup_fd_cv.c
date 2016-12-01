ddabeb24b2f6f8a6feef775b6252ba31c720cbf3
Ken Payson <kpayson@google.com>
2016-11-30 19:07:45 -0800

    Shard global cv table

--- src/core/lib/iomgr/wakeup_fd_cv.c
+++ src/core/lib/iomgr/wakeup_fd_cv.c
@@ -49,51 +49,56 @@
 
 #define MAX_TABLE_RESIZE 256
 
-extern cv_fd_table g_cvfds;
+extern cv_fd_table g_cvfds[TABLE_SHARDS];
 
 static grpc_error* cv_fd_init(grpc_wakeup_fd* fd_info) {
   unsigned int i, newsize;
   int idx;
-  gpr_mu_lock(&g_cvfds.mu);
-  if (!g_cvfds.free_fds) {
-    newsize = GPR_MIN(g_cvfds.size * 2, g_cvfds.size + MAX_TABLE_RESIZE);
-    g_cvfds.cvfds = gpr_realloc(g_cvfds.cvfds, sizeof(fd_node) * newsize);
-    for (i = g_cvfds.size; i < newsize; i++) {
-      g_cvfds.cvfds[i].is_set = 0;
-      g_cvfds.cvfds[i].cvs = NULL;
-      g_cvfds.cvfds[i].next_free = g_cvfds.free_fds;
-      g_cvfds.free_fds = &g_cvfds.cvfds[i];
+  int shard = rand() % TABLE_SHARDS;
+  gpr_mu_lock(&g_cvfds[shard].mu);
+  if (!g_cvfds[shard].free_fds) {
+    newsize = GPR_MIN(g_cvfds[shard].size * 2, g_cvfds[shard].size + MAX_TABLE_RESIZE);
+    g_cvfds[shard].cvfds = gpr_realloc(g_cvfds[shard].cvfds, sizeof(fd_node) * newsize);
+    for (i = g_cvfds[shard].size; i < newsize; i++) {
+      g_cvfds[shard].cvfds[i].is_set = 0;
+      g_cvfds[shard].cvfds[i].cvs = NULL;
+      g_cvfds[shard].cvfds[i].next_free = g_cvfds[shard].free_fds;
+      g_cvfds[shard].free_fds = &g_cvfds[shard].cvfds[i];
     }
-    g_cvfds.size = newsize;
+    g_cvfds[shard].size = newsize;
   }
 
-  idx = (int)(g_cvfds.free_fds - g_cvfds.cvfds);
-  g_cvfds.free_fds = g_cvfds.free_fds->next_free;
-  g_cvfds.cvfds[idx].cvs = NULL;
-  g_cvfds.cvfds[idx].is_set = 0;
-  fd_info->read_fd = IDX_TO_FD(idx);
+  idx = (int)(g_cvfds[shard].free_fds - g_cvfds[shard].cvfds);
+  g_cvfds[shard].free_fds = g_cvfds[shard].free_fds->next_free;
+  g_cvfds[shard].cvfds[idx].cvs = NULL;
+  g_cvfds[shard].cvfds[idx].is_set = 0;
+  fd_info->read_fd = SHARD_IDX_TO_FD(shard, idx);
   fd_info->write_fd = -1;
-  gpr_mu_unlock(&g_cvfds.mu);
+  gpr_mu_unlock(&g_cvfds[shard].mu);
   return GRPC_ERROR_NONE;
 }
 
 static grpc_error* cv_fd_wakeup(grpc_wakeup_fd* fd_info) {
   cv_node* cvn;
-  gpr_mu_lock(&g_cvfds.mu);
-  g_cvfds.cvfds[FD_TO_IDX(fd_info->read_fd)].is_set = 1;
-  cvn = g_cvfds.cvfds[FD_TO_IDX(fd_info->read_fd)].cvs;
+  int shard = FD_TO_SHARD(fd_info->read_fd);
+  gpr_mu_lock(&g_cvfds[shard].mu);
+  g_cvfds[shard].cvfds[FD_TO_IDX(fd_info->read_fd)].is_set = 1;
+  cvn = g_cvfds[shard].cvfds[FD_TO_IDX(fd_info->read_fd)].cvs;
   while (cvn) {
+    gpr_mu_lock(cvn->mu);
     gpr_cv_signal(cvn->cv);
+    gpr_mu_unlock(cvn->mu);
     cvn = cvn->next;
   }
-  gpr_mu_unlock(&g_cvfds.mu);
+  gpr_mu_unlock(&g_cvfds[shard].mu);
   return GRPC_ERROR_NONE;
 }
 
 static grpc_error* cv_fd_consume(grpc_wakeup_fd* fd_info) {
-  gpr_mu_lock(&g_cvfds.mu);
-  g_cvfds.cvfds[FD_TO_IDX(fd_info->read_fd)].is_set = 0;
-  gpr_mu_unlock(&g_cvfds.mu);
+  int shard = FD_TO_SHARD(fd_info->read_fd);
+  gpr_mu_lock(&g_cvfds[shard].mu);
+  g_cvfds[shard].cvfds[FD_TO_IDX(fd_info->read_fd)].is_set = 0;
+  gpr_mu_unlock(&g_cvfds[shard].mu);
   return GRPC_ERROR_NONE;
 }
 
@@ -101,12 +106,13 @@ static void cv_fd_destroy(grpc_wakeup_fd* fd_info) {
   if (fd_info->read_fd == 0) {
     return;
   }
-  gpr_mu_lock(&g_cvfds.mu);
+  int shard = FD_TO_SHARD(fd_info->read_fd);
+  gpr_mu_lock(&g_cvfds[shard].mu);
   // Assert that there are no active pollers
-  GPR_ASSERT(!g_cvfds.cvfds[FD_TO_IDX(fd_info->read_fd)].cvs);
-  g_cvfds.cvfds[FD_TO_IDX(fd_info->read_fd)].next_free = g_cvfds.free_fds;
-  g_cvfds.free_fds = &g_cvfds.cvfds[FD_TO_IDX(fd_info->read_fd)];
-  gpr_mu_unlock(&g_cvfds.mu);
+  GPR_ASSERT(!g_cvfds[shard].cvfds[FD_TO_IDX(fd_info->read_fd)].cvs);
+  g_cvfds[shard].cvfds[FD_TO_IDX(fd_info->read_fd)].next_free = g_cvfds[shard].free_fds;
+  g_cvfds[shard].free_fds = &g_cvfds[shard].cvfds[FD_TO_IDX(fd_info->read_fd)];
+  gpr_mu_unlock(&g_cvfds[shard].mu);
 }
 
 static int cv_check_availability(void) { return 1; }
