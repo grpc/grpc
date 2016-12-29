260f7afb5abdfab1148742ccb15a4cdfabba9c7f
yang-g <yangg@google.com>
2016-12-29 14:08:13 -0800

    more cleanup

--- include/grpc++/health_check_service_interface.h
+++ include/grpc++/health_check_service_interface.h
@@ -38,25 +38,30 @@
 
 namespace grpc {
 
-const char kDefaultHealthCheckServiceInterfaceArg[] =
-    "grpc.default_health_check_service_interface";
+const char kHealthCheckServiceInterfaceArg[] =
+    "grpc.health_check_service_interface";
 
+// The gRPC server uses this interface to expose the health checking service
+// without depending on protobuf.
 class HealthCheckServiceInterface {
  public:
   virtual ~HealthCheckServiceInterface() {}
+
+  // Set or change the serving status of the given service_name.
   virtual void SetServingStatus(const grpc::string& service_name,
                                 bool serving) = 0;
   // Apply to all registered service names.
   virtual void SetServingStatus(bool serving) = 0;
 };
 
-bool DefaultHealthCheckServiceEnabled();
-
 // Enable/disable the default health checking service. This applies to all C++
 // servers created afterwards. For each server, user can override the default
 // with a HealthCheckServiceServerBuilderOption.
 // NOT thread safe.
 void EnableDefaultHealthCheckService(bool enable);
+
+// NOT thread safe.
+bool DefaultHealthCheckServiceEnabled();
 
 }  // namespace grpc
 
