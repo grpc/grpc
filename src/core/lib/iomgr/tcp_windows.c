460da03d0c335bf9c81e5fc035299e9ddf5804d1
Craig Tiller <craig.tiller@gmail.com>
2017-01-03 08:45:29 -0800

    Fixup Windows source files for new closure APIs

--- src/core/lib/iomgr/tcp_windows.c
+++ src/core/lib/iomgr/tcp_windows.c
@@ -203,7 +203,7 @@ static void win_read(grpc_exec_ctx *exec_ctx, grpc_endpoint *ep,
 
   if (tcp->shutting_down) {
     grpc_closure_sched(exec_ctx, cb,
-                        GRPC_ERROR_CREATE("TCP socket is shutting down"), NULL);
+                        GRPC_ERROR_CREATE("TCP socket is shutting down"));
     return;
   }
 
@@ -241,7 +241,7 @@ static void win_read(grpc_exec_ctx *exec_ctx, grpc_endpoint *ep,
     if (wsa_error != WSA_IO_PENDING) {
       info->wsa_error = wsa_error;
       grpc_closure_sched(exec_ctx, &tcp->on_read,
-                          GRPC_WSA_ERROR(info->wsa_error, "WSARecv"), NULL);
+                          GRPC_WSA_ERROR(info->wsa_error, "WSARecv"));
       return;
     }
   }
@@ -291,7 +291,7 @@ static void win_write(grpc_exec_ctx *exec_ctx, grpc_endpoint *ep,
 
   if (tcp->shutting_down) {
     grpc_closure_sched(exec_ctx, cb,
-                        GRPC_ERROR_CREATE("TCP socket is shutting down"), NULL);
+                        GRPC_ERROR_CREATE("TCP socket is shutting down"));
     return;
   }
 
@@ -340,8 +340,7 @@ static void win_write(grpc_exec_ctx *exec_ctx, grpc_endpoint *ep,
     int wsa_error = WSAGetLastError();
     if (wsa_error != WSA_IO_PENDING) {
       TCP_UNREF(exec_ctx, tcp, "write");
-      grpc_closure_sched(exec_ctx, cb, GRPC_WSA_ERROR(wsa_error, "WSASend"),
-                          NULL);
+      grpc_closure_sched(exec_ctx, cb, GRPC_WSA_ERROR(wsa_error, "WSASend"));
       return;
     }
   }
