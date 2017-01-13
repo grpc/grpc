b0e4629742cc259cad05003e8bb72a034dd9bf30
Jan Tattermusch <jtattermusch@google.com>
2017-01-13 19:38:46 +0100

    dont use win crash handler on mingw

--- include/grpc/impl/codegen/port_platform.h
+++ include/grpc/impl/codegen/port_platform.h
@@ -99,10 +99,13 @@
 #define GPR_POSIX_STRING
 #define GPR_POSIX_TIME
 #else
+#if !defined(__MINGW32__)
+/* TODO(jtattermusch): make win crash handler compile under MINGW */
+#define GPR_WINDOWS_CRASH_HANDLER 1
+#endif
 #define GPR_GETPID_IN_PROCESS_H 1
 #define GPR_WINDOWS_TMPFILE
 #define GPR_WINDOWS_LOG
-#define GPR_WINDOWS_CRASH_HANDLER 1
 #define GPR_WINDOWS_STRING
 #define GPR_WINDOWS_TIME
 #endif
