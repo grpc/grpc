d87b9fb4c445182bf24d4e7782d624cc0ba557dd
Muxi Yan <mxyan@google.com>
2017-01-12 17:40:24 -0800

    clang-format and add copyright info

--- src/core/ext/transport/cronet/transport/cronet_transport.c
+++ src/core/ext/transport/cronet/transport/cronet_transport.c
@@ -857,9 +857,8 @@ static enum e_op_result execute_stream_op(grpc_exec_ctx *exec_ctx,
       grpc_closure_sched(exec_ctx, stream_op->recv_initial_metadata_ready,
                          GRPC_ERROR_NONE);
     } else if (stream_state->state_callback_received[OP_FAILED]) {
-      grpc_closure_sched(
-          exec_ctx, stream_op->recv_initial_metadata_ready,
-          GRPC_ERROR_NONE);
+      grpc_closure_sched(exec_ctx, stream_op->recv_initial_metadata_ready,
+                         GRPC_ERROR_NONE);
     } else {
       grpc_chttp2_incoming_metadata_buffer_publish(
           &oas->s->state.rs.initial_metadata, stream_op->recv_initial_metadata);
@@ -921,9 +920,8 @@ static enum e_op_result execute_stream_op(grpc_exec_ctx *exec_ctx,
       result = ACTION_TAKEN_NO_CALLBACK;
     } else if (stream_state->state_callback_received[OP_FAILED]) {
       CRONET_LOG(GPR_DEBUG, "Stream failed.");
-      grpc_closure_sched(
-          exec_ctx, stream_op->recv_message_ready,
-          GRPC_ERROR_NONE);
+      grpc_closure_sched(exec_ctx, stream_op->recv_message_ready,
+                         GRPC_ERROR_NONE);
       stream_state->state_op_done[OP_RECV_MESSAGE] = true;
       result = ACTION_TAKEN_NO_CALLBACK;
     } else if (stream_state->rs.read_stream_closed == true) {
