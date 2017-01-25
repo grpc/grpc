6f12c7496948678eb24c14f54ac99e5452ea07fc
Mark D. Roth <roth@google.com>
2017-01-25 15:02:56 -0800

    clang-format

--- test/core/end2end/fuzzers/api_fuzzer.c
+++ test/core/end2end/fuzzers/api_fuzzer.c
@@ -934,8 +934,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
               } else {
                 has_ops |= 1 << GRPC_OP_SEND_MESSAGE;
                 g_active_call->send_message =
-                    op->data.send_message.send_message =
-                    read_message(&inp);
+                    op->data.send_message.send_message = read_message(&inp);
               }
               break;
             case GRPC_OP_SEND_CLOSE_FROM_CLIENT:
