2ac87369a3ebed06c2813d7839463374c5da5788
Sree Kuchibhotla <sreek@google.com>
2017-01-30 10:45:39 -0800

    fix grpc_fd_shutdown invocation after latest merge

--- test/core/iomgr/pollset_set_test.c
+++ test/core/iomgr/pollset_set_test.c
@@ -142,7 +142,7 @@ static void cleanup_test_fds(grpc_exec_ctx *exec_ctx, test_fd *tfds,
   int release_fd;
 
   for (int i = 0; i < num_fds; i++) {
-    grpc_fd_shutdown(exec_ctx, tfds[i].fd);
+    grpc_fd_shutdown(exec_ctx, tfds[i].fd, GRPC_ERROR_CREATE("fd cleanup"));
     grpc_exec_ctx_flush(exec_ctx);
 
     /* grpc_fd_orphan frees the memory allocated for grpc_fd. Normally it also
