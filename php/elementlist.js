6c0c55edeffb657a1961d7058f9f0304b0c75149
Stanley Cheung <stanleycheung@google.com>
2017-02-14 00:34:20 -0800

    PHP: Update generated API docs

---
+++ php/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Grpc\\AbstractCall"],["c","Grpc\\BaseStub"],["c","Grpc\\BidiStreamingCall"],["c","Grpc\\ClientStreamingCall"],["c","Grpc\\ServerStreamingCall"],["c","Grpc\\UnaryCall"]];
