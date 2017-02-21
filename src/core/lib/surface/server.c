a2b0e56823f0415912f964bff6a4ce02002dcb33
Eduardo Caceres <edu_kueyar@hotmail.com>
2017-02-21 20:33:40 +0100

    Correct formatting

--- src/core/lib/surface/server.c
+++ src/core/lib/surface/server.c
@@ -1198,7 +1198,7 @@ void grpc_server_setup_transport(grpc_exec_ctx *exec_ctx, grpc_server *s,
       crm->server_registered_method = rm;
       crm->flags = rm->flags;
       crm->has_host = has_host;
-      if(has_host)  {
+      if (has_host) {
         crm->host = host;
       }
       crm->method = method;
