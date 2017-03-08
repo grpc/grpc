49e9d483494b5ec7fbb3aa2a2504554feba6edf1
Craig Tiller <ctiller@google.com>
2017-03-08 14:15:59 -0800

    Use the spiffy new diff tool

--- tools/jenkins/run_performance.sh
+++ tools/jenkins/run_performance.sh
@@ -34,5 +34,5 @@ set -ex
 # Enter the gRPC repo root
 cd $(dirname $0)/../..
 
-tools/run_tests/run_microbenchmark.py -c --diff_perf origin/$ghprbTargetBranch
+tools/run_tests/run_microbenchmark.py -c summary --diff_perf origin/$ghprbTargetBranch
 
