16ea0151203cbe5755202e564b989941b81ca3c0
alefallas <alefallas@users.noreply.github.com>
2017-03-08 14:50:51 +0200

    Fix compilation warning
    Fix warning when compiling with visual studio. Conversion from size_t to int

--- src/cpp/server/server_builder.cc
+++ src/cpp/server/server_builder.cc
@@ -270,7 +270,7 @@ std::unique_ptr<Server> ServerBuilder::BuildAndStart() {
   //  2. cqs_: Completion queues added via AddCompletionQueue() call
 
   // All sync cqs (if any) are frequently polled by ThreadManager
-  int num_frequently_polled_cqs = sync_server_cqs->size();
+  size_t num_frequently_polled_cqs = sync_server_cqs->size();
 
   for (auto it = sync_server_cqs->begin(); it != sync_server_cqs->end(); ++it) {
     grpc_server_register_completion_queue(server->server_, (*it)->cq(),
