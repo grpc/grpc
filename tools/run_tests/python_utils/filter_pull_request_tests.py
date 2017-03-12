8e64d901fd7efd39868a889159329f41f74e3f9e
Siddharth Shukla <siddharth.shukla@uber.com>
2017-03-12 19:50:18 +0100

    Modify shebang on python scripts
    Change shebangs to `#!/usr/bin/env python` instead of
    `#!/usr/bin/env python2.7`.

--- tools/run_tests/python_utils/filter_pull_request_tests.py
+++ tools/run_tests/python_utils/filter_pull_request_tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 # Copyright 2015, Google Inc.
 # All rights reserved.
 #
--- tools/run_tests/run_microbenchmark.py
+++ tools/run_tests/run_microbenchmark.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 # Copyright 2017, Google Inc.
 # All rights reserved.
 #
