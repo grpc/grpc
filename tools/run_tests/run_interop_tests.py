6ce6166bd0451db14893b896384517bb95fbfea3
Eric Gribkoff <ericgribkoff@google.com>
2017-03-16 10:27:00 -0700

    reenable health check, disable other interop tests

--- tools/run_tests/run_interop_tests.py
+++ tools/run_tests/run_interop_tests.py
@@ -964,7 +964,7 @@ try:
                           manual_cmd_log=server_manual_cmd_log)
     if not args.manual_run:
       job = dockerjob.DockerJob(spec)
-      #job.wait_for_healthy(timeout_seconds=240)
+      job.wait_for_healthy(timeout_seconds=240)
       print(subprocess.check_output(['docker', 'version']))
       server_jobs[lang] = job
       http2_badserver_ports = tuple([
