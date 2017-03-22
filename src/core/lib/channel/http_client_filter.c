ce7b947ebe2310cb5a3ed061774de51459d63c6a
Makarand Dharmapurikar <makarandd@google.com>
2017-03-22 15:48:19 -0700

    free the allocated slice to fix asan failure

--- src/core/lib/channel/http_client_filter.c
+++ src/core/lib/channel/http_client_filter.c
@@ -342,6 +342,7 @@ static grpc_error *hc_mutate_op(grpc_exec_ctx *exec_ctx,
         calld->on_complete = op->on_complete;
         op->on_complete = &calld->hc_on_complete;
         op->send_message = NULL;
+        grpc_slice_unref_internal(exec_ctx, path_with_query_slice);
       } else {
         /* Not all data is available. Fall back to POST. */
         gpr_log(GPR_DEBUG,
