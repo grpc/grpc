6f7dcd0ade302ab348832bf3e765198be34278f9
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2017-03-27 18:51:55 +0200

    Clang-format.

--- test/cpp/qps/qps_json_driver.cc
+++ test/cpp/qps/qps_json_driver.cc
@@ -40,10 +40,10 @@
 #include <gflags/gflags.h>
 #include <grpc/support/log.h>
 
+#include "test/cpp/qps/benchmark_config.h"
 #include "test/cpp/qps/driver.h"
 #include "test/cpp/qps/parse_json.h"
 #include "test/cpp/qps/report.h"
-#include "test/cpp/qps/benchmark_config.h"
 
 DEFINE_string(scenarios_file, "",
               "JSON file containing an array of Scenario objects");
