8e2fd9ca4d8cdf610dcb9fc89cceeb6e51ad2467
Craig Tiller <ctiller@google.com>
2017-03-27 09:32:24 -0700

    Avoid spin polling

--- src/core/lib/iomgr/ev_epoll_linux.c
+++ src/core/lib/iomgr/ev_epoll_linux.c
@@ -1487,8 +1487,9 @@ static int poll_deadline_to_millis_timeout(gpr_timespec deadline,
     return 0;
   }
   timeout = gpr_time_sub(deadline, now);
-  return gpr_time_to_millis(gpr_time_add(
-      timeout, gpr_time_from_nanos(GPR_NS_PER_MS - 1, GPR_TIMESPAN)));
+  return GPR_MAX(
+      1, gpr_time_to_millis(gpr_time_add(
+             timeout, gpr_time_from_nanos(GPR_NS_PER_MS - 1, GPR_TIMESPAN))));
 }
 
 static void fd_become_readable(grpc_exec_ctx *exec_ctx, grpc_fd *fd,
