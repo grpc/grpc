01704b41b1d5d50360d9e92d46046e49206dbfe5
Craig Tiller <ctiller@google.com>
2017-03-28 14:43:41 -0700

    Backfill

--- tools/line_count/summarize-history.py
+++ tools/line_count/summarize-history.py
@@ -7,8 +7,8 @@ def daterange(start, end):
   for n in range(int((end - start).days)):
     yield start + datetime.timedelta(n)
 
-start_date = datetime.date(2014, 11, 26)
-end_date = datetime.date(2017, 3, 26)
+start_date = datetime.date(2017, 3, 26)
+end_date = datetime.date(2017, 3, 29)
 
 for dt in daterange(start_date, end_date):
   dmy = dt.strftime('%Y-%m-%d')
