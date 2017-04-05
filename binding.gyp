grpc/grpc

commit 6f62c0adca4921c8d0e75ec5b9886280ead5b7f1

Disable libuv core changes in Node library

binding.gyp
@@ -51,7 +51,9 @@
     'conditions': [
       ['runtime=="node"', {
         'defines': [
-          'GRPC_UV'
+          # Disabling this while bugs are ironed out. Uncomment this to
+          # re-enable libuv integration in C core.
+          # 'GRPC_UV'
         ]
       }],
       ['OS!="win" and runtime=="electron"', {

templates/binding.gyp.template
@@ -53,7 +53,9 @@
       'conditions': [
         ['runtime=="node"', {
           'defines': [
-            'GRPC_UV'
+            # Disabling this while bugs are ironed out. Uncomment this to
+            # re-enable libuv integration in C core.
+            # 'GRPC_UV'
           ]
         }],
         ['OS!="win" and runtime=="electron"', {

commit 8f60965f22ba099faffce08d5036f5a818632ea0

Fix memory leak in latest 1.1.* branch
See https://github.com/grpc/grpc/issues/10445
This commit effectively reverts the commit https://github.com/grpc/grpc/commit/6f62c0adca4921c8d0e75ec5b9886280ead5b7f1 which introduced this issue for the 1.1.* branch.

binding.gyp
@@ -39,11 +39,7 @@
 {
   'variables': {
     'runtime%': 'node',
-    # UV integration in C core is disabled by default while bugs are ironed
-    # out. It can be re-enabled for one build by setting the npm config
-    # variable grpc_uv to true, and it can be re-enabled permanently by
-    # setting it to true here.
-    'grpc_uv%': 'false'
+    'grpc_uv%': 'true'
   },
   'target_defaults': {
     'include_dirs': [
@@ -56,8 +52,6 @@
     'conditions': [
       ['runtime=="node" and grpc_uv=="true"', {
         'defines': [
-          # Disabling this while bugs are ironed out. Uncomment this to
-          # re-enable libuv integration in C core.
           'GRPC_UV'
         ]
       }],

templates/binding.gyp.template
@@ -41,11 +41,7 @@
   {
     'variables': {
       'runtime%': 'node',
-      # UV integration in C core is disabled by default while bugs are ironed
-      # out. It can be re-enabled for one build by setting the npm config
-      # variable grpc_uv to true, and it can be re-enabled permanently by
-      # setting it to true here.
-      'grpc_uv%': 'false'
+      'grpc_uv%': 'true'
     },
     'target_defaults': {
       'include_dirs': [
@@ -58,8 +54,6 @@
       'conditions': [
         ['runtime=="node" and grpc_uv=="true"', {
           'defines': [
-            # Disabling this while bugs are ironed out. Uncomment this to
-            # re-enable libuv integration in C core.
             'GRPC_UV'
           ]
         }],