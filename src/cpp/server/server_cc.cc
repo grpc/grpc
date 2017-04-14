eafeea48a981d799c2c893102b6f0cbe25163a7f
yang-g <yangg@google.com>
2017-04-14 12:34:10 -0700

    Update ServerGlobalCallback::AddPort api

--- src/cpp/server/server_cc.cc
+++ src/cpp/server/server_cc.cc
@@ -490,7 +490,7 @@ int Server::AddListeningPort(const grpc::string& addr,
                              ServerCredentials* creds) {
   GPR_ASSERT(!started_);
   int port = creds->AddPortToServer(addr, server_);
-  global_callbacks_->AddPort(this, port);
+  global_callbacks_->AddPort(this, addr, creds, port);
   return port;
 }
 
