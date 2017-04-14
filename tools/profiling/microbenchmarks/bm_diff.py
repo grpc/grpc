bc442e1ff0e2fd8107d41219c3c52472324efa77
Craig Tiller <ctiller@google.com>
2017-04-14 08:33:55 -0700

    Declare jobs

--- tools/profiling/microbenchmarks/bm_diff.py
+++ tools/profiling/microbenchmarks/bm_diff.py
@@ -147,6 +147,7 @@ finally:
   subprocess.check_call(['git', 'checkout', where_am_i])
   subprocess.check_call(['git', 'submodule', 'update'])
 
+jobs = []
 for loop in range(0, args.loops):
   jobs.extend(x for x in itertools.chain(
     (collect1(bm, 'opt', 'new', loop) for bm in args.benchmarks),
