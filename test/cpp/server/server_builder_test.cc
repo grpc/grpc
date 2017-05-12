c8a6822b0e0afa53b2ec4c92610f7175e4be5127
Muxi Yan <mxyan@google.com>
2017-05-12 12:21:16 -0700

    Revert "Add cpp test"
    This reverts commit e22bd48cd93cf4c0b777e124631f14bb21073c6c.

--- test/cpp/server/server_builder_test.cc
+++ test/cpp/server/server_builder_test.cc
@@ -40,8 +40,6 @@
 #include "src/proto/grpc/testing/echo.grpc.pb.h"
 #include "test/core/util/port.h"
 
-#include <grpc/support/workaround_list.h>
-
 namespace grpc {
 namespace {
 
@@ -87,15 +85,6 @@ TEST(ServerBuilderTest, CreateServerRepeatedPortWithDisallowedReusePort) {
                 .AddChannelArgument(GRPC_ARG_ALLOW_REUSEPORT, 0)
                 .BuildAndStart(),
             nullptr);
-}
-
-TEST(ServerBuilderTest, CreateServerOnePortWithCronetCompressionWorkaround) {
-  ServerBuilder()
-      .RegisterService(&g_service)
-      .AddListeningPort(g_port, InsecureServerCredentials())
-      .EnableWorkaround(GRPC_WORKAROUND_ID_CRONET_COMPRESSION)
-      .BuildAndStart()
-      ->Shutdown();
 }
 
 }  // namespace
