92eb7fbc05903def20434c110193481913c2e38a
Alexander Polcyn <apolcyn@google.com>
2017-05-18 11:49:15 -0700

    Bump to version 1.3.4

--- src/csharp/build_packages_dotnetcli.bat
+++ src/csharp/build_packages_dotnetcli.bat
@@ -28,7 +28,7 @@
 @rem OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 @rem Current package versions
-set VERSION=1.3.3
+set VERSION=1.3.4
 
 @rem Adjust the location of nuget.exe
 set NUGET=C:\nuget\nuget.exe
