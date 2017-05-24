4c15fac09f1dcd686f777e7d3d5e9b526f77457b
Yuchen Zeng <zyc@google.com>
2017-05-24 16:40:20 -0700

    Fix another memory leak

--- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_wrapper.c
+++ src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_wrapper.c
@@ -451,6 +451,7 @@ static void on_dns_lookup_done_cb(grpc_exec_ctx *exec_ctx, void *arg,
   }
   grpc_closure_sched(exec_ctx, r->on_resolve_address_done,
                      GRPC_ERROR_REF(error));
+  grpc_lb_addresses_destroy(exec_ctx, r->lb_addrs);
   gpr_free(r);
 }
 
