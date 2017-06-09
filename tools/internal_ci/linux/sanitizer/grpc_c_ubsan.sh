ad4fcd4322aa687a9791cef0c41a819b9895b44b
Matt Kwong <mattkwong@google.com>
2017-06-09 14:03:12 -0700

    Make Linux Kokoro tests upload results to BQ

--- tools/internal_ci/linux/sanitizer/grpc_c_ubsan.sh
+++ tools/internal_ci/linux/sanitizer/grpc_c_ubsan.sh
@@ -35,4 +35,4 @@ cd $(dirname $0)/../../../..
 
 source tools/internal_ci/helper_scripts/prepare_build_linux_rc
 
-tools/run_tests/run_tests_matrix.py -f c ubsan --inner_jobs 16 -j 1 --internal_ci
+tools/run_tests/run_tests_matrix.py -f c ubsan --inner_jobs 16 -j 1 --internal_ci --bq_result_table aggregate_results
