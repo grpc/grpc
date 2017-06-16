ae452238cd65bb249de13fb9c0b7390414b21620
murgatroid99 <mlumish@google.com>
2017-06-15 17:32:32 -0700

    Fix missing return after callback in a function

--- src/node/src/client.js
+++ src/node/src/client.js
@@ -105,6 +105,7 @@ function _write(chunk, encoding, callback) {
     /* Once a write fails, just call the callback immediately to let the caller
        flush any pending writes. */
     setImmediate(callback);
+    return;
   }
   try {
     message = this.serialize(chunk);
