4b7eb18f22e00e0b1e9ca3eb9d7a0c7d88aed925
Muxi Yan <mxyan@google.com>
2017-07-12 16:52:31 -0700

    Update more missing files

--- tools/codegen/core/gen_nano_proto.sh
+++ tools/codegen/core/gen_nano_proto.sh
@@ -89,7 +89,7 @@ pushd "$(dirname $INPUT_PROTO)" > /dev/null
 
 protoc \
 --plugin=protoc-gen-nanopb="$GRPC_ROOT/third_party/nanopb/nanopb/generator/protoc-gen-nanopb" \
---nanopb_out='-T -L#include\ \"third_party/nanopb/nanopb/pb.h\"'":$OUTPUT_DIR" \
+--nanopb_out='-T -L#include\ <nanopb/pb.h>'":$OUTPUT_DIR" \
 "$(basename $INPUT_PROTO)"
 
 readonly PROTO_BASENAME=$(basename $INPUT_PROTO .proto)
