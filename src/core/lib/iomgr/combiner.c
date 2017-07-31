84d6924fa0cd28ffde1ada52fa4be7426a08149c
yang-g <yangg@google.com>
2017-07-31 13:39:09 -0700

    Use zalloc and remove now redundant sets

--- src/core/lib/iomgr/combiner.c
+++ src/core/lib/iomgr/combiner.c
@@ -73,13 +73,10 @@ static const grpc_closure_scheduler_vtable finally_scheduler = {
 static void offload(grpc_exec_ctx *exec_ctx, void *arg, grpc_error *error);
 
 grpc_combiner *grpc_combiner_create(void) {
-  grpc_combiner *lock = gpr_malloc(sizeof(*lock));
+  grpc_combiner *lock = gpr_zalloc(sizeof(*lock));
   gpr_ref_init(&lock->refs, 1);
-  lock->next_combiner_on_this_exec_ctx = NULL;
-  lock->time_to_execute_final_list = false;
   lock->scheduler.vtable = &scheduler;
   lock->finally_scheduler.vtable = &finally_scheduler;
-  lock->initiating_exec_ctx_or_null = 0;
   gpr_atm_no_barrier_store(&lock->state, STATE_UNORPHANED);
   gpr_mpscq_init(&lock->queue);
   grpc_closure_list_init(&lock->final_list);
