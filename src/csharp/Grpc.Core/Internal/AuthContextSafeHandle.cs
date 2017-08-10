9210293d9e861e16a296da2cdf8cd4df30b85544
Jan Tattermusch <jtattermusch@google.com>
2017-08-10 09:07:53 +0200

    dont use generic version of Marshal.SizeOf

--- src/csharp/Grpc.Core/Internal/AuthContextSafeHandle.cs
+++ src/csharp/Grpc.Core/Internal/AuthContextSafeHandle.cs
@@ -75,8 +75,7 @@ namespace Grpc.Core.Internal
         private AuthProperty PtrToAuthProperty(IntPtr authPropertyPtr)
         {
             #pragma warning disable 0618
-            // We need to use the obsolete non-generic version of Marshal.PtrToStructure, because the generic version is not available
-            // in net45 on Windows.
+            // We need to use the obsolete non-generic version of Marshal.PtrToStructure, because the generic version is not available in net45
             var nativeAuthProperty = (NativeAuthProperty) Marshal.PtrToStructure(authPropertyPtr, typeof(NativeAuthProperty));
             #pragma warning restore 0618
             var name = Marshal.PtrToStringAnsi(nativeAuthProperty.Name);
