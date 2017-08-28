138eea81f9af6e941672301b271f276548f0a58e
Jan Tattermusch <jtattermusch@google.com>
2017-08-28 16:37:30 +0200

    cherrypick fixed version of ares_build.h

--- third_party/cares/ares_build.h
+++ third_party/cares/ares_build.h
@@ -199,7 +199,7 @@
 #    define CARES_TYPEOF_ARES_SSIZE_T long
 #  endif
 #else
-#  define CARES_TYPEOF_ARES_SSIZE_T ssize_t;
+#  define CARES_TYPEOF_ARES_SSIZE_T ssize_t
 #endif
 
 typedef CARES_TYPEOF_ARES_SSIZE_T ares_ssize_t;
