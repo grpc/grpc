3218e2b975a71cc32aedad71d00267e07244c109
Muxi Yan <mxyan@google.com>
2017-09-14 10:29:42 -0700

    minor build fix

--- src/core/lib/compression/stream_compression_gzip.c
+++ src/core/lib/compression/stream_compression_gzip.c
@@ -183,7 +183,7 @@ grpc_stream_compression_context_create_gzip(
   GPR_ASSERT(method == GRPC_STREAM_COMPRESSION_GZIP_COMPRESS ||
              method == GRPC_STREAM_COMPRESSION_GZIP_DECOMPRESS);
   grpc_stream_compression_context_gzip *gzip_ctx =
-      (grpc_stream_compression_context *)gpr_zalloc(
+      (grpc_stream_compression_context_gzip *)gpr_zalloc(
           sizeof(grpc_stream_compression_context_gzip));
   int r;
   if (gzip_ctx == NULL) {
