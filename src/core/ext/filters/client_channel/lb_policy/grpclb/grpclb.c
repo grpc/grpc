667acc818755058431ba3bc20ce5e310f4e0e0f6
Juanli Shen <juanlishen@google.com>
2017-09-18 16:16:24 -0700

    Fix backend restart issue

--- src/core/ext/filters/client_channel/lb_policy/grpclb/grpclb.c
+++ src/core/ext/filters/client_channel/lb_policy/grpclb/grpclb.c
@@ -1820,7 +1820,8 @@ static void glb_update_locked(grpc_exec_ctx *exec_ctx, grpc_lb_policy *policy,
     grpc_lb_addresses_destroy(exec_ctx, glb_policy->fallback_backend_addresses);
     glb_policy->fallback_backend_addresses =
         extract_backend_addresses_locked(exec_ctx, addresses);
-    if (glb_policy->rr_policy != NULL) {
+    if (glb_policy->lb_fallback_timeout_ms > 0 &&
+        !glb_policy->fallback_timer_active) {
       rr_handover_locked(exec_ctx, glb_policy);
     }
   }
