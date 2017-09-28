7c69cd22799baa32da08cbb1fcf50ccb68dd8392
Auxilus <raghavsphadke@gmail.com>
2017-09-28 10:16:21 +0530

    Update extconf.rb

--- src/ruby/ext/grpc/extconf.rb
+++ src/ruby/ext/grpc/extconf.rb
@@ -62,7 +62,7 @@ ENV['ARCH_FLAGS'] = RbConfig::CONFIG['ARCH_FLAG']
 ENV['ARCH_FLAGS'] = '-arch i386 -arch x86_64' if RUBY_PLATFORM =~ /darwin/
 ENV['CFLAGS'] = '-DGPR_BACKWARDS_COMPATIBILITY_MODE'
 
-output_dir = File.expand_path(RbConfig::CONFIG['topdir']) 
+output_dir = File.expand_path(RbConfig::CONFIG['topdir'])
 grpc_lib_dir = File.join(output_dir, 'libs', grpc_config)
 ENV['BUILDDIR'] = output_dir
 
