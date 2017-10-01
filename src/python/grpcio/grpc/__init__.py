5e475d270198b8eb9e503f573225db7f55930ef4
Mehrdad Afshari <mmx@google.com>
2017-09-30 23:53:54 -0700

    Add server interceptors to gRPC Python
    Implements the service-side interceptor machinery for gRPC Python.

--- src/python/grpcio/grpc/__init__.py
+++ src/python/grpcio/grpc/__init__.py
@@ -1021,6 +1021,105 @@ class ServicerContext(six.with_metaclass(abc.ABCMeta, RpcContext)):
         raise NotImplementedError()
 
 
+##############  Service-Side Interceptor Interfaces & Classes  #################
+
+
+class UnaryUnaryServerInterceptor(six.with_metaclass(abc.ABCMeta)):
+    """Affords intercepting unary-unary RPCs on the service-side.
+
+This is an EXPERIMENTAL API and is subject to change without notice."""
+
+    @abc.abstractmethod
+    def intercept_unary_unary_handler(self, handler, method, request,
+                                      servicer_context):
+        """Intercepts unary-unary RPCs on the service-side.
+
+    Args:
+      handler: The handler to continue processing the RPC.
+      It takes a request value and a ServicerContext object
+      and returns a response value.
+      method: The full method name of the RPC.
+      request: The request value for the RPC.
+      servicer_context: The context of the current RPC.
+
+    Returns:
+      The RPC response.
+    """
+        raise NotImplementedError()
+
+
+class UnaryStreamServerInterceptor(six.with_metaclass(abc.ABCMeta)):
+    """Affords intercepting unary-stream RPCs on the service-side.
+
+    This is an EXPERIMENTAL API and is subject to change without notice."""
+
+    @abc.abstractmethod
+    def intercept_unary_stream_handler(self, handler, method, request,
+                                       servicer_context):
+        """Intercepts unary-stream RPCs on the service-side.
+
+    Args:
+      handler: The handler to continue processing the RPC.
+      It takes a request value and a ServicerContext object
+      and returns an iterator of response values.
+      method: The full method name of the RPC.
+      request: The request value for the RPC.
+      servicer_context: The context of the current RPC.
+
+    Returns:
+      An iterator of RPC response values.
+    """
+        raise NotImplementedError()
+
+
+class StreamUnaryServerInterceptor(six.with_metaclass(abc.ABCMeta)):
+    """Affords intercepting stream-unary RPCs on the service-side.
+
+    This is an EXPERIMENTAL API and is subject to change without notice."""
+
+    @abc.abstractmethod
+    def intercept_stream_unary_handler(self, handler, method, request_iterator,
+                                       servicer_context):
+        """Intercepts stream-unary RPCs on the service-side.
+
+    Args:
+      handler: The handler to continue processing the RPC.
+      It takes an iterator of request values and
+      a ServicerContext object and returns a response value.
+      method: The full method name of the RPC.
+      request_iterator: An iterator of request values for the RPC.
+      servicer_context: The context of the current RPC.
+
+    Returns:
+      The RPC response.
+    """
+        raise NotImplementedError()
+
+
+class StreamStreamServerInterceptor(six.with_metaclass(abc.ABCMeta)):
+    """Affords intercepting stream-stream RPCs on the service-side.
+
+    This is an EXPERIMENTAL API and is subject to change without notice."""
+
+    @abc.abstractmethod
+    def intercept_stream_stream_handler(self, handler, method, request_iterator,
+                                        servicer_context):
+        """Intercepts stream-stream RPCs on the service-side.
+
+    Args:
+      handler: The handler to continue processing the RPC.
+      It takes a request value and a ServicerContext object
+      and returns an iterator of response values.
+      method: The full method name of the RPC.
+      request_iterator: An iterator of request values for the RPC.
+      servicer_context: The context of the current RPC.
+
+    Returns:
+      An iterator of RPC response values.
+    """
+        raise NotImplementedError()
+
+
 #####################  Service-Side Handler Interfaces  ########################
 
 
@@ -1504,6 +1603,36 @@ def intercept_channel(channel, *interceptors):
     return _interceptor.intercept_channel(channel, *interceptors)
 
 
+def intercept_server(
+        server,  # pylint: disable=redefined-outer-name
+        *interceptors):
+    """Creates an intercepted server.
+
+  This is an EXPERIMENTAL API and is subject to change without notice.
+
+  Args:
+    server: A Server.
+    interceptors: Zero or more objects of type
+      UnaryUnaryServerInterceptor,
+      UnaryStreamServerInterceptor,
+      StreamUnaryServerInterceptor, or
+      StreamStreamServerInterceptor.
+      Interceptors are given control in the order they are listed.
+
+  Returns:
+    A Server that intercepts each received RPC via the provided interceptors.
+
+  Raises:
+    TypeError: If interceptor does not derive from any of
+      UnaryUnaryServerInterceptor,
+      UnaryStreamServerInterceptor,
+      StreamUnaryServerInterceptor, or
+      StreamStreamServerInterceptor.
+  """
+    from grpc import _interceptor  # pylint: disable=cyclic-import
+    return _interceptor.intercept_server(server, *interceptors)
+
+
 def server(thread_pool,
            handlers=None,
            options=None,
@@ -1539,9 +1668,11 @@ __all__ = (
     'ChannelCredentials', 'CallCredentials', 'AuthMetadataContext',
     'AuthMetadataPluginCallback', 'AuthMetadataPlugin', 'ServerCredentials',
     'UnaryUnaryClientInterceptor', 'UnaryUnaryMultiCallable',
-    'UnaryStreamClientInterceptor', 'UnaryStreamMultiCallable',
+    'UnaryUnaryServerInterceptor', 'UnaryStreamClientInterceptor',
+    'UnaryStreamMultiCallable', 'UnaryStreamServerInterceptor',
     'StreamUnaryClientInterceptor', 'StreamUnaryMultiCallable',
-    'StreamStreamClientInterceptor', 'StreamStreamMultiCallable', 'Channel',
+    'StreamUnaryServerInterceptor', 'StreamStreamClientInterceptor',
+    'StreamStreamMultiCallable', 'StreamStreamServerInterceptor', 'Channel',
     'ServicerContext', 'RpcMethodHandler', 'HandlerCallDetails',
     'GenericRpcHandler', 'ServiceRpcHandler', 'Server',
     'unary_unary_rpc_method_handler', 'unary_stream_rpc_method_handler',
@@ -1550,7 +1681,8 @@ __all__ = (
     'metadata_call_credentials', 'access_token_call_credentials',
     'composite_call_credentials', 'composite_channel_credentials',
     'ssl_server_credentials', 'channel_ready_future', 'insecure_channel',
-    'intercept_channel', 'secure_channel', 'server',)
+    'intercept_channel', 'intercept_server', 'secure_channel', 'server',)
+
 ############################### Extension Shims ################################
 
 # Here to maintain backwards compatibility; avoid using these in new code!
--- src/python/grpcio/grpc/_interceptor.py
+++ src/python/grpcio/grpc/_interceptor.py
@@ -190,3 +190,145 @@ def intercept_channel(channel, *interceptors):
                             'grpc.StreamStreamClientInterceptor or ')
         channel = _InterceptingChannel(channel, interceptor)
     return channel
+
+
+class _InterceptingRpcMethodHandler(grpc.RpcMethodHandler):
+
+    def __init__(self, rpc_method_handler, method, interceptor):
+        self._rpc_method_handler = rpc_method_handler
+        self._method = method
+        self._interceptor = interceptor
+
+    @property
+    def request_streaming(self):
+        return self._rpc_method_handler.request_streaming
+
+    @property
+    def response_streaming(self):
+        return self._rpc_method_handler.response_streaming
+
+    @property
+    def request_deserializer(self):
+        return self._rpc_method_handler.request_deserializer
+
+    @property
+    def response_serializer(self):
+        return self._rpc_method_handler.response_serializer
+
+    @property
+    def unary_unary(self):
+        if not isinstance(self._interceptor, grpc.UnaryUnaryServerInterceptor):
+            return self._rpc_method_handler.unary_unary
+
+        def adaptation(request, servicer_context):
+
+            def handler(request, servicer_context):
+                return self._rpc_method_handler.unary_unary(request,
+                                                            servicer_context)
+
+            return self._interceptor.intercept_unary_unary_handler(
+                handler, self._method, request, servicer_context)
+
+        return adaptation
+
+    @property
+    def unary_stream(self):
+        if not isinstance(self._interceptor, grpc.UnaryStreamServerInterceptor):
+            return self._rpc_method_handler.unary_stream
+
+        def adaptation(request, servicer_context):
+
+            def handler(request, servicer_context):
+                return self._rpc_method_handler.unary_stream(request,
+                                                             servicer_context)
+
+            return self._interceptor.intercept_unary_stream_handler(
+                handler, self._method, request, servicer_context)
+
+        return adaptation
+
+    @property
+    def stream_unary(self):
+        if not isinstance(self._interceptor, grpc.StreamUnaryServerInterceptor):
+            return self._rpc_method_handler.stream_unary
+
+        def adaptation(request_iterator, servicer_context):
+
+            def handler(request_iterator, servicer_context):
+                return self._rpc_method_handler.stream_unary(request_iterator,
+                                                             servicer_context)
+
+            return self._interceptor.intercept_stream_unary_handler(
+                handler, self._method, request_iterator, servicer_context)
+
+        return adaptation
+
+    @property
+    def stream_stream(self):
+        if not isinstance(self._interceptor,
+                          grpc.StreamStreamServerInterceptor):
+            return self._rpc_method_handler.stream_stream
+
+        def adaptation(request_iterator, servicer_context):
+
+            def handler(request_iterator, servicer_context):
+                return self._rpc_method_handler.stream_stream(request_iterator,
+                                                              servicer_context)
+
+            return self._interceptor.intercept_stream_stream_handler(
+                handler, self._method, request_iterator, servicer_context)
+
+        return adaptation
+
+
+class _InterceptingGenericRpcHandler(grpc.GenericRpcHandler):
+
+    def __init__(self, handler, interceptor):
+        self._handler = handler
+        self._interceptor = interceptor
+
+    def service(self, handler_call_details):
+        result = self._handler.service(handler_call_details)
+        if result:
+            result = _InterceptingRpcMethodHandler(
+                result, handler_call_details.method, self._interceptor)
+        return result
+
+
+class _InterceptingServer(grpc.Server):
+
+    def __init__(self, server, interceptor):
+        self._server = server
+        self._interceptor = interceptor
+
+    def add_generic_rpc_handlers(self, generic_rpc_handlers):
+        handlers = (_InterceptingGenericRpcHandler(handler, self._interceptor)
+                    for handler in generic_rpc_handlers)
+        return self._server.add_generic_rpc_handlers(handlers)
+
+    def add_insecure_port(self, *args, **kwargs):
+        return self._server.add_insecure_port(*args, **kwargs)
+
+    def add_secure_port(self, *args, **kwargs):
+        return self._server.add_secure_port(*args, **kwargs)
+
+    def start(self, *args, **kwargs):
+        return self._server.start(*args, **kwargs)
+
+    def stop(self, *args, **kwargs):
+        return self._server.stop(*args, **kwargs)
+
+
+def intercept_server(server, *interceptors):
+    for interceptor in reversed(interceptors):
+        if not isinstance(interceptor, grpc.UnaryUnaryServerInterceptor) and \
+           not isinstance(interceptor, grpc.UnaryStreamServerInterceptor) and \
+           not isinstance(interceptor, grpc.StreamUnaryServerInterceptor) and \
+           not isinstance(interceptor, grpc.StreamStreamServerInterceptor):
+            raise TypeError('interceptor must be '
+                            'grpc.UnaryUnaryServerInterceptor or '
+                            'grpc.UnaryStreamServerInterceptor or '
+                            'grpc.StreamUnaryServerInterceptor or '
+                            'grpc.StreamStreamServerInterceptor or ')
+        server = _InterceptingServer(server, interceptor)
+    return server
