96162c7d1ffd1f2710cb28f8e53e850fac4c7844
Yash Tibrewal <yashkt@google.com>
2017-10-06 15:49:31 -0700

    Flow control fix for designated initializers

--- src/core/ext/transport/chttp2/transport/flow_control.cc
+++ src/core/ext/transport/chttp2/transport/flow_control.cc
@@ -399,15 +399,16 @@ static double get_pid_controller_guess(grpc_exec_ctx* exec_ctx,
   if (!tfc->pid_controller_initialized) {
     tfc->last_pid_update = now;
     tfc->pid_controller_initialized = true;
-    grpc_pid_controller_init(
-        &tfc->pid_controller,
-        (grpc_pid_controller_args){.gain_p = 4,
-                                   .gain_i = 8,
-                                   .gain_d = 0,
-                                   .initial_control_value = target,
-                                   .min_control_value = -1,
-                                   .max_control_value = 25,
-                                   .integral_range = 10});
+    grpc_pid_controller_init(&tfc->pid_controller,
+                             {
+                                 4,       // gain_p
+                                 8,       // gain_i
+                                 0,       // gain_d
+                                 target,  // initial_control_value
+                                 -1,      // min_control_value
+                                 25,      // max_control_value
+                                 10       // integral_range
+                             });
     return pow(2, target);
   }
   double bdp_error = target - grpc_pid_controller_last(&tfc->pid_controller);
