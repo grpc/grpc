4c9fa854f691726e73efeca20e7141c0f8dc10c6
ncteisen <ncteisen@gmail.com>
2017-10-24 08:59:05 -0700

    Add debugging prints

--- tools/profiling/microbenchmarks/bm_json.py
+++ tools/profiling/microbenchmarks/bm_json.py
@@ -157,6 +157,9 @@ def parse_name(name):
     rest = s[0]
     dyn_args = s[1:]
   name = rest
+  print (name)
+  print (dyn_args, _BM_SPECS[name]['dyn'])
+  print (tpl_args, _BM_SPECS[name]['tpl'])
   assert name in _BM_SPECS, '_BM_SPECS needs to be expanded for %s' % name
   assert len(dyn_args) == len(_BM_SPECS[name]['dyn'])
   assert len(tpl_args) == len(_BM_SPECS[name]['tpl'])
