3f6b10afba43e2855efd2fbeb3f1f4ea6c2a9657
Alexander Polcyn <apolcyn@google.com>
2017-10-28 16:57:29 -0700

    Use existing read buffer in security handshaker if present

---
+++ test/core/handshake/server_ssl_common.h
@@ -0,0 +1,22 @@
+//
+// Created by apolcyn on 10/31/17.
+//
+
+#ifndef GRPC_SERVER_SSL_COMMON_H
+#define GRPC_SERVER_SSL_COMMON_H
+
+#include <grpc/grpc.h>
+#include <grpc/grpc_security.h>
+#include <grpc/support/alloc.h>
+#include <grpc/support/log.h>
+#include <grpc/support/string_util.h>
+#include <grpc/support/sync.h>
+#include <grpc/support/thd.h>
+#include "src/core/lib/iomgr/load_file.h"
+#include "test/core/util/port.h"
+#include "test/core/util/test_config.h"
+
+bool server_ssl_test(const char *alpn_list[], unsigned int alpn_list_len,
+                     const char *alpn_expected);
+
+#endif  // GRPC_SERVER_SSL_COMMON_H
