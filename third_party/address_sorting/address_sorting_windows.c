003cf82abe4217a307049f595286a30528ef64cd
Alexander Polcyn <apolcyn@google.com>
2017-11-12 21:50:46 -0800

    Dont include unistd.h on windows

--- third_party/address_sorting/address_sorting_windows.c
+++ third_party/address_sorting/address_sorting_windows.c
@@ -48,7 +48,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <unistd.h>
 
 /* TODO : Fill in these functions; get address sorting functionality to work on
  * windows. */
