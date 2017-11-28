e8b331305cd9613867e5c012a78030b89d9ef814
Jan Tattermusch <jtattermusch@google.com>
2017-11-28 13:54:13 +0100

    basic pooling of requestcallcontext

--- src/csharp/Grpc.Microbenchmarks/SendMessageBenchmark.cs
+++ src/csharp/Grpc.Microbenchmarks/SendMessageBenchmark.cs
@@ -52,7 +52,7 @@ namespace Grpc.Microbenchmarks
 
         private void ThreadBody(int iterations, int payloadSize)
         {
-            var completionRegistry = new CompletionRegistry(environment, () => environment.BatchContextPool.Lease());
+            var completionRegistry = new CompletionRegistry(environment, () => environment.BatchContextPool.Lease(), () => RequestCallContextSafeHandle.Create());
             var cq = CompletionQueueSafeHandle.CreateAsync(completionRegistry);
             var call = CreateFakeCall(cq);
 
