14a4c935dd0407cad121d91dd959ce3aae16b002
Ning Ren <ningr@google.com>
2017-11-29 16:15:26 -0800

    Fix check_bazel_workspace.py, to compare hashes in "git submodule" with those in "bazel/grpc_deps.bzl".

--- tools/run_tests/sanity/check_bazel_workspace.py
+++ tools/run_tests/sanity/check_bazel_workspace.py
@@ -16,7 +16,6 @@
 
 from __future__ import print_function
 
-import ast
 import os
 import re
 import subprocess
@@ -25,25 +24,24 @@ import sys
 os.chdir(os.path.join(os.path.dirname(sys.argv[0]), '../../..'))
 
 git_hash_pattern = re.compile('[0-9a-f]{40}')
+bzl_hash_pattern = re.compile('([0-9a-f]{40})\.tar\.gz')
 
 # Parse git hashes from submodules
 git_submodules = subprocess.check_output('git submodule', shell=True).strip().split('\n')
 git_submodule_hashes = {re.search(git_hash_pattern, s).group() for s in git_submodules}
 
 # Parse git hashes from Bazel WORKSPACE {new_}http_archive rules
-with open('WORKSPACE', 'r') as f:
-  workspace_rules = [expr.value for expr in ast.parse(f.read()).body]
-
-http_archive_rules = [rule for rule in workspace_rules if rule.func.id.endswith('http_archive')]
-archive_urls = [kw.value.s for rule in http_archive_rules for kw in rule.keywords if kw.arg == 'url']
-workspace_git_hashes = {re.search(git_hash_pattern, url).group() for url in archive_urls}
+bzl_hashes = {}
+with open('bazel/grpc_deps.bzl', 'r') as f:
+  bzl_matches = [re.search(bzl_hash_pattern, s) for s in f]
+  bzl_hashes = {m.group(1) for m in bzl_matches if m}
 
 # Validate the equivalence of the git submodules and Bazel git dependencies. The
 # condition we impose is that there is a git submodule for every dependency in
 # the workspace, but not necessarily conversely. E.g. Bloaty is a dependency
 # not used by any of the targets built by Bazel.
-if len(workspace_git_hashes - git_submodule_hashes) > 0:
-    print("Found discrepancies between git submodules and Bazel WORKSPACE dependencies")
+if bzl_hashes != git_submodule_hashes:
+    print("Found discrepancies between git submodules and Bazel dependencies (in bazel/grpc_deps.bzl)")
     sys.exit(1)
 
 sys.exit(0)
