0780538bc3027e93bd1afabff3564dfac8b24c58
Mehrdad Afshari <mmx@google.com>
2017-12-05 14:19:34 -0800

    Bump version number to 1.7.3

--- src/php/ext/grpc/version.h
+++ src/php/ext/grpc/version.h
@@ -20,6 +20,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PHP_GRPC_VERSION "1.7.2"
+#define PHP_GRPC_VERSION "1.7.3"
 
 #endif /* VERSION_H */
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.7.2'
+    VERSION = '1.7.3'
   end
 end
