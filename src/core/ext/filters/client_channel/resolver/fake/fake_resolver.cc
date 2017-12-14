f60c326c61d9b8523b404c7d6178a7ec856cc203
Juanli Shen <juanlishen@google.com>
2017-12-13 18:21:50 -0800

    Revert changing _set_response to sync

--- src/core/ext/filters/client_channel/resolver/fake/fake_resolver.cc
+++ src/core/ext/filters/client_channel/resolver/fake/fake_resolver.cc
@@ -146,22 +146,47 @@ void grpc_fake_resolver_response_generator_unref(
   }
 }
 
+typedef struct set_response_closure_arg {
+  grpc_closure set_response_closure;
+  grpc_fake_resolver_response_generator* generator;
+  grpc_channel_args* response;
+  bool upon_error;
+} set_response_closure_arg;
+
+static void set_response_closure_fn(void* arg, grpc_error* error) {
+  set_response_closure_arg* closure_arg = (set_response_closure_arg*)arg;
+  grpc_fake_resolver_response_generator* generator = closure_arg->generator;
+  fake_resolver* r = generator->resolver;
+  bool upon_error = closure_arg->upon_error;
+  if (r->results_upon_error != nullptr) {
+    grpc_channel_args_destroy(r->results_upon_error);
+  }
+  r->results_upon_error = closure_arg->response;
+  if (r->next_results != nullptr) {
+    grpc_channel_args_destroy(r->next_results);
+  }
+  r->next_results =
+      upon_error ? nullptr : grpc_channel_args_copy(closure_arg->response);
+  gpr_free(closure_arg);
+  if (!upon_error) {
+    fake_resolver_maybe_finish_next_locked(r);
+  }
+}
+
 void grpc_fake_resolver_response_generator_set_response(
     grpc_fake_resolver_response_generator* generator,
     grpc_channel_args* response, bool upon_error) {
   GPR_ASSERT(generator->resolver != nullptr);
-  if (generator->resolver->next_results != nullptr) {
-    grpc_channel_args_destroy(generator->resolver->next_results);
-  }
-  generator->resolver->next_results =
-      upon_error ? nullptr : grpc_channel_args_copy(response);
-  if (generator->resolver->results_upon_error != nullptr) {
-    grpc_channel_args_destroy(generator->resolver->results_upon_error);
-  }
-  generator->resolver->results_upon_error = grpc_channel_args_copy(response);
-  if (!upon_error) {
-    fake_resolver_maybe_finish_next_locked(generator->resolver);
-  }
+  set_response_closure_arg* closure_arg =
+      (set_response_closure_arg*)gpr_zalloc(sizeof(*closure_arg));
+  closure_arg->generator = generator;
+  closure_arg->response = grpc_channel_args_copy(response);
+  closure_arg->upon_error = upon_error;
+  GRPC_CLOSURE_SCHED(GRPC_CLOSURE_INIT(&closure_arg->set_response_closure,
+                                       set_response_closure_fn, closure_arg,
+                                       grpc_combiner_scheduler(
+                                           generator->resolver->base.combiner)),
+                     GRPC_ERROR_NONE);
 }
 
 static void* response_generator_arg_copy(void* p) {
