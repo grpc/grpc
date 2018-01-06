1164fd6fca13e0b072dd8f08880534481455c15a
Muxi Yan <mxyan@google.com>
2018-01-05 17:22:13 -0800

    Fix copyright information

--- examples/cpp/helloworld/cocoapods/HelloWorldCpp/ViewController.mm
+++ examples/cpp/helloworld/cocoapods/HelloWorldCpp/ViewController.mm
@@ -1,10 +1,20 @@
-//
-//  ViewController.m
-//  HelloWorldCpp
-//
-//  Created by Muxi Yan on 1/3/18.
-//  Copyright © 2018 gRPC. All rights reserved.
-//
+/*
+ *
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 #import "ViewController.h"
 #import <grpc++/grpc++.h>
