e2e41ff12e9a66503472f3ffbd8abba97ebb9ad6
Ken Payson <kpayson@google.com>
2018-01-18 15:22:35 -0800

    Allow turning off fork support with env variable

--- src/core/lib/support/fork.cc
+++ src/core/lib/support/fork.cc
@@ -38,18 +38,25 @@ void grpc_fork_support_init() {
   fork_support_enabled = 1;
 #else
   fork_support_enabled = 0;
+#endif
   char* env = gpr_getenv("GRPC_ENABLE_FORK_SUPPORT");
   if (env != nullptr) {
     static const char* truthy[] = {"yes",  "Yes",  "YES", "true",
                                    "True", "TRUE", "1"};
+    static const char* falsey[] = {"no",    "No",    "NO", "false",
+                                   "False", "FALSE", "0"};
     for (size_t i = 0; i < GPR_ARRAY_SIZE(truthy); i++) {
       if (0 == strcmp(env, truthy[i])) {
         fork_support_enabled = 1;
       }
     }
+    for (size_t i = 0; i < GPR_ARRAY_SIZE(falsey); i++) {
+      if (0 == strcmp(env, falsey[i])) {
+        fork_support_enabled = 0;
+      }
+    }
     gpr_free(env);
   }
-#endif
   if (override_fork_support_enabled != -1) {
     fork_support_enabled = override_fork_support_enabled;
   }
