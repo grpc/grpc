10e602b1274ed1db47764fbe12d2043378fafc7c
Adele Zhou <adelez@google.com>
2018-02-27 11:21:18 -0800

    Increased timeout.

--- tools/internal_ci/linux/grpc_tsan_on_foundry.sh
+++ tools/internal_ci/linux/grpc_tsan_on_foundry.sh
@@ -38,7 +38,7 @@ source tools/internal_ci/helper_scripts/prepare_build_linux_rc
 "${KOKORO_GFILE_DIR}/bazel_wrapper.py" \
   --host_jvm_args=-Dbazel.DigestFunction=SHA256 \
   test --jobs="50" \
-  --test_timeout="1500,1500,1500,3600" \
+  --test_timeout="3600,3600,3600,3600" \
   --test_output=errors  \
   --verbose_failures=true  \
   --keep_going  \
