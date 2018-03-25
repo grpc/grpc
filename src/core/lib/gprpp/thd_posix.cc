c3d1190566016c2a3e8021de5aecd958b059cb0c
tanfenghua <29357216@qq.com>
2018-03-26 07:56:13 +0800

    The success is a pointer.

--- src/core/lib/gprpp/thd_posix.cc
+++ src/core/lib/gprpp/thd_posix.cc
@@ -110,7 +110,7 @@ class ThreadInternalsPosix
 
     GPR_ASSERT(pthread_attr_destroy(&attr) == 0);
 
-    if (!success) {
+    if (!(*success)) {
       /* don't use gpr_free, as this was allocated using malloc (see above) */
       free(info);
       dec_thd_count();
