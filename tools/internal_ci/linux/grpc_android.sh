6f6b83e6b45714814ce3a079c6949cfaaa6ee393
Eric Gribkoff <ericgribkoff@google.com>
2018-04-06 10:44:28 -0700

    Run C++ Android interops on Firebase Test Lab

--- tools/internal_ci/linux/grpc_android.sh
+++ tools/internal_ci/linux/grpc_android.sh
@@ -31,7 +31,32 @@ cd third_party/protobuf
 git fetch
 git cherry-pick 7daa320065f3bea2b54bf983337d1724f153422d -m 1
 
-cd ../../examples/android/helloworld
+
+# Build and run interop instrumentation tests on Firebase Test Lab
+
+cd "${REPO_ROOT}/src/android/test/interop/"
+./gradlew assembleDebug \
+    "-Pprotoc=${REPO_ROOT}/third_party/protobuf/src/protoc" \
+    "-Pgrpc_cpp_plugin=${REPO_ROOT}/bins/opt/grpc_cpp_plugin"
+./gradlew assembleDebugAndroidTest \
+    "-Pprotoc=${REPO_ROOT}/third_party/protobuf/src/protoc" \
+    "-Pgrpc_cpp_plugin=${REPO_ROOT}/bins/opt/grpc_cpp_plugin"
+gcloud firebase test android run \
+    --type instrumentation \
+    --app app/build/outputs/apk/debug/app-debug.apk \
+    --test app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+    --device model=Nexus6P,version=27,locale=en,orientation=portrait \
+    --device model=Nexus6P,version=26,locale=en,orientation=portrait \
+    --device model=Nexus6P,version=25,locale=en,orientation=portrait \
+    --device model=Nexus6P,version=24,locale=en,orientation=portrait \
+    --device model=Nexus6P,version=23,locale=en,orientation=portrait \
+    --device model=Nexus6,version=22,locale=en,orientation=portrait \
+    --device model=Nexus6,version=21,locale=en,orientation=portrait
+
+
+# Build hello world example
+
+cd "${REPO_ROOT}/examples/android/helloworld"
 ./gradlew build \
     "-Pprotoc=${REPO_ROOT}/third_party/protobuf/src/protoc" \
     "-Pgrpc_cpp_plugin=${REPO_ROOT}/bins/opt/grpc_cpp_plugin"
