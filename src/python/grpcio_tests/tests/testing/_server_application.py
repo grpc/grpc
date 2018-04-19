ce8e2fcd16fbfa35365bb69b03118d8ed98a8372
Mehrdad Afshari <mehrdada@users.noreply.github.com>
2018-04-18 18:10:35 -0700

    Eliminate unreachable suppression

--- src/python/grpcio_tests/tests/testing/_server_application.py
+++ src/python/grpcio_tests/tests/testing/_server_application.py
@@ -38,7 +38,7 @@ class FirstServiceServicer(services_pb2_grpc.FirstServiceServicer):
             context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
             context.set_details('Something is wrong with your request!')
         return
-        yield services_pb2.Strange()
+        yield services_pb2.Strange()  # pylint: disable=unreachable
 
     def StreUn(self, request_iterator, context):
         context.send_initial_metadata(((
