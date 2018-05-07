5792c052dd839ae24cb0924077634cb794617b37
Adele Zhou <adelez@google.com>
2018-05-07 11:17:47 -0700

    Update test jobs to 800 and set runs_per_test back to 2 to detect flakes.

--- tools/internal_ci/linux/grpc_bazel_on_foundry_dbg.sh
+++ tools/internal_ci/linux/grpc_bazel_on_foundry_dbg.sh
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-EXTRA_FLAGS="-c dbg --test_timeout=300,450,1200,3600 --runs_per_test_detects_flakes --runs_per_test=1"
+EXTRA_FLAGS="-c dbg --test_timeout=300,450,1200,3600 --runs_per_test_detects_flakes --runs_per_test=2"
 github/grpc/tools/internal_ci/linux/grpc_bazel_on_foundry_base.sh "${EXTRA_FLAGS}"
 
