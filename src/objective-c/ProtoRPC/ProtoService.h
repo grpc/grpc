5a412ac1130e602779478c2250dc8ac4b1dbcdcb
Juanli Shen <aspirinsjl@gmail.com>
2018-05-07 20:25:44 -0700

    Revert "Revert "Revert "Provide protocol for initializer of generated messages"""

--- src/objective-c/ProtoRPC/ProtoService.h
+++ src/objective-c/ProtoRPC/ProtoService.h
@@ -22,12 +22,6 @@
 @protocol GRXWriteable;
 @class GRXWriter;
 
-@protocol GRPCProtoServiceInit
-
-- (instancetype)initWithHost:(NSString *)host;
-
-@end
-
 __attribute__((deprecated("Please use GRPCProtoService."))) @interface ProtoService
     : NSObject -
       (instancetype)initWithHost : (NSString *)host packageName
