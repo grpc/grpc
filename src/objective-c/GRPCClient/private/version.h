16e58a2b2cf35484293ed0f43feced2093d4d2c8
Mehrdad Afshari <mmx@google.com>
2018-05-11 11:47:12 -0700

    Regenerate projects

--- src/objective-c/GRPCClient/private/version.h
+++ src/objective-c/GRPCClient/private/version.h
@@ -23,4 +23,4 @@
 // `tools/buildgen/generate_projects.sh`.
 
 
-#define GRPC_OBJC_VERSION_STRING @"1.11.1-pre1"
+#define GRPC_OBJC_VERSION_STRING @"1.11.1"
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.11.1.pre1'
+    VERSION = '1.11.1'
   end
 end
