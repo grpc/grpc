cbe33ccced13af6c0c3b1e74f32b7a45f925e85d
Carl Mastrangelo <notcarl@google.com>
2018-05-21 13:00:13 -0700

    Add GA tracking modifier

---
+++ tools/doc/addga.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Copyright 2018 The gRPC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exu -o pipefail
+
+# This script finds all html files in the current directory, and adds the
+# GA tracking snippet to them.
+
+read -r -d '' SNIPPET << EOF
+<script type="text/javascript">
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-60127042-1', 'auto');
+    ga('send', 'pageview');
+</script>
+EOF
+
+S="$(echo -n $SNIPPET | tr '\n' ' ')"
+
+for M in $(find -name \*.html);
+do
+  grep -q "i,s,o,g,r,a,m" "$M"
+  if [[ $? -ne 0 ]]; then
+    sed -i "s_</head>_${S}</head>_" "$M"
+  fi
+done;
