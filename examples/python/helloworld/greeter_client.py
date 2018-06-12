8234d14113f484c60ee8633dc060f8054adf3748
Nathaniel Manista <nathaniel@google.com>
2018-06-12 17:30:23 +0000

    Close channels in examples

--- examples/python/helloworld/greeter_client.py
+++ examples/python/helloworld/greeter_client.py
@@ -22,9 +22,12 @@ import helloworld_pb2_grpc
 
 
 def run():
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = helloworld_pb2_grpc.GreeterStub(channel)
-    response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+    # used in circumstances in which the with statement does not fit the needs
+    # of the code.
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
     print("Greeter client received: " + response.message)
 
 
