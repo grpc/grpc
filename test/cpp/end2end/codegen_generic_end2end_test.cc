c1961285fbfd585d6dc87a0d766138e34df93529
ncteisen <ncteisen@gmail.com>
2018-06-19 09:46:34 -0700

    Build out codegen test

--- test/cpp/end2end/codegen_generic_end2end_test.cc
+++ test/cpp/end2end/codegen_generic_end2end_test.cc
@@ -42,6 +42,7 @@
 
 #include <gtest/gtest.h>
 
+using grpc::ByteBuffer;
 using grpc::testing::EchoRequest;
 using grpc::testing::EchoResponse;
 
@@ -236,21 +237,21 @@ class CodegenGenericEnd2EndTest : public ::testing::Test {
 
   // For the client application to populate and send to server.
   EchoRequest send_request_;
-  ::grpc::ByteBuffer send_request_buffer_;
+  ByteBuffer send_request_buffer_;
 
   // For the server to give to gRPC to be populated by incoming request
   // from client.
   EchoRequest recv_request_;
-  ::grpc::ByteBuffer recv_request_buffer_;
+  ByteBuffer recv_request_buffer_;
 
   // For the server application to populate and send back to client.
   EchoResponse send_response_;
-  ::grpc::ByteBuffer send_response_buffer_;
+  ByteBuffer send_response_buffer_;
 
   // For the client to give to gRPC to be populated by incoming response
   // from server.
   EchoResponse recv_response_;
-  ::grpc::ByteBuffer recv_response_buffer_;
+  ByteBuffer recv_response_buffer_;
   Status recv_status_;
 
   // Both sides need contexts
@@ -449,6 +450,320 @@ TEST_F(CodegenGenericEnd2EndTest, CompileTest) {
       SType;
   ResetStub();
   auto service = BuildAndStartServer<SType>();
+}
+
+// Client uses proto, server uses generic codegen request, unary
+TEST_F(CodegenGenericEnd2EndTest, CodegenGenericRequestServerUnary) {
+  typedef grpc::testing::EchoTestService::WithCodegenGenericRequestMethod_Echo<
+      grpc::testing::EchoTestService::Service>
+      SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+  grpc::ServerAsyncResponseWriter<EchoResponse> response_writer(&srv_ctx_);
+
+  send_request_.set_message("hello unary");
+  std::unique_ptr<ClientAsyncResponseReader<EchoResponse>> response_reader(
+      stub_->AsyncEcho(&cli_ctx_, send_request_, cq_.get()));
+  service->RequestEcho(&srv_ctx_, &recv_request_buffer_, &response_writer,
+                       cq_.get(), cq_.get(), tag(2));
+  response_reader->Finish(&recv_response_, &recv_status_, tag(4));
+  Verifier().Expect(2, true).Verify(cq_.get());
+  EXPECT_TRUE(ParseFromByteBuffer(&recv_request_buffer_, &recv_request_));
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+  send_response_.set_message(recv_request_.message());
+  response_writer.Finish(send_response_, Status::OK, tag(3));
+  Verifier().Expect(3, true).Expect(4, true).Verify(cq_.get());
+
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+  EXPECT_TRUE(recv_status_.ok());
+}
+
+// Client uses proto, server uses generic codegen request, client streaming
+TEST_F(CodegenGenericEnd2EndTest, CodegenGenericRequestServerClientStreaming) {
+  typedef grpc::testing::EchoTestService::
+      WithCodegenGenericRequestMethod_RequestStream<
+          grpc::testing::EchoTestService::Service>
+          SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+
+  grpc::ServerAsyncReader<EchoResponse, ByteBuffer> srv_stream(&srv_ctx_);
+
+  send_request_.set_message("hello client streaming");
+  std::unique_ptr<ClientAsyncWriter<EchoRequest>> cli_stream(
+      stub_->AsyncRequestStream(&cli_ctx_, &recv_response_, cq_.get(), tag(1)));
+
+  service->RequestRequestStream(&srv_ctx_, &srv_stream, cq_.get(), cq_.get(),
+                                tag(2));
+
+  Verifier().Expect(2, true).Expect(1, true).Verify(cq_.get());
+
+  cli_stream->Write(send_request_, tag(3));
+  srv_stream.Read(&recv_request_buffer_, tag(4));
+  Verifier().Expect(3, true).Expect(4, true).Verify(cq_.get());
+  ParseFromByteBuffer(&recv_request_buffer_, &recv_request_);
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+
+  cli_stream->Write(send_request_, tag(5));
+  srv_stream.Read(&recv_request_buffer_, tag(6));
+  Verifier().Expect(5, true).Expect(6, true).Verify(cq_.get());
+
+  ParseFromByteBuffer(&recv_request_buffer_, &recv_request_);
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+  cli_stream->WritesDone(tag(7));
+  srv_stream.Read(&recv_request_buffer_, tag(8));
+  Verifier().Expect(7, true).Expect(8, false).Verify(cq_.get());
+
+  ParseFromByteBuffer(&recv_request_buffer_, &recv_request_);
+  send_response_.set_message(recv_request_.message());
+  srv_stream.Finish(send_response_, Status::OK, tag(9));
+  cli_stream->Finish(&recv_status_, tag(10));
+  Verifier().Expect(9, true).Expect(10, true).Verify(cq_.get());
+
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+  EXPECT_TRUE(recv_status_.ok());
+}
+
+// Client uses proto, server uses generic codegen request, server streaming
+TEST_F(CodegenGenericEnd2EndTest, CodegenGenericRequestServerServerStreaming) {
+  typedef grpc::testing::EchoTestService::
+      WithCodegenGenericRequestMethod_ResponseStream<
+          grpc::testing::EchoTestService::Service>
+          SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+  grpc::ServerAsyncWriter<EchoResponse> srv_stream(&srv_ctx_);
+
+  send_request_.set_message("hello server streaming");
+  std::unique_ptr<ClientAsyncReader<EchoResponse>> cli_stream(
+      stub_->AsyncResponseStream(&cli_ctx_, send_request_, cq_.get(), tag(1)));
+
+  service->RequestResponseStream(&srv_ctx_, &recv_request_buffer_, &srv_stream,
+                                 cq_.get(), cq_.get(), tag(2));
+
+  Verifier().Expect(1, true).Expect(2, true).Verify(cq_.get());
+  ParseFromByteBuffer(&recv_request_buffer_, &recv_request_);
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+
+  send_response_.set_message(recv_request_.message());
+  srv_stream.Write(send_response_, tag(3));
+  cli_stream->Read(&recv_response_, tag(4));
+  Verifier().Expect(3, true).Expect(4, true).Verify(cq_.get());
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+
+  srv_stream.Write(send_response_, tag(5));
+  cli_stream->Read(&recv_response_, tag(6));
+  Verifier().Expect(5, true).Expect(6, true).Verify(cq_.get());
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+
+  srv_stream.Finish(Status::OK, tag(7));
+  cli_stream->Read(&recv_response_, tag(8));
+  Verifier().Expect(7, true).Expect(8, false).Verify(cq_.get());
+
+  cli_stream->Finish(&recv_status_, tag(9));
+  Verifier().Expect(9, true).Verify(cq_.get());
+
+  EXPECT_TRUE(recv_status_.ok());
+}
+
+// Client uses proto, server uses generic codegen request, bidi streaming
+TEST_F(CodegenGenericEnd2EndTest, CodegenGenericRequestServerBidiStreaming) {
+  typedef grpc::testing::EchoTestService::
+      WithCodegenGenericRequestMethod_BidiStream<
+          grpc::testing::EchoTestService::Service>
+          SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+
+  grpc::ServerAsyncReaderWriter<EchoResponse, ByteBuffer> srv_stream(&srv_ctx_);
+
+  send_request_.set_message("hello bidi streaming");
+  std::unique_ptr<ClientAsyncReaderWriter<EchoRequest, EchoResponse>>
+      cli_stream(stub_->AsyncBidiStream(&cli_ctx_, cq_.get(), tag(1)));
+
+  service->RequestBidiStream(&srv_ctx_, &srv_stream, cq_.get(), cq_.get(),
+                             tag(2));
+
+  Verifier().Expect(1, true).Expect(2, true).Verify(cq_.get());
+
+  cli_stream->Write(send_request_, tag(3));
+  srv_stream.Read(&recv_request_buffer_, tag(4));
+  Verifier().Expect(3, true).Expect(4, true).Verify(cq_.get());
+  ParseFromByteBuffer(&recv_request_buffer_, &recv_request_);
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+
+  send_response_.set_message(recv_request_.message());
+  srv_stream.Write(send_response_, tag(5));
+  cli_stream->Read(&recv_response_, tag(6));
+  Verifier().Expect(5, true).Expect(6, true).Verify(cq_.get());
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+
+  cli_stream->WritesDone(tag(7));
+  srv_stream.Read(&recv_request_buffer_, tag(8));
+  Verifier().Expect(7, true).Expect(8, false).Verify(cq_.get());
+
+  srv_stream.Finish(Status::OK, tag(9));
+  cli_stream->Finish(&recv_status_, tag(10));
+  Verifier().Expect(9, true).Expect(10, true).Verify(cq_.get());
+
+  EXPECT_TRUE(recv_status_.ok());
+}
+
+// Client uses proto, server uses generic response codegen, unary
+TEST_F(CodegenGenericEnd2EndTest, CodegenGenericReponseServerUnary) {
+  typedef grpc::testing::EchoTestService::WithCodegenGenericResponseMethod_Echo<
+      grpc::testing::EchoTestService::Service>
+      SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+  grpc::GenericServerAsyncResponseWriter response_writer(&srv_ctx_);
+
+  send_request_.set_message("hello unary");
+  std::unique_ptr<ClientAsyncResponseReader<EchoResponse>> response_reader(
+      stub_->AsyncEcho(&cli_ctx_, send_request_, cq_.get()));
+  service->RequestEcho(&srv_ctx_, &recv_request_, &response_writer, cq_.get(),
+                       cq_.get(), tag(2));
+  response_reader->Finish(&recv_response_, &recv_status_, tag(4));
+  Verifier().Expect(2, true).Verify(cq_.get());
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+  send_response_.set_message(recv_request_.message());
+  EXPECT_TRUE(
+      SerializeToByteBufferInPlace(&send_response_, &send_response_buffer_));
+  response_writer.Finish(send_response_buffer_, Status::OK, tag(3));
+  Verifier().Expect(3, true).Expect(4, true).Verify(cq_.get());
+
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+  EXPECT_TRUE(recv_status_.ok());
+}
+
+// Client uses proto, server uses generic response codegen, client streaming
+TEST_F(CodegenGenericEnd2EndTest, CodegenGenericResponseServerClientStreaming) {
+  typedef grpc::testing::EchoTestService::
+      WithCodegenGenericResponseMethod_RequestStream<
+          grpc::testing::EchoTestService::Service>
+          SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+
+  grpc::ServerAsyncReader<ByteBuffer, EchoRequest> srv_stream(&srv_ctx_);
+
+  send_request_.set_message("hello client streaming");
+  std::unique_ptr<ClientAsyncWriter<EchoRequest>> cli_stream(
+      stub_->AsyncRequestStream(&cli_ctx_, &recv_response_, cq_.get(), tag(1)));
+
+  service->RequestRequestStream(&srv_ctx_, &srv_stream, cq_.get(), cq_.get(),
+                                tag(2));
+
+  Verifier().Expect(2, true).Expect(1, true).Verify(cq_.get());
+
+  cli_stream->Write(send_request_, tag(3));
+  srv_stream.Read(&recv_request_, tag(4));
+  Verifier().Expect(3, true).Expect(4, true).Verify(cq_.get());
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+
+  cli_stream->Write(send_request_, tag(5));
+  srv_stream.Read(&recv_request_, tag(6));
+  Verifier().Expect(5, true).Expect(6, true).Verify(cq_.get());
+
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+  cli_stream->WritesDone(tag(7));
+  srv_stream.Read(&recv_request_, tag(8));
+  Verifier().Expect(7, true).Expect(8, false).Verify(cq_.get());
+
+  send_response_.set_message(recv_request_.message());
+  SerializeToByteBufferInPlace(&send_response_, &send_response_buffer_);
+  srv_stream.Finish(send_response_buffer_, Status::OK, tag(9));
+  cli_stream->Finish(&recv_status_, tag(10));
+  Verifier().Expect(9, true).Expect(10, true).Verify(cq_.get());
+
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+  EXPECT_TRUE(recv_status_.ok());
+}
+
+// Client uses proto, server uses generic response codegen, server streaming
+TEST_F(CodegenGenericEnd2EndTest, CodegenGenericResponseServerServerStreaming) {
+  typedef grpc::testing::EchoTestService::
+      WithCodegenGenericMethod_ResponseStream<
+          grpc::testing::EchoTestService::Service>
+          SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+  grpc::GenericServerAsyncWriter srv_stream(&srv_ctx_);
+
+  send_request_.set_message("hello server streaming");
+  std::unique_ptr<ClientAsyncReader<EchoResponse>> cli_stream(
+      stub_->AsyncResponseStream(&cli_ctx_, send_request_, cq_.get(), tag(1)));
+
+  service->RequestResponseStream(&srv_ctx_, &recv_request_buffer_, &srv_stream,
+                                 cq_.get(), cq_.get(), tag(2));
+
+  Verifier().Expect(1, true).Expect(2, true).Verify(cq_.get());
+  ParseFromByteBuffer(&recv_request_buffer_, &recv_request_);
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+
+  send_response_.set_message(recv_request_.message());
+  SerializeToByteBufferInPlace(&send_response_, &send_response_buffer_);
+  srv_stream.Write(send_response_buffer_, tag(3));
+  cli_stream->Read(&recv_response_, tag(4));
+  Verifier().Expect(3, true).Expect(4, true).Verify(cq_.get());
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+
+  srv_stream.Write(send_response_buffer_, tag(5));
+  cli_stream->Read(&recv_response_, tag(6));
+  Verifier().Expect(5, true).Expect(6, true).Verify(cq_.get());
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+
+  srv_stream.Finish(Status::OK, tag(7));
+  cli_stream->Read(&recv_response_, tag(8));
+  Verifier().Expect(7, true).Expect(8, false).Verify(cq_.get());
+
+  cli_stream->Finish(&recv_status_, tag(9));
+  Verifier().Expect(9, true).Verify(cq_.get());
+
+  EXPECT_TRUE(recv_status_.ok());
+}
+
+// Client uses proto, server uses generic response codegen, bidi streaming
+TEST_F(CodegenGenericEnd2EndTest, CodegenGenericResponseServerBidiStreaming) {
+  typedef grpc::testing::EchoTestService::
+      WithCodegenGenericResponseMethod_BidiStream<
+          grpc::testing::EchoTestService::Service>
+          SType;
+  ResetStub();
+  auto service = BuildAndStartServer<SType>();
+
+  grpc::ServerAsyncReaderWriter<ByteBuffer, EchoRequest> srv_stream(&srv_ctx_);
+
+  send_request_.set_message("hello bidi streaming");
+  std::unique_ptr<ClientAsyncReaderWriter<EchoRequest, EchoResponse>>
+      cli_stream(stub_->AsyncBidiStream(&cli_ctx_, cq_.get(), tag(1)));
+
+  service->RequestBidiStream(&srv_ctx_, &srv_stream, cq_.get(), cq_.get(),
+                             tag(2));
+
+  Verifier().Expect(1, true).Expect(2, true).Verify(cq_.get());
+
+  cli_stream->Write(send_request_, tag(3));
+  srv_stream.Read(&recv_request_, tag(4));
+  Verifier().Expect(3, true).Expect(4, true).Verify(cq_.get());
+  EXPECT_EQ(send_request_.message(), recv_request_.message());
+
+  send_response_.set_message(recv_request_.message());
+  SerializeToByteBufferInPlace(&send_response_, &send_response_buffer_);
+  srv_stream.Write(send_response_buffer_, tag(5));
+  cli_stream->Read(&recv_response_, tag(6));
+  Verifier().Expect(5, true).Expect(6, true).Verify(cq_.get());
+  EXPECT_EQ(send_response_.message(), recv_response_.message());
+
+  cli_stream->WritesDone(tag(7));
+  srv_stream.Read(&recv_request_, tag(8));
+  Verifier().Expect(7, true).Expect(8, false).Verify(cq_.get());
+
+  srv_stream.Finish(Status::OK, tag(9));
+  cli_stream->Finish(&recv_status_, tag(10));
+  Verifier().Expect(9, true).Expect(10, true).Verify(cq_.get());
+
+  EXPECT_TRUE(recv_status_.ok());
 }
 
 }  // namespace
