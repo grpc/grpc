ed0dc067bac46de439fd57d6ae706c4157eb1bec
Mehrdad Afshari <mmx@google.com>
2018-06-22 11:07:45 -0700

    Regenerate Projects

--- src/cpp/common/version_cc.cc
+++ src/cpp/common/version_cc.cc
@@ -22,5 +22,5 @@
 #include <grpcpp/grpcpp.h>
 
 namespace grpc {
-grpc::string Version() { return "1.13.0-pre2"; }
+grpc::string Version() { return "1.13.0-pre3"; }
 }  // namespace grpc
--- src/php/ext/grpc/version.h
+++ src/php/ext/grpc/version.h
@@ -20,6 +20,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PHP_GRPC_VERSION "1.13.0RC2"
+#define PHP_GRPC_VERSION "1.13.0RC3"
 
 #endif /* VERSION_H */
