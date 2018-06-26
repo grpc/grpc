513d5fba60654389dece186393188dcdcd303b02
Juanli Shen <juanlishen@google.com>
2018-06-25 17:06:53 -0700

    Fix multiple definition

--- src/core/ext/filters/load_reporting/registered_opencensus_objects.h
+++ src/core/ext/filters/load_reporting/registered_opencensus_objects.h
@@ -21,8 +21,6 @@
 
 #include <grpc/support/port_platform.h>
 
-#include <grpc/support/log.h>
-
 #include "opencensus/stats/stats.h"
 
 #include "src/cpp/server/load_reporter/constants.h"
@@ -30,85 +28,84 @@
 namespace grpc {
 namespace load_reporter {
 
+// Note that the functions here are specified as inline to share the static
+// objects across all the translation units including this header. See more
+// details on https://en.cppreference.com/w/cpp/language/inline.
+
 // Measures.
 
-// TODO(juanlishen): This seems weird, but without the dummy logging,
-// server_load_reporting_end2end_test will fail on the opt build. See
-// https://github.com/census-instrumentation/opencensus-cpp/issues/164.
-
-::opencensus::stats::MeasureInt64 MeasureStartCount() {
-  gpr_log(GPR_DEBUG, "MeasureStartCount()");
-  static const ::opencensus::stats::MeasureInt64 start_count =
+inline ::opencensus::stats::MeasureInt64 MeasureStartCount() {
+  static const ::opencensus::stats::MeasureInt64 measure =
       ::opencensus::stats::MeasureInt64::Register(
           kMeasureStartCount, kMeasureStartCount, kMeasureStartCount);
-  return start_count;
+  return measure;
 }
 
-::opencensus::stats::MeasureInt64 MeasureEndCount() {
-  gpr_log(GPR_DEBUG, "MeasureEndCount()");
-  static const ::opencensus::stats::MeasureInt64 end_count =
+inline ::opencensus::stats::MeasureInt64 MeasureEndCount() {
+  static const ::opencensus::stats::MeasureInt64 measure =
       ::opencensus::stats::MeasureInt64::Register(
           kMeasureEndCount, kMeasureEndCount, kMeasureEndCount);
-  return end_count;
+  return measure;
 }
 
-::opencensus::stats::MeasureInt64 MeasureEndBytesSent() {
-  gpr_log(GPR_DEBUG, "MeasureEndBytesSent()");
-  static const ::opencensus::stats::MeasureInt64 end_bytes_sent =
+inline ::opencensus::stats::MeasureInt64 MeasureEndBytesSent() {
+  static const ::opencensus::stats::MeasureInt64 measure =
       ::opencensus::stats::MeasureInt64::Register(
           kMeasureEndBytesSent, kMeasureEndBytesSent, kMeasureEndBytesSent);
-  return end_bytes_sent;
+  return measure;
 }
 
-::opencensus::stats::MeasureInt64 MeasureEndBytesReceived() {
-  gpr_log(GPR_DEBUG, "MeasureEndBytesReceived()");
-  static const ::opencensus::stats::MeasureInt64 end_bytes_received =
+inline ::opencensus::stats::MeasureInt64 MeasureEndBytesReceived() {
+  static const ::opencensus::stats::MeasureInt64 measure =
       ::opencensus::stats::MeasureInt64::Register(kMeasureEndBytesReceived,
                                                   kMeasureEndBytesReceived,
                                                   kMeasureEndBytesReceived);
-  return end_bytes_received;
+  return measure;
 }
 
-::opencensus::stats::MeasureInt64 MeasureEndLatencyMs() {
-  gpr_log(GPR_DEBUG, "MeasureEndLatencyMs()");
-  static const ::opencensus::stats::MeasureInt64 end_latency_ms =
+inline ::opencensus::stats::MeasureInt64 MeasureEndLatencyMs() {
+  static const ::opencensus::stats::MeasureInt64 measure =
       ::opencensus::stats::MeasureInt64::Register(
           kMeasureEndLatencyMs, kMeasureEndLatencyMs, kMeasureEndLatencyMs);
-  return end_latency_ms;
+  return measure;
 }
 
-::opencensus::stats::MeasureDouble MeasureOtherCallMetric() {
-  gpr_log(GPR_DEBUG, "MeasureOtherCallMetric");
-  static const ::opencensus::stats::MeasureDouble other_call_metric =
+inline ::opencensus::stats::MeasureDouble MeasureOtherCallMetric() {
+  static const ::opencensus::stats::MeasureDouble measure =
       ::opencensus::stats::MeasureDouble::Register(kMeasureOtherCallMetric,
                                                    kMeasureOtherCallMetric,
                                                    kMeasureOtherCallMetric);
-  return other_call_metric;
+  return measure;
 }
 
 // Tags.
 
-opencensus::stats::TagKey TagKeyToken() {
-  static const auto token = opencensus::stats::TagKey::Register(kTagKeyToken);
+inline ::opencensus::stats::TagKey TagKeyToken() {
+  static const ::opencensus::stats::TagKey token =
+      opencensus::stats::TagKey::Register(kTagKeyToken);
   return token;
 }
 
-opencensus::stats::TagKey TagKeyHost() {
-  static const auto token = opencensus::stats::TagKey::Register(kTagKeyHost);
-  return token;
-}
-opencensus::stats::TagKey TagKeyUserId() {
-  static const auto token = opencensus::stats::TagKey::Register(kTagKeyUserId);
+inline ::opencensus::stats::TagKey TagKeyHost() {
+  static const ::opencensus::stats::TagKey token =
+      opencensus::stats::TagKey::Register(kTagKeyHost);
   return token;
 }
 
-opencensus::stats::TagKey TagKeyStatus() {
-  static const auto token = opencensus::stats::TagKey::Register(kTagKeyStatus);
+inline ::opencensus::stats::TagKey TagKeyUserId() {
+  static const ::opencensus::stats::TagKey token =
+      opencensus::stats::TagKey::Register(kTagKeyUserId);
   return token;
 }
 
-opencensus::stats::TagKey TagKeyMetricName() {
-  static const auto token =
+inline ::opencensus::stats::TagKey TagKeyStatus() {
+  static const ::opencensus::stats::TagKey token =
+      opencensus::stats::TagKey::Register(kTagKeyStatus);
+  return token;
+}
+
+inline ::opencensus::stats::TagKey TagKeyMetricName() {
+  static const ::opencensus::stats::TagKey token =
       opencensus::stats::TagKey::Register(kTagKeyMetricName);
   return token;
 }
