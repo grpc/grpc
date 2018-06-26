95800f416268f27f2563ffdcf3847fe9f930a1c2
Hope Casey-Allen <hcaseyal@google.com>
2018-06-26 15:11:28 -0700

    Remove allocation in server_auth_filter

--- src/core/lib/security/transport/server_auth_filter.cc
+++ src/core/lib/security/transport/server_auth_filter.cc
@@ -215,7 +215,7 @@ static grpc_error* init_call_elem(grpc_call_element* elem,
   // data and save it in the call context.
   grpc_server_security_context* server_ctx =
       grpc_server_security_context_create(args->arena);
-  server_ctx->auth_context = grpc_auth_context_create(chand->auth_context);
+  server_ctx->auth_context = GRPC_AUTH_CONTEXT_REF(chand->auth_context);
   calld->auth_context = server_ctx->auth_context;
   if (args->context[GRPC_CONTEXT_SECURITY].value != nullptr) {
     args->context[GRPC_CONTEXT_SECURITY].destroy(
