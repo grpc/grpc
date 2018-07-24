056d061b3138ed26afe3132ed0d3f7d900d4735b
Jan Tattermusch <jtattermusch@users.noreply.github.com>
2018-07-24 19:29:04 +0200

    Fix license year

--- tools/run_tests/artifacts/build_artifact_csharp_ios.sh
+++ tools/run_tests/artifacts/build_artifact_csharp_ios.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2016 The gRPC Authors
+# Copyright 2018 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
