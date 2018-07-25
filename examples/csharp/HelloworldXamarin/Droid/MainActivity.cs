705843c041f24ce5b2737fc15dfd45c34f26cf82
Jan Tattermusch <jtattermusch@google.com>
2018-07-25 17:30:19 +0200

    experimental helloworld client for android

--- examples/csharp/HelloworldXamarin/Droid/MainActivity.cs
+++ examples/csharp/HelloworldXamarin/Droid/MainActivity.cs
@@ -1,13 +1,15 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Grpc.Core;
+using Helloworld;
 
 namespace HelloworldXamarin.Droid
 {
     [Activity(Label = "HelloworldXamarin", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        //int count = 1;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -20,8 +22,26 @@ namespace HelloworldXamarin.Droid
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            button.Click += delegate { SayHello(button); };
         }
+
+            private void SayHello(Button button)
+            {
+
+              // use loopback on host machine: https://developer.android.com/studio/run/emulator-networking
+              Channel channel = new Channel("10.0.2.2:50051", ChannelCredentials.Insecure);
+
+              var client = new Greeter.GreeterClient(channel);
+              string user = "Xamarin";
+
+              var reply = client.SayHello(new HelloRequest { Name = user });
+
+              button.Text = "Greeting: " + reply.Message;
+
+              channel.ShutdownAsync().Wait();
+            }
     }
+
+    
 }
 
