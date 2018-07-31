d7b669fc07bb1a8c078ff49a11c18bee52fc602b
Alex Polcyn <apolcyn@google.com>
2018-07-31 22:32:35 +0000

    Make method const

--- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_windows.cc
+++ src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_windows.cc
@@ -363,7 +363,7 @@ class GrpcPolledFdWindows : public GrpcPolledFd {
     ScheduleAndNullWriteClosure(error);
   }
 
-  const bool gotten_into_driver_list() { return gotten_into_driver_list_; }
+  bool gotten_into_driver_list() const { return gotten_into_driver_list_; }
   void set_gotten_into_driver_list() { gotten_into_driver_list_ = true; }
 
   grpc_combiner* combiner_;
