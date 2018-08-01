2794a6cae6631b4b6412d8e2ac0ba41a79d37f5a
Srini Polavarapu <psrini@google.com>
2018-08-01 16:22:01 -0700

    regenerate projects

--- src/csharp/Grpc.Core/VersionInfo.cs
+++ src/csharp/Grpc.Core/VersionInfo.cs
@@ -38,6 +38,6 @@ namespace Grpc.Core
         /// <summary>
         /// Current version of gRPC C#
         /// </summary>
-        public const string CurrentVersion = "1.14.0-pre2";
+        public const string CurrentVersion = "1.14.0";
     }
 }
