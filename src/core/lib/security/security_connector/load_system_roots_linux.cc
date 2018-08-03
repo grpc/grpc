4651f94d35263ffb5284f87c2162fed5395230e6
tdbhacks <aburaggina@google.com>
2018-08-03 10:35:24 -0700

    Opening dir once to get total size + filenames in CreateRootCertsBundle

--- src/core/lib/security/security_connector/load_system_roots_linux.cc
+++ src/core/lib/security/security_connector/load_system_roots_linux.cc
@@ -41,6 +41,7 @@
 #include "src/core/lib/gpr/env.h"
 #include "src/core/lib/gpr/string.h"
 #include "src/core/lib/gpr/useful.h"
+#include "src/core/lib/gprpp/inlined_vector.h"
 #include "src/core/lib/iomgr/load_file.h"
 
 namespace grpc_core {
@@ -66,45 +67,16 @@ grpc_slice GetSystemRootCerts() {
   return grpc_empty_slice();
 }
 
-void GetAbsoluteFilePath(const char* path_buffer,
-                                          const char* valid_file_dir,
-                                          const char* file_entry_name) {
+void GetAbsoluteFilePath(const char* path_buffer, const char* valid_file_dir,
+                         const char* file_entry_name) {
   if (valid_file_dir == nullptr || file_entry_name == nullptr) {
     path_buffer = nullptr;
   }
-  // static char absolute_path[MAXPATHLEN];
-  int path_len = snprintf((char*)path_buffer, MAXPATHLEN, "%s/%s", valid_file_dir,
-                          file_entry_name);
+  int path_len = snprintf((char*)path_buffer, MAXPATHLEN, "%s/%s",
+                          valid_file_dir, file_entry_name);
   if (path_len == 0) {
     path_buffer = nullptr;
   }
-}
-
-size_t GetDirectoryTotalSize(const char* directory_path) {
-  struct dirent* directory_entry;
-  size_t total_size = 0;
-  // TODO: add logging when using opendir.
-  DIR* ca_directory = opendir(directory_path);
-  if (ca_directory == nullptr) {
-    return total_size;
-  }
-  while ((directory_entry = readdir(ca_directory)) != nullptr) {
-    struct stat dir_entry_stat;
-    const char* file_entry_name = directory_entry->d_name;
-    const char* file_path = static_cast<char*>(gpr_malloc(MAXPATHLEN));
-    GetAbsoluteFilePath(file_path, directory_path, file_entry_name);
-    int stat_return = stat(file_path, &dir_entry_stat);
-    gpr_free((char*)file_path);
-    if (stat_return == -1 || S_ISDIR(dir_entry_stat.st_mode) ||
-        strcmp(directory_entry->d_name, ".") == 0 ||
-        strcmp(directory_entry->d_name, "..") == 0) {
-      // no subdirectories.
-      continue;
-    }
-    total_size += dir_entry_stat.st_size;
-  }
-  closedir(ca_directory);
-  return total_size;
 }
 
 grpc_slice CreateRootCertsBundle(const char* certs_directory) {
@@ -114,12 +86,10 @@ grpc_slice CreateRootCertsBundle(const char* certs_directory) {
   }
   struct dirent* directory_entry;
   char* bundle_string = nullptr;
-  size_t total_bundle_size = GetDirectoryTotalSize(certs_directory);
-  bundle_string = static_cast<char*>(gpr_zalloc(total_bundle_size + 1));
+  InlinedVector<const char*, 1> roots_filenames;
+  size_t total_bundle_size = 0;
   DIR* ca_directory = opendir(certs_directory);
-  size_t bytes_read = 0;
   if (ca_directory == nullptr) {
-    gpr_free(bundle_string);
     return bundle_slice;
   }
   while ((directory_entry = readdir(ca_directory)) != nullptr) {
@@ -131,10 +101,23 @@ grpc_slice CreateRootCertsBundle(const char* certs_directory) {
     if (stat_return == -1 || S_ISDIR(dir_entry_stat.st_mode) ||
         strcmp(directory_entry->d_name, ".") == 0 ||
         strcmp(directory_entry->d_name, "..") == 0) {
-        if (file_path != nullptr) {
-          gpr_free((char*)file_path);
-        }
       // no subdirectories.
+      continue;
+    }
+    roots_filenames.push_back(file_path);
+    total_bundle_size += dir_entry_stat.st_size;
+  }
+  closedir(ca_directory);
+
+  bundle_string = static_cast<char*>(gpr_zalloc(total_bundle_size + 1));
+  const char* file_path = static_cast<char*>(gpr_malloc(MAXPATHLEN));
+  size_t bytes_read = 0;
+  for (size_t i = 0; i < roots_filenames.size(); i++) {
+    struct stat dir_entry_stat;
+    file_path = roots_filenames[i];
+    int stat_return = stat(file_path, &dir_entry_stat);
+    if (stat_return == -1) {
+      gpr_free((char*)file_path);
       continue;
     }
     int file_descriptor = open(file_path, O_RDONLY);
@@ -151,7 +134,6 @@ grpc_slice CreateRootCertsBundle(const char* certs_directory) {
     }
     gpr_free((char*)file_path);
   }
-  closedir(ca_directory);
   bundle_slice = grpc_slice_new(bundle_string, bytes_read, gpr_free);
   return bundle_slice;
 }
@@ -170,10 +152,8 @@ grpc_slice LoadSystemRootCerts() {
     result = GetSystemRootCerts();
   }
   if (GRPC_SLICE_IS_EMPTY(result)) {
-    for (size_t i = 0;
-         i < GPR_ARRAY_SIZE(linux_cert_directories_); i++) {
-      result = CreateRootCertsBundle(
-          linux_cert_directories_[i]);
+    for (size_t i = 0; i < GPR_ARRAY_SIZE(linux_cert_directories_); i++) {
+      result = CreateRootCertsBundle(linux_cert_directories_[i]);
       if (!GRPC_SLICE_IS_EMPTY(result)) {
         break;
       }
