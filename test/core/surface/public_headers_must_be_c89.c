b59d8674d24d9b40d8d8b2b40f2be0118ff524d2
Eric Gribkoff <ericgribkoff@google.com>
2018-08-10 10:13:36 -0700

    Python post-fork handler: exit if grpc shutdown fails

--- test/core/surface/public_headers_must_be_c89.c
+++ test/core/surface/public_headers_must_be_c89.c
@@ -77,6 +77,7 @@ int main(int argc, char **argv) {
   printf("%lx", (unsigned long) grpc_register_plugin);
   printf("%lx", (unsigned long) grpc_init);
   printf("%lx", (unsigned long) grpc_shutdown);
+  printf("%lx", (unsigned long) grpc_is_initialized);
   printf("%lx", (unsigned long) grpc_version_string);
   printf("%lx", (unsigned long) grpc_g_stands_for);
   printf("%lx", (unsigned long) grpc_completion_queue_factory_lookup);
