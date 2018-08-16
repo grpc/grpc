b840d5e45c03d7a3a5d371d0358b673a07f0dc65
Yang Gao <yangg@google.com>
2018-08-16 08:58:26 -0700

    Revert "Add more filter priority levels"

--- src/core/ext/filters/client_channel/lb_policy/grpclb/grpclb.cc
+++ src/core/ext/filters/client_channel/lb_policy/grpclb/grpclb.cc
@@ -1890,7 +1890,7 @@ void grpc_lb_policy_grpclb_init() {
           grpc_core::UniquePtr<grpc_core::LoadBalancingPolicyFactory>(
               grpc_core::New<grpc_core::GrpcLbFactory>()));
   grpc_channel_init_register_stage(GRPC_CLIENT_SUBCHANNEL,
-                                   GRPC_CHANNEL_INIT_PRIORITY_LOW,
+                                   GRPC_CHANNEL_INIT_BUILTIN_PRIORITY,
                                    maybe_add_client_load_reporting_filter,
                                    (void*)&grpc_client_load_reporting_filter);
 }
