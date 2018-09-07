6465fa21ea08b29278ded286149c64355735f92e
Juanli Shen <juanlishen@google.com>
2018-09-06 18:01:19 -0700

    Fix tsan

--- src/core/ext/filters/client_channel/subchannel_index.cc
+++ src/core/ext/filters/client_channel/subchannel_index.cc
@@ -96,7 +96,7 @@ static grpc_subchannel_key* subchannel_key_copy(grpc_subchannel_key* k) {
 int grpc_subchannel_key_compare(const grpc_subchannel_key* a,
                                 const grpc_subchannel_key* b) {
   // To pretend the keys are different, return a non-zero value.
-  if (g_force_creation) return 1;
+  if (GPR_UNLIKELY(gpr_atm_no_barrier_load(&g_force_creation))) return 1;
   int c = GPR_ICMP(a->args.filter_count, b->args.filter_count);
   if (c != 0) return c;
   if (a->args.filter_count > 0) {
@@ -314,5 +314,5 @@ void grpc_subchannel_index_shutdown(void) {
 }
 
 void grpc_subchannel_index_test_only_set_force_creation(bool force_creation) {
-  g_force_creation = force_creation;
+  gpr_atm_no_barrier_store(&g_force_creation, force_creation);
 }
