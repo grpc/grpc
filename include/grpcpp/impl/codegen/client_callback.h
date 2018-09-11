c82e94f1fe24e956fff794c978bf3c244f46e3db
Vijay Pai <vpai@google.com>
2018-09-11 14:48:06 -0700

    Allow second form of operator delete

--- include/grpcpp/impl/codegen/client_callback.h
+++ include/grpcpp/impl/codegen/client_callback.h
@@ -129,6 +129,13 @@ class ClientCallbackReaderWriter final {
     assert(size == sizeof(ClientCallbackReaderWriter));
   }
 
+  // This operator should never be called as the memory should be freed as part
+  // of the arena destruction. It only exists to provide a matching operator
+  // delete to the operator new so that some compilers will not complain (see
+  // https://github.com/grpc/grpc/issues/11301) Note at the time of adding this
+  // there are no tests catching the compiler warning.
+  static void operator delete(void*, void*) { assert(0); }
+
   void StartCall(std::function<void(Status)> on_completion) {
     StartCall(std::move(on_completion), nullptr);
   }
