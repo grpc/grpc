87592fe9d9c54d6178966f40f4182e0bf6445380
Jan Tattermusch <jtattermusch@google.com>
2018-09-14 11:47:44 -0700

    support custom logfile name in jobset.py

--- tools/run_tests/python_utils/jobset.py
+++ tools/run_tests/python_utils/jobset.py
@@ -176,13 +176,15 @@ class JobSpec(object):
                  timeout_retries=0,
                  kill_handler=None,
                  cpu_cost=1.0,
-                 verbose_success=False):
+                 verbose_success=False,
+                 logfilename=None):
         """
     Arguments:
       cmdline: a list of arguments to pass as the command line
       environ: a dictionary of environment variables to set in the child process
       kill_handler: a handler that will be called whenever job.kill() is invoked
       cpu_cost: number of cores per second this job needs
+      logfilename: use given file to store job's output, rather than using a temporary file
     """
         if environ is None:
             environ = {}
@@ -197,6 +199,10 @@ class JobSpec(object):
         self.kill_handler = kill_handler
         self.cpu_cost = cpu_cost
         self.verbose_success = verbose_success
+        self.logfilename = logfilename
+        if self.logfilename and self.flake_retries != 0 and self.timeout_retries != 0:
+            raise Exception(
+                'Cannot use custom logfile when retries are enabled')
 
     def identity(self):
         return '%r %r' % (self.cmdline, self.environ)
@@ -261,7 +267,15 @@ class Job(object):
         return self._spec
 
     def start(self):
-        self._tempfile = tempfile.TemporaryFile()
+        if self._spec.logfilename:
+            # make sure the log directory exists
+            logfile_dir = os.path.dirname(
+                os.path.abspath(self._spec.logfilename))
+            if not os.path.exists(logfile_dir):
+                os.makedirs(logfile_dir)
+            self._tempfile = open(self._spec.logfilename, 'w+')
+        else:
+            self._tempfile = tempfile.TemporaryFile()
         env = dict(os.environ)
         env.update(self._spec.environ)
         env.update(self._add_env)
