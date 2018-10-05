d8293c8ec969b1bd7f2884a1884b0e3421f22566
Moiz Haidry <mhaidry@google.com>
2018-10-05 16:10:40 -0700

    Redesigned the callback benchmark. The design supports running multiple channels with multiple RPCs per channel

--- test/cpp/qps/client_callback.cc
+++ test/cpp/qps/client_callback.cc
@@ -43,13 +43,15 @@ namespace testing {
  * Maintains context info per RPC
  */
 struct CallbackClientRpcContext {
-  CallbackClientRpcContext() : response_(), context_(), alarm_() {}
+  CallbackClientRpcContext(BenchmarkService::Stub* stub)
+      : response_(), context_(), alarm_(), stub_(stub) {}
 
   ~CallbackClientRpcContext() {}
 
   SimpleResponse response_;
   ClientContext context_;
   Alarm alarm_;
+  BenchmarkService::Stub* stub_;
 };
 
 static std::unique_ptr<BenchmarkService::Stub> BenchmarkStubCreator(
@@ -64,22 +66,25 @@ class CallbackClient
       : ClientImpl<BenchmarkService::Stub, SimpleRequest>(
             config, BenchmarkStubCreator) {
     num_threads_ = NumThreads(config);
-    threads_done_ = 0;
+    rpcs_done_ = 0;
     SetupLoadTest(config, num_threads_);
+    total_outstanding_rpcs_ =
+        config.client_channels() * config.outstanding_rpcs_per_channel();
   }
 
   virtual ~CallbackClient() {}
 
  protected:
   size_t num_threads_;
+  size_t total_outstanding_rpcs_;
   // The below mutex and condition variable is used by main benchmark thread to
   // wait on completion of all RPCs before shutdown
   std::mutex shutdown_mu_;
   std::condition_variable shutdown_cv_;
-  // Number of threads that have finished issuing RPCs
-  size_t threads_done_;
-  // Context data pointers for running a RPC
-  std::vector<std::unique_ptr<CallbackClientRpcContext>> ctxs_;
+  // Number of rpcs done after thread completion
+  size_t rpcs_done_;
+  // Per Thread Queue of Context data pointers for running a RPC
+  std::vector<std::vector<std::unique_ptr<CallbackClientRpcContext>>> ctxs_;
   virtual void InitThreadFuncImpl(size_t thread_idx) = 0;
   virtual bool ThreadFuncImpl(Thread* t, size_t thread_idx) = 0;
 
@@ -88,17 +93,18 @@ class CallbackClient
     ThreadFuncImpl(t, thread_idx);
   }
 
+  virtual void ScheduleRpc(Thread* t, size_t thread_idx, size_t queue_idx) = 0;
+
   /**
    * The main thread of the benchmark will be waiting on DestroyMultithreading.
-   * Increment the threads_done_ variable to signify that all Callback RPCs
-   * started by this particular thread have been completed and it is ok to join
-   * the thread. When the last thread increments the counter it should also
-   * signal the main thread's conditional variable.
+   * Increment the rpcs_done_ variable to signify that the Callback RPC
+   * after thread completion is done. When the last outstanding rpc increments
+   * the counter it should also signal the main thread's conditional variable.
    */
   void NotifyMainThreadOfThreadCompletion(size_t thread_idx) {
     std::lock_guard<std::mutex> l(shutdown_mu_);
-    threads_done_++;
-    if (threads_done_ == num_threads_) {
+    rpcs_done_++;
+    if (rpcs_done_ == total_outstanding_rpcs_) {
       shutdown_cv_.notify_one();
     }
   }
@@ -114,11 +120,11 @@ class CallbackClient
   }
 
   /**
-   * Wait until Callback RPCs invoked by all threads are done
+   * Wait until all outstanding Callback RPCs are done
    */
   void DestroyMultithreading() final {
     std::unique_lock<std::mutex> l(shutdown_mu_);
-    while (threads_done_ != num_threads_) {
+    while (rpcs_done_ != total_outstanding_rpcs_) {
       shutdown_cv_.wait(l);
     }
     EndThreads();
@@ -129,22 +135,21 @@ class CallbackUnaryClient final : public CallbackClient {
  public:
   CallbackUnaryClient(const ClientConfig& config) : CallbackClient(config) {
     ctxs_.resize(num_threads_);
+    for (size_t ch = 0; ch < config.client_channels(); ch++) {
+      for (size_t i = 0; i < config.outstanding_rpcs_per_channel(); i++) {
+        int bucket = (i * ch) % num_threads_;
+        ctxs_[bucket].emplace_back(
+            new CallbackClientRpcContext(channels_[ch].get_stub()));
+      }
+    }
     StartThreads(num_threads_);
   }
   ~CallbackUnaryClient() {}
 
  protected:
   bool ThreadFuncImpl(Thread* t, size_t thread_idx) override {
-    if (!closed_loop_) {
-      gpr_timespec next_issue_time = NextIssueTime(thread_idx);
-      // Start an alarm callback to run the internal callback after
-      // next_issue_time
-      ctxs_[thread_idx]->alarm_.experimental().Set(
-          next_issue_time, [this, t, thread_idx](bool ok) {
-            IssueUnaryCallbackRpc(t, thread_idx);
-          });
-    } else {
-      IssueUnaryCallbackRpc(t, thread_idx);
+    for (size_t i = 0; i < ctxs_[thread_idx].size(); i++) {
+      ScheduleRpc(t, thread_idx, i);
     }
     return true;
   }
@@ -152,18 +157,30 @@ class CallbackUnaryClient final : public CallbackClient {
   /**
    * Setup context data before running RPC
    */
-  void InitThreadFuncImpl(size_t thread_idx) override {
-    ctxs_[thread_idx].reset(new CallbackClientRpcContext);
-  }
+  void InitThreadFuncImpl(size_t thread_idx) override { return; }
 
  private:
-  void IssueUnaryCallbackRpc(Thread* t, size_t thread_idx) {
-    auto* stub = channels_[thread_idx % channels_.size()].get_stub();
+  void ScheduleRpc(Thread* t, size_t thread_idx, size_t queue_idx) override {
+    if (!closed_loop_) {
+      gpr_timespec next_issue_time = NextIssueTime(thread_idx);
+      // Start an alarm callback to run the internal callback after
+      // next_issue_time
+      ctxs_[thread_idx][queue_idx]->alarm_.experimental().Set(
+          next_issue_time, [this, t, thread_idx, queue_idx](bool ok) {
+            IssueUnaryCallbackRpc(t, thread_idx, queue_idx);
+          });
+    } else {
+      IssueUnaryCallbackRpc(t, thread_idx, queue_idx);
+    }
+  }
+
+  void IssueUnaryCallbackRpc(Thread* t, size_t thread_idx, size_t queue_idx) {
     GPR_TIMER_SCOPE("CallbackUnaryClient::ThreadFunc", 0);
     double start = UsageTimer::Now();
-    stub->experimental_async()->UnaryCall(
-        &ctxs_[thread_idx]->context_, &request_, &ctxs_[thread_idx]->response_,
-        [this, t, thread_idx, start](grpc::Status s) {
+    ctxs_[thread_idx][queue_idx]->stub_->experimental_async()->UnaryCall(
+        std::move(&ctxs_[thread_idx][queue_idx]->context_), &request_,
+        &ctxs_[thread_idx][queue_idx]->response_,
+        [this, t, thread_idx, start, queue_idx](grpc::Status s) {
           // Update Histogram with data from the callback run
           HistogramEntry entry;
           if (s.ok()) {
@@ -172,12 +189,15 @@ class CallbackUnaryClient final : public CallbackClient {
           entry.set_status(s.error_code());
           t->UpdateHistogram(&entry);
 
-          if (ThreadCompleted()) {
+          if (ThreadCompleted() || !s.ok()) {
             // Notify thread of completion
             NotifyMainThreadOfThreadCompletion(thread_idx);
           } else {
+            // Reallocate ctx for next RPC
+            ctxs_[thread_idx][queue_idx].reset(new CallbackClientRpcContext(
+                ctxs_[thread_idx][queue_idx]->stub_));
             // Schedule a new RPC
-            ThreadFunc(thread_idx, t);
+            ScheduleRpc(t, thread_idx, queue_idx);
           }
         });
   }
