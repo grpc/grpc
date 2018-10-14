9158493c41f4d47f5409e40760afe6babf17bb2e
kkm <kkm@smartaction.com>
2018-10-14 04:17:08 -0700

    Improve ProtoToolsPlatformTask tests

--- src/csharp/Grpc.Tools.Tests/ProtoToolsPlatformTaskTest.cs
+++ src/csharp/Grpc.Tools.Tests/ProtoToolsPlatformTaskTest.cs
@@ -16,38 +16,107 @@
 
 #endregion
 
-using System;
+using System.Runtime.InteropServices;  // For NETCORE tests.
 using Microsoft.Build.Framework;
 using Moq;
 using NUnit.Framework;
 
 namespace Grpc.Tools.Tests {
-  // This test requires that environment variables be set to the expected
-  // output of the task in its external test harness:
-  //   PROTOTOOLS_TEST_CPU = { x64 | x86 }
-  //   PROTOTOOLS_TEST_OS = { linux | macosx | windows }
   public class ProtoToolsPlatformTaskTest {
-    static string s_expectOs;
-    static string s_expectCpu;
+    ProtoToolsPlatform _task;
+    int _cpuMatched, _osMatched;
 
     [OneTimeSetUp]
-    public static void Init() {
-      s_expectCpu = Environment.GetEnvironmentVariable("PROTOTOOLS_TEST_CPU");
-      s_expectOs = Environment.GetEnvironmentVariable("PROTOTOOLS_TEST_OS");
-      if (s_expectCpu == null || s_expectOs == null)
-        Assert.Inconclusive("This test requires PROTOTOOLS_TEST_CPU and " +
-          "PROTOTOOLS_TEST_OS set in the environment to match the OS it runs on.");
+    public void SetUp() {
+      var mockEng = new Mock<IBuildEngine>();
+      _task = new ProtoToolsPlatform() {
+        BuildEngine = mockEng.Object
+      };
+      _task.Execute();
+    }
+
+    [OneTimeTearDown]
+    public void TearDown() {
+      Assert.AreEqual(1, _cpuMatched, "CPU type detection failed");
+      Assert.AreEqual(1, _osMatched, "OS detection failed");
+    }
+
+#if NETCORE
+    // PlatformAttribute not yet available in NUnit, coming soon:
+    // https://github.com/nunit/nunit/pull/3003.
+    // Use same test case names as under the full framework.
+    [Test]
+    public void CpuIsX86() {
+      if (RuntimeInformation.OSArchitecture == Architecture.X86) {
+        _cpuMatched++;
+        Assert.AreEqual("x86", _task.Cpu);
+      }
     }
 
     [Test]
-    public void CpuAndOsMatchExpected() {
-      var mockEng = new Mock<IBuildEngine>();
-      var task = new ProtoToolsPlatform() {
-        BuildEngine = mockEng.Object
-      };
-      task.Execute();
-      Assert.AreEqual(s_expectCpu, task.Cpu);
-      Assert.AreEqual(s_expectOs, task.Os);
+    public void CpuIsX64() {
+      if (RuntimeInformation.OSArchitecture == Architecture.X64) {
+        _cpuMatched++;
+        Assert.AreEqual("x64", _task.Cpu);
+      }
     }
+
+    [Test]
+    public void OsIsWindows() {
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+        _osMatched++;
+        Assert.AreEqual("windows", _task.Os);
+      }
+    }
+
+    [Test]
+    public void OsIsLinux() {
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+        _osMatched++;
+        Assert.AreEqual("linux", _task.Os);
+      }
+    }
+
+    [Test]
+    public void OsIsMacOsX() {
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+        _osMatched++;
+        Assert.AreEqual("macosx", _task.Os);
+      }
+    }
+
+#else  // !NETCORE, i.e. full framework.
+
+    [Test, Platform("32-Bit")]
+    public void CpuIsX86() {
+      _cpuMatched++;
+      Assert.AreEqual("x86", _task.Cpu);
+    }
+
+    [Test, Platform("64-Bit")]
+    public void CpuIsX64() {
+      _cpuMatched++;
+      Assert.AreEqual("x64", _task.Cpu);
+    }
+
+    [Test, Platform("Win")]
+    public void OsIsWindows() {
+      _osMatched++;
+      Assert.AreEqual("windows", _task.Os);
+    }
+
+    [Test, Platform("Linux")]
+    public void OsIsLinux() {
+      _osMatched++;
+      Assert.AreEqual("linux", _task.Os);
+    }
+
+    [Test, Platform("MacOSX")]
+    public void OsIsMacOsX() {
+      _osMatched++;
+      Assert.AreEqual("macosx", _task.Os);
+    }
+
+#endif  // NETCORE
   };
 }
