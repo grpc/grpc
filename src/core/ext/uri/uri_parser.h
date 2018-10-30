830c7bb4596342c2f093f7b49b631f0b3c5ff53d
ncteisen <ncteisen@gmail.com>
2018-10-30 11:58:43 -0700

    Sanity headers

--- src/core/ext/uri/uri_parser.h
+++ src/core/ext/uri/uri_parser.h
@@ -16,8 +16,8 @@
  *
  */
 
-#ifndef GRPC_CORE_EXT_FILTERS_CLIENT_CHANNEL_URI_PARSER_H
-#define GRPC_CORE_EXT_FILTERS_CLIENT_CHANNEL_URI_PARSER_H
+#ifndef GRPC_CORE_EXT_URI_URI_PARSER_H
+#define GRPC_CORE_EXT_URI_URI_PARSER_H
 
 #include <grpc/support/port_platform.h>
 
@@ -47,4 +47,4 @@ const char* grpc_uri_get_query_arg(const grpc_uri* uri, const char* key);
 /** destroy a uri */
 void grpc_uri_destroy(grpc_uri* uri);
 
-#endif /* GRPC_CORE_EXT_FILTERS_CLIENT_CHANNEL_URI_PARSER_H */
+#endif /* GRPC_CORE_EXT_URI_URI_PARSER_H */
