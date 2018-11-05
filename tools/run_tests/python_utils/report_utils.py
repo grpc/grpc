35c6e5541b256d6cce1d85db64533d847c3cc1cc
Richard Belleville <rbellevi@google.com>
2018-11-05 09:40:59 -0800

    Restore unnecessary 2to3 change

--- tools/run_tests/python_utils/report_utils.py
+++ tools/run_tests/python_utils/report_utils.py
@@ -13,8 +13,6 @@
 # limitations under the License.
 """Generate XML and HTML test reports."""
 
-
-
 try:
     from mako.runtime import Context
     from mako.template import Template
@@ -33,7 +31,9 @@ def _filter_msg(msg, output_format):
     if output_format in ['XML', 'HTML']:
         # keep whitespaces but remove formfeed and vertical tab characters
         # that make XML report unparseable.
-        filtered_msg = [x for x in msg.decode('UTF-8', 'ignore') if x in string.printable and x != '\f' and x != '\v']
+        filtered_msg = filter(
+            lambda x: x in string.printable and x != '\f' and x != '\v',
+            msg.decode('UTF-8', 'ignore'))
         if output_format == 'HTML':
             filtered_msg = filtered_msg.replace('"', '&quot;')
         return filtered_msg
