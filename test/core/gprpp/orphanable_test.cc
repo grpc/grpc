4345ef121a3422bd5de4d99bd758e4fd8567680d
Soheil Hassas Yeganeh <soheil@google.com>
2018-11-30 13:40:12 -0500

    Add debug-only tracing to grpc_core::RefCount
    Also, this patch removes the *WithTracing variants in favor of the new
    API.

--- test/core/gprpp/orphanable_test.cc
+++ test/core/gprpp/orphanable_test.cc
@@ -83,11 +83,11 @@ TEST(OrphanablePtr, InternallyRefCounted) {
 // things build properly in both debug and non-debug cases.
 DebugOnlyTraceFlag baz_tracer(true, "baz");
 
-class Baz : public InternallyRefCountedWithTracing<Baz> {
+class Baz : public InternallyRefCounted<Baz> {
  public:
   Baz() : Baz(0) {}
   explicit Baz(int value)
-      : InternallyRefCountedWithTracing<Baz>(&baz_tracer), value_(value) {}
+      : InternallyRefCounted<Baz>(&baz_tracer), value_(value) {}
   void Orphan() override { Unref(); }
   int value() const { return value_; }
 
