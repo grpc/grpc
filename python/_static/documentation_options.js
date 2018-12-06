5810a180617e5572507b9e0092e51386013fd80a
Lidi Zheng <lidiz@google.com>
2018-12-06 11:55:42 -0800

    Auto-update Python documentation

--- python/_static/documentation_options.js
+++ python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.16.0',
+    VERSION: '1.17.1rc1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
