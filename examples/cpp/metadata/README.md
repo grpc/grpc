580d43d03f20b0b92f4b7ae950225a1dcd3387a0
Hope Casey-Allen <hcaseyal@google.com>
2018-12-13 16:55:08 -0800

    Address reviewer comments and remove binary files

--- examples/cpp/metadata/README.md
+++ examples/cpp/metadata/README.md
@@ -57,12 +57,9 @@ If things go smoothly, you will see in the client terminal:
 
 And in the server terminal:
 
-"Header key: custom-bin , value: "
+"Header key: custom-bin , value: 01234567"
 "Header key: custom-header , value: Custom Value"
 "Header key: user-agent , value: grpc-c++/1.16.0-dev grpc-c/6.0.0-dev (linux; chttp2; gao)"
-
-Note that the value for custom-bin doesn't print nicely because it's a binary 
-value. You can indicate a binary value through appending "-bin" to the header key.
 
 We did not add the user-agent metadata as a custom header. This shows how 
 the gRPC framework adds some headers under the hood that may show up in the 
--- examples/cpp/metadata/greeter_server.cc
+++ examples/cpp/metadata/greeter_server.cc
@@ -46,7 +46,19 @@ class GreeterServiceImpl final : public Greeter::Service {
     std::cout << "Client metadata: " << std::endl;
     const std::multimap<grpc::string_ref, grpc::string_ref> metadata = context->client_metadata();
     for (auto iter = metadata.begin(); iter != metadata.end(); ++iter) {
-      std::cout << "Header key: " << iter->first << " , value: " << iter->second << std::endl;
+      std::cout << "Header key: " << iter->first << ", value: ";
+      // Check for binary value
+      size_t isbin = iter->first.find("-bin");
+      if ((isbin != std::string::npos) && (isbin + 4 == iter->first.size())) {
+        std::cout <<  std::hex;
+        for (auto c : iter->second) {
+          std::cout << static_cast<unsigned int>(c);
+        }
+        std::cout <<  std::dec;
+      } else {
+        std::cout << iter->second;
+      }
+      std::cout << std::endl;
     }
 
     context->AddInitialMetadata("custom-server-metadata", "initial metadata value");
