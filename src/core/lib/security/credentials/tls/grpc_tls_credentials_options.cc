bb212394c7295967e7832659fab30ccdbd626832
Yihua Zhang <yihuaz@google.com>
2019-01-03 20:28:35 -0800

    C++'fy TLS credentials options.

--- src/core/lib/security/credentials/tls/grpc_tls_credentials_options.cc
+++ src/core/lib/security/credentials/tls/grpc_tls_credentials_options.cc
@@ -27,10 +27,79 @@
 #include <grpc/support/log.h>
 #include <grpc/support/string_util.h>
 
-/** -- TLS credentials options API implementation. -- **/
+/** -- gRPC TLS key materials config API implementation. -- **/
+void grpc_tls_key_materials_config::set_key_materials(
+    const grpc_ssl_pem_key_cert_pair* key_cert_pairs, const char* root_certs,
+    size_t num) {
+  if (key_cert_pairs == nullptr || num == 0) {
+    gpr_log(GPR_ERROR, "Invalid arguments to set_key_materials()");
+    return;
+  }
+  pem_key_cert_pairs_ = static_cast<grpc_ssl_pem_key_cert_pair*>(
+      gpr_zalloc(sizeof(grpc_ssl_pem_key_cert_pair) * num));
+  for (size_t i = 0; i < num; i++) {
+    pem_key_cert_pairs_[i].private_key =
+        gpr_strdup(key_cert_pairs[i].private_key);
+    pem_key_cert_pairs_[i].cert_chain =
+        gpr_strdup(key_cert_pairs[i].cert_chain);
+  }
+  if (root_certs != nullptr) {
+    pem_root_certs_ = gpr_strdup(root_certs);
+  }
+  num_key_cert_pairs_ = num;
+}
+
+grpc_tls_key_materials_config::~grpc_tls_key_materials_config() {
+  for (size_t i = 0; i < num_key_cert_pairs_; i++) {
+    gpr_free((void*)pem_key_cert_pairs_[i].private_key);
+    gpr_free((void*)pem_key_cert_pairs_[i].cert_chain);
+  }
+  gpr_free(pem_key_cert_pairs_);
+  gpr_free((void*)pem_root_certs_);
+}
+
+/** -- gRPC TLS credential reload config API implementation. -- **/
+grpc_tls_credential_reload_config::grpc_tls_credential_reload_config(
+    const void* config_user_data,
+    int (*schedule)(void* config_user_data,
+                    grpc_tls_credential_reload_arg* arg),
+    void (*cancel)(void* config_user_data, grpc_tls_credential_reload_arg* arg),
+    void (*destruct)(void* config_user_data))
+    : config_user_data_(const_cast<void*>(config_user_data)),
+      schedule_(schedule),
+      cancel_(cancel),
+      destruct_(destruct) {}
+
+grpc_tls_credential_reload_config::~grpc_tls_credential_reload_config() {
+  if (destruct_ != nullptr) {
+    destruct_((void*)config_user_data_);
+  }
+}
+
+/** -- gRPC TLS server authorization check API implementation. -- **/
+grpc_tls_server_authorization_check_config::
+    grpc_tls_server_authorization_check_config(
+        const void* config_user_data,
+        int (*schedule)(void* config_user_data,
+                        grpc_tls_server_authorization_check_arg* arg),
+        void (*cancel)(void* config_user_data,
+                       grpc_tls_server_authorization_check_arg* arg),
+        void (*destruct)(void* config_user_data))
+    : config_user_data_(const_cast<void*>(config_user_data)),
+      schedule_(schedule),
+      cancel_(cancel),
+      destruct_(destruct) {}
+
+grpc_tls_server_authorization_check_config::
+    ~grpc_tls_server_authorization_check_config() {
+  if (destruct_ != nullptr) {
+    destruct_((void*)config_user_data_);
+  }
+}
+
+/** -- Wrapper APIs declared in grpc_security.h -- **/
 grpc_tls_credentials_options* grpc_tls_credentials_options_create() {
-  return static_cast<grpc_tls_credentials_options*>(
-      gpr_zalloc(sizeof(grpc_tls_credentials_options)));
+  return grpc_core::New<grpc_tls_credentials_options>();
 }
 
 void grpc_tls_credentials_options_set_cert_request_type(
@@ -42,7 +111,7 @@ void grpc_tls_credentials_options_set_cert_request_type(
             "grpc_tls_credentials_options_set_cert_request_type()");
     return;
   }
-  options->cert_request_type = type;
+  options->set_cert_request_type(type);
 }
 
 void grpc_tls_credentials_options_set_key_materials_config(
@@ -54,7 +123,7 @@ void grpc_tls_credentials_options_set_key_materials_config(
             "grpc_tls_credentials_options_set_key_materials_config()");
     return;
   }
-  options->key_materials_config = config;
+  options->set_key_materials_config(config);
 }
 
 void grpc_tls_credentials_options_set_credential_reload_config(
@@ -66,7 +135,7 @@ void grpc_tls_credentials_options_set_credential_reload_config(
             "grpc_tls_credentials_options_set_credential_reload_config()");
     return;
   }
-  options->credential_reload_config = config;
+  options->set_credential_reload_config(config);
 }
 
 void grpc_tls_credentials_options_set_server_authorization_check_config(
@@ -79,30 +148,16 @@ void grpc_tls_credentials_options_set_server_authorization_check_config(
         "grpc_tls_credentials_options_set_server_authorization_check_config()");
     return;
   }
-  options->server_authorization_check_config = config;
+  options->set_server_authorization_check_config(config);
 }
 
-void grpc_tls_credentials_options_destroy(
-    grpc_tls_credentials_options* options) {
-  if (options == nullptr) {
-    return;
-  }
-  grpc_tls_key_materials_config_destroy(options->key_materials_config);
-  grpc_tls_credential_reload_config_unref(options->credential_reload_config);
-  grpc_tls_server_authorization_check_config_unref(
-      options->server_authorization_check_config);
-  gpr_free(options);
-}
-
-/** -- TLS key materials config API implementation. -- **/
-GRPCAPI grpc_tls_key_materials_config* grpc_tls_key_materials_config_create() {
-  return static_cast<grpc_tls_key_materials_config*>(
-      gpr_zalloc(sizeof(grpc_tls_key_materials_config)));
+grpc_tls_key_materials_config* grpc_tls_key_materials_config_create() {
+  return grpc_core::New<grpc_tls_key_materials_config>();
 }
 
 void grpc_tls_key_materials_config_set_key_materials(
     grpc_tls_key_materials_config* config,
-    grpc_ssl_pem_key_cert_pair* key_cert_pairs, const char* root_certs,
+    const grpc_ssl_pem_key_cert_pair* key_cert_pairs, const char* root_certs,
     size_t num) {
   if (config == nullptr || key_cert_pairs == nullptr || num == 0) {
     gpr_log(GPR_ERROR,
@@ -110,57 +165,9 @@ void grpc_tls_key_materials_config_set_key_materials(
             "grpc_tls_key_materials_config_set_key_materials()");
     return;
   }
-  config->pem_key_cert_pairs = static_cast<grpc_ssl_pem_key_cert_pair*>(
-      gpr_zalloc(sizeof(grpc_ssl_pem_key_cert_pair) * num));
-  for (size_t i = 0; i < num; i++) {
-    config->pem_key_cert_pairs[i].private_key =
-        gpr_strdup(key_cert_pairs[i].private_key);
-    config->pem_key_cert_pairs[i].cert_chain =
-        gpr_strdup(key_cert_pairs[i].cert_chain);
-  }
-  config->pem_root_certs = gpr_strdup(root_certs);
-  config->num_key_cert_pairs = num;
+  config->set_key_materials(key_cert_pairs, root_certs, num);
 }
 
-void grpc_tls_key_materials_config_destroy(
-    grpc_tls_key_materials_config* config) {
-  if (config == nullptr) {
-    return;
-  }
-  for (size_t i = 0; i < config->num_key_cert_pairs; i++) {
-    gpr_free((void*)config->pem_key_cert_pairs[i].private_key);
-    gpr_free((void*)config->pem_key_cert_pairs[i].cert_chain);
-  }
-  gpr_free(config->pem_key_cert_pairs);
-  gpr_free((void*)config->pem_root_certs);
-  gpr_free(config);
-}
-
-static grpc_tls_key_materials_config* key_materials_config_copy(
-    grpc_tls_key_materials_config* config) {
-  if (config == nullptr || config->pem_key_cert_pairs == nullptr ||
-      config->num_key_cert_pairs == 0) {
-    return nullptr;
-  }
-  grpc_tls_key_materials_config* new_config =
-      static_cast<grpc_tls_key_materials_config*>(
-          gpr_zalloc(sizeof(*new_config)));
-  new_config->pem_root_certs = gpr_strdup(config->pem_root_certs);
-  grpc_ssl_pem_key_cert_pair* pem_key_cert_pairs =
-      static_cast<grpc_ssl_pem_key_cert_pair*>(gpr_zalloc(
-          sizeof(grpc_ssl_pem_key_cert_pair) * config->num_key_cert_pairs));
-  for (size_t i = 0; i < config->num_key_cert_pairs; i++) {
-    pem_key_cert_pairs[i].private_key =
-        gpr_strdup(config->pem_key_cert_pairs[i].private_key);
-    pem_key_cert_pairs[i].cert_chain =
-        gpr_strdup(config->pem_key_cert_pairs[i].cert_chain);
-  }
-  new_config->pem_key_cert_pairs = pem_key_cert_pairs;
-  new_config->num_key_cert_pairs = config->num_key_cert_pairs;
-  return new_config;
-}
-
-/** -- TLS credential reload config API implementation. -- **/
 grpc_tls_credential_reload_config* grpc_tls_credential_reload_config_create(
     const void* config_user_data,
     int (*schedule)(void* config_user_data,
@@ -173,38 +180,10 @@ grpc_tls_credential_reload_config* grpc_tls_credential_reload_config_create(
         "Schedule API is nullptr in creating TLS credential reload config.");
     return nullptr;
   }
-  grpc_tls_credential_reload_config* config =
-      static_cast<grpc_tls_credential_reload_config*>(
-          gpr_zalloc(sizeof(*config)));
-  config->config_user_data = config_user_data;
-  config->schedule = schedule;
-  config->cancel = cancel;
-  config->destruct = destruct;
-  gpr_ref_init(&config->refcount, 1);
-  return config;
+  return grpc_core::New<grpc_tls_credential_reload_config>(
+      config_user_data, schedule, cancel, destruct);
 }
 
-grpc_tls_credential_reload_config* grpc_tls_credential_reload_config_ref(
-    grpc_tls_credential_reload_config* config) {
-  if (config == nullptr) {
-    return nullptr;
-  }
-  gpr_ref(&config->refcount);
-  return config;
-}
-
-void grpc_tls_credential_reload_config_unref(
-    grpc_tls_credential_reload_config* config) {
-  if (config == nullptr || !gpr_unref(&config->refcount)) {
-    return;
-  }
-  if (config->destruct != nullptr) {
-    config->destruct((void*)config->config_user_data);
-  }
-  gpr_free(config);
-}
-
-/** -- TLS server authorization check config API implementation. -- **/
 grpc_tls_server_authorization_check_config*
 grpc_tls_server_authorization_check_config_create(
     const void* config_user_data,
@@ -219,51 +198,6 @@ grpc_tls_server_authorization_check_config_create(
             "check config.");
     return nullptr;
   }
-  grpc_tls_server_authorization_check_config* config =
-      static_cast<grpc_tls_server_authorization_check_config*>(
-          gpr_zalloc(sizeof(*config)));
-  config->config_user_data = config_user_data;
-  config->schedule = schedule;
-  config->destruct = destruct;
-  gpr_ref_init(&config->refcount, 1);
-  return config;
-}
-
-grpc_tls_server_authorization_check_config*
-grpc_tls_server_authorization_check_config_ref(
-    grpc_tls_server_authorization_check_config* config) {
-  if (config == nullptr) {
-    return nullptr;
-  }
-  gpr_ref(&config->refcount);
-  return config;
-}
-
-void grpc_tls_server_authorization_check_config_unref(
-    grpc_tls_server_authorization_check_config* config) {
-  if (config == nullptr || !gpr_unref(&config->refcount)) {
-    return;
-  }
-  if (config->destruct != nullptr) {
-    config->destruct((void*)config->config_user_data);
-  }
-  gpr_free(config);
-}
-
-grpc_tls_credentials_options* grpc_tls_credentials_options_copy(
-    const grpc_tls_credentials_options* options) {
-  if (options == nullptr) {
-    return nullptr;
-  }
-  grpc_tls_credentials_options* new_options =
-      grpc_tls_credentials_options_create();
-  new_options->cert_request_type = options->cert_request_type;
-  new_options->key_materials_config =
-      key_materials_config_copy(options->key_materials_config);
-  new_options->credential_reload_config =
-      grpc_tls_credential_reload_config_ref(options->credential_reload_config);
-  new_options->server_authorization_check_config =
-      grpc_tls_server_authorization_check_config_ref(
-          options->server_authorization_check_config);
-  return new_options;
+  return grpc_core::New<grpc_tls_server_authorization_check_config>(
+      config_user_data, schedule, cancel, destruct);
 }
