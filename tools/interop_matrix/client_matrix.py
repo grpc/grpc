bf273ff00f6b955ecd1f2e9f64f0c2907d358840
Penn (Dapeng) Zhang <zdapeng@google.com>
2019-01-15 11:01:03 -0800

    Add grpc-java 1.18.0 to interop matrix
    release note: no

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -139,6 +139,7 @@ LANG_RELEASE_MATRIX = {
         ('v1.15.0', ReleaseInfo()),
         ('v1.16.1', ReleaseInfo()),
         ('v1.17.1', ReleaseInfo()),
+        ('v1.18.0', ReleaseInfo()),
     ]),
     'python':
     OrderedDict([
