9f12bfd48edcbb4c29c20a67082c3b4915aeaafa
Tommy Chen <toc007@ucsd.edu>
2019-01-14 16:35:33 -0800

    added test case from issue #16728

--- src/ruby/spec/pb/codegen/package_option_spec.rb
+++ src/ruby/spec/pb/codegen/package_option_spec.rb
@@ -49,4 +49,40 @@ describe 'Code Generation Options' do
       end
     end
   end
+  it 'should be able to refer to message in the correct namespace' do
+    fail 'CONFIG env variable unexpectedly unset' unless ENV['CONFIG']
+    bins_sub_dir = ENV['CONFIG']
+
+    pb_dir = File.dirname(__FILE__)
+    bins_dir = File.join('..', '..', '..', '..', '..', 'bins', bins_sub_dir)
+
+    plugin = File.join(bins_dir, 'grpc_ruby_plugin')
+    protoc = File.join(bins_dir, 'protobuf', 'protoc')
+
+    # Generate the service from the proto
+    Dir.mktmpdir(nil, File.dirname(__FILE__)) do |tmp_dir|
+      gen_file = system(protoc,
+                        '-I',
+                        'grpc/testing',
+                        "--grpc_out=#{tmp_dir}", # generate the service
+                        "--ruby_out=#{tmp_dir}", # generate the definitions
+                        "--plugin=protoc-gen-grpc=#{plugin}",
+                        'messages.proto',
+                        'service.proto',
+                        chdir: pb_dir,
+                        out: File::NULL)
+
+      expect(gen_file).to be_truthy
+      begin
+        $LOAD_PATH.push(tmp_dir)
+        expect { MyService::Request }.to raise_error(NameError)
+        expect { MyService::Service }.to raise_error(NameError)
+        expect(require('service_services_pb')).to be_truthy
+        expect { MyService::Request }.to_not raise_error
+        expect { MyService::Service }.to_not raise_error
+      ensure
+        $LOAD_PATH.delete(tmp_dir)
+      end
+    end
+  end
 end
