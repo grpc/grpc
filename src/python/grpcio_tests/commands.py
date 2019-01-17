e63d9fb8916810ed98d7a9a12c8cf14e1782230e
Eric Gribkoff <ericgribkoff@google.com>
2019-01-17 09:00:17 -0800

    Revert https://github.com/grpc/grpc/pull/17597
    Pending resolution of https://github.com/grpc/grpc/issues/17569
    
    Revert "fix test"
    
    This reverts commit 4e3e46df2249bbd6ba8f3330c0a44ca508d0f35c.
    
    Revert "use test constants, fix formatting"
    
    This reverts commit e678187996bb7239315f30d9e50734c50ee4027b.
    
    Revert "skip test with gevent"
    
    This reverts commit b74af8c70bad274c4955a0e242c725409872342d.
    
    Revert "Add Watch method to health check service"
    
    This reverts commit 71e7e6ddc73175df0793748e290e29321934fd7c.

--- src/python/grpcio_tests/commands.py
+++ src/python/grpcio_tests/commands.py
@@ -141,7 +141,6 @@ class TestGevent(setuptools.Command):
         'unit._exit_test.ExitTest.test_in_flight_partial_unary_stream_call',
         'unit._exit_test.ExitTest.test_in_flight_partial_stream_unary_call',
         'unit._exit_test.ExitTest.test_in_flight_partial_stream_stream_call',
-        'health_check._health_servicer_test.HealthServicerTest.test_cancelled_watch_removed_from_watch_list',
         # TODO(https://github.com/grpc/grpc/issues/17330) enable these three tests
         'channelz._channelz_servicer_test.ChannelzServicerTest.test_many_subchannels',
         'channelz._channelz_servicer_test.ChannelzServicerTest.test_many_subchannels_and_sockets',
