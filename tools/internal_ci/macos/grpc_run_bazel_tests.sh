8aea53cb86256c8edd0f47299df81278d90582de
Prashant Jaikumar <pjaikumar@google.com>
2019-02-13 11:23:09 -0800

    Fix copyright string in grpc_cfstream.cfg

--- tools/internal_ci/macos/grpc_run_bazel_tests.sh
+++ tools/internal_ci/macos/grpc_run_bazel_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2019 gRPC authors.
+# Copyright 2019 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
