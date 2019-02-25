d63f0087671e0653e95a106eebf72d384875e5ce
Srini Polavarapu <psrini@google.com>
2019-02-25 09:40:44 -0800

    Regenerate projects

--- src/cpp/common/version_cc.cc
+++ src/cpp/common/version_cc.cc
@@ -22,5 +22,5 @@
 #include <grpcpp/grpcpp.h>
 
 namespace grpc {
-grpc::string Version() { return "1.19.0-pre1"; }
+grpc::string Version() { return "1.19.0"; }
 }  // namespace grpc
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.19.0.pre1'
+    VERSION = '1.19.0'
   end
 end
