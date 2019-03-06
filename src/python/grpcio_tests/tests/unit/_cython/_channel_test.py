6cfe29e775f3c9b63314f09c88fe3d49a7447b3f
Lidi Zheng <scallopsky@gmail.com>
2019-03-05 16:25:22 -0800

    Fix typo

--- src/python/grpcio_tests/tests/unit/_cython/_channel_test.py
+++ src/python/grpcio_tests/tests/unit/_cython/_channel_test.py
@@ -57,7 +57,7 @@ class ChannelTest(unittest.TestCase):
     def test_multiple_channels_lonely_connectivity(self):
         _in_parallel(_create_loop_destroy, ())
 
-    unittest.skip('See if windows are happy now')
+    @unittest.skip('See if windows are happy now')
     def test_negative_deadline_connectivity(self):
         channel = _channel()
         connectivity = channel.check_connectivity_state(True)
