f42ad52c44f5d06b3418676611d5758e5061f3d5
Lidi Zheng <lidiz@google.com>
2019-03-13 12:40:10 -0700

    Update `--dest-dir` to `-d`

--- test/distrib/python/test_packages.sh
+++ test/distrib/python/test_packages.sh
@@ -41,7 +41,7 @@ PYTHON=$VIRTUAL_ENV/bin/python
 
 function validate_wheel_hashes() {
   for file in "$@"; do
-    "$PYTHON" -m wheel unpack "$file" --dest-dir /tmp || return 1
+    "$PYTHON" -m wheel unpack "$file" -d /tmp || return 1
   done
   return 0
 }
