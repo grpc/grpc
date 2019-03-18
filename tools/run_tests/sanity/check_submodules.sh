fb15daf8b9896fd62f6d3444d3e14e90ffe0b9e0
Vishal Powar <vishal.powar@gmail.com>
2019-03-18 16:56:07 -0700

    Add generated upb code for endpoints information.
    Also,
      - Update upb submodule for new code generator and update generated
      files.

--- tools/run_tests/sanity/check_submodules.sh
+++ tools/run_tests/sanity/check_submodules.sh
@@ -40,7 +40,7 @@ cat << EOF | awk '{ print $1 }' | sort > "$want_submodules"
  9245d481eb3e890f708ff2d7dadf2a10c04748ba third_party/libcxxabi (heads/release_60)
  582743bf40c5d3639a70f98f183914a2c0cd0680 third_party/protobuf (v3.7.0-rc.2-20-g582743bf)
  e143189bf6f37b3957fb31743df6a1bcf4a8c685 third_party/protoc-gen-validate (v0.0.10)
- ed9faae0993704b033c594b072d65e1bf19207fa third_party/upb (heads/master)
+ fa88c6017ddb490aa78c57bea682193f533ed69a third_party/upb (heads/master)
  cacf7f1d4e3d44d871b605da3b647f07d718623f third_party/zlib (v1.2.11)
 EOF
 
