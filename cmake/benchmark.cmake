e5a5a5b5ee461b8dabada97b49daeca68cdeb62b
Bill Feng <yfen@google.com>
2019-03-18 17:24:40 -0700

    attempt #2 at fixing CMake builds

--- cmake/benchmark.cmake
+++ cmake/benchmark.cmake
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set(BENCHMARK_ENABLE_GTEST_TESTS OFF)
+set(BENCHMARK_ENABLE_GTEST_TESTS OFF CACHE BOOL "Turn off gTest in gBenchmark")
 
 if("${gRPC_BENCHMARK_PROVIDER}" STREQUAL "module")
   if(NOT BENCHMARK_ROOT_DIR)
