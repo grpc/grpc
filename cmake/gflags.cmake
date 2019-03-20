a4a5c43a6f3e10f541bd733321130b9828d29ad0
Bill Feng <yfen@google.com>
2019-03-20 10:45:57 -0700

    updated cmake gflags target

--- cmake/gflags.cmake
+++ cmake/gflags.cmake
@@ -11,17 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set(gRPC_GFLAGS_PROVIDER "module" CACHE STRING "portability fix")
+
 if("${gRPC_GFLAGS_PROVIDER}" STREQUAL "module")
-  message("gRPC GFLAGS is MODULE")
   if(NOT GFLAGS_ROOT_DIR)
     set(GFLAGS_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/gflags)
   endif()
   if(EXISTS "${GFLAGS_ROOT_DIR}/CMakeLists.txt")
-    message("gRPC GFLAGS adding subdirectory")
     add_subdirectory(${GFLAGS_ROOT_DIR} third_party/gflags)
     if(TARGET gflags_static)
-      set(_gRPC_GFLAGS_LIBRARIES gflags_static)
+      set(_gRPC_GFLAGS_LIBRARIES gflags::gflags)
       set(_gRPC_GFLAGS_INCLUDE_DIR "${CMAKE_CURRENT_BINARY_DIR}/third_party/gflags/include")
     endif()
   else()
@@ -32,7 +30,7 @@ elseif("${gRPC_GFLAGS_PROVIDER}" STREQUAL "package")
   # Use "CONFIG" as there is no built-in cmake module for gflags.
   find_package(gflags REQUIRED CONFIG)
   if(TARGET gflags)
-    set(_gRPC_GFLAGS_LIBRARIES gflags)
+    set(_gRPC_GFLAGS_LIBRARIES gflags::gflags)
     set(_gRPC_GFLAGS_INCLUDE_DIR ${GFLAGS_INCLUDE_DIR})
   endif()
   set(_gRPC_FIND_GFLAGS "if(NOT gflags_FOUND)\n  find_package(gflags CONFIG)\nendif()")
