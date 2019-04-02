c4b7423ad019bb1d834c35cc3105305d24f3f2c0
Karthik Ravi Shankar <karthikrs@google.com>
2019-04-01 17:51:18 -0700

    Fix errors due to rebase

--- include/grpcpp/ext/proto_server_reflection_plugin_impl.h
+++ include/grpcpp/ext/proto_server_reflection_plugin_impl.h
@@ -23,19 +23,20 @@
 #include <grpcpp/support/config.h>
 
 namespace grpc {
-class ServerInitializer;
 class ProtoServerReflection;
 }  // namespace grpc
 
 namespace grpc_impl {
+class ServerInitializer;
+
 namespace reflection {
 
 class ProtoServerReflectionPlugin : public ::grpc::ServerBuilderPlugin {
  public:
   ProtoServerReflectionPlugin();
   ::grpc::string name() override;
-  void InitServer(::grpc::ServerInitializer* si) override;
-  void Finish(::grpc::ServerInitializer* si) override;
+  void InitServer(::grpc_impl::ServerInitializer* si) override;
+  void Finish(::grpc_impl::ServerInitializer* si) override;
   void ChangeArguments(const ::grpc::string& name, void* value) override;
   bool has_async_methods() const override;
   bool has_sync_methods() const override;
