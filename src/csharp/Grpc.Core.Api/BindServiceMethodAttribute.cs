0de1cd2d66faa6a2380b46dab5c834663a4f9996
Jan Tattermusch <jtattermusch@users.noreply.github.com>
2019-04-05 05:26:01 +0200

    Update license header

--- src/csharp/Grpc.Core.Api/BindServiceMethodAttribute.cs
+++ src/csharp/Grpc.Core.Api/BindServiceMethodAttribute.cs
@@ -1,5 +1,5 @@
 #region Copyright notice and license
-// Copyright 2015 gRPC authors.
+// Copyright 2019 The gRPC Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
