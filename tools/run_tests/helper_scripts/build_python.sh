9ebf1d1d89531e973b907a1913dd6006c700d1e4
Kostis Lolos <klolos@arrikto.com>
2019-04-22 18:33:04 +0300

    Fix bug in Python builder script
    Fix a bug in the Python builder script where the name of the directory
    to return to was stored in the $PWD variable, which is overwritten upon
    changing directory.
    
    Signed-off-by: Kostis Lolos <klolos@arrikto.com>

--- tools/run_tests/helper_scripts/build_python.sh
+++ tools/run_tests/helper_scripts/build_python.sh
@@ -157,11 +157,10 @@ curl https://bootstrap.pypa.io/get-pip.py | $VENV_PYTHON
 # pip-installs the directory specified. Used because on MSYS the vanilla Windows
 # Python gets confused when parsing paths.
 pip_install_dir() {
-  PWD=$(pwd)
-  cd "$1"
+  pushd "$1"
   ($VENV_PYTHON setup.py build_ext -c "$TOOLCHAIN" || true)
   $VENV_PYTHON -m pip install --no-deps .
-  cd "$PWD"
+  popd
 }
 
 case "$VENV" in
