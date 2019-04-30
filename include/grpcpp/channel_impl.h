e060cd7519f065db308ed04e765269221b609950
Karthik Ravi Shankar <karthikrs@google.com>
2019-04-30 16:36:05 -0700

    Fix formatting errors

--- include/grpcpp/channel_impl.h
+++ include/grpcpp/channel_impl.h
@@ -25,8 +25,8 @@
 #include <grpc/grpc.h>
 #include <grpcpp/impl/call.h>
 #include <grpcpp/impl/codegen/channel_interface.h>
-#include <grpcpp/impl/codegen/completion_queue.h>
 #include <grpcpp/impl/codegen/client_interceptor.h>
+#include <grpcpp/impl/codegen/completion_queue.h>
 #include <grpcpp/impl/codegen/config.h>
 #include <grpcpp/impl/codegen/grpc_library.h>
 #include <grpcpp/impl/codegen/sync.h>
