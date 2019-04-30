360e501db21abae4b77f27dae9e3f5827c3bdfca
Daniel Alm <Daniel.Alm@ForumD.net>
2019-04-30 14:17:35 +0200

    Add `void` arguments to two function declarations.

--- include/grpc/grpc_security.h
+++ include/grpc/grpc_security.h
@@ -641,7 +641,7 @@ typedef struct grpc_tls_credentials_options grpc_tls_credentials_options;
 
 /** Create an empty TLS credentials options. It is used for
  *  experimental purpose for now and subject to change. */
-GRPCAPI grpc_tls_credentials_options* grpc_tls_credentials_options_create();
+GRPCAPI grpc_tls_credentials_options* grpc_tls_credentials_options_create(void);
 
 /** Set grpc_ssl_client_certificate_request_type field in credentials options
     with the provided type. options should not be NULL.
@@ -683,7 +683,7 @@ GRPCAPI int grpc_tls_credentials_options_set_server_authorization_check_config(
 
 /** Create an empty grpc_tls_key_materials_config instance.
  *  It is used for experimental purpose for now and subject to change. */
-GRPCAPI grpc_tls_key_materials_config* grpc_tls_key_materials_config_create();
+GRPCAPI grpc_tls_key_materials_config* grpc_tls_key_materials_config_create(void);
 
 /** Set grpc_tls_key_materials_config instance with provided a TLS certificate.
     config will take the ownership of pem_root_certs and pem_key_cert_pairs.
