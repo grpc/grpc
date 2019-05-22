f13abc071cb91c06e90f7121c66f39bae59ec691
Arjun Roy <arjunroy@google.com>
2019-05-21 19:02:27 -0700

    Inlined and saved some ops for grpc_is_binary_header.
    Various improvements for parsing/encoding:
    
    BM_HpackEncoderEncodeDeadline 171ns ± 0% 164ns ± 0% -3.74% (p=0.000 n=20+17)
    BM_HpackEncoderEncodeHeader<SingleInternedKeyElem>/0/16384 102ns ± 1% 98ns ± 0% -3.28% (p=0.000 n=20+19)
    BM_HpackParserParseHeader<AddIndexedSingleStaticElem, UnrefHeader> 177ns ± 1% 165ns ± 1% -6.41% (p=0.000 n=19+18)
    BM_HpackParserParseHeader<NonIndexedBinaryElem<1, true>, UnrefHeader> 212ns ± 2% 199ns ± 1% -6.28% (p=0.000 n=20+18)

--- src/core/ext/transport/cronet/transport/cronet_transport.cc
+++ src/core/ext/transport/cronet/transport/cronet_transport.cc
@@ -38,6 +38,7 @@
 #include "src/core/lib/slice/slice_internal.h"
 #include "src/core/lib/slice/slice_string_helpers.h"
 #include "src/core/lib/surface/channel.h"
+#include "src/core/lib/surface/validate_metadata.h"
 #include "src/core/lib/transport/metadata_batch.h"
 #include "src/core/lib/transport/static_metadata.h"
 #include "src/core/lib/transport/transport_impl.h"
@@ -419,7 +420,7 @@ static void convert_cronet_array_to_metadata(
     grpc_slice key = grpc_slice_intern(
         grpc_slice_from_static_string(header_array->headers[i].key));
     grpc_slice value;
-    if (grpc_is_binary_header(key)) {
+    if (grpc_is_refcounted_slice_binary_header(key)) {
       value = grpc_slice_from_static_string(header_array->headers[i].value);
       value = grpc_slice_intern(grpc_chttp2_base64_decode_with_length(
           value, grpc_chttp2_base64_infer_length_after_decode(value)));
@@ -746,7 +747,7 @@ static void convert_metadata_to_cronet_headers(
     curr = curr->next;
     char* key = grpc_slice_to_c_string(GRPC_MDKEY(mdelem));
     char* value;
-    if (grpc_is_binary_header(GRPC_MDKEY(mdelem))) {
+    if (grpc_is_binary_header_internal(GRPC_MDKEY(mdelem))) {
       grpc_slice wire_value = grpc_chttp2_base64_encode(GRPC_MDVALUE(mdelem));
       value = grpc_slice_to_c_string(wire_value);
       grpc_slice_unref_internal(wire_value);
