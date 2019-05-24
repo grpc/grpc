a1542b5a413d87b2fa6cd2757b42c4b75b088728
Muxi Yan <mxyan@google.com>
2019-05-24 12:51:14 -0700

    clang-format

--- src/objective-c/tests/CronetTests/CronetUnitTests.mm
+++ src/objective-c/tests/CronetTests/CronetUnitTests.mm
@@ -40,8 +40,8 @@
 #import "test/core/util/test_config.h"
 
 #define GRPC_SHADOW_BORINGSSL_SYMBOLS
-#import "src/core/tsi/grpc_shadow_boringssl.h"
 #import <openssl_grpc/ssl.h>"
+#import "src/core/tsi/grpc_shadow_boringssl.h"
 
 static void drain_cq(grpc_completion_queue *cq) {
   grpc_event ev;
