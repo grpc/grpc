31ec9a74aebfe9d7036a5a1b0099e3e23be5bea1
Mark D. Roth <roth@google.com>
2019-05-28 14:23:01 -0700

    More LB policy API improvements.

--- src/core/ext/filters/client_channel/lb_policy_factory.h
+++ src/core/ext/filters/client_channel/lb_policy_factory.h
@@ -37,7 +37,7 @@ class LoadBalancingPolicyFactory {
   /// Caller does NOT take ownership of result.
   virtual const char* name() const GRPC_ABSTRACT;
 
-  virtual RefCountedPtr<ParsedLoadBalancingConfig> ParseLoadBalancingConfig(
+  virtual RefCountedPtr<LoadBalancingPolicy::Config> ParseLoadBalancingConfig(
       const grpc_json* json, grpc_error** error) const GRPC_ABSTRACT;
 
   virtual ~LoadBalancingPolicyFactory() {}
