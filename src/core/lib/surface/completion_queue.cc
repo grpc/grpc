724565372f3b1301f98776bfdc2460b58b50fcbc
Karthik Ravi Shankar <karthikrs@google.com>
2019-05-29 16:42:06 -0700

    Fix the ref count issue

--- src/core/lib/surface/completion_queue.cc
+++ src/core/lib/surface/completion_queue.cc
@@ -820,7 +820,6 @@ static void cq_end_op_for_pluck(
 static void functor_callback(void* arg, grpc_error* error) {
   auto* functor = static_cast<grpc_experimental_completion_queue_functor*>(arg);
   functor->functor_run(functor, error == GRPC_ERROR_NONE);
-  GRPC_ERROR_UNREF(error);
 }
 
 /* Complete an event on a completion queue of type GRPC_CQ_CALLBACK */
