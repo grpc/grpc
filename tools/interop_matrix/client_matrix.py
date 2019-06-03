ad103834caf394a93a46fbadbe90ee9cd3bbd58b
Jan Tattermusch <jtattermusch@users.noreply.github.com>
2019-06-03 13:55:13 +0200

    Fix syntax error

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -145,7 +145,7 @@ LANG_RELEASE_MATRIX = {
         ('v1.18.0', ReleaseInfo(runtimes=['java_oracle8'])),
         ('v1.19.0', ReleaseInfo(runtimes=['java_oracle8'])),
         ('v1.20.0', ReleaseInfo(runtimes=['java_oracle8'])),
-        ('v1.21.0', ReleaseInfo(runtimes=['java_oracle8']))),
+        ('v1.21.0', ReleaseInfo(runtimes=['java_oracle8'])),
     ]),
     'python':
     OrderedDict([
