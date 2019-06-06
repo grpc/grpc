64389f31b7553bfae77d54709c1b5e400a5c71ac
Richard Belleville <rbellevi@google.com>
2019-06-06 15:15:40 -0700

    Formally support Python 3.7

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'License :: OSI Approved :: Apache Software License',
 ]
 
--- src/python/grpcio_reflection/setup.py
+++ src/python/grpcio_reflection/setup.py
@@ -50,6 +50,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'License :: OSI Approved :: Apache Software License',
 ]
 
