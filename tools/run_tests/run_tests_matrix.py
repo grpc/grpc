1f1eff115afc3aa95c6bf288e52490b893488426
Muxi Yan <mxyan@google.com>
2019-06-11 11:57:37 -0700

    Split extra arguments

--- tools/run_tests/run_tests_matrix.py
+++ tools/run_tests/run_tests_matrix.py
@@ -542,7 +542,7 @@ if __name__ == "__main__":
         extra_args.append('%s' % args.bq_result_table)
         extra_args.append('--measure_cpu_costs')
     if args.extra_args:
-        extra_args.append('%s' % args.extra_args)
+        extra_args.extend(('%s' % args.extra_args).split())
 
     all_jobs = _create_test_jobs(extra_args=extra_args, inner_jobs=args.inner_jobs) + \
                _create_portability_test_jobs(extra_args=extra_args, inner_jobs=args.inner_jobs)
