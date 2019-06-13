0bf20eed19f4951babac496dc41d9e18dbc86c1e
Zhehao "Tony" Lu <tonyzhehaolu@google.com>
2019-06-12 19:16:05 -0700

    Setting up unit tests

---
+++ src/objective-c/tests/UnitTests/CacheInterceptorTests.m
@@ -0,0 +1,42 @@
+//
+//  CacheInterceptorTests.m
+//  UnitTests
+//
+//  Created by Tony Lu on 6/12/19.
+//  Copyright © 2019 gRPC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "../../GRPCClient/CacheInterceptor.h"
+
+/**
+ * Mocking interceptormanager
+ */
+@interface TestInterceptorManager : GRPCInterceptorManager
+
+@end
+
+@implementation TestInterceptorManager
+
+
+@end
+
+/**
+ * Test main class
+ */
+@interface CacheInterceptorTests : XCTestCase
+
+@end
+
+@implementation CacheInterceptorTests
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+}
+
+- (void)test1 { // needs renaming later
+}
+
+@end
