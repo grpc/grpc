f5c2a904f39a12dd8bc6c99c5837abb787f42e74
Zhehao "Tony" Lu <tonyzhehaolu@google.com>
2019-06-17 19:33:20 -0700

    Added revalidation feature and some basic tests. Refactored code and fixed a fatal bug

--- src/objective-c/tests/UnitTests/CacheInterceptorTests.m
+++ src/objective-c/tests/UnitTests/CacheInterceptorTests.m
@@ -1,10 +1,20 @@
-//
-//  CacheInterceptorTests.m
-//  UnitTests
-//
-//  Created by Tony Lu on 6/12/19.
-//  Copyright © 2019 gRPC. All rights reserved.
-//
+/*
+ *
+ * Copyright 2019 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 #import <XCTest/XCTest.h>
 #import <GRPCClient/GRPCCall.h>
@@ -24,42 +34,58 @@
 
 @property(readwrite) __weak GRPCInterceptor *interceptor;
 @property(readonly) NSUInteger callThruCount;
+@property(readwrite) NSMutableDictionary *headerChecker;
+@property(readonly) BOOL headerCheckPassed;
+
 - (instancetype)init;
 
 @end
 
 @implementation TestInterceptorManager {
   __weak GRPCInterceptor *_interceptor;
-  NSDictionary *_responseInitialMetadata;
+  NSDictionary *_responseHeaders;
   id _responseData;
-  NSDictionary *_responseTrailingMetadata;
+  NSDictionary *_responseTrailers;
   NSUInteger _callThruCount;
+  NSMutableDictionary *_headerChecker;
+  BOOL _headerCheckPassed;
 }
 
 @synthesize callThruCount = _callThruCount;
+@synthesize headerCheckPassed = _headerCheckPassed;
 
 - (instancetype)init {
   GRPCCall2Internal *nextInterceptor = [[GRPCCall2Internal alloc] init];
   self = [super initWithNextInterceptor:nextInterceptor];
   _callThruCount = 0;
   _responseData = TEST_DUMMY_DATA;
+  _headerChecker = [[NSMutableDictionary alloc] init];
+  _headerCheckPassed = YES;
   return self;
 }
 
-- (void)setResponseInitialMetadata:(NSDictionary *)metaData {
-  _responseInitialMetadata = metaData;
+- (void)setResponseHeaders:(NSDictionary *)metaData {
+  _responseHeaders = metaData;
 }
 
 - (void)setResponseData:(id)data {
   _responseData = data;
 }
 
-- (void)setResponseTrailingMetadata:(NSDictionary *)metaData {
-  _responseTrailingMetadata = metaData;
+- (void)setResponseTrailers:(NSDictionary *)metaData {
+  _responseTrailers = metaData;
 }
 
 - (void)startNextInterceptorWithRequest:(GRPCRequestOptions *)requestOptions
                             callOptions:(GRPCCallOptions *)callOptions {
+  NSDictionary *metadata = callOptions.initialMetadata;
+  for (NSString *key in _headerChecker) {
+    NSString *entry = [metadata objectForKey:key];
+    if (!entry || ![entry isEqualToString:_headerChecker[key]]) {
+      _headerCheckPassed = NO;
+      return;
+    }
+  }
 }
 
 - (void)writeNextInterceptorWithData:(id)data {
@@ -69,13 +95,13 @@
 - (void)finishNextInterceptor {
   ++_callThruCount;
   dispatch_async(_interceptor.dispatchQueue, ^{
-    [_interceptor didReceiveInitialMetadata:_responseInitialMetadata];
+    [_interceptor didReceiveInitialMetadata:_responseHeaders];
   });
   dispatch_async(_interceptor.dispatchQueue, ^{
     [_interceptor didReceiveData:_responseData];
   });
   dispatch_async(_interceptor.dispatchQueue, ^{
-    [_interceptor didCloseWithTrailingMetadata:_responseTrailingMetadata error:nil];
+    [_interceptor didCloseWithTrailingMetadata:_responseTrailers error:nil];
   });
 }
 
@@ -141,6 +167,8 @@
   id<GRPCInterceptorFactory> factory = _context;
   _callOptions = [[GRPCMutableCallOptions alloc] init];
   _callOptions.interceptorFactories = @[ factory ];
+  
+  _manager.responseHeaders = @{ @"cache-control": @"public, max-age=10" }; // default to cacheable header
 }
 
 - (void)tearDown { /* might not be needed */ }
@@ -175,21 +203,12 @@
 }
 
 - (void)testNoStoreHeader {
-  _manager.responseInitialMetadata = @{ @"cache-control": @"no-store" };
+  _manager.responseHeaders = @{ @"cache-control": @"no-store" };
   
   for (int i = 0; i < 5; ++i) {
     [self makeCallWithData:TEST_DUMMY_DATA];
   }
   XCTAssertEqual(5, _manager.callThruCount);
-}
-
-- (void)testCacheableHeader {
-  _manager.responseInitialMetadata = @{ @"cache-control": @"private" }; // can be omitted
-  
-  for (int i = 0; i < 5; ++i) {
-    [self makeCallWithData:TEST_DUMMY_DATA];
-  }
-  XCTAssertEqual(1, _manager.callThruCount);
 }
 
 - (void)testCacheLimit {
@@ -259,6 +278,89 @@
   [self makeCallWithData:NUMBER(1)];
   [self makeCallWithData:NUMBER(3)];
   XCTAssertEqual(5, _manager.callThruCount);
+}
+
+- (void)testMaxAge {
+  _manager.responseHeaders = @{ @"cache-control": @"public, max-age=0" };
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  sleep(1);
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  XCTAssertEqual(2, _manager.callThruCount);
+}
+
+- (void)testMaxAgeExpirationWithOtherRequests {
+  for (int i = 1; i <= 5; ++i) {
+    if (i == 3) {
+      _manager.responseHeaders = @{ @"cache-control": @"public, max-age=1" };
+    } else {
+      _manager.responseHeaders = @{ @"cache-control": @"private, max-age=3600" };
+    }
+    [self makeCallWithData:NUMBER(i)];
+  }
+  XCTAssertEqual(5, _manager.callThruCount);
+  [self makeCallWithData:NUMBER(3)];
+  XCTAssertEqual(5, _manager.callThruCount);
+  [self makeCallWithData:NUMBER(6)];
+  XCTAssertEqual(6, _manager.callThruCount);
+  sleep(1);
+  [self makeCallWithData:NUMBER(3)];
+  XCTAssertEqual(7, _manager.callThruCount);
+  [self makeCallWithData:NUMBER(5)];
+  XCTAssertEqual(7, _manager.callThruCount);
+}
+
+- (void)testETagHeader {
+  _manager.responseHeaders = @{ @"cache-control": @"public, max-age=1",
+                                @"etag": @"googleLLC",
+                                @"status": @"200"
+                                };
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  XCTAssertEqual(1, _manager.callThruCount);
+  XCTAssertTrue([_initialMetadata[@"status"] isEqualToString:@"200"]);
+  
+  sleep(1);
+  _manager.headerChecker[@"if-none-match"] = @"googleLLC";
+  _manager.responseHeaders = @{ @"status": @"304",
+                                @"cache-control": @"public, max-age=1"
+                                };
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  XCTAssertTrue(_manager.headerCheckPassed);
+  XCTAssertEqual(2, _manager.callThruCount);
+  XCTAssertTrue([TEST_DUMMY_DATA isEqualToString:(NSString *)_data]);
+  XCTAssertTrue([_initialMetadata[@"status"] isEqualToString:@"304"]);
+  
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  XCTAssertTrue(_manager.headerCheckPassed);
+  XCTAssertEqual(2, _manager.callThruCount);
+}
+
+- (void)testLastModifiedHeader {
+  _manager.responseHeaders = @{ @"cache-control": @"private, max-age=1",
+                                @"last-modified": @"Mon, 17 Jun 2019 18:00:00 GMT",
+                                @"status": @"200"
+                                };
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  
+  _manager.headerChecker[@"if-modified-since"] = @"Mon, 17 Jun 2019 18:00:00 GMT";
+  sleep(1);
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  XCTAssertTrue(_manager.headerCheckPassed);
+  XCTAssertEqual(2, _manager.callThruCount);
+  _manager.responseHeaders = @{ @"cache-control": @"private, max-age=1",
+                                @"date": @"Mon, 17 Jun 2019 19:00:00 GMT",
+                                @"status": @"200"
+                                };
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  XCTAssertEqual(2, _manager.callThruCount); // still using cache
+  sleep(1);
+  [self makeCallWithData:TEST_DUMMY_DATA]; // now last-modified becomes 19:00 (date header)
+  XCTAssertEqual(3, _manager.callThruCount);
+  
+  sleep(1);
+  _manager.headerChecker[@"if-modified-since"] = @"Mon, 17 Jun 2019 19:00:00 GMT";
+  [self makeCallWithData:TEST_DUMMY_DATA];
+  XCTAssertTrue(_manager.headerCheckPassed);
 }
 
 @end
