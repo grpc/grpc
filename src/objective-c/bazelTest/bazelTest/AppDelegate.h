a3665cfd3ff39faa769c46ae32e97e9659106c0a
Tony Lu <tonyzhehaolu@google.com>
2019-06-25 12:20:12 -0700

    Setting up test project

---
+++ src/objective-c/bazelTest/bazelTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bazelTest
+//
+//  Created by Tony Lu on 6/25/19.
+//  Copyright © 2019 Tony Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/objective-c/bazelTest/bazelTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  bazelTest
+//
+//  Created by Tony Lu on 6/25/19.
+//  Copyright © 2019 Tony Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
