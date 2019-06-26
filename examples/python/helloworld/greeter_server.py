1e62124cfb3b3c30cc10a9f8fb1ff25ee2e4aa56
Lidi Zheng <lidiz@google.com>
2019-06-26 09:52:39 -0700

    Revert changes to helloworld example

--- examples/python/helloworld/greeter_server.py
+++ examples/python/helloworld/greeter_server.py
@@ -36,8 +36,11 @@ def serve():
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-
-    server.wait_for_termination()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
 
 
 if __name__ == '__main__':
