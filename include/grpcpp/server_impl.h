4d625c4cb14e6a15f9d72d105610a74e1712c8f9
Karthik Ravi Shankar <karthikrs@google.com>
2019-06-26 16:50:47 -0700

    Keep more methods in grpc_impl

--- include/grpcpp/server_impl.h
+++ include/grpcpp/server_impl.h
@@ -82,9 +82,9 @@ class Server : public grpc::ServerInterface, private grpc::GrpcLibraryCodegen {
     /// Called before server is created.
     virtual void UpdateArguments(grpc_impl::ChannelArguments* args) {}
     /// Called before application callback for each synchronous server request
-    virtual void PreSynchronousRequest(ServerContext* context) = 0;
+    virtual void PreSynchronousRequest(grpc_impl::ServerContext* context) = 0;
     /// Called after application callback for each synchronous server request
-    virtual void PostSynchronousRequest(ServerContext* context) = 0;
+    virtual void PostSynchronousRequest(grpc_impl::ServerContext* context) = 0;
     /// Called before server is started.
     virtual void PreServerStart(Server* server) {}
     /// Called after a server port is added.
