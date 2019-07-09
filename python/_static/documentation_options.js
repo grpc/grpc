740cfdf61ad5189fe607ced61873075e83ce6391
Richard Belleville <rbellevi@google.com>
2019-07-09 16:57:03 -0700

    Auto-update Python documentation

--- python/_static/documentation_options.js
+++ python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.21.1',
+    VERSION: '1.22.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
