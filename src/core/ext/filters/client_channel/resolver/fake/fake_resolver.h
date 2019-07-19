fdd5377d497239c3e5fe1cdab94cef100be87722
Qiancheng Zhao <qianchengz@google.com>
2019-07-19 12:15:16 -0700

    use MutexLock instead of ReleasableMutexLock

--- src/core/ext/filters/client_channel/resolver/fake/fake_resolver.h
+++ src/core/ext/filters/client_channel/resolver/fake/fake_resolver.h
@@ -44,9 +44,6 @@ class FakeResolverResponseGenerator
  public:
   FakeResolverResponseGenerator() {}
 
-  // Set the corresponding FakeResolver to this generator.
-  void SetFakeResolver(FakeResolver* resolver);
-
   // Instructs the fake resolver associated with the response generator
   // instance to trigger a new resolution with the specified result. If the
   // resolver is not available yet, delays response setting until it is. This
@@ -78,12 +75,15 @@ class FakeResolverResponseGenerator
       const grpc_channel_args* args);
 
  private:
+  friend class FakeResolver;
+  // Set the corresponding FakeResolver to this generator.
+  void SetFakeResolver(FakeResolver* resolver);
+
   static void SetResponseLocked(void* arg, grpc_error* error);
   static void SetReresolutionResponseLocked(void* arg, grpc_error* error);
   static void SetFailureLocked(void* arg, grpc_error* error);
 
-  // Mutex synchronizes accesses to shared data from FakeResolver and
-  // FakeResolverResponseGenerator.
+  // Mutex protecting the members below.
   Mutex mu_;
   FakeResolver* resolver_ = nullptr;  // Do not own.
   Resolver::Result result_;
