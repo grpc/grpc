6e7ed1441c88b0d996c7c95ca6abbad9352ae73a
Tony Lu <tonyzhehaolu@google.com>
2019-07-19 16:29:24 -0700

    Added copyright statements

--- src/objective-c/examples/watchOS-sample/watchOS-sample WatchKit Extension/InterfaceController.m
+++ src/objective-c/examples/watchOS-sample/watchOS-sample WatchKit Extension/InterfaceController.m
@@ -1,11 +1,20 @@
-//
-//  InterfaceController.m
-//  watchOS-sample WatchKit Extension
-//
-//  Created by Tony Lu on 7/12/19.
-//  Copyright © 2019 Tony Lu. All rights reserved.
-//
-
+/*
+ *
+ * Copyright 2019 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 #import "InterfaceController.h"
 
 #import <RemoteTest/Messages.pbobjc.h>
