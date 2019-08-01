f03f065bf8a286001ce5578a5e0345c566cdd9d3
Prashant Jaikumar <pjaikumar@google.com>
2019-07-31 18:18:57 -0700

    Reproduce bug when time jumps

--- test/cpp/common/timer_test.cc
+++ test/cpp/common/timer_test.cc
@@ -20,9 +20,55 @@
 #include <gtest/gtest.h>
 
 #include "src/core/lib/iomgr/closure.h"
+#include "src/core/lib/iomgr/error.h"
 #include "src/core/lib/iomgr/exec_ctx.h"
 #include "src/core/lib/iomgr/timer.h"
 #include "test/core/util/test_config.h"
+
+static gpr_mu g_mu;
+extern gpr_timespec (*gpr_now_impl)(gpr_clock_type clock_type);
+gpr_timespec (*gpr_now_impl_orig)(gpr_clock_type clock_type) = gpr_now_impl;
+static int g_time_shift_sec = 0;
+static int g_time_shift_nsec = 0;
+static gpr_timespec now_impl(gpr_clock_type clock) {
+  auto ts = gpr_now_impl_orig(clock);
+  // We only manipulate the realtime clock to simulate changes in wall-clock
+  // time
+  if (clock != GPR_CLOCK_REALTIME) {
+    return ts;
+  }
+  GPR_ASSERT(ts.tv_nsec >= 0);
+  GPR_ASSERT(ts.tv_nsec < GPR_NS_PER_SEC);
+  gpr_mu_lock(&g_mu);
+  ts.tv_sec += g_time_shift_sec;
+  ts.tv_nsec += g_time_shift_nsec;
+  gpr_mu_unlock(&g_mu);
+  if (ts.tv_nsec >= GPR_NS_PER_SEC) {
+    ts.tv_nsec -= GPR_NS_PER_SEC;
+    ++ts.tv_sec;
+  } else if (ts.tv_nsec < 0) {
+    --ts.tv_sec;
+    ts.tv_nsec = GPR_NS_PER_SEC + ts.tv_nsec;
+  }
+  return ts;
+}
+
+// offset the value returned by gpr_now(GPR_CLOCK_REALTIME) by msecs
+// milliseconds
+static void set_now_offset(int msecs) {
+  gpr_mu_lock(&g_mu);
+  g_time_shift_sec = msecs / 1000;
+  g_time_shift_nsec = (msecs % 1000) * 1e6;
+  gpr_mu_unlock(&g_mu);
+}
+
+// restore the original implementation of gpr_now()
+static void reset_now_offset() {
+  gpr_mu_lock(&g_mu);
+  g_time_shift_sec = 0;
+  g_time_shift_nsec = 0;
+  gpr_mu_unlock(&g_mu);
+}
 
 
 namespace grpc {
@@ -40,10 +86,34 @@ TEST(TimerTest, TimerExpiry) {
   grpc_shutdown();
 }
 
+TEST(TimerTest, TimeJumpsBackwards) {
+  grpc_init();
+  grpc_core::ExecCtx exec_ctx;
+  gpr_log(GPR_ERROR, "sleeping for 5 seconds");
+  set_now_offset(-3000);
+  sleep(5);
+  gpr_log(GPR_ERROR, "sleep done");
+  reset_now_offset();
+  grpc_shutdown();
+}
+
+TEST(TimerTest, TimeJumpsForward) {
+  grpc_init();
+  grpc_core::ExecCtx exec_ctx;
+  gpr_log(GPR_ERROR, "sleeping for 5 seconds");
+  set_now_offset(3000);
+  sleep(5);
+  gpr_log(GPR_ERROR, "sleep done");
+  reset_now_offset();
+  grpc_shutdown();
+}
+
 }  // namespace
 }  // namespace grpc
 
 int main(int argc, char** argv) {
+  gpr_mu_init(&g_mu);
+  gpr_now_impl = now_impl;
   grpc::testing::TestEnvironment env(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
