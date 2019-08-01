7372c195e717e65c1a60e7b07165d0eaf89b7509
Jan Tattermusch <jtattermusch@google.com>
2019-08-01 04:00:23 +0200

    adjust iteration count in scalability benchmarks

--- src/csharp/Grpc.Microbenchmarks/CompletionRegistryBenchmark.cs
+++ src/csharp/Grpc.Microbenchmarks/CompletionRegistryBenchmark.cs
@@ -27,7 +27,7 @@ namespace Grpc.Microbenchmarks
         [Params(false, true)]
         public bool UseSharedRegistry { get; set; }
 
-        const int Iterations = 1000000;  // High number to make the overhead of RunConcurrent negligible.
+        const int Iterations = 5 * 1000 * 1000;  // High number to make the overhead of RunConcurrent negligible.
         [Benchmark(OperationsPerInvoke = Iterations)]
         public void RegisterExtract()
         {
