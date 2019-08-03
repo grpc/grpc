28ed900adca35ca2583d1b4144cf9a5ff3105b9d
Tony Lu <tonyzhehaolu@google.com>
2019-08-02 17:41:35 -0700

    Increased timeout limit

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -1115,7 +1115,7 @@ class ObjCLanguage(object):
         out.append(
             self.config.job_spec(
                 ['src/objective-c/tests/build_one_example.sh'],
-                timeout_seconds=10 * 60,
+                timeout_seconds=20 * 60,
                 shortname='ios-buildtest-example-watchOS-sample',
                 cpu_cost=1e6,
                 environ={
@@ -1126,7 +1126,7 @@ class ObjCLanguage(object):
         out.append(
             self.config.job_spec(
                 ['src/objective-c/tests/build_one_example.sh'],
-                timeout_seconds=10 * 60,
+                timeout_seconds=20 * 60,
                 shortname='ios-buildtest-example-watchOS-sample-framework',
                 cpu_cost=1e6,
                 environ={
