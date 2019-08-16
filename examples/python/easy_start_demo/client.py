5ca7452c51d4ac87185db1d3025b84c517605397
kerbalwzy <kerbalwzy@gmail.com>
2019-08-16 11:57:35 +0800

    fix:Update the constant name. Replace abbreviations with complete forms of words

--- examples/python/easy_start_demo/client.py
+++ examples/python/easy_start_demo/client.py
@@ -15,22 +15,22 @@ sys.path.insert(0, os.path.join(BaseDir, "demo_grpc_pbs"))
 
 from demo_grpc_pbs import demo_pb2, demo_pb2_grpc
 
-ServerAddress = "127.0.0.1:23334"
-ClientId = 1
+SERVER_ADDRESS = "localhost:23334"
+CLIENT_ID = 1
 
 
 # 简单模式
-# Simple Method
+# unary-unary
 def simple_method(stub):
     print("--------------Call SimpleMethod Begin--------------")
-    req = demo_pb2.Request(Cid=ClientId, ReqMsg="called by Python client")
-    resp = stub.SimpleMethod(req)
-    print("resp from server(%d), the message=%s" % (resp.Sid, resp.RespMsg))
+    request = demo_pb2.Request(client_id=CLIENT_ID, request_data="called by Python client")
+    response = stub.SimpleMethod(request)
+    print("resp from server(%d), the message=%s" % (response.server_id, response.response_data))
     print("--------------Call SimpleMethod Over---------------")
 
 
 # 客户端流模式（在一次调用中, 客户端可以多次向服务器传输数据, 但是服务器只能返回一次响应）
-# Request-streaming (In a single call, the client can transfer data to the server several times,
+# stream-unary (In a single call, the client can transfer data to the server several times,
 # but the server can only return a response once.)
 def client_streaming_method(stub):
     print("--------------Call ClientStreamingMethod Begin--------------")
@@ -39,50 +39,50 @@ def client_streaming_method(stub):
     # create a generator
     def request_messages():
         for i in range(5):
-            req = demo_pb2.Request(Cid=ClientId, ReqMsg=("called by Python client, message:%d" % i))
-            yield req
+            request = demo_pb2.Request(client_id=CLIENT_ID, request_data=("called by Python client, message:%d" % i))
+            yield request
 
-    resp = stub.ClientStreamingMethod(request_messages())
-    print("resp from server(%d), the message=%s" % (resp.Sid, resp.RespMsg))
+    response = stub.ClientStreamingMethod(request_messages())
+    print("resp from server(%d), the message=%s" % (response.server_id, response.response_data))
     print("--------------Call ClientStreamingMethod Over---------------")
 
 
 # 服务端流模式（在一次调用中, 客户端只能一次向服务器传输数据, 但是服务器可以多次返回响应）
-# Response-streaming (In a single call, the client can only transmit data to the server at one time,
+# unary-stream (In a single call, the client can only transmit data to the server at one time,
 # but the server can return the response many times.)
 def server_streaming_method(stub):
     print("--------------Call ServerStreamingMethod Begin--------------")
-    req = demo_pb2.Request(Cid=ClientId, ReqMsg="called by Python client")
-    resp_s = stub.ServerStreamingMethod(req)
-    for resp in resp_s:
-        print("recv from server(%d), message=%s" % (resp.Sid, resp.RespMsg))
+    request = demo_pb2.Request(client_id=CLIENT_ID, request_data="called by Python client")
+    response_iterator = stub.ServerStreamingMethod(request)
+    for response in response_iterator:
+        print("recv from server(%d), message=%s" % (response.server_id, response.response_data))
 
     print("--------------Call ServerStreamingMethod Over---------------")
 
 
 # 双向流模式 (在一次调用中, 客户端和服务器都可以向对方多次收发数据)
-# Bidirectional Streaming (In a single call, both client and server can send and receive data
+# stream-stream (In a single call, both client and server can send and receive data
 # to each other multiple times.)
 def bidirectional_streaming_method(stub):
     print("--------------Call BidirectionalStreamingMethod Begin---------------")
 
     # 创建一个生成器
     # create a generator
-    def req_messages():
+    def request_messages():
         for i in range(5):
-            req = demo_pb2.Request(Cid=ClientId, ReqMsg=("called by Python client, message: %d" % i))
-            yield req
+            request = demo_pb2.Request(client_id=CLIENT_ID, request_data=("called by Python client, message: %d" % i))
+            yield request
             time.sleep(1)
 
-    resp_s = stub.BidirectionalStreamingMethod(req_messages())
-    for resp in resp_s:
-        print("recv from server(%d), message=%s" % (resp.Sid, resp.RespMsg))
+    response_iterator = stub.BidirectionalStreamingMethod(request_messages())
+    for response in response_iterator:
+        print("recv from server(%d), message=%s" % (response.server_id, response.response_data))
 
     print("--------------Call BidirectionalStreamingMethod Over---------------")
 
 
 def main():
-    with grpc.insecure_channel(ServerAddress) as channel:
+    with grpc.insecure_channel(SERVER_ADDRESS) as channel:
         stub = demo_pb2_grpc.GRPCDemoStub(channel)
 
         simple_method(stub)
--- examples/python/easy_start_demo/server.py
+++ examples/python/easy_start_demo/server.py
@@ -18,61 +18,61 @@ sys.path.insert(0, os.path.join(BaseDir, "demo_grpc_pbs"))
 
 from demo_grpc_pbs import demo_pb2, demo_pb2_grpc
 
-ServerAddress = '127.0.0.1:23334'
-ServerId = 1
+SERVER_ADDRESS = 'localhost:23334'
+SERVER_ID = 1
 
 
 class DemoServer(demo_pb2_grpc.GRPCDemoServicer):
 
     # 简单模式
-    # Simple
+    # unary-unary
     def SimpleMethod(self, request, context):
-        print("SimpleMethod called by client(%d) the message: %s" % (request.Cid, request.ReqMsg))
-        resp = demo_pb2.Response(Sid=ServerId, RespMsg="Python server SimpleMethod Ok!!!!")
-        return resp
+        print("SimpleMethod called by client(%d) the message: %s" % (request.client_id, request.request_data))
+        response = demo_pb2.Response(server_id=SERVER_ID, response_data="Python server SimpleMethod Ok!!!!")
+        return response
 
     # 客户端流模式（在一次调用中, 客户端可以多次向服务器传输数据, 但是服务器只能返回一次响应）
-    # Request-streaming (In a single call, the client can transfer data to the server several times,
+    # stream-unary (In a single call, the client can transfer data to the server several times,
     # but the server can only return a response once.)
     def ClientStreamingMethod(self, request_iterator, context):
         print("ClientStreamingMethod called by client...")
-        for req in request_iterator:
-            print("recv from client(%d), message= %s" % (req.Cid, req.ReqMsg))
-        resp = demo_pb2.Response(Sid=ServerId, RespMsg="Python server ClientStreamingMethod ok")
-        return resp
+        for request in request_iterator:
+            print("recv from client(%d), message= %s" % (request.client_id, request.request_data))
+        response = demo_pb2.Response(server_id=SERVER_ID, response_data="Python server ClientStreamingMethod ok")
+        return response
 
     # 服务端流模式（在一次调用中, 客户端只能一次向服务器传输数据, 但是服务器可以多次返回响应）
-    # Response-streaming (In a single call, the client can only transmit data to the server at one time,
+    # unary-stream (In a single call, the client can only transmit data to the server at one time,
     # but the server can return the response many times.)
     def ServerStreamingMethod(self, request, context):
-        print("ServerStreamingMethod called by client(%d), message= %s" % (request.Cid, request.ReqMsg))
+        print("ServerStreamingMethod called by client(%d), message= %s" % (request.client_id, request.request_data))
 
         # 创建一个生成器
         # create a generator
         def response_messages():
             for i in range(5):
-                resp = demo_pb2.Response(Sid=ServerId, RespMsg=("send by Python server, message=%d" % i))
-                yield resp
+                response = demo_pb2.Response(server_id=SERVER_ID, response_data=("send by Python server, message=%d" % i))
+                yield response
 
         return response_messages()
 
     # 双向流模式 (在一次调用中, 客户端和服务器都可以向对方多次收发数据)
-    # Bidirectional Streaming (In a single call, both client and server can send and receive data
+    # stream-stream (In a single call, both client and server can send and receive data
     # to each other multiple times.)
     def BidirectionalStreamingMethod(self, request_iterator, context):
         print("BidirectionalStreamingMethod called by client...")
 
         # 开启一个子线程去接收数据
         # Open a sub thread to receive data
-        def parse_req():
-            for req in request_iterator:
-                print("recv from client(%d), message= %s" % (req.Cid, req.ReqMsg))
+        def parse_request():
+            for request in request_iterator:
+                print("recv from client(%d), message= %s" % (request.client_id, request.request_data))
 
-        t = Thread(target=parse_req)
+        t = Thread(target=parse_request)
         t.start()
 
         for i in range(5):
-            yield demo_pb2.Response(Sid=ServerId, RespMsg=("send by Python server, message= %d" % i))
+            yield demo_pb2.Response(server_id=SERVER_ID, response_data=("send by Python server, message= %d" % i))
 
         t.join()
 
@@ -82,7 +82,7 @@ def main():
 
     demo_pb2_grpc.add_GRPCDemoServicer_to_server(DemoServer(), server)
 
-    server.add_insecure_port(ServerAddress)
+    server.add_insecure_port(SERVER_ADDRESS)
     print("------------------start Python GRPC server")
     server.start()
 
