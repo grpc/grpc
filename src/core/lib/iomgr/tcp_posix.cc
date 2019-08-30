3476df0b5021b7517d8c756ffdffc771349e8964
Yash Tibrewal <yashkt@google.com>
2019-08-30 15:04:06 -0700

    Add in ref to error

--- src/core/lib/iomgr/tcp_posix.cc
+++ src/core/lib/iomgr/tcp_posix.cc
@@ -1020,7 +1020,7 @@ static void tcp_handle_write(void* arg /* grpc_tcp */, grpc_error* error) {
   if (error != GRPC_ERROR_NONE) {
     cb = tcp->write_cb;
     tcp->write_cb = nullptr;
-    GRPC_CLOSURE_SCHED(cb, error);
+    GRPC_CLOSURE_SCHED(cb, GRPC_ERROR_REF(error));
     TCP_UNREF(tcp, "write");
     return;
   }
