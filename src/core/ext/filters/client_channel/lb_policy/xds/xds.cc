b77e1d8a96c93ba536bbee21838ddd536a2cd488
Mark D. Roth <roth@google.com>
2019-09-30 15:04:20 -0700

    add logging to attempt to debug timeouts

--- src/core/ext/filters/client_channel/lb_policy/xds/xds.cc
+++ src/core/ext/filters/client_channel/lb_policy/xds/xds.cc
@@ -872,7 +872,13 @@ class XdsLb::LbChannelState::StateWatcher
   explicit StateWatcher(RefCountedPtr<LbChannelState> parent)
       : AsyncConnectivityStateWatcherInterface(
             grpc_combiner_scheduler(parent->xdslb_policy_->combiner())),
-        parent_(std::move(parent)) {}
+        parent_(std::move(parent)) {
+gpr_log(GPR_INFO, "CREATE StateWatcher %p", this);
+  }
+
+  ~StateWatcher() {
+gpr_log(GPR_INFO, "DESTROY StateWatcher %p", this);
+  }
 
  private:
   void OnConnectivityStateChange(grpc_connectivity_state new_state) override {
