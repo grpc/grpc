db577b33604b23a2b8aaf92c9ac70018207f0b85
Yash Tibrewal <yashkt@google.com>
2019-10-22 19:22:45 -0700

    Convert some GRPC_CLOSURE_SCHED to GRPC_CLOSURE_RUN

--- src/core/lib/iomgr/tcp_posix.cc
+++ src/core/lib/iomgr/tcp_posix.cc
@@ -415,7 +415,7 @@ static void call_read_cb(grpc_tcp* tcp, grpc_error* error) {
 
   tcp->read_cb = nullptr;
   tcp->incoming_buffer = nullptr;
-  GRPC_CLOSURE_SCHED(cb, error);
+  GRPC_CLOSURE_RUN(cb, error);
 }
 
 #define MAX_READ_IOVEC 4
@@ -643,7 +643,7 @@ static void tcp_read(grpc_endpoint* ep, grpc_slice_buffer* incoming_buffer,
      * right thing (i.e calls tcp_do_read() which either reads the available
      * bytes or calls notify_on_read() to be notified when new bytes become
      * available */
-    GRPC_CLOSURE_SCHED(&tcp->read_done_closure, GRPC_ERROR_NONE);
+    GRPC_CLOSURE_RUN(&tcp->read_done_closure, GRPC_ERROR_NONE);
   }
 }
 
@@ -1023,7 +1023,7 @@ static void tcp_handle_write(void* arg /* grpc_tcp */, grpc_error* error) {
   if (error != GRPC_ERROR_NONE) {
     cb = tcp->write_cb;
     tcp->write_cb = nullptr;
-    GRPC_CLOSURE_SCHED(cb, GRPC_ERROR_REF(error));
+    GRPC_CLOSURE_RUN(cb, GRPC_ERROR_REF(error));
     TCP_UNREF(tcp, "write");
     return;
   }
@@ -1072,11 +1072,11 @@ static void tcp_write(grpc_endpoint* ep, grpc_slice_buffer* buf,
 
   tcp->outgoing_buffer_arg = arg;
   if (buf->length == 0) {
-    GRPC_CLOSURE_SCHED(
-        cb, grpc_fd_is_shutdown(tcp->em_fd)
-                ? tcp_annotate_error(
-                      GRPC_ERROR_CREATE_FROM_STATIC_STRING("EOF"), tcp)
-                : GRPC_ERROR_NONE);
+    GRPC_CLOSURE_RUN(cb,
+                     grpc_fd_is_shutdown(tcp->em_fd)
+                         ? tcp_annotate_error(
+                               GRPC_ERROR_CREATE_FROM_STATIC_STRING("EOF"), tcp)
+                         : GRPC_ERROR_NONE);
     tcp_shutdown_buffer_list(tcp);
     return;
   }
@@ -1098,7 +1098,7 @@ static void tcp_write(grpc_endpoint* ep, grpc_slice_buffer* buf,
       const char* str = grpc_error_string(error);
       gpr_log(GPR_INFO, "write: %s", str);
     }
-    GRPC_CLOSURE_SCHED(cb, error);
+    GRPC_CLOSURE_RUN(cb, error);
   }
 }
 
