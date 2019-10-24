c66bdcdbe0c1d3f1b3c829368ae799b3ed79c2da
Lidi Zheng <lidiz@google.com>
2019-10-23 19:09:41 -0700

    Unblock the compilation speed up

--- tools/run_tests/helper_scripts/build_python.sh
+++ tools/run_tests/helper_scripts/build_python.sh
@@ -122,7 +122,7 @@ export LANG=en_US.UTF-8
 
 # Allow build_ext to build C/C++ files in parallel
 # by enabling a monkeypatch. It speeds up the build a lot.
-export GRPC_PYTHON_BUILD_EXT_COMPILER_JOBS=4
+export GRPC_PYTHON_BUILD_EXT_COMPILER_JOBS=${GRPC_PYTHON_BUILD_EXT_COMPILER_JOBS:-4}
 
 # If ccache is available on Linux, use it.
 if [ "$(is_linux)" ]; then
