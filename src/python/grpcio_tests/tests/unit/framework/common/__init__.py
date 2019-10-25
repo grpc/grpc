3b652bc3ef7b849e6cdfaf5534a3c6162c34516f
Richard Belleville <rbellevi@google.com>
2019-10-25 15:23:45 -0700

    WIP. Start writing signatures

--- src/python/grpcio_tests/tests/unit/framework/common/__init__.py
+++ src/python/grpcio_tests/tests/unit/framework/common/__init__.py
@@ -11,3 +11,45 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import contextlib
+import socket
+
+
+def get_socket(bind_address='localhost', sock_options=(socket.SO_REUSEPORT,)):
+    """Opens a listening socket on an arbitrary port.
+
+    Useful for reserving a port for a system-under-test.
+
+    Args:
+      bind_address: The host to which to bind.
+      sock_options: A sequence of socket options to apply to the socket.
+
+    Returns:
+      A tuple containing:
+        - the address to which the socket is bound
+        - the port to which the socket is bound
+        - the socket object itself
+    """
+    _sock_options = sock_options if sock_options else []
+    for address_family in (socket.AF_INET, socket.AF_INET6):
+        try:
+            sock = socket.socket(address_family, socket.SOCK_STREAM)
+            for sock_option in _sock_options:
+                sock.setsockopt(socket.SOL_SOCKET, sock_option, 1)
+            sock.bind((bind_address, 0))
+            sock.listen(1)
+            return bind_address, sock.getsockname()[1], sock
+        except socket.error:
+            continue
+    raise RuntimeError("Failed to find to {} with sock_options {}".format(bind_address, sock_options))
+
+
+@contextlib.contextmanager
+def listening_socket(bind_address='localhost', sock_options=(socket.SO_REUSEPORT,)):
+    # TODO: Docstring.
+    host, port, sock = get_socket(bind_address=bind_address, sock_options=sock_options)
+    try:
+        yield host, port
+    finally:
+        sock.close()
