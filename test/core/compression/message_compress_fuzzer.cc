07a7b9fe0f02b2b8504dbc992862a0f33a4a76ca
Samuel Lijin <grpc@sxlijin.com>
2019-10-25 00:32:25 -0700

    Fuzz gRPC message/stream compression and decompression
    Google had a company-wide FuzzIt hackathon recently, so I went digging
    in gRPC and found this, which means I probably don't know what I'm doing :)
    
    release notes: no

---
+++ test/core/compression/message_compress_fuzzer.cc
@@ -0,0 +1,36 @@
+#include <fuzzer/FuzzedDataProvider.h>
+#include <grpc/grpc.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "src/core/lib/compression/message_compress.h"
+#include "src/core/lib/security/credentials/credentials.h"
+#include "test/core/util/memory_counters.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  FuzzedDataProvider data_provider(data, size);
+  const auto compression_algorithm =
+      static_cast<grpc_message_compression_algorithm>(
+          data_provider.ConsumeIntegralInRange<int>(
+              0, GRPC_MESSAGE_COMPRESS_ALGORITHMS_COUNT - 1));
+  const auto fuzz_data = data_provider.ConsumeRemainingBytes<char>();
+
+  grpc_core::testing::LeakDetector leak_detector(true);
+  grpc_init();
+  grpc_test_only_control_plane_credentials_force_init();
+  grpc_slice_buffer input_buffer;
+  grpc_slice_buffer_init(&input_buffer);
+  grpc_slice_buffer_add(&input_buffer, grpc_slice_from_copied_buffer(
+                                           fuzz_data.data(), fuzz_data.size()));
+  grpc_slice_buffer output_buffer;
+  grpc_slice_buffer_init(&output_buffer);
+
+  grpc_msg_compress(compression_algorithm, &input_buffer, &output_buffer);
+
+  grpc_slice_buffer_destroy(&input_buffer);
+  grpc_slice_buffer_destroy(&output_buffer);
+  grpc_test_only_control_plane_credentials_destroy();
+  grpc_shutdown_blocking();
+  return 0;
+}
---
+++ test/core/compression/stream_compression_fuzzer.cc
@@ -0,0 +1,33 @@
+#include <grpc/grpc.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "src/core/lib/compression/stream_compression.h"
+#include "src/core/lib/security/credentials/credentials.h"
+#include "test/core/util/memory_counters.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  grpc_core::testing::LeakDetector leak_detector(true);
+  grpc_init();
+  grpc_test_only_control_plane_credentials_force_init();
+  auto* context = grpc_stream_compression_context_create(
+      GRPC_STREAM_COMPRESSION_GZIP_COMPRESS);
+  grpc_slice_buffer input_buffer;
+  grpc_slice_buffer_init(&input_buffer);
+  grpc_slice_buffer_add(
+      &input_buffer,
+      grpc_slice_from_copied_buffer(reinterpret_cast<const char*>(data), size));
+  grpc_slice_buffer output_buffer;
+  grpc_slice_buffer_init(&output_buffer);
+
+  grpc_stream_compress(context, &input_buffer, &output_buffer, nullptr,
+                       (~(size_t)0), GRPC_STREAM_COMPRESSION_FLUSH_SYNC);
+
+  grpc_stream_compression_context_destroy(context);
+  grpc_slice_buffer_destroy(&input_buffer);
+  grpc_slice_buffer_destroy(&output_buffer);
+  grpc_test_only_control_plane_credentials_destroy();
+  grpc_shutdown_blocking();
+  return 0;
+}
