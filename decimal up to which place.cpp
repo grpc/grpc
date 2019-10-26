2fc8a0ea526d3508c8113f244983d352b9bb8544
sachin12-dot <57026263+sachin12-dot@users.noreply.github.com>
2019-10-26 11:07:50 +0530

    create decimal up to which place.cpp

---
+++ decimal up to which place.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int w;
+    long int q;
+    char a;
+    float b;
+    double c;
+    cin>>w;
+    cin>>q;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cout<<w<<endl;
+    cout<<q<<endl;
+    cout<<a<<endl;;
+    cout<<fixed<<setprecision(3)<<b<<endl;
+    cout<<fixed<<setprecision(9)<<c;
+
+    return 0;
+}
