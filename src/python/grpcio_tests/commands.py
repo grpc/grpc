e2ee7bb679cfc41d86ea8267750648df7b0bd44a
Lidi Zheng <lidiz@google.com>
2019-10-30 17:00:16 -0700

    Disable unit._channel_close_test.ChannelCloseTest

--- src/python/grpcio_tests/commands.py
+++ src/python/grpcio_tests/commands.py
@@ -191,7 +191,8 @@ class TestGevent(setuptools.Command):
     )
     BANNED_WINDOWS_TESTS = (
         # TODO(https://github.com/grpc/grpc/pull/15411) enable this test
-        'unit._dns_resolver_test.DNSResolverTest.test_connect_loopback',)
+        'unit._dns_resolver_test.DNSResolverTest.test_connect_loopback',
+        'unit._channel_close_test.ChannelCloseTest',)
     description = 'run tests with gevent.  Assumes grpc/gevent are installed'
     user_options = []
 
