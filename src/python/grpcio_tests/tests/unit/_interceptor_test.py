10e7befbf49fcc7efc226857ef6f41de536c2c1d
Richard Belleville <rbellevi@google.com>
2019-11-06 11:51:17 -0800

    Appease pylint

--- src/python/grpcio_tests/tests/unit/_interceptor_test.py
+++ src/python/grpcio_tests/tests/unit/_interceptor_test.py
@@ -563,7 +563,7 @@ class InterceptorTest(unittest.TestCase):
 
     # NOTE: The single-threaded unary-stream path does not support the
     # grpc.Future interface, so this test does not apply.
-    @unittest.skipIf(os.getenv("GRPC_SINGLE_THREADED_UNARY_STREAM"))
+    @unittest.skipIf(os.getenv("GRPC_SINGLE_THREADED_UNARY_STREAM"), "Not supported.")
     def testInterceptedUnaryRequestStreamResponseWithError(self):
         request = _EXCEPTION_REQUEST
 
