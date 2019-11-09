bc345de78856ef11cc770990cf0deae6f21f5fba
Stanley Cheung <stanleycheung@google.com>
2019-11-09 10:59:04 -0800

    Fix int64_t type

--- src/php/ext/grpc/php_grpc.c
+++ src/php/ext/grpc/php_grpc.c
@@ -271,7 +271,7 @@ static void custom_logger(gpr_log_func_args* args) {
     return;
   }
 
-  gpr_asprintf(&prefix, "%s%ld.%09" PRId32 " %s:%d]",
+  gpr_asprintf(&prefix, "%s%" PRId64 ".%09" PRId32 " %s:%d]",
                gpr_log_severity_string(args->severity), now.tv_sec,
                now.tv_nsec, display_file, args->line);
 
