4ba1864cd9d4592f597830e5e779263ff4864e9e
Muxi Yan <mxyan@google.com>
2019-11-15 08:55:36 -0800

    Use gRPC repo to store boringssl prefix headers

--- tools/distrib/upgrade_boringssl_objc.sh
+++ tools/distrib/upgrade_boringssl_objc.sh
@@ -22,27 +22,24 @@ cd "$(dirname $0)"
 cd ../../third_party/boringssl
 
 BORINGSSL_COMMIT=$(git rev-parse HEAD)
+BORINGSSL_PREFIX_HEADERS_DIR=src/objective-c/boringssl_prefix_headers
 
 # Do the following in grpc root directory
 cd ../..
 
 docker build tools/dockerfile/grpc_objc/generate_boringssl_prefix_header -t grpc/boringssl_prefix_header
-mkdir -p ./boringssl_prefix_header_out
-docker run -it --rm -v $(pwd)/boringssl_prefix_header_out:/output grpc/boringssl_prefix_header $BORINGSSL_COMMIT
+mkdir -p $BORINGSSL_PREFIX_HEADERS_DIR
+docker run -it --rm -v $(pwd)/$BORINGSSL_PREFIX_HEADERS_DIR:/output grpc/boringssl_prefix_header $BORINGSSL_COMMIT
+git add $BORINGSSL_PREFIX_HEADERS_DIR/boringssl_prefix_symbols-$BORINGSSL_COMMIT.h
 
-# increase the minor version by 1
+# Increase the minor version by 1
 POD_VER=$(cat templates/src/objective-c/BoringSSL-GRPC.podspec.template | grep 'version = ' | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
 POD_VER_NEW="${POD_VER%.*}.$((${POD_VER##*.}+1))"
 sed -i.grpc_back -e "s/version = '$POD_VER'/version = '$POD_VER_NEW'/g" templates/src/objective-c/BoringSSL-GRPC.podspec.template
 sed -i.grpc_back -e "s/dependency 'BoringSSL-GRPC', '$POD_VER'/dependency 'BoringSSL-GRPC', '$POD_VER_NEW'/g" templates/gRPC-Core.podspec.template
 rm templates/src/objective-c/BoringSSL-GRPC.podspec.template.grpc_back templates/gRPC-Core.podspec.template.grpc_back
 
-RED=$'\e[1;31m'
-DEFAULT=$'\e[0m'
-echo ""
-echo "Upgrade is completed."
-echo "${RED}TODOs:"
-echo "1. The script generated prefix header at /boringssl_prefix_header_out/boringssl_prefix_symbols-$BORINGSSL_COMMIT.h. Upload it to gRPC Team Cloud Storage at location gs://grpc_boringssl_prefix_headers."
-echo "2. Regenerate the project.${DEFAULT}"
+# Regenerated the project
+tools/buildgen/generate_projects.sh
 
 exit 0
