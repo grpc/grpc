4dd753084a4aba49c3776db76c14ccbdc570808c
Esun Kim <veblush@google.com>
2019-11-19 22:21:15 -0800

    Fix the version of twine to 2.0

--- tools/run_tests/artifacts/build_artifact_python.sh
+++ tools/run_tests/artifacts/build_artifact_python.sh
@@ -134,7 +134,7 @@ fi
 
 # Ensure the generated artifacts are valid.
 "${PYTHON}" -m virtualenv venv || { "${PYTHON}" -m pip install virtualenv && "${PYTHON}" -m virtualenv venv; }
-venv/bin/python -m pip install twine
+venv/bin/python -m pip install "twine<=2.0"
 venv/bin/python -m twine check dist/* tools/distrib/python/grpcio_tools/dist/*
 rm -rf venv/
 
