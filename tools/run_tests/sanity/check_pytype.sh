355287204cbb5b4c2c595490da88ed01472e9825
Lidi Zheng <lidiz@google.com>
2019-12-09 13:03:38 -0800

    Make pytype check script depends on the configuration

--- tools/run_tests/sanity/check_pytype.sh
+++ tools/run_tests/sanity/check_pytype.sh
@@ -3,6 +3,4 @@
 JOBS=`nproc` || JOBS=4
 python3 -m pip install pytype==2019.11.27
 
-for dir in src/python/grpcio*; do
-    python3 -m pytype --keep-going -j "$JOBS" --strict-import $dir
-done
+python3 -m pytype --keep-going -j "$JOBS" --strict-import --config "setup.cfg"
