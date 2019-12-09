ea1b5204eaab2984f729cd1b10a1a676e5cc22ae
Lidi Zheng <lidiz@google.com>
2019-12-09 10:26:28 -0800

    Make gen_build_yaml py23 agnostic

--- test/core/end2end/gen_build_yaml.py
+++ test/core/end2end/gen_build_yaml.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 # Copyright 2015 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,6 +16,7 @@
 
 """Generates the appropriate build.json data for all the end2end tests."""
 
+from __future__ import print_function
 
 import yaml
 import collections
@@ -405,7 +406,7 @@ def main():
           for t in END2END_TESTS.keys()
       )
   }
-  print yaml.dump(json)
+  print(yaml.dump(json))
 
 
 if __name__ == '__main__':
