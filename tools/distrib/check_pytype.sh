ae3c098e021769e5e365a2b17a01abbb9c5ce81d
Lidi Zheng <lidiz@google.com>
2019-12-11 10:16:51 -0800

    Fix the styling

--- tools/distrib/check_pytype.sh
+++ tools/distrib/check_pytype.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-JOBS=`nproc` || JOBS=4
+JOBS=$(nproc) || JOBS=4
 python3 -m pip install pytype==2019.11.27
 
 python3 -m pytype --keep-going -j "$JOBS" --strict-import --config "setup.cfg"
