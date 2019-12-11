d35b24bd60ad4e121f38eb3c54df67533084ed2f
Richard Belleville <rbellevi@google.com>
2019-12-11 12:26:14 -0800

    Use dynamic stubs in helloworld example

--- examples/python/helloworld/greeter_client.py
+++ examples/python/helloworld/greeter_client.py
@@ -18,8 +18,7 @@ import logging
 
 import grpc
 
-import helloworld_pb2
-import helloworld_pb2_grpc
+protos, services = grpc.protos_and_services("helloworld.proto")
 
 
 def run():
@@ -27,8 +26,8 @@ def run():
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+        stub = services.GreeterStub(channel)
+        response = stub.SayHello(protos.HelloRequest(name='you'))
     print("Greeter client received: " + response.message)
 
 
