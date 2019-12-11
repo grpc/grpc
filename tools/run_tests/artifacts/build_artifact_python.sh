28ca0c6b6a8f0e843735c6fa80dbc9159788bd82
Lidi Zheng <lidiz@google.com>
2019-12-11 12:22:30 -0800

    Install necessary packages earlier

--- tools/run_tests/artifacts/build_artifact_python.sh
+++ tools/run_tests/artifacts/build_artifact_python.sh
@@ -22,6 +22,9 @@ export PYTHON=${PYTHON:-python}
 export PIP=${PIP:-pip}
 export AUDITWHEEL=${AUDITWHEEL:-auditwheel}
 
+# Install mandatory Python dependencies to avoid source wheel build failure.
+"${PIP}" install -rrequirements.txt
+
 # Allow build_ext to build C/C++ files in parallel
 # by enabling a monkeypatch. It speeds up the build a lot.
 # Use externally provided GRPC_PYTHON_BUILD_EXT_COMPILER_JOBS value if set.
@@ -96,8 +99,6 @@ fi
 # are in a docker image or in a virtualenv.
 if [ "$GRPC_BUILD_GRPCIO_TOOLS_DEPENDENTS" != "" ]
 then
-  "${PIP}" install -rrequirements.txt
-
   if [ "$("$PYTHON" -c "import sys; print(sys.version_info[0])")" == "2" ]
   then
     "${PIP}" install futures>=2.2.0
