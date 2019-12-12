e27782c1c56a3cb95b7f4804fc243cdd4863cade
Lidi Zheng <lidiz@google.com>
2019-12-11 18:08:33 -0800

    Simplify the check buildifier script logic

--- tools/distrib/buildifier_format_code.sh
+++ tools/distrib/buildifier_format_code.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -ex
+#! /bin/bash -e
 # Copyright 2019 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- tools/run_tests/sanity/check_buildifier.sh
+++ tools/run_tests/sanity/check_buildifier.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -x
+#! /bin/bash
 # Copyright 2019 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,13 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# The script to sanitize Bazel files
+# The script to check if Bazel files needs to be formatted.
 
 GIT_ROOT="$(dirname "$0")/../../.."
-TMP_ROOT="/tmp/buildifier_grpc"
-rm -rf "${TMP_ROOT}"
-git clone -- "$GIT_ROOT" "$TMP_ROOT"
-buildifier -r -v -mode=diff $TMP_ROOT
+"$GIT_ROOT/tools/distrib/buildifier_format_code.sh" -mode=diff
 result=$?
 
 if [[ ${result} != 0 ]]; then
