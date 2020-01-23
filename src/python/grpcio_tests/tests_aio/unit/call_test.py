48a8072bc90791738dadb26ae7500079ca12f830
Lidi Zheng <lidiz@google.com>
2020-01-22 16:28:29 -0800

    Add timeout tests

--- src/python/grpcio_tests/tests_aio/unit/call_test.py
+++ src/python/grpcio_tests/tests_aio/unit/call_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests behavior of the grpc.aio.UnaryUnaryCall class."""
+"""Tests behavior of the Call classes."""
 
 import asyncio
 import datetime
