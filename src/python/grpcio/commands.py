09d121f65e5c873b5c4f6332698995753b3a2068
Lidi Zheng <lidiz@google.com>
2020-01-28 13:32:59 -0800

    Fix the windows build failure

--- src/python/grpcio/commands.py
+++ src/python/grpcio/commands.py
@@ -232,18 +232,21 @@ class BuildExt(build_ext.build_ext):
         #   If we are not using a permissive compiler that's OK with being
         #   passed wrong std flags, swap out compile function by adding a filter
         #   for it.
-        if not compiler_ok_with_extra_std():
-            old_compile = self.compiler._compile
+        if platform.system() != 'Windows':
+            if not compiler_ok_with_extra_std():
+                old_compile = self.compiler._compile
 
-            def new_compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
-                if src[-2:] == '.c':
-                    extra_postargs = [
-                        arg for arg in extra_postargs if not '-std=c++' in arg
-                    ]
-                return old_compile(obj, src, ext, cc_args, extra_postargs,
-                                   pp_opts)
+                def new_compile(obj, src, ext, cc_args, extra_postargs,
+                                pp_opts):
+                    if src[-2:] == '.c':
+                        extra_postargs = [
+                            arg for arg in extra_postargs
+                            if not '-std=c++' in arg
+                        ]
+                    return old_compile(obj, src, ext, cc_args, extra_postargs,
+                                       pp_opts)
 
-            self.compiler._compile = new_compile
+                self.compiler._compile = new_compile
 
         compiler = self.compiler.compiler_type
         if compiler in BuildExt.C_OPTIONS:
