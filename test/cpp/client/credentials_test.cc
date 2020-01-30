3818f81fb84f923db80b05d66d0c729cfc313461
Matthew Stevenson <mattstev@google.com>
2020-01-30 08:43:16 -0800

    Update from master.

--- test/cpp/client/credentials_test.cc
+++ test/cpp/client/credentials_test.cc
@@ -737,7 +737,6 @@ TEST_F(CredentialsTest, TlsServerAuthorizationCheckConfigErrorMessages) {
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  //grpc::testing::TestEnvironment env(argc, argv);
   grpc_init();
   int ret = RUN_ALL_TESTS();
   grpc_shutdown();
