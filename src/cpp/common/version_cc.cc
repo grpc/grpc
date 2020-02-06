9b678350ec63100dfeb81929a7e45813b191dd3a
Alexander Polcyn <apolcyn@google.com>
2020-02-05 22:59:21 -0800

    Regenerate projects

--- src/cpp/common/version_cc.cc
+++ src/cpp/common/version_cc.cc
@@ -22,5 +22,5 @@
 #include <grpcpp/grpcpp.h>
 
 namespace grpc {
-grpc::string Version() { return "1.27.0"; }
+grpc::string Version() { return "1.27.1"; }
 }  // namespace grpc
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.27.0'
+    VERSION = '1.27.1'
   end
 end
