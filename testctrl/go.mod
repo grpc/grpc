eaf4d3ab1721609384a786f20a0e32d93d22be54
Ben Reed <benreed@google.com>
2020-02-11 14:46:51 -0800

    Add experimental benchmark driver and service
    Signed-off-by: Ben Reed <benreed@google.com>

---
+++ testctrl/go.mod
@@ -0,0 +1,9 @@
+module github.com/codeblooded/testctrl
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/google/uuid v1.1.1
+	google.golang.org/grpc v1.27.1
+)
---
+++ testctrl/main.go
@@ -0,0 +1,64 @@
+// Copyright 2020 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/codeblooded/testctrl/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func runDriver() {
+	// TODO: Add logic for running a driver alone.
+}
+
+func runService(port int, enableReflection bool) {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("Failed to listen on port %d: %v", port, err)
+	}
+
+	grpcServer := grpc.NewServer()
+	proto.RegisterTestCtrlServer(grpcServer, &TestCtrlServerImpl{})
+
+	if enableReflection {
+		log.Println("Enabling reflection for grpc_cli; avoid this flag in production.")
+		reflection.Register(grpcServer)
+	}
+
+	log.Printf("Running gRPC server (insecure) on port %d", port)
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatalf("Server unexpectedly crashed: %v", err)
+	}
+}
+
+func main() {
+	port := flag.Int("port", 50051, "Port to start the service.")
+	disableService := flag.Bool("disableService", false, "Disable gRPC service, running only a local driver.")
+	enableReflection := flag.Bool("enableReflection", false, "Enable reflection to interact with grpc_cli.")
+	flag.Parse()
+
+	if *disableService {
+		runDriver()
+	} else {
+		runService(*port, *enableReflection)
+	}
+}
