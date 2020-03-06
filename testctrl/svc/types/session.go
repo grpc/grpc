b5e84cf7ef6aef3ffe42be68d7587e72382bb3c0
Ben Reed <benreed@google.com>
2020-03-06 12:46:50 -0800

    Add methods to filter worker components by kind
    This commit implements the ClientWorkers and ServerWorkers methods on
    a Session. It allows users of the session to quickly access all server
    or client components without writing logic to iterate through the list
    of all workers.

--- testctrl/svc/types/session.go
+++ testctrl/svc/types/session.go
@@ -49,6 +49,16 @@ func (t *Session) Workers() []*Component {
 	return t.workers
 }
 
+// ClientWorkers returns the slice of all the workers with a kind of ClientComponent.
+func (t *Session) ClientWorkers() []*Component {
+	return t.filterWorkers(ClientComponent)
+}
+
+// ServerWorkers returns the slice of all the workers with a kind of ServerComponent.
+func (t *Session) ServerWorkers() []*Component {
+	return t.filterWorkers(ServerComponent)
+}
+
 // Scenario returns the raw test scenario protobuf. The scenario is not parsed into
 // implementation-specific types, because the service is designed to know nothing about the tests.
 func (t *Session) Scenario() *pb.Scenario {
@@ -58,4 +68,17 @@ func (t *Session) Scenario() *pb.Scenario {
 // CreateTime returns the time that the Session was instantiated.
 func (t *Session) CreateTime() time.Time {
 	return t.createTime
+}
+
+// filterWorkers returns a slice with only a specific ComponentKind.
+func (t *Session) filterWorkers(k ComponentKind) []*Component {
+	var cs []*Component
+
+	for _, w := range t.workers {
+		if w.Kind() == k {
+			cs = append(cs, w)
+		}
+	}
+
+	return cs
 }
