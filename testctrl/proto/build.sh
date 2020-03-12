0c0d7ddac366f1343827e19b95204207edb913c7
Ben Reed <benreed@google.com>
2020-03-11 17:13:30 -0700

    Fix protobuf builds, remove Makefile and rename service file
    This commit cleans up the code by improving the process to build
    protobufs. It removes the Makefile in favor of a single script,
    proto/build.sh.
    
    All built protobufs that are shared by grpc are removed at the direction
    of the grpc-go team. Instead, the protobuf plugin uses command line
    flags to resolve to the compiled versions in the grpc-go repository.
    Similarly, the Google APIs built protobufs are also removed. The go
    protobuf plugin now points to the repo with prebuilt versions.
    
    This change does introduce some inconsistencies. While the protobuf
    compiler relies on the local versions of the protobufs, the go compiler
    will rely on the built versions over the network. If these two locations
    are at different versions, the code will break.
    
    Finally, the commit cleans up some files. It changes all references to
    the grpc protobufs to the grpc-go compiled versions. It renames the
    test_sessions.proto file to scheduling_service.proto, and it places the
    compiled protobufs alongside the definition (similar to grpc-go).

---
+++ testctrl/proto/build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# PATHS specifies the type of output for the go protobuf plugin. See the
+# documentation at: https://github.com/golang/protobuf#parameters.
+PATHS="paths=source_relative"
+
+# PLUGINS defines the plugins for the go protobuf plugin.
+PLUGINS="plugins=grpc"
+
+# MODS contains the map of protobuf imports to actual compiled go packages. It
+# should not be modified by hand. Use define-import to add an entry.
+MODS=""
+
+# INCLUDES contains the list of include paths for the protobuf compiler. It
+# should not be modified by hand. Use include to add an entry.
+INCLUDES=""
+
+# define-import accepts two arguments: the import path found in the proto and
+# the path of the go code that the go plugin should actually use in the import
+# statement.
+function define-import {
+  if [ -z "$MODS" ]
+  then
+    MODS="M$1=$2"
+  else
+    MODS="$MODS,M$1=$2"
+  fi
+}
+
+# include adds a formatted include path that can be passed to protoc to the
+# $INCLUDES variable.
+function include {
+  INCLUDES="$INCLUDES -I $1"
+}
+
+# Google API protos are needed for the long-running operations service messages
+# and annotations.
+#
+# Unfortunately, the version in ../third_party may not match
+# the compiled version of the define-import statement. There are 2 possible
+# solutions: force grpc to remain up-to-date or download the latest protobuf
+# file from github and use that to compile. Both are not desirable.
+include "../../third_party/googleapis"
+define-import "google/longrunning/operations.proto" "google.golang.org/genproto/googleapis/longrunning"
+
+# The protobufs found in src/proto/grpc/testing are needed by reference.
+# Due to their package names, the entire grpc must be included.
+#
+# As with the Google API protos, the version in grpc/grpc may not match the
+# compiled version referenced in grpc/grpc-go. That said, the grpc-go team felt
+# this was the best approach.
+#
+# A possible solution would be to download the protobufs from grpc/grpc-proto on
+# github and use them to compile. However, this is also not desirable.
+include "../../"  # root of grpc/grpc
+define-import "src/proto/grpc/testing/control.proto" "google.golang.org/grpc/benchmark/grpc_testing"
+
+# The local location of the proto files.
+include "./"
+
+CMD="protoc $INCLUDES --go_out=$PATHS,$PLUGINS,$MODS:. scheduling/v1/*.proto"
+echo "$CMD"
+eval "$CMD"
+
