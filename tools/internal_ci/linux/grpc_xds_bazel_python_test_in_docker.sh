56861fb9709fe0ba947e6e5d83ddb88386245c1b
Richard Belleville <rbellevi@google.com>
2020-03-18 23:26:20 +0000

    Fix path to target

--- tools/internal_ci/linux/grpc_xds_bazel_python_test_in_docker.sh
+++ tools/internal_ci/linux/grpc_xds_bazel_python_test_in_docker.sh
@@ -45,7 +45,7 @@ touch "$TOOLS_DIR"/src/proto/grpc/testing/__init__.py
     "$PROTO_SOURCE_DIR"/messages.proto \
     "$PROTO_SOURCE_DIR"/empty.proto
 
-bazel build //src/python/grpcio_tests/tests/interop:xds_interop_client
+bazel build //src/python/grpcio_tests/tests_py3_only/interop:xds_interop_client
 
 GRPC_VERBOSITY=debug GRPC_TRACE=xds_client,xds_resolver,cds_lb,xds_lb "$PYTHON" \
   tools/run_tests/run_xds_tests.py \
@@ -53,4 +53,4 @@ GRPC_VERBOSITY=debug GRPC_TRACE=xds_client,xds_resolver,cds_lb,xds_lb "$PYTHON" 
     --project_id=grpc-testing \
     --gcp_suffix=$(date '+%s') \
     --verbose \
-    --client_cmd='bazel run //src/python/grpcio_tests/tests/interop:xds_interop_client -- --server=xds-experimental:///{server_uri} --stats_port={stats_port} --qps={qps} --verbose'
+    --client_cmd='bazel run //src/python/grpcio_tests/tests_py3_only/interop:xds_interop_client -- --server=xds-experimental:///{server_uri} --stats_port={stats_port} --qps={qps} --verbose'
