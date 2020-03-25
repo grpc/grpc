1bf2b543f77c8e60c1eda0b07ab34e783a29d4a9
Ben Reed <benreed@google.com>
2020-03-24 18:13:34 -0700

    Simplify Object and improve Health type
    Objects updated their health value by considering the status of the
    pod and its container. A map was used to map the container and pod
    states to a health value. Through experimentation, it became clear
    that the container state alone was sufficient for setting health.
    This commit replaces the map with simple conditionals.
    
    In addition, this commit renames values of the Health enum for clarity.
    The value `Unhealthy` indicated a pod may fail. The value `Failed`
    indicated that an unhealthy object terminated. Both were considered
    "unhealthy" values. This commit preemptively sets the `Failed` value
    when the state is irrecoverable and replaces `Unhealthy` with
    `NotReady` for recoverable states. Finally, it renames `Done` to
    `Succeeded` to distinguish the failing states.

--- testctrl/svc/orch/object_test.go
+++ testctrl/svc/orch/object_test.go
@@ -3,9 +3,6 @@ package orch
 import (
 	"testing"
 
-	"k8s.io/api/core/v1"
-
-	orchTest "github.com/grpc/grpc/testctrl/svc/orch/test"
 	"github.com/grpc/grpc/testctrl/svc/types"
 	"github.com/grpc/grpc/testctrl/svc/types/test"
 )
@@ -29,71 +26,6 @@ func TestNewObjects(t *testing.T) {
 	for _, c := range cs {
 		if x := set[c.Name()]; x == nil {
 			t.Errorf("NewObjects did not create an object for component %v", c.Name())
-		}
-	}
-}
-
-func TestObjectUpdate(t *testing.T) {
-	cases := []struct {
-		phase          v1.PodPhase
-		cstatus        orchTest.ContainerStateCase
-		expectedHealth Health
-	}{
-		// pod pending cases
-		{v1.PodPending, orchTest.ContainerStateCases.Terminated, Unhealthy},
-		{v1.PodPending, orchTest.ContainerStateCases.Terminating, Unhealthy},
-		{v1.PodPending, orchTest.ContainerStateCases.Waiting, Unknown},
-		{v1.PodPending, orchTest.ContainerStateCases.CrashWaiting, Unhealthy},
-		{v1.PodPending, orchTest.ContainerStateCases.Running, Unknown},
-		{v1.PodPending, orchTest.ContainerStateCases.Empty, Unknown},
-
-		// pod running cases
-		{v1.PodRunning, orchTest.ContainerStateCases.Terminated, Unhealthy},
-		{v1.PodRunning, orchTest.ContainerStateCases.Terminating, Unhealthy},
-		{v1.PodRunning, orchTest.ContainerStateCases.Waiting, Unhealthy},
-		{v1.PodRunning, orchTest.ContainerStateCases.CrashWaiting, Unhealthy},
-		{v1.PodRunning, orchTest.ContainerStateCases.Running, Healthy},
-		{v1.PodRunning, orchTest.ContainerStateCases.Empty, Unhealthy},
-
-		// pod succeeded cases
-		{v1.PodSucceeded, orchTest.ContainerStateCases.Terminated, Done},
-		{v1.PodSucceeded, orchTest.ContainerStateCases.Terminating, Done},
-		{v1.PodSucceeded, orchTest.ContainerStateCases.Waiting, Done},
-		{v1.PodSucceeded, orchTest.ContainerStateCases.CrashWaiting, Failed},
-		{v1.PodSucceeded, orchTest.ContainerStateCases.Running, Done},
-		{v1.PodSucceeded, orchTest.ContainerStateCases.Empty, Done},
-
-		// pod failed cases
-		{v1.PodFailed, orchTest.ContainerStateCases.Terminated, Failed},
-		{v1.PodFailed, orchTest.ContainerStateCases.Terminating, Failed},
-		{v1.PodFailed, orchTest.ContainerStateCases.Waiting, Failed},
-		{v1.PodFailed, orchTest.ContainerStateCases.CrashWaiting, Failed},
-		{v1.PodFailed, orchTest.ContainerStateCases.Running, Failed},
-		{v1.PodFailed, orchTest.ContainerStateCases.Empty, Failed},
-
-		// pod unknown cases
-		{v1.PodUnknown, orchTest.ContainerStateCases.Terminated, Unhealthy},
-		{v1.PodUnknown, orchTest.ContainerStateCases.Terminating, Unhealthy},
-		{v1.PodUnknown, orchTest.ContainerStateCases.Waiting, Unhealthy},
-		{v1.PodUnknown, orchTest.ContainerStateCases.CrashWaiting, Unhealthy},
-		{v1.PodUnknown, orchTest.ContainerStateCases.Running, Unhealthy},
-		{v1.PodUnknown, orchTest.ContainerStateCases.Empty, Unknown},
-	}
-
-	for _, c := range cases {
-		status := v1.PodStatus{
-			Phase: c.phase,
-			ContainerStatuses: []v1.ContainerStatus{
-				c.cstatus.Status,
-			},
-		}
-
-		o := NewObjects(test.NewComponentBuilder().Build())[0]
-		o.Update(status)
-
-		if o.Health() != c.expectedHealth {
-			t.Errorf("Object Update set health '%v' but expected '%v' for pod phase '%v' and status '%v'",
-				o.Health(), c.expectedHealth, c.phase, c.cstatus.Description)
 		}
 	}
 }
