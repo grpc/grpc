96024a9ad30c9863f7a94716ba79e6f087368067
Richard Belleville <rbellevi@google.com>
2020-04-10 19:22:55 -0700

    Update documentation

--- examples/python/multiprocessing/server.py
+++ examples/python/multiprocessing/server.py
@@ -67,12 +67,6 @@ def _run_server(bind_address):
     _LOGGER.info('Starting new server.')
     options = (('grpc.so_reuseport', 1),)
 
-    # WARNING: This example takes advantage of SO_REUSEPORT. Due to the
-    # limitations of manylinux1, none of our precompiled Linux wheels currently
-    # support this option. (https://github.com/grpc/grpc/issues/18210). To take
-    # advantage of this feature, install from source with
-    # `pip install grpcio --no-binary grpcio`.
-
     server = grpc.server(futures.ThreadPoolExecutor(
         max_workers=_THREAD_CONCURRENCY,),
                          options=options)
