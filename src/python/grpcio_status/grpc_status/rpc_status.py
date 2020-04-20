7f693a07bb3258ccffd0ec769ae98574ae11da63
Lidi Zheng <lidiz@google.com>
2020-04-20 11:16:51 -0700

    Make pylint happy

--- src/python/grpcio_status/grpc_status/rpc_status.py
+++ src/python/grpcio_status/grpc_status/rpc_status.py
@@ -85,5 +85,5 @@ __all__ = [
 ]
 
 if sys.version_info[0] >= 3 and sys.version_info[1] >= 6:
-    from . import _async as aio
+    from . import _async as aio  # pylint: disable=unused-import
     __all__.append('aio')
