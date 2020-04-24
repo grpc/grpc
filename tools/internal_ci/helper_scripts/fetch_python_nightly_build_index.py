b338c0f7a0ff0ec27ec3d32f778f4a85b6d7d28c
Lidi Zheng <lidiz@google.com>
2020-04-24 12:49:07 -0700

    Use Python to fetch compiled packages

--- tools/internal_ci/helper_scripts/fetch_python_nightly_build_index.py
+++ tools/internal_ci/helper_scripts/fetch_python_nightly_build_index.py
@@ -12,24 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -ex
+import requests
+import xml.etree.ElementTree as ET
 
-# To grpc repo root
-cd $(dirname $0)/../../..
-ROOT=$(pwd)
-
-source tools/internal_ci/helper_scripts/prepare_build_linux_rc
-
-nighly_build_python_index=$(python3 tools/internal_ci/helper_scripts/fetch_python_nightyl_build_index.py)
-
-python3 -m pip install -U nox virtualenv
-
-git clone https://github.com/googleapis/python-firestore
-cd python-firestore
-python3 -m nox --sessions unit-3.8
-
-source .nox/unit-3.8/bin/activate
-pip install -U --pre --extra-index-url=${nighly_build_python_index} grpcio
-deactivate
-
-python3 -m nox --reuse-existing-virtualenvs --sessions unit-3.8
+resp = requests.get('https://packages.grpc.io/')
+root = ET.fromstring(resp.text)
+print('https://packages.grpc.io/' +
+      root[0][0].attrib['path'].replace('index.xml', 'python'))
