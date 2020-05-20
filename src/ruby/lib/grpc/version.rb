846e23dcdc19ab47b8dfe63be34b8a25d530f3b8
Stanley Cheung <stanleycheung@google.com>
2020-05-20 14:25:55 -0700

    Bump version to 1.28.2 for Obj-C

--- src/ruby/lib/grpc/version.rb
+++ src/ruby/lib/grpc/version.rb
@@ -14,5 +14,5 @@
 
 # GRPC contains the General RPC module.
 module GRPC
-  VERSION = '1.28.1'
+  VERSION = '1.28.2'
 end
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.28.1'
+    VERSION = '1.28.2'
   end
 end
