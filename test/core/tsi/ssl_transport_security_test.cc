ff045fde0d04649cf6bdd271ea7cb6da51d6bab4
yihuaz <yihuaz@google.com>
2020-06-07 16:01:38 -0700

    Update multi-domain key materials

--- test/core/tsi/ssl_transport_security_test.cc
+++ test/core/tsi/ssl_transport_security_test.cc
@@ -875,8 +875,8 @@ void ssl_tsi_test_extract_x509_subject_names() {
   GPR_ASSERT(tsi_ssl_extract_x509_subject_names_from_pem_cert(cert, &peer) ==
              TSI_OK);
   // tsi_peer should include one common name, one certificate, one security
-  // level, six SAN fields, and two URI fields.
-  size_t expected_property_count = 10;
+  // level, seven SAN fields, three URI fields.
+  size_t expected_property_count = 12;
   GPR_ASSERT(peer.property_count == expected_property_count);
   // Check common name
   const char* expected_cn = "xpigors";
@@ -893,10 +893,12 @@ void ssl_tsi_test_extract_x509_subject_names() {
   GPR_ASSERT(check_subject_alt_name(&peer, "foo.test.domain.com") == 1);
   GPR_ASSERT(check_subject_alt_name(&peer, "bar.test.domain.com") == 1);
   // Check URI
+  GPR_ASSERT(check_subject_alt_name(&peer, "spiffe://foo.com/bar/baz") == 1);
   GPR_ASSERT(
       check_subject_alt_name(&peer, "https://foo.test.domain.com/test") == 1);
   GPR_ASSERT(
       check_subject_alt_name(&peer, "https://bar.test.domain.com/test") == 1);
+  GPR_ASSERT(check_uri(&peer, "spiffe://foo.com/bar/baz") == 1);
   GPR_ASSERT(check_uri(&peer, "https://foo.test.domain.com/test") == 1);
   GPR_ASSERT(check_uri(&peer, "https://bar.test.domain.com/test") == 1);
   // Check email address
