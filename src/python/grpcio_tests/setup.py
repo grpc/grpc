9a3ae476586fbf3ca21600f3e4711fecd1f0aebd
Lidi Zheng <lidiz@google.com>
2020-06-12 10:48:47 -0700

    Pump up the minimum version of google-auth to 1.17.1

--- src/python/grpcio_tests/setup.py
+++ src/python/grpcio_tests/setup.py
@@ -43,8 +43,8 @@ INSTALL_REQUIRES = (
     'grpcio-status>={version}'.format(version=grpc_version.VERSION),
     'grpcio-tools>={version}'.format(version=grpc_version.VERSION),
     'grpcio-health-checking>={version}'.format(version=grpc_version.VERSION),
-    'oauth2client>=1.4.7', 'protobuf>=3.6.0', 'six>=1.10', 'google-auth>=1.0.0',
-    'requests>=2.14.2')
+    'oauth2client>=1.4.7', 'protobuf>=3.6.0', 'six>=1.10',
+    'google-auth>=1.17.1', 'requests>=2.14.2')
 
 if not PY3:
     INSTALL_REQUIRES += ('futures>=2.2.0',)
