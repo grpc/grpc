74a18e963070df8b75a39c4a1913123fa2b2b654
Donna Dionne <donnadionne@google.com>
2020-06-22 15:58:37 -0700

    Remove having "re2" added explicitly.

--- tools/buildgen/generate_build_additions.sh
+++ tools/buildgen/generate_build_additions.sh
@@ -20,7 +20,6 @@ gen_build_yaml_dirs="  \
   src/boringssl        \
   src/benchmark        \
   src/proto            \
-  src/re2              \
   src/upb              \
   src/zlib             \
   src/c-ares           \
