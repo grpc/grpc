0fe7738330724b9139c24b068b64dc8875442615
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2020-06-23 14:37:21 -0700

    Add grpc-java v1.30.2 to interop test client matrix

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -221,6 +221,7 @@ LANG_RELEASE_MATRIX = {
             ('v1.29.0', ReleaseInfo()),
             ('v1.30.0', ReleaseInfo()),
             ('v1.30.1', ReleaseInfo()),
+            ('v1.30.2', ReleaseInfo()),
         ]),
     'python':
         OrderedDict([
