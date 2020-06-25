cca2f48886ce84e825aa14b648a9f83f29959f7a
DoHyun Cheon <dohyunc@google.com>
2020-06-25 01:41:35 -0700

    Evaluate Args

---
+++ src/core/lib/security/authorization/evaluate_args.h
@@ -0,0 +1,23 @@
+//
+// Created by dohyunc on 6/17/20.
+//
+
+#ifndef GRPC_EVALUATE_ARGS_H
+#define GRPC_EVALUATE_ARGS_H
+
+#include <src/core/lib/iomgr/endpoint.h>
+#include <src/core/lib/security/context/security_context.h>
+#include <src/core/lib/transport/metadata_batch.h>
+
+
+//Holds grpc values that will be converted to the necessary Envoy args.
+struct EvaluateArgs {
+  const grpc_metadata_batch* metadata;
+  const grpc_endpoint* endpoint;
+  const grpc_auth_context* auth_context;
+};
+
+
+
+
+#endif  // GRPC_EVALUATE_ARGS_H
