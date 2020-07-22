f39f341ee135f960f935da3237a1fe46f9883026
Jan Tattermusch <jtattermusch@google.com>
2020-07-22 14:59:15 +0200

    regenerate projects

--- src/csharp/build_unitypackage.bat
+++ src/csharp/build_unitypackage.bat
@@ -13,7 +13,7 @@
 @rem limitations under the License.
 
 @rem Current package versions
-set VERSION=2.31.0-dev
+set VERSION=2.31.0-pre1
 
 @rem Adjust the location of nuget.exe
 set NUGET=C:\nuget\nuget.exe
