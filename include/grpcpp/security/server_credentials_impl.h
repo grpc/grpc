1de0bfd9e2310a00cbb6467a8f3cb7fe6866c801
Karthik Ravi Shankar <karthikrs@google.com>
2020-07-24 12:47:33 -0700

    Revert "Revert "Move create_channel and credentials from ::grpc_impl to ::grpc""

--- include/grpcpp/security/server_credentials_impl.h
+++ include/grpcpp/security/server_credentials_impl.h
@@ -81,7 +81,7 @@ std::shared_ptr<ServerCredentials> LocalServerCredentials(
 
 /// Builds TLS ServerCredentials given TLS options.
 std::shared_ptr<ServerCredentials> TlsServerCredentials(
-    const TlsCredentialsOptions& options);
+    const grpc::experimental::TlsCredentialsOptions& options);
 
 }  // namespace experimental
 }  // namespace grpc_impl
--- src/cpp/client/secure_credentials.cc
+++ src/cpp/client/secure_credentials.cc
@@ -38,7 +38,7 @@
 #include "src/cpp/client/create_channel_internal.h"
 #include "src/cpp/common/secure_auth_context.h"
 
-namespace grpc_impl {
+namespace grpc {
 
 static grpc::internal::GrpcLibraryInitializer g_gli_initializer;
 SecureChannelCredentials::SecureChannelCredentials(
@@ -388,9 +388,6 @@ std::shared_ptr<CallCredentials> MetadataCredentialsFromPlugin(
       c_plugin, GRPC_PRIVACY_AND_INTEGRITY, nullptr));
 }
 
-}  // namespace grpc_impl
-
-namespace grpc {
 namespace {
 void DeleteWrapper(void* wrapper, grpc_error* /*ignored*/) {
   MetadataCredentialsPluginWrapper* w =
