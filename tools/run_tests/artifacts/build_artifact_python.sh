b8b13cf70cec9d7b76543b5403f8c01e8bbe7341
Micah Kornfield <micahk@google.com>
2020-08-05 10:30:34 -0700

    dont remove .S and .asm file

--- tools/run_tests/artifacts/build_artifact_python.sh
+++ tools/run_tests/artifacts/build_artifact_python.sh
@@ -53,6 +53,7 @@ clean_non_source_files() {
     | grep -v '\.c$' | grep -v '\.cc$' | grep -v '\.cpp$' \
     | grep -v '\.h$' | grep -v '\.hh$' | grep -v '\.inc$' \
     | grep -v '\.s$' | grep -v '\.py$' | grep -v '\.hpp$' \
+    | grep -v '\.S$' | grep -v '\.asm$'                   \
     | while read -r file; do
       rm -f "$file" || true
     done
