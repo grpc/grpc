f4d18f2d954e472cddb9cfcdc379548cd4bad51c
Yash Tibrewal <yashkt@google.com>
2020-08-28 16:59:55 -0700

    Reviewer comments

--- src/core/lib/security/certificate_provider.h
+++ src/core/lib/security/certificate_provider.h
@@ -24,8 +24,6 @@
 #include "src/core/lib/gprpp/ref_counted_ptr.h"
 #include "src/core/lib/iomgr/pollset_set.h"
 
-namespace grpc_core {
-
 // TODO(yashkt): After https://github.com/grpc/grpc/pull/23572, remove this
 // forward declaration and include the header for the distributor instead.
 struct grpc_tls_certificate_distributor;
@@ -43,8 +41,7 @@ struct grpc_tls_certificate_provider
     : public RefCounted<grpc_tls_certificate_provider> {
  public:
   grpc_tls_certificate_provider()
-      : distributor_(MakeRefCounted<grpc_tls_certificate_distributor>()),
-        interested_parties_(grpc_pollset_set_create()) {}
+      : interested_parties_(grpc_pollset_set_create()) {}
 
   virtual ~grpc_tls_certificate_provider() {
     grpc_pollset_set_destroy(interested_parties_);
@@ -52,15 +49,11 @@ struct grpc_tls_certificate_provider
 
   grpc_pollset_set* interested_parties() const { return interested_parties_; }
 
-  RefCountedPtr<grpc_tls_certificate_distributor> distributor() const {
-    return distributor_;
-  }
+  virtual RefCountedPtr<grpc_tls_certificate_distributor> distributor()
+      const = 0;
 
  private:
-  RefCountedPtr<grpc_tls_certificate_distributor> distributor_;
   grpc_pollset_set* interested_parties_;
 };
-
-}  // namespace grpc_core
 
 #endif  // GRPC_CORE_LIB_SECURITY_CERTIFICATE_PROVIDER_H
