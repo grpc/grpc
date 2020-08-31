7b2a4780eb570a71d98b473afd4801cae964eba4
Alexander Polcyn <apolcyn@google.com>
2020-08-31 16:22:38 -0700

    Add currently-failing test for thread safety of grpc_pick_unused_port

---
+++ test/core/util/concurrent_port_picking_test.cc
@@ -0,0 +1,73 @@
+/*
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <grpc/support/port_platform.h>
+
+#include <thread>
+#include <vector>
+#include <algorithm>
+
+#include <gmock/gmock.h>
+
+#include "absl/memory/memory.h"
+
+#include <grpc/grpc.h>
+#include <grpc/support/log.h>
+
+#include "test/core/util/port.h"
+#include "test/core/util/test_config.h"
+
+namespace {
+
+TEST(PortPickerTest, TestPortPickingIsThreadSafe) {
+  // Test that port-picking is thread safe, so that we don't
+  // get suprising behavior in tests that assume it.
+  // 64 threads is small enough so as to not overload the port server
+  // (used in some environments), but large enough to hit concurrency issues
+  // if they exist.
+  const int kNumConcurrentPicks = 64;
+  std::vector<int> ports;
+  ports.resize(kNumConcurrentPicks);
+  std::vector<std::unique_ptr<std::thread>> port_picking_threads;
+  port_picking_threads.reserve(kNumConcurrentPicks);
+  for (int i = 0; i < kNumConcurrentPicks; i++) {
+    port_picking_threads.push_back(absl::make_unique<std::thread>([&ports, i]() {
+      GPR_ASSERT(ports[i] == 0);
+      ports[i] = grpc_pick_unused_port_or_die();
+    }));
+  }
+  for (auto &t : port_picking_threads) {
+    t->join();
+  }
+  std::sort(ports.begin(), ports.end());
+  for (int i = 1; i < kNumConcurrentPicks; i++) {
+    ASSERT_NE(ports[i], 0);
+    ASSERT_NE(ports[i - 1], ports[i]) << "expected all picked ports to be unique, but found two which are the same";
+  }
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  grpc::testing::TestEnvironment env(argc, argv);
+  grpc_init();
+  auto result = RUN_ALL_TESTS();
+  grpc_shutdown();
+  return result;
+}
