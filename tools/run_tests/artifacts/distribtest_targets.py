455e2c9847b18a657fe121b7c8ed4e3287f2ca66
root <hannahshisfb@gmail.com>
2020-10-02 17:13:36 +0000

    replace jessie to stretch, using name PHP7 instead of PHP

--- tools/run_tests/artifacts/distribtest_targets.py
+++ tools/run_tests/artifacts/distribtest_targets.py
@@ -227,11 +227,11 @@ class RubyDistribTest(object):
         return self.name
 
 
-class PHPDistribTest(object):
+class PHP7DistribTest(object):
     """Tests PHP package"""
 
     def __init__(self, platform, arch, docker_suffix=None):
-        self.name = 'php_%s_%s_%s' % (platform, arch, docker_suffix)
+        self.name = 'php7_%s_%s_%s' % (platform, arch, docker_suffix)
         self.platform = platform
         self.arch = arch
         self.docker_suffix = docker_suffix
@@ -246,7 +246,7 @@ class PHPDistribTest(object):
         if self.platform == 'linux':
             return create_docker_jobspec(
                 self.name,
-                'tools/dockerfile/distribtest/php_%s_%s' %
+                'tools/dockerfile/distribtest/php7_%s_%s' %
                 (self.docker_suffix, self.arch),
                 'test/distrib/php/run_distrib_test.sh',
                 copy_rel_path='test/distrib')
@@ -374,6 +374,6 @@ def targets():
         RubyDistribTest('linux', 'x64', 'ubuntu1604'),
         RubyDistribTest('linux', 'x64', 'ubuntu1804'),
         # PHP
-        PHPDistribTest('linux', 'x64', 'jessie'),
-        PHPDistribTest('macos', 'x64'),
+        PHP7DistribTest('linux', 'x64', 'stretch'),
+        PHP7DistribTest('macos', 'x64'),
     ]
