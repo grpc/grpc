b410523fb332d3fd9efdc5e8d865c911f8653ead
0xCipher <64740712+0xCipher-max@users.noreply.github.com>
2020-10-03 18:07:44 +0530

    Create Calculator.cpp
    I have created a prgram that can calculate two numbers.Please Accept ASAP!!!!!!!!!

---
+++ src/cpp/calculator.cpp
@@ -0,0 +1,40 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << num1+num2;
+            break;
+
+        case '-':
+            cout << num1-num2;
+            break;
+
+        case '*':
+            cout << num1*num2;
+            break;
+
+        case '/':
+            cout << num1/num2;
+            break;
+
+        default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;
+    }
+
+    return 0;
+}
