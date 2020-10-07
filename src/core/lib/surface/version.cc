aa7b10f659a1257f890e9c2fc6ffe93e23135730
Donna Dionne <donnadionne@google.com>
2020-10-07 13:38:47 -0700

    Bump version to v1.33.x-pre1: regenerate projects

--- src/core/lib/surface/version.cc
+++ src/core/lib/surface/version.cc
@@ -23,6 +23,6 @@
 
 #include <grpc/grpc.h>
 
-const char* grpc_version_string(void) { return "12.0.0"; }
+const char* grpc_version_string(void) { return "13.0.0"; }
 
 const char* grpc_g_stands_for(void) { return "geeky"; }
--- src/csharp/build_unitypackage.bat
+++ src/csharp/build_unitypackage.bat
@@ -13,7 +13,7 @@
 @rem limitations under the License.
 
 @rem Current package versions
-set VERSION=2.33.0-dev
+set VERSION=2.33.0-pre1
 
 @rem Adjust the location of nuget.exe
 set NUGET=nuget
