4d7803c57b5a6373db8ef8ab45075143295c2880
ZhenLian <zhenlian@google.com>
2020-10-13 14:17:43 -0700

    try splitting builds into secure and unsecure

--- include/grpcpp/security/tls_certificate_provider.h
+++ include/grpcpp/security/tls_certificate_provider.h
@@ -17,6 +17,7 @@
 #ifndef GRPCPP_SECURITY_TLS_CERTIFICATE_PROVIDER_H
 #define GRPCPP_SECURITY_TLS_CERTIFICATE_PROVIDER_H
 
+#include <grpc/grpc_security.h>
 #include <grpc/grpc_security_constants.h>
 #include <grpc/status.h>
 #include <grpc/support/log.h>
@@ -25,7 +26,7 @@
 #include <memory>
 #include <vector>
 
-typedef struct grpc_tls_certificate_provider grpc_tls_certificate_provider;
+//typedef struct grpc_tls_certificate_provider grpc_tls_certificate_provider;
 
 namespace grpc {
 namespace experimental {
