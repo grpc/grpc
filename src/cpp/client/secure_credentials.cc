9850478dec673e35c4adf6a589459a543efe1171
ZhenLian <zhenlian@google.com>
2020-10-13 20:39:12 -0700

    fix comments in 10/13

--- src/cpp/client/secure_credentials.cc
+++ src/cpp/client/secure_credentials.cc
@@ -289,7 +289,7 @@ std::shared_ptr<ChannelCredentials> LocalCredentials(
 
 // Builds TLS Credentials given TLS options.
 std::shared_ptr<ChannelCredentials> TlsCredentials(
-    const TlsCredentialsOptions& options) {
+    const TlsChannelCredentialsOptions& options) {
   return WrapChannelCredentials(
       grpc_tls_credentials_create(options.c_credentials_options()));
 }
