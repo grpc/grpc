b3c0cc287324765f3a0d43864467a25b40c044d3
root <hannahshisfb@gmail.com>
2020-10-16 18:22:29 +0000

    add metadata and context to parameters

--- src/php/tests/interop/Grpc/Testing/LoadBalancerStatsServiceStub.php
+++ src/php/tests/interop/Grpc/Testing/LoadBalancerStatsServiceStub.php
@@ -2,5 +2,14 @@
 // DO NOT EDIT
 namespace Grpc\Testing;
 class LoadBalancerStatsServiceStub {
+    /**
+     * @return array of [response, initialMetadata, status]
+     */
+    function getClientStats(
+    \Grpc\Testing\LoadBalancerStatsRequest $request,
+    array $metadata,
+    \Grpc\ServerContext $serverContext) {
+        return [null, [], \Grpc\Status::unimplemented()];
+    }
 }
 
