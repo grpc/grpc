0e2acc39dacf602b22bd64afc95e84d12fba8a6a
Donna Dionne <donnadionne@google.com>
2020-10-20 10:20:08 -0700

    Updating grpc version.

--- src/ruby/lib/grpc/version.rb
+++ src/ruby/lib/grpc/version.rb
@@ -14,5 +14,5 @@
 
 # GRPC contains the General RPC module.
 module GRPC
-  VERSION = '1.33.0'
+  VERSION = '1.33.1'
 end
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.33.0'
+    VERSION = '1.33.1'
   end
 end
