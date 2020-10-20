d9db781b214c15ead2fdedeadabe0fe057ef2c33
ZhenLian <zhenlian@google.com>
2020-10-20 16:26:19 -0700

    trying to fix MacOS/Windows tests

--- include/grpcpp/security/server_credentials.h
+++ include/grpcpp/security/server_credentials.h
@@ -59,7 +59,7 @@ struct SslServerCredentialsOptions {
 };
 
 /// Wrapper around \a grpc_server_credentials, a way to authenticate a server.
-class ServerCredentials {
+class ServerCredentials : private grpc::GrpcLibraryCodegen {
  public:
   virtual ~ServerCredentials();
 
