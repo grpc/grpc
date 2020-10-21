c78018891d55710e4c17fd14e4261ca281595774
ZhenLian <zhenlian@google.com>
2020-10-20 20:55:35 -0700

    switch back to implementation without GrpcLibraryCodegen

--- include/grpcpp/security/tls_certificate_provider.h
+++ include/grpcpp/security/tls_certificate_provider.h
@@ -36,13 +36,13 @@ namespace experimental {
 // Interface for a class that handles the process to fetch credential data.
 // Implementations should be a wrapper class of an internal provider
 // implementation.
-class CertificateProviderInterface : private grpc::GrpcLibraryCodegen {
+class CertificateProviderInterface {
  public:
   virtual ~CertificateProviderInterface() = default;
   virtual grpc_tls_certificate_provider* c_provider() = 0;
 
  private:
-//  grpc::GrpcLibraryCodegen codegen_;
+  //  grpc::GrpcLibraryCodegen codegen_;
 };
 
 // A struct that stores the credential data presented to the peer in handshake
