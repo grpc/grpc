2d725d785d1666cb0bc029d1a6aabf198db2759d
Alisha Nanda <alishananda@google.com>
2020-10-23 01:00:47 +0000

    Added ADDRESS_NAME arg to resolver.h.

--- src/core/ext/filters/client_channel/resolver.h
+++ src/core/ext/filters/client_channel/resolver.h
@@ -19,9 +19,8 @@
 #ifndef GRPC_CORE_EXT_FILTERS_CLIENT_CHANNEL_RESOLVER_H
 #define GRPC_CORE_EXT_FILTERS_CLIENT_CHANNEL_RESOLVER_H
 
-#include <grpc/support/port_platform.h>
-
 #include <grpc/impl/codegen/grpc_types.h>
+#include <grpc/support/port_platform.h>
 
 #include "src/core/ext/filters/client_channel/server_address.h"
 #include "src/core/ext/filters/client_channel/service_config.h"
@@ -31,6 +30,9 @@
 #include "src/core/lib/iomgr/work_serializer.h"
 
 extern grpc_core::DebugOnlyTraceFlag grpc_trace_resolver_refcount;
+
+// Name associated with individual address, if available.
+#define GRPC_ARG_ADDRESS_NAME "grpc.address_name";
 
 namespace grpc_core {
 
