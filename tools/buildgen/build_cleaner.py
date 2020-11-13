a9361ecc627973724dc3f3f0be43f650f8a5911a
Lidi Zheng <lidiz@google.com>
2020-11-13 12:16:57 -0800

    Update build_cleaner

--- tools/buildgen/build_cleaner.py
+++ tools/buildgen/build_cleaner.py
@@ -83,7 +83,7 @@ def cleaned_build_yaml_dict_as_string(indict):
 if __name__ == '__main__':
     for filename in sys.argv[1:]:
         with open(filename) as f:
-            js = yaml.load(f)
+            js = yaml.load(f, Loader=yaml.FullLoader)
         output = cleaned_build_yaml_dict_as_string(js)
         if TEST:
             with open(filename) as f:
