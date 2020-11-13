e0290d9d051ee0afccf87ed1dab2b8dbda8bcbbb
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-11-13 09:51:07 +0300

    update php version

--- tools/gce/linux_kokoro_performance_worker_init.sh
+++ tools/gce/linux_kokoro_performance_worker_init.sh
@@ -162,8 +162,8 @@ gem install bundler
 
 # PHP dependencies
 sudo apt-get install -y php7.2 php7.2-dev php-pear unzip zlib1g-dev
-sudo wget https://phar.phpunit.de/phpunit-5.7.27.phar && \
-    sudo mv phpunit-5.7.27.phar /usr/local/bin/phpunit && \
+sudo wget https://phar.phpunit.de/phpunit-6.5.14.phar && \
+    sudo mv phpunit-6.5.14.phar /usr/local/bin/phpunit && \
     sudo chmod +x /usr/local/bin/phpunit
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
