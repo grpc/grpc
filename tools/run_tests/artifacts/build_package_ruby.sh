db521f52f9b70afdb04d6e22cb26c98ec193b40e
Jan Tattermusch <jtattermusch@google.com>
2020-12-02 16:12:43 +0100

    fixup shellcheck problem

--- tools/run_tests/artifacts/build_package_ruby.sh
+++ tools/run_tests/artifacts/build_package_ruby.sh
@@ -42,7 +42,7 @@ for arch in {x86,x64}; do
   esac
   for plat in {windows,linux,macos}; do
     # skip non-existent macos x86 protoc artifact
-    if [[ "${plat}_${arch}" == "macos_x86"]]
+    if [[ "${plat}_${arch}" == "macos_x86" ]]
     then
       continue
     fi
