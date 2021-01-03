5f1a5424a496371fca1695322093ee2092291189
Donna Dionne <donnadionne@google.com>
2021-01-02 19:51:25 -0800

    Retry the timeout test

--- src/core/lib/iomgr/exec_ctx.cc
+++ src/core/lib/iomgr/exec_ctx.cc
@@ -109,13 +109,17 @@ grpc_millis grpc_timespec_to_millis_round_up(gpr_timespec ts) {
 }
 
 grpc_millis grpc_cycle_counter_to_millis_round_down(gpr_cycle_counter cycles) {
-  return timespan_to_millis_round_down(
-      gpr_cycle_counter_sub(cycles, g_start_cycle));
+  auto time = timespec_to_millis_round_down(gpr_convert_clock_type(
+      gpr_cycle_counter_to_time(cycles), g_start_time.clock_type));
+  gpr_log(GPR_ERROR, "DONNA new code round down tv_sec: %" PRId64, time);
+  return time;
 }
 
 grpc_millis grpc_cycle_counter_to_millis_round_up(gpr_cycle_counter cycles) {
-  return timespan_to_millis_round_up(
-      gpr_cycle_counter_sub(cycles, g_start_cycle));
+  auto time = timespec_to_millis_round_up(gpr_convert_clock_type(
+      gpr_cycle_counter_to_time(cycles), g_start_time.clock_type));
+  gpr_log(GPR_ERROR, "DONNA new code round up tv_sec: %" PRId64, time);
+  return time;
 }
 
 namespace grpc_core {
