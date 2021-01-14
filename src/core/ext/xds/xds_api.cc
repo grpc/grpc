3854cb60d250b0c7673e8897933d98e72cd30931
Ashitha Santhosh <ashithasantosh@google.com>
2021-01-14 12:58:05 -0800

    Use StrCat instead of StrFormat.

--- src/core/ext/xds/xds_api.cc
+++ src/core/ext/xds/xds_api.cc
@@ -893,10 +893,8 @@ grpc_error* RoutePathMatchParse(const envoy_config_route_v3_RouteMatch* match,
       StringMatcher::Create(type, match_string, case_sensitive);
   if (!string_matcher.ok()) {
     return GRPC_ERROR_CREATE_FROM_COPIED_STRING(
-        absl::StrFormat("path matcher: %s",
-                        std::string(string_matcher.status().message()))
+        absl::StrCat("path matcher: ", string_matcher.status().message())
             .c_str());
-    ;
   }
   route->matchers.path_matcher = std::move(string_matcher.value());
   return GRPC_ERROR_NONE;
@@ -960,10 +958,8 @@ grpc_error* RouteHeaderMatchersParse(
                               present_match, invert_match);
     if (!header_matcher.ok()) {
       return GRPC_ERROR_CREATE_FROM_COPIED_STRING(
-          absl::StrFormat("header matcher: %s",
-                          std::string(header_matcher.status().message()))
+          absl::StrCat("header matcher: ", header_matcher.status().message())
               .c_str());
-      ;
     }
     route->matchers.header_matchers.emplace_back(
         std::move(header_matcher.value()));
@@ -1407,8 +1403,8 @@ grpc_error* CommonTlsContextParse(
                                   /*case_sensitive=*/!ignore_case);
         if (!string_matcher.ok()) {
           return GRPC_ERROR_CREATE_FROM_COPIED_STRING(
-              absl::StrFormat("string matcher: %s",
-                              std::string(string_matcher.status().message()))
+              absl::StrCat("string matcher: ",
+                           string_matcher.status().message())
                   .c_str());
         }
         if (type == StringMatcher::Type::SAFE_REGEX && ignore_case) {
