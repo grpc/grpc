1b9b70ee0a0739be66fddf11fdc1759d6a92a528
Alexander Polcyn <apolcyn@google.com>
2021-01-20 12:05:51 -0800

    Prevent nullptr arg

--- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_wrapper.cc
+++ src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_wrapper.cc
@@ -124,7 +124,9 @@ void AresRequest::OnDoneScheduler::Orphan() {
 AresRequest::OnDoneScheduler::~OnDoneScheduler() {
   GRPC_CARES_TRACE_LOG("request: %p ~OnDoneScheduler", parent());
   GPR_ASSERT(parent()->fds_ == nullptr);
-  ares_destroy(parent()->channel_);
+  if (parent()->channel_ != nullptr {
+    ares_destroy(parent()->channel_);
+  }
   ServerAddressList* addresses = parent()->addresses_out_->get();
   if (addresses != nullptr) {
     AddressSortingSort(parent(), addresses, "service-addresses");
