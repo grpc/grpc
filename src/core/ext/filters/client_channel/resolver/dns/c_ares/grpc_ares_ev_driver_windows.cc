f635ba63ce256d567f0a678d5dd71ad6e5ca6873
Alexander Polcyn <apolcyn@google.com>
2021-01-26 10:43:40 -0800

    Debug windows opt failure

--- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_windows.cc
+++ src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_ev_driver_windows.cc
@@ -246,7 +246,7 @@ class GrpcPolledFdWindows {
           break;
         case WRITE_PENDING:
         case WRITE_WAITING_FOR_VERIFICATION_UPON_RETRY:
-          abort();
+          GPR_ASSERT(0);
       }
     }
   }
@@ -346,7 +346,7 @@ class GrpcPolledFdWindows {
       case SOCK_STREAM:
         return SendVTCP(wsa_error_ctx, iov, iov_count);
       default:
-        abort();
+        GPR_ASSERT(0);
     }
   }
 
@@ -415,7 +415,7 @@ class GrpcPolledFdWindows {
         tcp_write_state_ = WRITE_IDLE;
         return total_sent;
     }
-    abort();
+    GPR_ASSERT(0);
   }
 
   static void OnTcpConnect(void* arg, grpc_error* error) {
@@ -481,7 +481,7 @@ class GrpcPolledFdWindows {
       case SOCK_STREAM:
         return ConnectTCP(wsa_error_ctx, target, target_len);
       default:
-        abort();
+        GPR_ASSERT(0);
     }
   }
 
@@ -720,7 +720,7 @@ class SockToPolledFdMap {
         return node->polled_fd;
       }
     }
-    abort();
+    GPR_ASSERT(0);
   }
 
   void RemoveEntry(SOCKET s) {
@@ -735,7 +735,7 @@ class SockToPolledFdMap {
       }
       prev = &node->next;
     }
-    abort();
+    GPR_ASSERT(0);
   }
 
   /* These virtual socket functions are called from within the c-ares
