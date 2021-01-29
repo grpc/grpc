82f472c4a536f3e5a0ade083379994ac2905cbe6
root <hannahshisfb@gmail.com>
2021-01-29 20:43:20 +0000

    run generate_proto_php.sh, before add php server

--- src/php/tests/interop/Grpc/Testing/ClientConfigureRequest.php
+++ src/php/tests/interop/Grpc/Testing/ClientConfigureRequest.php
@@ -27,6 +27,13 @@ class ClientConfigureRequest extends \Google\Protobuf\Internal\Message
      * Generated from protobuf field <code>repeated .grpc.testing.ClientConfigureRequest.Metadata metadata = 2;</code>
      */
     private $metadata;
+    /**
+     * The deadline to use, in seconds, for all RPCs.  If unset or zero, the
+     * client will use the default from the command-line.
+     *
+     * Generated from protobuf field <code>int32 timeout_sec = 3;</code>
+     */
+    protected $timeout_sec = 0;
 
     /**
      * Constructor.
@@ -38,6 +45,9 @@ class ClientConfigureRequest extends \Google\Protobuf\Internal\Message
      *           The types of RPCs the client sends.
      *     @type \Grpc\Testing\ClientConfigureRequest\Metadata[]|\Google\Protobuf\Internal\RepeatedField $metadata
      *           The collection of custom metadata to be attached to RPCs sent by the client.
+     *     @type int $timeout_sec
+     *           The deadline to use, in seconds, for all RPCs.  If unset or zero, the
+     *           client will use the default from the command-line.
      * }
      */
     public function __construct($data = NULL) {
@@ -93,6 +103,34 @@ class ClientConfigureRequest extends \Google\Protobuf\Internal\Message
     {
         $arr = GPBUtil::checkRepeatedField($var, \Google\Protobuf\Internal\GPBType::MESSAGE, \Grpc\Testing\ClientConfigureRequest\Metadata::class);
         $this->metadata = $arr;
+
+        return $this;
+    }
+
+    /**
+     * The deadline to use, in seconds, for all RPCs.  If unset or zero, the
+     * client will use the default from the command-line.
+     *
+     * Generated from protobuf field <code>int32 timeout_sec = 3;</code>
+     * @return int
+     */
+    public function getTimeoutSec()
+    {
+        return $this->timeout_sec;
+    }
+
+    /**
+     * The deadline to use, in seconds, for all RPCs.  If unset or zero, the
+     * client will use the default from the command-line.
+     *
+     * Generated from protobuf field <code>int32 timeout_sec = 3;</code>
+     * @param int $var
+     * @return $this
+     */
+    public function setTimeoutSec($var)
+    {
+        GPBUtil::checkInt32($var);
+        $this->timeout_sec = $var;
 
         return $this;
     }
