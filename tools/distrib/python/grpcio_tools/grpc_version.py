69869b94bb15714afdc6de340698a86b13c6bcda
Lidi Zheng <lidiz@google.com>
2021-02-09 11:50:21 -0800

    Regenerate projects

--- tools/distrib/python/grpcio_tools/grpc_version.py
+++ tools/distrib/python/grpcio_tools/grpc_version.py
@@ -14,5 +14,5 @@
 
 # AUTO-GENERATED FROM `$REPO_ROOT/templates/tools/distrib/python/grpcio_tools/grpc_version.py.template`!!!
 
-VERSION = '1.36.0.dev0'
+VERSION = '1.36.0rc1'
 PROTOBUF_VERSION = '3.14.0'
