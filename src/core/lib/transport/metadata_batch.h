c45e6b111288ab7befcab2eddfb54be1b610db9f
Ashitha Santhosh <ashithasantosh@google.com>
2021-02-09 20:52:44 -0800

    Add getter to grpc_metadata_batch.

--- src/core/lib/transport/metadata_batch.h
+++ src/core/lib/transport/metadata_batch.h
@@ -23,9 +23,12 @@
 
 #include <stdbool.h>
 
+#include "absl/types/optional.h"
+
 #include <grpc/grpc.h>
 #include <grpc/slice.h>
 #include <grpc/support/time.h>
+
 #include "src/core/lib/iomgr/exec_ctx.h"
 #include "src/core/lib/transport/metadata.h"
 #include "src/core/lib/transport/static_metadata.h"
@@ -77,6 +80,17 @@ grpc_error* grpc_metadata_batch_substitute(grpc_metadata_batch* batch,
 
 void grpc_metadata_batch_set_value(grpc_linked_mdelem* storage,
                                    const grpc_slice& value);
+
+/** Returns metadata value(s) for the specified key.
+    If the key is not present in the batch, returns absl::nullopt.
+    If the key is present exactly once in the batch, returns a string_view of
+    that value.
+    If the key is present more than once in the batch, constructs a
+    comma-concatenated string of all values in concatenated_value and returns a
+    string_view of that string. */
+absl::optional<absl::string_view> grpc_metadata_batch_get_value(
+    grpc_metadata_batch* batch, absl::string_view target_key,
+    std::string* concatenated_value);
 
 /** Add \a storage to the beginning of \a batch. storage->md is
     assumed to be valid.
