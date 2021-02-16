bd1bc71d03a0c59a0351699ba73cc67cc3dc0000
Richard Belleville <gnossen@gmail.com>
2021-02-16 12:24:31 -0800

    Merge pull request #25456 from gnossen/fix_interop_breakage
    Bump xDS Interop Tests to Python 3

--- tools/internal_ci/linux/grpc_xds_bazel_python_test_in_docker.sh
+++ tools/internal_ci/linux/grpc_xds_bazel_python_test_in_docker.sh
@@ -23,7 +23,7 @@ ${name}')
 cd /var/local/git/grpc
 
 VIRTUAL_ENV=$(mktemp -d)
-virtualenv "$VIRTUAL_ENV"
+virtualenv "$VIRTUAL_ENV" -p python3
 PYTHON="$VIRTUAL_ENV"/bin/python
 "$PYTHON" -m pip install --upgrade pip==19.3.1
 "$PYTHON" -m pip install --upgrade grpcio-tools google-api-python-client google-auth-httplib2 oauth2client
