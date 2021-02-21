b27f5a7ffebe8194ea90b76ff341af83f7afb2af
Donna Dionne <donnadionne@google.com>
2021-02-20 23:04:26 -0800

    Attempting a new cmake file: header-only

--- cmake/xxhash.cmake
+++ cmake/xxhash.cmake
@@ -16,38 +16,5 @@
 # transitively so `_gRPC_XXHASH_INCLUDE_DIR` should be set for gRPC
 # to find header files.
 
-if(gRPC_XXHASH_PROVIDER STREQUAL "module")
-	if(NOT XXHASH_ROOT_DIR)
-		set(XXHASH_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/xxhash/cmake_unofficial)
-  endif()
-  if(EXISTS "${XXHASH_ROOT_DIR}/CMakeLists.txt")
-    # Explicitly disable BUILD_TESTING to avoid xxhash's CMakeLists.txt triggering https://github.com/grpc/grpc/issues/23586
-    option(BUILD_TESTING "xxhash.cmake explicitly disabled CTest's BUILD_TESTING option." OFF)
-
-    include_directories("${XXHASH_ROOT_DIR}")
-    add_subdirectory(${XXHASH_ROOT_DIR} third_party/xxhash)
-
-    if(TARGET xxhash)
-	    set(_gRPC_XXHASH_LIBRARIES xxhash)
-	    set(_gRPC_XXHASH_INCLUDE_DIR "${XXHASH_ROOT_DIR}" "${CMAKE_CURRENT_BINARY_DIR}/third_party/xxhash")
-      if(gRPC_INSTALL AND _gRPC_INSTALL_SUPPORTED_FROM_MODULE)
-        install(TARGETS xxhash EXPORT gRPCTargets
-          RUNTIME DESTINATION ${gRPC_INSTALL_BINDIR}
-          LIBRARY DESTINATION ${gRPC_INSTALL_LIBDIR}
-          ARCHIVE DESTINATION ${gRPC_INSTALL_LIBDIR})
-      endif()
-    endif()
-  else()
-	  message(WARNING "gRPC_XXHASH_PROVIDER is \"module\" but XXHASH_ROOT_DIR(${XXHASH_ROOT_DIR}) is wrong")
-  endif()
-  if(gRPC_INSTALL AND NOT _gRPC_INSTALL_SUPPORTED_FROM_MODULE)
-	  message(WARNING "gRPC_INSTALL will be forced to FALSE because gRPC_XXHASH_PROVIDER is \"module\"  and CMake version (${CMAKE_VERSION}) is less than 3.13.")
-    set(gRPC_INSTALL FALSE)
-  endif()
-elseif(gRPC_XXHASH_PROVIDER STREQUAL "package")
-  find_package(xxhash REQUIRED)
-  if(TARGET xxhash)
-	  set(_gRPC_XXHASH_LIBRARIES xxhash)
-  endif()
-  set(_gRPC_FIND_XXHASH "if(NOT xxhash_FOUND)\n  find_package(xxhash)\nendif()")
-endif()
+set(_gRPC_XXHASH_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third_party/xxhash")
+set(_gRPC_XXHASH_LIBRARIES xxhash)
--- src/core/ext/filters/client_channel/resolver/xds/xds_resolver.cc
+++ src/core/ext/filters/client_channel/resolver/xds/xds_resolver.cc
@@ -22,6 +22,7 @@
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
 #include "re2/re2.h"
+#define XXH_INLINE_ALL
 #include "xxhash.h"
 
 #include "src/core/ext/filters/client_channel/config_selector.h"
