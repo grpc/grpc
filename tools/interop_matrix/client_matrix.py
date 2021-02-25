476845a98364d472a79d00e4744cbbd077644614
Menghan Li <menghanl@google.com>
2021-02-24 16:56:20 -0800

    Add v1.36.0 release of grpc-go to interop matrix

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -182,6 +182,7 @@ LANG_RELEASE_MATRIX = {
              ReleaseInfo(runtimes=['go1.11'], testcases_file='go__v1.20.0')),
             ('v1.34.0', ReleaseInfo(runtimes=['go1.11'])),
             ('v1.35.0', ReleaseInfo(runtimes=['go1.11'])),
+            ('v1.36.0', ReleaseInfo(runtimes=['go1.11'])),
         ]),
     'java':
         OrderedDict([
