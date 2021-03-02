39cc797025f44944dd060dabbda2dfaa42765338
Lidi Zheng <lidiz@google.com>
2021-03-02 11:25:01 -0800

    [xDS Proto] Modernize buildgen scripts (#25512)
    * [xDS Proto] Modernize buildgen scripts
    
    * This PR only covers some buildgen scripts that will be used by xDS
    proto
    * This PR also improves the debuggability of some scripts
    * Merge with master
    
    * Adopt reviewer's advices

--- tools/buildgen/_utils.py
+++ tools/buildgen/_utils.py
@@ -1,4 +1,5 @@
-# Copyright 2015 gRPC authors.
+#!/usr/bin/env python3
+# Copyright 2020 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,18 +12,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Allows dot-accessible dictionaries."""
+"""Utility functions for build file generation scripts."""
+
+import os
+import sys
+import types
+import importlib.util
+from typing import Any, Union, Mapping, List
+
+
+def import_python_module(path: str) -> types.ModuleType:
+    """Imports the Python file at the given path, returns a module object."""
+    module_name = os.path.basename(path).replace('.py', '')
+    spec = importlib.util.spec_from_file_location(module_name, path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
 
 
 class Bunch(dict):
+    """Allows dot-accessible dictionaries."""
 
-    def __init__(self, d):
+    def __init__(self, d: Mapping):
         dict.__init__(self, d)
         self.__dict__.update(d)
 
 
-# Converts any kind of variable to a Bunch
-def to_bunch(var):
+def to_bunch(var: Any) -> Any:
+    """Converts any kind of variable to a Bunch."""
     if isinstance(var, list):
         return [to_bunch(i) for i in var]
     if isinstance(var, dict):
@@ -36,12 +54,12 @@ def to_bunch(var):
         return var
 
 
-# Merges JSON 'add' into JSON 'dst'
-def merge_json(dst, add):
+def merge_json(dst: Union[Mapping, List], add: Union[Mapping, List]) -> None:
+    """Merges JSON objects recursively."""
     if isinstance(dst, dict) and isinstance(add, dict):
         for k, v in add.items():
             if k in dst:
-                if k == '#':
+                if k.startswith('#'):
                     continue
                 merge_json(dst[k], v)
             else:
@@ -49,6 +67,6 @@ def merge_json(dst, add):
     elif isinstance(dst, list) and isinstance(add, list):
         dst.extend(add)
     else:
-        raise Exception(
+        raise TypeError(
             'Tried to merge incompatible objects %s %s\n\n%r\n\n%r' %
             (type(dst).__name__, type(add).__name__, dst, add))
