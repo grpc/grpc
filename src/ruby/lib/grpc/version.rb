93b308fb564a7703cfc92092fbc1da1d7514a833
Lidi Zheng <lidiz@google.com>
2021-03-01 16:25:00 -0800

    Bump version to 1.36.1 (#25574)

--- src/ruby/lib/grpc/version.rb
+++ src/ruby/lib/grpc/version.rb
@@ -14,5 +14,5 @@
 
 # GRPC contains the General RPC module.
 module GRPC
-  VERSION = '1.36.0'
+  VERSION = '1.36.1'
 end
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.36.0'
+    VERSION = '1.36.1'
   end
 end
