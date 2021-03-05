b8ba030cfd9458e64759538cf59005c277dd005e
ZhenLian <zhenlian@google.com>
2021-03-04 23:00:56 -0800

    custom verification proposed C core changes

--- test/core/util/tls_utils.cc
+++ test/core/util/tls_utils.cc
@@ -16,6 +16,8 @@
 
 #include "test/core/util/tls_utils.h"
 
+#include <grpc/support/log.h>
+
 #include "src/core/lib/gpr/tmpfile.h"
 #include "src/core/lib/iomgr/load_file.h"
 #include "src/core/lib/slice/slice_internal.h"
@@ -69,6 +71,98 @@ std::string GetFileContents(const char* path) {
   std::string credential = std::string(StringViewFromSlice(slice));
   grpc_slice_unref(slice);
   return credential;
+}
+
+SyncExternalVerifier::SyncExternalVerifier(bool is_good) {
+  auto* user_data = new UserData();
+  user_data->self = this;
+  user_data->is_good = is_good;
+  base_.user_data = user_data;
+  base_.verify = Verify;
+  base_.cancel = Cancel;
+  base_.destruct = Destruct;
+}
+
+int SyncExternalVerifier::Verify(
+    void* user_data, grpc_tls_custom_verification_check_request* request,
+    grpc_tls_on_custom_verification_check_done_cb callback,
+    void* callback_arg) {
+  auto* data = static_cast<UserData*>(user_data);
+  if (data->is_good) {
+    request->status = GRPC_STATUS_OK;
+    return true;  // Synchronous call
+  }
+  request->status = GRPC_STATUS_UNAUTHENTICATED;
+  request->error_details = gpr_strdup("SyncExternalVerifierBadVerify failed");
+  return true;  // Synchronous call
+}
+
+void SyncExternalVerifier::Destruct(void* user_data) {
+  auto* data = static_cast<UserData*>(user_data);
+  delete data->self;
+  delete data;
+}
+
+AsyncExternalVerifier::AsyncExternalVerifier(bool is_good,
+                                             gpr_event* event_ptr) {
+  auto* user_data = new UserData();
+  user_data->self = this;
+  user_data->is_good = is_good;
+  user_data->event_ptr = event_ptr;
+  base_.user_data = user_data;
+  base_.verify = Verify;
+  base_.cancel = Cancel;
+  base_.destruct = Destruct;
+}
+
+int AsyncExternalVerifier::Verify(
+    void* user_data, grpc_tls_custom_verification_check_request* request,
+    grpc_tls_on_custom_verification_check_done_cb callback,
+    void* callback_arg) {
+  auto* data = static_cast<UserData*>(user_data);
+  // Creates the thread args we use when creating the thread.
+  ThreadArgs* thread_args = new ThreadArgs();
+  thread_args->request = request;
+  thread_args->callback = callback;
+  thread_args->callback_arg = callback_arg;
+  thread_args->event_ptr = data->event_ptr;
+  thread_args->is_good = data->is_good;
+  data->thread = new grpc_core::Thread("AsyncExternalVerifierVerify",
+                                       &AsyncExternalVerifierVerifyCb,
+                                       static_cast<void*>(thread_args), nullptr,
+                                       Thread::Options().set_joinable(false));
+  data->thread->Start();
+  return false;  // Asynchronous call
+}
+
+void AsyncExternalVerifier::Destruct(void* user_data) {
+  auto* data = static_cast<UserData*>(user_data);
+  if (data->thread != nullptr) {
+    delete data->thread;
+  }
+  if (data->self != nullptr) {
+    delete data->self;
+  }
+  delete data;
+}
+
+void AsyncExternalVerifier::AsyncExternalVerifierVerifyCb(void* args) {
+  ThreadArgs* thread_args = static_cast<ThreadArgs*>(args);
+  grpc_tls_custom_verification_check_request* request = thread_args->request;
+  grpc_tls_on_custom_verification_check_done_cb callback =
+      thread_args->callback;
+  void* callback_arg = thread_args->callback_arg;
+  if (thread_args->is_good) {
+    callback(request, callback_arg, GRPC_STATUS_OK, "");
+  } else {
+    callback(request, callback_arg, GRPC_STATUS_UNAUTHENTICATED,
+             "AsyncExternalVerifierBadVerify failed");
+  }
+  // Now we can notify the main testing thread that the thread object is set.
+  if (thread_args->event_ptr != nullptr) {
+    gpr_event_set(thread_args->event_ptr, reinterpret_cast<void*>(1));
+  }
+  delete thread_args;
 }
 
 }  // namespace testing
--- test/core/util/tls_utils.h
+++ test/core/util/tls_utils.h
@@ -14,6 +14,7 @@
 // limitations under the License.
 //
 
+#include "src/core/lib/gprpp/thd.h"
 #include "src/core/lib/security/security_connector/ssl_utils.h"
 
 namespace grpc_core {
@@ -42,6 +43,78 @@ PemKeyCertPairList MakeCertKeyPairs(absl::string_view private_key,
                                     absl::string_view certs);
 
 std::string GetFileContents(const char* path);
+
+class SyncExternalVerifier {
+ public:
+  SyncExternalVerifier(bool is_good);
+
+  grpc_tls_certificate_verifier_external* base() { return &base_; }
+
+ private:
+  struct UserData {
+    SyncExternalVerifier* self = nullptr;
+    bool is_good = false;
+  };
+
+  static int Verify(void* user_data,
+                    grpc_tls_custom_verification_check_request* request,
+                    grpc_tls_on_custom_verification_check_done_cb callback,
+                    void* callback_arg);
+
+  static void Cancel(void* user_data,
+                     grpc_tls_custom_verification_check_request* request) {}
+
+  static void Destruct(void* user_data);
+
+  grpc_tls_certificate_verifier_external base_;
+};
+
+class AsyncExternalVerifier {
+ public:
+  // The constructor of an async verifier that can be shared by multiple tests.
+  //
+  // is_good: if we want the check of the async verifier to return good result
+  // event_ptr: an event used to notify the main thread that the async callback
+  // is completed. Not setting this field will cause many threading problems,
+  // e.g. calling the verifier's Destruct() function while the async callback
+  // started by verifier's Verify() function is still running. For tests that
+  // don't need to be notified(e.g. in case when the check_peer() of the
+  // security connector is not invoked), pass nullptr here.
+  AsyncExternalVerifier(bool is_good, gpr_event* event_ptr);
+
+  grpc_tls_certificate_verifier_external* base() { return &base_; }
+
+ private:
+  struct UserData {
+    AsyncExternalVerifier* self = nullptr;
+    grpc_core::Thread* thread = nullptr;
+    bool is_good = false;
+    gpr_event* event_ptr = nullptr;
+  };
+  // This is the arg we will pass in when creating the thread, and retrieve it
+  // later in the thread callback.
+  struct ThreadArgs {
+    grpc_tls_custom_verification_check_request* request = nullptr;
+    grpc_tls_on_custom_verification_check_done_cb callback;
+    void* callback_arg = nullptr;
+    bool is_good = false;
+    gpr_event* event_ptr = nullptr;
+  };
+
+  static int Verify(void* user_data,
+                    grpc_tls_custom_verification_check_request* request,
+                    grpc_tls_on_custom_verification_check_done_cb callback,
+                    void* callback_arg);
+
+  static void Cancel(void* user_data,
+                     grpc_tls_custom_verification_check_request* request) {}
+
+  static void Destruct(void* user_data);
+
+  static void AsyncExternalVerifierVerifyCb(void* args);
+
+  grpc_tls_certificate_verifier_external base_;
+};
 
 }  // namespace testing
 
