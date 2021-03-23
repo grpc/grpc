6e72b5b405e5ed23f1d6bf191aea10300fdd1879
Ryan Kohler <ryankohler@google.com>
2021-03-23 11:26:15 -0700

    changes requested by roth@

--- src/core/lib/security/credentials/external/aws_external_account_credentials.cc
+++ src/core/lib/security/credentials/external/aws_external_account_credentials.cc
@@ -30,7 +30,7 @@ namespace {
 const char* kExpectedEnvironmentId = "aws1";
 
 const char* kRegionEnvVar = "AWS_REGION";
-const char* kDefaultRegionEnvVar = "AWS_REGION";
+const char* kDefaultRegionEnvVar = "AWS_DEFAULT_REGION";
 const char* kAccessKeyIdEnvVar = "AWS_ACCESS_KEY_ID";
 const char* kSecretAccessKeyEnvVar = "AWS_SECRET_ACCESS_KEY";
 const char* kSessionTokenEnvVar = "AWS_SESSION_TOKEN";
@@ -142,8 +142,7 @@ void AwsExternalAccountCredentials::RetrieveSubjectToken(
 void AwsExternalAccountCredentials::RetrieveRegion() {
   UniquePtr<char> region_from_env(gpr_getenv(kRegionEnvVar));
   if (region_from_env == nullptr) {
-    UniquePtr<char> default_region_from_env(gpr_getenv(kDefaultRegionEnvVar));
-    region_from_env = std::move(default_region_from_env);
+    region_from_env = UniquePtr<char>(gpr_getenv(kDefaultRegionEnvVar));
   }
   if (region_from_env != nullptr) {
     region_ = std::string(region_from_env.get());
