54b7ae6351a86e98048a7a61ec459b2eb6361bce
Wanlin Du <wanlindu@google.com>
2021-03-30 20:11:17 -0700

    Run one example load test to explore process
    This commit altering Kokoro build scripts to understand the
    necessary environment required to run load test through
    gRPC OSS benchamrking infrastructure.

--- tools/internal_ci/linux/grpc_e2e_performance_v2.sh
+++ tools/internal_ci/linux/grpc_e2e_performance_v2.sh
@@ -19,5 +19,28 @@ cd $(dirname $0)/../../..
 
 source tools/internal_ci/helper_scripts/prepare_build_linux_rc
 
+# This is to insure we can push and pull images from gcr.io, we do not
+# necessarily need it to run load tests, but would need it when we employ
+# pre-built images in the optimization.
+gcloud auth configure-docker
+
+# Connect to benchmarks-prod cluster
+gcloud config set project grpc-testing
+gcloud container clusters get-credentials benchmarks-prod \
+    --zone us-central1-b --project grpc-testing
+
+# This step subject to change, just easier to get one file to run a single test
+wget https://raw.githubusercontent.com/grpc/test-infra/master/config/samples/go_example_loadtest.yaml
+
+# The original version of the client is a bit old, update to a newer version.
+kubectl version --client
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+sudo mv kubectl $(which kubectl)
+kubectl version --client
+
+kubectl apply -f go_example_loadtest.yaml
+
 echo "TODO: Add gRPC OSS Benchmarks here..."
 
