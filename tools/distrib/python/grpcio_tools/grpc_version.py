44c40ac23023b7b3dd82744372c06817cc203898
Richard Belleville <rbellevi@google.com>
2021-04-06 15:48:56 -0700

    Bump to 1.37.0 for Final Release (#25891)
    * Bump version to 1.37.0
    
    * Regenerate projects

--- tools/distrib/python/grpcio_tools/grpc_version.py
+++ tools/distrib/python/grpcio_tools/grpc_version.py
@@ -14,5 +14,5 @@
 
 # AUTO-GENERATED FROM `$REPO_ROOT/templates/tools/distrib/python/grpcio_tools/grpc_version.py.template`!!!
 
-VERSION = '1.37.0rc1'
+VERSION = '1.37.0'
 PROTOBUF_VERSION = '3.15.2'
