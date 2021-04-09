7e117ff9541ff2ff4627d93a9d2f6579bf84203a
ZhenLian <zhenlian@google.com>
2021-04-08 23:16:38 -0700

    TlsCredentials: add cancel_check_peer() for the security connector

--- src/core/lib/security/security_connector/tls/tls_security_connector.h
+++ src/core/lib/security/security_connector/tls/tls_security_connector.h
@@ -60,6 +60,11 @@ class TlsChannelSecurityConnector final
                   grpc_core::RefCountedPtr<grpc_auth_context>* auth_context,
                   grpc_closure* on_peer_checked) override;
 
+  void cancel_check_peer(grpc_closure* on_peer_checked,
+                         grpc_error* error) override {
+    // TODO(ZhenLian): call verifier->cancel() once the verifier is ready.
+  }
+
   int cmp(const grpc_security_connector* other_sc) const override;
 
   bool check_call_host(absl::string_view host, grpc_auth_context* auth_context,
@@ -160,6 +165,11 @@ class TlsServerSecurityConnector final : public grpc_server_security_connector {
   void check_peer(tsi_peer peer, grpc_endpoint* ep,
                   grpc_core::RefCountedPtr<grpc_auth_context>* auth_context,
                   grpc_closure* on_peer_checked) override;
+
+  void cancel_check_peer(grpc_closure* on_peer_checked,
+                         grpc_error* error) override {
+    // TODO(ZhenLian): call verifier->cancel() once the verifier is ready.
+  }
 
   int cmp(const grpc_security_connector* other) const override;
 
