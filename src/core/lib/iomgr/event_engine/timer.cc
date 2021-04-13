9a2d5622673effa602c3ce83187193f4ac3b1820
AJ Heller <hork@google.com>
2021-04-13 13:24:47 -0700

    Add EventEngine-iomgr timer stubs

---
+++ src/core/lib/iomgr/event_engine/timer.cc
@@ -0,0 +1,35 @@
+// Copyright 2021 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#if defined(GRPC_EVENT_ENGINE_TEST)
+
+#include <grpc/support/port_platform.h>
+
+#include <grpc/event_engine/event_engine.h>
+
+#include "src/core/lib/iomgr/timer.h"
+
+void timer_init(grpc_timer* timer, grpc_millis, grpc_closure* closure) {}
+void timer_cancel(grpc_timer* timer) {}
+
+/* Internal API */
+grpc_timer_check_result timer_check(grpc_millis* next) {}
+void timer_list_init() {}
+void timer_list_shutdown(void) {}
+void timer_consume_kick(void) {}
+
+grpc_timer_vtable grpc_event_engine_timer_vtable = {
+    timer_init,      timer_cancel,        timer_check,
+    timer_list_init, timer_list_shutdown, timer_consume_kick} grpc_timer_vtable;
+
+#endif  // GRPC_EVENT_ENGINE_TEST
