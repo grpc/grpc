eddcc066905e62986e801740487e0737b7e78f36
Mark D. Roth <roth@google.com>
2021-04-15 10:59:31 -0700

    fix build

--- src/core/lib/channel/connected_channel.cc
+++ src/core/lib/channel/connected_channel.cc
@@ -139,7 +139,7 @@ static void connected_channel_start_transport_stream_op_batch(
   GRPC_CALL_COMBINER_STOP(calld->call_combiner, "passed batch to transport");
 }
 
-static void connected_channel_pre_cancel_call(grpc_call_element* elem,
+static void connected_channel_pre_cancel_call(grpc_call_element* /*elem*/,
                                               grpc_error* error) {
   GRPC_ERROR_UNREF(error);
 }
