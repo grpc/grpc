1e777d8f1d260894d8ca15d8167a5a042147b0ca
Richard Belleville <rbellevi@google.com>
2021-04-16 17:50:33 +0000

    Fix up docker images

--- src/python/grpcio_tests/tests_py3_only/interop/build_client_image.sh
+++ src/python/grpcio_tests/tests_py3_only/interop/build_client_image.sh
@@ -5,11 +5,11 @@ set -x
 VERSION=$(git rev-parse HEAD)
 
 PROJECT=grpc-testing
-TAG=gcr.io/${PROJECT}/python_xds_interop_server:$VERSION
+TAG=gcr.io/${PROJECT}/python_xds_interop_client:$VERSION
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../../.."
 
 docker build \
     -t ${TAG} \
-    -f src/python/grpcio_tests/tests_py3_only/interop/Dockerfile.server \
+    -f src/python/grpcio_tests/tests_py3_only/interop/Dockerfile.client \
     .
