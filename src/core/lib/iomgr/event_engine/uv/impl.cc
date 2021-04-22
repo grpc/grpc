5275d80b4521d6ead42f1fbf814a718de26fdc08
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2021-04-22 16:49:08 -0700

    Using proper Status class.

--- src/core/lib/iomgr/event_engine/uv/impl.cc
+++ src/core/lib/iomgr/event_engine/uv/impl.cc
@@ -2,83 +2,6 @@
 #include <functional>
 #include <future>
 
-// ow
-namespace util {
-namespace error {
-// These values must match error codes defined in google/rpc/code.proto.
-enum Code {
-  OK = 0,
-  CANCELLED = 1,
-  UNKNOWN = 2,
-  INVALID_ARGUMENT = 3,
-  DEADLINE_EXCEEDED = 4,
-  NOT_FOUND = 5,
-  ALREADY_EXISTS = 6,
-  PERMISSION_DENIED = 7,
-  UNAUTHENTICATED = 16,
-  RESOURCE_EXHAUSTED = 8,
-  FAILED_PRECONDITION = 9,
-  ABORTED = 10,
-  OUT_OF_RANGE = 11,
-  UNIMPLEMENTED = 12,
-  INTERNAL = 13,
-  UNAVAILABLE = 14,
-  DATA_LOSS = 15,
-};
-}  // namespace error
-
-class Status {
- public:
-  // Creates a "successful" status.
-  Status();
-
-  // Create a status in the canonical error space with the specified
-  // code, and error message.  If "code == 0", error_message is
-  // ignored and a Status object identical to Status::OK is
-  // constructed.
-  Status(error::Code error_code, std::string error_message);
-  Status(const Status&);
-  Status& operator=(const Status& x);
-  ~Status() {}
-
-  // Some pre-defined Status objects
-  static const Status OK;             // Identical to 0-arg constructor
-  static const Status CANCELLED;
-  static const Status UNKNOWN;
-
-  // Accessor
-  bool ok() const {
-    return error_code_ == error::OK;
-  }
-  int error_code() const {
-    return error_code_;
-  }
-  error::Code code() const {
-    return error_code_;
-  }
-  std::string error_message() const {
-    return error_message_;
-  }
-  std::string message() const {
-    return error_message_;
-  }
-
-  bool operator==(const Status& x) const;
-  bool operator!=(const Status& x) const {
-    return !operator==(x);
-  }
-
-  // Return a combination of the error code name and message.
-  std::string ToString() const;
-
- private:
-  error::Code error_code_;
-  std::string error_message_;
-};
-
-}  // namespace util
-
-
 #include "grpc/event_engine/event_engine.h"
 #include "src/core/lib/gprpp/atomic.h"
 #include "src/core/lib/gprpp/mpscq.h"
@@ -286,12 +209,16 @@ absl::StatusOr<int> uvListener::Bind(
     int r = uv_tcp_bind(&uvTCP_->tcp_, addr.address(), 0 /* flags */);
     switch (r) {
       case UV_EINVAL:
-        p.set_value(-1 /*util::Status(util::error::INVALID_ARGUMENT, "uv_tcp_bind said we passed an invalid argument to it")*/);
+        p.set_value(absl::Status(
+            absl::StatusCode::kInvalidArgument,
+            "uv_tcp_bind said we passed an invalid argument to it"));
         return;
       case 0:
         break;
       default:
-        p.set_value(-1 /*util::Status(util::error::UNKNOWN, "uv_tcp_bind returned an error code we don't know about")*/);
+        p.set_value(absl::Status(
+            absl::StatusCode::kUnknown,
+            "uv_tcp_bind returned an error code we don't know about"));
         return;
     }
     sockaddr_storage boundAddr;
@@ -300,12 +227,16 @@ absl::StatusOr<int> uvListener::Bind(
                            reinterpret_cast<sockaddr*>(&boundAddr), &addrLen);
     switch (r) {
       case UV_EINVAL:
-        p.set_value(-1 /*util::Status(util::error::INVALID_ARGUMENT, "uv_tcp_getsockname said we passed an invalid argument to it")*/);
+        p.set_value(absl::Status(
+            absl::StatusCode::kInvalidArgument,
+            "uv_tcp_getsockname said we passed an invalid argument to it"));
         return;
       case 0:
         break;
       default:
-        p.set_value(-1 /*util::Status(util::error::UNKNOWN, "uv_tcp_getsockname returned an error code we don't know about")*/);
+        p.set_value(absl::Status(
+            absl::StatusCode::kUnknown,
+            "uv_tcp_getsockname returned an error code we don't know about"));
         return;
     }
     switch (boundAddr.ss_family) {
@@ -320,7 +251,9 @@ absl::StatusOr<int> uvListener::Bind(
         break;
       }
       default:
-        p.set_value(-1 /*util::Status(util::error::INVALID_ARGUMENT, "returned socket address in :Bind is neither IPv4 nor IPv6")*/);
+        p.set_value(absl::Status(
+            absl::StatusCode::kInvalidArgument,
+            "returned socket address in :Bind is neither IPv4 nor IPv6"));
         break;
     }
   });
