f2bd3920ac0bd0b3066e490b2e2daa6d7b15beee
Alexander Polcyn <apolcyn@google.com>
2021-04-23 14:14:30 -0700

    add test which displays an example of the issue

--- test/core/iomgr/timer_init_busy_loop_test.cc
+++ test/core/iomgr/timer_init_busy_loop_test.cc
@@ -33,24 +33,27 @@
 #include <grpc/support/string_util.h>
 #include <grpc/support/time.h>
 
-#include "src/core/lib/iomgr/exec_ctx.h"
 #include "src/core/lib/iomgr/error.h"
-#include "src/core/lib/iomgr/work_serializer.h"
+#include "src/core/lib/iomgr/exec_ctx.h"
 #include "src/core/lib/iomgr/timer.h"
+#include "src/core/lib/iomgr/work_serializer.h"
 
 #include "test/core/util/test_config.h"
 
-
 namespace {
+
+gpr_timespec g_test_start_time;
 
 class WorkSerializedTimerLoop {
  public:
   WorkSerializedTimerLoop() {
-    GRPC_CLOSURE_INIT(&on_schedule_timer_, OnScheduleTimer, this, grpc_schedule_on_exec_ctx);
+    GRPC_CLOSURE_INIT(&on_schedule_timer_, OnScheduleTimer, this,
+                      grpc_schedule_on_exec_ctx);
   }
 
   void Start() {
-    grpc_core::ExecCtx::Run(DEBUG_LOCATION, &on_schedule_timer_, GRPC_ERROR_NONE);
+    grpc_core::ExecCtx::Run(DEBUG_LOCATION, &on_schedule_timer_,
+                            GRPC_ERROR_NONE);
   }
 
   void Shutdown() {
@@ -63,43 +66,90 @@ class WorkSerializedTimerLoop {
   static void OnScheduleTimer(void* arg, grpc_error* error) {
     WorkSerializedTimerLoop* self = static_cast<WorkSerializedTimerLoop*>(arg);
     GRPC_ERROR_REF(error);
-    self->work_serializer_->Run([self, error] {
-      grpc_core::MutexLock lock(&self->mu_);
-      if (error != GRPC_ERROR_NONE || self->shutdown_) {
-        GRPC_ERROR_UNREF(error);
-        gpr_log(GPR_INFO, "test timer loop quitting, shutdown_:%d, error: %s", self->shutdown_, grpc_error_string(error));
-        delete self;
-        return;
-      }
-      // Note that we explicitly do not call InvalidateNow(). This test is meant to
-      // display and example of real scenarios in the code base that do not call InvalidateNow
-      // when scheduling periodic timer events.
-      grpc_millis timeout = grpc_core::ExecCtx::Get()->Now() + 500;
-      grpc_timer_init(&self->schedule_timer_alarm_, timeout, &self->on_schedule_timer_);
-      gpr_log(GPR_INFO, "just scheduled a timer to go off in 500 ms. ExecCtx::Get()->Now(): %ld", grpc_core::ExecCtx::Get()->Now());
-      gpr_sleep_until(gpr_time_add(gpr_now(GPR_CLOCK_MONOTONIC), gpr_time_from_millis(100, GPR_TIMESPAN)));
-      gpr_log(GPR_INFO, "just woke up from 1 ms sleep");
-      GRPC_ERROR_UNREF(error);
-    }, DEBUG_LOCATION);
-    gpr_log(GPR_INFO, "just appended timer_init to work serializer");
+    self->work_serializer_->Run(
+        [self, error] {
+          grpc_core::MutexLock lock(&self->mu_);
+          // abort if this timer loop has been spinning for 5 minutes
+          if (gpr_time_cmp(
+                  gpr_now(GPR_CLOCK_MONOTONIC),
+                  gpr_time_add(g_test_start_time,
+                               gpr_time_from_seconds(30, GPR_TIMESPAN))) > 0) {
+            gpr_log(GPR_ERROR,
+                    "test timer loop has been spinning for more than 5 "
+                    "minutes, this indicates a busy-loop bug");
+            GPR_ASSERT(0);
+          }
+          if (error != GRPC_ERROR_NONE || self->shutdown_) {
+            GRPC_ERROR_UNREF(error);
+            gpr_log(GPR_INFO,
+                    "test timer loop quitting, shutdown_:%d, error: %s",
+                    self->shutdown_, grpc_error_string(error));
+            delete self;
+            return;
+          }
+          // Schedule a timer to go off in 500 milliseconds. Note that this is
+          // explicitly less than the 1 second of delay that was inserted at the
+          // beginning of the test. The goal is to try to repro a bug related to
+          // time caching where such delay will cause the 500ms timer to fire
+          // immediately.
+          grpc_millis timeout = grpc_core::ExecCtx::Get()->Now() + 500;
+          // Note that we explicitly do not call ExecCtx::InvalidateNow(). This
+          // test is meant to display an example of real scenarios in the code
+          // base that do not call InvalidateNow when scheduling such periodic
+          // timer events.
+          gpr_log(GPR_INFO,
+                  "scheduled a timer to go off in 500 ms. "
+                  "ExecCtx::Get()->Now(): %ld",
+                  grpc_core::ExecCtx::Get()->Now());
+          grpc_timer_init(&self->schedule_timer_alarm_, timeout,
+                          &self->on_schedule_timer_);
+          // Sleep for 1 ms to simulate some arbitrary delay that gives time for
+          // the timer thread to invoke the timer callback we just scheduled,
+          // pushing this lambda back onto the work serializer queue that we're
+          // currently holding. If things are correct, then the 500 ms timer
+          // just scheduled should very rarely fire before this sleep ends, and
+          // thus the timer thread should very rarely have enough time to do
+          // that.
+          gpr_sleep_until(gpr_time_add(gpr_now(GPR_CLOCK_MONOTONIC),
+                                       gpr_time_from_millis(1, GPR_TIMESPAN)));
+          gpr_log(
+              GPR_INFO,
+              "thread calling grpc_timer_init just woke up from 1 ms sleep");
+          GRPC_ERROR_UNREF(error);
+        },
+        DEBUG_LOCATION);
+    gpr_log(GPR_INFO,
+            "just appended timer-scheduling callback to work serializer");
   }
 
-  std::shared_ptr<grpc_core::WorkSerializer> work_serializer_ = std::make_shared<grpc_core::WorkSerializer>();
+  std::shared_ptr<grpc_core::WorkSerializer> work_serializer_ =
+      std::make_shared<grpc_core::WorkSerializer>();
   grpc_closure on_schedule_timer_;
   grpc_timer schedule_timer_alarm_;
   grpc_core::Mutex mu_;
   bool shutdown_ = false;
 };
 
-TEST(TimerInitBusyLoopTest, TestBusyLoopDoesNotHappenWithPeriodicTimerEventsOnAWorkSerializer) {
-  WorkSerializedTimerLoop* work_serialized_timer_loop = new WorkSerializedTimerLoop();
+TEST(TimerInitBusyLoopTest,
+     TestBusyLoopDoesNotHappenWithPeriodicTimerEventsOnAWorkSerializer) {
+  WorkSerializedTimerLoop* work_serialized_timer_loop =
+      new WorkSerializedTimerLoop();
   {
     grpc_core::ExecCtx exec_ctx;
-    gpr_log(GPR_INFO, "begin test after sleeping for 1 second, ExecCtd::Get()->Now(): %ld", grpc_core::ExecCtx::Get()->Now());
-    // Add some initial delay to try cause skew between this thread's view of the
-    // time and the timer manager thread's view of the time (which can happen with
-    // if the time is cached in the ExecCtx).
-    gpr_sleep_until(gpr_time_add(gpr_now(GPR_CLOCK_MONOTONIC), gpr_time_from_seconds(1, GPR_TIMESPAN)));
+    g_test_start_time = gpr_now(GPR_CLOCK_MONOTONIC);
+    // Explicitly invoke ExecCtx::Now(), in order to try to repro a bug related
+    // to caching of the ExecCtx::now_ field.
+    grpc_millis now = grpc_core::ExecCtx::Get()->Now();
+    gpr_log(GPR_INFO,
+            "begin timer loop after sleeping for 1 second, "
+            "ExecCtx::Get()->Now(): %ld",
+            now);
+    // Add some initial delay to try cause skew between this thread's view of
+    // the time and the timer manager thread's view of the time (which can
+    // happen with if the time is cached in the ExecCtx). This simulates
+    // arbitrary delay in real code which can cause such skew.
+    gpr_sleep_until(gpr_time_add(gpr_now(GPR_CLOCK_MONOTONIC),
+                                 gpr_time_from_seconds(1, GPR_TIMESPAN)));
     work_serialized_timer_loop->Start();
   }
   {
