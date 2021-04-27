8664c8334c05d322fbbdfb9e3b24601a23e9363c
Richard Belleville <rbellevi@google.com>
2021-04-27 15:01:25 -0700

    Bump to 1.37.1 (#26040)
    * Bump to 1.37.1
    
    * Regenerate projects

--- src/ruby/lib/grpc/version.rb
+++ src/ruby/lib/grpc/version.rb
@@ -14,5 +14,5 @@
 
 # GRPC contains the General RPC module.
 module GRPC
-  VERSION = '1.37.0'
+  VERSION = '1.37.1'
 end
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.37.0'
+    VERSION = '1.37.1'
   end
 end
