fe4301e8fc4fc0d38e0958efa215378c43de9d7d
Lidi Zheng <lidiz@google.com>
2021-05-05 10:51:28 -0700

    Improve documentation

--- src/python/grpcio_admin/grpc_admin/__init__.py
+++ src/python/grpcio_admin/grpc_admin/__init__.py
@@ -18,6 +18,23 @@ from grpc_channelz.v1 import channelz
 
 
 def add_admin_servicers(server):
+    """Register admin servicers to a server.
+
+    gRPC provides some predefined admin services to make debugging easier by
+    exposing gRPC's internal states. Each existing admin service is packaged as
+    a separate library, and the documentation of the predefined admin services
+    is usually scattered. It can be time consuming to get the dependency
+    management, module initialization, and library import right for each one of
+    them. 
+
+    This API provides a convenient way to create a gRPC server to expose admin
+    services. With this, any new admin services that you may add in the future
+    are automatically available via the admin interface just by upgrading your
+    gRPC version.
+
+    Args:
+        server: A gRPC server to which all admin services will be added.
+    """
     channelz.add_channelz_servicer(server)
     grpc_csds.add_csds_servicer(server)
 
