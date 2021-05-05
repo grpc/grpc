717949afddcbfdbfd910b491838f03a36332da08
Wanlin Du <wanlindu@google.com>
2021-05-04 19:15:22 -0700

    Employ prebuilt images in continuous build.
    This commits updated Kokoro build job to use prebuilt images to
    run tests. The loadtest template was generated using
    loadtest_template.py.

--- tools/internal_ci/linux/grpc_e2e_performance_v2.sh
+++ tools/internal_ci/linux/grpc_e2e_performance_v2.sh
@@ -12,7 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -ex
+
+# Trap the error instead of exit directly
+set -x
 
 # Enter the gRPC repo root
 cd $(dirname $0)/../../..
@@ -29,21 +31,48 @@ gcloud config set project grpc-testing
 gcloud container clusters get-credentials benchmarks-prod \
     --zone us-central1-b --project grpc-testing
 
+# Set up environment variables
+export PREBUILD_IMAGE_PREFIX="gcr.io/grpc-testing/e2etesting/pre_built_workers"
+export PREBUILT_IMAGE_TAG=$KOKORO_BUILD_INITIATOR-$(date '+%F-%H-%M-%S')
+export ROOT_DIRECTORY_OF_DOCKERFILES="../test-infra/containers/pre_built_workers/"
+
+# Clone test-infra repository to one upper level directory than grpc
+cd ..
+git clone --recursive https://github.com/grpc/test-infra.git
+cd grpc
+
+# Log the error and exit directly
+errorCode=$?
+if [ $errorCode -ne 0 ]; then
+  echo "an error occurred before pushing images, exit directly"
+  exit $errorCode
+fi
+
+# Build and push prebuilt images for running tests
+go run ../test-infra/tools/prepare_prebuilt_workers/prepare_prebuilt_workers.go \
+  -l cxx:master \
+  -l go:master \
+  -p $PREBUILD_IMAGE_PREFIX \
+  -t $PREBUILT_IMAGE_TAG \
+  -r $ROOT_DIRECTORY_OF_DOCKERFILES
+
 # This is subject to change. Runs a single test and does not wait for the
 # result.
 tools/run_tests/performance/loadtest_config.py -l c++ -l go \
     -t ./tools/run_tests/performance/templates/loadtest_template_basic_all_languages.yaml \
     -s client_pool=workers-8core -s server_pool=workers-8core \
     -s big_query_table=e2e_benchmarks.experimental_results \
-    -s timeout_seconds=900 --prefix="kokoro-test" -u "$(date +%Y%m%d%H%M%S)" \
+    -s timeout_seconds=900 --prefix="kokoro-test" -u $PREBUILT_IMAGE_TAG \
+    -s prebuilt_image_prefix=$PREBUILD_IMAGE_PREFIX \
+    -s prebuilt_image_tag=$PREBUILT_IMAGE_TAG \
     -r '(go_generic_sync_streaming_ping_pong_secure|go_protobuf_sync_unary_ping_pong_secure|cpp_protobuf_async_streaming_qps_unconstrained_secure)$' \
-    -o ./loadtest.yaml
+    -o ./loadtest_with_prebuilt_images.yaml
 
-# Dump the contents of the loadtest.yaml (since loadtest_config.py doesn't
-# list the scenarios that will be run).
-cat ./loadtest.yaml
+# Dump the contents of the loadtest_with_prebuilt_images.yaml (since
+# loadtest_config.py doesn't list the scenarios that will be run).
+cat ./loadtest_with_prebuilt_images.yaml
 
-# The original version of the client is a bit old, update to the latest release
+# The original version of the client is old, update to the latest release
 # version v1.21.0.
 kubectl version --client
 curl -sSL -O https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl
@@ -52,4 +81,15 @@ chmod +x kubectl
 sudo mv kubectl $(which kubectl)
 kubectl version --client
 
-kubectl apply -f ./loadtest.yaml
+kubectl apply -f ./loadtest_with_prebuilt_images.yaml
+
+# Log the error and proceed to delete all pushed images
+errorCode=$?
+if [ $errorCode -ne 0 ]; then
+  echo "an error occurred while running the test, continue to delete all pushed images"
+fi
+
+# Delete all images built for this build
+go run ../test-infra/tools/delete_prebuilt_workers/delete_prebuilt_workers.go \
+  -p $PREBUILD_IMAGE_PREFIX \
+  -t $PREBUILT_IMAGE_TAG
