00f101b35db3ef417d466d21e96359293ba8cb3c
Alexander Midlash <amidlash@google.com>
2021-05-10 18:14:30 +0000

    pr cleanup: use builtin bazel wrapper

--- tools/internal_ci/linux/grpc_aws_experiment_remote.sh
+++ tools/internal_ci/linux/grpc_aws_experiment_remote.sh
@@ -5,17 +5,9 @@ sudo apt update
 sudo apt install -y build-essential autoconf libtool pkg-config cmake python python-pip clang
 sudo pip install six
 
-# setup bazel
-BAZEL=bazel-4.0.0-linux-arm64
-wget https://github.com/bazelbuild/bazel/releases/download/4.0.0/$BAZEL
-wget https://github.com/bazelbuild/bazel/releases/download/4.0.0/${BAZEL}.sha256
-chmod +x ${BAZEL}
-sha256sum -c ${BAZEL}.sha256
-
-# clone and built repo
 cd grpc
 # build with bazel
 export CXX=clang++
 export CC=clang
-../${BAZEL} test --config=dbg //test/...
+tools/bazel test --config=dbg //test/...
 
