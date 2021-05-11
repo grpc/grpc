39f2db1cfba92efe30dcd135230d0d3a335d0214
Jan Tattermusch <jtattermusch@google.com>
2021-05-11 14:20:54 +0200

    less verbose unzip

--- tools/internal_ci/linux/grpc_aws_experiment.sh
+++ tools/internal_ci/linux/grpc_aws_experiment.sh
@@ -12,7 +12,7 @@ AWS_CREDENTIALS=${KOKORO_KEYSTORE_DIR}/73836_grpc_aws_ec2_credentials
 
 ## Setup aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 sudo ./aws/install 
 aws --version
 
