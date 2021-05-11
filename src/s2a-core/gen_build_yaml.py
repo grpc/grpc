da5bcec5c089d87a6a2a9b146ded40c9987dec50
Matthew Stevenson <mattstev@google.com>
2021-05-11 11:44:04 -0700

    Another update to S2A Core repo.

--- src/s2a-core/gen_build_yaml.py
+++ src/s2a-core/gen_build_yaml.py
@@ -1,12 +1,12 @@
-#!/ usr / bin / env python2 .7
+#!/usr/bin/env python2.7
 
-# Copyright 2021 gRPC authors.
+# Copyright 2015 gRPC authors.
 #
-# Licensed under the Apache License, Version 2.0(the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#   http:  // www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
