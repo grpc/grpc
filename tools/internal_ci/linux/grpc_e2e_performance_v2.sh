3847bb6a3b792a35ec584f9ea1ad0d921e80399c
Wanlin Du <wanlindu@google.com>
2021-05-10 17:38:52 -0700

    Address comments

--- tools/internal_ci/linux/grpc_e2e_performance_v2.sh
+++ tools/internal_ci/linux/grpc_e2e_performance_v2.sh
@@ -12,15 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# If there is a error within the function, will exit directly
-deleteImages() {
-  echo "an error has occurred after pushing the images, deleting images"
-  go run ../test-infra/tools/delete_prebuilt_workers/delete_prebuilt_workers.go \
-  -p $PREBUILT_IMAGE_PREFIX \
-  -t $UNIQUE_IDENTIFIER
-}
-
 set -ex
 
 # Enter the gRPC repo root
@@ -40,30 +31,35 @@ gcloud container clusters get-credentials benchmarks-prod \
 
 # Set up environment variables
 PREBUILT_IMAGE_PREFIX="gcr.io/grpc-testing/e2etesting/pre_built_workers/"${KOKORO_BUILD_INITIATOR}
-UNIQUE_IDENTIFIER=$(date '+%F-%H-%M-%S')
+UNIQUE_IDENTIFIER=$(date +%Y%m%d%H%M%S)
 ROOT_DIRECTORY_OF_DOCKERFILES="../test-infra/containers/pre_built_workers/"
-# enable fast building
-export DOCKER_BUILDKIT=1
+GRPC_CORE_GITREF="$(git ls-remote https://github.com/grpc/grpc.git master | cut -c1-7)"
+GRPC_GO_GITREF="$(git ls-remote https://github.com/grpc/grpc-go.git master | cut -c1-7)"
+GRPC_JAVA_GITREF="$(git ls-remote https://github.com/grpc/grpc-java.git master | cut -c1-7)"
+
 
 # Clone test-infra repository to one upper level directory than grpc
 cd ..
-git clone --recursive https://github.com/jtattermusch/test-infra.git
-cd test-infra
-git checkout prebuilt_improvements
-cd ../grpc
+git clone --recursive https://github.com/grpc/test-infra.git
+cd grpc
 
-trap deleteImages ERR
+# If there is a error within the function, will exit directly
+deleteImages() {
+  echo "an error has occurred after pushing the images, deleting images"
+  go run ../test-infra/tools/delete_prebuilt_workers/delete_prebuilt_workers.go \
+  -p $PREBUILT_IMAGE_PREFIX \
+  -t $UNIQUE_IDENTIFIER
+}
+trap deleteImages EXIT
 
 # Build and push prebuilt images for running tests
 go run ../test-infra/tools/prepare_prebuilt_workers/prepare_prebuilt_workers.go \
-  -l cxx:master \
-  -l go:master \
-  -l ruby:master \
-  -l python:master \
-  -l csharp:master \
-  -l php:master \
-  -l java:master \
-  -l node:master \
+  -l cxx:$GRPC_CORE_GITREF \
+  -l csharp:$GRPC_CORE_GITREF \
+  -l go:$GRPC_GO_GITREF \
+  -l java:$GRPC_JAVA_GITREF \
+  -l python:$GRPC_CORE_GITREF \
+  -l ruby:$GRPC_CORE_GITREF \
   -p $PREBUILT_IMAGE_PREFIX \
   -t $UNIQUE_IDENTIFIER \
   -r $ROOT_DIRECTORY_OF_DOCKERFILES
@@ -74,9 +70,10 @@ tools/run_tests/performance/loadtest_config.py -l c++ -l go \
     -t ./tools/run_tests/performance/templates/loadtest_template_basic_all_languages.yaml \
     -s client_pool=workers-8core -s server_pool=workers-8core \
     -s big_query_table=e2e_benchmarks.experimental_results \
-    -s timeout_seconds=900 --prefix=$KOKORO_BUILD_INITIATOR -u $UNIQUE_IDENTIFIER \
+    -s timeout_seconds=900 \
     -s prebuilt_image_prefix=$PREBUILT_IMAGE_PREFIX \
     -s prebuilt_image_tag=$UNIQUE_IDENTIFIER \
+    --prefix=$KOKORO_BUILD_INITIATOR -u $UNIQUE_IDENTIFIER \
     -r '(go_generic_sync_streaming_ping_pong_secure|go_protobuf_sync_unary_ping_pong_secure|cpp_protobuf_async_streaming_qps_unconstrained_secure)$' \
     -o ./loadtest_with_prebuilt_images.yaml
 
@@ -94,8 +91,3 @@ sudo mv kubectl $(which kubectl)
 kubectl version --client
 
 kubectl apply -f ./loadtest_with_prebuilt_images.yaml
-
-# Delete all images built for this build
-go run ../test-infra/tools/delete_prebuilt_workers/delete_prebuilt_workers.go \
-  -p $PREBUILT_IMAGE_PREFIX \
-  -t $UNIQUE_IDENTIFIER
