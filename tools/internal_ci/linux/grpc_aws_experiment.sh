1efa132114bd969f32352e670e7af2234349fe5b
Jan Tattermusch <jtattermusch@google.com>
2021-05-11 14:31:05 +0200

    using rsync for copying workspace is much faster

--- tools/internal_ci/linux/grpc_aws_experiment.sh
+++ tools/internal_ci/linux/grpc_aws_experiment.sh
@@ -83,10 +83,11 @@ sleep 2m
 
 WORKLOAD=grpc_aws_experiment_remote.sh
 REMOTE_SCRIPT_FAILURE=0
-echo "Copying to remote instance..."
-scp -i ~/.ssh/temp_client_key -r github/grpc ubuntu@$IP:
+echo "Copying workspace to remote instance..."
+# use rsync over ssh since it's much faster than scp
+time rsync -e "ssh -i ~/.ssh/temp_client_key" -a github/grpc ubuntu@$IP:~/workspace
 echo "Beginning CI workload..."
-ssh -i ~/.ssh/temp_client_key ubuntu@$IP "uname -a; ls -l; bash grpc/tools/internal_ci/linux/$WORKLOAD" || REMOTE_SCRIPT_FAILURE=$?
+ssh -i ~/.ssh/temp_client_key ubuntu@$IP "uname -a; cd ~/workspace; ls -l; bash grpc/tools/internal_ci/linux/$WORKLOAD" || REMOTE_SCRIPT_FAILURE=$?
 
 # Match return value
 echo "returning $REMOTE_SCRIPT_FAILURE based on script output"
