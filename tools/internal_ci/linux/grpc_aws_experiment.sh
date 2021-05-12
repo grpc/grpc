dbbbcce8367313cb6eb12c05e35afdbbee5af2a2
Jan Tattermusch <jtattermusch@google.com>
2021-05-12 11:58:37 +0200

    shutdown AWS instance once possible

--- tools/internal_ci/linux/grpc_aws_experiment.sh
+++ tools/internal_ci/linux/grpc_aws_experiment.sh
@@ -96,6 +96,11 @@ time rsync -e "ssh -i ~/.ssh/temp_client_key" -a github/grpc ubuntu@$IP:~/worksp
 echo "Beginning CI workload..."
 ssh -i ~/.ssh/temp_client_key ubuntu@$IP "uname -a; cd ~/workspace; ls -l; bash grpc/tools/internal_ci/linux/$WORKLOAD" || REMOTE_SCRIPT_EXITCODE=$?
 
+# Regardless of the remote script's result (success or failure), initiate shutdown of AWS instance a minute from now.
+# The small delay is useful to make sure the ssh session doesn't hang up on us if shutdown happens too quickly.
+echo "Shutting down instance $ID."
+ssh -i ~/.ssh/temp_client_key ubuntu@$IP "shutdown +1" || echo "WARNING: Failed to initiate AWS instance shutdown."
+
 # Match exitcode
 echo "Exiting with exitcode $REMOTE_SCRIPT_EXITCODE based on remote script output."
 exit $REMOTE_SCRIPT_EXITCODE
