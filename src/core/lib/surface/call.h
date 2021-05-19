977fcca287d7fc04cb6fc029e9be68d43141c0da
Vijay Pai <vpai@google.com>
2021-05-19 09:13:10 -0700

    ADD TODO requested by markdroth

--- src/core/lib/surface/call.h
+++ src/core/lib/surface/call.h
@@ -122,6 +122,8 @@ grpc_compression_algorithm grpc_call_compression_for_level(
 
 /* Returns whether or not the call's receive message operation failed because of
  * an error (as opposed to a graceful end-of-stream) */
+/* TODO(markdroth): This is currently available only to the C++ API.
+                    Move to surface API if requested by other languages. */
 bool grpc_call_failed_before_recv_message(grpc_call* c);
 
 extern grpc_core::TraceFlag grpc_call_error_trace;
