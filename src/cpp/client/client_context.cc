4d7ac642cc8ff57d7d19914a34186715eaec5947
Vijay Pai <vpai@google.com>
2021-05-25 16:06:47 -0700

    Remove unneeded change in client_context.cc

--- src/cpp/client/client_context.cc
+++ src/cpp/client/client_context.cc
@@ -23,6 +23,7 @@
 #include <grpc/support/alloc.h>
 #include <grpc/support/log.h>
 #include <grpc/support/string_util.h>
+
 #include <grpcpp/impl/codegen/interceptor_common.h>
 #include <grpcpp/impl/codegen/sync.h>
 #include <grpcpp/impl/grpc_library.h>
