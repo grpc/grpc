838183810a617ccd717cadd6ecb00c81804812f8
Matthew Stevenson <mattstev@google.com>
2021-06-09 13:15:08 -0700

    try updating s2a_core submodules before running distrib test

--- test/distrib/cpp/run_distrib_test_cmake.sh
+++ test/distrib/cpp/run_distrib_test_cmake.sh
@@ -53,6 +53,9 @@ popd
 
 # Install s2a_core
 mkdir -p "third_party/s2a_core/cmake/build"
+cd "third_party/s2a_core"
+git submodule update --init
+cd ../..
 pushd "third_party/s2a_core/cmake/build"
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE ../..
 make -j4 install
