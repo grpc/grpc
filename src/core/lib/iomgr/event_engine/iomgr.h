5d66f01671b48e2873a26ddbe613de31f15fdd48
AJ Heller <hork@google.com>
2021-06-16 13:30:53 -0700

    Add port_platform to headers

--- src/core/lib/iomgr/event_engine/iomgr.h
+++ src/core/lib/iomgr/event_engine/iomgr.h
@@ -13,6 +13,8 @@
 // limitations under the License.
 #ifndef GRPC_CORE_LIB_IOMGR_EVENT_ENGINE_IOMGR_H
 #define GRPC_CORE_LIB_IOMGR_EVENT_ENGINE_IOMGR_H
+#include <grpc/support/port_platform.h>
+
 #include <grpc/event_engine/event_engine.h>
 
 // This can be called anywhere in the EE-based iomgr impl where we need to
