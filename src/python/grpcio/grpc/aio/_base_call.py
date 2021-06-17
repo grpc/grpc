b32582473fbde4dea876e44d4500acd3398b0fff
Dante Camarena <hello@mhdante.com>
2021-06-17 02:09:26 -0400

    Python AIO: Relax Base Call typing to support optional metadata
    This is needed since InterceptedCall implements the _base_call.Call ABC. Other implementers may define a more

--- src/python/grpcio/grpc/aio/_base_call.py
+++ src/python/grpcio/grpc/aio/_base_call.py
@@ -86,7 +86,7 @@ class Call(RpcContext, metaclass=ABCMeta):
     """The abstract base class of an RPC on the client-side."""
 
     @abstractmethod
-    async def initial_metadata(self) -> Metadata:
+    async def initial_metadata(self) -> Optional[Metadata]:
         """Accesses the initial metadata sent by the server.
 
         Returns:
@@ -94,7 +94,7 @@ class Call(RpcContext, metaclass=ABCMeta):
         """
 
     @abstractmethod
-    async def trailing_metadata(self) -> Metadata:
+    async def trailing_metadata(self) -> Optional[Metadata]:
         """Accesses the trailing metadata sent by the server.
 
         Returns:
