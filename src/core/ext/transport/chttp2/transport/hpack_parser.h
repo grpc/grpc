5f4f5d076798f58c05b3258858fc9f95aa2c9d5e
Craig Tiller <craig.tiller@gmail.com>
2021-07-13 09:40:18 -0700

    sketch cpp interface

--- src/core/ext/transport/chttp2/transport/hpack_parser.h
+++ src/core/ext/transport/chttp2/transport/hpack_parser.h
@@ -27,84 +27,95 @@
 #include "src/core/ext/transport/chttp2/transport/hpack_table.h"
 #include "src/core/lib/transport/metadata.h"
 
-typedef struct grpc_chttp2_hpack_parser grpc_chttp2_hpack_parser;
+namespace grpc_core {
 
-typedef grpc_error_handle (*grpc_chttp2_hpack_parser_state)(
-    grpc_chttp2_hpack_parser* p, const uint8_t* beg, const uint8_t* end);
+class HPackParser {
+ public:
+  enum class Boundary { None, EndOfHeaders, EndOfStream };
+  enum class Priority { None, Included };
 
-struct grpc_chttp2_hpack_parser_string {
-  bool copied;
-  struct {
-    grpc_slice referenced;
+  // User specified structure called for each received header.
+  class Sink {
+   public:
+    virtual void OnHeader(grpc_mdelem md) = 0;
+  };
+
+  HPackParser();
+  ~HPackParser();
+
+  HPackParser(const HPackParser&) = delete;
+  HPackParser& operator=(const HPackParser&) = delete;
+
+  void BeginFrame(Sink* sink, Boundary boundary, Priority priority);
+
+  grpc_error_handle Parse(grpc_chttp2_hpack_parser* p, const grpc_slice& slice);
+
+ private:
+  using State = grpc_error_handle (*)(grpc_chttp2_hpack_parser* p,
+                                      const uint8_t* beg, const uint8_t* end);
+
+  struct String {
+    bool copied;
     struct {
-      char* str;
-      uint32_t length;
-      uint32_t capacity;
-    } copied;
-  } data;
-};
-struct grpc_chttp2_hpack_parser {
-  /* user specified callback for each header output */
-  grpc_error_handle (*on_header)(void* user_data, grpc_mdelem md);
-  void* on_header_user_data;
+      grpc_slice referenced;
+      struct {
+        char* str;
+        uint32_t length;
+        uint32_t capacity;
+      } copied;
+    } data;
+  };
 
-  grpc_error_handle last_error;
+  grpc_error_handle last_error_;
 
-  /* current parse state - or a function that implements it */
-  grpc_chttp2_hpack_parser_state state;
-  /* future states dependent on the opening op code */
-  const grpc_chttp2_hpack_parser_state* next_state;
-  /* what to do after skipping prioritization data */
-  grpc_chttp2_hpack_parser_state after_prioritization;
-  /* the refcount of the slice that we're currently parsing */
-  grpc_slice_refcount* current_slice_refcount;
-  /* the value we're currently parsing */
+  // current parse state - or a function that implements it
+  State state_;
+  // future states dependent on the opening op code
+  const State* next_state_;
+  // what to do after skipping prioritization data
+  State after_prioritization_;
+  // the refcount of the slice that we're currently parsing
+  grpc_slice_refcount* current_slice_refcount_;
+  // the value we're currently parsing
   union {
     uint32_t* value;
     grpc_chttp2_hpack_parser_string* str;
-  } parsing;
-  /* string parameters for each chunk */
-  grpc_chttp2_hpack_parser_string key;
-  grpc_chttp2_hpack_parser_string value;
-  /* parsed index */
-  uint32_t index;
-  /* When we parse a value string, we determine the metadata element for a
-     specific index, which we need again when we're finishing up with that
-     header. To avoid calculating the metadata element for that index a second
-     time at that stage, we cache (and invalidate) the element here. */
-  grpc_mdelem md_for_index;
+  } parsing_;
+  // string parameters for each chunk
+  String key_;
+  String value_;
+  // parsed index
+  uint32_t index_;
+  // When we parse a value string, we determine the metadata element for a
+  // specific index, which we need again when we're finishing up with that
+  // header. To avoid calculating the metadata element for that index a second
+  // time at that stage, we cache (and invalidate) the element here.
+  grpc_mdelem md_for_index_;
 #ifndef NDEBUG
-  int64_t precomputed_md_index;
+  int64_t precomputed_md_index_;
 #endif
-  /* length of source bytes for the currently parsing string */
-  uint32_t strlen;
-  /* number of source bytes read for the currently parsing string */
-  uint32_t strgot;
-  /* huffman decoding state */
-  int16_t huff_state;
-  /* is the string being decoded binary? */
-  uint8_t binary;
-  /* is the current string huffman encoded? */
-  uint8_t huff;
-  /* is a dynamic table update allowed? */
-  uint8_t dynamic_table_update_allowed;
-  /* set by higher layers, used by grpc_chttp2_header_parser_parse to signal
-     it should append a metadata boundary at the end of frame */
-  uint8_t is_boundary;
-  uint8_t is_eof;
+  // length of source bytes for the currently parsing string
+  uint32_t strlen_;
+  // number of source bytes read for the currently parsing string
+  uint32_t strgot_;
+  // huffman decoding state
+  int16_t huff_state_;
+  // is the string being decoded binary?
+  bool binary_;
+  // is the current string huffman encoded?
+  bool huff_;
+  // is a dynamic table update allowed?
+  uint8_t dynamic_table_update_allowed_;
+  // set by higher layers, used by grpc_chttp2_header_parser_parse to signal
+  // it should append a metadata boundary at the end of frame
+  Boundary boundary_;
   uint32_t base64_buffer;
 
-  /* hpack table */
+  // hpack table
   grpc_chttp2_hptbl table;
 };
 
-void grpc_chttp2_hpack_parser_init(grpc_chttp2_hpack_parser* p);
-void grpc_chttp2_hpack_parser_destroy(grpc_chttp2_hpack_parser* p);
-
-void grpc_chttp2_hpack_parser_set_has_priority(grpc_chttp2_hpack_parser* p);
-
-grpc_error_handle grpc_chttp2_hpack_parser_parse(grpc_chttp2_hpack_parser* p,
-                                                 const grpc_slice& slice);
+}  // namespace grpc_core
 
 /* wraps grpc_chttp2_hpack_parser_parse to provide a frame level parser for
    the transport */
