dc2d4b5683bc40d771cac45f055d5a9eaff70d98
Mark D. Roth <roth@google.com>
2021-07-30 07:57:04 -0700

    add separate channel arg to enable hedging

--- test/core/end2end/tests/retry_per_attempt_recv_timeout.cc
+++ test/core/end2end/tests/retry_per_attempt_recv_timeout.cc
@@ -124,6 +124,8 @@ static void test_retry_per_attempt_recv_timeout(
   grpc_arg args[] = {
       grpc_channel_arg_integer_create(
           const_cast<char*>(GRPC_ARG_ENABLE_RETRIES), 1),
+      grpc_channel_arg_integer_create(
+          const_cast<char*>(GRPC_ARG_EXPERIMENTAL_ENABLE_HEDGING), 1),
       grpc_channel_arg_string_create(
           const_cast<char*>(GRPC_ARG_SERVICE_CONFIG),
           const_cast<char*>(
