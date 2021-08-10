18c0dd0431dd941df91bb3889236dcc0769fb671
Lidi Zheng <lidiz@google.com>
2021-08-10 13:50:16 -0700

    Update xDS client/server image per-branch tag after build (#26955)
    * Update xDS client/server image per-branch tag after build
    
    * Address reviewers' comments
    
    * Replace the entire string instead of only prefix
    
    * Update regex
    
    * Update tools/internal_ci/linux/grpc_xds_k8s_install_test_driver.sh
    
    Co-authored-by: Sergii Tkachenko <hi@sergii.org>
    
    Co-authored-by: Sergii Tkachenko <hi@sergii.org>

--- tools/internal_ci/linux/grpc_xds_k8s_install_test_driver.sh
+++ tools/internal_ci/linux/grpc_xds_k8s_install_test_driver.sh
@@ -361,3 +361,20 @@ local_setup_test_driver() {
   mkdir -p "${TEST_XML_OUTPUT_DIR}"
 }
 
+#######################################
+# Tag and push the given Docker image
+# Arguments:
+#   The Docker image name
+#   The Docker image original tag name
+#   The Docker image new tag name
+# Outputs:
+#   Writes the output to stdout, stderr, files
+#######################################
+tag_and_push_docker_image() {
+  local image_name="$1"
+  local from_tag="$2"
+  local to_tag="$3"
+
+  docker tag "${image_name}:${from_tag}" "${image_name}:${to_tag}"
+  docker push "${image_name}:${to_tag}"
+}
