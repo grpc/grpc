bf4c08f79f63e67f60987bf752887b0906332521
Emmanuel Nwogu-Agbakwuribe <itsemmanuel@google.com>
2021-08-10 05:31:09 +0000

    Added std::string

--- include/grpcpp/security/tls_certificate_provider.h
+++ include/grpcpp/security/tls_certificate_provider.h
@@ -98,7 +98,7 @@ class DataWatcherCertificateProvider : public CertificateProviderInterface {
 
   grpc_tls_certificate_provider* c_provider() override { return c_provider_; }
 
-  grpc::Status SetRootCertificate(const string& root_certificate);
+  grpc::Status SetRootCertificate(const std::string& root_certificate);
 
   grpc::Status SetKeyCertificatePairs(
       const std::vector<IdentityKeyCertPair>& identity_key_cert_pairs);
