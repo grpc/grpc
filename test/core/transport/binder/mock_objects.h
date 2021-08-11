075f56f70804037cc2eb8ba01e7a7477e5ff7aa1
Ta-Wei Tu <waynetu@google.com>
2021-08-11 21:06:01 +0800

    Fix typo

--- test/core/transport/binder/mock_objects.h
+++ test/core/transport/binder/mock_objects.h
@@ -1,4 +1,4 @@
-// Copyright 2021 gRPC authorsabsl::string_view//
+// Copyright 2021 gRPC authors.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
