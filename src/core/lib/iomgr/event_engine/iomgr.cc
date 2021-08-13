aa16b26c6283b56458961cbe274400d046ee3261
AJ Heller <hork@google.com>
2021-08-13 10:13:15 -0700

    Add TODO to instantiate the default

--- src/core/lib/iomgr/event_engine/iomgr.cc
+++ src/core/lib/iomgr/event_engine/iomgr.cc
@@ -46,6 +46,7 @@ using ::grpc_event_engine::experimental::Promise;
 
 EventEngine* g_event_engine = nullptr;
 
+// TODO(nnoble): Instantiate the default EventEngine if none have been provided.
 void iomgr_platform_init(void) { GPR_ASSERT(g_event_engine != nullptr); }
 
 void iomgr_platform_flush(void) {}
