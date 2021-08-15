a510972f1839f07a0c1d213b77d99bab569edd91
ctiller <ctiller@users.noreply.github.com>
2021-08-15 03:08:56 +0000

    Automated change: Fix sanity tests

--- src/core/lib/slice/percent_encoding.cc
+++ src/core/lib/slice/percent_encoding.cc
@@ -22,8 +22,8 @@
 
 #include <grpc/support/log.h>
 
-#include "src/core/lib/slice/slice_internal.h"
 #include "src/core/lib/gprpp/bitset.h"
+#include "src/core/lib/slice/slice_internal.h"
 
 #include <cstdint>
 
