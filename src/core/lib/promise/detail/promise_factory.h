f35a7d77b993bc4ac5f8cc37243f150e9c1b2517
ctiller <ctiller@users.noreply.github.com>
2021-08-23 05:43:09 +0000

    Automated change: Fix sanity tests

--- src/core/lib/promise/detail/promise_factory.h
+++ src/core/lib/promise/detail/promise_factory.h
@@ -110,14 +110,16 @@ PromiseFactoryImpl(F&& f, A&& arg) {
 // Promote a callable() -> T|Poll<T> to a PromiseFactory(A) -> Promise<T>
 // by dropping the argument passed to the factory.
 template <typename A, typename F>
-absl::enable_if_t<!IsVoidCallable<ResultOf<F()>>::value, PromiseLike<RemoveCVRef<F>>>
+absl::enable_if_t<!IsVoidCallable<ResultOf<F()>>::value,
+                  PromiseLike<RemoveCVRef<F>>>
 PromiseFactoryImpl(F f, A&&) {
   return PromiseLike<F>(std::move(f));
 }
 
 // Promote a callable() -> T|Poll<T> to a PromiseFactory() -> Promise<T>
 template <typename F>
-absl::enable_if_t<!IsVoidCallable<ResultOf<F()>>::value, PromiseLike<RemoveCVRef<F>>>
+absl::enable_if_t<!IsVoidCallable<ResultOf<F()>>::value,
+                  PromiseLike<RemoveCVRef<F>>>
 PromiseFactoryImpl(F f) {
   return PromiseLike<F>(std::move(f));
 }
