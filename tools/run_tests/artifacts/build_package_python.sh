c6d0574557a38e0d3e4fa4129baba7ee64f632f0
Lidi Zheng <lidiz@google.com>
2021-08-30 00:02:19 -0700

    Allow different wheel library to produce different tag order

--- tools/run_tests/artifacts/build_package_python.sh
+++ tools/run_tests/artifacts/build_package_python.sh
@@ -36,7 +36,7 @@ strip_binary_wheel() {
     DISTRIBUTION_NAME=$(basename "$WHEEL_PATH" | cut -d '-' -f 1)
     VERSION=$(basename "$WHEEL_PATH" | cut -d '-' -f 2)
     ${PYTHON} -m wheel pack "$TEMP_WHEEL_DIR/$DISTRIBUTION_NAME-$VERSION" -d "$TEMP_WHEEL_DIR"
-    mv "$TEMP_WHEEL_DIR/$WHEEL_FILE" "$WHEEL_PATH"
+    mv "$TEMP_WHEEL_DIR/*.whl" "$WHEEL_PATH"
 }
 
 for wheel in artifacts/*.whl; do
