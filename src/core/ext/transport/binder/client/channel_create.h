8661c1e3b8f9400caea6c27e88252f69970a3e45
Ta-Wei Tu <waynetu@google.com>
2021-09-06 14:33:29 +0800

    [binder] Add CreateCustomBinderChannel()
    This allows users to pass in custom channel arguments.

--- src/core/ext/transport/binder/client/channel_create.h
+++ src/core/ext/transport/binder/client/channel_create.h
@@ -22,6 +22,7 @@
 #include <grpc/impl/codegen/grpc_types.h>
 #include <grpc/support/port_platform.h>
 #include <grpcpp/channel.h>
+#include <grpcpp/support/channel_arguments.h>
 #include <jni.h>
 
 #include "absl/strings/string_view.h"
@@ -41,6 +42,13 @@ void BindToOnDeviceServerService(void* jni_env_void, jobject application,
 std::shared_ptr<grpc::Channel> CreateBinderChannel(
     void* jni_env_void, jobject application, absl::string_view package_name,
     absl::string_view class_name);
+
+// Need to be invoked after BindToOnDeviceServerService
+// Create a new Channel from server package name and service class name and with
+// custom channel arguments.
+std::shared_ptr<grpc::Channel> CreateCustomBinderChannel(
+    void* jni_env_void, jobject application, absl::string_view package_name,
+    absl::string_view class_name, const ChannelArguments& args);
 
 }  // namespace experimental
 }  // namespace grpc
