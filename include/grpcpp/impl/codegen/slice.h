82d889fa19407d5220c9d1ef6ff08239b222faf5
Craig Tiller <ctiller@google.com>
2021-09-08 16:15:42 +0000

    rerun after merge

--- include/grpcpp/impl/codegen/slice.h
+++ include/grpcpp/impl/codegen/slice.h
@@ -21,11 +21,10 @@
 
 // IWYU pragma: private, include <grpcpp/support/slice.h>
 
+#include <grpc/impl/codegen/slice.h>
 #include <grpcpp/impl/codegen/config.h>
 #include <grpcpp/impl/codegen/core_codegen_interface.h>
 #include <grpcpp/impl/codegen/string_ref.h>
-
-#include <grpc/impl/codegen/slice.h>
 
 namespace grpc {
 
