b18d2e1a4735f76b97baff468897c3bc07d8b412
Richard Belleville <rbellevi@google.com>
2021-09-16 19:20:48 -0700

    Yapf

--- tools/run_tests/xds_k8s_test_driver/framework/xds_flags.py
+++ tools/run_tests/xds_k8s_test_driver/framework/xds_flags.py
@@ -36,9 +36,10 @@ RESOURCE_SUFFIX = flags.DEFINE_string(
 NETWORK = flags.DEFINE_string("network",
                               default="default",
                               help="GCP Network ID")
-ROUTER_SCOPE = flags.DEFINE_string("router_scope",
-                              default="fooscope",
-                              help="Scope specified in router if using AppNet APIs")
+ROUTER_SCOPE = flags.DEFINE_string(
+    "router_scope",
+    default="fooscope",
+    help="Scope specified in router if using AppNet APIs")
 # Mirrors --xds-server-uri argument of Traffic Director gRPC Bootstrap
 XDS_SERVER_URI = flags.DEFINE_string(
     "xds_server_uri",
