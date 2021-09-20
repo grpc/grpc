54c4f4bb9898f67fb35fe4bbbcb344efa201e3fb
Craig Tiller <craig.tiller@gmail.com>
2021-09-19 22:26:23 -0700

    promise fuzzer

--- test/core/promise/promise_fuzzer.cc
+++ test/core/promise/promise_fuzzer.cc
@@ -12,10 +12,167 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "src/core/lib/promise/join.h"
+#include "src/core/lib/promise/map.h"
+#include "src/core/lib/promise/promise.h"
+#include "src/core/lib/promise/race.h"
+#include "src/core/lib/promise/seq.h"
 #include "src/libfuzzer/libfuzzer_macro.h"
 #include "test/core/promise/promise_fuzzer.pb.h"
 
 bool squelch = true;
 bool leak_check = true;
 
-DEFINE_PROTO_FUZZER(const promise_fuzzer::Msg& msg) {}
+namespace grpc_core {
+using IntHdl = std::shared_ptr<int>;
+
+template <typename T>
+using PromiseFactory = std::function<Promise<T>()>;
+
+static Promise<IntHdl> MakePromise(const promise_fuzzer::Promise& p);
+static PromiseFactory<IntHdl> MakePromiseFactory(
+    const promise_fuzzer::PromiseFactory& p);
+
+static PromiseFactory<IntHdl> MakePromiseFactory(
+    const promise_fuzzer::PromiseFactory& p) {
+  switch (p.promise_factory_type_case()) {
+    case promise_fuzzer::PromiseFactory::kPromise:
+      return [p]() { return MakePromise(p.promise()); };
+    case promise_fuzzer::PromiseFactory::PROMISE_FACTORY_TYPE_NOT_SET:
+      break;
+  }
+  return
+      [] { return []() -> Poll<IntHdl> { return std::make_shared<int>(42); }; };
+}
+
+static Promise<IntHdl> MakePromise(const promise_fuzzer::Promise& p) {
+  switch (p.promise_type_case()) {
+    case promise_fuzzer::Promise::kSeq:
+      switch (p.seq().promise_factories_size()) {
+        case 1:
+          return Seq(MakePromise(p.seq().first()),
+                     MakePromiseFactory(p.seq().promise_factories(0)));
+        case 2:
+          return Seq(MakePromise(p.seq().first()),
+                     MakePromiseFactory(p.seq().promise_factories(0)),
+                     MakePromiseFactory(p.seq().promise_factories(1)));
+        case 3:
+          return Seq(MakePromise(p.seq().first()),
+                     MakePromiseFactory(p.seq().promise_factories(0)),
+                     MakePromiseFactory(p.seq().promise_factories(1)),
+                     MakePromiseFactory(p.seq().promise_factories(2)));
+        case 4:
+          return Seq(MakePromise(p.seq().first()),
+                     MakePromiseFactory(p.seq().promise_factories(0)),
+                     MakePromiseFactory(p.seq().promise_factories(1)),
+                     MakePromiseFactory(p.seq().promise_factories(2)),
+                     MakePromiseFactory(p.seq().promise_factories(3)));
+        case 5:
+          return Seq(MakePromise(p.seq().first()),
+                     MakePromiseFactory(p.seq().promise_factories(0)),
+                     MakePromiseFactory(p.seq().promise_factories(1)),
+                     MakePromiseFactory(p.seq().promise_factories(2)),
+                     MakePromiseFactory(p.seq().promise_factories(3)),
+                     MakePromiseFactory(p.seq().promise_factories(4)));
+        case 6:
+          return Seq(MakePromise(p.seq().first()),
+                     MakePromiseFactory(p.seq().promise_factories(0)),
+                     MakePromiseFactory(p.seq().promise_factories(1)),
+                     MakePromiseFactory(p.seq().promise_factories(2)),
+                     MakePromiseFactory(p.seq().promise_factories(3)),
+                     MakePromiseFactory(p.seq().promise_factories(4)),
+                     MakePromiseFactory(p.seq().promise_factories(5)));
+      }
+      break;
+    case promise_fuzzer::Promise::kJoin:
+      switch (p.join().promises_size()) {
+        case 1:
+          return Map(Join(MakePromise(p.join().promises(0))),
+                     [](std::tuple<IntHdl> t) { return std::get<0>(t); });
+        case 2:
+          return Map(
+              Join(MakePromise(p.join().promises(0)),
+                   MakePromise(p.join().promises(1))),
+              [](std::tuple<IntHdl, IntHdl> t) { return std::get<0>(t); });
+        case 3:
+          return Map(Join(MakePromise(p.join().promises(0)),
+                          MakePromise(p.join().promises(1)),
+                          MakePromise(p.join().promises(2))),
+                     [](std::tuple<IntHdl, IntHdl, IntHdl> t) {
+                       return std::get<0>(t);
+                     });
+        case 4:
+          return Map(Join(MakePromise(p.join().promises(0)),
+                          MakePromise(p.join().promises(1)),
+                          MakePromise(p.join().promises(2)),
+                          MakePromise(p.join().promises(3))),
+                     [](std::tuple<IntHdl, IntHdl, IntHdl, IntHdl> t) {
+                       return std::get<0>(t);
+                     });
+        case 5:
+          return Map(Join(MakePromise(p.join().promises(0)),
+                          MakePromise(p.join().promises(1)),
+                          MakePromise(p.join().promises(2)),
+                          MakePromise(p.join().promises(3)),
+                          MakePromise(p.join().promises(4))),
+                     [](std::tuple<IntHdl, IntHdl, IntHdl, IntHdl, IntHdl> t) {
+                       return std::get<0>(t);
+                     });
+        case 6:
+          return Map(
+              Join(MakePromise(p.join().promises(0)),
+                   MakePromise(p.join().promises(1)),
+                   MakePromise(p.join().promises(2)),
+                   MakePromise(p.join().promises(3)),
+                   MakePromise(p.join().promises(4)),
+                   MakePromise(p.join().promises(5))),
+              [](std::tuple<IntHdl, IntHdl, IntHdl, IntHdl, IntHdl, IntHdl> t) {
+                return std::get<0>(t);
+              });
+      }
+      break;
+    case promise_fuzzer::Promise::kRace:
+      switch (p.race().promises_size()) {
+        case 1:
+          return Race(MakePromise(p.race().promises(0)));
+        case 2:
+          return Race(MakePromise(p.race().promises(0)),
+                      MakePromise(p.race().promises(1)));
+        case 3:
+          return Race(MakePromise(p.race().promises(0)),
+                      MakePromise(p.race().promises(1)),
+                      MakePromise(p.race().promises(2)));
+        case 4:
+          return Race(MakePromise(p.race().promises(0)),
+                      MakePromise(p.race().promises(1)),
+                      MakePromise(p.race().promises(2)),
+                      MakePromise(p.race().promises(3)));
+        case 5:
+          return Race(MakePromise(p.race().promises(0)),
+                      MakePromise(p.race().promises(1)),
+                      MakePromise(p.race().promises(2)),
+                      MakePromise(p.race().promises(3)),
+                      MakePromise(p.race().promises(4)));
+        case 6:
+          return Race(MakePromise(p.race().promises(0)),
+                      MakePromise(p.race().promises(1)),
+                      MakePromise(p.race().promises(2)),
+                      MakePromise(p.race().promises(3)),
+                      MakePromise(p.race().promises(4)),
+                      MakePromise(p.race().promises(5)));
+      }
+      break;
+    case promise_fuzzer::Promise::PromiseTypeCase::PROMISE_TYPE_NOT_SET:
+      break;
+  }
+  return [] { return std::make_shared<int>(42); };
+}
+}  // namespace grpc_core
+
+DEFINE_PROTO_FUZZER(const promise_fuzzer::Msg& msg) {
+  if (!msg.has_promise()) {
+    return;
+  }
+  auto promise = grpc_core::MakePromise(msg.promise());
+  promise();
+}
