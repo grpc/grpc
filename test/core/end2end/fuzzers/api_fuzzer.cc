fb1483f483a8e93bcc70c7dcdcb3d9c61df1ef93
Craig Tiller <ctiller@google.com>
2021-09-21 08:12:55 +0000

    more fixes

--- test/core/end2end/fuzzers/api_fuzzer.cc
+++ test/core/end2end/fuzzers/api_fuzzer.cc
@@ -407,11 +407,13 @@ typedef struct {
   grpc_closure* closure;
   grpc_endpoint** ep;
   gpr_timespec deadline;
+  grpc_slice_allocator* slice_allocator;
 } future_connect;
 
 static void do_connect(void* arg, grpc_error* error) {
   future_connect* fc = static_cast<future_connect*>(arg);
   if (error != GRPC_ERROR_NONE) {
+    grpc_slice_allocator_destroy(fc->slice_allocator);
     *fc->ep = nullptr;
     grpc_core::ExecCtx::Run(DEBUG_LOCATION, fc->closure, GRPC_ERROR_REF(error));
   } else if (g_server != nullptr) {
@@ -423,7 +425,7 @@ static void do_connect(void* arg, grpc_error* error) {
     grpc_transport* transport =
         grpc_create_chttp2_transport(nullptr, server, false, grpc_resource_user_create(g_resource_quota, "transport-user"));
     g_server->core_server->SetupTransport(transport, nullptr, nullptr, nullptr);
-    grpc_chttp2_transport_start_reading(transport, nullptr, nullptr);
+    grpc_chttp2_transport_start_reading(transport, nullptr, nullptr, nullptr);
 
     grpc_core::ExecCtx::Run(DEBUG_LOCATION, fc->closure, GRPC_ERROR_NONE);
   } else {
@@ -432,10 +434,11 @@ static void do_connect(void* arg, grpc_error* error) {
   gpr_free(fc);
 }
 
-static void sched_connect(grpc_closure* closure, grpc_endpoint** ep,
+static void sched_connect(grpc_closure* closure, grpc_slice_allocator* slice_allocator, grpc_endpoint** ep,
                           gpr_timespec deadline) {
   if (gpr_time_cmp(deadline, gpr_now(deadline.clock_type)) < 0) {
     *ep = nullptr;
+    grpc_slice_allocator_destroy(slice_allocator);
     grpc_core::ExecCtx::Run(
         DEBUG_LOCATION, closure,
         GRPC_ERROR_CREATE_FROM_STATIC_STRING("Connect deadline exceeded"));
@@ -446,12 +449,14 @@ static void sched_connect(grpc_closure* closure, grpc_endpoint** ep,
   fc->closure = closure;
   fc->ep = ep;
   fc->deadline = deadline;
+  fc->slice_allocator = slice_allocator;
   grpc_timer_init(
       &fc->timer, GPR_MS_PER_SEC + grpc_core::ExecCtx::Get()->Now(),
       GRPC_CLOSURE_CREATE(do_connect, fc, grpc_schedule_on_exec_ctx));
 }
 
 static void my_tcp_client_connect(grpc_closure* closure, grpc_endpoint** ep,
+                                  grpc_slice_allocator* slice_allocator,
                                   grpc_pollset_set* /*interested_parties*/,
                                   const grpc_channel_args* /*channel_args*/,
                                   const grpc_resolved_address* /*addr*/,
