7d883773458a52a7f47097403f2517ed19f1eb78
Mark D. Roth <roth@google.com>
2021-09-25 01:03:43 +0000

    fix copyright headers

--- test/core/client_channel/rls_lb_config_parser_test.cc
+++ test/core/client_channel/rls_lb_config_parser_test.cc
@@ -1,5 +1,5 @@
 //
-// Copyright 2021 gRpc authors.
+// Copyright 2021 gRPC authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- test/cpp/end2end/rls_end2end_test.cc
+++ test/cpp/end2end/rls_end2end_test.cc
@@ -1,5 +1,5 @@
 //
-// Copyright 2020 gRpc authors.
+// Copyright 2020 gRPC authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
