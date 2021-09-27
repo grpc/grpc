33be35e1c68337c6a2ff51cc7cc50a573e43132c
Jan Tattermusch <jtattermusch@google.com>
2021-09-27 14:28:25 +0200

    revert unnecessary changes

--- tools/internal_ci/linux/grpc_build_submodule_at_head.sh
+++ tools/internal_ci/linux/grpc_build_submodule_at_head.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2020 The gRPC authors.
+# Copyright 2017 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
