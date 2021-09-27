1bf904994b29d47c7d2ae405f3c6f01f9a5ffe3b
AJ Heller <hork@google.com>
2021-09-27 14:26:31 -0700

    EventEngine Test Suite: Timers

---
+++ test/core/event_engine/test_suite/noop_event_engine_test.cc
@@ -0,0 +1,98 @@
+// Copyright 2021 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <thread>
+
+#include "absl/status/status.h"
+#include "absl/time/clock.h"
+
+#include <grpc/event_engine/event_engine.h>
+#include <grpc/event_engine/slice_allocator.h>
+#include <grpc/support/log.h>
+
+#include "test/core/event_engine/test_suite/event_engine_test.h"
+
+using ::grpc_event_engine::experimental::EndpointConfig;
+using ::grpc_event_engine::experimental::EventEngine;
+using ::grpc_event_engine::experimental::SliceAllocator;
+using ::grpc_event_engine::experimental::SliceAllocatorFactory;
+
+constexpr absl::Duration SLEEP_TIME = absl::Milliseconds(100);
+
+class NoopEventEngine : public grpc_event_engine::experimental::EventEngine {
+ public:
+  void Run(EventEngine::Closure* closure) override { closure->Run(); }
+
+  void Run(std::function<void()> closure) override { closure(); }
+
+  absl::StatusOr<std::unique_ptr<Listener>> CreateListener(
+      Listener::AcceptCallback on_accept,
+      std::function<void(absl::Status)> on_shutdown,
+      const EndpointConfig& config,
+      std::unique_ptr<SliceAllocatorFactory> slice_allocator_factory) override {
+    return nullptr;
+  }
+
+  EventEngine::TaskHandle RunAt(absl::Time when,
+                                EventEngine::Closure* closure) override {
+    std::thread t([=]() {
+      while (absl::Now() < when) {
+        absl::SleepFor(SLEEP_TIME);
+        if (shutting_down_) return;
+      }
+      closure->Run();
+    });
+    return {-1, -1};
+  }
+  EventEngine::TaskHandle RunAt(absl::Time when,
+                                std::function<void()> closure) override {
+    threads_.emplace_back([=]() {
+      while (absl::Now() < when) {
+        absl::SleepFor(SLEEP_TIME);
+        if (shutting_down_) return;
+      }
+      closure();
+    });
+    return {-1, -1};
+  }
+
+  bool Cancel(EventEngine::TaskHandle handle) override { return false; }
+
+  absl::Status Connect(EventEngine::OnConnectCallback on_connect,
+                       const EventEngine::ResolvedAddress& addr,
+                       const EndpointConfig& args,
+                       std::unique_ptr<SliceAllocator> slice_allocator,
+                       absl::Time deadline) override {
+    return absl::OkStatus();
+  }
+
+  std::unique_ptr<DNSResolver> GetDNSResolver() override { return nullptr; }
+
+  bool IsWorkerThread() override { return false; }
+
+  ~NoopEventEngine() {
+    shutting_down_ = true;
+    for (std::thread& t : threads_) {
+      t.join();
+    }
+  }
+
+ private:
+  std::vector<std::thread> threads_;
+  bool shutting_down_ = false;
+};
+
+std::unique_ptr<EventEngine> EventEngineTest::NewEventEngine() {
+  return absl::make_unique<NoopEventEngine>();
+}
---
+++ test/core/event_engine/test_suite/timer_test.cc
@@ -0,0 +1,108 @@
+// Copyright 2021 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "absl/time/time.h"
+
+#include <grpc/event_engine/event_engine.h>
+#include <grpc/support/log.h>
+
+#include "src/core/lib/gprpp/sync.h"
+#include "test/core/event_engine/test_suite/event_engine_test.h"
+
+using ::testing::ElementsAre;
+
+class EventEngineTimerTest : public EventEngineTest {
+ protected:
+  grpc_core::Mutex mu_;
+  grpc_core::CondVar cv_;
+  bool signaled ABSL_GUARDED_BY(mu_) = false;
+};
+
+TEST_F(EventEngineTimerTest, ImmediateCallbackIsExecutedQuickly) {
+  auto engine = this->NewEventEngine();
+  grpc_core::MutexLock lock(&mu_);
+  engine->RunAt(absl::Now(), [this]() {
+    grpc_core::MutexLock lock(&mu_);
+    signaled = true;
+    cv_.Signal();
+  });
+  cv_.WaitWithTimeout(&mu_, absl::Microseconds(250));
+  ASSERT_TRUE(signaled);
+}
+
+TEST_F(EventEngineTimerTest, SupportsCancellation) {
+  auto engine = this->NewEventEngine();
+  auto handle = engine->RunAt(absl::InfiniteFuture(), []() {});
+  ASSERT_TRUE(engine->Cancel(handle));
+}
+
+TEST_F(EventEngineTimerTest, CancelledCallbackIsNotExecuted) {
+  {
+    auto engine = this->NewEventEngine();
+    auto handle = engine->RunAt(absl::InfiniteFuture(), [this]() {
+      grpc_core::MutexLock lock(&mu_);
+      signaled = true;
+    });
+    ASSERT_TRUE(engine->Cancel(handle));
+  }
+  // The engine is deleted, and all closures should have been flushed
+  grpc_core::MutexLock lock(&mu_);
+  ASSERT_FALSE(signaled);
+}
+
+TEST_F(EventEngineTimerTest, TimersRespectScheduleOrdering) {
+  // Note: this is a brittle test if the first call to `RunAt` takes longer than
+  // a second.
+  std::vector<uint8_t> ordered;
+  uint8_t count = 0;
+  grpc_core::MutexLock lock(&mu_);
+  {
+    auto engine = this->NewEventEngine();
+    engine->RunAt(absl::Now() + absl::Seconds(1), [&]() {
+      grpc_core::MutexLock lock(&mu_);
+      ordered.push_back(2);
+      count++;
+      cv_.Signal();
+    });
+    engine->RunAt(absl::Now(), [&]() {
+      grpc_core::MutexLock lock(&mu_);
+      ordered.push_back(1);
+      count++;
+      cv_.Signal();
+    });
+    // Ensure both callbacks have run. Simpler than a mutex.
+    while (count != 2) {
+      cv_.WaitWithTimeout(&mu_, absl::Microseconds(100));
+    }
+  }
+  // The engine is deleted, and all closures should have been flushed beforehand
+  ASSERT_THAT(ordered, ElementsAre(1, 2));
+}
+
+TEST_F(EventEngineTimerTest, CancellingExecutedCallbackIsNoopAndReturnsFalse) {
+  auto engine = this->NewEventEngine();
+  grpc_core::MutexLock lock(&mu_);
+  auto handle = engine->RunAt(absl::Now(), [this]() {
+    grpc_core::MutexLock lock(&mu_);
+    signaled = true;
+    cv_.Signal();
+  });
+  cv_.WaitWithTimeout(&mu_, absl::Seconds(10));
+  ASSERT_TRUE(signaled);
+  // The callback has run, and now we'll try to cancel it.
+  ASSERT_FALSE(engine->Cancel(handle));
+}
