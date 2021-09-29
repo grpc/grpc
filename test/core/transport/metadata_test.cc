6043615c4dbf0f71d656e34b5359770cc1ad07f9
Craig Tiller <ctiller@google.com>
2021-09-29 01:51:33 +0000

    fixes

--- test/core/transport/metadata_test.cc
+++ test/core/transport/metadata_test.cc
@@ -34,6 +34,7 @@
 #include "src/core/ext/transport/chttp2/transport/hpack_utils.h"
 #include "src/core/ext/transport/chttp2/transport/incoming_metadata.h"
 #include "src/core/lib/gpr/string.h"
+#include "src/core/lib/iomgr/error.h"
 #include "src/core/lib/iomgr/exec_ctx.h"
 #include "src/core/lib/slice/slice_internal.h"
 #include "src/core/lib/transport/metadata_batch.h"
@@ -411,12 +412,15 @@ static void test_grpc_metadata_batch_get_value_returns_multiple_values(void) {
 static void test_grpc_chttp2_incoming_metadata_replace_or_add_works(void) {
   grpc_core::Arena* arena = grpc_core::Arena::Create(1024);
   grpc_chttp2_incoming_metadata_buffer buffer(arena);
-  GRPC_LOG_IF_ERROR(grpc_chttp2_incoming_metadata_buffer_add(
-      &buffer, grpc_mdelem_from_slices(grpc_slice_from_static_string("a"),
-                                       grpc_slice_from_static_string("b"))));
-  GRPC_LOG_IF_ERROR(grpc_chttp2_incoming_metadata_buffer_replace_or_add(
-      &buffer, grpc_slice_from_static_string("a"),
-      grpc_slice_malloc(1024 * 1024 * 1024)));
+  GRPC_LOG_IF_ERROR("incoming_buffer_add",
+                    grpc_chttp2_incoming_metadata_buffer_add(
+                        &buffer, grpc_mdelem_from_slices(
+                                     grpc_slice_from_static_string("a"),
+                                     grpc_slice_from_static_string("b"))));
+  GRPC_LOG_IF_ERROR("incoming_buffer_replace_or_add",
+                    grpc_chttp2_incoming_metadata_buffer_replace_or_add(
+                        &buffer, grpc_slice_from_static_string("a"),
+                        grpc_slice_malloc(1024 * 1024 * 1024)));
   arena->Destroy();
 }
 
