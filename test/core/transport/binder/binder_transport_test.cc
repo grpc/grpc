8e015a3f6b9c7c7c66e40829b6bccdbb93da1bac
Craig Tiller <craig.tiller@gmail.com>
2021-09-30 10:41:02 -0700

    test fixes

--- test/core/transport/binder/binder_transport_test.cc
+++ test/core/transport/binder/binder_transport_test.cc
@@ -190,7 +190,8 @@ struct MakeSendInitialMetadata {
 
   std::vector<grpc_linked_mdelem> storage;
   grpc_linked_mdelem method_ref_storage;
-  grpc_metadata_batch grpc_initial_metadata{};
+  grpc_core::ScopedArena arena = grpc_core::MakeScopedArena(1024);
+  grpc_metadata_batch grpc_initial_metadata{arena.get()};
 };
 
 struct MakeSendMessage {
@@ -221,7 +222,8 @@ struct MakeSendTrailingMetadata {
         &grpc_trailing_metadata;
   }
 
-  grpc_metadata_batch grpc_trailing_metadata{};
+  grpc_core::ScopedArena arena = grpc_core::MakeScopedArena(1024);
+  grpc_metadata_batch grpc_trailing_metadata{arena.get()};
 };
 
 struct MakeRecvInitialMetadata {
@@ -243,7 +245,8 @@ struct MakeRecvInitialMetadata {
   ~MakeRecvInitialMetadata() {}
 
   MockGrpcClosure ready;
-  grpc_metadata_batch grpc_initial_metadata;
+  grpc_core::ScopedArena arena = grpc_core::MakeScopedArena(1024);
+  grpc_metadata_batch grpc_initial_metadata{arena.get()};
   absl::Notification notification;
 };
 
@@ -285,7 +288,8 @@ struct MakeRecvTrailingMetadata {
   ~MakeRecvTrailingMetadata() {}
 
   MockGrpcClosure ready;
-  grpc_metadata_batch grpc_trailing_metadata;
+  grpc_core::ScopedArena arena = grpc_core::MakeScopedArena(1024);
+  grpc_metadata_batch grpc_trailing_metadata{arena.get()};
   absl::Notification notification;
 };
 
