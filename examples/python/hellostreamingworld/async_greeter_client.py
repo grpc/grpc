7338bcd140ae86534d4ef89677753f057bf0779e
Michael Mercier <michael.mercier@ryax.tech>
2021-10-01 16:04:01 +0200

    Format and lint + remove exernal generator

--- examples/python/hellostreamingworld/async_greeter_client.py
+++ examples/python/hellostreamingworld/async_greeter_client.py
@@ -17,28 +17,32 @@ import asyncio
 import logging
 
 import grpc
+
 import hellostreamingworld_pb2
 import hellostreamingworld_pb2_grpc
 
 
 async def run() -> None:
-    async with grpc.aio.insecure_channel('localhost:50051') as channel:
+    async with grpc.aio.insecure_channel("localhost:50051") as channel:
         stub = hellostreamingworld_pb2_grpc.MultiGreeterStub(channel)
 
         # Read from an async generator
-        async for response in stub.sayHello(hellostreamingworld_pb2.HelloRequest(name='you')):
-            print("Greeter client received from async generator: " + response.message)
+        async for response in stub.sayHello(
+            hellostreamingworld_pb2.HelloRequest(name="you")
+        ):
+            print("Greeter client received from async generator: " +
+                  response.message)
 
         # Direct read from the stub
-        hello_stream = stub.sayHello(
-            hellostreamingworld_pb2.HelloRequest(name='you'))
+        hello_stream = stub.sayHello(hellostreamingworld_pb2.HelloRequest(name="you"))
         while True:
             response = await hello_stream.read()
             if response == grpc.aio.EOF:
                 break
-            print("Greeter client received from direct read: " + response.message)
+            print("Greeter client received from direct read: " +
+                  response.message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig()
     asyncio.run(run())
--- examples/python/hellostreamingworld/async_greeter_server.py
+++ examples/python/hellostreamingworld/async_greeter_server.py
@@ -17,38 +17,34 @@ import asyncio
 import logging
 
 import grpc
-from hellostreamingworld_pb2_grpc import MultiGreeterServicer, add_MultiGreeterServicer_to_server
-from hellostreamingworld_pb2 import HelloRequest, HelloReply
+from hellostreamingworld_pb2 import HelloReply
+from hellostreamingworld_pb2 import HelloRequest
+from hellostreamingworld_pb2_grpc import MultiGreeterServicer
+from hellostreamingworld_pb2_grpc import add_MultiGreeterServicer_to_server
 
 
 NUMBER_OF_REPLY = 10
 
 
-async def say_hello_generator(request):
-    for i in range(NUMBER_OF_REPLY):
-        yield HelloReply(message=f"Hello number {i}, {request.name}!")
-
-
 class Greeter(MultiGreeterServicer):
-
     async def sayHello(
-            self, request: HelloRequest,
-            context: grpc.aio.ServicerContext) -> HelloReply:
-        logging.info(f"Serving sayHello request {request}")
-        async for msg in say_hello_generator(request):
-            yield msg
+        self, request: HelloRequest, context: grpc.aio.ServicerContext
+    ) -> HelloReply:
+        logging.info("Serving sayHello request %s", request)
+        for i in range(NUMBER_OF_REPLY):
+          yield HelloReply(message=f"Hello number {i}, {request.name}!")
 
 
 async def serve() -> None:
     server = grpc.aio.server()
     add_MultiGreeterServicer_to_server(Greeter(), server)
-    listen_addr = '[::]:50051'
+    listen_addr = "[::]:50051"
     server.add_insecure_port(listen_addr)
     logging.info("Starting server on %s", listen_addr)
     await server.start()
     await server.wait_for_termination()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     asyncio.run(serve())
