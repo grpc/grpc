7f086e80963d9cd05d108d009611bf101f67be87
Esun Kim <veblush@google.com>
2021-10-05 15:52:31 -0700

    Use StatusAllocHeapPtr for call_combiner and lockfree_event

--- src/core/lib/gprpp/status_helper.h
+++ src/core/lib/gprpp/status_helper.h
@@ -176,6 +176,17 @@ void StatusFreePtr(uintptr_t ptr);
 /// This shouldn't be used except migration purpose.
 absl::Status StatusGetFromPtr(uintptr_t ptr);
 
+/// Returns ptr that is allocated in the heap memory and the given status is
+/// copied into. This ptr can be used to get Status later and should be
+/// freed by StatusFreeHeapPtr. This can be 0 in case of OkStatus.
+uintptr_t StatusAllocHeapPtr(absl::Status s);
+
+/// Frees the allocated status at heap ptr.
+void StatusFreeHeapPtr(uintptr_t ptr);
+
+/// Get the status from a heap ptr.
+absl::Status StatusGetFromHeapPtr(uintptr_t ptr);
+
 }  // namespace internal
 
 }  // namespace grpc_core
