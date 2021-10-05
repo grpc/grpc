9148687f10b3dfe94e17da16e2a3eec0c44a180f
Craig Tiller <ctiller@google.com>
2021-10-04 21:22:41 -0700

    Update memory_allocator.cc

--- src/core/lib/event_engine/memory_allocator.cc
+++ src/core/lib/event_engine/memory_allocator.cc
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <grpc/impl/codegen/port_platform.h>
 #include <grpc/support/port_platform.h>
 
 #include <grpc/event_engine/memory_allocator.h>
