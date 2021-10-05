ef1ef083b7dd1c52eef2616cd96dc73e306452a0
AJ Heller <hork@google.com>
2021-10-05 10:46:25 -0700

    Support system package libuv; fix cmake linker error

--- cmake/libuv.cmake
+++ cmake/libuv.cmake
@@ -13,11 +13,18 @@
 # limitations under the License.
 
 # TODO(hork): support module and package versions
-if(NOT LIBUV_ROOT_DIR)
-  set(LIBUV_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/libuv)
+if(gRPC_LIBUV_PROVIDER STREQUAL "module")
+  if(NOT LIBUV_ROOT_DIR)
+    set(LIBUV_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/libuv)
+  endif()
+  add_subdirectory("${LIBUV_ROOT_DIR}" third_party/libuv)
+  include_directories("${LIBUV_ROOT_DIR}/include" "${LIBUV_ROOT_DIR}/src")
+  set(_gRPC_LIBUV_LIBRARIES uv)
+  set(_gRPC_LIBUV_INCLUDE_DIR "${LIBUV_ROOT_DIR}/include" "${LIBUV_ROOT_DIR}/src")
+elseif(gRPC_LIBUV_PROVIDER STREQUAL "package")
+  find_package(uv REQUIRED)
+  if(TARGET uv::uv)
+    set(_gRPC_LIBUV_LIBRARIES uv::uv)
+  endif()
+  set(_gRPC_FIND_LIBUV "if(NOT uv_FOUND)\n  find_package(uv)\nendif()")
 endif()
-
-add_subdirectory("${LIBUV_ROOT_DIR}" third_party/libuv)
-include_directories("${LIBUV_ROOT_DIR}/include", "${LIBUV_ROOT_DIR}/src")
-
-set(_gRPC_LIBUV_INCLUDE_DIR "${LIBUV_ROOT_DIR}/include" "${LIBUV_ROOT_DIR}/src")
