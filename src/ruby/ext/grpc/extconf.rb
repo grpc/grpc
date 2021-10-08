1acf969a7bbf5b4c3068c1de96037a80e6c5a624
Benoit Daloze <benoit.daloze@oracle.com>
2021-10-08 19:34:06 +0200

    Keep the rb_tr_abi_version symbol for TruffleRuby in grpc_c.so
    * Otherwise TruffleRuby cannot verify the ABI version is correct.
    * See https://github.com/oracle/truffleruby/issues/2386

--- src/ruby/ext/grpc/extconf.rb
+++ src/ruby/ext/grpc/extconf.rb
@@ -114,6 +114,7 @@ end
 $CFLAGS << ' -I' + File.join(grpc_root, 'include')
 
 ext_export_file = File.join(grpc_root, 'src', 'ruby', 'ext', 'grpc', 'ext-export')
+ext_export_file += '-truffleruby' if RUBY_ENGINE == 'truffleruby'
 $LDFLAGS << ' -Wl,--version-script="' + ext_export_file + '.gcc"' if linux
 $LDFLAGS << ' -Wl,-exported_symbols_list,"' + ext_export_file + '.clang"' if apple_toolchain?
 
