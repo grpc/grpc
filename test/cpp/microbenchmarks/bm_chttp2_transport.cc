1df153f46ee232b5c899ff7d0ababea95ec5ffa9
Craig Tiller <ctiller@google.com>
2021-10-13 08:44:57 -0700

    Revert "Revert "Move metadata setting into HPackParser (#27545)" (#27686)" (#27688)
    This reverts commit 0e6ced8800fe6d52ea7af50971570e3be26b1d45.

--- test/cpp/microbenchmarks/bm_chttp2_transport.cc
+++ test/cpp/microbenchmarks/bm_chttp2_transport.cc
@@ -323,9 +323,10 @@ static void BM_StreamCreateSendInitialMetadataDestroy(benchmark::State& state) {
     op.payload = &op_payload;
   };
 
+  auto arena = grpc_core::MakeScopedArena(1024);
   std::vector<grpc_mdelem> elems = Metadata::GetElems();
   std::vector<grpc_linked_mdelem> storage(elems.size());
-  grpc_metadata_batch b;
+  grpc_metadata_batch b(arena.get());
   for (size_t i = 0; i < elems.size(); i++) {
     GPR_ASSERT(GRPC_LOG_IF_ERROR(
         "addmd", grpc_metadata_batch_add_tail(&b, &storage[i], elems[i])));
@@ -427,7 +428,8 @@ static void BM_TransportStreamSend(benchmark::State& state) {
   std::vector<grpc_mdelem> elems =
       RepresentativeClientInitialMetadata::GetElems();
   std::vector<grpc_linked_mdelem> storage(elems.size());
-  grpc_metadata_batch b;
+  auto arena = grpc_core::MakeScopedArena(1024);
+  grpc_metadata_batch b(arena.get());
   for (size_t i = 0; i < elems.size(); i++) {
     GPR_ASSERT(GRPC_LOG_IF_ERROR(
         "addmd", grpc_metadata_batch_add_tail(&b, &storage[i], elems[i])));
@@ -563,10 +565,11 @@ static void BM_TransportStreamRecv(benchmark::State& state) {
     op.payload = &op_payload;
   };
 
+  auto arena = grpc_core::MakeScopedArena(1024);
   std::vector<grpc_mdelem> elems =
       RepresentativeClientInitialMetadata::GetElems();
   std::vector<grpc_linked_mdelem> storage(elems.size());
-  grpc_metadata_batch b;
+  grpc_metadata_batch b(arena.get());
   for (size_t i = 0; i < elems.size(); i++) {
     GPR_ASSERT(GRPC_LOG_IF_ERROR(
         "addmd", grpc_metadata_batch_add_tail(&b, &storage[i], elems[i])));
@@ -629,7 +632,7 @@ static void BM_TransportStreamRecv(benchmark::State& state) {
   });
 
   reset_op();
-  auto b_recv = absl::make_unique<grpc_metadata_batch>();
+  auto b_recv = absl::make_unique<grpc_metadata_batch>(arena.get());
   op.send_initial_metadata = true;
   op.payload->send_initial_metadata.send_initial_metadata = &b;
   op.recv_initial_metadata = true;
