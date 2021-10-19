60eb0a017955692f5ee950a024dfb547c376b5af
Craig Tiller <craig.tiller@gmail.com>
2021-10-18 22:46:28 -0700

    storage-classes

--- src/core/lib/slice/slice.h
+++ src/core/lib/slice/slice.h
@@ -17,26 +17,83 @@
 
 namespace grpc_core {
 
-class Slice {
+namespace slice_detail {
+
+enum class Storage {
+  kStatic,
+  kBorrowed,
+  kOwned,
+  kInlined,
+  kUniquelyOwned,
+  kUnknown,
+};
+
+static constexpr bool Reffable(Storage storage) {
+  return storage == Storage::kOwned || storage == Storage::kUnknown;
+}
+
+static constexpr bool Mutable(Storage storage) {
+  return storage == Storage::kUniquelyOwned || storage == Storage::kInlined;
+}
+
+template <Storage kStorage>
+struct IntoMutable;
+
+template <bool kOwned>
+class UnderlyingSlice {
+ protected:
+  ~UnderlyingSlice() = default;
+
+ private:
+  grpc_slice slice_;
+};
+
+template <>
+class UnderlyingSlice<true> {
  public:
-  Slice() : Slice(grpc_empty_slice()) {}
-  Slice(const Slice&) = delete;
-  Slice& operator=(const Slice&) = delete;
-  Slice(Slice&& other) { other.Swap(this); }
-  Slice& operator=(Slice&& other) {
-    other.Swap(this);
-    return *this;
+  UnderlyingSlice(const UnderlyingSlice&) = delete;
+  UnderlyingSlice& operator=(const UnderlyingSlice&) = delete;
+
+  const grpc_slice& c_slice() { return slice_; }
+
+ protected:
+  ~UnderlyingSlice() { grpc_slice_unref_internal(slice_); }
+
+ private:
+  grpc_slice slice_;
+};
+
+template <Storage kStorage>
+class BasicSlice : public UnderlyingSlice<kStorage == Storage::kOwned> {
+ public:
+  absl::enable_if_t<Reffable(kStorage), BasicSlice> Ref() const {
+    return BasicSlice(grpc_slice_ref_internal(slice_));
+  }
+  BasicSlice<Storage::kUniquelyOwned> Copy() const {
+    return BasicSlice<Storage::kUniquelyOwned>(grpc_slice_copy(slice_));
+  }
+  BasicSlice<IntoMutable<kStorage>::kStorage> IntoMutable() {
+    return BasicSlice<IntoMutable<kStorage>::kStorage>(
+        IntoMutable<kStorage>::From(c_slice()));
   }
 
-  Slice Ref() const { return Slice(grpc_slice_ref_internal(slice_)); }
-  Slice Copy() const { return Slice(grpc_slice_copy(slice_)); }
+  operator const BasicSlice<Storage::kUnknown>&() const {
+    return *reinterpret_cast<const BasicSlice<Storage::kUnknown>*>(this);
+  }
 
   // Named constructors
-  static Slice FromC(const grpc_slice& slice) { return Slice(slice); }
+  static BasicSlice FromC(const grpc_slice& slice) {
+    GPR_ASSERT(CompatibleStorage(slice.refcount, kStorage));
+    return Slice(slice);
+  }
 
   // Iterator access to the underlying bytes
-  uint8_t* begin() { return GRPC_SLICE_START_PTR(slice_); }
-  uint8_t* end() { return GRPC_SLICE_END_PTR(slice_); }
+  absl::enable_if_t<Mutable<kStorage>, uint8_t*> begin() {
+    return GRPC_SLICE_START_PTR(slice_);
+  }
+  absl::enable_if_t<Mutable<kStorage>, uint8_t*> end() {
+    return GRPC_SLICE_END_PTR(slice_);
+  }
   const uint8_t* begin() const { return GRPC_SLICE_START_PTR(slice_); }
   const uint8_t* end() const { return GRPC_SLICE_END_PTR(slice_); }
   const uint8_t* cbegin() const { return GRPC_SLICE_START_PTR(slice_); }
@@ -44,13 +101,14 @@ class Slice {
 
   // Array access
   uint8_t operator[](size_t i) const { return GRPC_SLICE_START_PTR(slice_)[i]; }
-  uint8_t& operator[](size_t i) { return GRPC_SLICE_START_PTR(slice_)[i]; }
-
-  // Borrow the underlying object.
-  const grpc_slice& c_slice() { return slice_; }
+  absl::enable_if_t<Mutable<kStorage>, uint8_t&> operator[](size_t i) {
+    return GRPC_SLICE_START_PTR(slice_)[i];
+  }
 
   // Access underlying data
-  uint8_t* data() { return GRPC_SLICE_START_PTR(slice_); }
+  absl::enable_if_t<Mutable<kStorage>, uint8_t*> data() {
+    return GRPC_SLICE_START_PTR(slice_);
+  }
   const uint8_t* data() const { return GRPC_SLICE_START_PTR(slice_); }
 
   // Size of the slice
@@ -59,28 +117,32 @@ class Slice {
   bool empty() const { return size() == 0; }
 
   // Comparisons
-  bool operator==(const Slice& rhs) const {
-    return grpc_slice_eq(slice_, rhs.slice_);
+  template <Storage kOtherStorage>
+  bool operator==(const BasicSlice<kOtherStorage>& rhs) const {
+    return grpc_slice_eq(c_slice(), rhs.c_slice());
   }
-  bool operator<(const Slice& rhs) const {
-    return grpc_slice_cmp(slice_, rhs.slice_) < 0;
+  template <Storage kOtherStorage>
+  bool operator<(const BasicSlice<kOtherStorage>& rhs) const {
+    return grpc_slice_cmp(c_slice(), rhs.c_slice()) < 0;
   }
-  bool operator>(const Slice& rhs) const {
-    return grpc_slice_cmp(slice_, rhs.slice_) > 0;
+  template <Storage kOtherStorage>
+  bool operator>(const BasicSlice<kOtherStorage>& rhs) const {
+    return grpc_slice_cmp(c_slice(), rhs.c_slice()) > 0;
   }
 
   // Does this slice point to the same object with the same length?
-  bool is_equivalent_to(const Slice& other) const {
+  template <Storage kOtherStorage>
+  bool is_equivalent_to(const BasicSlice<kOtherStorage>& other) const {
     return grpc_slice_is_equivalent(slice_, other.slice_) != 0;
   }
-
- protected:
-  void SwapUnderlying(Slice* slice) { std::swap(slice_, slice->slice_); }
-  explicit Slice(grpc_slice slice) : slice_(slice) {}
-
- private:
-  grpc_slice slice_;
 };
+
+}  // namespace slice_detail
+
+using Slice = slice_detail::BasicSlice<slice_detail::Storage::kUnknown>;
+using StaticSlice = slice_detail::BasicSlice<slice_detail::Storage::kStatic>;
+using ExternalSlice =
+    slice_detail::BasicSlice<slice_detail::Storage::kBorrowed>;
 
 }  // namespace grpc_core
 
