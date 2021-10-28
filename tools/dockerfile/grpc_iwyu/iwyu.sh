9051bdec097f12f63f14babe10bd1547ac663e4f
Craig Tiller <ctiller@google.com>
2021-10-28 14:59:31 -0700

    Update iwyu.sh

--- tools/dockerfile/grpc_iwyu/iwyu.sh
+++ tools/dockerfile/grpc_iwyu/iwyu.sh
@@ -21,19 +21,22 @@ export PATH=${PATH}:/iwyu_build/bin
 
 cat compile_commands.json | sed "s,\"file\": \",\"file\": \"${IWYU_ROOT}/,g" > compile_commands_for_iwyu.json
 
-# run clang tidy for all source files
+# figure out which files to include
 cat compile_commands.json | jq -r '.[].file' \
   | grep -E "^src/core/lib/promise/" \
   | grep -v -E "/upb-generated/|/upbdefs-generated/" \
   | sort \
   | tee iwyu_files.txt
 
+# run iwyu, filtering out changes to port_platform.h
 xargs -a iwyu_files.txt /iwyu/iwyu_tool.py -p compile_commands_for_iwyu.json -j 16 \
   | grep -v -E "port_platform.h" \
   | tee iwyu.out
 
+# apply the suggested changes
 /iwyu/fix_includes.py --nocomments < iwyu.out || true
 
+# reformat sources, since iwyu gets this wrong
 xargs -a iwyu_files.txt clang-format-11 -i
 
 # TODO(ctiller): expand this to match the clang-tidy directories:
