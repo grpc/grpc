cd4a648d7135a67810702f5efa2d88f0e97ae32e
Alexander Polcyn <apolcyn@google.com>
2021-10-28 13:05:41 -0700

    fix api fuzzer build

--- test/core/end2end/fuzzers/api_fuzzer.cc
+++ test/core/end2end/fuzzers/api_fuzzer.cc
@@ -130,8 +130,7 @@ grpc_ares_request* my_dns_lookup_ares_locked(
     grpc_pollset_set* /*interested_parties*/, grpc_closure* on_done,
     std::unique_ptr<grpc_core::ServerAddressList>* addresses,
     std::unique_ptr<grpc_core::ServerAddressList>* /*balancer_addresses*/,
-    char** /*service_config_json*/, int /*query_timeout*/,
-    std::shared_ptr<grpc_core::WorkSerializer> /*combiner*/) {
+    char** /*service_config_json*/, int /*query_timeout*/) {
   addr_req* r = new addr_req();
   r->addr = gpr_strdup(addr);
   r->on_done = on_done;
