86015f19c93bc65c98cb2111b51bba00fba0eca3
Yash Tibrewal <yashkt@google.com>
2021-11-01 04:12:36 -0700

    clang-format

--- src/core/lib/iomgr/exec_ctx_work_serializer.h
+++ src/core/lib/iomgr/exec_ctx_work_serializer.h
@@ -34,20 +34,22 @@
 namespace grpc_core {
 
 // ExecCtxWorkSerializer is a mechanism to schedule callbacks in a synchronized
-// manner through ExecCtx. 
+// manner through ExecCtx.
 //
-// All callbacks scheduled on an ExecCtxWorkSerializer instance will be executed serially on a thread's ExecCtx. The API provides a
-// FIFO guarantee to the execution of callbacks scheduled on the thread. 
+// All callbacks scheduled on an ExecCtxWorkSerializer instance will be executed
+// serially on a thread's ExecCtx. The API provides a FIFO guarantee to the
+// execution of callbacks scheduled on the thread.
 
-// When Run() is invoked with a callback, it is added on to a queue which is drained
-// when ExecCtx is flushed. The ExecCtx used for drained the callbacks is
-// determined by the size of the queue of callbacks. If the queue is empty, the
-// thread invoking `Run()` to add a callback to the queue shares its ExecCtx. If
-// the queue already has other callbacks, the current callback is simply added
-// to the queue. 
+// When Run() is invoked with a callback, it is added on to a queue which is
+// drained when ExecCtx is flushed. The ExecCtx used for drained the callbacks
+// is determined by the size of the queue of callbacks. If the queue is empty,
+// the thread invoking `Run()` to add a callback to the queue shares its
+// ExecCtx. If the queue already has other callbacks, the current callback is
+// simply added to the queue.
 //
-// The prime reason to use ExecCtxWorkSerializer instead of WorkSerializer is for the ability to execute callbacks without worrying about
-// the locks being held when scheduling a callback.
+// The prime reason to use ExecCtxWorkSerializer instead of WorkSerializer is
+// for the ability to execute callbacks without worrying about the locks being
+// held when scheduling a callback.
 class ABSL_LOCKABLE ExecCtxWorkSerializer {
  public:
   ExecCtxWorkSerializer();
