4e9c914930e6417b1f9c8f8761e6a727afa81571
Craig Tiller <craig.tiller@gmail.com>
2021-11-01 23:29:48 -0700

    mac progress

--- test/core/iomgr/ios/CFStreamTests/CFStreamEndpointTests.mm
+++ test/core/iomgr/ios/CFStreamTests/CFStreamEndpointTests.mm
@@ -125,8 +125,7 @@ static bool compare_slice_buffer_with_buffer(grpc_slice_buffer *slices, const ch
   /* connect to it */
   XCTAssertEqual(getsockname(svr_fd, (struct sockaddr *)addr, (socklen_t *)&resolved_addr.len), 0);
   init_event_closure(&done, &connected);
-  grpc_tcp_client_connect(&done, &ep_, grpc_slice_allocator_create_unlimited(), nullptr, nullptr,
-                          &resolved_addr, GRPC_MILLIS_INF_FUTURE);
+  grpc_tcp_client_connect(&done, &ep_, nullptr, nullptr, &resolved_addr, GRPC_MILLIS_INF_FUTURE);
 
   /* await the connection */
   do {
