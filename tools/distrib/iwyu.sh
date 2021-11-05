730ddcae953f6c6c091a80c6447f6453e8c0d3c7
Craig Tiller <ctiller@google.com>
2021-11-05 10:03:50 -0700

    Update iwyu.sh

--- tools/distrib/iwyu.sh
+++ tools/distrib/iwyu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2015 gRPC authors.
+# Copyright 2021 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
