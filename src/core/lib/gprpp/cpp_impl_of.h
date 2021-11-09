6faa9cc7675f5b7afd56250af34edf954f847452
Craig Tiller <craig.tiller@gmail.com>
2021-11-09 15:20:28 -0800

    Fix issue leading to bad pointers being returned on Windows.

--- src/core/lib/gprpp/cpp_impl_of.h
+++ src/core/lib/gprpp/cpp_impl_of.h
@@ -32,7 +32,7 @@ class CppImplOf {
   }
 
   // Retrieve a c pointer (of the same ownership as this)
-  CType* c_ptr() { return reinterpret_cast<CType*>(this); }
+  CType* c_ptr() { return reinterpret_cast<CType*>(static_cast<CppType*>(this)); }
 
  protected:
   ~CppImplOf() = default;
