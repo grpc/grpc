3111329e49aa86d939fa1c9ef89a8b15b6792560
Craig Tiller <ctiller@google.com>
2021-11-10 19:01:21 +0000

    fix

--- src/core/lib/event_engine/memory_allocator.cc
+++ src/core/lib/event_engine/memory_allocator.cc
@@ -47,7 +47,7 @@ class SliceRefCount {
 
  private:
   grpc_slice_refcount base_;
-  std::atomic<size_t> refs_{0};
+  std::atomic<size_t> refs_{1};
   std::shared_ptr<internal::MemoryAllocatorImpl> allocator_;
   size_t size_;
 };
