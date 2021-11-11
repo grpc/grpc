0695542412c9a0dbc028dded8ddd1a9c047fc0d9
Craig Tiller <ctiller@google.com>
2021-11-10 17:29:52 -0800

    hyrums law fixes

--- src/core/lib/iomgr/tcp_posix.cc
+++ src/core/lib/iomgr/tcp_posix.cc
@@ -1743,7 +1743,8 @@ grpc_endpoint* grpc_tcp_create(grpc_fd* em_fd,
   tcp->incoming_buffer = nullptr;
   tcp->target_length = static_cast<double>(tcp_read_chunk_size);
   tcp->min_read_chunk_size = tcp_min_read_chunk_size;
-  tcp->cur_read_chunk_size = tcp_min_read_chunk_size;
+  tcp->cur_read_chunk_size =
+      (tcp_max_read_chunk_size + 3 * tcp_min_read_chunk_size) / 4;
   tcp->max_read_chunk_size = tcp_max_read_chunk_size;
   tcp->bytes_read_this_round = 0;
   /* Will be set to false by the very first endpoint read function */
