48323d8ea6239b59b7d47de5999cf1114f1fc97b
Craig Tiller <ctiller@google.com>
2021-11-15 11:52:35 -0800

    Ensure JSON parser can consume dumped JSON (#27994)
    * Ensure JSON parser can consume dumped JSON
    
    * fixes
    
    * fixes
    
    * Update fuzzer.cc

--- test/core/json/fuzzer.cc
+++ test/core/json/fuzzer.cc
@@ -30,8 +30,14 @@ bool leak_check = true;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   grpc_error_handle error = GRPC_ERROR_NONE;
-  grpc_core::Json::Parse(
+  auto json = grpc_core::Json::Parse(
       absl::string_view(reinterpret_cast<const char*>(data), size), &error);
+  if (error == GRPC_ERROR_NONE) {
+    auto text2 = json.Dump();
+    auto json2 = grpc_core::Json::Parse(text2, &error);
+    GPR_ASSERT(error == GRPC_ERROR_NONE);
+    GPR_ASSERT(json == json2);
+  }
   GRPC_ERROR_UNREF(error);
   return 0;
 }
