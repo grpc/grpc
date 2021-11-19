281f96025b07a64078ce4330851a16383fdb6e54
Craig Tiller <craig.tiller@gmail.com>
2021-11-19 11:01:53 -0800

    Document more

--- src/core/lib/promise/arena_promise.h
+++ src/core/lib/promise/arena_promise.h
@@ -26,19 +26,30 @@ namespace grpc_core {
 
 namespace arena_promise_detail {
 
+// Type erased promise stored in the arena.
 template <typename T>
 class ImplInterface {
  public:
+  // Poll the promise, once.
   virtual Poll<T> PollOnce() = 0;
+  // Destroy the underlying callable object if there is one.
+  // Since we don't delete (the arena owns the memory) but we may need to call a
+  // destructor, we expose this for when the ArenaPromise object is destroyed.
   virtual void Destroy() = 0;
 
  protected:
   ~ImplInterface() = default;
 };
 
+// Implementation of ImplInterface for an empty object.
+// Used when an empty ArenaPromise is created, or when the ArenaPromise is moved
+// from. Since in either case these objects should not be polled, we simply
+// crash if it is.
 template <typename T>
 class NullImpl final : public ImplInterface<T> {
  public:
+  ~NullImpl() = delete;
+
   Poll<T> PollOnce() override {
     abort();
     GPR_UNREACHABLE_CODE(return Pending{});
@@ -51,22 +62,41 @@ class NullImpl final : public ImplInterface<T> {
   }
 };
 
+// Implementation of ImplInterface for a callable object.
 template <typename T, typename Callable>
 class CallableImpl final : public ImplInterface<T> {
  public:
+  ~CallableImpl() = delete;
+
   explicit CallableImpl(Callable&& callable) : callable_(std::move(callable)) {}
+  // Forward polls to the callable object.
   Poll<T> PollOnce() override { return callable_(); }
+  // Destroy destructs the callable object.
   void Destroy() override { this->~CallableImpl(); }
 
  private:
   Callable callable_;
 };
 
+// If a callable object is empty we can substitute any instance of that callable
+// for the one we call (for how could we tell the difference)?
+// Since this corresponds to a lambda with no fields, and we expect these to be
+// reasonably common, we can elide the arena allocation entirely and simply poll
+// a global shared instance.
+// (this comes up often when the promise only accesses context data from the
+// containing activity).
 template <typename T, typename Callable>
 class SharedImpl final : public ImplInterface<T> {
  public:
+  ~SharedImpl() = delete;
+
+  // Call the callable. Since it's empty it can't access any member variables,
+  // and as such we can choose any address for the object.
   Poll<T> PollOnce() override { return (*static_cast<Callable*>(nullptr))(); }
+  // Nothing to destroy.
   void Destroy() override {}
+  // Return a pointer to the shared instance - these are singletons, and are
+  // needed just to get the vtable in place.
   static SharedImpl* Get() {
     static SharedImpl impl;
     return &impl;
@@ -74,9 +104,10 @@ class SharedImpl final : public ImplInterface<T> {
 
  private:
   SharedImpl() = default;
-  ~SharedImpl() = default;
 };
 
+// Redirector type: given a callable type, expose a Make() function that creates
+// the appropriate underlying implementation.
 template <typename T, typename Callable, typename Ignored = void>
 struct ChooseImplForCallable;
 
@@ -97,6 +128,7 @@ struct ChooseImplForCallable<
   }
 };
 
+// Wrap ChooseImplForCallable with a friend approachable syntax.
 template <typename T, typename Callable>
 ImplInterface<T>* MakeImplForCallable(Arena* arena, Callable&& callable) {
   return ChooseImplForCallable<T, Callable>::Make(
@@ -118,8 +150,10 @@ class ArenaPromise {
       : impl_(arena_promise_detail::MakeImplForCallable<T>(
             arena, std::forward<Callable>(callable))) {}
 
+  // ArenaPromise is not copyable.
   ArenaPromise(const ArenaPromise&) = delete;
   ArenaPromise& operator=(const ArenaPromise&) = delete;
+  // ArenaPromise is movable.
   ArenaPromise(ArenaPromise&& other) noexcept : impl_(other.impl_) {
     other.impl_ = arena_promise_detail::NullImpl<T>::Get();
   }
@@ -129,11 +163,14 @@ class ArenaPromise {
     return *this;
   }
 
+  // Destruction => call Destroy on the underlying impl object.
   ~ArenaPromise() { impl_->Destroy(); }
 
+  // Expose the promise interface: a call operator that returns Poll<T>.
   Poll<T> operator()() { return impl_->PollOnce(); }
 
  private:
+  // Underlying impl object.
   arena_promise_detail::ImplInterface<T>* impl_ =
       arena_promise_detail::NullImpl<T>::Get();
 };
