f9fd5734edc74b107f138c29818fbcdb3923241b
Jan Tattermusch <jtattermusch@google.com>
2021-12-01 11:47:42 +0100

    faster grpc_build_artifacts cleanup

--- tools/internal_ci/linux/grpc_build_artifacts.sh
+++ tools/internal_ci/linux/grpc_build_artifacts.sh
@@ -26,4 +26,11 @@ set -e  # rvm commands are very verbose
 rvm --default use ruby-2.4.1
 set -ex
 
-tools/run_tests/task_runner.py -f artifact linux -j 12
+tools/run_tests/task_runner.py -f artifact linux -j 12 || FAILED="true"
+
+tools/internal_ci/helper_scripts/delete_nonartifacts.sh || true
+
+if [ "$FAILED" != "" ]
+then
+  exit 1
+fi
