ac72c624b5cc852ac454da63e0e145b166411549
Craig Tiller <ctiller@google.com>
2021-12-03 22:56:12 +0000

    Fix memory leak

--- src/core/lib/transport/metadata_batch.h
+++ src/core/lib/transport/metadata_batch.h
@@ -324,11 +324,13 @@ class AppendHelper {
   }
 
   GPR_ATTRIBUTE_NOINLINE void NotFound(absl::string_view key) {
-    GRPC_LOG_IF_ERROR("AppendMetadata",
-               container_->Append(grpc_mdelem_from_slices(
+    grpc_mdelem elem = grpc_mdelem_from_slices(
                    grpc_slice_intern(
                        grpc_slice_from_static_buffer(key.data(), key.length())),
-                   value_.TakeCSlice())));
+                   value_.TakeCSlice());
+    if (!GRPC_LOG_IF_ERROR("AppendMetadata", container_->Append(elem))) {
+      GRPC_MDELEM_UNREF(elem);
+    }
   }
 
  private:
