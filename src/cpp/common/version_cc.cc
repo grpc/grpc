35aa4a183df6794dea6b4365d467b5977b1804bd
Esun Kim <veblush@google.com>
2021-12-14 09:10:45 -0800

    Generate projects

--- src/cpp/common/version_cc.cc
+++ src/cpp/common/version_cc.cc
@@ -22,5 +22,5 @@
 #include <grpcpp/grpcpp.h>
 
 namespace grpc {
-std::string Version() { return "1.43.0-pre1"; }
+std::string Version() { return "1.43.0"; }
 }  // namespace grpc
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.43.0.pre1'
+    VERSION = '1.43.0'
   end
 end
