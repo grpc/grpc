8a9db3e7da7a3a9c53d84663279bf5ecbaed82a5
Easwar Swaminathan <easwars@google.com>
2021-12-15 00:21:20 +0000

    Add v1.43.0 release of grpc-go to interop matrix

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -195,6 +195,7 @@ LANG_RELEASE_MATRIX = {
             ('v1.40.0', ReleaseInfo(runtimes=['go1.16'])),
             ('v1.41.0', ReleaseInfo(runtimes=['go1.16'])),
             ('v1.42.0', ReleaseInfo(runtimes=['go1.16'])),
+            ('v1.43.0', ReleaseInfo(runtimes=['go1.16'])),
         ]),
     'java':
         OrderedDict([
