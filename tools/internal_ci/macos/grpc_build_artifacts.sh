6195a500f7140329c17e2c57c10433f2e61e93a6
Alexander Polcyn <apolcyn@google.com>
2021-12-23 16:38:23 -0800

    update rubygems before setup env script

--- tools/internal_ci/macos/grpc_build_artifacts.sh
+++ tools/internal_ci/macos/grpc_build_artifacts.sh
@@ -35,11 +35,11 @@ python3.7 -m pip install -U cython setuptools==44.1.1 wheel --user
 python3.8 -m pip install -U cython setuptools==44.1.1 wheel --user
 python3.9 -m pip install -U cython setuptools==44.1.1 wheel --user
 
-# needed to build ruby artifacts
-time bash tools/distrib/build_ruby_environment_macos.sh
-
 gem install rubygems-update
 update_rubygems
+
+# needed to build ruby artifacts
+time bash tools/distrib/build_ruby_environment_macos.sh
 
 tools/run_tests/task_runner.py -f artifact macos ${TASK_RUNNER_EXTRA_FILTERS} || FAILED="true"
 
