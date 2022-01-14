f9bd0f97605e33b14f0cd2d062b59940a327dbe7
Jan Tattermusch <jtattermusch@google.com>
2022-01-14 10:16:53 +0100

    fine tune grpc_distribtests_ruby.sh parallelism

--- tools/internal_ci/linux/grpc_distribtests_ruby.sh
+++ tools/internal_ci/linux/grpc_distribtests_ruby.sh
@@ -27,7 +27,7 @@ source tools/internal_ci/helper_scripts/prepare_build_linux_rc
 source tools/internal_ci/helper_scripts/prepare_build_linux_ruby_artifact_rc
 
 # Build all ruby linux artifacts (this step actually builds all the binary wheels and source archives)
-tools/run_tests/task_runner.py -f artifact linux ruby ${TASK_RUNNER_EXTRA_FILTERS} -j 2 --inner_jobs 16 -x build_artifacts/sponge_log.xml || FAILED="true"
+tools/run_tests/task_runner.py -f artifact linux ruby ${TASK_RUNNER_EXTRA_FILTERS} -j 6 --inner_jobs 6 -x build_artifacts/sponge_log.xml || FAILED="true"
 
 # Ruby "build_package" step is basically just a passthough for the "grpc" gems, so it's enough to just
 # copy the native gems directly to the "distribtests" step and skip the "build_package" phase entirely.
