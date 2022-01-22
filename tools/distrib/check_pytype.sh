f85d74fece69c4c5b28554a4395e682ddb721816
Esun Kim <veblush@google.com>
2022-01-21 16:38:53 -0800

    Update sanity docker

--- tools/distrib/check_pytype.sh
+++ tools/distrib/check_pytype.sh
@@ -14,6 +14,6 @@
 # limitations under the License.
 
 JOBS=$(nproc) || JOBS=4
-python3 -m pip install pytype==2019.11.27
+python3 -m pip install pytype==2022.1.13
 
 python3 -m pytype --keep-going -j "$JOBS" --strict-import --config "setup.cfg"
