d1cfaa1bfe5f43123877536dafed8fbb481dbe69
Jose Alberto <6731364+jagranados-dev@users.noreply.github.com>
2022-01-27 11:41:21 +0100

    Added SSL greeter client and server

---
+++ examples/cpp/helloworld/greeter_server_ssl.cc
@@ -0,0 +1,117 @@
+/**
+ * Copyright 2022 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+#include <memory>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include "helloworld.grpc.pb.h"
+#endif
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
+
+// Logic and data behind the server's behavior.
+class GreeterServiceImpl final : public Greeter::Service
+{
+  Status SayHello ( ServerContext* context,
+                    const HelloRequest* request,
+                    HelloReply* reply ) override
+  {
+    std::string prefix { "Hello " };
+
+    reply->set_message ( prefix + request->name () );
+
+    return Status::OK;
+  }
+};
+
+void read ( const std::string& filename, std::string& data )
+{
+  std::ifstream file ( filename.c_str (), std::ios::in );
+
+  if ( file.is_open () )
+  {
+    std::stringstream ss;
+    ss << file.rdbuf ();
+
+    file.close ();
+
+    data = ss.str ();
+  }
+}
+
+void run_server ()
+{
+  std::string server_address { "localhost:50051" };
+
+  grpc::EnableDefaultHealthCheckService ( true );
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin ();
+
+  ServerBuilder builder;
+
+  // Use the gen_certs.sh for the generation of required certificates
+  // [!] Be carefull here using a server.crt with the CN != localhost [!]
+  std::string key, cert, root;
+
+  read ( "server.crt", cert );
+  read ( "server.key", key );
+  read ( "ca.crt", root );
+
+  // Configure SSL options
+  grpc::SslServerCredentialsOptions::PemKeyCertPair keycert = { key, cert	};
+  grpc::SslServerCredentialsOptions sslOps;
+  sslOps.pem_root_certs = root;
+  sslOps.pem_key_cert_pairs.push_back ( keycert );
+
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort ( server_address, grpc::SslServerCredentials ( sslOps ));
+
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  GreeterServiceImpl service;
+  builder.RegisterService ( &service );
+
+  // Finally assemble the server.
+  std::unique_ptr < Server > server ( builder.BuildAndStart () );
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait ();
+}
+
+int main ( int, char** )
+{
+  run_server ();
+
+  return 0;
+}
