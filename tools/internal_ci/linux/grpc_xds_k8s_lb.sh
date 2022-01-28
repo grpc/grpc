04ff97af38d5c99d70752d063abb047b845bb7cd
yifeizhuang <zivy@google.com>
2022-01-28 15:51:42 -0800

    Revert "Revert "Add api listener test for k8s (#27534)" (#28719)"
    This reverts commit c35b93f28d204e3d3df06bc4f3ccb856be2cbb22.

--- tools/internal_ci/linux/grpc_xds_k8s_lb.sh
+++ tools/internal_ci/linux/grpc_xds_k8s_lb.sh
@@ -155,7 +155,7 @@ main() {
   cd "${TEST_DRIVER_FULL_DIR}"
   local failed_tests=0
   run_alpha_test subsetting_test || (( failed_tests++ ))
-  test_suites=("change_backend_service_test" "failover_test" "remove_neg_test" "round_robin_test" "affinity_test")
+  test_suites=("api_listener_test" "change_backend_service_test" "failover_test" "remove_neg_test" "round_robin_test" "affinity_test")
   for test in "${test_suites[@]}"; do
     run_test $test || (( failed_tests++ ))
   done
