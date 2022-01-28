012e452f2a4486e48abdaa66ad3d45abd1a6b1c9
Jan Tattermusch <jtattermusch@google.com>
2022-01-28 11:49:11 +0100

    update copyrights as per review comments

--- tools/run_tests/dockerize/build_and_run_docker.sh
+++ tools/run_tests/dockerize/build_and_run_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2015 gRPC authors.
+# Copyright 2016 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
