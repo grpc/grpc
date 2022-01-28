d78de7e0afde5de149bfd745a521aa2d83882f2d
Yash Tibrewal <yashkt@google.com>
2022-01-28 10:55:01 -0800

    Revert "Cleanup run docker machinery in run_tests.py, task_runner.py and elsewhere (#28704)"
    This reverts commit be723121fca2a1055aa4078ffdb3a9f414634769.

--- tools/run_tests/dockerize/docker_run.sh
+++ tools/run_tests/dockerize/docker_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2015 gRPC authors.
+# Copyright 2016 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,13 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# This script is invoked by build_and_run_docker.sh inside a docker
+# This script is invoked by build_docker_* inside a docker
 # container. You should never need to call this script on your own.
 
-set -e
-
-# TODO(jtattermusch): added in https://github.com/grpc/grpc/pull/17303, should be removed.
-export CONFIG=${config:-opt}
+set -ex
 
 if [ "$RELATIVE_COPY_PATH" == "" ]
 then
@@ -35,39 +32,13 @@ else
   cp -r "$EXTERNAL_GIT_ROOT/$RELATIVE_COPY_PATH"/* "/var/local/git/grpc/$RELATIVE_COPY_PATH"
 fi
 
-# ensure the "reports" directory exists
-mkdir -p reports
-
-# TODO(jtattermusch): this is garbage, remove it.
 $POST_GIT_STEP
 
-# TODO(jtattermusch): is this necessary?
+if [ -x "$(command -v rvm)" ]
+then
+  rvm use ruby-2.1
+fi
+
 cd /var/local/git/grpc
 
-exit_code=0
-${DOCKER_RUN_SCRIPT_COMMAND} || exit_code=$?
-
-# copy reports/ dir and files matching one of the patterns to the well-known
-# location of report dir mounted to the docker container.
-# --parents preserves the directory structure for files matched by find.
-cp -r reports/ /var/local/report_dir
-find . -name report.xml -exec cp --parents {} /var/local/report_dir \;
-find . -name sponge_log.xml -exec cp --parents {} /var/local/report_dir \;
-find . -name 'report_*.xml' -exec cp --parents {} /var/local/report_dir \;
-chmod -R ugo+r /var/local/report_dir || true
-
-# Move contents of OUTPUT_DIR from under the workspace to a directory that will be visible to the docker host.
-if [ "${OUTPUT_DIR}" != "" ]
-then
-  # create the directory if it doesn't exist yet.
-  mkdir -p "${OUTPUT_DIR}"
-  mv "${OUTPUT_DIR}" /var/local/output_dir || exit_code=$?
-  chmod -R ugo+r /var/local/output_dir || true
-fi
-
-if [ -x "$(command -v ccache)" ]
-then
-  ccache --show-stats || true
-fi
-
-exit $exit_code
+$RUN_COMMAND
