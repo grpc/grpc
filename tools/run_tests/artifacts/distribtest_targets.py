c8c1774ec465ed5ed26a8e856046ec75fc199eeb
Jan Tattermusch <jtattermusch@users.noreply.github.com>
2022-02-01 11:33:56 +0100

    Revert "Revert "Cleanup run docker machinery in run_tests.py, task_runner.py and elsewhere (#28704)" (#28741)" (#28748)
    * Revert "Revert "Cleanup run docker machinery in run_tests.py, task_runner.py and elsewhere (#28704)" (#28741)"
    
    This reverts commit 9a79d44e9b60bdbfda78fe1b1fbe8ee86f14c1d4.
    
    * fix python distribtest failure on fedora34

--- tools/run_tests/artifacts/distribtest_targets.py
+++ tools/run_tests/artifacts/distribtest_targets.py
@@ -31,7 +31,6 @@ def create_docker_jobspec(name,
                           timeout_seconds=30 * 60):
     """Creates jobspec for a task running under docker."""
     environ = environ.copy()
-    environ['RUN_COMMAND'] = shell_command
     # the entire repo will be cloned if copy_rel_path is not set.
     if copy_rel_path:
         environ['RELATIVE_COPY_PATH'] = copy_rel_path
@@ -41,7 +40,8 @@ def create_docker_jobspec(name,
         docker_args += ['-e', '%s=%s' % (k, v)]
     docker_env = {
         'DOCKERFILE_DIR': dockerfile_dir,
-        'DOCKER_RUN_SCRIPT': 'tools/run_tests/dockerize/docker_run.sh'
+        'DOCKER_RUN_SCRIPT': 'tools/run_tests/dockerize/docker_run.sh',
+        'DOCKER_RUN_SCRIPT_COMMAND': shell_command,
     }
     jobspec = jobset.JobSpec(
         cmdline=['tools/run_tests/dockerize/build_and_run_docker.sh'] +
