475583f7ed0cb7c8a4556d12d63a0f2c3e5ab599
AJ Heller <hork@google.com>
2022-02-04 22:26:52 +0000

    rm DNS

--- test/core/util/run_with_poller.sh
+++ test/core/util/run_with_poller.sh
@@ -12,7 +12,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 set -ex
-echo "DO NOT SUBMIT - '$@'"
 "$@"
