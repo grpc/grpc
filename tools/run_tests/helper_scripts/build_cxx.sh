0ec0479ded1f8c291610bee0f6a5771e416edfc2
Jan Tattermusch <jtattermusch@users.noreply.github.com>
2022-02-08 18:41:56 +0100

    run_tests.py cleanup and simplification (#28808)
    * only support cmake for CLanguage in run_tests.py
    
    * add support for run_tests.py build step environ
    
    * switch C/C++ run_tests.py build to build_cxx script
    
    * CLanguage cleanup
    
    * build C# entirely with build_csharp script
    
    * move entire PHP build to build_php.sh
    
    * fixup C# build on linux and mac
    
    * run_dep_checks Makefile target is deprecated
    
    * get rid of the "makefile" logic in run_tests.py
    
    * fixup C# build on linux and mac
    
    * XML_REPORT env variable is useless for --use_docker runs
    
    * add a TODO
    
    * move "main" functionality towards end of run_tests.py
    
    * use self.args instead of global
    
    * yapf format
    
    * remove the no longer useful --update_submodules features of run_tests.py
    
    * fix check_epollexclusive check in run_tests.py

--- tools/run_tests/helper_scripts/build_cxx.sh
+++ tools/run_tests/helper_scripts/build_cxx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2015 gRPC authors.
+# Copyright 2022 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,3 +22,6 @@ cd cmake/build
 
 # MSBUILD_CONFIG's values are suitable for cmake as well
 cmake -DgRPC_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE="${MSBUILD_CONFIG}" "$@" ../..
+
+# GRPC_RUN_TESTS_CXX_LANGUAGE_SUFFIX will be set to either "c" or "cxx"
+make -j"${GRPC_RUN_TESTS_JOBS}" "buildtests_${GRPC_RUN_TESTS_CXX_LANGUAGE_SUFFIX}" "tools_${GRPC_RUN_TESTS_CXX_LANGUAGE_SUFFIX}" "check_epollexclusive"
