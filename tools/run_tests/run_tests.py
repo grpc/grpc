426c2c341f47bfc6414c022fa630c59f2727c47c
Jan Tattermusch <jtattermusch@google.com>
2022-02-09 11:07:19 +0100

    make cmake_ninja_vs2015 build the default for C/C++

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -252,27 +252,32 @@ class CLanguage(object):
             _check_compiler(self.args.compiler, [
                 'default',
                 'cmake',
+                'cmake_ninja_vs2015',
+                'cmake_ninja_vs2017',
                 'cmake_vs2015',
                 'cmake_vs2017',
                 'cmake_vs2019',
-                'cmake_ninja_vs2015',
-                'cmake_ninja_vs2017',
             ])
             _check_arch(self.args.arch, ['default', 'x64', 'x86'])
 
             activate_vs_tools = ''
-            if self.args.compiler == 'cmake_vs2019':
-                cmake_generator = 'Visual Studio 16 2019'
-            elif self.args.compiler == 'cmake_vs2017':
-                cmake_generator = 'Visual Studio 15 2017'
-            elif self.args.compiler == 'cmake_ninja_vs2015':
+            if self.args.compiler == 'cmake_ninja_vs2015' or self.args.compiler == 'cmake' or self.args.compiler == 'default':
+                # cmake + ninja build is the default because it is faster and supports boringssl assembly optimizations
+                # the compiler used is exactly the same as for cmake_vs2015
                 cmake_generator = 'Ninja'
                 activate_vs_tools = '2015'
             elif self.args.compiler == 'cmake_ninja_vs2017':
                 cmake_generator = 'Ninja'
                 activate_vs_tools = '2017'
-            else:
+            elif self.args.compiler == 'cmake_vs2015':
                 cmake_generator = 'Visual Studio 14 2015'
+            elif self.args.compiler == 'cmake_vs2017':
+                cmake_generator = 'Visual Studio 15 2017'
+            elif self.args.compiler == 'cmake_vs2019':
+                cmake_generator = 'Visual Studio 16 2019'
+            else:
+                print('should never reach here.')
+                sys.exit(1)
 
             self._cmake_configure_extra_args = []
             self._cmake_generator_windows = cmake_generator
