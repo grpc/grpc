9d3acabcf629989f8beff2775ce0ef70b07865a5
Jan Tattermusch <jtattermusch@google.com>
2022-02-09 11:04:09 +0100

    support building C/C++ with ninja

--- tools/run_tests/helper_scripts/build_cxx.bat
+++ tools/run_tests/helper_scripts/build_cxx.bat
@@ -21,10 +21,43 @@ cd cmake
 mkdir build
 cd build
 
-cmake -DgRPC_BUILD_TESTS=ON %* ../.. || goto :error
+If "%GRPC_BUILD_ACTIVATE_VS_TOOLS%" == "2015" (
+  @rem set cl.exe build environment to build with VS2015 tooling
+  @rem this is required for Ninja build to work
+  call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" %GRPC_BUILD_VS_TOOLS_ARCHITECTURE%
+  @rem restore command echo
+  echo on
+)
 
-@rem GRPC_RUN_TESTS_CXX_LANGUAGE_SUFFIX will be set to either "c" or "cxx"
-cmake --build . --target buildtests_%GRPC_RUN_TESTS_CXX_LANGUAGE_SUFFIX% --config %MSBUILD_CONFIG% || goto :error
+@rem Workaround a bug where VS150COMNTOOLS is not set due to a bug in VS 2017 installer
+@rem see https://developercommunity.visualstudio.com/t/installing-visualstudio-build-tools-doesnt-add-env/17435
+set "VS150COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\"
+
+If "%GRPC_BUILD_ACTIVATE_VS_TOOLS%" == "2017" (
+  @rem set cl.exe build environment to build with VS2017 tooling
+  @rem this is required for Ninja build to work
+  call "%VS150COMNTOOLS%..\..\VC\vcvarsall.bat" %GRPC_BUILD_VS_TOOLS_ARCHITECTURE%
+  @rem restore command echo
+  echo on
+)
+
+If "%GRPC_CMAKE_GENERATOR%" == "Ninja" (
+  @rem Use ninja
+
+  @rem Select MSVC compiler (cl.exe) explicitly to make sure we don't end up gcc from mingw or cygwin
+  @rem (both are on path in kokoro win workers)
+  cmake -G "%GRPC_CMAKE_GENERATOR%" -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" -DgRPC_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE="%MSBUILD_CONFIG%" %* ../.. || goto :error
+
+  ninja -j%GRPC_RUN_TESTS_JOBS% buildtests_%GRPC_RUN_TESTS_CXX_LANGUAGE_SUFFIX% || goto :error
+
+) else (
+  @rem Use one of the Visual Studio generators.
+
+  cmake -G "%GRPC_CMAKE_GENERATOR%" -A "%GRPC_CMAKE_ARCHITECTURE%" -DgRPC_BUILD_TESTS=ON -DgRPC_BUILD_MSVC_MP_COUNT=%GRPC_RUN_TESTS_JOBS% %* ../.. || goto :error
+
+  @rem GRPC_RUN_TESTS_CXX_LANGUAGE_SUFFIX will be set to either "c" or "cxx"
+  cmake --build . --target buildtests_%GRPC_RUN_TESTS_CXX_LANGUAGE_SUFFIX% --config %MSBUILD_CONFIG% || goto :error
+)
 
 endlocal
 
