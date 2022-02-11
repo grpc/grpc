5fad8858edb5a85e63dbb55a89191f2f8a527cdf
Jan Tattermusch <jtattermusch@google.com>
2022-02-11 10:18:26 +0100

    set --inner_jobs for windows grpc_build_artifact job

--- tools/internal_ci/windows/grpc_build_artifacts.bat
+++ tools/internal_ci/windows/grpc_build_artifacts.bat
@@ -29,8 +29,7 @@ set PREPARE_BUILD_INSTALL_DEPS_CSHARP=true
 set PREPARE_BUILD_INSTALL_DEPS_PYTHON=true
 call tools/internal_ci/helper_scripts/prepare_build_windows.bat || exit /b 1
 
-@rem TODO(jtattermusch): set --inner_jobs args
-python tools/run_tests/task_runner.py -f artifact windows %TASK_RUNNER_EXTRA_FILTERS% -j 4
+python tools/run_tests/task_runner.py -f artifact windows %TASK_RUNNER_EXTRA_FILTERS% -j 4 --inner_jobs 4
 set RUNTESTS_EXITCODE=%errorlevel%
 
 bash tools/internal_ci/helper_scripts/store_artifacts_from_moved_src_tree.sh
