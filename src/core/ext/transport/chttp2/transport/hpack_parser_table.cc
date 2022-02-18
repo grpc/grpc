3af51f8b78d3fbcf0892b64e0a8c1ed9dc7c3a42
Craig Tiller <ctiller@google.com>
2022-02-17 22:05:54 -0800

    fix

--- src/core/ext/transport/chttp2/transport/hpack_parser_table.cc
+++ src/core/ext/transport/chttp2/transport/hpack_parser_table.cc
@@ -218,9 +218,9 @@ GPR_ATTRIBUTE_NOINLINE HPackTable::Memento MakeMemento(size_t i) {
   return grpc_metadata_batch::Parse(
       sm.key, Slice::FromStaticString(sm.value),
       strlen(sm.key) + strlen(sm.value) + hpack_constants::kEntryOverhead,
-      [sm](absl::string_view, const Slice&) {
+      [i](absl::string_view, const Slice&) {
         // We know this will be invalid, but we don't care.
-        if (sm.key == ":method" && sm.value == "GET") return;
+        if (i == 1) return;
         abort();  // not expecting to see this
       });
 }
