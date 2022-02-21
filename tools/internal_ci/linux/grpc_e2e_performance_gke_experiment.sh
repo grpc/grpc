c9c5fe392580e212b5b971d6c9c3b4f11bff9237
Paulo Castello da Costa <pcastello@google.com>
2022-02-20 22:29:55 -0800

    Pin test-infra version to v1.0.0.

--- tools/internal_ci/linux/grpc_e2e_performance_gke_experiment.sh
+++ tools/internal_ci/linux/grpc_e2e_performance_gke_experiment.sh
@@ -63,11 +63,12 @@ TEST_INFRA_GOVERSION=go1.17.1
 go get "golang.org/dl/${TEST_INFRA_GOVERSION}"
 "${TEST_INFRA_GOVERSION}" download
 
-# Note: Submodules are not required for tools build.
-pushd ..
-git clone --depth 1 https://github.com/grpc/test-infra.git
-cd test-infra
-git log -1 --oneline
+# Fetch test-infra repository and build all tools.
+git init -b master ../test-infra
+pushd ../test-infra
+# Tools are built from HEAD.
+git fetch --depth 1 https://github.com/grpc/test-infra.git
+git checkout --detach FETCH_HEAD
 make GOCMD="${TEST_INFRA_GOVERSION}" all-tools
 popd
 
