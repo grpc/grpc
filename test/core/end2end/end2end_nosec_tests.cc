be70a8676c994de94f1d859b43bc732ac1a6481c
Mark D. Roth <roth@google.com>
2022-02-22 12:56:36 -0800

    Revert "Third attempt: Implement transparent retries (#28925)" (#28942)
    This reverts commit 9e7453409a31ed4ffcc103eeb16249c756dbf077.

--- test/core/end2end/end2end_nosec_tests.cc
+++ test/core/end2end/end2end_nosec_tests.cc
@@ -177,12 +177,6 @@ extern void retry_throttled(grpc_end2end_test_config config);
 extern void retry_throttled_pre_init(void);
 extern void retry_too_many_attempts(grpc_end2end_test_config config);
 extern void retry_too_many_attempts_pre_init(void);
-extern void retry_transparent_goaway(grpc_end2end_test_config config);
-extern void retry_transparent_goaway_pre_init(void);
-extern void retry_transparent_max_concurrent_streams(grpc_end2end_test_config config);
-extern void retry_transparent_max_concurrent_streams_pre_init(void);
-extern void retry_transparent_not_sent_on_wire(grpc_end2end_test_config config);
-extern void retry_transparent_not_sent_on_wire_pre_init(void);
 extern void retry_unref_before_finish(grpc_end2end_test_config config);
 extern void retry_unref_before_finish_pre_init(void);
 extern void retry_unref_before_recv(grpc_end2end_test_config config);
@@ -289,9 +283,6 @@ void grpc_end2end_tests_pre_init(void) {
   retry_streaming_succeeds_before_replay_finished_pre_init();
   retry_throttled_pre_init();
   retry_too_many_attempts_pre_init();
-  retry_transparent_goaway_pre_init();
-  retry_transparent_max_concurrent_streams_pre_init();
-  retry_transparent_not_sent_on_wire_pre_init();
   retry_unref_before_finish_pre_init();
   retry_unref_before_recv_pre_init();
   server_finishes_request_pre_init();
@@ -390,9 +381,6 @@ void grpc_end2end_tests(int argc, char **argv,
     retry_streaming_succeeds_before_replay_finished(config);
     retry_throttled(config);
     retry_too_many_attempts(config);
-    retry_transparent_goaway(config);
-    retry_transparent_max_concurrent_streams(config);
-    retry_transparent_not_sent_on_wire(config);
     retry_unref_before_finish(config);
     retry_unref_before_recv(config);
     server_finishes_request(config);
@@ -705,18 +693,6 @@ void grpc_end2end_tests(int argc, char **argv,
     }
     if (0 == strcmp("retry_too_many_attempts", argv[i])) {
       retry_too_many_attempts(config);
-      continue;
-    }
-    if (0 == strcmp("retry_transparent_goaway", argv[i])) {
-      retry_transparent_goaway(config);
-      continue;
-    }
-    if (0 == strcmp("retry_transparent_max_concurrent_streams", argv[i])) {
-      retry_transparent_max_concurrent_streams(config);
-      continue;
-    }
-    if (0 == strcmp("retry_transparent_not_sent_on_wire", argv[i])) {
-      retry_transparent_not_sent_on_wire(config);
       continue;
     }
     if (0 == strcmp("retry_unref_before_finish", argv[i])) {
