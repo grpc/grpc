b06b44aeab17f2e940e310eb627090520b0511fd
Benjamin A. Beasley <code@musicinmybrain.net>
2022-02-23 13:07:56 -0500

    Better express intent in EvaluateArgsTest.EmptyMetadata
    Instead of comparing for equality with an empty string_view, evaluate
    the IsEmpty() predicate.

--- test/core/security/evaluate_args_test.cc
+++ test/core/security/evaluate_args_test.cc
@@ -32,9 +32,9 @@ class EvaluateArgsTest : public ::testing::Test {
 
 TEST_F(EvaluateArgsTest, EmptyMetadata) {
   EvaluateArgs args = util_.MakeEvaluateArgs();
-  EXPECT_EQ(args.GetPath(), std::string_view());
-  EXPECT_EQ(args.GetMethod(), std::string_view());
-  EXPECT_EQ(args.GetAuthority(), std::string_view());
+  EXPECT_THAT(args.GetPath(), ::testing::IsEmpty());
+  EXPECT_THAT(args.GetMethod(), ::testing::IsEmpty());
+  EXPECT_THAT(args.GetAuthority(), ::testing::IsEmpty());
   EXPECT_EQ(args.GetHeaderValue("some_key", nullptr), absl::nullopt);
 }
 
