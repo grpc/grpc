6daaee6847bf976888fed58fe69d250a134181d1
yifeizhuang <zivy@google.com>
2022-03-04 14:55:46 -0800

    ./bin/isort.sh

--- tools/run_tests/xds_k8s_test_driver/tests/url_map/metadata_filter_test.py
+++ tools/run_tests/xds_k8s_test_driver/tests/url_map/metadata_filter_test.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import logging
 import json
+import logging
 from typing import Tuple
 
 from absl import flags
