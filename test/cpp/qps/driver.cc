e85bb8c262fb0d75c5ce6fe34a1489b0f14473ee
Wanlin Du <wanlindu@google.com>
2022-03-09 16:31:53 -0800

    Update the scenario results to include start and end time.

--- test/cpp/qps/driver.cc
+++ test/cpp/qps/driver.cc
@@ -24,6 +24,7 @@
 #include <thread>
 #include <unordered_map>
 #include <vector>
+#include <chrono>
 
 #include <grpc/support/alloc.h>
 #include <grpc/support/log.h>
@@ -573,6 +574,11 @@ std::unique_ptr<ScenarioResult> RunScenario(
 
   // Start a run
   gpr_log(GPR_INFO, "Starting");
+
+  std::chrono::seconds start_epoch_time =
+        std::chrono::duration_cast<std::chrono::seconds>(
+            std::chrono::system_clock::now().time_since_epoch());
+
   for (size_t i = 0; i < num_servers; i++) {
     auto server = &servers[i];
     if (!server->stream->Write(server_mark)) {
@@ -623,6 +629,10 @@ std::unique_ptr<ScenarioResult> RunScenario(
   // the result.
   bool client_finish_first =
       (client_config.rpc_type() != STREAMING_FROM_SERVER);
+  
+  std::chrono::seconds end_epoch_time =
+        std::chrono::duration_cast<std::chrono::seconds>(
+            std::chrono::system_clock::now().time_since_epoch());
 
   FinishClients(clients, client_mark);
 
@@ -650,6 +660,11 @@ std::unique_ptr<ScenarioResult> RunScenario(
     rrc->set_status_code(it->first);
     rrc->set_count(it->second);
   }
+  
+  // Fill in start and end time for the test scenario
+  result->mutable_summary()->set_start(start_epoch_time.count());
+  result->mutable_summary()->set_end(end_epoch_time.count());
+
   postprocess_scenario_result(result.get());
   return result;
 }
