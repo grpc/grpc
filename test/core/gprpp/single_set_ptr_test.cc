3129756b1c0eba4773bf4aefe09e5601c9e2f5c2
Craig Tiller <ctiller@google.com>
2022-03-16 08:24:18 -0700

    fix

--- test/core/gprpp/single_set_ptr_test.cc
+++ test/core/gprpp/single_set_ptr_test.cc
@@ -45,6 +45,7 @@ TEST(SingleSetPtrTest, CanReset) {
 TEST(SingleSetPtrTest, LotsOfSetters) {
   SingleSetPtr<int> p;
   std::vector<std::thread> threads;
+  threads.reserve(100);
   for (int i = 0; i < 100; i++) {
     threads.emplace_back([&p, i]() { p.Set(new int(i)); });
   }
