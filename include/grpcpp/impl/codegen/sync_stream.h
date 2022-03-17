ea10781bc200d74bc0d987993cfb439eb0115930
Craig Tiller <ctiller@google.com>
2022-03-17 15:41:25 -0700

    remove spurious change

--- include/grpcpp/impl/codegen/sync_stream.h
+++ include/grpcpp/impl/codegen/sync_stream.h
@@ -20,8 +20,6 @@
 
 // IWYU pragma: private, include <grpcpp/support/sync_stream.h>
 
-#include "call_op_set.h"
-
 #include <grpcpp/impl/codegen/call.h>
 #include <grpcpp/impl/codegen/channel_interface.h>
 #include <grpcpp/impl/codegen/client_context.h>
