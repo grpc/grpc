624aeadeb18a2bbfddf4bfdf687df9d4a745a75e
AJ Heller <hork@google.com>
2022-03-24 15:53:48 +0000

    iOS fix

--- src/objective-c/tests/CronetTests/CoreCronetEnd2EndTests.mm
+++ src/objective-c/tests/CronetTests/CoreCronetEnd2EndTests.mm
@@ -192,8 +192,9 @@ static char *roots_filename;
 }
 
 - (void)testIndividualCase:(char *)test_case {
+  char *argv[] = {(char *)"h2_ssl", test_case};
   for (int i = 0; i < sizeof(configs) / sizeof(*configs); i++) {
-    grpc_end2end_tests(2, {(char *)"h2_ssl", test_case}, configs[i]);
+    grpc_end2end_tests(2, argv, configs[i]);
   }
 }
 
