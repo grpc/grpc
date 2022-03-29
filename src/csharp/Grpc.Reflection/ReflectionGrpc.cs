44f2c004c5f9c88ed4beeadf2587f1810ceebb2b
Mahdi K. Fard <xclud@users.noreply.github.com>
2022-03-29 18:03:57 +0430

    Fix double-space in xmldoc generation. (#29224)
    * Fix double-space in xmldoc generation.
    
    * regenerate C# protos
    
    Co-authored-by: Jan Tattermusch <jtattermusch@google.com>

--- src/csharp/Grpc.Reflection/ReflectionGrpc.cs
+++ src/csharp/Grpc.Reflection/ReflectionGrpc.cs
@@ -169,7 +169,7 @@ namespace Grpc.Reflection.V1Alpha {
           .AddMethod(__Method_ServerReflectionInfo, serviceImpl.ServerReflectionInfo).Build();
     }
 
-    /// <summary>Register service method with a service binder with or without implementation. Useful when customizing the  service binding logic.
+    /// <summary>Register service method with a service binder with or without implementation. Useful when customizing the service binding logic.
     /// Note: this method is part of an experimental API that can change or be removed without any prior notice.</summary>
     /// <param name="serviceBinder">Service methods will be bound by calling <c>AddMethod</c> on this object.</param>
     /// <param name="serviceImpl">An object implementing the server-side handling logic.</param>
