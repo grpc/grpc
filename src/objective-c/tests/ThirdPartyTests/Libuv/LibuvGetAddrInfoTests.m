7f86b011a0f5de2f5242a7488daa37c5dc6b9c88
Hannah Shi <hannahshisfb@gmail.com>
2022-03-29 08:11:43 -0700

    comment address

--- src/objective-c/tests/ThirdPartyTests/Libuv/LibuvGetAddrInfoTests.m
+++ src/objective-c/tests/ThirdPartyTests/Libuv/LibuvGetAddrInfoTests.m
@@ -1,3 +1,21 @@
+/*
+ *
+ * Copyright 2021 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #import <XCTest/XCTest.h>
 
 #import "test/runner.h"
@@ -21,11 +39,11 @@ TEST_ENTRY(getaddrinfo_basic_sync)
 TEST_ENTRY(getaddrinfo_concurrent)
 TASK_LIST_END
 
-@interface LibuvGetAddrInfoTests2 : XCTestCase
+@interface LibuvGetAddrInfoTests : XCTestCase
 
 @end
 
-@implementation LibuvGetAddrInfoTests2
+@implementation LibuvGetAddrInfoTests
 
 - (void)testGetAddrInfoAll {
   for (task_entry_t* task = TEST_TASKS; task->main; task++) {
