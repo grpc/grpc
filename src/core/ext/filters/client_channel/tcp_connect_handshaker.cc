de0859e3e45c93e03ca3c7cbe2a710a28fdf3e64
Anirudh Ramachandra <anramach@google.com>
2022-04-04 09:50:24 -0700

    Clang format

--- src/core/ext/filters/client_channel/tcp_connect_handshaker.cc
+++ src/core/ext/filters/client_channel/tcp_connect_handshaker.cc
@@ -155,7 +155,7 @@ void TCPConnectHandshaker::Connected(void* arg, grpc_error_handle error) {
       }
       return;
     }
-    
+
     GPR_ASSERT(self->endpoint_ != nullptr);
     self->args_->endpoint = self->endpoint_;
     self->endpoint_ = nullptr;
