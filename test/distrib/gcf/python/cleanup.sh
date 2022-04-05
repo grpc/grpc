7dde9a7a246177bb603144a0d9b1f023403eda5e
Richard Belleville <rbellevi@google.com>
2022-04-05 10:10:35 -0700

    That copyright script doesn't work with shebangs

--- test/distrib/gcf/python/cleanup.sh
+++ test/distrib/gcf/python/cleanup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright 2022 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/bash
 
 # Shellcheck cant find the included file.
 # shellcheck disable=SC1091
