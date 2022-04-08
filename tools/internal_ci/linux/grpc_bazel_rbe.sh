5756eb18e5b048c026d7a7a9b784e525ac5fdba9
Jan Tattermusch <jtattermusch@google.com>
2022-04-08 16:38:48 +0200

    use move_src_tree_and_respawn_itself_rc for bazel RBE tests on linux

--- tools/internal_ci/linux/grpc_bazel_rbe.sh
+++ tools/internal_ci/linux/grpc_bazel_rbe.sh
@@ -15,6 +15,9 @@
 
 set -ex
 
+# avoid slow finalization after the script has exited.
+source $(dirname $0)/../../../tools/internal_ci/helper_scripts/move_src_tree_and_respawn_itself_rc
+
 # change to grpc repo root
 cd $(dirname $0)/../../..
 
