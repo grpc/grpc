1988ebe6547df3a6b45d46a495ff5845cc9740d2
Benjamin A. Beasley <code@musicinmybrain.net>
2022-04-08 15:34:49 -0400

    Do not segfault when peer CN is absent
    In HostNameCertificateVerifier::Verify, do not use the
    peer_info->common_name if it is a null pointer.

--- src/core/lib/security/credentials/tls/grpc_tls_certificate_verifier.cc
+++ src/core/lib/security/credentials/tls/grpc_tls_certificate_verifier.cc
@@ -142,7 +142,8 @@ bool HostNameCertificateVerifier::Verify(
     const char* common_name = request->peer_info.common_name;
     // We are using the target name sent from the client as a matcher to match
     // against identity name on the peer cert.
-    if (VerifySubjectAlternativeName(common_name, std::string(target_host))) {
+    if (common_name != nullptr &&
+        VerifySubjectAlternativeName(common_name, std::string(target_host))) {
       return true;  // synchronous check
     }
   }
