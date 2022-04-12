2a0d6db29a87eadbc0256b7f07d5d98ad263bb24
Vignesh Babu <vigneshbabu@google.com>
2022-04-12 23:08:57 +0000

    trying again

--- src/core/lib/resource_quota/memory_quota.cc
+++ src/core/lib/resource_quota/memory_quota.cc
@@ -400,8 +400,7 @@ void BasicMemoryQuota::Start() {
           std::tuple<const char*, RefCountedPtr<ReclaimerQueue::Handle>> arg) {
         auto reclaimer = std::move(std::get<1>(arg));
         if (GRPC_TRACE_FLAG_ENABLED(grpc_resource_quota_trace)) {
-          double free =
-              std::max(0L, reinterpret_cast<long>(self->free_bytes_.load()));
+          double free = std::max(intptr_t(0), self->free_bytes_.load());
           size_t quota_size = self->quota_size_.load();
           gpr_log(GPR_INFO,
                   "RQ: %s perform %s reclamation. Available free bytes: %f, "
@@ -465,7 +464,7 @@ void BasicMemoryQuota::FinishReclamation(uint64_t token, Waker waker) {
                                                    std::memory_order_relaxed,
                                                    std::memory_order_relaxed)) {
     if (GRPC_TRACE_FLAG_ENABLED(grpc_resource_quota_trace)) {
-      double free = std::max(0L, reinterpret_cast<long>(free_bytes_.load()));
+      double free = std::max(intptr_t(0), free_bytes_.load());
       size_t quota_size = quota_size_.load();
       gpr_log(GPR_INFO,
               "RQ: %s reclamation complete. Available free bytes: %f, "
