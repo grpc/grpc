93d79a82bbef14844900c61353d07b2d522f6115
Yash Tibrewal <yashkt@google.com>
2022-04-15 15:01:10 -0700

    clang-format

--- src/core/ext/filters/http/server/http_server_filter.cc
+++ src/core/ext/filters/http/server/http_server_filter.cc
@@ -106,7 +106,8 @@ static grpc_error_handle hs_filter_incoming_metadata(grpc_call_element* elem,
       case grpc_core::HttpMethodMetadata::kPost:
         break;
       case grpc_core::HttpMethodMetadata::kPut:
-        if (static_cast<channel_data*>(elem->channel_data)->allow_put_requests) {
+        if (static_cast<channel_data*>(elem->channel_data)
+                ->allow_put_requests) {
           break;
         }
         ABSL_FALLTHROUGH_INTENDED;
