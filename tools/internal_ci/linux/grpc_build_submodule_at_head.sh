a628ab4a83a2a9b12f8cd2be0b107af3e63a3743
Lidi Zheng <lidiz@google.com>
2022-04-18 15:23:10 -0700

    Use the macOS approach

--- tools/internal_ci/linux/grpc_build_submodule_at_head.sh
+++ tools/internal_ci/linux/grpc_build_submodule_at_head.sh
@@ -20,7 +20,7 @@ set -ex
 # change to grpc repo root
 cd $(dirname $0)/../../..
 
-export GRPC_BUILD_COMPILE_PYTHON=TRUE
+export PREPARE_BUILD_INSTALL_DEPS_PYTHON=true
 source tools/internal_ci/helper_scripts/prepare_build_linux_rc
 
 # Submodule name is passed as the RUN_TESTS_FLAGS variable
