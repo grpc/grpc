85999770fb06da9844408d5595bf1318cd4d662d
AJ Heller <hork@google.com>
2022-04-18 21:02:11 +0000

    Remove unmatched grpc_init from *ChannelFromFd methods
    These unmatched grpc_init calls were preventing grpc_shutdown from
    completing, which notably showed up in the only tests we have that
    exercise these methods: `client_interceptors_end2end_tests.cc`.
    
    These inits appear to be unnecessary, so I removed them. But if I've
    missed some reason they're needed, an alternative is to find the right
    place to add the corresponding `init_lib.shutdown();` call. I'm not sure
    where that would be at this point.
    
    See b/175634383

--- src/cpp/client/create_channel_posix.cc
+++ src/cpp/client/create_channel_posix.cc
@@ -34,8 +34,6 @@ class ChannelArguments;
 
 std::shared_ptr<Channel> CreateInsecureChannelFromFd(const std::string& target,
                                                      int fd) {
-  grpc::internal::GrpcLibrary init_lib;
-  init_lib.init();
   grpc_channel_credentials* creds = grpc_insecure_credentials_create();
   auto channel = CreateChannelInternal(
       "", grpc_channel_create_from_fd(target.c_str(), fd, creds, nullptr),
@@ -47,8 +45,6 @@ std::shared_ptr<Channel> CreateInsecureChannelFromFd(const std::string& target,
 
 std::shared_ptr<Channel> CreateCustomInsecureChannelFromFd(
     const std::string& target, int fd, const grpc::ChannelArguments& args) {
-  internal::GrpcLibrary init_lib;
-  init_lib.init();
   grpc_channel_args channel_args;
   args.SetChannelArgs(&channel_args);
   grpc_channel_credentials* creds = grpc_insecure_credentials_create();
@@ -67,8 +63,6 @@ std::shared_ptr<Channel> CreateCustomInsecureChannelWithInterceptorsFromFd(
     std::vector<
         std::unique_ptr<grpc::experimental::ClientInterceptorFactoryInterface>>
         interceptor_creators) {
-  grpc::internal::GrpcLibrary init_lib;
-  init_lib.init();
   grpc_channel_args channel_args;
   args.SetChannelArgs(&channel_args);
   grpc_channel_credentials* creds = grpc_insecure_credentials_create();
