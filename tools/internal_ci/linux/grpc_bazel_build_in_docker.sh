84292802b735f38cab21222040c48b2d948b3ec5
Jan Tattermusch <jtattermusch@google.com>
2022-04-22 11:18:55 +0200

    additional cleanup & improvements

--- tools/internal_ci/linux/grpc_bazel_build_in_docker.sh
+++ tools/internal_ci/linux/grpc_bazel_build_in_docker.sh
@@ -18,22 +18,28 @@
 # For now //examples/android/binder/ are excluded because it needs Android
 # SDK/NDK to be installed to build
 python3 tools/run_tests/python_utils/bazel_report_helper.py --report_path bazel_build_with_strict_warnings
-bazel_build_with_strict_warnings/bazel_wrapper build --define=use_strict_warning=true \
-	-- \
-	:all \
-	//src/core/... \
-	//src/compiler/... \
-	//test/... \
-	//examples/... \
-	-//examples/android/binder/...
+bazel_build_with_strict_warnings/bazel_wrapper \
+  --bazelrc=tools/remote_build/include/test_locally_with_resultstore_results.bazelrc \
+  build \
+  --define=use_strict_warning=true \
+  -- \
+  :all \
+  //src/core/... \
+  //src/compiler/... \
+  //test/... \
+  //examples/... \
+  -//examples/android/binder/...
 
 # TODO(veblush): Remove this test after migration to abseil-status is done.
 python3 tools/run_tests/python_utils/bazel_report_helper.py --report_path bazel_build_with_abseil_status
-bazel_build_with_abseil_status/bazel_wrapper build --define=use_strict_warning=true --define=use_abseil_status=true \
-	-- \
-	//src/core/... \
-	//src/compiler/... \
-	//test/...
+bazel_build_with_abseil_status/bazel_wrapper \
+  --bazelrc=tools/remote_build/include/test_locally_with_resultstore_results.bazelrc \
+  build \
+  --define=use_strict_warning=true --define=use_abseil_status=true \
+  -- \
+  //src/core/... \
+  //src/compiler/... \
+  //test/...
 
 # TODO(jtattersmusch): Adding a build here for --define=grpc_no_xds is not ideal
 # and we should find a better place for this. Refer
@@ -46,6 +52,6 @@ EXIT_CODE=0
 bazel build //test/cpp/end2end/xds:xds_end2end_test --define=grpc_no_xds=true || EXIT_CODE=$?
 if [ $EXIT_CODE -eq 0 ]
 then
-	echo "Building xds_end2end_test succeeded even with --define=grpc_no_xds=true"
-	exit 1
+  echo "Building xds_end2end_test succeeded even with --define=grpc_no_xds=true"
+  exit 1
 fi
--- tools/internal_ci/linux/grpc_binder_transport_apk_build_in_docker.sh
+++ tools/internal_ci/linux/grpc_binder_transport_apk_build_in_docker.sh
@@ -24,7 +24,10 @@ echo $ANDROID_NDK_HOME
 # defined as int due to an accident).
 # The python option is for disabling python2 enforcement when packing APK
 python3 tools/run_tests/python_utils/bazel_report_helper.py --report_path bazel_binder_example_app
-bazel_binder_example_app/bazel_wrapper build --define=use_strict_warning=true \
+bazel_binder_example_app/bazel_wrapper \
+  --bazelrc=tools/remote_build/include/test_locally_with_resultstore_results.bazelrc \
+  build \
+  --define=use_strict_warning=true \
   --fat_apk_cpu=x86_64,arm64-v8a \
   --extra_toolchains=@rules_python//python:autodetecting_toolchain_nonstrict \
   //examples/android/binder/java/io/grpc/binder/cpp/exampleclient:app \
@@ -33,5 +36,8 @@ bazel_binder_example_app/bazel_wrapper build --define=use_strict_warning=true \
 # Make sure the Java code that will be invoked by binder transport
 # implementation builds
 python3 tools/run_tests/python_utils/bazel_report_helper.py --report_path bazel_binder_connection_helper
-bazel_binder_connection_helper/bazel_wrapper build --define=use_strict_warning=true \
+bazel_binder_connection_helper/bazel_wrapper \
+  --bazelrc=tools/remote_build/include/test_locally_with_resultstore_results.bazelrc \
+  build \
+  --define=use_strict_warning=true \
   @binder_transport_android_helper//io/grpc/binder/cpp:connection_helper
