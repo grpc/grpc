6521d2f6e527481e17711820c020349f8bf07d51
Craig Tiller <ctiller@google.com>
2022-04-26 08:16:58 -0700

    fix copyright

--- tools/run_tests/sanity/check_banned_filenames.py
+++ tools/run_tests/sanity/check_banned_filenames.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2018 gRPC authors.
+# Copyright 2022 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
