c30ef87fb32642c1290a5e698eb183a853f123e1
Donna Dionne <donnadionne@google.com>
2022-04-28 00:00:20 -0700

    addressing code review comments

--- src/core/ext/filters/client_channel/lb_policy/outlier_detection/outlier_detection.cc
+++ src/core/ext/filters/client_channel/lb_policy/outlier_detection/outlier_detection.cc
@@ -93,7 +93,7 @@ class OutlierDetectionLb : public LoadBalancingPolicy {
           subchannel_state_(std::move(subchannel_state)) {
       if (subchannel_state_ != nullptr) {
         subchannel_state_->subchannels_.insert(this);
-        if (subchannel_state_->ejection_time.has_value()) {
+        if (subchannel_state_->ejection_time_.has_value()) {
           ejected_ = true;
         }
       }
@@ -177,23 +177,16 @@ class OutlierDetectionLb : public LoadBalancingPolicy {
   class SubchannelState : public RefCounted<SubchannelState> {
    public:
     struct Bucket {
-      struct CallCounters {
-        std::atomic<uint64_t> successes;
-        std::atomic<uint64_t> failures;
-      };
-      Bucket() {
-        current_bucket = absl::make_unique<CallCounters>();
-        backup_bucket = absl::make_unique<CallCounters>();
-      }
-      std::unique_ptr<CallCounters> current_bucket;
-      std::unique_ptr<CallCounters> backup_bucket;
-      // The bucket used to update call counts.
-      // Points to either current_bucket or active_bucket.
-      std::atomic<CallCounters*> active_bucket;
+      std::atomic<uint64_t> successes;
+      std::atomic<uint64_t> failures;
     };
-    Bucket bucket;
-    absl::optional<Timestamp> ejection_time;
-    uint32_t mutiplier;
+    std::unique_ptr<Bucket> current_bucket_ = absl::make_unique<Bucket>();
+    std::unique_ptr<Bucket> backup_bucket_ = absl::make_unique<Bucket>();
+    // The bucket used to update call counts.
+    // Points to either current_bucket or active_bucket.
+    std::atomic<Bucket*> active_bucket_;
+    absl::optional<Timestamp> ejection_time_;
+    uint32_t mutiplier_;
     std::set<SubchannelWrapper*> subchannels_;
   };
 
@@ -243,9 +236,9 @@ class OutlierDetectionLb : public LoadBalancingPolicy {
     RefCountedPtr<OutlierDetectionLb> outlier_detection_policy_;
   };
 
-  class DetectionTimer : public InternallyRefCounted<DetectionTimer> {
+  class EjectionTimer : public InternallyRefCounted<EjectionTimer> {
    public:
-    explicit DetectionTimer(RefCountedPtr<OutlierDetectionLb> parent);
+    explicit EjectionTimer(RefCountedPtr<OutlierDetectionLb> parent);
 
     void Orphan() override;
 
@@ -283,7 +276,7 @@ class OutlierDetectionLb : public LoadBalancingPolicy {
   absl::Status status_;
   RefCountedPtr<RefCountedPicker> picker_;
   std::map<std::string, RefCountedPtr<SubchannelState>> subchannel_state_map_;
-  OrphanablePtr<DetectionTimer> detection_timer_;
+  OrphanablePtr<EjectionTimer> ejection_timer_;
 };
 
 //
@@ -365,9 +358,9 @@ class OutlierDetectionLb::Picker::SubchannelCallTracker
     // calculations.
     if (subchannel_state_ != nullptr) {
       if (args.status.ok()) {
-        subchannel_state_->bucket.current_bucket->successes.fetch_add(1);
+        subchannel_state_->current_bucket_->successes.fetch_add(1);
       } else {
-        subchannel_state_->bucket.current_bucket->failures.fetch_add(1);
+        subchannel_state_->current_bucket_->failures.fetch_add(1);
       }
     }
   }
@@ -473,7 +466,7 @@ void OutlierDetectionLb::UpdateLocked(UpdateArgs args) {
   // Update config.
   config_ = std::move(args.config);
   // Update outlier detection timer.
-  // detection_timer_ = MakeOrphanable<DetectionTimer>(Ref());
+  //ejection_timer_ = MakeOrphanable<EjectionTimer>(Ref());
   // Create policy if needed.
   if (child_policy_ == nullptr) {
     child_policy_ = CreateChildPolicyLocked(args.args);
@@ -482,8 +475,9 @@ void OutlierDetectionLb::UpdateLocked(UpdateArgs args) {
     std::set<std::string> current_addresses;
     for (const ServerAddress& address : *args.addresses) {
       std::string address_key = MakeKeyForAddress(address);
-      if (subchannel_state_map_[address_key] == nullptr) {
-        subchannel_state_map_[address_key] = MakeRefCounted<SubchannelState>();
+      auto& subchannel_state = subchannel_state_map_[address_key];
+      if (subchannel_state == nullptr) {
+        subchannel_state = MakeRefCounted<SubchannelState>();
       }
       gpr_log(GPR_INFO, "donna print address %s", address_key.c_str());
       current_addresses.emplace(address_key);
@@ -615,10 +609,10 @@ void OutlierDetectionLb::Helper::AddTraceEvent(TraceSeverity severity,
 }
 
 //
-// OutlierDetectionLb::DetectionTimer
+// OutlierDetectionLb::EjectionTimer
 //
 
-OutlierDetectionLb::DetectionTimer::DetectionTimer(
+OutlierDetectionLb::EjectionTimer::EjectionTimer(
     RefCountedPtr<OutlierDetectionLb> parent)
     : parent_(std::move(parent)) {
   GRPC_CLOSURE_INIT(&on_timer_, OnTimer, this, nullptr);
@@ -629,7 +623,7 @@ OutlierDetectionLb::DetectionTimer::DetectionTimer(
                   &on_timer_);
 }
 
-void OutlierDetectionLb::DetectionTimer::Orphan() {
+void OutlierDetectionLb::EjectionTimer::Orphan() {
   if (timer_pending_) {
     timer_pending_ = false;
     grpc_timer_cancel(&timer_);
@@ -637,29 +631,28 @@ void OutlierDetectionLb::DetectionTimer::Orphan() {
   Unref();
 }
 
-void OutlierDetectionLb::DetectionTimer::OnTimer(void* arg,
-                                                 grpc_error_handle error) {
-  auto* self = static_cast<DetectionTimer*>(arg);
+void OutlierDetectionLb::EjectionTimer::OnTimer(void* arg,
+                                                grpc_error_handle error) {
+  auto* self = static_cast<EjectionTimer*>(arg);
   (void)GRPC_ERROR_REF(error);  // ref owned by lambda
   self->parent_->work_serializer()->Run(
       [self, error]() { self->OnTimerLocked(error); }, DEBUG_LOCATION);
 }
 
-void OutlierDetectionLb::DetectionTimer::OnTimerLocked(
-    grpc_error_handle error) {
+void OutlierDetectionLb::EjectionTimer::OnTimerLocked(grpc_error_handle error) {
   if (error == GRPC_ERROR_NONE && timer_pending_) {
-    for (auto& entry : parent_->subchannel_state_map_) {
+    for (auto& p : parent_->subchannel_state_map_) {
+      auto* subchannel_state = p.second.get();
       // 1. Record the timestamp for use when ejecting addresses in this
       // iteration.
-      entry.second->ejection_time = ExecCtx::Get()->Now();
+      subchannel_state->ejection_time_ = ExecCtx::Get()->Now();
       // 2. For each address, swap the call counter's buckets in that address's
       // map entry.
-      entry.second->bucket.backup_bucket->successes = 0;
-      entry.second->bucket.backup_bucket->failures = 0;
-      entry.second->bucket.current_bucket.swap(
-          entry.second->bucket.backup_bucket);
-      entry.second->bucket.active_bucket.store(
-          entry.second->bucket.current_bucket.get());
+      subchannel_state->backup_bucket_->successes = 0;
+      subchannel_state->backup_bucket_->failures = 0;
+      subchannel_state->current_bucket_.swap(subchannel_state->backup_bucket_);
+      subchannel_state->active_bucket_.store(
+          subchannel_state->current_bucket_.get());
       // 3. If the success_rate_ejection configuration field is set, run the
       // success rate algorithm.
       // 4. If the failure_percentage_ejection configuration field is set, run
