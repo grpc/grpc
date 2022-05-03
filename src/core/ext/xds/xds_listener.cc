e12f2b9bb6682b0c9b95df66d2a98d205ab7eca4
Costin Manolache <costin@google.com>
2022-05-03 18:31:01 +0000

    Remove invalid validation for listener

--- src/core/ext/xds/xds_listener.cc
+++ src/core/ext/xds/xds_listener.cc
@@ -976,10 +976,8 @@ grpc_error_handle LdsResourceParse(
       envoy_config_listener_v3_Listener_api_listener(listener);
   const envoy_config_core_v3_Address* address =
       envoy_config_listener_v3_Listener_address(listener);
-  if (api_listener != nullptr && address != nullptr) {
-    return GRPC_ERROR_CREATE_FROM_STATIC_STRING(
-        "Listener has both address and ApiListener");
-  }
+  // Current envoy proto has address as required, validators will reject if
+  // missing.
   if (api_listener == nullptr && address == nullptr) {
     return GRPC_ERROR_CREATE_FROM_STATIC_STRING(
         "Listener has neither address nor ApiListener");
