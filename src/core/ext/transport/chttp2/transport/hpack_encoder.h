73eb85984a8f75ea600b7597f7b4d8008a347c91
Craig Tiller <ctiller@google.com>
2022-05-05 15:19:00 -0700

    fixes

--- src/core/ext/transport/chttp2/transport/hpack_encoder.h
+++ src/core/ext/transport/chttp2/transport/hpack_encoder.h
@@ -49,6 +49,8 @@ extern grpc_core::TraceFlag grpc_http_trace;
 namespace grpc_core {
 
 class HPackCompressor {
+  class SliceIndex;  // IWYU pragma: keep
+
  public:
   HPackCompressor() = default;
   ~HPackCompressor() = default;
