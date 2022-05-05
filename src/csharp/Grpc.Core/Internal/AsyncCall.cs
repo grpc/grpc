906b7300a350ac3f0f67d59cc8447c3bdcb231c9
Jan Tattermusch <jtattermusch@google.com>
2022-05-05 10:54:30 +0200

    fix #24421

--- src/csharp/Grpc.Core/Internal/AsyncCall.cs
+++ src/csharp/Grpc.Core/Internal/AsyncCall.cs
@@ -648,14 +648,20 @@ namespace Grpc.Core.Internal
             if (status.StatusCode != StatusCode.OK)
             {
                 streamingResponseCallFinishedTcs.SetException(new RpcException(status, receivedStatus.Trailers));
-                if (status.StatusCode == StatusCode.Cancelled || origCancelRequested)
-                {
-                    // Make sure the exception set to the Task is observed,
-                    // otherwise this can trigger "Unobserved exception" when the response stream
-                    // is not read until its end and the task created by the TCS is garbage collected.
-                    // See https://github.com/grpc/grpc/issues/17458
-                    var _ = streamingResponseCallFinishedTcs.Task.Exception;
-                }
+                // Make sure the exception set to the Task is observed,
+                // otherwise this can trigger "Unobserved exception" when the response stream
+                // is not read until its end (by invoking MoveNext() until the point where it either returns false or throws an exception)
+                // and the task created by the TCS is garbage collected.
+                // Note that the streamingResponseCallFinishedTcs.Task itself is never exposed to the user
+                // and its result only ever "observed" by MoveNext() implementation:
+                // https://github.com/grpc/grpc/blob/1cd6e69347cbf62a012477fe184ee6fa8f25d32c/src/csharp/Grpc.Core/Internal/ClientResponseStream.cs#L59.
+                // Even though generally the response stream should always be read until the end (since that's how you get streaming call's responses and status code)
+                // there are cases where this shouldn't be strictly required and thus the user
+                // shouldn't need to worry about observing result of an internal task that's
+                // only exposed to them indirectly.
+                // See https://github.com/grpc/grpc/issues/17458
+                // See https://github.com/grpc/grpc/issues/24421
+                var _ = streamingResponseCallFinishedTcs.Task.Exception;
                 return;
             }
 
