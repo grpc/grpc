0e0b79b0bda875305c23c319c71bc4b4929e0390
ctiller <ctiller@users.noreply.github.com>
2022-05-05 23:43:43 +0000

    Automated change: Fix sanity tests

--- src/core/lib/surface/lame_client.cc
+++ src/core/lib/surface/lame_client.cc
@@ -39,7 +39,6 @@
 #include "src/core/lib/debug/trace.h"
 #include "src/core/lib/gpr/useful.h"
 #include "src/core/lib/gprpp/debug_location.h"
-#include "src/core/lib/gprpp/orphanable.h"
 #include "src/core/lib/gprpp/ref_counted_ptr.h"
 #include "src/core/lib/gprpp/sync.h"
 #include "src/core/lib/iomgr/call_combiner.h"
