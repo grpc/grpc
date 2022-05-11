fd86f36998446dc678e527f3f52e69a906a1a3c9
Craig Tiller <ctiller@google.com>
2022-05-10 23:25:42 -0700

    fix

--- src/core/lib/event_engine/resolved_address.cc
+++ src/core/lib/event_engine/resolved_address.cc
@@ -11,13 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #include <grpc/support/port_platform.h>
 
 #include <string.h>
-#include <sys/socket.h>
 
 #include <grpc/event_engine/event_engine.h>
 #include <grpc/support/log.h>
+
+// IWYU pragma: no_include <sys/socket.h>
 
 namespace grpc_event_engine {
 namespace experimental {
