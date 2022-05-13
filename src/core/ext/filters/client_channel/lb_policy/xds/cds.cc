a8ac64589b0bd3b5b35ea833e735bb79e488af81
Mark D. Roth <roth@google.com>
2022-05-13 18:01:48 +0000

    improve trace log message

--- src/core/ext/filters/client_channel/lb_policy/xds/cds.cc
+++ src/core/ext/filters/client_channel/lb_policy/xds/cds.cc
@@ -224,8 +224,7 @@ void CdsLb::Helper::UpdateState(grpc_connectivity_state state,
                                 std::unique_ptr<SubchannelPicker> picker) {
   if (parent_->shutting_down_ || parent_->child_policy_ == nullptr) return;
   if (GRPC_TRACE_FLAG_ENABLED(grpc_cds_lb_trace)) {
-    gpr_log(GPR_INFO,
-            "[cdslb %p] state updated by child: %s message_state: (%s)", this,
+    gpr_log(GPR_INFO, "[cdslb %p] state updated by child: %s (%s)", this,
             ConnectivityStateName(state), status.ToString().c_str());
   }
   parent_->channel_control_helper()->UpdateState(state, status,
