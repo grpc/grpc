695a7df4e16b35816d823a937f98c0fb3a0bad55
AJ Heller <hork@google.com>
2022-05-13 00:34:14 +0000

    iwyu

--- src/core/lib/event_engine/handle_containers.h
+++ src/core/lib/event_engine/handle_containers.h
@@ -14,12 +14,15 @@
 #ifndef GRPC_CORE_LIB_EVENT_ENGINE_HANDLE_CONTAINERS_H
 #define GRPC_CORE_LIB_EVENT_ENGINE_HANDLE_CONTAINERS_H
 
+#include <stddef.h>
+#include <cstdint>
+#include <utility>
+
+#include <grpc/event_engine/event_engine.h>
 #include <grpc/support/port_platform.h>
 
 #include "absl/container/flat_hash_set.h"
 #include "absl/hash/hash.h"
-
-#include <grpc/event_engine/event_engine.h>
 
 namespace grpc_event_engine {
 namespace experimental {
