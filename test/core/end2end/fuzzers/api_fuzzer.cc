c11f66faef25f68fc195d11add1a62c9fe61a838
AJ Heller <hork@google.com>
2022-05-16 16:23:38 -0700

    Reland DNSResolver cancellation (#29581 and #29631) (#29670)
    * Reland DNSResolver cancellation (#29581 and #29631)
    
    * add failing test: pollset_set deletion after DNSResolver cancellation
    
    * Allow interested_parties to make like a pumpkin after cancellation
    
    * cleanup
    
    * iwyu
    
    * Automated change: Fix sanity tests
    
    * get the pollset_set join correct
    
    * reviewer feedback
    
    Co-authored-by: drfloob <drfloob@users.noreply.github.com>

--- test/core/end2end/fuzzers/api_fuzzer.cc
+++ test/core/end2end/fuzzers/api_fuzzer.cc
@@ -114,24 +114,18 @@ namespace {
 
 class FuzzerDNSResolver : public grpc_core::DNSResolver {
  public:
-  class FuzzerDNSRequest : public grpc_core::DNSResolver::Request {
+  class FuzzerDNSRequest {
    public:
     FuzzerDNSRequest(
         absl::string_view name,
         std::function<void(absl::StatusOr<std::vector<grpc_resolved_address>>)>
             on_done)
-        : name_(std::string(name)), on_done_(std::move(on_done)) {}
-
-    void Start() override {
-      Ref().release();  // ref held by timer callback
+        : name_(std::string(name)), on_done_(std::move(on_done)) {
       grpc_timer_init(
           &timer_,
           grpc_core::Duration::Seconds(1) + grpc_core::ExecCtx::Get()->Now(),
           GRPC_CLOSURE_CREATE(FinishResolve, this, grpc_schedule_on_exec_ctx));
     }
-
-    // cancellation not implemented
-    void Orphan() override { Unref(); }
 
    private:
     static void FinishResolve(void* arg, grpc_error_handle error) {
@@ -145,7 +139,7 @@ class FuzzerDNSResolver : public grpc_core::DNSResolver {
       } else {
         self->on_done_(absl::UnknownError("Resolution failed"));
       }
-      self->Unref();
+      delete self;
     }
 
     const std::string name_;
@@ -161,13 +155,13 @@ class FuzzerDNSResolver : public grpc_core::DNSResolver {
     return instance;
   }
 
-  grpc_core::OrphanablePtr<grpc_core::DNSResolver::Request> ResolveName(
+  TaskHandle ResolveName(
       absl::string_view name, absl::string_view /* default_port */,
       grpc_pollset_set* /* interested_parties */,
       std::function<void(absl::StatusOr<std::vector<grpc_resolved_address>>)>
           on_done) override {
-    return grpc_core::MakeOrphanable<FuzzerDNSRequest>(name,
-                                                       std::move(on_done));
+    new FuzzerDNSRequest(name, std::move(on_done));
+    return kNullHandle;
   }
 
   absl::StatusOr<std::vector<grpc_resolved_address>> ResolveNameBlocking(
@@ -175,6 +169,9 @@ class FuzzerDNSResolver : public grpc_core::DNSResolver {
       absl::string_view /* default_port */) override {
     GPR_ASSERT(0);
   }
+
+  // FuzzerDNSResolver does not support cancellation.
+  bool Cancel(TaskHandle /*handle*/) override { return false; }
 };
 
 }  // namespace
