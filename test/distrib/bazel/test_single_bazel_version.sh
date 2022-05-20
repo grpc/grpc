ca6fc7f2c57719ca49ba71934a1383573b54d813
Lidi Zheng <lidiz@google.com>
2022-05-20 15:09:27 -0700

    Try fix

--- test/distrib/bazel/test_single_bazel_version.sh
+++ test/distrib/bazel/test_single_bazel_version.sh
@@ -23,6 +23,7 @@ fi
 VERSION="$1"
 
 cd "$(dirname "$0")"/../../..
+ROOT_DIR="$(pwd)"
 
 EXCLUDED_TARGETS=(
   # iOS platform fails the analysis phase since there is no toolchain available
@@ -52,14 +53,25 @@ export OVERRIDE_BAZEL_VERSION="$VERSION"
 # when running under bazel docker image, the workspace is read only.
 export OVERRIDE_BAZEL_WRAPPER_DOWNLOAD_DIR=/tmp
 
-# pick the Bazel wrapper
-BAZEL="$(pwd)/tools/bazel"
+# Picks the Bazel wrapper that will be used throughout the tests. This Bazel
+# wrapper will read above OVERRIDE_BAZEL_VERSION and
+# OVERRIDE_BAZEL_WRAPPER_DOWNLOAD_DIR flags to download the correct version of
+# Bazel. By pining the Bazel wrapper, we can avoid the uncertainty of which
+# Bazel is running.
+BAZEL="${ROOT_DIR}/tools/bazel"
+
+function check_bazel_version() {
+  if ! (${BAZEL} version | grep -q "${VERSION}"); then
+    echo "Incorrect Bazel version! Want=${VERSION} Seen=$($BAZEL version)"
+    exit 1
+  fi
+}
 
 # validate the Bazel version
-if ! ($BAZEL version | grep -q "${VERSION}"); then
-  echo "Incorrect Bazel version! Want=${VERSION} Seen=$($BAZEL version)"
-  exit 1
-fi
+check_bazel_version
+
+# clean the caches and downloaded archives to make sure we build from scratch
+$BAZEL clean --expunge
 
 # test building all targets
 $BAZEL build -- //... "${EXCLUDED_TARGETS[@]}" || FAILED_TESTS="${FAILED_TESTS}Build "
@@ -67,12 +79,10 @@ $BAZEL build -- //... "${EXCLUDED_TARGETS[@]}" || FAILED_TESTS="${FAILED_TESTS}B
 for TEST_DIRECTORY in "${TEST_DIRECTORIES[@]}"; do
   pushd "test/distrib/bazel/$TEST_DIRECTORY/"
 
-  # validate the Bazel version again, since we have a different WORKSPACE file
-  if ! ($BAZEL version | grep -q "${VERSION}"); then
-    echo "Incorrect Bazel version! Want=${VERSION} Seen=$($BAZEL version)"
-    exit 1
-  fi
+  # clean the caches and downloaded archives to make sure we build from scratch
   $BAZEL clean --expunge
+  # validate the Bazel version again, since we have a different WORKSPACE file
+  check_bazel_version
   $BAZEL test --cache_test_results=no --test_output=all //:all || FAILED_TESTS="${FAILED_TESTS}${TEST_DIRECTORY} Distribtest"
 
   popd
