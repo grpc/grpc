0eda91fa2822690e365ea5b78b4a8e4c28f86e80
Craig Tiller <ctiller@google.com>
2022-05-25 12:40:25 -0700

    Revert "[c++14] Simplify macro thats always the same now (#29601)" (#29784)
    This reverts commit 7faa624d8e7062986cc6e7a46fd5dd4dcb7725a0.

--- src/core/lib/gprpp/bitset.h
+++ src/core/lib/gprpp/bitset.h
@@ -24,6 +24,12 @@
 
 #include "src/core/lib/gpr/useful.h"
 
+#if __cplusplus > 201103l
+#define GRPC_BITSET_CONSTEXPR_MUTATOR constexpr
+#else
+#define GRPC_BITSET_CONSTEXPR_MUTATOR
+#endif
+
 namespace grpc_core {
 
 // Given a bit count as an integer, vend as member type `Type` a type with
@@ -84,10 +90,12 @@ class BitSet {
   constexpr BitSet() : units_{} {}
 
   // Set bit i to true
-  constexpr void set(int i) { units_[unit_for(i)] |= mask_for(i); }
+  GRPC_BITSET_CONSTEXPR_MUTATOR void set(int i) {
+    units_[unit_for(i)] |= mask_for(i);
+  }
 
   // Set bit i to is_set
-  constexpr void set(int i, bool is_set) {
+  GRPC_BITSET_CONSTEXPR_MUTATOR void set(int i, bool is_set) {
     if (is_set) {
       set(i);
     } else {
@@ -96,7 +104,9 @@ class BitSet {
   }
 
   // Set bit i to false
-  constexpr void clear(int i) { units_[unit_for(i)] &= ~mask_for(i); }
+  GRPC_BITSET_CONSTEXPR_MUTATOR void clear(int i) {
+    units_[unit_for(i)] &= ~mask_for(i);
+  }
 
   // Return true if bit i is set
   constexpr bool is_set(int i) const {
