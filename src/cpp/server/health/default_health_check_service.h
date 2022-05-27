67f85a67354c3c5eecb6b66d987ebde371c97b48
Craig Tiller <ctiller@google.com>
2022-05-27 10:22:07 -0700

    fix

--- src/cpp/server/health/default_health_check_service.h
+++ src/cpp/server/health/default_health_check_service.h
@@ -32,6 +32,7 @@
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
 #include <grpcpp/impl/codegen/async_generic_service.h>  // IWYU pragma: keep
+#include <grpcpp/impl/codegen/async_unary_call.h>       // IWYU pragma: keep
 #include <grpcpp/impl/service_type.h>
 #include <grpcpp/support/byte_buffer.h>
 #include <grpcpp/support/config.h>
