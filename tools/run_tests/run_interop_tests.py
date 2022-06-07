f56ac8482e715c55e6528107d7f2b4a7fbb1078c
yifeizhuang <zivy@google.com>
2022-06-07 16:35:14 -0700

    test

--- tools/run_tests/run_interop_tests.py
+++ tools/run_tests/run_interop_tests.py
@@ -679,13 +679,14 @@ _SERVERS = [
 ]
 
 _TEST_CASES = [
-    'large_unary', 'empty_unary', 'ping_pong', 'empty_stream',
-    'client_streaming', 'server_streaming', 'cancel_after_begin',
-    'cancel_after_first_response', 'timeout_on_sleeping_server',
-    'custom_metadata', 'status_code_and_message', 'unimplemented_method',
-    'client_compressed_unary', 'server_compressed_unary',
-    'client_compressed_streaming', 'server_compressed_streaming',
-    'unimplemented_service', 'special_status_message', 'orca_per_rpc',
+    # 'large_unary', 'empty_unary', 'ping_pong', 'empty_stream',
+    # 'client_streaming', 'server_streaming', 'cancel_after_begin',
+    # 'cancel_after_first_response', 'timeout_on_sleeping_server',
+    # 'custom_metadata', 'status_code_and_message', 'unimplemented_method',
+    # 'client_compressed_unary', 'server_compressed_unary',
+    # 'client_compressed_streaming', 'server_compressed_streaming',
+    # 'unimplemented_service', 'special_status_message',
+    # 'orca_per_rpc',
     'orca_oob'
 ]
 
