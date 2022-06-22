89c1f0f2a14b2ca91f44f6143196998d4493d797
Mark D. Roth <roth@google.com>
2022-06-22 14:59:55 +0000

    NACK endpoint weight of 0

--- src/core/ext/xds/xds_endpoint.cc
+++ src/core/ext/xds/xds_endpoint.cc
@@ -164,13 +164,16 @@ grpc_error_handle ServerAddressParseAndAppend(
     return GRPC_ERROR_CREATE_FROM_STATIC_STRING("Invalid port.");
   }
   // Find load_balancing_weight for the endpoint.
-  int32_t weight = 0;
+  uint32_t weight = 1;
   const google_protobuf_UInt32Value* load_balancing_weight =
       envoy_config_endpoint_v3_LbEndpoint_load_balancing_weight(lb_endpoint);
   if (load_balancing_weight != nullptr) {
     weight = google_protobuf_UInt32Value_value(load_balancing_weight);
+    if (weight == 0) {
+      return GRPC_ERROR_CREATE_FROM_STATIC_STRING(
+          "Invalid endpoint weight of 0.");
+    }
   }
-  weight = std::max(1, weight);
   // Populate grpc_resolved_address.
   grpc_resolved_address addr;
   grpc_error_handle error =
