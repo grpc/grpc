958a314c390af6fe5e415884551859ce9d704b6b
Richard Belleville <rbellevi@google.com>
2022-06-27 19:53:21 +0000

    Show the initial state of the egg directory

--- src/python/grpcio_tests/setup.py
+++ src/python/grpcio_tests/setup.py
@@ -46,6 +46,11 @@ INSTALL_REQUIRES = (
     'oauth2client>=1.4.7', 'protobuf>=3.12.0', 'six>=1.10',
     'google-auth>=1.17.2', 'requests>=2.14.2')
 
+print("AAAAAAAAAAAA Showing egg directory.")
+print(subprocess.check_output("ls /var/local/git/grpc/src/python/grpcio_tests/.eggs"
+                        shell=True,
+                              stderr=subprocess.STDOUT).decode('ascii'))
+
 print("AAAAAAAAAAAA Running pip freeze.")
 print(subprocess.check_output("{} -m pip freeze".format(sys.executable),
                         shell=True,
