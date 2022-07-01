c835402dd9f5193ffe3531a2eeb3787da7ceb70c
AJ Heller <hork@google.com>
2022-07-01 09:24:06 -0700

    Add SRV and TXT record lookup methods to the iomgr API (#30078)
    * Rename ResolveName to LookupHostname (same as EventEngine)
    
    * Add stubs and no-op impls for LookupTXT and LookupSRV
    
    * add native resolver tests that assert unimplemented
    
    * extract custom name_server-setting logic and remove goto
    
    * Separate SRV queries from grpc_dns_lookup_ares
    
    * add necessary fixits before merging
    
    * Automated change: Fix sanity tests
    
    * fix missing ExecCtx on resolver tests
    
    * separate out TXT lookup from hostname lookup (now all 3 are separate)
    
    * rm DNS and update docs
    
    * fix the fixer (forgot to add deps to BUILD)
    
    * remove unused SRV and TXT args from ares hostname lookup method
    
    * rename hostname-only ares dns lookup method
    
    * refactor AresRequest using template method pattern
    
    * Add name_server to Ares LookupHostname internals (needs iomgr API change)
    
    * fix resolver test, callback should not be called on cancellation
    
    * implement Ares-iomgr SRV and TXT lookup methods (verified manually)
    
    Used a custom bind server with some redacted tests from
    `resolve_address_test` to ensure both are working as expected.
    
    * cleanup cruft
    
    * unify common ares request setup logic between A, AAAA, TXT, and SRV
    
    * generate_projects
    
    * comment out unused args
    
    * DNSResolver iomgr API uses Duration; hostname has all args now
    
    * rm stale TODOs
    
    * windows fix - bad variable name
    
    * windows fix
    
    * Automated change: Fix sanity tests
    
    * reviewer feedback
    
    * make protected members private
    
    * move common properties to AresRequest base class
    
    * localhost TXT results are empty, not an error
    
    * reviewer feedback
    
    * fix
    
    Co-authored-by: drfloob <drfloob@users.noreply.github.com>

--- src/core/lib/iomgr/resolve_address_windows.cc
+++ src/core/lib/iomgr/resolve_address_windows.cc
@@ -34,6 +34,7 @@
 #include <grpc/support/time.h>
 
 #include "src/core/lib/address_utils/sockaddr_utils.h"
+#include "src/core/lib/event_engine/event_engine_factory.h"
 #include "src/core/lib/gpr/string.h"
 #include "src/core/lib/gprpp/host_port.h"
 #include "src/core/lib/gprpp/thd.h"
@@ -48,6 +49,8 @@
 namespace grpc_core {
 namespace {
 
+using ::grpc_event_engine::experimental::GetDefaultEventEngine;
+
 class NativeDNSRequest {
  public:
   NativeDNSRequest(
@@ -61,11 +64,11 @@ class NativeDNSRequest {
 
  private:
   // Callback to be passed to grpc Executor to asynch-ify
-  // ResolveNameBlocking
+  // LookupHostnameBlocking
   static void DoRequestThread(void* rp, grpc_error_handle /*error*/) {
     NativeDNSRequest* r = static_cast<NativeDNSRequest*>(rp);
     auto result =
-        GetDNSResolver()->ResolveNameBlocking(r->name_, r->default_port_);
+        GetDNSResolver()->LookupHostnameBlocking(r->name_, r->default_port_);
     // running inline is safe since we've already been scheduled on the executor
     r->on_done_(std::move(result));
     delete r;
@@ -85,18 +88,19 @@ NativeDNSResolver* NativeDNSResolver::GetOrCreate() {
   return instance;
 }
 
-DNSResolver::TaskHandle NativeDNSResolver::ResolveName(
-    absl::string_view name, absl::string_view default_port,
-    grpc_pollset_set* /* interested_parties */,
+DNSResolver::TaskHandle NativeDNSResolver::LookupHostname(
     std::function<void(absl::StatusOr<std::vector<grpc_resolved_address>>)>
-        on_done) {
-  new NativeDNSRequest(name, default_port, std::move(on_done));
+        on_resolved,
+    absl::string_view name, absl::string_view default_port,
+    Duration /* timeout */, grpc_pollset_set* /* interested_parties */,
+    absl::string_view /* name_server */) {
+  new NativeDNSRequest(name, default_port, std::move(on_resolved));
   return kNullHandle;
 }
 
 absl::StatusOr<std::vector<grpc_resolved_address>>
-NativeDNSResolver::ResolveNameBlocking(absl::string_view name,
-                                       absl::string_view default_port) {
+NativeDNSResolver::LookupHostnameBlocking(absl::string_view name,
+                                          absl::string_view default_port) {
   ExecCtx exec_ctx;
   struct addrinfo hints;
   struct addrinfo *result = NULL, *resp;
@@ -156,6 +160,32 @@ done:
   GRPC_ERROR_UNREF(error);
   return error_result;
 }
+
+DNSResolver::TaskHandle NativeDNSResolver::LookupSRV(
+    std::function<void(absl::StatusOr<std::vector<grpc_resolved_address>>)>
+        on_resolved,
+    absl::string_view /* name */, Duration /* deadline */,
+    grpc_pollset_set* /* interested_parties */,
+    absl::string_view /* name_server */) {
+  GetDefaultEventEngine()->Run([on_resolved] {
+    on_resolved(absl::UnimplementedError(
+        "The Native resolver does not support looking up SRV records"));
+  });
+  return {-1, -1};
+};
+
+DNSResolver::TaskHandle NativeDNSResolver::LookupTXT(
+    std::function<void(absl::StatusOr<std::string>)> on_resolved,
+    absl::string_view /* name */, Duration /* timeout */,
+    grpc_pollset_set* /* interested_parties */,
+    absl::string_view /* name_server */) {
+  // Not supported
+  GetDefaultEventEngine()->Run([on_resolved] {
+    on_resolved(absl::UnimplementedError(
+        "The Native resolver does not support looking up TXT records"));
+  });
+  return {-1, -1};
+};
 
 bool NativeDNSResolver::Cancel(TaskHandle /*handle*/) { return false; }
 
