2cb15ec42d5db14475e2caa3fee1f491ee08a8e8
AJ Heller <hork@google.com>
2022-07-05 19:04:27 +0000

    Revert "Revert "Add SRV and TXT record lookup methods to the iomgr API (#30078)" (#30176)"
    This reverts commit 2c3acbb2b2bbaca2833a923a4003ab6fe52ec5f8.

--- test/core/end2end/goaway_server_test.cc
+++ test/core/end2end/goaway_server_test.cc
@@ -28,6 +28,7 @@
 #include <grpc/support/log.h>
 
 #include "src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_wrapper.h"
+#include "src/core/lib/event_engine/event_engine_factory.h"
 #include "src/core/lib/iomgr/resolve_address.h"
 #include "src/core/lib/iomgr/resolve_address_impl.h"
 #include "src/core/lib/iomgr/sockaddr.h"
@@ -46,8 +47,7 @@ static grpc_ares_request* (*iomgr_dns_lookup_ares)(
     const char* dns_server, const char* addr, const char* default_port,
     grpc_pollset_set* interested_parties, grpc_closure* on_done,
     std::unique_ptr<grpc_core::ServerAddressList>* addresses,
-    std::unique_ptr<grpc_core::ServerAddressList>* balancer_addresses,
-    char** service_config_json, int query_timeout_ms);
+    int query_timeout_ms);
 
 static void (*iomgr_cancel_ares_request)(grpc_ares_request* request);
 
@@ -59,27 +59,57 @@ static void set_resolve_port(int port) {
 
 namespace {
 
+using ::grpc_event_engine::experimental::GetDefaultEventEngine;
+
 grpc_core::DNSResolver* g_default_dns_resolver;
 
 class TestDNSResolver : public grpc_core::DNSResolver {
  public:
-  TaskHandle ResolveName(
-      absl::string_view name, absl::string_view default_port,
-      grpc_pollset_set* interested_parties,
+  TaskHandle LookupHostname(
       std::function<void(absl::StatusOr<std::vector<grpc_resolved_address>>)>
-          on_done) override {
+          on_resolved,
+      absl::string_view name, absl::string_view default_port,
+      grpc_core::Duration timeout, grpc_pollset_set* interested_parties,
+      absl::string_view name_server) override {
     if (name != "test") {
-      return g_default_dns_resolver->ResolveName(
-          name, default_port, interested_parties, std::move(on_done));
+      return g_default_dns_resolver->LookupHostname(
+          std::move(on_resolved), name, default_port, timeout,
+          interested_parties, name_server);
     }
-    MakeDNSRequest(std::move(on_done));
+    MakeDNSRequest(std::move(on_resolved));
     return kNullHandle;
   }
 
-  absl::StatusOr<std::vector<grpc_resolved_address>> ResolveNameBlocking(
+  absl::StatusOr<std::vector<grpc_resolved_address>> LookupHostnameBlocking(
       absl::string_view name, absl::string_view default_port) override {
-    return g_default_dns_resolver->ResolveNameBlocking(name, default_port);
+    return g_default_dns_resolver->LookupHostnameBlocking(name, default_port);
   }
+
+  TaskHandle LookupSRV(
+      std::function<void(absl::StatusOr<std::vector<grpc_resolved_address>>)>
+          on_resolved,
+      absl::string_view /* name */, grpc_core::Duration /* timeout */,
+      grpc_pollset_set* /* interested_parties */,
+      absl::string_view /* name_server */) override {
+    GetDefaultEventEngine()->Run([on_resolved] {
+      on_resolved(absl::UnimplementedError(
+          "The Testing DNS resolver does not support looking up SRV records"));
+    });
+    return {-1, -1};
+  };
+
+  TaskHandle LookupTXT(
+      std::function<void(absl::StatusOr<std::string>)> on_resolved,
+      absl::string_view /* name */, grpc_core::Duration /* timeout */,
+      grpc_pollset_set* /* interested_parties */,
+      absl::string_view /* name_server */) override {
+    // Not supported
+    GetDefaultEventEngine()->Run([on_resolved] {
+      on_resolved(absl::UnimplementedError(
+          "The Testing DNS resolver does not support looking up TXT records"));
+    });
+    return {-1, -1};
+  };
 
   bool Cancel(TaskHandle /*handle*/) override { return false; }
 
@@ -114,12 +144,12 @@ static grpc_ares_request* my_dns_lookup_ares(
     const char* dns_server, const char* addr, const char* default_port,
     grpc_pollset_set* interested_parties, grpc_closure* on_done,
     std::unique_ptr<grpc_core::ServerAddressList>* addresses,
-    std::unique_ptr<grpc_core::ServerAddressList>* balancer_addresses,
-    char** service_config_json, int query_timeout_ms) {
+    int query_timeout_ms) {
   if (0 != strcmp(addr, "test")) {
-    return iomgr_dns_lookup_ares(
-        dns_server, addr, default_port, interested_parties, on_done, addresses,
-        balancer_addresses, service_config_json, query_timeout_ms);
+    // A records should suffice
+    return iomgr_dns_lookup_ares(dns_server, addr, default_port,
+                                 interested_parties, on_done, addresses,
+                                 query_timeout_ms);
   }
 
   grpc_error_handle error = GRPC_ERROR_NONE;
@@ -160,9 +190,9 @@ int main(int argc, char** argv) {
   g_default_dns_resolver = grpc_core::GetDNSResolver();
   auto* resolver = new TestDNSResolver();
   grpc_core::SetDNSResolver(resolver);
-  iomgr_dns_lookup_ares = grpc_dns_lookup_ares;
+  iomgr_dns_lookup_ares = grpc_dns_lookup_hostname_ares;
   iomgr_cancel_ares_request = grpc_cancel_ares_request;
-  grpc_dns_lookup_ares = my_dns_lookup_ares;
+  grpc_dns_lookup_hostname_ares = my_dns_lookup_ares;
   grpc_cancel_ares_request = my_cancel_ares_request;
 
   int was_cancelled1;
