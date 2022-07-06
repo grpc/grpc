5202e873ff2f5e501f74f139215793dda5026b57
Ming-Chuan <mingcl@google.com>
2022-07-06 21:20:01 +0800

    BinderTransport: Support Intent URI (#30104)
    This commit adds a new overloaded version of `CreateBinderChannel`, that accepts
    an intent URI (which should be able to be parsed by
    https://developer.android.com/reference/android/content/Intent#parseUri(java.lang.String,%20int) )
    for specifying the component to connect to.
    
    Later we will deprecate the old APIs that accepts `package_name`,
    `class_name`, and `action_name` separately. Intent URI seems to be a
    better and more flexible way to specify a component to connect to.
    
    `grpc.binder.custom_android_intent_action_name` channel arg is deprecated now.
    User should use URI to specify custom action instead.

--- src/core/ext/transport/binder/client/connection_id_generator.cc
+++ src/core/ext/transport/binder/client/connection_id_generator.cc
@@ -44,13 +44,11 @@ std::string StripToLength(const std::string& s, size_t len) {
 
 namespace grpc_binder {
 
-std::string ConnectionIdGenerator::Generate(absl::string_view package_name,
-                                            absl::string_view class_name) {
+std::string ConnectionIdGenerator::Generate(absl::string_view uri) {
   // reserve some room for serial number
   const size_t kReserveForNumbers = 15;
-  std::string s = StripToLength(
-      absl::StrCat(Normalize(package_name), "-", Normalize(class_name)),
-      kPathLengthLimit - kReserveForNumbers);
+  std::string s =
+      StripToLength(Normalize(uri), kPathLengthLimit - kReserveForNumbers);
   std::string ret;
   {
     grpc_core::MutexLock l(&m_);
