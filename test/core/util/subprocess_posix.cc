856abd9446a055279b946008610e3e73473315de
Craig Tiller <ctiller@google.com>
2022-07-20 16:18:19 -0700

    [iwyu] Add test/core/util (#30289)
    * [iwyu] Add test/core/util
    
    * Automated change: Fix sanity tests
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    Co-authored-by: ctiller <ctiller@users.noreply.github.com>

--- test/core/util/subprocess_posix.cc
+++ test/core/util/subprocess_posix.cc
@@ -20,14 +20,9 @@
 
 #ifdef GPR_POSIX_SUBPROCESS
 
-#include <assert.h>
 #include <errno.h>
 #include <signal.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
