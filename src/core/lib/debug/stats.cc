3849ce5cbbe95031d9baf1d3ec9b8fc7be9fe7b1
panzhongxian <panzhongxian@users.noreply.github.com>
2022-07-25 02:39:54 +0000

    Automated change: Fix sanity tests

--- src/core/lib/debug/stats.cc
+++ src/core/lib/debug/stats.cc
@@ -31,7 +31,6 @@
 
 #include <grpc/support/alloc.h>
 #include <grpc/support/cpu.h>
-#include <grpc/support/sync.h>
 
 grpc_stats_data* grpc_stats_per_cpu_storage = nullptr;
 static size_t g_num_cores;
