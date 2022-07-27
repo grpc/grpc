57aaec12e3069bc6adc86e4daa4538fb8d4f8626
ctiller <ctiller@users.noreply.github.com>
2022-07-27 06:40:59 +0000

    Automated change: Fix sanity tests

--- src/core/lib/surface/init.cc
+++ src/core/lib/surface/init.cc
@@ -36,7 +36,6 @@
 #include "src/core/lib/channel/channel_args.h"
 #include "src/core/lib/channel/channel_stack_builder.h"
 #include "src/core/lib/config/core_configuration.h"
-#include "src/core/lib/debug/stats.h"
 #include "src/core/lib/debug/trace.h"
 #include "src/core/lib/gprpp/fork.h"
 #include "src/core/lib/gprpp/sync.h"
