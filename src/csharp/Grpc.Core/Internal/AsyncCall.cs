233825a6469badd5c34dbe1e88769fe104854ab3
tony <tony.newell@pobox.com>
2022-07-28 11:01:09 +0100

    Fix hang in AsyncCall.UnaryCall
    Issue https://github.com/grpc/grpc/issues/29854
    An exception thrown by decoding status code was not being handled
    correctly and this caused the call to hang.

--- src/csharp/Grpc.Core/Internal/AsyncCall.cs
+++ src/csharp/Grpc.Core/Internal/AsyncCall.cs
@@ -122,6 +122,9 @@ namespace Grpc.Core.Internal
                             catch (Exception e)
                             {
                                 Logger.Error(e, "Exception occurred while invoking completion delegate.");
+                                unaryResponseTcs.SetException(new RpcException(
+                                    new Status(StatusCode.Internal,
+                                        "Exception occurred while invoking completion delegate", e)));
                             }
                         }
                         finally
