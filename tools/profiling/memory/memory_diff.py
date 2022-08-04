d4605934145982f062b4de3ae37a8cdeb33c48fe
Nancy Chen <chennancy@google.com>
2022-08-04 23:06:48 +0000

    removing warning

--- tools/profiling/memory/memory_diff.py
+++ tools/profiling/memory/memory_diff.py
@@ -117,7 +117,7 @@ else:
                     text += '{}: {}\n'.format(key, cur[key])
                 else:
                     text += '{}: {} -> {}\n'.format(key, old[key], cur[key])
-                    if key is 'client call' or 'server call':
+                    if key == 'client call' or 'server call':
                         call_diff_size += cur[key] - old[key]
                     else:
                         channel_diff_size += cur[key] - old[key]
