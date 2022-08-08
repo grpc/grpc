57d99678a2c3ba51a933cf4572c540eb358497d0
Cheng-Yu Chung <chengyuc@google.com>
2022-08-08 13:50:43 -0400

    Fix tool error

--- tools/distrib/fix_build_deps.py
+++ tools/distrib/fix_build_deps.py
@@ -402,6 +402,7 @@ for dirname in [
     parsing_path = dirname
     exec(
         open('%sBUILD' % (dirname + '/' if dirname else ''), 'r').read(), {
+            'alias': lambda name, **kwargs: None,
             'load': lambda filename, *args: None,
             'licenses': lambda licenses: None,
             'package': lambda **kwargs: None,
