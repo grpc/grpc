ebf9b7ea776c526510e5c7bc5aac2f447c29a353
AJ Heller <hork@google.com>
2022-08-08 14:09:31 -0700

    manual sanitize

--- src/core/lib/event_engine/event_engine.cc
+++ src/core/lib/event_engine/event_engine.cc
@@ -22,8 +22,8 @@
 #include <grpc/event_engine/event_engine.h>
 
 #include "src/core/lib/debug/trace.h"
-#include "src/core/lib/event_engine/default_event_engine_factory.h"
 #include "src/core/lib/event_engine/default_event_engine.h"
+#include "src/core/lib/event_engine/default_event_engine_factory.h"
 #include "src/core/lib/event_engine/trace.h"
 #include "src/core/lib/gprpp/sync.h"
 
