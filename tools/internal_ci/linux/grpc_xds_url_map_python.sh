796a8ddcfe629c1ef7beae117efce2004886ecd9
Sergii Tkachenko <sergiitk@google.com>
2022-08-08 13:44:19 -0700

    xDS interop: add missing image tagging to the buildscripts (#30520) (#30529)

--- tools/internal_ci/linux/grpc_xds_url_map_python.sh
+++ tools/internal_ci/linux/grpc_xds_url_map_python.sh
@@ -49,6 +49,9 @@ build_test_app_docker_images() {
   gcloud -q auth configure-docker
 
   docker push "${CLIENT_IMAGE_NAME}:${GIT_COMMIT}"
+  if is_version_branch "${TESTING_VERSION}"; then
+    tag_and_push_docker_image "${CLIENT_IMAGE_NAME}" "${GIT_COMMIT}" "${TESTING_VERSION}"
+  fi
 }
 
 #######################################
