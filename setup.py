f95e454ee8440f7fd5ef88bc6ba4e5ce41592a4d
git <abaelhe@icloud.com>
2022-08-10 02:07:50 +0800

    PR:#30522, Google Style, yapf -i --style google setup.py

--- setup.py
+++ setup.py
@@ -214,11 +214,11 @@ def check_linker_need_libatomic():
         return False
     # Double-check to see if -latomic actually can solve the problem.
     # https://github.com/grpc/grpc/issues/22491
-    cpp_test = subprocess.Popen(
-        cxx + ['-x', 'c++', '-std=c++14', '-', '-latomic'],
-        stdin=PIPE,
-        stdout=PIPE,
-        stderr=PIPE)
+    cpp_test = subprocess.Popen(cxx +
+                                ['-x', 'c++', '-std=c++14', '-', '-latomic'],
+                                stdin=PIPE,
+                                stdout=PIPE,
+                                stderr=PIPE)
     cpp_test.communicate(input=code_test)
     return cpp_test.returncode == 0
 
