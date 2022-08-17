7b5fb9ecd577ff6fd1a11b336733a5f00a219fa0
Craig Tiller <ctiller@google.com>
2022-08-17 13:15:33 -0700

    x

---
+++ test/core/event_engine/mock_event_engine.h
@@ -0,0 +1,51 @@
+// Copyright 2022 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MOCK_EVENT_ENGINE_H
+#define MOCK_EVENT_ENGINE_H
+
+#include <gmock/gmock.h>
+
+#include <grpc/event_engine/event_engine.h>
+
+namespace grpc_event_engine {
+namespace experimental {
+
+class MockEventEngine : public EventEngine {
+ public:
+  MOCK_METHOD(
+      absl::StatusOr<std::unique_ptr<Listener>>, CreateListener,
+      (Listener::AcceptCallback on_accept,
+       absl::AnyInvocable<void(absl::Status)> on_shutdown,
+       const EndpointConfig& config,
+       std::unique_ptr<MemoryAllocatorFactory> memory_allocator_factory));
+  MOCK_METHOD(ConnectionHandle, Connect,
+              (OnConnectCallback on_connect, const ResolvedAddress& addr,
+               const EndpointConfig& args, MemoryAllocator memory_allocator,
+               Duration timeout));
+  MOCK_METHOD(bool, IsWorkerThread, ());
+  MOCK_METHOD(std::unique_ptr<DNSResolver>, GetDNSResolver,
+              (const DNSResolver::ResolverOptions& options));
+  MOCK_METHOD(void, Run, (Closure * closure));
+  MOCK_METHOD(void, Run, (absl::AnyInvocable<void()> closure));
+  MOCK_METHOD(TaskHandle, RunAfter, (Duration when, Closure* closure));
+  MOCK_METHOD(TaskHandle, RunAfter,
+              (Duration when, absl::AnyInvocable<void()> closure));
+  MOCK_METHOD(bool, Cancel, (TaskHandle handle));
+};
+
+}  // namespace experimental
+}  // namespace grpc_event_engine
+
+#endif
