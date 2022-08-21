c3e1ff384b748c0a160343e9b8684956f3ee4bfa
Craig Tiller <ctiller@google.com>
2022-08-21 08:09:19 -0700

    windows fix

--- test/core/util/tls_utils.cc
+++ test/core/util/tls_utils.cc
@@ -45,6 +45,10 @@ void TmpFile::RewriteFile(absl::string_view data) {
   // Create a new file containing new data.
   std::string new_name = CreateTmpFileAndWriteData(data);
   GPR_ASSERT(!new_name.empty());
+#ifdef GPR_WINDOWS
+  // Remove the old file.
+  GPR_ASSERT(remove(name_.c_str()) == 0);
+#endif
   // Rename the new file to the original name.
   GPR_ASSERT(rename(new_name.c_str(), name_.c_str()) == 0);
 }
