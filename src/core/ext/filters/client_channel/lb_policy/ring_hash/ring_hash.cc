5a4238fa7837b2d34e90ba293fa0a90e2ee43ebf
markdroth <markdroth@users.noreply.github.com>
2022-08-23 20:45:25 +0000

    Automated change: Fix sanity tests

--- src/core/ext/filters/client_channel/lb_policy/ring_hash/ring_hash.cc
+++ src/core/ext/filters/client_channel/lb_policy/ring_hash/ring_hash.cc
@@ -56,7 +56,6 @@
 #include "src/core/lib/gpr/string.h"
 #include "src/core/lib/gprpp/debug_location.h"
 #include "src/core/lib/gprpp/orphanable.h"
-#include "src/core/lib/gprpp/ref_counted.h"
 #include "src/core/lib/gprpp/ref_counted_ptr.h"
 #include "src/core/lib/gprpp/sync.h"
 #include "src/core/lib/gprpp/unique_type_name.h"
