6e45ed80f044b245b6bdc0c6f881a88b2d8cd711
Sergii Tkachenko <sergiitk@google.com>
2022-08-24 12:53:51 -0700

    xDS interop: enable pod log collection in the buildscripts

--- tools/internal_ci/linux/grpc_xds_k8s_lb.sh
+++ tools/internal_ci/linux/grpc_xds_k8s_lb.sh
@@ -101,16 +101,21 @@ run_test() {
   # Test driver usage:
   # https://github.com/grpc/grpc/tree/master/tools/run_tests/xds_k8s_test_driver#basic-usage
   local test_name="${1:?Usage: run_test test_name}"
+  local out_dir="${TEST_XML_OUTPUT_DIR}/${test_name}"
+  mkdir -pv "${out_dir}"
   set -x
   python3 -m "tests.${test_name}" \
     --flagfile="${TEST_DRIVER_FLAGFILE}" \
     --kube_context="${KUBE_CONTEXT}" \
     --secondary_kube_context="${SECONDARY_KUBE_CONTEXT}" \
-    --server_image="${SERVER_IMAGE_NAME}:${GIT_COMMIT}" \
-    --client_image="${CLIENT_IMAGE_NAME}:${GIT_COMMIT}" \
+    --server_image="${SERVER_IMAGE_NAME}:v1.48.x" \
+    --client_image="${CLIENT_IMAGE_NAME}:v1.48.x" \
     --testing_version="${TESTING_VERSION}" \
-    --xml_output_file="${TEST_XML_OUTPUT_DIR}/${test_name}/sponge_log.xml" \
-    ${@:2}
+    --collect_app_logs \
+    --log_dir="${out_dir}" \
+    --xml_output_file="${out_dir}/sponge_log.xml" \
+    ${@:2} \
+    |& tee "${out_dir}/sponge_log.log"
 }
 
 run_alpha_test() {
@@ -157,13 +162,14 @@ main() {
   else
     local_setup_test_driver "${script_dir}"
   fi
-  build_docker_images_if_needed
+  # build_docker_images_if_needed
 
   # Run tests
   cd "${TEST_DRIVER_FULL_DIR}"
   local failed_tests=0
-  run_alpha_test subsetting_test || (( failed_tests++ ))
-  test_suites=("api_listener_test" "change_backend_service_test" "failover_test" "remove_neg_test" "round_robin_test" "affinity_test")
+  # run_alpha_test subsetting_test || (( failed_tests++ ))
+  # test_suites=("api_listener_test" "change_backend_service_test" "failover_test" "remove_neg_test" "round_robin_test" "affinity_test")
+  test_suites=("failover_test")
   for test in "${test_suites[@]}"; do
     run_test $test || (( failed_tests++ ))
   done
