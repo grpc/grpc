cd52b131b7573fad443b89148939219d196532ca
Craig Tiller <ctiller@google.com>
2022-08-27 06:49:56 -0700

    Introduce flag

--- src/core/lib/iomgr/tcp_posix.cc
+++ src/core/lib/iomgr/tcp_posix.cc
@@ -98,6 +98,10 @@ typedef size_t msg_iovlen_type;
 extern grpc_core::TraceFlag grpc_tcp_trace;
 
 GPR_GLOBAL_CONFIG_DECLARE_BOOL(grpc_experimental_enable_tcp_frame_size_tuning);
+GPR_GLOBAL_CONFIG_DEFINE_BOOL(
+    grpc_experimental_tcp_read_chunks, false,
+    "Allocate only 8kb or 64kb chunks for TCP reads to reduce pressure on "
+    "malloc to recycle arbitrary large blocks");
 
 namespace grpc_core {
 
