874c488077252e988723da2576c00bdd105476a8
AJ Heller <hork@google.com>
2022-09-08 15:48:47 -0700

    Add sparsely-populated multi-threaded benchmarks.

--- test/core/event_engine/bm_work_queue.cc
+++ test/core/event_engine/bm_work_queue.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 #include <grpc/support/port_platform.h>
 
+#include <cmath>
 #include <deque>
 #include <sstream>
 
@@ -31,6 +32,28 @@ namespace {
 
 using ::grpc_event_engine::experimental::WorkQueue;
 
+grpc_core::Mutex globalMu;
+std::vector<WorkQueue<intptr_t>*>* globalWorkQueueList;
+std::vector<std::deque<intptr_t>*>* globalDequeList;
+std::vector<grpc_core::Mutex>* globalDequeMutexList;
+
+static void GlobalSetup(const benchmark::State& state) {
+  // called for every test, resets all state
+  globalWorkQueueList = new std::vector<WorkQueue<intptr_t>*>();
+  globalWorkQueueList->reserve(state.threads());
+  globalDequeList = new std::vector<std::deque<intptr_t>*>();
+  globalDequeList->reserve(state.threads());
+  globalDequeMutexList = new std::vector<grpc_core::Mutex>(
+      std::vector<grpc_core::Mutex>(state.threads()));
+}
+
+static void GlobalTeardown(const benchmark::State& state) {
+  // called for every test, resets all state
+  delete globalWorkQueueList;
+  delete globalDequeList;
+  delete globalDequeMutexList;
+}
+
 static void BM_WorkQueueIntptrPopFront(benchmark::State& state) {
   WorkQueue<intptr_t> queue;
   int element_count = state.range(0);
@@ -44,88 +67,182 @@ static void BM_WorkQueueIntptrPopFront(benchmark::State& state) {
       if (popped.has_value()) ++cnt;
     } while (cnt < element_count);
   }
-  state.SetItemsProcessed(element_count * state.iterations());
+  state.counters["Added"] = element_count * state.iterations();
+  state.counters["Popped"] = state.counters["Added"];
+  state.counters["Steal Rate"] =
+      benchmark::Counter(state.counters["Popped"], benchmark::Counter::kIsRate);
 }
 BENCHMARK(BM_WorkQueueIntptrPopFront)
-    ->Range(1, 4096)
+    ->Setup(GlobalSetup)
+    ->Teardown(GlobalTeardown)
+    ->Range(1, 512)
     ->UseRealTime()
     ->MeasureProcessCPUTime();
 
-WorkQueue<intptr_t>* globalQueue;
-std::atomic_int globalProcessedCount{0};
-
 static void BM_MultithreadedWorkQueuePopBack(benchmark::State& state) {
-  if (state.thread_index() == 0) {
-    // Setup
-    globalQueue = new WorkQueue<intptr_t>();
-    globalProcessedCount = 0;
-  }
+  if (state.thread_index() == 0)
+    (*globalWorkQueueList)[0] = new WorkQueue<intptr_t>();
   int element_count = state.range(0);
   for (auto _ : state) {
     int cnt = 0;
-    for (int i = 0; i < element_count; i++) globalQueue->Add(1);
+    auto* queue = (*globalWorkQueueList)[0];
+    for (int i = 0; i < element_count; i++) queue->Add(1);
     absl::optional<intptr_t> popped;
     cnt = 0;
     do {
-      popped = globalQueue->PopBack();
+      popped = queue->PopBack();
       if (popped.has_value()) ++cnt;
     } while (cnt < element_count);
   }
-  globalProcessedCount += element_count * state.iterations();
+  state.counters["Added"] = element_count * state.iterations();
+  state.counters["Popped"] = state.counters["Added"];
+  state.counters["Steal Rate"] =
+      benchmark::Counter(state.counters["Popped"], benchmark::Counter::kIsRate);
   if (state.thread_index() == 0) {
-    // Teardown
-    assert(globalQueue->Empty());
-    delete globalQueue;
-    state.SetItemsProcessed(globalProcessedCount.load());
+    delete (*globalWorkQueueList)[0];
   }
 }
 BENCHMARK(BM_MultithreadedWorkQueuePopBack)
-    ->Range(1, 4096)
+    ->Setup(GlobalSetup)
+    ->Teardown(GlobalTeardown)
+    ->Range(1, 512)
     ->UseRealTime()
     ->MeasureProcessCPUTime()
     ->Threads(1)
     ->Threads(4)
     ->ThreadPerCpu();
 
-grpc_core::Mutex globalDequeMutex;
-std::deque<intptr_t>* globalDeque ABSL_GUARDED_BY(globalDequeMutex);
-
 static void BM_StdDequeLIFO(benchmark::State& state) {
-  if (state.thread_index() == 0) {
-    // Setup
-    globalProcessedCount = 0;
-    grpc_core::MutexLock lock(&globalDequeMutex);
-    globalDeque = new std::deque<intptr_t>();
-  }
+  if (state.thread_index() == 0)
+    (*globalDequeList)[0] = new std::deque<intptr_t>();
+  auto& mu = (*globalDequeMutexList)[0];
   int element_count = state.range(0);
   for (auto _ : state) {
+    auto* queue = (*globalDequeList)[0];
     for (int i = 0; i < element_count; i++) {
-      grpc_core::MutexLock lock(&globalDequeMutex);
-      globalDeque->emplace_back(1);
+      grpc_core::MutexLock lock(&mu);
+      queue->emplace_back(1);
     }
     for (int i = 0; i < element_count; i++) {
-      grpc_core::MutexLock lock(&globalDequeMutex);
-      intptr_t popped = globalDeque->back();
-      globalDeque->pop_back();
+      grpc_core::MutexLock lock(&mu);
+      intptr_t popped = queue->back();
+      queue->pop_back();
       assert(popped == 1);
     }
   }
-  globalProcessedCount += element_count * state.iterations();
+  state.counters["Added"] = element_count * state.iterations();
+  state.counters["Popped"] = state.counters["Added"];
+  state.counters["Steal Rate"] =
+      benchmark::Counter(state.counters["Popped"], benchmark::Counter::kIsRate);
   if (state.thread_index() == 0) {
-    // Teardown
-    grpc_core::MutexLock lock(&globalDequeMutex);
-    assert(globalDeque->empty());
-    delete globalDeque;
-    state.SetItemsProcessed(globalProcessedCount.load());
+    delete (*globalDequeList)[0];
   }
 }
 BENCHMARK(BM_StdDequeLIFO)
-    ->Range(1, 4096)
+    ->Setup(GlobalSetup)
+    ->Teardown(GlobalTeardown)
+    ->Range(1, 512)
     ->UseRealTime()
     ->MeasureProcessCPUTime()
     ->Threads(1)
     ->Threads(4)
     ->ThreadPerCpu();
+
+void PerThreadArguments(benchmark::internal::Benchmark* b) {
+  b->Setup(GlobalSetup)
+      ->Teardown(GlobalTeardown)
+      ->ArgsProduct({/*pop_attempts=*/{10, 50, 250},
+                     /*pct_fill=*/{2, 10, 50}})
+      ->UseRealTime()
+      ->MeasureProcessCPUTime()
+      ->Threads(10)
+      ->ThreadPerCpu();
+}
+
+static void BM_WorkQueuePerThread(benchmark::State& state) {
+  WorkQueue<intptr_t> local_queue;
+  {
+    grpc_core::MutexLock lock(&globalMu);
+    (*globalWorkQueueList)[state.thread_index()] = &local_queue;
+  }
+  int element_count = state.range(0);
+  float pct_fill = state.range(1) / 100.0;
+  for (auto _ : state) {
+    // sparsely populate a queue
+    for (int i = 0; i < std::ceil(element_count * pct_fill); i++) {
+      local_queue.Add(1);
+    }
+    // attempt to pop from all thread queues `element_count` times
+    int pop_attempts = 0;
+    auto iq = globalWorkQueueList->begin();
+    while (pop_attempts++ < element_count) {
+      // may not get a value if the queue being looked at from another thread
+      (*iq)->PopBack();
+      if (iq == globalWorkQueueList->end()) {
+        iq = globalWorkQueueList->begin();
+      } else {
+        iq++;
+      };
+    }
+  }
+  state.counters["Added"] =
+      std::ceil(element_count * pct_fill) * state.iterations();
+  state.counters["Steal Attempts"] = element_count * state.iterations();
+  state.counters["Steal Rate"] = benchmark::Counter(
+      state.counters["Steal Attempts"], benchmark::Counter::kIsRate);
+  if (state.thread_index() == 0) {
+    for (auto* queue : *globalWorkQueueList) {
+      assert(queue->Empty());
+    }
+  }
+}
+BENCHMARK(BM_WorkQueuePerThread)->Apply(PerThreadArguments);
+
+static void BM_StdDequePerThread(benchmark::State& state) {
+  std::deque<intptr_t> local_queue;
+  (*globalDequeList)[state.thread_index()] = &local_queue;
+  int element_count = state.range(0);
+  float pct_fill = state.range(1) / 100.0;
+  auto& local_mu = (*globalDequeMutexList)[state.thread_index()];
+  for (auto _ : state) {
+    // sparsely populate a queue
+    for (int i = 0; i < std::ceil(element_count * pct_fill); i++) {
+      grpc_core::MutexLock lock(&local_mu);
+      local_queue.emplace_back(1);
+    }
+    int pop_attempts = 0;
+    auto iq = globalDequeList->begin();
+    auto mu = globalDequeMutexList->begin();
+    while (pop_attempts++ < element_count) {
+      {
+        grpc_core::MutexLock lock(&*mu);
+        if (!(*iq)->empty()) {
+          assert((*iq)->back() == 1);
+          (*iq)->pop_back();
+        }
+      }
+      if (iq == globalDequeList->end()) {
+        iq = globalDequeList->begin();
+        mu = globalDequeMutexList->begin();
+      } else {
+        ++iq;
+        ++mu;
+      };
+    }
+  }
+  state.counters["Added"] =
+      std::ceil(element_count * pct_fill) * state.iterations();
+  state.counters["Steal Attempts"] = element_count * state.iterations();
+  state.counters["Steal Rate"] = benchmark::Counter(
+      state.counters["Steal Attempts"], benchmark::Counter::kIsRate);
+  if (state.thread_index() == 0) {
+    for (auto* queue : *globalDequeList) {
+      assert(queue->empty());
+    }
+  }
+}
+BENCHMARK(BM_StdDequePerThread)->Apply(PerThreadArguments);
+
 }  // namespace
 
 // Some distros have RunSpecifiedBenchmarks under the benchmark namespace,
