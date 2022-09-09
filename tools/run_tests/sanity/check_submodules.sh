d547df28721f3ef46c6d512634b10ca8024d69b1
AJ Heller <hork@google.com>
2022-09-08 18:01:56 -0700

    Upgrade google/benchmark to v1.7.0 (#30902)
    * Upgrade google/benchmark to v1.7.0
    
    Reason: Setup and Teardown methods are nice to have for multithreaded
    tests.
    
    * use version tag
    
    * Revert "use version tag"
    
    This reverts commit 6c7f5e0d35d830d40a6d2d80590e6a98493e152a.

--- tools/run_tests/sanity/check_submodules.sh
+++ tools/run_tests/sanity/check_submodules.sh
@@ -26,7 +26,7 @@ want_submodules=$(mktemp /tmp/submXXXXXX)
 git submodule | awk '{ print $2 " " $1 }' | sort >"$submodules"
 cat <<EOF | sort >"$want_submodules"
 third_party/abseil-cpp 273292d1cfc0a94a65082ee350509af1d113344d
-third_party/benchmark 0baacde3618ca617da95375e0af13ce1baadea47
+third_party/benchmark 361e8d1cfe0c6c36d30b39f1b61302ece5507320
 third_party/bloaty 60209eb1ccc34d5deefb002d1b7f37545204f7f2
 third_party/boringssl-with-bazel b9232f9e27e5668bc0414879dcdedb2a59ea75f2
 third_party/cares/cares 6654436a307a5a686b008c1d4c93b0085da6e6d8
