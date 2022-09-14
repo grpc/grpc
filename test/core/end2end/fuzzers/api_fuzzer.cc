c2ab8c99bb3104d1a4e017aba065938b3fa50767
Craig Tiller <ctiller@google.com>
2022-09-14 14:19:36 -0700

    Revert "Revert "[c++] Move environment functions to C++ (#30937)" (#30986)" (#30988)
    This reverts commit 96264e07b88e33c3b18fd43b107db1650684d4fe.

--- test/core/end2end/fuzzers/api_fuzzer.cc
+++ test/core/end2end/fuzzers/api_fuzzer.cc
@@ -53,8 +53,8 @@
 #include "src/core/lib/channel/channel_args.h"
 #include "src/core/lib/channel/channelz.h"
 #include "src/core/lib/event_engine/default_event_engine.h"
-#include "src/core/lib/gpr/env.h"
 #include "src/core/lib/gprpp/debug_location.h"
+#include "src/core/lib/gprpp/env.h"
 #include "src/core/lib/gprpp/time.h"
 #include "src/core/lib/iomgr/closure.h"
 #include "src/core/lib/iomgr/endpoint.h"
@@ -814,9 +814,9 @@ static grpc_channel_credentials* ReadChannelCreds(
 DEFINE_PROTO_FUZZER(const api_fuzzer::Msg& msg) {
   grpc_event_engine::experimental::ResetDefaultEventEngine();
   grpc_test_only_set_slice_hash_seed(0);
-  char* grpc_trace_fuzzer = gpr_getenv("GRPC_TRACE_FUZZER");
-  if (squelch && grpc_trace_fuzzer == nullptr) gpr_set_log_function(dont_log);
-  gpr_free(grpc_trace_fuzzer);
+  if (squelch && !grpc_core::GetEnv("GRPC_TRACE_FUZZER").has_value()) {
+    gpr_set_log_function(dont_log);
+  }
   grpc_set_tcp_client_impl(&fuzz_tcp_client_vtable);
   grpc_event_engine::experimental::SetDefaultEventEngineFactory(
       [actions = msg.event_engine_actions()]() {
