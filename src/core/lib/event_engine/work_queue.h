af306334ded33b7c791a152ed1b9dbc37b0009fe
AJ Heller <hork@google.com>
2022-09-15 15:59:38 -0700

    fix header guards

--- src/core/lib/event_engine/work_queue.h
+++ src/core/lib/event_engine/work_queue.h
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#ifndef GRPC_CORE_LIB_EVENT_ENGINE_WORKQUEUE_H
-#define GRPC_CORE_LIB_EVENT_ENGINE_WORKQUEUE_H
+#ifndef GRPC_CORE_LIB_EVENT_ENGINE_WORK_QUEUE_H
+#define GRPC_CORE_LIB_EVENT_ENGINE_WORK_QUEUE_H
 
 #include <grpc/support/port_platform.h>
 
@@ -122,4 +122,4 @@ class WorkQueue {
 }  // namespace experimental
 }  // namespace grpc_event_engine
 
-#endif  // GRPC_CORE_LIB_EVENT_ENGINE_WORKQUEUE_H
+#endif  // GRPC_CORE_LIB_EVENT_ENGINE_WORK_QUEUE_H
