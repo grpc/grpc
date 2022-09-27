ea9075f73b78472e10298e597699a671cba44ea1
AJ Heller <hork@google.com>
2022-09-26 19:25:55 -0700

    copy params for each lambda callback
    This fixes segfaults when we cannot ensure all callbacks are complete
    before exiting the test.

--- test/cpp/microbenchmarks/bm_event_engine_run.cc
+++ test/cpp/microbenchmarks/bm_event_engine_run.cc
@@ -23,7 +23,7 @@
 
 #include "src/core/lib/event_engine/common_closures.h"
 #include "src/core/lib/event_engine/default_event_engine.h"
-#include "src/core/lib/event_engine/promise.h"
+#include "src/core/lib/gprpp/notification.h"
 #include "test/core/util/test_config.h"
 #include "test/cpp/microbenchmarks/helpers.h"
 #include "test/cpp/util/test_config.h"
@@ -33,11 +33,7 @@ namespace {
 using ::grpc_event_engine::experimental::AnyInvocableClosure;
 using ::grpc_event_engine::experimental::EventEngine;
 using ::grpc_event_engine::experimental::GetDefaultEventEngine;
-using ::grpc_event_engine::experimental::Promise;
 using ::grpc_event_engine::experimental::ResetDefaultEventEngine;
-
-std::atomic_int gCnt{0};
-Promise<bool> gDone{false};
 
 struct FanoutParameters {
   int depth;
@@ -46,21 +42,21 @@ struct FanoutParameters {
 };
 
 void BM_EventEngine_RunSmallLambda(benchmark::State& state) {
-  int cb_count = state.range(0);
-  Promise<bool> p{false};
-  auto cb = [&p, cb_count]() {
-    if (++gCnt == cb_count) p.Set(true);
-  };
   auto engine = GetDefaultEventEngine();
+  const int cb_count = state.range(0);
+  std::atomic_int count{0};
   for (auto _ : state) {
+    state.PauseTiming();
+    grpc_core::Notification signal;
+    auto cb = [&signal, &count, cb_count]() {
+      if (++count == cb_count) signal.Notify();
+    };
+    state.ResumeTiming();
     for (int i = 0; i < cb_count; i++) {
       engine->Run(cb);
     }
-    p.Get();
-    state.PauseTiming();
-    p.Reset();
-    gCnt.store(0);
-    state.ResumeTiming();
+    signal.WaitForNotification();
+    count.store(0);
   }
   ResetDefaultEventEngine();
   state.SetItemsProcessed(cb_count * state.iterations());
@@ -72,23 +68,23 @@ BENCHMARK(BM_EventEngine_RunSmallLambda)
 
 void BM_EventEngine_RunLargeLambda(benchmark::State& state) {
   int cb_count = state.range(0);
-  Promise<bool> p{false};
   // large lambdas require an extra allocation
   std::string extra = "12345678";
-  auto cb = [&p, cb_count, extra]() {
-    (void)extra;
-    if (++gCnt == cb_count) p.Set(true);
-  };
   auto engine = GetDefaultEventEngine();
+  std::atomic_int count{0};
   for (auto _ : state) {
+    state.PauseTiming();
+    grpc_core::Notification signal;
+    auto cb = [&signal, &count, cb_count, extra]() {
+      (void)extra;
+      if (++count == cb_count) signal.Notify();
+    };
+    state.ResumeTiming();
     for (int i = 0; i < cb_count; i++) {
       engine->Run(cb);
     }
-    p.Get();
-    state.PauseTiming();
-    p.Reset();
-    gCnt.store(0);
-    state.ResumeTiming();
+    signal.WaitForNotification();
+    count.store(0);
   }
   ResetDefaultEventEngine();
   state.SetItemsProcessed(cb_count * state.iterations());
@@ -100,10 +96,11 @@ BENCHMARK(BM_EventEngine_RunLargeLambda)
 
 void BM_EventEngine_RunClosure(benchmark::State& state) {
   int cb_count = state.range(0);
-  Promise<bool> p{false};
-  AnyInvocableClosure closure([&p, cb_count]() {
-    if (++gCnt == cb_count) {
-      p.Set(true);
+  grpc_core::Notification* signal = new grpc_core::Notification();
+  std::atomic_int count{0};
+  AnyInvocableClosure closure([signal_holder = &signal, cb_count, &count]() {
+    if (++count == cb_count) {
+      (*signal_holder)->Notify();
     }
   });
   auto engine = GetDefaultEventEngine();
@@ -111,12 +108,14 @@ void BM_EventEngine_RunClosure(benchmark::State& state) {
     for (int i = 0; i < cb_count; i++) {
       engine->Run(&closure);
     }
-    p.Get();
+    signal->WaitForNotification();
     state.PauseTiming();
-    p.Reset();
-    gCnt.store(0);
+    delete signal;
+    signal = new grpc_core::Notification();
+    count.store(0);
     state.ResumeTiming();
   }
+  delete signal;
   ResetDefaultEventEngine();
   state.SetItemsProcessed(cb_count * state.iterations());
 }
@@ -126,17 +125,18 @@ BENCHMARK(BM_EventEngine_RunClosure)
     ->UseRealTime();
 
 void FanoutTestArguments(benchmark::internal::Benchmark* b) {
-  // TODO(hork): enable when the engines are fast enough to run more than 1
-  // iteration:
+  // TODO(hork): enable when the engines are fast enough to run these:
   // ->Args({10000, 1})  // chain of callbacks scheduling callbacks
   // ->Args({1, 10000})  // flat scheduling of callbacks
   // ->Args({5, 6})      // depth 5, fans out to 9,330 callbacks
+  //  ->Args({2, 100})   // depth 2, fans out 10,101 callbacks
+  //  ->Args({4, 10})    // depth 4, fans out to 11,110 callbacks
   b->Args({1000, 1})     // chain of callbacks scheduling callbacks
       ->Args({100, 1})   // chain of callbacks scheduling callbacks
       ->Args({1, 1000})  // flat scheduling of callbacks
       ->Args({1, 100})   // flat scheduling of callbacks
-      ->Args({2, 100})   // depth 2, fans out 10,101 callbacks
-      ->Args({4, 10})    // depth 4, fans out to 11,110 callbacks
+      ->Args({2, 70})    // depth 2, fans out 4971
+      ->Args({4, 8})     // depth 4, fans out 4681
       ->UseRealTime()
       ->MeasureProcessCPUTime();
 }
@@ -156,17 +156,26 @@ FanoutParameters GetFanoutParameters(benchmark::State& state) {
   return params;
 }
 
-void FanOutCallback(EventEngine* engine, const FanoutParameters& params,
+// EventEngine callback for Lambda FanOut tests
+//
+// Note that params are copied each time for 2 reasons: 1) callbacks will
+// inevitably continue to shut down after the end of the test, so a reference
+// parameter will become invalid and crash some callbacks, and 2) in my RBE
+// tests, copies are slightly faster than a shared_ptr<FanoutParams>
+// alternative.
+void FanOutCallback(EventEngine* engine, const FanoutParameters params,
+                    grpc_core::Notification& signal, std::atomic_int& count,
                     int processing_layer) {
-  int local_cnt = gCnt.fetch_add(1, std::memory_order_acq_rel);
-  if (local_cnt + 1 == params.limit) {
-    gDone.Set(true);
+  int local_cnt = count.fetch_add(1, std::memory_order_acq_rel) + 1;
+  if (local_cnt == params.limit) {
+    signal.Notify();
     return;
   }
+  GPR_DEBUG_ASSERT(local_cnt < params.limit);
   if (params.depth == processing_layer) return;
   for (int i = 0; i < params.fanout; i++) {
-    engine->Run([engine, &params, processing_layer]() {
-      FanOutCallback(engine, params, processing_layer + 1);
+    engine->Run([engine, params, processing_layer, &count, &signal]() {
+      FanOutCallback(engine, params, signal, count, processing_layer + 1);
     });
   }
 }
@@ -175,13 +184,12 @@ void BM_EventEngine_Lambda_FanOut(benchmark::State& state) {
   auto params = GetFanoutParameters(state);
   auto engine = GetDefaultEventEngine();
   for (auto _ : state) {
-    FanOutCallback(engine, params, /*processing_layer=*/0);
-    gDone.Get();
-    // cleanup
-    state.PauseTiming();
-    gCnt.store(0);
-    gDone.Reset();
-    state.ResumeTiming();
+    std::atomic_int count{0};
+    grpc_core::Notification signal;
+    FanOutCallback(engine, params, signal, count, /*processing_layer=*/0);
+    do {
+      signal.WaitForNotification();
+    } while (count.load() != params.limit);
   }
   state.SetItemsProcessed(params.limit * state.iterations());
 }
@@ -189,11 +197,17 @@ BENCHMARK(BM_EventEngine_Lambda_FanOut)->Apply(FanoutTestArguments);
 
 void ClosureFanOutCallback(EventEngine::Closure* child_closure,
                            EventEngine* engine,
-                           const FanoutParameters& params) {
-  int local_cnt = gCnt.fetch_add(1, std::memory_order_acq_rel);
-  if (local_cnt + 1 == params.limit) {
-    gDone.Set(true);
+                           grpc_core::Notification** signal_holder,
+                           std::atomic_int& count,
+                           const FanoutParameters params) {
+  int local_cnt = count.fetch_add(1, std::memory_order_acq_rel) + 1;
+  if (local_cnt == params.limit) {
+    (*signal_holder)->Notify();
     return;
+  }
+  if (local_cnt > params.limit) {
+    gpr_log(GPR_ERROR, "Ran too many closures: %d/%d", local_cnt, params.limit);
+    abort();
   }
   if (child_closure == nullptr) return;
   for (int i = 0; i < params.fanout; i++) {
@@ -207,25 +221,32 @@ void BM_EventEngine_Closure_FanOut(benchmark::State& state) {
   std::vector<EventEngine::Closure*> closures;
   closures.reserve(params.depth + 2);
   closures.push_back(nullptr);
+  grpc_core::Notification* signal = new grpc_core::Notification();
+  std::atomic_int count{0};
   // prepare a unique closure for each depth
   for (int i = 0; i <= params.depth; i++) {
     // call the previous closure (e.g., closures[2] calls closures[1] during
     // fanout)
-    closures.push_back(
-        new AnyInvocableClosure([i, engine, &closures, &params]() {
-          ClosureFanOutCallback(closures[i], engine, params);
+    closures.push_back(new AnyInvocableClosure(
+        [i, engine, &closures, params, signal_holder = &signal, &count]() {
+          ClosureFanOutCallback(closures[i], engine, signal_holder, count,
+                                params);
         }));
   }
   for (auto _ : state) {
-    GPR_DEBUG_ASSERT(gCnt.load(std::memory_order_relaxed) == 0);
+    GPR_DEBUG_ASSERT(count.load(std::memory_order_relaxed) == 0);
     engine->Run(closures[params.depth + 1]);
-    gDone.Get();
+    do {
+      signal->WaitForNotification();
+    } while (count.load() != params.limit);
     // cleanup
     state.PauseTiming();
-    gCnt.store(0);
-    gDone.Reset();
+    delete signal;
+    signal = new grpc_core::Notification();
+    count.store(0);
     state.ResumeTiming();
   }
+  delete signal;
   state.SetItemsProcessed(params.limit * state.iterations());
   for (auto i : closures) delete i;
 }
