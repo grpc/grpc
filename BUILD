2ee2c91c92bcd7ca2f79d727de68f59350b1b4ad
AJ Heller <hork@google.com>
2022-09-28 08:39:56 -0700

    Reland x2: Make GetDefaultEventEngine return a shared_ptr (#30619)
    * Reland x2: Make GetDefaultEventEngine return a shared_ptr
    
    * remove thread leak from NativeDNSResolver
    
    This is not going to work for resolvers that support cancellation.
    
    * give resolvers bounded lifetimes
    
    Some resolver own EventEngines. EventEngines cannot run off the end of
    the process since they have unjoined threads (problematic in a small set
    of environments). This gives resolvers bounded lifetimes, and allows
    replacement of resolvers without ASAN issues of deleting resolvers in
    active use (occurs in tests).
    
    * fix
    
    * fix windows
    
    * fix surface init test
    
    * fix
    
    * sanitize
    
    * use after move
    
    * the test must wait for the callback to be destroyed
    
    * windows fix: delete the resolver on iomgr shutdown, not before
    
    * Make TimerManager threads non-joinable
    
    On gRPC shutdown, any unjoined TimerManager threads will cause TSAN to
    detect thread leaks. This fix resolves issues I saw in end2end test
    shutdown in another PR, where a single timer manager thread was always
    alive after the test ended.
    
    The long-term solution is to integrate the new ThreadPool here, but this
    unblocks me for now.
    
    * backport fix
    
    * fix
    
    * shared_ptr<EventEngine> in EventEngine benchmarks

--- BUILD
+++ BUILD
@@ -1423,6 +1423,7 @@ grpc_cc_library(
     external_deps = ["absl/status"],
     deps = [
         "activity",
+        "context",
         "default_event_engine",
         "event_engine_base_hdrs",
         "exec_ctx",
@@ -1665,6 +1666,7 @@ grpc_cc_library(
         "absl/status",
         "absl/types:optional",
         "absl/types:variant",
+        "absl/utility",
     ],
     language = "c++",
     public_hdrs = [
@@ -2985,9 +2987,13 @@ grpc_cc_library(
     ],
     external_deps = ["absl/functional:any_invocable"],
     deps = [
+        "context",
         "default_event_engine_factory",
         "event_engine_base_hdrs",
+        "event_engine_trace",
         "gpr",
+        "grpc_trace",
+        "no_destruct",
     ],
 )
 
@@ -3416,6 +3422,7 @@ grpc_cc_library(
         "json",
         "latch",
         "memory_quota",
+        "no_destruct",
         "notification",
         "orphanable",
         "packed_table",
@@ -4066,6 +4073,7 @@ grpc_cc_library(
         "closure",
         "config",
         "debug_location",
+        "default_event_engine",
         "exec_ctx",
         "exec_ctx_wakeup_scheduler",
         "gpr",
--- test/core/end2end/fuzzers/api_fuzzer.cc
+++ test/core/end2end/fuzzers/api_fuzzer.cc
@@ -138,6 +138,7 @@ static void finish_resolve(void* arg, grpc_error_handle error) {
 
 namespace {
 
+using ::grpc_event_engine::experimental::FuzzingEventEngine;
 using ::grpc_event_engine::experimental::GetDefaultEventEngine;
 
 class FuzzerDNSResolver : public grpc_core::DNSResolver {
@@ -177,11 +178,7 @@ class FuzzerDNSResolver : public grpc_core::DNSResolver {
     grpc_timer timer_;
   };
 
-  // Gets the singleton instance, possibly creating it first
-  static FuzzerDNSResolver* GetOrCreate() {
-    static FuzzerDNSResolver* instance = new FuzzerDNSResolver();
-    return instance;
-  }
+  explicit FuzzerDNSResolver(FuzzingEventEngine* engine) : engine_(engine) {}
 
   TaskHandle LookupHostname(
       std::function<void(absl::StatusOr<std::vector<grpc_resolved_address>>)>
@@ -206,7 +203,7 @@ class FuzzerDNSResolver : public grpc_core::DNSResolver {
       absl::string_view /* name */, grpc_core::Duration /* timeout */,
       grpc_pollset_set* /* interested_parties */,
       absl::string_view /* name_server */) override {
-    GetDefaultEventEngine()->Run([on_resolved] {
+    engine_->Run([on_resolved] {
       grpc_core::ApplicationCallbackExecCtx app_exec_ctx;
       grpc_core::ExecCtx exec_ctx;
       on_resolved(absl::UnimplementedError(
@@ -221,7 +218,7 @@ class FuzzerDNSResolver : public grpc_core::DNSResolver {
       grpc_pollset_set* /* interested_parties */,
       absl::string_view /* name_server */) override {
     // Not supported
-    GetDefaultEventEngine()->Run([on_resolved] {
+    engine_->Run([on_resolved] {
       grpc_core::ApplicationCallbackExecCtx app_exec_ctx;
       grpc_core::ExecCtx exec_ctx;
       on_resolved(absl::UnimplementedError(
@@ -232,6 +229,9 @@ class FuzzerDNSResolver : public grpc_core::DNSResolver {
 
   // FuzzerDNSResolver does not support cancellation.
   bool Cancel(TaskHandle /*handle*/) override { return false; }
+
+ private:
+  FuzzingEventEngine* engine_;
 };
 
 }  // namespace
@@ -820,21 +820,23 @@ DEFINE_PROTO_FUZZER(const api_fuzzer::Msg& msg) {
     gpr_set_log_function(dont_log);
   }
   grpc_set_tcp_client_impl(&fuzz_tcp_client_vtable);
-  grpc_event_engine::experimental::SetDefaultEventEngineFactory(
+
+  grpc_event_engine::experimental::SetEventEngineFactory(
       [actions = msg.event_engine_actions()]() {
-        return absl::make_unique<
-            grpc_event_engine::experimental::FuzzingEventEngine>(
-            grpc_event_engine::experimental::FuzzingEventEngine::Options(),
-            actions);
+        return absl::make_unique<FuzzingEventEngine>(
+            FuzzingEventEngine::Options(), actions);
       });
-  grpc_event_engine::experimental::GetDefaultEventEngine();
+  auto engine =
+      std::dynamic_pointer_cast<FuzzingEventEngine>(GetDefaultEventEngine());
+  FuzzingEventEngine::SetGlobalNowImplEngine(engine.get());
   grpc_init();
   grpc_timer_manager_set_threading(false);
   {
     grpc_core::ExecCtx exec_ctx;
     grpc_core::Executor::SetThreadingAll(false);
   }
-  grpc_core::SetDNSResolver(FuzzerDNSResolver::GetOrCreate());
+  grpc_core::ResetDNSResolver(
+      absl::make_unique<FuzzerDNSResolver>(engine.get()));
   grpc_dns_lookup_hostname_ares = my_dns_lookup_ares;
   grpc_cancel_ares_request = my_cancel_ares_request;
 
@@ -871,14 +873,10 @@ DEFINE_PROTO_FUZZER(const api_fuzzer::Msg& msg) {
   while (action_index < msg.actions_size() || g_channel != nullptr ||
          g_server != nullptr || pending_channel_watches > 0 ||
          pending_pings > 0 || ActiveCall() != nullptr) {
-    static_cast<grpc_event_engine::experimental::FuzzingEventEngine*>(
-        grpc_event_engine::experimental::GetDefaultEventEngine())
-        ->Tick();
+    engine->Tick();
 
     if (action_index == msg.actions_size()) {
-      static_cast<grpc_event_engine::experimental::FuzzingEventEngine*>(
-          grpc_event_engine::experimental::GetDefaultEventEngine())
-          ->FuzzingDone();
+      engine->FuzzingDone();
       if (g_channel != nullptr) {
         grpc_channel_destroy(g_channel);
         g_channel = nullptr;
@@ -1225,4 +1223,5 @@ DEFINE_PROTO_FUZZER(const api_fuzzer::Msg& msg) {
 
   grpc_resource_quota_unref(g_resource_quota);
   grpc_shutdown_blocking();
+  FuzzingEventEngine::UnsetGlobalNowImplEngine(engine.get());
 }
