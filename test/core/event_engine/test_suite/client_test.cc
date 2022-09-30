c2f7aa261617d11873700a307e9fca2037978903
Vignesh Babu <vigneshbabu@google.com>
2022-09-30 17:43:27 +0000

    iwyu

--- test/core/event_engine/test_suite/client_test.cc
+++ test/core/event_engine/test_suite/client_test.cc
@@ -30,8 +30,10 @@
 
 #include <grpc/event_engine/event_engine.h>
 #include <grpc/event_engine/memory_allocator.h>
+#include <grpc/grpc.h>
 #include <grpc/support/log.h>
 
+#include "src/core/lib/channel/channel_args.h"
 #include "src/core/lib/event_engine/channel_args_endpoint_config.h"
 #include "src/core/lib/gprpp/notification.h"
 #include "src/core/lib/iomgr/exec_ctx.h"
