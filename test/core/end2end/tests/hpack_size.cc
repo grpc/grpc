1e8817eddf1ba09537b6f0963c77dd5ccf87b02e
Craig Tiller <ctiller@google.com>
2022-10-04 10:52:59 -0700

    fix

--- test/core/end2end/tests/hpack_size.cc
+++ test/core/end2end/tests/hpack_size.cc
@@ -22,6 +22,7 @@
 
 #include <algorithm>
 #include <memory>
+#include <queue>
 #include <string>
 #include <vector>
 
@@ -393,19 +394,24 @@ void hpack_size(grpc_end2end_test_config config) {
   size_t i, j;
 
   auto event_engine = grpc_event_engine::experimental::GetDefaultEventEngine();
-  std::vector<std::shared_ptr<absl::Notification>> dones;
+  std::queue<std::shared_ptr<absl::Notification>> dones;
   for (i = 0; i < GPR_ARRAY_SIZE(interesting_sizes); i++) {
     for (j = 0; j < GPR_ARRAY_SIZE(interesting_sizes); j++) {
       auto done = std::make_shared<absl::Notification>();
-      dones.push_back(done);
+      while (dones.size() > 4) {
+        dones.front()->WaitForNotification();
+        dones.pop();
+      }
+      dones.push(done);
       event_engine->Run([done, config, i, j] {
         test_size(config, interesting_sizes[i], interesting_sizes[j]);
         done->Notify();
       });
     }
   }
-  for (auto& done : dones) {
-    done->WaitForNotification();
+  while (!dones.empty()) {
+    dones.front()->WaitForNotification();
+    dones.pop();
   }
 }
 
