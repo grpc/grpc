ff2625a7fc00badb5dccc4664db931d4f0a3954c
Mark D. Roth <roth@google.com>
2022-10-07 18:31:04 +0000

    fix test

--- test/core/xds/xds_client_test.cc
+++ test/core/xds/xds_client_test.cc
@@ -366,7 +366,7 @@ class XdsClientTest : public ::testing::Test {
       }
       return result;
     }
-    void InitUpbSymtab(upb_DefPool* /*symtab*/) const override {}
+    void InitUpbSymtab(XdsClient*, upb_DefPool* /*symtab*/) const override {}
 
     static google::protobuf::Any EncodeAsAny(const ResourceStruct& resource) {
       google::protobuf::Any any;
