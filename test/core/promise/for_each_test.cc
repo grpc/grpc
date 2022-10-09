2083fca5c824bae3ec1913bb1781feac729c383f
Craig Tiller <ctiller@google.com>
2022-10-09 15:09:50 -0700

    fix

--- test/core/promise/for_each_test.cc
+++ test/core/promise/for_each_test.cc
@@ -99,6 +99,7 @@ class MoveableUntilPolled {
   }
 
   Poll<absl::Status> operator()() {
+    Activity::current()->ForceImmediateRepoll();
     ++polls_;
     if (polls_ == 10) return absl::OkStatus();
     return Pending();
