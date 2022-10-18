3981105be660fea2c62eeb29642188c79f2e2a1b
Cheng-Yu Chung <chengyuc@google.com>
2022-10-18 11:34:24 -0400

    Remove `include/grpcpp/impl/codegen/status_code_enum.h` (#31284)

--- src/cpp/client/secure_credentials.cc
+++ src/cpp/client/secure_credentials.cc
@@ -42,6 +42,7 @@
 #include <grpcpp/support/channel_arguments.h>
 #include <grpcpp/support/slice.h>
 #include <grpcpp/support/status.h>
+#include <grpcpp/support/status_code_enum.h>
 
 #include "src/core/lib/event_engine/default_event_engine.h"
 #include "src/core/lib/gprpp/env.h"
