957729089a705c0c988d7593ea75801126d89820
Craig Tiller <ctiller@google.com>
2022-10-19 20:03:09 -0700

    fix

--- src/core/lib/channel/promise_based_filter.cc
+++ src/core/lib/channel/promise_based_filter.cc
@@ -25,7 +25,6 @@
 #include <grpc/status.h>
 
 #include "src/core/lib/channel/channel_stack.h"
-#include "src/core/lib/event_engine/default_event_engine.h"
 #include "src/core/lib/gprpp/manual_constructor.h"
 #include "src/core/lib/gprpp/status_helper.h"
 #include "src/core/lib/iomgr/error.h"
