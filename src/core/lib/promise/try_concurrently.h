7bf2d030b275d4d64b2f389e0822dfe6ee6be8ff
Craig Tiller <ctiller@google.com>
2022-10-22 00:41:58 -0700

    [promise] CallPushPull -> more general TryConcurrently

---
+++ src/core/lib/promise/try_concurrently.h
@@ -0,0 +1,281 @@
+// Copyright 2022 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GRPC_CORE_LIB_PROMISE_TRY_CONCURRENTLY_H
+#define GRPC_CORE_LIB_PROMISE_TRY_CONCURRENTLY_H
+
+#include <grpc/support/port_platform.h>
+
+#include <stddef.h>
+
+#include <cstdint>
+#include <utility>
+
+#include "absl/types/variant.h"
+
+#include <grpc/support/log.h>
+
+#include "src/core/lib/gprpp/construct_destruct.h"
+#include "src/core/lib/promise/detail/promise_like.h"
+#include "src/core/lib/promise/detail/status.h"
+#include "src/core/lib/promise/poll.h"
+
+namespace grpc_core {
+
+namespace promise_detail {
+
+template <typename Promise>
+struct Necessary {
+  PromiseLike<Promise> promise;
+  static constexpr bool must_complete() { return true; }
+};
+
+template <typename Promise>
+struct Helper {
+  PromiseLike<Promise> promise;
+  static constexpr bool must_complete() { return false; }
+};
+
+template <typename... Ts>
+class FusedSet;
+
+template <typename T, typename... Ts>
+class FusedSet<T, Ts...> : public FusedSet<Ts...> {
+ public:
+  explicit FusedSet(T&& x, Ts&&... xs)
+      : FusedSet<Ts...>(std::forward<T>(xs)...) {
+    Construct(&wrapper_, std::forward<T>(x));
+  }
+  explicit FusedSet(T&& x, FusedSet<Ts...>&& xs)
+      : FusedSet<Ts...>(std::forward<FusedSet<Ts...>>(xs)) {
+    Construct(&wrapper_, std::forward<T>(x));
+  }
+  // Empty destructor: consumers must call Destroy() to ensure cleanup occurs
+  ~FusedSet() {}
+
+  FusedSet(const FusedSet&) = delete;
+  FusedSet& operator=(const FusedSet&) = delete;
+
+  // Assumes all 'done_bits' are 0
+  FusedSet(FusedSet&& other) noexcept : FusedSet<Ts...>(std::move(other)) {
+    Construct(&wrapper_, std::move(other.wrapper_));
+  }
+  FusedSet& operator=(FusedSet&& other) noexcept {
+    FusedSet<Ts...>::operator=(std::move(other));
+    wrapper_ = std::move(other.wrapper_);
+    return *this;
+  }
+
+  static constexpr size_t Size() { return 1 + sizeof...(Ts); }
+
+  static constexpr uint8_t NecessaryBits() {
+    return (T::must_complete() ? 1 : 0) |
+           (FusedSet<Ts...>::NecessaryBits() << 1);
+  }
+
+  template <int kDoneBit>
+  void Destroy(uint8_t done_bits) {
+    if ((done_bits & (1 << kDoneBit)) == 0) {
+      Destruct(&wrapper_);
+    }
+    FusedSet<Ts...>::template Destroy<kDoneBit + 1>(done_bits);
+  }
+
+  template <typename Result, int kDoneBit>
+  Poll<Result> Run(uint8_t& done_bits) {
+    if ((done_bits & (1 << kDoneBit)) == 0) {
+      auto p = wrapper_.promise();
+      if (auto* status = absl::get_if<kPollReadyIdx>(&p)) {
+        done_bits |= (1 << kDoneBit);
+        Destruct(&wrapper_);
+        if (!IsStatusOk(*status)) {
+          return StatusCast<Result>(std::move(*status));
+        }
+      }
+    }
+    return FusedSet<Ts...>::template Run<Result, kDoneBit + 1>(done_bits);
+  }
+
+  template <typename P>
+  FusedSet<P, T, Ts...> With(P x) {
+    return FusedSet<P, T, Ts...>(std::move(x), std::move(*this));
+  }
+
+ private:
+  union {
+    T wrapper_;
+  };
+};
+
+template <>
+class FusedSet<> {
+ public:
+  static constexpr size_t Size() { return 0; }
+  static constexpr uint8_t NecessaryBits() { return 0; }
+
+  template <typename Result, int kDoneBit>
+  Poll<Result> Run(uint8_t) {
+    return Pending{};
+  }
+  template <int kDoneBit>
+  void Destroy(uint8_t) {}
+
+  template <typename P>
+  FusedSet<P> With(P x) {
+    return FusedSet<P>(std::move(x));
+  }
+};
+
+template <typename Main, typename PreMain, typename PostMain>
+class TryConcurrently {
+ public:
+  TryConcurrently(Main main, PreMain pre_main, PostMain post_main)
+      : done_bits_(0),
+        pre_main_(std::move(pre_main)),
+        post_main_(std::move(post_main)) {
+    Construct(&main_, std::move(main));
+  }
+
+  TryConcurrently(const TryConcurrently&) = delete;
+  TryConcurrently& operator=(const TryConcurrently&) = delete;
+  TryConcurrently(TryConcurrently&& other) noexcept
+      : done_bits_(0),
+        pre_main_(std::move(other.pre_main_)),
+        post_main_(std::move(other.post_main_)) {
+    GPR_ASSERT(other.done_bits_ == 0);
+    Construct(&main_, std::move(other.main_));
+  }
+  TryConcurrently& operator=(TryConcurrently&& other) noexcept {
+    GPR_ASSERT(other.done_bits_ == 0);
+    done_bits_ = 0;
+    pre_main_ = std::move(other.pre_main_);
+    post_main_ = std::move(other.post_main_);
+    Construct(&main_, std::move(other.main_));
+    return *this;
+  }
+
+  ~TryConcurrently() {
+    if (done_bits_ & 1) {
+      Destruct(&result_);
+    } else {
+      Destruct(&main_);
+    }
+    pre_main_.template Destroy<1>(done_bits_);
+    post_main_.template Destroy<1 + PreMain::Size()>(done_bits_);
+  }
+
+  using Result =
+      typename PollTraits<decltype(std::declval<PromiseLike<Main>>()())>::Type;
+
+  Poll<Result> operator()() {
+    auto r = pre_main_.template Run<Result, 1>(done_bits_);
+    if (auto* status = absl::get_if<Result>(&r)) {
+      GPR_ASSERT(!IsStatusOk(*status));
+      return std::move(*status);
+    }
+    if ((done_bits_ & 1) == 0) {
+      auto p = main_();
+      if (auto* status = absl::get_if<kPollReadyIdx>(&p)) {
+        done_bits_ |= 1;
+        Destruct(&main_);
+        Construct(&result_, std::move(*status));
+      }
+    }
+    r = post_main_.template Run<Result, 1 + PreMain::Size()>(done_bits_);
+    if (auto* status = absl::get_if<Result>(&r)) {
+      GPR_ASSERT(!IsStatusOk(*status));
+      return std::move(*status);
+    }
+    static const uint8_t kNecessaryBits =
+        1 | (PreMain::NecessaryBits() << 1) |
+        (PostMain::NecessaryBits() << (1 + PreMain::Size()));
+    if ((done_bits_ & kNecessaryBits) == kNecessaryBits) {
+      return std::move(result_);
+    }
+    return Pending{};
+  }
+
+  template <typename P>
+  auto NecessaryPush(P p);
+  template <typename P>
+  auto NecessaryPull(P p);
+  template <typename P>
+  auto Push(P p);
+  template <typename P>
+  auto Pull(P p);
+
+ private:
+  uint8_t done_bits_;
+  PreMain pre_main_;
+  union {
+    PromiseLike<Main> main_;
+    Result result_;
+  };
+  PostMain post_main_;
+};
+
+template <typename Main, typename PreMain, typename PostMain>
+auto MakeTryConcurrently(Main&& main, PreMain&& pre_main,
+                         PostMain&& post_main) {
+  return TryConcurrently<Main, PreMain, PostMain>(
+      std::forward<Main>(main), std::forward<PreMain>(pre_main),
+      std::forward<PostMain>(post_main));
+}
+
+template <typename Main, typename PreMain, typename PostMain>
+template <typename P>
+auto TryConcurrently<Main, PreMain, PostMain>::NecessaryPush(P p) {
+  GPR_DEBUG_ASSERT(done_bits_ == 0);
+  return MakeTryConcurrently(std::move(main_),
+                             pre_main_.With(Necessary<P>{std::move(p)}),
+                             std::move(post_main_));
+}
+
+template <typename Main, typename PreMain, typename PostMain>
+template <typename P>
+auto TryConcurrently<Main, PreMain, PostMain>::NecessaryPull(P p) {
+  GPR_DEBUG_ASSERT(done_bits_ == 0);
+  return MakeTryConcurrently(std::move(main_), std::move(pre_main_),
+                             post_main_.With(Necessary<P>{std::move(p)}));
+}
+
+template <typename Main, typename PreMain, typename PostMain>
+template <typename P>
+auto TryConcurrently<Main, PreMain, PostMain>::Push(P p) {
+  GPR_DEBUG_ASSERT(done_bits_ == 0);
+  return MakeTryConcurrently(std::move(main_),
+                             pre_main_.With(Helper<P>{std::move(p)}),
+                             std::move(post_main_));
+}
+
+template <typename Main, typename PreMain, typename PostMain>
+template <typename P>
+auto TryConcurrently<Main, PreMain, PostMain>::Pull(P p) {
+  GPR_DEBUG_ASSERT(done_bits_ == 0);
+  return MakeTryConcurrently(std::move(main_), std::move(pre_main_),
+                             post_main_.With(Helper<P>{std::move(p)}));
+}
+
+}  // namespace promise_detail
+
+template <typename Main>
+auto TryConcurrently(Main main) {
+  return promise_detail::MakeTryConcurrently(std::move(main),
+                                             promise_detail::FusedSet<>(),
+                                             promise_detail::FusedSet<>());
+}
+
+}  // namespace grpc_core
+
+#endif  // GRPC_CORE_LIB_PROMISE_TRY_CONCURRENTLY_H
---
+++ test/core/promise/try_concurrently_test.cc
@@ -0,0 +1,83 @@
+// Copyright 2022 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/core/lib/promise/try_concurrently.h"
+
+#include "absl/status/status.h"
+#include "gtest/gtest.h"
+
+namespace grpc_core {
+
+TEST(TryConcurrentlyTest, Immediate) {
+  auto a = TryConcurrently([] { return absl::OkStatus(); });
+  EXPECT_EQ(a(), Poll<absl::Status>(absl::OkStatus()));
+  auto b = TryConcurrently([] { return absl::OkStatus(); }).NecessaryPush([] {
+    return absl::OkStatus();
+  });
+  EXPECT_EQ(b(), Poll<absl::Status>(absl::OkStatus()));
+  auto c = TryConcurrently([] { return absl::OkStatus(); }).NecessaryPull([] {
+    return absl::OkStatus();
+  });
+  EXPECT_EQ(c(), Poll<absl::Status>(absl::OkStatus()));
+  auto d = TryConcurrently([] {
+             return absl::OkStatus();
+           }).NecessaryPull([] {
+               return absl::OkStatus();
+             }).NecessaryPush([] { return absl::OkStatus(); });
+  EXPECT_EQ(d(), Poll<absl::Status>(absl::OkStatus()));
+  auto e = TryConcurrently([] {
+             return absl::OkStatus();
+           }).Push([]() -> Poll<absl::Status> { return Pending{}; });
+  EXPECT_EQ(e(), Poll<absl::Status>(absl::OkStatus()));
+  auto f = TryConcurrently([] {
+             return absl::OkStatus();
+           }).Pull([]() -> Poll<absl::Status> { return Pending{}; });
+  EXPECT_EQ(f(), Poll<absl::Status>(absl::OkStatus()));
+}
+
+TEST(TryConcurrentlyTest, Paused) {
+  auto a = TryConcurrently([]() -> Poll<absl::Status> { return Pending{}; });
+  EXPECT_EQ(a(), Poll<absl::Status>(Pending{}));
+  auto b = TryConcurrently([]() {
+             return absl::OkStatus();
+           }).NecessaryPush([]() -> Poll<absl::Status> { return Pending{}; });
+  EXPECT_EQ(b(), Poll<absl::Status>(Pending{}));
+  auto c = TryConcurrently([]() {
+             return absl::OkStatus();
+           }).NecessaryPull([]() -> Poll<absl::Status> { return Pending{}; });
+  EXPECT_EQ(c(), Poll<absl::Status>(Pending{}));
+}
+
+TEST(TryConcurrentlyTest, OneFailed) {
+  auto a = TryConcurrently(
+      []() -> Poll<absl::Status> { return absl::UnknownError("bah"); });
+  EXPECT_EQ(a(), Poll<absl::Status>(absl::UnknownError("bah")));
+  auto b = TryConcurrently([]() -> Poll<absl::Status> {
+             return Pending{};
+           }).NecessaryPush([] { return absl::UnknownError("humbug"); });
+  EXPECT_EQ(b(), Poll<absl::Status>(absl::UnknownError("humbug")));
+  auto c = TryConcurrently([]() -> Poll<absl::Status> {
+             return Pending{};
+           }).NecessaryPull([]() -> Poll<absl::Status> {
+    return absl::UnknownError("wha");
+  });
+  EXPECT_EQ(c(), Poll<absl::Status>(absl::UnknownError("wha")));
+}
+
+}  // namespace grpc_core
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
