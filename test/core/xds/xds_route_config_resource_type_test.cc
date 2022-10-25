5be0fd117c299e513cbf5fa3e1488b1ddd338de5
Mark D. Roth <roth@google.com>
2022-10-25 18:52:34 +0000

    WIP

--- test/core/xds/xds_route_config_resource_type_test.cc
+++ test/core/xds/xds_route_config_resource_type_test.cc
@@ -44,13 +44,16 @@
 #include "src/core/lib/gprpp/time.h"
 #include "src/core/lib/iomgr/error.h"
 #include "src/core/lib/json/json.h"
+#include "src/proto/grpc/lookup/v1/rls_config.pb.h"
 #include "src/proto/grpc/testing/xds/v3/fault.pb.h"
 #include "src/proto/grpc/testing/xds/v3/http_filter_rbac.pb.h"
 #include "src/proto/grpc/testing/xds/v3/route.pb.h"
 #include "src/proto/grpc/testing/xds/v3/typed_struct.pb.h"
 #include "test/core/util/test_config.h"
+#include "test/core/util/scoped_env_var.h"
 
 using envoy::config::route::v3::RouteConfiguration;
+using grpc::lookup::v1::RouteLookupClusterSpecifier;
 
 namespace grpc_core {
 namespace testing {
@@ -159,10 +162,72 @@ TEST_F(XdsRouteConfigTest, MinimumValidConfig) {
 }
 
 //
-// cluster type tests
+// RLS tests
 //
 
-// using ClusterTypeTest = XdsRouteConfigTest;
+using RlsTest = XdsRouteConfigTest;
+
+TEST_F(RlsTest, Basic) {
+  ScopedExperimentalEnvVar env_var("GRPC_EXPERIMENTAL_XDS_RLS_LB");
+  RouteConfiguration route_config;
+  route_config.set_name("foo");
+  auto* cluster_specifier_plugin = route_config.add_cluster_specifier_plugins();
+  auto* typed_extension_config = cluster_specifier_plugin->mutable_extension();
+  typed_extension_config->set_name("rls");
+  RouteLookupClusterSpecifier rls_cluster_specifier;
+  auto* rls_config = rls_cluster_specifier.mutable_route_lookup_config();
+  rls_config->set_cache_size_bytes(1024);
+  rls_config->set_lookup_service("rls.example.com");
+  auto* grpc_keybuilder = rls_config->add_grpc_keybuilders();
+  grpc_keybuilder->add_names()->set_service("service");
+  typed_extension_config->mutable_typed_config()->PackFrom(
+      rls_cluster_specifier);
+  auto* vhost = route_config.add_virtual_hosts();
+  vhost->add_domains("*");
+  auto* route_proto = vhost->add_routes();
+  route_proto->mutable_match()->set_prefix("");
+  route_proto->mutable_route()->set_cluster_specifier_plugin("rls");
+  std::string serialized_resource;
+  ASSERT_TRUE(route_config.SerializeToString(&serialized_resource));
+  auto* resource_type = XdsRouteConfigResourceType::Get();
+  auto decode_result =
+      resource_type->Decode(decode_context_, serialized_resource);
+  ASSERT_TRUE(decode_result.resource.ok()) << decode_result.resource.status();
+  ASSERT_TRUE(decode_result.name.has_value());
+  EXPECT_EQ(*decode_result.name, "foo");
+  auto& resource =
+      static_cast<XdsRouteConfigResource&>(**decode_result.resource);
+  EXPECT_THAT(
+      resource.cluster_specifier_plugin_map,
+      ::testing::ElementsAre(
+          ::testing::Pair(
+              "rls",
+              "[{\"rls_experimental\":{"
+              "\"childPolicy\":[{\"cds_experimental\":{}}],"
+              "\"childPolicyConfigTargetFieldName\":\"cluster\","
+              "\"routeLookupConfig\":{"
+              "\"cacheSizeBytes\":\"1024\","
+              "\"grpcKeybuilders\":[{\"names\":[{\"service\":\"service\"}]}],"
+              "\"lookupService\":\"rls.example.com\"}}}]")));
+  ASSERT_EQ(resource.virtual_hosts.size(), 1UL);
+  EXPECT_THAT(resource.virtual_hosts[0].domains, ::testing::ElementsAre("*"));
+  EXPECT_THAT(resource.virtual_hosts[0].typed_per_filter_config,
+              ::testing::ElementsAre());
+  ASSERT_EQ(resource.virtual_hosts[0].routes.size(), 1UL);
+  auto& route = resource.virtual_hosts[0].routes[0];
+  auto& matchers = route.matchers;
+  EXPECT_EQ(matchers.path_matcher.ToString(), "StringMatcher{prefix=}");
+  EXPECT_THAT(matchers.header_matchers, ::testing::ElementsAre());
+  EXPECT_FALSE(matchers.fraction_per_million.has_value());
+  auto* action =
+      absl::get_if<XdsRouteConfigResource::Route::RouteAction>(&route.action);
+  ASSERT_NE(action, nullptr);
+  auto* plugin_name = absl::get_if<
+      XdsRouteConfigResource::Route::RouteAction::ClusterSpecifierPluginName>(
+          &action->action);
+  ASSERT_NE(plugin_name, nullptr);
+  EXPECT_EQ(plugin_name->cluster_specifier_plugin_name, "rls");
+}
 
 }  // namespace
 }  // namespace testing
