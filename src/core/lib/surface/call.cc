7440b6161e2733dfdb8c34c7a63a2b94b5bd6869
Craig Tiller <ctiller@google.com>
2022-11-03 23:50:28 -0700

    [test] Ensure SEND_STATUS & (RECV_MESSAGE, RECV_CLOSE_ON_SERVER) do not inhabit the same batch

--- src/core/lib/surface/call.cc
+++ src/core/lib/surface/call.cc
@@ -1364,6 +1364,16 @@ grpc_call_error FilterStackCall::StartBatch(const grpc_op* ops, size_t nops,
     seen_ops |= (1u << ops[i].op);
   }
 
+  if (!is_client() &&
+      (seen_ops & (1u << GRPC_OP_SEND_STATUS_FROM_SERVER)) != 0 &&
+      (seen_ops & ((1u << GRPC_OP_RECV_MESSAGE) |
+                   (1u << GRPC_OP_RECV_CLOSE_ON_SERVER))) != 0) {
+    gpr_log(GPR_ERROR,
+            "******************* SEND_STATUS WITH RECV_MESSAGE "
+            "*******************");
+    return GRPC_CALL_ERROR;
+  }
+
   GRPC_CALL_LOG_BATCH(GPR_INFO, ops, nops);
 
   if (nops == 0) {
