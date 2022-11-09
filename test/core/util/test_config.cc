de754a0f9c2d1f9a5a4a58cdc7074b64b3952107
AJ Heller <hork@google.com>
2022-11-08 16:24:05 -0800

    [TEST] Remove the wait for async shutdown on tests
    This better simulates real-life uses of the library.

--- test/core/util/test_config.cc
+++ test/core/util/test_config.cc
@@ -158,20 +158,20 @@ TestEnvironment::TestEnvironment(int* argc, char** argv) {
 }
 
 TestEnvironment::~TestEnvironment() {
-  // This will wait until gRPC shutdown has actually happened to make sure
-  // no gRPC resources (such as thread) are active. (timeout = 10s)
-  if (!grpc_wait_until_shutdown(10)) {
-    gpr_log(GPR_ERROR, "Timeout in waiting for gRPC shutdown");
-  }
-  if (BuiltUnderMsan()) {
-    // This is a workaround for MSAN. MSAN doesn't like having shutdown thread
-    // running. Although the code above waits until shutdown is done, chances
-    // are that thread itself is still alive. To workaround this problem, this
-    // is going to wait for 0.5 sec to give a chance to the shutdown thread to
-    // exit. https://github.com/grpc/grpc/issues/23695
-    gpr_sleep_until(gpr_time_add(gpr_now(GPR_CLOCK_REALTIME),
-                                 gpr_time_from_millis(500, GPR_TIMESPAN)));
-  }
+  // // This will wait until gRPC shutdown has actually happened to make sure
+  // // no gRPC resources (such as thread) are active. (timeout = 10s)
+  // if (!grpc_wait_until_shutdown(10)) {
+  //   gpr_log(GPR_ERROR, "Timeout in waiting for gRPC shutdown");
+  // }
+  // if (BuiltUnderMsan()) {
+  //   // This is a workaround for MSAN. MSAN doesn't like having shutdown thread
+  //   // running. Although the code above waits until shutdown is done, chances
+  //   // are that thread itself is still alive. To workaround this problem, this
+  //   // is going to wait for 0.5 sec to give a chance to the shutdown thread to
+  //   // exit. https://github.com/grpc/grpc/issues/23695
+  //   gpr_sleep_until(gpr_time_add(gpr_now(GPR_CLOCK_REALTIME),
+  //                                gpr_time_from_millis(500, GPR_TIMESPAN)));
+  // }
   gpr_log(GPR_INFO, "TestEnvironment ends");
 }
 
