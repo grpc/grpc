116826a9a3c5c643c1e4ce65e4a2db52c9080b15
Mark D. Roth <roth@google.com>
2022-11-09 19:09:57 +0000

    fix client_lb_end2end_test

--- test/cpp/end2end/client_lb_end2end_test.cc
+++ test/cpp/end2end/client_lb_end2end_test.cc
@@ -2798,10 +2798,9 @@ TEST_F(ControlPlaneStatusRewritingTest, RewritesFromConfigSelector) {
     bool Equals(const ConfigSelector* other) const override {
       return status_ == static_cast<const FailConfigSelector*>(other)->status_;
     }
-    CallConfig GetCallConfig(GetCallConfigArgs /*args*/) override {
-      CallConfig config;
-      config.status = status_;
-      return config;
+    absl::StatusOr<CallConfig> GetCallConfig(GetCallConfigArgs /*args*/)
+        override {
+      return status_;
     }
 
    private:
