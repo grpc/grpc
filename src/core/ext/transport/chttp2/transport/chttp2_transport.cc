1bf3cfd552af6fb3dbeb041c40f5da9955a6ad25
Craig Tiller <ctiller@google.com>
2022-11-13 22:24:05 -0800

    turn on some traces to try and debug ruby

--- src/core/ext/transport/chttp2/transport/chttp2_transport.cc
+++ src/core/ext/transport/chttp2/transport/chttp2_transport.cc
@@ -127,7 +127,7 @@ static int g_default_max_pings_without_data = DEFAULT_MAX_PINGS_BETWEEN_DATA;
 static int g_default_max_ping_strikes = DEFAULT_MAX_PING_STRIKES;
 
 #define MAX_CLIENT_STREAM_ID 0x7fffffffu
-grpc_core::TraceFlag grpc_http_trace(false, "http");
+grpc_core::TraceFlag grpc_http_trace(true, "http");
 grpc_core::TraceFlag grpc_keepalive_trace(false, "http_keepalive");
 grpc_core::DebugOnlyTraceFlag grpc_trace_chttp2_refcount(false,
                                                          "chttp2_refcount");
