1e2af690f2f777d516ef889e31f8452ef761f6b4
Vignesh Babu <vigneshbabu@google.com>
2022-11-15 10:06:06 -0800

    Add a check to flush stale TracedBuffer entries to prevent them growing too long. (#31588)
    * Add a check to flush stale TracedBuffer entries to prevent them growing too long
    
    * update
    
    * fix tsan and sanity
    
    * sanity
    
    * review comments

--- src/core/lib/event_engine/posix_engine/traced_buffer_list.h
+++ src/core/lib/event_engine/posix_engine/traced_buffer_list.h
@@ -110,6 +110,8 @@ struct Timestamps {
 
 class TracedBufferList {
  public:
+  TracedBufferList() = default;
+  ~TracedBufferList() = default;
   // Add a new entry in the TracedBuffer list pointed to by head. Also saves
   // sendmsg_time with the current timestamp.
   void AddNewEntry(int32_t seq_no, int fd, void* arg);
@@ -138,9 +140,13 @@ class TracedBufferList {
   class TracedBuffer {
    public:
     TracedBuffer(uint32_t seq_no, void* arg) : seq_no_(seq_no), arg_(arg) {}
+    // Returns true if the TracedBuffer is considered stale at the given
+    // timestamp.
+    bool Finished(gpr_timespec ts);
 
    private:
     friend class TracedBufferList;
+    gpr_timespec last_timestamp_;
     TracedBuffer* next_ = nullptr;
     uint32_t seq_no_; /* The sequence number for the last byte in the buffer */
     void* arg_;       /* The arg to pass to timestamps_callback */
