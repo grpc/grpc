54331f6ee85bd4f29d7a903f47685b9c854ffbd7
Luwei Ge <lwge@google.com>
2022-11-16 18:22:34 +0000

    remove reversed tls version fixtures

--- test/core/end2end/fixtures/h2_tls_wrong_versions.cc
+++ test/core/end2end/fixtures/h2_tls_wrong_versions.cc
@@ -70,46 +70,6 @@ static grpc_end2end_test_fixture chttp2_create_fixture_simple_fullstack_2(
   return f;
 }
 
-static grpc_end2end_test_fixture chttp2_create_fixture_simple_fullstack_3(
-    const grpc_channel_args*, const grpc_channel_args*) {
-  grpc_end2end_test_fixture f;
-  int port = grpc_pick_unused_port_or_die();
-  fullstack_secure_fixture_data* ffd = new fullstack_secure_fixture_data();
-  memset(&f, 0, sizeof(f));
-  ffd->localaddr = grpc_core::JoinHostPort("localhost", port);
-  SetChannelTlsVersion(ffd, SecurityPrimitives::TlsVersion::V_13,
-                       SecurityPrimitives::TlsVersion::V_12);
-  SetServerTlsVersion(ffd, SecurityPrimitives::TlsVersion::V_12,
-                      SecurityPrimitives::TlsVersion::V_12);
-  SetCertificateProvider(ffd,
-                         SecurityPrimitives::ProviderType::STATIC_PROVIDER);
-  SetCertificateVerifier(
-      ffd, SecurityPrimitives::VerifierType::EXTERNAL_SYNC_VERIFIER);
-  f.fixture_data = ffd;
-  f.cq = grpc_completion_queue_create_for_next(nullptr);
-  return f;
-}
-
-static grpc_end2end_test_fixture chttp2_create_fixture_simple_fullstack_4(
-    const grpc_channel_args*, const grpc_channel_args*) {
-  grpc_end2end_test_fixture f;
-  int port = grpc_pick_unused_port_or_die();
-  fullstack_secure_fixture_data* ffd = new fullstack_secure_fixture_data();
-  memset(&f, 0, sizeof(f));
-  ffd->localaddr = grpc_core::JoinHostPort("localhost", port);
-  SetChannelTlsVersion(ffd, SecurityPrimitives::TlsVersion::V_13,
-                       SecurityPrimitives::TlsVersion::V_13);
-  SetServerTlsVersion(ffd, SecurityPrimitives::TlsVersion::V_13,
-                      SecurityPrimitives::TlsVersion::V_12);
-  SetCertificateProvider(ffd,
-                         SecurityPrimitives::ProviderType::STATIC_PROVIDER);
-  SetCertificateVerifier(
-      ffd, SecurityPrimitives::VerifierType::EXTERNAL_SYNC_VERIFIER);
-  f.fixture_data = ffd;
-  f.cq = grpc_completion_queue_create_for_next(nullptr);
-  return f;
-}
-
 static grpc_end2end_test_config configs[] = {
     {
         // client: static data provider + sync verifier + tls 1.2 - 1.2
@@ -129,28 +89,6 @@ static grpc_end2end_test_config configs[] = {
         kH2TLSFeatureMask,
         "foo.test.google.fr",
         chttp2_create_fixture_simple_fullstack_2,
-        chttp2_init_client,
-        chttp2_init_server,
-        chttp2_tear_down_secure_fullstack,
-    },
-    {
-        // client: static data provider + sync verifier + tls 1.3 - 1.2
-        // server: static data provider + sync verifier + tls 1.2 - 1.2
-        "chttp2/simple_ssl_fullstack",
-        kH2TLSFeatureMask,
-        "foo.test.google.fr",
-        chttp2_create_fixture_simple_fullstack_3,
-        chttp2_init_client,
-        chttp2_init_server,
-        chttp2_tear_down_secure_fullstack,
-    },
-    {
-        // client: static data provider + sync verifier + tls 1.3 - 1.3
-        // server: static data provider + sync verifier + tls 1.3 - 1.2
-        "chttp2/simple_ssl_fullstack",
-        kH2TLSFeatureMask,
-        "foo.test.google.fr",
-        chttp2_create_fixture_simple_fullstack_4,
         chttp2_init_client,
         chttp2_init_server,
         chttp2_tear_down_secure_fullstack,
