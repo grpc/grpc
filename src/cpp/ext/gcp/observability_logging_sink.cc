8768d778bf28db8977785a5d3f78c665f7f00d47
Craig Tiller <ctiller@google.com>
2022-11-15 18:19:26 -0800

    Revert "Observability Logging: Interfaces and structure (#31651)"
    This reverts commit 2146107e25de48b5090b210441ddae7bd0edd8a5.

--- src/cpp/ext/gcp/observability_logging_sink.cc
+++ src/cpp/ext/gcp/observability_logging_sink.cc
@@ -23,49 +23,17 @@
 #include <stddef.h>
 
 #include <algorithm>
-#include <utility>
-
-#include "absl/types/optional.h"
-#include "google/logging/v2/logging.grpc.pb.h"
-
-#include <grpcpp/grpcpp.h>
-
-#include "src/core/lib/gprpp/env.h"
-#include "src/cpp/ext/filters/census/open_census_call_tracer.h"
 
 namespace grpc {
 namespace internal {
 
 ObservabilityLoggingSink::ObservabilityLoggingSink(
-    GcpObservabilityConfig::CloudLogging logging_config, std::string project_id)
-    : project_id_(std::move(project_id)) {
+    GcpObservabilityConfig::CloudLogging logging_config) {
   for (auto& client_rpc_event_config : logging_config.client_rpc_events) {
-    client_configs_.emplace_back(client_rpc_event_config);
+    client_configs.emplace_back(client_rpc_event_config);
   }
   for (auto& server_rpc_event_config : logging_config.server_rpc_events) {
-    server_configs_.emplace_back(server_rpc_event_config);
-  }
-  std::string endpoint;
-  absl::optional<std::string> endpoint_env =
-      grpc_core::GetEnv("GOOGLE_CLOUD_CPP_LOGGING_SERVICE_V2_ENDPOINT");
-  if (endpoint_env.has_value() && !endpoint_env->empty()) {
-    endpoint = std::move(*endpoint_env);
-  } else {
-    endpoint = "logging.googleapis.com";
-  }
-  ChannelArguments args;
-  // Disable observability for RPCs on this channel
-  args.SetInt(GRPC_ARG_ENABLE_OBSERVABILITY, 0);
-  // Set keepalive time to 24 hrs to effectively disable keepalive ping, but
-  // still enable KEEPALIVE_TIMEOUT to get the TCP_USER_TIMEOUT effect.
-  args.SetInt(GRPC_ARG_KEEPALIVE_TIME_MS, 24 * 60 * 60 * 1000 /* 24 hours */);
-  args.SetInt(GRPC_ARG_KEEPALIVE_TIMEOUT_MS, 20 * 1000 /* 20 seconds */);
-  stub_ = google::logging::v2::LoggingServiceV2::NewStub(
-      CreateCustomChannel(endpoint, GoogleDefaultCredentials(), args));
-  absl::optional<std::string> authority_env =
-      grpc_core::GetEnv("GOOGLE_CLOUD_CPP_LOGGING_SERVICE_V2_ENDPOINT");
-  if (authority_env.has_value() && !authority_env->empty()) {
-    authority_ = std::move(*endpoint_env);
+    server_configs.emplace_back(server_rpc_event_config);
   }
 }
 
@@ -80,7 +48,7 @@ LoggingSink::Config ObservabilityLoggingSink::FindMatch(
       path.substr(0, pos);  // service name is before the '/'
   absl::string_view method =
       path.substr(pos + 1);  // method name starts after the '/'
-  const auto& configs = is_client ? client_configs_ : server_configs_;
+  const auto& configs = is_client ? client_configs : server_configs;
   for (const auto& config : configs) {
     for (const auto& config_method : config.parsed_methods) {
       if ((config_method.service == "*") ||
@@ -96,27 +64,6 @@ LoggingSink::Config ObservabilityLoggingSink::FindMatch(
     }
   }
   return LoggingSink::Config(0, 0);
-}
-
-void ObservabilityLoggingSink::LogEntry(Entry /* entry */) {
-  struct CallContext {
-    ClientContext context;
-    google::logging::v2::WriteLogEntriesRequest request;
-    google::logging::v2::WriteLogEntriesResponse response;
-  };
-  // TODO(yashykt): Implement batching so that we can batch a bunch of log
-  // entries into a single entry. Also, set a reasonable deadline on the
-  // context, and actually use the entry.
-  CallContext* call = new CallContext;
-  call->context.set_authority(authority_);
-  stub_->async()->WriteLogEntries(&(call->context), &(call->request),
-                                  &(call->response), [call](Status status) {
-                                    if (!status.ok()) {
-                                      // TODO(yashykt): Log the contents of the
-                                      // request on a failure.
-                                    }
-                                    delete call;
-                                  });
 }
 
 ObservabilityLoggingSink::Configuration::Configuration(
