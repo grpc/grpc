285a9fdd80c28d740df6a1e483d2d8fd0336e6c0
Cheng-Yu Chung <chengyuc@google.com>
2022-11-22 11:07:55 -0500

    Remove `include/grpcpp/impl/codegen/interceptor_common.h` (#31530)
    * Remove `include/grpcpp/impl/codegen/interceptor_common.h`
    
    * Automated change: Fix sanity tests
    
    * Automated change: Fix sanity tests

--- src/cpp/server/server_cc.cc
+++ src/cpp/server/server_cc.cc
@@ -45,10 +45,10 @@
 #include <grpcpp/impl/call.h>
 #include <grpcpp/impl/call_op_set.h>
 #include <grpcpp/impl/call_op_set_interface.h>
-#include <grpcpp/impl/codegen/interceptor_common.h>
 #include <grpcpp/impl/codegen/sync.h>
 #include <grpcpp/impl/completion_queue_tag.h>
 #include <grpcpp/impl/grpc_library.h>
+#include <grpcpp/impl/interceptor_common.h>
 #include <grpcpp/impl/metadata_map.h>
 #include <grpcpp/impl/rpc_method.h>
 #include <grpcpp/impl/rpc_service_method.h>
--- tools/doxygen/Doxyfile.c++.internal
+++ tools/doxygen/Doxyfile.c++.internal
@@ -1015,6 +1015,7 @@ include/grpcpp/impl/create_auth_context.h \
 include/grpcpp/impl/delegating_channel.h \
 include/grpcpp/impl/grpc_library.h \
 include/grpcpp/impl/intercepted_channel.h \
+include/grpcpp/impl/interceptor_common.h \
 include/grpcpp/impl/metadata_map.h \
 include/grpcpp/impl/method_handler_impl.h \
 include/grpcpp/impl/rpc_method.h \
