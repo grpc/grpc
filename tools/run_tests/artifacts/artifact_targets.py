73f4e3ac31f0036a07881122e896b3020c16f52e
Richard Belleville <rbellevi@google.com>
2022-11-29 23:58:09 +0000

    Turn off boringssl assembly optimizations

--- tools/run_tests/artifacts/artifact_targets.py
+++ tools/run_tests/artifacts/artifact_targets.py
@@ -134,6 +134,7 @@ class PythonArtifact:
         if self.platform == "macos":
             environ['ARCHFLAGS'] = "-arch arm64 -arch x86_64"
             environ["GRPC_UNIVERSAL2_REPAIR"] = "true"
+            environ['GRPC_BUILD_WITH_BORING_SSL_ASM'] = "true"
 
         if self.platform == 'linux_extra':
             # Crosscompilation build for armv7 (e.g. Raspberry Pi)
