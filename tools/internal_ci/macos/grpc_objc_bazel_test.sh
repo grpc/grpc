6bdc772fcafa2133845264b1919359f0f150bc99
Hannah Shi <hannahshisfb@gmail.com>
2022-12-06 15:52:16 -0800

    run cf stream test with bazel (#31555)

--- tools/internal_ci/macos/grpc_objc_bazel_test.sh
+++ tools/internal_ci/macos/grpc_objc_bazel_test.sh
@@ -58,6 +58,7 @@ TEST_TARGETS=(
   //src/objective-c/tests:UnitTests
   //src/objective-c/tests:CronetTests
   //src/objective-c/tests:PerfTests
+  //src/objective-c/tests:CFStreamTests
   //src/objective-c/tests:tvtests_build_test
   # codegen plugin tests
   //src/objective-c/tests:objc_codegen_plugin_test
