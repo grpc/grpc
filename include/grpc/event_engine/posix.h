ea016cf2c913e95fb1ee980c67265bae791836e9
Vignesh Babu <vigneshbabu@google.com>
2022-12-06 07:45:06 +0000

    comments

--- include/grpc/event_engine/posix.h
+++ include/grpc/event_engine/posix.h
@@ -28,10 +28,10 @@
 #include <grpc/event_engine/port.h>
 #include <grpc/event_engine/slice_buffer.h>
 
-#ifdef GRPC_EVENT_ENGINE_POSIX
-
 namespace grpc_event_engine {
 namespace experimental {
+
+#ifdef GRPC_EVENT_ENGINE_POSIX
 
 /// This defines an EventEngine interface that all posix specific event engines
 /// must implement.
@@ -40,25 +40,27 @@ class PosixEventEngine : public EventEngine {
    public:
     /// Returns the file descriptor associated with the posix endpoint.
     virtual int GetWrappedFd() = 0;
+
     /// Shutdown the endpoint. After this function call its illegal to invoke
     /// any other methods on the endpoint.
     /// \a release_fd - a pointer to hold the file descriptor associated with
     /// the posix endpoint. If it is not nullptr, then the file descriptor is
     /// not closed while the endpoint is shutdown. The pointer will be updated
-    /// to hold the endpoint's assocaited file descriptor.
+    /// to hold the endpoint's associated file descriptor.
     /// \a on_release_cb - a callback to be invoked when the endpoint is
-    /// shutdown and the file descriptor is released. It should only be invoked
+    /// shutdown and the file descriptor is released. It should be invoked only
     /// if release_fd is not nullptr.
     virtual void Shutdown(
         int* release_fd,
         absl::AnyInvocable<void(absl::Status)> on_release_cb) = 0;
   };
+
   /// Creates a PosixEventEngineEndpoint from an fd which is already assumed to
   /// be connected to a remote peer.
   /// \a fd - The connected socket file descriptor.
   /// \a peer_addr - The address of the peer to which the provided fd has been
   /// connected.
-  /// \a config - Additional configuration to apply to the endpoint.
+  /// \a config - Additional configuration to applied to the endpoint.
   /// \a memory_allocator - The endpoint may use the provided memory allocator
   /// to track memory allocations.
   virtual std::unique_ptr<PosixEventEngineEndpoint> CreateEndpointFromFd(
@@ -76,9 +78,13 @@ class PosixEventEngine : public EventEngine {
     /// new client connection.
     /// \a memory_allocation - The callback may use the provided memory
     /// allocator to handle memory allocation operations.
+    /// \a pending_data - If specified, it holds any pending data that may have
+    /// already been read over the new client connection. Otherwise, it is
+    /// assumed that no data has been read over the new client connection.
     using PosixAcceptCallback = absl::AnyInvocable<void(
         int listener_fd, std::unique_ptr<Endpoint> endpoint,
-        MemoryAllocator memory_allocator)>;
+        MemoryAllocator memory_allocator, SliceBuffer* pending_data)>;
+
     /// Called when a posix listener bind operation completes. A single bind
     /// operation may trigger creation of multiple listener fds. This callback
     /// should be invoked once on each newly created and bound fd. If the
@@ -89,16 +95,33 @@ class PosixEventEngine : public EventEngine {
     /// address.
     using OnPosixBindNewFdCallback =
         absl::AnyInvocable<void(absl::StatusOr<int> listener_fd)>;
+
     /// Bind an address/port to this Listener.
     ///
     /// It is expected that multiple addresses/ports can be bound to this
     /// Listener before Listener::Start has been called. Returns either the
-    /// bound port or an appropriate error status. The on_bind_new_fd callback
-    /// is invoked once for each newly bound listener fd that may be created by
-    /// this Bind operation.
+    /// bound port or an appropriate error status.
+    /// \a addr - The address to listen for incoming connections.
+    /// \a on_bind_new_fd The callback is invoked once for each newly bound
+    /// listener fd that may be created by this Bind operation.
     virtual absl::StatusOr<int> Bind(
         const ResolvedAddress& addr,
         OnPosixBindNewFdCallback on_bind_new_fd) = 0;
+
+    /// Handle an externally accepted client connection.
+    ///
+    /// This may be invoked to process a new client connection accepted by an
+    /// external listening fd.
+    /// \a listener_fd - The external listening socket fd that accepted the new
+    /// client connection.
+    /// \a fd - The socket file descriptor representing the new client
+    /// connection.
+    /// \a pending_data - If specified, it holds any pending data that may have
+    /// already been read over the externally accepted client connection.
+    /// Otherwise, it is assumed that no data has been read over the new client
+    /// connection.
+    virtual absl::Status HandleExternalConnection(
+        int listener_fd, int fd, SliceBuffer* pending_data) = 0;
   };
 
   /// Factory method to create a network listener / server.
@@ -123,8 +146,9 @@ class PosixEventEngine : public EventEngine {
       std::unique_ptr<MemoryAllocatorFactory> memory_allocator_factory) = 0;
 };
 
+#endif  // GRPC_EVENT_ENGINE_POSIX
+
 }  // namespace experimental
 }  // namespace grpc_event_engine
 
-#endif  // GRPC_EVENT_ENGINE_POSIX
 #endif  // GRPC_EVENT_ENGINE_POSIX_H
