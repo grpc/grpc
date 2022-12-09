87c2c3d7525842dcbf6cc6aae48be5a2f1b42d38
Craig Tiller <ctiller@google.com>
2022-12-09 09:48:10 -0800

    [promises] Convert client load reporting to promises

--- src/core/ext/filters/client_channel/lb_policy/grpclb/client_load_reporting_filter.h
+++ src/core/ext/filters/client_channel/lb_policy/grpclb/client_load_reporting_filter.h
@@ -23,6 +23,21 @@
 
 #include "src/core/lib/channel/channel_fwd.h"
 #include "src/core/lib/channel/channel_stack.h"
+#include "src/core/lib/channel/promise_based_filter.h"
+
+namespace grpc_core {
+class ClientLoadReportingFilter : public ChannelFilter {
+ public:
+  static const grpc_channel_filter kFilter;
+
+  static absl::StatusOr<ClientLoadReportingFilter> Create(
+      const ChannelArgs& args, ChannelFilter::Args filter_args);
+
+  // Construct a promise for one call.
+  ArenaPromise<ServerMetadataHandle> MakeCallPromise(
+      CallArgs call_args, NextPromiseFactory next_promise_factory) override;
+};
+}  // namespace grpc_core
 
 extern const grpc_channel_filter grpc_client_load_reporting_filter;
 
