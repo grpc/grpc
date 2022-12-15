71e872b74a2022e26ff6057ed9238de48755ef1e
Jan Tattermusch <jtattermusch@google.com>
2022-12-15 15:12:00 +0100

    fix copyright note

--- src/csharp/Grpc.Tools.Tests/scripts/fakeprotoc.py
+++ src/csharp/Grpc.Tools.Tests/scripts/fakeprotoc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2022 gRPC authors.
+# Copyright 2022 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
