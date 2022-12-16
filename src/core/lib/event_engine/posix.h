33711bba2091a8b914bd2b093a1e112a092c4eb4
Vignesh Babu <vigneshbabu@google.com>
2022-12-16 10:53:29 +0000

    format

--- src/core/lib/event_engine/posix.h
+++ src/core/lib/event_engine/posix.h
@@ -33,7 +33,7 @@ namespace experimental {
 
 /// This defines an interface that posix specific event engines endpoints
 /// may implement to support additional file descriptor related functionality.
-class PosixEndpointWithFdSupport: public EventEngine::Endpoint {
+class PosixEndpointWithFdSupport : public EventEngine::Endpoint {
  public:
   /// Returns the file descriptor associated with the posix endpoint.
   virtual int GetWrappedFd() = 0;
@@ -55,7 +55,7 @@ class PosixEndpointWithFdSupport: public EventEngine::Endpoint {
 
 /// Defines an interface that posix event engine listeners may implement to
 /// support additional file descriptor related functionality.
-class PosixListenerWithFdSupport: public EventEngine::Listener {
+class PosixListenerWithFdSupport : public EventEngine::Listener {
  public:
   /// Called when a posix listener bind operation completes. A single bind
   /// operation may trigger creation of multiple listener fds. This callback
@@ -98,7 +98,7 @@ class PosixListenerWithFdSupport: public EventEngine::Listener {
 
 /// Defines an interface that posix event engines may implement to
 /// support additional file descriptor related functionality.
-class PosixEventEngineWithFdSupport: public EventEngine {
+class PosixEventEngineWithFdSupport : public EventEngine {
  public:
   /// Creates a posix specific EventEngine::Endpoint from an fd which is already
   /// assumed to be connected to a remote peer. \a fd - The connected socket
