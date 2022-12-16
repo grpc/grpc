aaef80c4f97ad0b582cd2fe335d01a388f87f688
Craig Tiller <ctiller@google.com>
2022-12-15 19:59:20 -0800

    [c++20] Demonstrate coroutines being used as promises

---
+++ test/core/promise/cpp20_test.cc
@@ -0,0 +1,94 @@
+// Copyright 2022 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <coroutine>
+#include <optional>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include "src/core/lib/gprpp/construct_destruct.h"
+#include "src/core/lib/promise/detail/promise_like.h"
+#include "src/core/lib/promise/poll.h"
+
+namespace grpc_core {
+namespace {
+
+// A wrapper around a coroutine that can be used as a promise.
+template <typename T>
+class Async {
+ public:
+  struct promise_type {
+    using Handle = std::coroutine_handle<promise_type>;
+    std::optional<T> value;
+    Async<T> get_return_object() {
+      return Async<T>{Handle::from_promise(*this)};
+    }
+    std::suspend_always initial_suspend() noexcept { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
+    void unhandled_exception() {}
+    void return_value(T value) { value = std::move(value); }
+  };
+
+  explicit Async(typename promise_type::Handle coroutine)
+      : coroutine_(coroutine) {}
+
+  Async(const Async&) = delete;
+  Async& operator=(const Async&) = delete;
+  Async(Async&& t) noexcept : coroutine_(std::move(t.coroutine_)) {}
+  Async& operator=(Async&& t) noexcept {
+    std::swap(coroutine_, t.coroutine_);
+    return *this;
+  }
+  ~Async() {
+    if (coroutine_) {
+      coroutine_.destroy();
+    }
+  }
+
+  Poll<T> operator()() {
+    coroutine_.resume();
+    if (coroutine_.done()) {
+      return std::move(*coroutine_.promise().value);
+    } else {
+      return Pending{};
+    }
+  }
+
+ private:
+  typename promise_type::Handle coroutine_;
+};
+
+Async<int> TestFunction() {
+  printf("TestFunction.0\n");
+  co_await std::suspend_always{};
+  printf("TestFunction.1\n");
+  co_return 42;
+}
+
+TEST(Cpp20Test, CoroutinesArePromises) {
+  auto f = TestFunction();
+  printf("initial poll\n");
+  EXPECT_EQ(f(), Poll<int>(Pending{}));
+  printf("final poll\n");
+  EXPECT_EQ(f(), Poll<int>(42));
+}
+
+}  // namespace
+}  // namespace grpc_core
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
