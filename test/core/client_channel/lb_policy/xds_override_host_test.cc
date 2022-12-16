c71b71f01b8e1cce57bbe1fd10cd658ad4e35f24
Eugene Ostroukhov <eostroukhov@google.com>
2022-12-16 10:31:37 -0800

    fixup: update test

--- test/core/client_channel/lb_policy/xds_override_host_test.cc
+++ test/core/client_channel/lb_policy/xds_override_host_test.cc
@@ -41,6 +41,36 @@ class XdsOverrideHostTest : public LoadBalancingPolicyTest {
          Json::Object{{"childPolicy", Json::Array{{child_policy_config}}}}}}});
   }
 
+  absl::optional<RefCountedPtr<LoadBalancingPolicy::SubchannelPicker>>
+  InitRoundRobin(absl::Span<const absl::string_view> addresses) {
+    absl::optional<RefCountedPtr<LoadBalancingPolicy::SubchannelPicker>>
+        maybe_picker;
+    EXPECT_EQ(ApplyUpdate(BuildUpdate(addresses, MakeXdsOverrideHostConfig()),
+                          policy_.get()),
+              absl::OkStatus());
+    ExpectConnectingUpdate();
+    RefCountedPtr<LoadBalancingPolicy::SubchannelPicker> picker;
+    for (size_t i = 0; i < addresses.size(); ++i) {
+      auto* subchannel = FindSubchannel(addresses[i]);
+      EXPECT_NE(subchannel, nullptr);
+      if (subchannel == nullptr) {
+        return absl::nullopt;
+      }
+      EXPECT_TRUE(subchannel->ConnectionRequested());
+      subchannel->SetConnectivityState(GRPC_CHANNEL_CONNECTING);
+      subchannel->SetConnectivityState(GRPC_CHANNEL_READY);
+      if (i == 0) {
+        auto picker = WaitForConnected();
+        ExpectRoundRobinPicks(picker.get(), {addresses[0]});
+      } else {
+        maybe_picker = WaitForRoundRobinListChange(
+            absl::MakeSpan(addresses).subspan(0, i),
+            absl::MakeSpan(addresses).subspan(0, i + 1));
+      }
+    }
+    return maybe_picker;
+  }
+
   OrphanablePtr<LoadBalancingPolicy> policy_;
 };
 
@@ -95,28 +125,9 @@ TEST_F(XdsOverrideHostTest, OverrideHost) {
   // Send address list to LB policy.
   const std::array<absl::string_view, 3> kAddresses = {
       "ipv4:127.0.0.1:441", "ipv4:127.0.0.1:442", "ipv4:127.0.0.1:443"};
-  EXPECT_EQ(ApplyUpdate(BuildUpdate(kAddresses, MakeXdsOverrideHostConfig()),
-                        policy_.get()),
-            absl::OkStatus());
-  ExpectConnectingUpdate();
-  RefCountedPtr<LoadBalancingPolicy::SubchannelPicker> picker;
-  for (size_t i = 0; i < kAddresses.size(); ++i) {
-    auto* subchannel = FindSubchannel(kAddresses[i]);
-    ASSERT_NE(subchannel, nullptr);
-    EXPECT_TRUE(subchannel->ConnectionRequested());
-    subchannel->SetConnectivityState(GRPC_CHANNEL_CONNECTING);
-    subchannel->SetConnectivityState(GRPC_CHANNEL_READY);
-    if (i == 0) {
-      auto picker = WaitForConnected();
-      ExpectRoundRobinPicks(picker.get(), {kAddresses[0]});
-    } else {
-      auto maybe_picker = WaitForRoundRobinListChange(
-          absl::MakeSpan(kAddresses).subspan(0, i),
-          absl::MakeSpan(kAddresses).subspan(0, i + 1));
-      ASSERT_TRUE(maybe_picker.has_value());
-      picker = std::move(*maybe_picker);
-    }
-  }
+  auto maybe_picker = InitRoundRobin(kAddresses);
+  ASSERT_TRUE(maybe_picker.has_value());
+  auto picker = *maybe_picker;
   ASSERT_NE(picker, nullptr);
   // Check that the host is overridden
   std::map<UniqueTypeName, std::string> call_attributes{
@@ -129,58 +140,30 @@ TEST_F(XdsOverrideHostTest, OverrideHost) {
 }
 
 TEST_F(XdsOverrideHostTest, OverrideHostSubchannelNotFound) {
-  const std::array<absl::string_view, 2> kAddresses = {"ipv4:127.0.0.1:441",
-                                                       "ipv4:127.0.0.1:442"};
-  EXPECT_EQ(ApplyUpdate(BuildUpdate(kAddresses,
-                                    MakeXdsOverrideHostConfig("round_robin")),
-                        policy_.get()),
-            absl::OkStatus());
-  ExpectConnectingUpdate();
-  // Ready up both subchannels
-  for (auto address : kAddresses) {
-    auto subchannel = FindSubchannel({address});
-    ASSERT_NE(subchannel, nullptr);
-    ASSERT_TRUE(subchannel->ConnectionRequested());
-    subchannel->SetConnectivityState(GRPC_CHANNEL_CONNECTING);
-    subchannel->SetConnectivityState(GRPC_CHANNEL_READY);
-  }
-  WaitForConnected();
-  ExpectState(GRPC_CHANNEL_READY);
-  auto picker = ExpectState(GRPC_CHANNEL_READY);
+  // Send address list to LB policy.
+  const std::array<absl::string_view, 3> kAddresses = {
+      "ipv4:127.0.0.1:441", "ipv4:127.0.0.1:442", "ipv4:127.0.0.1:443"};
+  auto maybe_picker = InitRoundRobin(kAddresses);
+  ASSERT_TRUE(maybe_picker.has_value());
+  auto picker = *maybe_picker;
   ASSERT_NE(picker, nullptr);
-  // Make sure child policy works
-  EXPECT_NE(ExpectPickComplete(picker.get()), ExpectPickComplete(picker.get()));
-  // Check that the host is overridden
   std::map<UniqueTypeName, std::string> call_attributes{
       {XdsHostOverrideTypeName(), "no such host"}};
-  std::unordered_set<absl::optional<std::string>> picks{
-      ExpectPickComplete(picker.get(), call_attributes),
-      ExpectPickComplete(picker.get(), call_attributes)};
+  std::unordered_set<std::string> picks;
+  for (size_t i = 0; i < kAddresses.size(); ++i) {
+    auto pick = ExpectPickComplete(picker.get(), call_attributes);
+    ASSERT_TRUE(pick.has_value());
+    picks.emplace(*pick);
+  }
   ASSERT_THAT(picks, UnorderedElementsAreArray(kAddresses));
 }
 
 TEST_F(XdsOverrideHostTest, SubchannelsComeAndGo) {
   const std::array<absl::string_view, 3> kAddresses = {
       "ipv4:127.0.0.1:441", "ipv4:127.0.0.1:442", "ipv4:127.0.0.1:443"};
-  EXPECT_EQ(ApplyUpdate(BuildUpdate(kAddresses,
-                                    MakeXdsOverrideHostConfig("round_robin")),
-                        policy_.get()),
-            absl::OkStatus());
-  ExpectConnectingUpdate();
-  RefCountedPtr<LoadBalancingPolicy::SubchannelPicker> picker;
-  // Ready up all subchannels
-  for (auto address : kAddresses) {
-    auto subchannel = FindSubchannel({address});
-    ASSERT_NE(subchannel, nullptr);
-    ASSERT_TRUE(subchannel->ConnectionRequested());
-    subchannel->SetConnectivityState(GRPC_CHANNEL_CONNECTING);
-    subchannel->SetConnectivityState(GRPC_CHANNEL_READY);
-  }
-  picker = WaitForConnected();
-  ExpectState(GRPC_CHANNEL_READY);
-  ExpectState(GRPC_CHANNEL_READY);
-  ExpectState(GRPC_CHANNEL_READY);
-  picker = ExpectState(GRPC_CHANNEL_READY);
+  auto maybe_picker = InitRoundRobin(kAddresses);
+  ASSERT_TRUE(maybe_picker.has_value());
+  auto picker = *maybe_picker;
   ASSERT_NE(picker, nullptr);
   // Make sure child policy works
   EXPECT_NE(ExpectPickComplete(picker.get()), ExpectPickComplete(picker.get()));
