4d4fed1ef7784bfb919184e7ccb1487d69e5465c
Alisha Nanda <alishananda@google.com>
2022-12-16 22:43:42 -0800

    [resource_quota] Fix allocator experiment bug (#31919)
    * Fix allocator exp
    
    * fix alloc exp

--- src/core/lib/resource_quota/memory_quota.h
+++ src/core/lib/resource_quota/memory_quota.h
@@ -417,14 +417,14 @@ class GrpcMemoryAllocatorImpl final : public EventEngineMemoryAllocatorImpl {
   // Return all free bytes to quota.
   void ReturnFree() {
     size_t ret = free_bytes_.exchange(0, std::memory_order_acq_rel);
+    if (ret == 0) return;
     if (GRPC_TRACE_FLAG_ENABLED(grpc_resource_quota_trace)) {
       gpr_log(GPR_INFO, "Allocator %p returning %zu bytes to quota", this, ret);
     }
-    if (ret == 0) return;
-    memory_quota_->MaybeMoveAllocator(this, /*old_free_bytes=*/ret,
-                                      /*new_free_bytes=*/0);
     taken_bytes_.fetch_sub(ret, std::memory_order_relaxed);
     memory_quota_->Return(ret);
+    memory_quota_->MaybeMoveAllocator(this, /*old_free_bytes=*/ret,
+                                      /*new_free_bytes=*/0);
   }
 
   // Post a reclamation function.
