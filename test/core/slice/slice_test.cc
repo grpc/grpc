f7a6fac24c06dc20f4363ff9765654c1e78442a6
Vignesh Babu <vigneshbabu@google.com>
2022-12-30 11:36:12 -0800

    Expand SliceCast to support casts to MutableSlice type (#31996)
    * Expand SliceCast to support casts to MutableSlice type
    
    * update

--- test/core/slice/slice_test.cc
+++ test/core/slice/slice_test.cc
@@ -448,6 +448,19 @@ TEST(SliceTest, MutableSliceCastWorks) {
   EXPECT_EQ(&slice, &test.c_slice());
   slice = grpc_slice_from_static_string("goodbye world!");
   EXPECT_EQ(test.as_string_view(), "goodbye world!");
+
+  MutableSlice& m_cpp_slice = SliceCast<MutableSlice>(test);
+  EXPECT_EQ(&m_cpp_slice.c_slice(), &test.c_slice());
+  m_cpp_slice = MutableSlice::FromCopiedString("hello world again!");
+  // Change the first byte.
+  m_cpp_slice[0] = 'e';
+  EXPECT_EQ(test.as_string_view(), "eello world again!");
+
+  MutableSlice& m_c_slice = SliceCast<MutableSlice>(slice);
+  EXPECT_EQ(&m_c_slice.c_slice(), &slice);
+  // Restore the first byte.
+  GRPC_SLICE_START_PTR(slice)[0] = 'h';
+  EXPECT_EQ(m_c_slice.as_string_view(), "hello world again!");
 }
 
 }  // namespace
