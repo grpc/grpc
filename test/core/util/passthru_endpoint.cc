e6c78c7f589de47c2b4e22972fab7fac74479fdc
yijiem <yijiem@google.com>
2023-01-03 22:20:12 -0800

    early out hack in passthru_endpoint

--- test/core/util/passthru_endpoint.cc
+++ test/core/util/passthru_endpoint.cc
@@ -492,14 +492,24 @@ static void sched_next_channel_action_locked(half* m) {
     shutdown_locked(m, err);
     return;
   }
-  m->parent->channel_effects->timer_handle = GetDefaultEventEngine()->RunAfter(
-      grpc_core::Duration::Milliseconds(
-          m->parent->channel_effects->actions[0].wait_ms),
-      [m] {
-        grpc_core::ApplicationCallbackExecCtx callback_exec_ctx;
-        grpc_core::ExecCtx exec_ctx;
-        do_next_sched_channel_action(m, absl::OkStatus());
-      });
+  // HACK
+  if (m->parent->channel_effects->actions[0].wait_ms == 0) {
+    // early out
+    grpc_core::ExecCtx::Run(DEBUG_LOCATION,
+                            GRPC_CLOSURE_CREATE(do_next_sched_channel_action, m,
+                                                grpc_schedule_on_exec_ctx),
+                            absl::OkStatus());
+  } else {
+    m->parent->channel_effects->timer_handle =
+        GetDefaultEventEngine()->RunAfter(
+            grpc_core::Duration::Milliseconds(
+                m->parent->channel_effects->actions[0].wait_ms),
+            [m] {
+              grpc_core::ApplicationCallbackExecCtx callback_exec_ctx;
+              grpc_core::ExecCtx exec_ctx;
+              do_next_sched_channel_action(m, absl::OkStatus());
+            });
+  }
 }
 
 void start_scheduling_grpc_passthru_endpoint_channel_effects(
