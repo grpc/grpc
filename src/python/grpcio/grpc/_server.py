fabe5f72dab5ab6fd7f95c863d92d2a280ee009c
Xuan Wang <xuanwn@google.com>
2023-01-06 00:48:29 +0000

    fix format

--- src/python/grpcio/grpc/_server.py
+++ src/python/grpcio/grpc/_server.py
@@ -19,10 +19,10 @@ import collections
 from concurrent import futures
 import enum
 import logging
+import sys
 import threading
 import time
 import traceback
-import sys
 from typing import (Any, Callable, Iterable, Iterator, List, Mapping, Optional,
                     Sequence, Set, Tuple, Union)
 
@@ -626,7 +626,7 @@ def _unary_response_in_pool(
                     rpc_event, state, response, response_serializer)
                 if serialized_response is not None:
                     _status(rpc_event, state, serialized_response)
-    except Exception as exp:  # pylint: disable=broad-except
+    except Exception:  # pylint: disable=broad-except
         traceback.print_exception(*sys.exc_info())
     finally:
         cygrpc.uninstall_context()
@@ -666,7 +666,7 @@ def _stream_response_in_pool(
                 if proceed:
                     _send_message_callback_to_blocking_iterator_adapter(
                         rpc_event, state, send_response, response_iterator)
-    except Exception as exp:  # pylint: disable=broad-except
+    except Exception:  # pylint: disable=broad-except
         traceback.print_exception(*sys.exc_info())
     finally:
         cygrpc.uninstall_context()
