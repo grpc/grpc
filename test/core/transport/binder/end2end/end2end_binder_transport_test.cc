4d29f75a11819060d5b2021a03866522e6c41b7b
Cheng-Yu Chung <chengyuc@google.com>
2023-01-11 06:33:27 -0500

    Fix flakiness of `End2EndBinderTransportTest/UnaryCallServerTimeout`
    Add `grpc_test_slowdown_factor()` for the deadline in `End2EndBinderTransportTest/UnaryCallServerTimeout` to reduce flakiness

--- test/core/transport/binder/end2end/end2end_binder_transport_test.cc
+++ test/core/transport/binder/end2end/end2end_binder_transport_test.cc
@@ -121,7 +121,9 @@ TEST_P(End2EndBinderTransportTest, UnaryCallWithNonOkStatus) {
 TEST_P(End2EndBinderTransportTest, UnaryCallServerTimeout) {
   std::unique_ptr<grpc::testing::EchoTestService::Stub> stub = NewStub();
   grpc::ClientContext context;
-  context.set_deadline(absl::ToChronoTime(absl::Now() + absl::Seconds(1)));
+  constexpr auto timeout_ms = absl::Milliseconds(1 * 1000);  /// 1 second
+  context.set_deadline(absl::ToChronoTime(
+      absl::Now() + (timeout_ms * grpc_test_slowdown_factor())));
   grpc::testing::EchoRequest request;
   grpc::testing::EchoResponse response;
   request.set_message("UnaryCallServerTimeout");
