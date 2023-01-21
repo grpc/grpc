e92679789ee1e9808a3047d11fb6307a9bc33376
Vignesh Babu <vigneshbabu@google.com>
2023-01-21 00:38:37 +0000

    fix include guards

--- src/core/lib/event_engine/shim.h
+++ src/core/lib/event_engine/shim.h
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#ifndef GRPC_CORE_LIB_EVENT_ENGINE_SHIM_H
-#define GRPC_CORE_LIB_EVENT_ENGINE_SHIM_H
+#ifndef GRPC_SRC_CORE_LIB_EVENT_ENGINE_SHIM_H
+#define GRPC_SRC_CORE_LIB_EVENT_ENGINE_SHIM_H
 
 #include <grpc/support/port_platform.h>
 
@@ -24,4 +24,4 @@ bool UseEventEngineClient();
 }  // namespace experimental
 }  // namespace grpc_event_engine
 
-#endif  // GRPC_CORE_LIB_EVENT_ENGINE_SHIM_H
+#endif  // GRPC_SRC_CORE_LIB_EVENT_ENGINE_SHIM_H
--- src/core/lib/iomgr/event_engine_shims/endpoint.h
+++ src/core/lib/iomgr/event_engine_shims/endpoint.h
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#ifndef GRPC_CORE_LIB_IOMGR_EVENT_ENGINE_SHIMS_ENDPOINT_H
-#define GRPC_CORE_LIB_IOMGR_EVENT_ENGINE_SHIMS_ENDPOINT_H
+#ifndef GRPC_SRC_CORE_LIB_IOMGR_EVENT_ENGINE_SHIMS_ENDPOINT_H
+#define GRPC_SRC_CORE_LIB_IOMGR_EVENT_ENGINE_SHIMS_ENDPOINT_H
 #include <grpc/support/port_platform.h>
 
 #include <grpc/event_engine/event_engine.h>
@@ -31,4 +31,4 @@ grpc_endpoint* grpc_event_engine_endpoint_create(
 }  // namespace experimental
 }  // namespace grpc_event_engine
 
-#endif  // GRPC_CORE_LIB_IOMGR_EVENT_ENGINE_SHIMS_ENDPOINT_H
+#endif  // GRPC_SRC_CORE_LIB_IOMGR_EVENT_ENGINE_SHIMS_ENDPOINT_H
