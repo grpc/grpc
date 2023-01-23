52b3756c5c7e9f4f41a6693224a4704df33f7d8b
AJ Heller <hork@google.com>
2023-01-23 12:58:59 -0800

    iwyu

--- test/core/event_engine/event_engine_test_utils.cc
+++ test/core/event_engine/event_engine_test_utils.cc
@@ -38,6 +38,7 @@
 #include "src/core/lib/event_engine/channel_args_endpoint_config.h"
 #include "src/core/lib/event_engine/tcp_socket_utils.h"
 #include "src/core/lib/gprpp/notification.h"
+#include "src/core/lib/gprpp/time.h"
 #include "src/core/lib/resource_quota/memory_quota.h"
 
 // IWYU pragma: no_include <sys/socket.h>
