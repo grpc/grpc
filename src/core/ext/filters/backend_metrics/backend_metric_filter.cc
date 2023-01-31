2b8e52e3555ea14f3945b6cd4ffe41cf716cc09e
Yousuk Seung <ysseung@google.com>
2023-01-31 13:55:06 -0800

    sanity check errors fixed

--- src/core/ext/filters/backend_metrics/backend_metric_filter.cc
+++ src/core/ext/filters/backend_metrics/backend_metric_filter.cc
@@ -1,3 +1,17 @@
+// Copyright 2023 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <grpc/support/port_platform.h>
 
 #include "src/core/ext/filters/backend_metrics/backend_metric_filter.h"
@@ -64,7 +78,7 @@ const grpc_channel_filter BackendMetricFilter::kFilter =
         "backend_metric");
 
 absl::StatusOr<BackendMetricFilter> BackendMetricFilter::Create(
-    const ChannelArgs& channel_args, ChannelFilter::Args) {
+    const ChannelArgs&, ChannelFilter::Args) {
   return BackendMetricFilter();
 }
 
