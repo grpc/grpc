cb8437fcccdd9a8e57445f9740e4a6957dcefae7
Craig Tiller <ctiller@google.com>
2023-01-31 20:50:15 -0800

    strict-bs

--- src/core/lib/experiments/config.cc
+++ src/core/lib/experiments/config.cc
@@ -153,7 +153,7 @@ void ForceEnableExperiment(absl::string_view experiment, bool enable) {
 #else
 namespace grpc_core {
 void PrintExperimentsList() {}
-void ForceEnableExperiment(absl::string_view experiment_name, bool enable) {
+void ForceEnableExperiment(absl::string_view experiment_name, bool) {
   Crash(absl::StrCat("ForceEnableExperiment(\"", experiment_name,
                      "\") called in final build"));
 }
