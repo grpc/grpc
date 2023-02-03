887411f2766b61c51e3ac880726c0e3fccc25e26
Yousuk Seung <ysseung@google.com>
2023-02-03 08:50:08 -0800

    clang fixes.

--- src/core/ext/filters/backend_metrics/backend_metric_provider.h
+++ src/core/ext/filters/backend_metrics/backend_metric_provider.h
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #ifndef GRPC_SRC_CORE_EXT_FILTERS_BACKEND_METRICS_BACKEND_METRIC_PROVIDER_H
 #define GRPC_SRC_CORE_EXT_FILTERS_BACKEND_METRICS_BACKEND_METRIC_PROVIDER_H
 
