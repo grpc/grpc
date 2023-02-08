8972217e9a497382947521c94b04b182beddda7e
Richard Belleville <rbellevi@google.com>
2023-02-08 01:52:14 +0000

    Revert

--- tools/run_tests/xds_k8s_test_driver/bin/freeze.sh
+++ tools/run_tests/xds_k8s_test_driver/bin/freeze.sh
@@ -24,4 +24,4 @@ python3 -m virtualenv "${VENV_NAME}"
 "${VENV_NAME}"/bin/pip install -r requirements.txt
 "${VENV_NAME}"/bin/pip freeze > requirements.lock
 
-rm -rf "${VENV_NAME}/"
+rm -rf "${VENV_NAME}"
