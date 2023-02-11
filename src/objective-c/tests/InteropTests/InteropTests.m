f1a57e3061b1555c4a230a2c90e4a54f688d401d
yijiem <yijiem@google.com>
2023-02-10 16:57:04 -0800

    review

--- src/objective-c/tests/InteropTests/InteropTests.m
+++ src/objective-c/tests/InteropTests/InteropTests.m
@@ -1602,9 +1602,8 @@ static dispatch_once_t initGlobalInterceptorFactory;
 // TODO(b/268379869): This test has a race and is flaky in any configurations. One possible way to
 // deflake this test is to find a way to disable ping ack on the interop server for this test case.
 - (void)testKeepaliveWithV2API {
-  if (/* DISABLES CODE */ (YES)) {
-    return;
-  }
+  return;
+
   GRPCTestRunWithFlakeRepeats(self, ^(GRPCTestWaiter waiterBlock, GRPCTestAssert assertBlock) {
     RMTTestService *service = [RMTTestService serviceWithHost:[[self class] host]];
     if ([[self class] transport] == gGRPCCoreCronetID) {
