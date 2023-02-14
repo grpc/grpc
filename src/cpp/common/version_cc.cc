daca32c08f9a4be824aba9c6b345395552bd0080
Esun Kim <veblush@google.com>
2023-02-14 10:49:19 -0800

    Version bump to 1.44.1

--- src/cpp/common/version_cc.cc
+++ src/cpp/common/version_cc.cc
@@ -22,5 +22,5 @@
 #include <grpcpp/grpcpp.h>
 
 namespace grpc {
-std::string Version() { return "1.44.0"; }
+std::string Version() { return "1.44.1"; }
 }  // namespace grpc
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.44.0'
+    VERSION = '1.44.1'
   end
 end
