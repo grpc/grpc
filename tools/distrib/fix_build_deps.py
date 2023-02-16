6119b2d3e1c8ad66de23d3e753a059067b340fff
Yash Tibrewal <yashkt@google.com>
2023-02-16 22:43:08 +0000

    BUILD cleanup

--- tools/distrib/fix_build_deps.py
+++ tools/distrib/fix_build_deps.py
@@ -423,7 +423,6 @@ for dirname in [
         "",
         "src/core",
         "src/cpp/ext/gcp",
-        "src/cpp/ext/filters/census",
         "src/cpp/ext/filters/logging",
         "test/core/uri",
         "test/core/util",
