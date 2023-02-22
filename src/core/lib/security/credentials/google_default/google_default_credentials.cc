45579b7bf6ea5df416fdb1f689892ea25a9c6d0f
Jin Qin <qinjin@google.com>
2023-02-22 02:02:25 +0000

    feat: C++ Auth lib: Remove 3PI config url validation

--- src/core/lib/security/credentials/google_default/google_default_credentials.cc
+++ src/core/lib/security/credentials/google_default/google_default_credentials.cc
@@ -254,57 +254,6 @@ static int is_metadata_server_reachable() {
   return detector.success;
 }
 
-namespace {
-
-bool ValidateUrlField(const Json& json, const std::string& field) {
-  auto it = json.object_value().find(field);
-  if (it == json.object_value().end()) {
-    return true;
-  }
-  if (it->second.type() != Json::Type::STRING ||
-      it->second.string_value().empty()) {
-    return false;
-  }
-  absl::StatusOr<grpc_core::URI> url =
-      grpc_core::URI::Parse(it->second.string_value());
-  if (!url.ok()) return false;
-  if (!absl::EqualsIgnoreCase(url->scheme(), "https")) {
-    return false;
-  }
-  absl::string_view host;
-  absl::string_view port;
-  grpc_core::SplitHostPort(url->authority(), &host, &port);
-  if (absl::ConsumeSuffix(&host, ".p.googleapis.com")) {
-    if (absl::StartsWith(host, "sts-") ||
-        absl::StartsWith(host, "iamcredentials-")) {
-      return true;
-    }
-  } else if (absl::ConsumeSuffix(&host, ".googleapis.com")) {
-    if (host == "sts" || host == "iamcredentials") {
-      return true;
-    } else if (absl::StartsWith(host, "sts.") ||
-               absl::StartsWith(host, "iamcredentials.")) {
-      return true;
-    } else if (absl::EndsWith(host, ".sts") ||
-               absl::EndsWith(host, ".iamcredentials")) {
-      return true;
-    } else if (absl::EndsWith(host, "-sts") ||
-               absl::EndsWith(host, "-iamcredentials")) {
-      return true;
-    }
-  }
-  return false;
-}
-
-bool ValidateExteralAccountCredentials(const Json& json) {
-  return json.type() == Json::Type::OBJECT &&
-         ValidateUrlField(json, "token_url") &&
-         ValidateUrlField(json, "service_account_impersonation_url") &&
-         ValidateUrlField(json, "token_info_url");
-}
-
-}  // namespace
-
 // Takes ownership of creds_path if not NULL.
 static grpc_error_handle create_default_creds_from_path(
     const std::string& creds_path,
@@ -363,11 +312,6 @@ static grpc_error_handle create_default_creds_from_path(
     goto end;
   }
 
-  // Finally try an external account credentials.
-  if (!ValidateExteralAccountCredentials(json)) {
-    error = GRPC_ERROR_CREATE("Invalid external account credentials format.");
-    goto end;
-  }
   result = grpc_core::ExternalAccountCredentials::Create(json, {}, &error);
 
 end:
