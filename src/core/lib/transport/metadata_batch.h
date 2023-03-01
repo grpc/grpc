2485fa94bd8a723e5c977d55a3ce10b301b437f8
Craig Tiller <ctiller@google.com>
2023-03-01 09:17:49 -0800

    [chttp2] Fix fuzzer found bug (#32507)
    <!--
    
    If you know who should review your pull request, please assign it to
    that
    person, otherwise the pull request would get assigned randomly.
    
    If your pull request is for a specific language, please add the
    appropriate
    lang label.
    
    -->

--- src/core/lib/transport/metadata_batch.h
+++ src/core/lib/transport/metadata_batch.h
@@ -49,6 +49,16 @@
 
 namespace grpc_core {
 
+// Given a metadata key and a value, return the encoded size.
+// Defaults to calling the key's Encode() method and then calculating the size
+// of that, but can be overridden for specific keys if there's a better way of
+// doing this.
+// May return 0 if the size is unknown/unknowable.
+template <typename Key>
+size_t EncodedSizeOfKey(Key, const typename Key::ValueType& value) {
+  return Key::Encode(value).size();
+}
+
 // grpc-timeout metadata trait.
 // ValueType is defined as Timestamp - an absolute timestamp (i.e. a
 // deadline!), that is converted to a duration by transports before being
@@ -89,6 +99,10 @@ struct TeMetadata {
   static const char* DisplayValue(ValueType te);
   static const char* DisplayMemento(MementoType te) { return DisplayValue(te); }
 };
+
+inline size_t EncodedSizeOfKey(TeMetadata, TeMetadata::ValueType x) {
+  return x == TeMetadata::kTrailers ? 8 : 0;
+}
 
 // content-type metadata trait.
 struct ContentTypeMetadata {
@@ -139,6 +153,8 @@ struct HttpSchemeMetadata {
     return DisplayValue(content_type);
   }
 };
+
+size_t EncodedSizeOfKey(HttpSchemeMetadata, HttpSchemeMetadata::ValueType x);
 
 // method metadata trait.
 struct HttpMethodMetadata {
@@ -361,6 +377,11 @@ struct GrpcLbClientStatsMetadata {
     return nullptr;
   }
 };
+
+inline size_t EncodedSizeOfKey(GrpcLbClientStatsMetadata,
+                               GrpcLbClientStatsMetadata::ValueType) {
+  return 0;
+}
 
 // lb-token metadata
 struct LbTokenMetadata : public SimpleSliceBasedMetadata {
