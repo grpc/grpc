b630d042604c323661620dc0526ff0c11d0f43ba
AJ Heller <hork@google.com>
2023-03-06 17:59:12 -0800

    fix linux *SAN tests

--- test/core/end2end/run_end2end_tests.py
+++ test/core/end2end/run_end2end_tests.py
@@ -24,7 +24,7 @@ from typing import Tuple
 
 
 def GetBinaryAbsolutePath(fixture_name: str) -> str:
-    """Finds the absolute path to the test binary using bazel's runfile library.
+    """Finds the absolute path to the test binary using bazel's runfile tools.
 
     On Windows, there are no symlinks in the runfiles directory like there are
     on Posix systems. This tool solves the problem of locating the test
@@ -38,20 +38,28 @@ def GetBinaryAbsolutePath(fixture_name: str) -> str:
 
 def SplitBinaryPathByRunfileLocation(abspath: str) -> Tuple[str, str]:
     """Converts the path to platform-specific cwd and related path strings."""
-    path = Path(abspath)
-    return path.parent, path.name
+    exec_cwd, exec_path = None, abspath
+    if sys.platform == "win32":
+        # Minimize the path length. This is not done for Linux/Mac because it
+        # affects the *SAN suppressions list search.
+        path = Path(abspath)
+        exec_cwd = path.parent
+        exec_path = path.name
+    return exec_cwd, exec_path
 
 
 def main() -> None:
     assert len(sys.argv) >= 2, "Usage: run.py test_fixture [test ...]"
     executable_name = GetBinaryAbsolutePath(sys.argv[1])
-    # To avoid long filenames on Windows, split the executable name into a reasonable cwd and path.
+    # To avoid long filenames on Windows, split the executable name into a
+    # reasonable cwd and path.
     exec_cwd, exec_path = SplitBinaryPathByRunfileLocation(executable_name)
     cmds = [exec_path]
     if len(sys.argv) > 2:
         cmds.extend(sys.argv[2:])
     print(f"Executing {cmds} in cwd={exec_cwd}")
-    # On Windows, shell=True is necessary for `cwd` to be used for the binary search path.
+    # On Windows, shell=True is necessary for `cwd` to be used for the binary
+    # search path.
     subprocess.run(' '.join(cmds), check=True, cwd=exec_cwd, shell=True)
 
 
