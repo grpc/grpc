9c3dc19639e8d505f8314c4f13147c7afc40509d
Cheng-Yu Chung <chengyuc@google.com>
2023-03-08 13:56:29 -0500

    Fix IWYU

--- test/core/transport/promise_endpoint_test.cc
+++ test/core/transport/promise_endpoint_test.cc
@@ -14,6 +14,8 @@
 
 #include "src/core/lib/transport/promise_endpoint.h"
 
+// IWYU pragma: no_include <sys/socket.h>
+
 #include <algorithm>
 #include <cstring>
 #include <functional>
@@ -27,7 +29,7 @@
 #include "gtest/gtest.h"
 
 #include <grpc/event_engine/event_engine.h>
-#include <grpc/event_engine/port.h>
+#include <grpc/event_engine/port.h>  // IWYU pragma: keep
 #include <grpc/event_engine/slice_buffer.h>
 #include <grpc/support/log.h>
 
