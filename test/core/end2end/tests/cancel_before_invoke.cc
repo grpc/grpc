74ae7b272ba7bd8c7f40e3100977fbce1153e0c7
Craig Tiller <ctiller@google.com>
2023-03-11 16:56:25 -0800

    cancel-before-invoke

--- test/core/end2end/tests/cancel_before_invoke.cc
+++ test/core/end2end/tests/cancel_before_invoke.cc
@@ -36,117 +36,87 @@
 #include "test/core/end2end/end2end_tests.h"
 #include "test/core/util/test_config.h"
 
-static std::unique_ptr<grpc_core::CoreTestFixture> begin_test(
-    const grpc_core::CoreTestConfiguration& config, const char* test_name,
-    size_t num_ops, grpc_channel_args* client_args,
-    grpc_channel_args* server_args) {
-  gpr_log(GPR_INFO, "Running test: %s/%s [%" PRIdPTR " ops]", test_name,
-          config.name, num_ops);
-  auto f = config.create_fixture(grpc_core::ChannelArgs::FromC(client_args),
-                                 grpc_core::ChannelArgs::FromC(server_args));
-  f->InitServer(grpc_core::ChannelArgs::FromC(server_args));
-  f->InitClient(grpc_core::ChannelArgs::FromC(client_args));
-  return f;
+namespace grpc_core {
+
+TEST_P(CoreEnd2endTest, CancelBeforeInvoke6) {
+  auto c = NewClientCall("/service/method").Create();
+  grpc_call_cancel(c.c_call(), nullptr);
+  CoreEnd2endTest::IncomingStatusOnClient server_status;
+  CoreEnd2endTest::IncomingMetadata server_initial_metadata;
+  CoreEnd2endTest::IncomingMessage server_message;
+  c.NewBatch(1)
+      .RecvStatusOnClient(server_status)
+      .SendInitialMetadata({})
+      .SendMessage(RandomSlice(1024))
+      .SendCloseFromClient()
+      .RecvInitialMetadata(server_initial_metadata)
+      .RecvMessage(server_message);
+  Expect(1, AnyStatus());
+  Step();
+  EXPECT_EQ(server_status.status(), GRPC_STATUS_CANCELLED);
 }
 
-// Cancel before invoke
-static void test_cancel_before_invoke(
-    const grpc_core::CoreTestConfiguration& config, size_t test_ops) {
-  grpc_op ops[6];
-  grpc_op* op;
-  grpc_call* c;
-  auto f =
-      begin_test(config, "cancel_before_invoke", test_ops, nullptr, nullptr);
-  grpc_core::CqVerifier cqv(f->cq());
-  grpc_metadata_array initial_metadata_recv;
-  grpc_metadata_array trailing_metadata_recv;
-  grpc_metadata_array request_metadata_recv;
-  grpc_call_details call_details;
-  grpc_status_code status;
-  grpc_call_error error;
-  grpc_slice details;
-  grpc_byte_buffer* response_payload_recv = nullptr;
-  grpc_slice request_payload_slice =
-      grpc_slice_from_copied_string("hello world");
-  grpc_byte_buffer* request_payload =
-      grpc_raw_byte_buffer_create(&request_payload_slice, 1);
-
-  gpr_timespec deadline = grpc_timeout_seconds_to_deadline(5);
-  c = grpc_channel_create_call(f->client(), nullptr, GRPC_PROPAGATE_DEFAULTS,
-                               f->cq(), grpc_slice_from_static_string("/foo"),
-                               nullptr, deadline, nullptr);
-  GPR_ASSERT(c);
-
-  GPR_ASSERT(GRPC_CALL_OK == grpc_call_cancel(c, nullptr));
-
-  grpc_metadata_array_init(&initial_metadata_recv);
-  grpc_metadata_array_init(&trailing_metadata_recv);
-  grpc_metadata_array_init(&request_metadata_recv);
-  grpc_call_details_init(&call_details);
-
-  memset(ops, 0, sizeof(ops));
-  op = ops;
-  op->op = GRPC_OP_RECV_STATUS_ON_CLIENT;
-  op->data.recv_status_on_client.trailing_metadata = &trailing_metadata_recv;
-  op->data.recv_status_on_client.status = &status;
-  op->data.recv_status_on_client.status_details = &details;
-  op->flags = 0;
-  op->reserved = nullptr;
-  op++;
-  op->op = GRPC_OP_SEND_INITIAL_METADATA;
-  op->data.send_initial_metadata.count = 0;
-  op->flags = 0;
-  op->reserved = nullptr;
-  op++;
-  op->op = GRPC_OP_SEND_MESSAGE;
-  op->data.send_message.send_message = request_payload;
-  op->flags = 0;
-  op->reserved = nullptr;
-  op++;
-  op->op = GRPC_OP_SEND_CLOSE_FROM_CLIENT;
-  op->flags = 0;
-  op->reserved = nullptr;
-  op++;
-  op->op = GRPC_OP_RECV_INITIAL_METADATA;
-  op->data.recv_initial_metadata.recv_initial_metadata = &initial_metadata_recv;
-  op->flags = 0;
-  op->reserved = nullptr;
-  op++;
-  op->op = GRPC_OP_RECV_MESSAGE;
-  op->data.recv_message.recv_message = &response_payload_recv;
-  op->flags = 0;
-  op->reserved = nullptr;
-  op++;
-  error = grpc_call_start_batch(c, ops, test_ops, grpc_core::CqVerifier::tag(1),
-                                nullptr);
-  GPR_ASSERT(GRPC_CALL_OK == error);
-
-  // Filter based stack tracks this as a failed op, promise based stack tracks
-  // it as a successful one with a failed request. The latter probably makes
-  // more sense, but since we can't tell from outside which case we have we
-  // accept either.
-  cqv.Expect(grpc_core::CqVerifier::tag(1), grpc_core::CqVerifier::AnyStatus());
-  cqv.Verify();
-
-  GPR_ASSERT(status == GRPC_STATUS_CANCELLED);
-
-  grpc_metadata_array_destroy(&initial_metadata_recv);
-  grpc_metadata_array_destroy(&trailing_metadata_recv);
-  grpc_metadata_array_destroy(&request_metadata_recv);
-  grpc_call_details_destroy(&call_details);
-
-  grpc_byte_buffer_destroy(request_payload);
-  grpc_byte_buffer_destroy(response_payload_recv);
-  grpc_slice_unref(details);
-
-  grpc_call_unref(c);
+TEST_P(CoreEnd2endTest, CancelBeforeInvoke5) {
+  auto c = NewClientCall("/service/method").Create();
+  grpc_call_cancel(c.c_call(), nullptr);
+  CoreEnd2endTest::IncomingStatusOnClient server_status;
+  CoreEnd2endTest::IncomingMetadata server_initial_metadata;
+  c.NewBatch(1)
+      .RecvStatusOnClient(server_status)
+      .SendInitialMetadata({})
+      .SendMessage(RandomSlice(1024))
+      .SendCloseFromClient()
+      .RecvInitialMetadata(server_initial_metadata);
+  Expect(1, AnyStatus());
+  Step();
+  EXPECT_EQ(server_status.status(), GRPC_STATUS_CANCELLED);
 }
 
-void cancel_before_invoke(const grpc_core::CoreTestConfiguration& config) {
-  size_t i;
-  for (i = 1; i <= 6; i++) {
-    test_cancel_before_invoke(config, i);
-  }
+TEST_P(CoreEnd2endTest, CancelBeforeInvoke4) {
+  auto c = NewClientCall("/service/method").Create();
+  grpc_call_cancel(c.c_call(), nullptr);
+  CoreEnd2endTest::IncomingStatusOnClient server_status;
+  c.NewBatch(1)
+      .RecvStatusOnClient(server_status)
+      .SendInitialMetadata({})
+      .SendMessage(RandomSlice(1024))
+      .SendCloseFromClient();
+  Expect(1, AnyStatus());
+  Step();
+  EXPECT_EQ(server_status.status(), GRPC_STATUS_CANCELLED);
 }
 
-void cancel_before_invoke_pre_init(void) {}
+TEST_P(CoreEnd2endTest, CancelBeforeInvoke3) {
+  auto c = NewClientCall("/service/method").Create();
+  grpc_call_cancel(c.c_call(), nullptr);
+  CoreEnd2endTest::IncomingStatusOnClient server_status;
+  c.NewBatch(1)
+      .RecvStatusOnClient(server_status)
+      .SendInitialMetadata({})
+      .SendMessage(RandomSlice(1024));
+  Expect(1, AnyStatus());
+  Step();
+  EXPECT_EQ(server_status.status(), GRPC_STATUS_CANCELLED);
+}
+
+TEST_P(CoreEnd2endTest, CancelBeforeInvoke2) {
+  auto c = NewClientCall("/service/method").Create();
+  grpc_call_cancel(c.c_call(), nullptr);
+  CoreEnd2endTest::IncomingStatusOnClient server_status;
+  c.NewBatch(1).RecvStatusOnClient(server_status).SendInitialMetadata({});
+  Expect(1, AnyStatus());
+  Step();
+  EXPECT_EQ(server_status.status(), GRPC_STATUS_CANCELLED);
+}
+
+TEST_P(CoreEnd2endTest, CancelBeforeInvoke1) {
+  auto c = NewClientCall("/service/method").Create();
+  grpc_call_cancel(c.c_call(), nullptr);
+  CoreEnd2endTest::IncomingStatusOnClient server_status;
+  c.NewBatch(1).RecvStatusOnClient(server_status);
+  Expect(1, AnyStatus());
+  Step();
+  EXPECT_EQ(server_status.status(), GRPC_STATUS_CANCELLED);
+}
+
+}  // namespace grpc_core
