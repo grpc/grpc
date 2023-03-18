857897a59d2015bb2fd5750f08df8c1623a30460
Stan Hu <stanhu@gmail.com>
2023-03-18 07:30:28 -0700

    ruby: Avoid environment flags when building gprc code
    https://github.com/grpc/grpc/pull/27660 caused `CPPFLAGS` to inherit
    from the environment, but this can cause the Makefile to use external
    include files for re2 and other libraries if `-I` flags are defined.
    
    This commit reverts to the original behavior of only using
    `RbConfig::CONFIG` values to avoid using the wrong headers.

--- src/ruby/ext/grpc/extconf.rb
+++ src/ruby/ext/grpc/extconf.rb
@@ -34,16 +34,11 @@ def env_unset?(name)
   ENV[name].nil? || ENV[name].size == 0
 end
 
-def rbconfig_set?(name)
-  RbConfig::CONFIG[name] && RbConfig::CONFIG[name].size > 0
-end
-
 def inherit_rbconfig(name)
-  ENV[name] = RbConfig::CONFIG[name] if env_unset?(name) && rbconfig_set?(name)
+  ENV[name] = RbConfig::CONFIG[name] || ''
 end
 
 def env_append(name, string)
-  ENV[name] ||= ''
   ENV[name] += ' ' + string
 end
 
