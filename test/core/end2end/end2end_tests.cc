db21e3b40cdbbc4005887a0333246350e9e40984
ctiller <ctiller@users.noreply.github.com>
2023-03-21 22:38:01 +0000

    Automated change: Fix sanity tests

--- test/core/end2end/end2end_tests.cc
+++ test/core/end2end/end2end_tests.cc
@@ -21,7 +21,6 @@
 
 #include "absl/memory/memory.h"
 #include "absl/random/random.h"
-#include "test/core/end2end/end2end_tests.h"
 
 #include <grpc/byte_buffer_reader.h>
 #include <grpc/compression.h>
