9888d05304a99df5b22ab3395cf26edca0f090ff
Craig Tiller <ctiller@google.com>
2023-03-20 21:29:59 -0700

    fix ub

--- fuzztest/fuzztest_test.cc
+++ fuzztest/fuzztest_test.cc
@@ -20,5 +20,7 @@
 
 TEST(MyTestSuite, OnePlustTwoIsTwoPlusOne) { EXPECT_EQ(1 + 2, 2 + 1); }
 
-void IntegerAdditionCommutes(int a, int b) { EXPECT_EQ(a + b, b + a); }
+void IntegerAdditionCommutes(unsigned a, unsigned b) {
+  EXPECT_EQ(a + b, b + a);
+}
 FUZZ_TEST(MyTestSuite, IntegerAdditionCommutes);
