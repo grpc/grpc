951c52ec250dbda9e7e06a771f3c03d9c0179883
Jan Tattermusch <jtattermusch@google.com>
2023-03-22 17:14:04 +0100

    Switch to protobuf's /CMakeLists.txt instead of /cmake/CMakeLists.txt (#32678)
    Try to react to https://github.com/protocolbuffers/protobuf/issues/12263
    and https://github.com/protocolbuffers/protobuf/pull/12295
    
    Protobuf's /cmake/CMakeLists.txt is now deprecated if favor of
    /CMakeLists.txt
    
    
    https://github.com/protocolbuffers/protobuf/blob/58ecf38aa521ba812a0c1a0ae9f108dd311c739c/cmake/CMakeLists.txt#L3
    
    In this PR:
    - Adjust `cmake/protobuf.cmake` to use the right CMakeLists.txt
    - adjust the C++ distribtests accordingly

--- test/distrib/cpp/run_distrib_test_cmake_pkgconfig.sh
+++ test/distrib/cpp/run_distrib_test_cmake_pkgconfig.sh
@@ -40,7 +40,7 @@ popd
 # Install protobuf
 mkdir -p "third_party/protobuf/cmake/build"
 pushd "third_party/protobuf/cmake/build"
-cmake -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release ..
+cmake -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release ../..
 make "-j${GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS}" install
 popd
 
