882c179507e30547bbf4aca1a15691e91c77776b
Sergii Tkachenko <sergiitk@google.com>
2023-03-24 16:39:02 -0700

    PSM interop: add retries for k8s API 504 Gateway Timeout

--- tools/run_tests/xds_k8s_test_driver/framework/infrastructure/k8s.py
+++ tools/run_tests/xds_k8s_test_driver/framework/infrastructure/k8s.py
@@ -276,6 +276,11 @@ class KubernetesNamespace:  # pylint: disable=too-many-public-methods
             # resumes normal operation.
             return _quick_recovery_retryer()
 
+        # 504 Gateway Timeout:
+        # "Timeout: request did not complete within the allotted timeout"
+        if code == 504:
+            return _server_restart_retryer()
+
         return None
 
     @classmethod
