0e1e44d480d98652f4141eab86c9eca009a697a0
Craig Tiller <ctiller@google.com>
2023-03-31 15:54:37 -0700

    allow for reuse of our suites

--- test/core/end2end/end2end_tests.cc
+++ test/core/end2end/end2end_tests.cc
@@ -19,8 +19,11 @@
 
 #include "test/core/end2end/end2end_tests.h"
 
+#include <regex>
+
 #include "absl/memory/memory.h"
 #include "absl/random/random.h"
+#include "end2end_tests.h"
 
 #include <grpc/byte_buffer_reader.h>
 #include <grpc/compression.h>
