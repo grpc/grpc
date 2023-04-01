20845401159d9a8d0781f50aebeaafc8aa683246
AJ Heller <hork@google.com>
2023-04-01 14:31:39 -0700

    Revert "Revert "[EventEngine] Implement the EventEngine-driven client channel resolver." (#32768)"
    This reverts commit b5a2e90fbf59174a0c98df8477970c1f2f16b62c.

--- src/core/ext/filters/client_channel/resolver/dns/dns_resolver_plugin.h
+++ src/core/ext/filters/client_channel/resolver/dns/dns_resolver_plugin.h
@@ -11,15 +11,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#ifndef GRPC_SRC_CORE_EXT_FILTERS_CLIENT_CHANNEL_RESOLVER_DNS_DNS_RESOLVER_PLUGIN_H
+#define GRPC_SRC_CORE_EXT_FILTERS_CLIENT_CHANNEL_RESOLVER_DNS_DNS_RESOLVER_PLUGIN_H
 #include <grpc/support/port_platform.h>
 
-#include "src/core/lib/debug/trace.h"
+#include "src/core/lib/config/core_configuration.h"
 
-grpc_core::TraceFlag grpc_event_engine_trace(false, "event_engine");
-grpc_core::TraceFlag grpc_event_engine_dns_trace(false, "event_engine_dns");
-grpc_core::TraceFlag grpc_event_engine_endpoint_trace(false,
-                                                      "event_engine_endpoint");
-grpc_core::TraceFlag grpc_event_engine_endpoint_data_trace(
-    false, "event_engine_endpoint_data");
-grpc_core::TraceFlag grpc_event_engine_poller_trace(false,
-                                                    "event_engine_poller");
+namespace grpc_core {
+
+// Centralized decision logic about which client channel DNS resolver to enable.
+void RegisterDnsResolver(CoreConfiguration::Builder* builder);
+
+}  // namespace grpc_core
+
+#endif  // GRPC_SRC_CORE_EXT_FILTERS_CLIENT_CHANNEL_RESOLVER_DNS_DNS_RESOLVER_PLUGIN_H
