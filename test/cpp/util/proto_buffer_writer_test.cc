9e17ab88c02fb2d221fb538c988a52c460ad4c5b
Esun Kim <veblush@google.com>
2023-04-13 12:50:51 -0700

    Added test

--- test/cpp/util/proto_buffer_writer_test.cc
+++ test/cpp/util/proto_buffer_writer_test.cc
@@ -25,52 +25,51 @@ namespace grpc {
 namespace {
 
 TEST(ProtoBufferWriterTest, Next) {
-    ByteBuffer buffer;
-    ProtoBufferWriter writer(&buffer, 16, 256);
-    // 1st next
-    void* data1;
-    int size1;
-    EXPECT_TRUE(writer.Next(&data1, &size1));
-    EXPECT_GT(size1, 0);
-    memset(data1, 1, size1);
-    // 2nd next
-    void* data2;
-    int size2;
-    EXPECT_TRUE(writer.Next(&data2, &size2));
-    EXPECT_GT(size2, 0);
-    memset(data2, 2, size2);
-    // Done
-    EXPECT_EQ(writer.ByteCount(), size1 + size2);
-    EXPECT_EQ(buffer.Length(), size1 + size2);
-    Slice slice;
-    buffer.DumpToSingleSlice(&slice);
-    EXPECT_EQ(memcmp(slice.begin(), data1, size1), 0);
-    EXPECT_EQ(memcmp(slice.begin() + size1, data2, size2), 0);
+  ByteBuffer buffer;
+  ProtoBufferWriter writer(&buffer, 16, 256);
+  // 1st next
+  void* data1;
+  int size1;
+  EXPECT_TRUE(writer.Next(&data1, &size1));
+  EXPECT_GT(size1, 0);
+  memset(data1, 1, size1);
+  // 2nd next
+  void* data2;
+  int size2;
+  EXPECT_TRUE(writer.Next(&data2, &size2));
+  EXPECT_GT(size2, 0);
+  memset(data2, 2, size2);
+  // Done
+  EXPECT_EQ(writer.ByteCount(), size1 + size2);
+  EXPECT_EQ(buffer.Length(), size1 + size2);
+  Slice slice;
+  buffer.DumpToSingleSlice(&slice);
+  EXPECT_EQ(memcmp(slice.begin(), data1, size1), 0);
+  EXPECT_EQ(memcmp(slice.begin() + size1, data2, size2), 0);
 }
 
 #ifdef GRPC_PROTOBUF_CORD_SUPPORT_ENABLED
 
 TEST(ProtoBufferWriterTest, WriteCord) {
-    ByteBuffer buffer;
-    ProtoBufferWriter writer(&buffer, 16, 4096);
-    // Cord
-    absl::Cord cord;
-    std::string str1 = std::string(1024, 'a');
-    cord.Append(str1);
-    std::string str2 = std::string(1024, 'b');
-    cord.Append(str2);
-    writer.WriteCord(cord);
-    // Done
-    EXPECT_EQ(writer.ByteCount(), str1.size() + str2.size());
-    EXPECT_EQ(buffer.Length(), str1.size() + str2.size());
-    Slice slice;
-    buffer.DumpToSingleSlice(&slice);
-    EXPECT_EQ(memcmp(slice.begin(), str1.c_str(), str1.size()), 0);
-    EXPECT_EQ(memcmp(slice.begin() + str1.size(), str2.c_str(), str2.size()), 0);
+  ByteBuffer buffer;
+  ProtoBufferWriter writer(&buffer, 16, 4096);
+  // Cord
+  absl::Cord cord;
+  std::string str1 = std::string(1024, 'a');
+  cord.Append(str1);
+  std::string str2 = std::string(1024, 'b');
+  cord.Append(str2);
+  writer.WriteCord(cord);
+  // Done
+  EXPECT_EQ(writer.ByteCount(), str1.size() + str2.size());
+  EXPECT_EQ(buffer.Length(), str1.size() + str2.size());
+  Slice slice;
+  buffer.DumpToSingleSlice(&slice);
+  EXPECT_EQ(memcmp(slice.begin(), str1.c_str(), str1.size()), 0);
+  EXPECT_EQ(memcmp(slice.begin() + str1.size(), str2.c_str(), str2.size()), 0);
 }
 
 #endif  // GRPC_PROTOBUF_CORD_SUPPORT_ENABLED
-
 
 }  // namespace
 }  // namespace grpc
