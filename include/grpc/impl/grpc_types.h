9badf2894c7cc708ea8a365bb06000b5c55f90e1
AJ Heller <hork@google.com>
2023-04-18 20:03:08 -0700

    [tcp] Add channel argument for SO_RVCBUF size. (#32887)
    Example usage:
    
    ```
    ServerBuilder builder;
    builder.AddChannelArgument(GRPC_ARG_TCP_RECEIVE_BUFFER_SIZE, 1024*1024);
    ```

--- include/grpc/impl/grpc_types.h
+++ include/grpc/impl/grpc_types.h
@@ -368,6 +368,8 @@ typedef struct {
    issued by the tcp_write(). By default, this is set to 4. */
 #define GRPC_ARG_TCP_TX_ZEROCOPY_MAX_SIMULT_SENDS \
   "grpc.experimental.tcp_tx_zerocopy_max_simultaneous_sends"
+/* Overrides the TCP socket recieve buffer size, SO_RCVBUF. */
+#define GRPC_ARG_TCP_RECEIVE_BUFFER_SIZE "grpc.tcp_receive_buffer_size"
 /* Timeout in milliseconds to use for calls to the grpclb load balancer.
    If 0 or unset, the balancer calls will have no deadline. */
 #define GRPC_ARG_GRPCLB_CALL_TIMEOUT_MS "grpc.grpclb_call_timeout_ms"
