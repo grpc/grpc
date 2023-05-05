da397005cce8950e48b014ff16b7848c9f196bbe
Easwar Swaminathan <easwars@google.com>
2023-05-05 11:58:35 -0700

    Update client_matrix.py
    We just pushed a patch release for 1.54

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -215,7 +215,7 @@ LANG_RELEASE_MATRIX = {
             ('v1.51.0', ReleaseInfo(runtimes=['go1.16'])),
             ('v1.52.3', ReleaseInfo(runtimes=['go1.19'])),
             ('v1.53.0', ReleaseInfo(runtimes=['go1.19'])),
-            ('v1.54.0', ReleaseInfo(runtimes=['go1.19'])),
+            ('v1.54.1', ReleaseInfo(runtimes=['go1.19'])),
             ('v1.55.0', ReleaseInfo(runtimes=['go1.19'])),
         ]),
     'java':
