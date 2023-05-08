33185fec023e81140ea23c74944e56cb1a2f0fa1
Craig Tiller <ctiller@google.com>
2023-05-08 16:12:50 -0700

    iwyu

--- src/core/ext/filters/client_channel/service_config_channel_arg_filter.cc
+++ src/core/ext/filters/client_channel/service_config_channel_arg_filter.cc
@@ -33,8 +33,6 @@
 #include "src/core/lib/channel/channel_args.h"
 #include "src/core/lib/channel/channel_fwd.h"
 #include "src/core/lib/channel/channel_stack.h"
-#include "src/core/lib/channel/channel_stack_builder.h"
-#include "src/core/lib/channel/context.h"
 #include "src/core/lib/config/core_configuration.h"
 #include "src/core/lib/gprpp/ref_counted_ptr.h"
 #include "src/core/lib/iomgr/closure.h"
