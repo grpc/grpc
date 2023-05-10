c3bd9b83f5cb5f7a69f5aabc37c260af160e6616
Craig Tiller <ctiller@google.com>
2023-05-10 16:03:04 -0700

    proj

--- test/core/end2end/end2end_test_fuzzer.cc
+++ test/core/end2end/end2end_test_fuzzer.cc
@@ -13,13 +13,10 @@
 // limitations under the License.
 
 #include <stddef.h>
-#include <stdint.h>
 
 #include <algorithm>
-#include <array>
 #include <chrono>
 #include <memory>
-#include <numeric>
 #include <string>
 #include <type_traits>
 #include <utility>
@@ -50,6 +47,7 @@
 #include "test/core/event_engine/fuzzing_event_engine/fuzzing_event_engine.pb.h"
 #include "test/core/util/fuzz_config_vars.h"
 #include "test/core/util/fuzz_config_vars.pb.h"
+#include "test/core/util/osa_distance.h"
 
 using ::grpc_event_engine::experimental::FuzzingEventEngine;
 using ::grpc_event_engine::experimental::GetDefaultEventEngine;
@@ -69,64 +67,6 @@ int force_experiments = []() {
   grpc_core::ForceEnableExperiment("event_engine_listener", true);
   return 1;
 }();
-
-namespace {
-// copy-paste from absl
-uint8_t CappedDamerauLevenshteinDistance(absl::string_view s1,
-                                         absl::string_view s2, uint8_t cutoff) {
-  const uint8_t MAX_SIZE = 100;
-  const uint8_t _cutoff = std::min(MAX_SIZE, cutoff);
-  const uint8_t cutoff_plus_1 = static_cast<uint8_t>(_cutoff + 1);
-
-  if (s1.size() > s2.size()) std::swap(s1, s2);
-  if (s1.size() + _cutoff < s2.size() || s2.size() > MAX_SIZE)
-    return cutoff_plus_1;
-
-  if (s1.empty()) return static_cast<uint8_t>(s2.size());
-
-  // Lower diagonal bound: y = x - lower_diag
-  const uint8_t lower_diag =
-      _cutoff - static_cast<uint8_t>(s2.size() - s1.size());
-  // Upper diagonal bound: y = x + upper_diag
-  const uint8_t upper_diag = _cutoff;
-
-  // d[i][j] is the number of edits required to convert s1[0, i] to s2[0, j]
-  std::array<std::array<uint8_t, MAX_SIZE + 2>, MAX_SIZE + 2> d;
-  std::iota(d[0].begin(), d[0].begin() + upper_diag + 1, 0);
-  d[0][cutoff_plus_1] = cutoff_plus_1;
-  for (size_t i = 1; i <= s1.size(); ++i) {
-    // Deduce begin of relevant window.
-    size_t j_begin = 1;
-    if (i > lower_diag) {
-      j_begin = i - lower_diag;
-      d[i][j_begin - 1] = cutoff_plus_1;
-    } else {
-      d[i][0] = static_cast<uint8_t>(i);
-    }
-
-    // Deduce end of relevant window.
-    size_t j_end = i + upper_diag;
-    if (j_end > s2.size()) {
-      j_end = s2.size();
-    } else {
-      d[i][j_end + 1] = cutoff_plus_1;
-    }
-
-    for (size_t j = j_begin; j <= j_end; ++j) {
-      const uint8_t deletion_distance = d[i - 1][j] + 1;
-      const uint8_t insertion_distance = d[i][j - 1] + 1;
-      const uint8_t mismatched_tail_cost = s1[i - 1] == s2[j - 1] ? 0 : 1;
-      const uint8_t mismatch_distance = d[i - 1][j - 1] + mismatched_tail_cost;
-      uint8_t transposition_distance = _cutoff + 1;
-      if (i > 1 && j > 1 && s1[i - 1] == s2[j - 2] && s1[i - 2] == s2[j - 1])
-        transposition_distance = d[i - 2][j - 2] + 1;
-      d[i][j] = std::min({cutoff_plus_1, deletion_distance, insertion_distance,
-                          mismatch_distance, transposition_distance});
-    }
-  }
-  return d[s1.size()][s2.size()];
-}
-}  // namespace
 
 DEFINE_PROTO_FUZZER(const core_end2end_test_fuzzer::Msg& msg) {
   grpc_core::g_is_fuzzing_core_e2e_tests = true;
@@ -158,7 +98,7 @@ DEFINE_PROTO_FUZZER(const core_end2end_test_fuzzer::Msg& msg) {
                      test.make_test(test.config));
                }});
     }
-    GPR_ASSERT(tests.size() > 0);
+    GPR_ASSERT(!tests.empty());
     return tests;
   }();
   static const auto only_experiment =
@@ -172,9 +112,8 @@ DEFINE_PROTO_FUZZER(const core_end2end_test_fuzzer::Msg& msg) {
       absl::StrCat(msg.suite(), ".", msg.test(), "/", msg.config());
   size_t best_test = 0;
   for (size_t i = 0; i < tests.size(); i++) {
-    if (CappedDamerauLevenshteinDistance(test_name, tests[i].name, 100) <
-        CappedDamerauLevenshteinDistance(test_name, tests[best_test].name,
-                                         100)) {
+    if (grpc_core::OsaDistance(test_name, tests[i].name) <
+        grpc_core::OsaDistance(test_name, tests[best_test].name)) {
       best_test = i;
     }
   }
