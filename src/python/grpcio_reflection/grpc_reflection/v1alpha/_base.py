74d00f2c9c483bebcc4e347d3286fea20889ec9a
Thomas Zeugner <tomsoftware@gmx.de>
2023-05-11 20:20:14 +0200

    [reflection] python: reflection response returns dependencies (#32909)
    Fix #32899 - include all dependency-descriptors in the
    ServerReflectionResponse
    
    Using the C# Server-code as blue print:
    
    https://github.com/grpc/grpc-dotnet/blob/6bf44820a2ef6af5845830728cd4b3d1a8053ed4/src/Grpc.Reflection/ReflectionServiceImpl.cs#L148
    
    see also:
       - The cpp-server implementation of this:
    
    https://github.com/grpc/grpc/blob/d299f5ecce9f8e9a8f35571f39bdcfdbb2106d36/src/cpp/ext/proto_server_reflection.cc#L208
      - The go-server implementation of this:
    
    https://github.com/grpc/grpc-go/blob/2cd95c7514a3d02aa2d98591c013885cb44fbdeb/reflection/serverreflection.go#L184
    `return s.fileDescWithDependencies(d.ParentFile(), sentFileDescriptors)`
    
    ---------
    
    Co-authored-by: Xuan Wang <xuanwn@google.com>

--- src/python/grpcio_reflection/grpc_reflection/v1alpha/_base.py
+++ src/python/grpcio_reflection/grpc_reflection/v1alpha/_base.py
@@ -30,13 +30,29 @@ def _not_found_error():
         ))
 
 
+def _collect_transitive_dependencies(descriptor, seen_files):
+    seen_files.update({descriptor.name: descriptor})
+    for dependency in descriptor.dependencies:
+        if not dependency.name in seen_files:
+            # descriptors cannot have circular dependencies
+            _collect_transitive_dependencies(dependency, seen_files)
+
+
 def _file_descriptor_response(descriptor):
-    proto = descriptor_pb2.FileDescriptorProto()
-    descriptor.CopyToProto(proto)
-    serialized_proto = proto.SerializeToString()
+    # collect all dependencies
+    descriptors = {}
+    _collect_transitive_dependencies(descriptor, descriptors)
+
+    # serialize all descriptors
+    serialized_proto_list = []
+    for d_key in descriptors:
+        proto = descriptor_pb2.FileDescriptorProto()
+        descriptors[d_key].CopyToProto(proto)
+        serialized_proto_list.append(proto.SerializeToString())
+
     return _reflection_pb2.ServerReflectionResponse(
         file_descriptor_response=_reflection_pb2.FileDescriptorResponse(
-            file_descriptor_proto=(serialized_proto,)),)
+            file_descriptor_proto=(serialized_proto_list)),)
 
 
 class BaseReflectionServicer(_reflection_pb2_grpc.ServerReflectionServicer):
