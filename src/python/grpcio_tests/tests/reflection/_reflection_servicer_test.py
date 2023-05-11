4c448e576b232a27f7a928bb4f5f0bf3063c2919
Xuan Wang <xuanwn@google.com>
2023-05-11 16:32:10 -0700

    Revert "[reflection] python: reflection response returns dependencies" (#33088)
    Reverts grpc/grpc#32909
    
    It's breaking some internal test
    (//net/grpc/python:internal_tests/unit/_default_reflection_test), revert
    for now to investigate.

--- src/python/grpcio_tests/tests/reflection/_reflection_servicer_test.py
+++ src/python/grpcio_tests/tests/reflection/_reflection_servicer_test.py
@@ -25,7 +25,6 @@ from grpc_reflection.v1alpha import reflection_pb2_grpc
 
 from src.proto.grpc.testing import empty_pb2
 from src.proto.grpc.testing.proto2 import empty2_extensions_pb2
-from src.proto.grpc.testing.proto2 import empty2_pb2
 from tests.unit import test_common
 
 _EMPTY_PROTO_FILE_NAME = 'src/proto/grpc/testing/empty.proto'
@@ -129,11 +128,9 @@ class ReflectionServicerTest(unittest.TestCase):
         expected_responses = (
             reflection_pb2.ServerReflectionResponse(
                 valid_host='',
-                file_descriptor_response=reflection_pb2.
-                FileDescriptorResponse(file_descriptor_proto=(
-                    _file_descriptor_to_proto(empty2_extensions_pb2.DESCRIPTOR),
-                    _file_descriptor_to_proto(empty2_pb2.DESCRIPTOR),
-                ))),
+                file_descriptor_response=reflection_pb2.FileDescriptorResponse(
+                    file_descriptor_proto=(_file_descriptor_to_proto(
+                        empty2_extensions_pb2.DESCRIPTOR),))),
             reflection_pb2.ServerReflectionResponse(
                 valid_host='',
                 error_response=reflection_pb2.ErrorResponse(
