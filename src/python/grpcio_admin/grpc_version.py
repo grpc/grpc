a789191e298702e16298649c189e0b6fff5d35f0
Richard Belleville <rbellevi@google.com>
2023-06-02 22:08:00 +0000

    regenerate projects

--- src/python/grpcio_admin/grpc_version.py
+++ src/python/grpcio_admin/grpc_version.py
@@ -14,4 +14,4 @@
 
 # AUTO-GENERATED FROM `$REPO_ROOT/templates/src/python/grpcio_admin/grpc_version.py.template`!!!
 
-VERSION = '1.55.1'
+VERSION = '1.55.2'
--- src/ruby/lib/grpc/version.rb
+++ src/ruby/lib/grpc/version.rb
@@ -14,5 +14,5 @@
 
 # GRPC contains the General RPC module.
 module GRPC
-  VERSION = '1.55.1'
+  VERSION = '1.55.2'
 end
