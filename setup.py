64c5c397a7b7010254bcde5a9bf4724e26d8e8c9
Xuan Wang <xuanwn@google.com>
2023-06-07 19:07:27 +0000

    Add TODO

--- setup.py
+++ setup.py
@@ -422,6 +422,8 @@ DEFINE_MACROS += (('GRPC_DO_NOT_INSTANTIATE_POSIX_POLLER', 1),)
 # <inttypes.h> was included in core/lib/channel/call_tracer.h.
 # This macro should already be defined in grpc/grpc.h through port_platform.h,
 # but we're still having issue in aarch64, so we manually define the macro here.
+# TODO(xuanwn): Figure out what's going on in the aarch64 build so we can support
+# gcc + Bazel.
 DEFINE_MACROS += (('__STDC_FORMAT_MACROS', None),)
 
 LDFLAGS = tuple(EXTRA_LINK_ARGS)
