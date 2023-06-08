78af09e3c87962e1ccad8c7a8961688a7fd5a353
Vignesh Babu <vigneshbabu@google.com>
2023-06-08 19:01:36 +0000

    sanity

--- test/cpp/end2end/resource_quota_end2end_stress_test.cc
+++ test/cpp/end2end/resource_quota_end2end_stress_test.cc
@@ -1,3 +1,17 @@
+// Copyright 2023 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <memory>
 #include <string>
 #include <thread>
@@ -34,7 +48,7 @@ constexpr int kPayloadSizeBytes = 1024 * 1024;
 constexpr int kNumParallelChannels = 1024;
 }  // namespace
 
-class EchoClientUnaryReactor : public ::grpc::ClientUnaryReactor {
+class EchoClientUnaryReactor : public grpc::ClientUnaryReactor {
  public:
   EchoClientUnaryReactor(ClientContext* ctx, EchoTestService::Stub* stub,
                          const std::string payload, Status* status)
@@ -69,7 +83,7 @@ class EchoServerUnaryReactor : public ServerUnaryReactor {
   EchoServerUnaryReactor(CallbackServerContext* /*ctx*/,
                          const EchoRequest* request, EchoResponse* response) {
     response->set_message(request->message());
-    Finish(::grpc::Status::OK);
+    Finish(grpc::Status::OK);
   }
 
  private:
@@ -94,7 +108,7 @@ class End2EndResourceQuotaUnaryTest : public ::testing::Test {
     ServerBuilder builder;
     builder.AddListeningPort(server_address_, InsecureServerCredentials());
     builder.SetResourceQuota(
-        ::grpc::ResourceQuota("TestService").Resize(kResourceQuotaSizeBytes));
+        grpc::ResourceQuota("TestService").Resize(kResourceQuotaSizeBytes));
     builder.RegisterService(&grpc_service_);
     server_ = builder.BuildAndStart();
   }
