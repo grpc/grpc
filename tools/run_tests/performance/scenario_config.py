6a2868991fbfccd85d8689af09e97547663935b5
Steven Wang <steven.l.wang@linux.intel.com>
2023-06-14 01:42:05 +0000

    [Test] Update the two files for the code style.
    Update json_run_localhost_scenarios.bzl and scenario_config.py
    for the code style.

--- tools/run_tests/performance/scenario_config.py
+++ tools/run_tests/performance/scenario_config.py
@@ -41,7 +41,12 @@ HISTOGRAM_PARAMS = {
 # non-ping-pong tests (since we can only specify per-channel numbers, the
 # actual target will be slightly higher)
 
-OUTSTANDING_REQUESTS = {"async": 6400, "async-limited": 800, "sync": 1000, "callback": 6400}
+OUTSTANDING_REQUESTS = {
+    "async": 6400,
+    "async-limited": 800,
+    "sync": 1000,
+    "callback": 6400,
+}
 
 # wide is the number of client channels in multi-channel tests (1 otherwise)
 WIDE = 64
