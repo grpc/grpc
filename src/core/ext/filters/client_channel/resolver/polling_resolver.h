b9813e82a52b22363d0bd3bc36ad9c29df9fcc1e
Anirudh Ramachandra <anramach@google.com>
2023-06-16 09:09:15 -0700

    Return raw pointer back instead of shared pointer

--- src/core/ext/filters/client_channel/resolver/polling_resolver.h
+++ src/core/ext/filters/client_channel/resolver/polling_resolver.h
@@ -69,7 +69,7 @@ class PollingResolver : public Resolver {
   const std::string& name_to_resolve() const { return name_to_resolve_; }
   grpc_pollset_set* interested_parties() const { return interested_parties_; }
   const ChannelArgs& channel_args() const { return channel_args_; }
-  std::shared_ptr<WorkSerializer> work_serializer() { return work_serializer_; }
+  WorkSerializer* work_serializer() { return work_serializer_.get(); }
 
  private:
   void MaybeStartResolvingLocked();
