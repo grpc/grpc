709e664fd3ea3c33253e7a590778976323700b97
Craig Tiller <ctiller@google.com>
2023-06-16 15:15:48 -0700

    iwyu

--- test/core/end2end/end2end_test_fuzzer.cc
+++ test/core/end2end/end2end_test_fuzzer.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <stdio.h>
+
 #include <algorithm>
 #include <chrono>
 #include <memory>
