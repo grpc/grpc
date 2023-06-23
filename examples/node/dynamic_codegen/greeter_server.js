1bf3f56c9798e8e162ffe1383bcef22580e1defb
Michael Lumish <mlumish@google.com>
2023-06-22 17:48:33 -0700

    [examples] Replace Node example files with references to the new location (#33522)
    This change is part of a migration of the Node examples from this
    repository to the grpc/grpc-node repository, along with
    grpc/grpc-node#2474. I replaced the files with references to the new
    location instead of deleting them so that people following links in
    existing external resources would find information about the new
    location instead of an unhelpful 404 error. I discovered that even some
    of the individual `.js` files are linked from other places, so to be
    safe I performed this replacement on every file in the directory.

--- examples/node/dynamic_codegen/greeter_server.js
+++ examples/node/dynamic_codegen/greeter_server.js
@@ -16,37 +16,4 @@
  *
  */
 
-var PROTO_PATH = __dirname + '/../../protos/helloworld.proto';
-
-var grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
-var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
-
-/**
- * Implements the SayHello RPC method.
- */
-function sayHello(call, callback) {
-  callback(null, {message: 'Hello ' + call.request.name});
-}
-
-/**
- * Starts an RPC server that receives requests for the Greeter service at the
- * sample server port
- */
-function main() {
-  var server = new grpc.Server();
-  server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
-  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-    server.start();
-  });
-}
-
-main();
+// These examples have been moved to https://github.com/grpc/grpc-node/tree/master/examples
