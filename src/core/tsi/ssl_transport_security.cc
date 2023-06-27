de37d785eba66c6efc0cda99c164405442f4499a
Matthew Stevenson <mattstev@google.com>
2023-06-27 09:27:10 -0700

    Remove use of SSL_CTX_set_client_CA_list in ssl_transport_security.cc.

--- src/core/tsi/ssl_transport_security.cc
+++ src/core/tsi/ssl_transport_security.cc
@@ -2207,7 +2207,6 @@ tsi_result tsi_create_ssl_server_handshaker_factory_with_options(
           gpr_log(GPR_ERROR, "Invalid verification certs.");
           break;
         }
-        SSL_CTX_set_client_CA_list(impl->ssl_contexts[i], root_names);
       }
       switch (options->client_certificate_request) {
         case TSI_DONT_REQUEST_CLIENT_CERTIFICATE:
