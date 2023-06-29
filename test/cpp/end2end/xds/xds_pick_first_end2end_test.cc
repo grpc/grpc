05a491a5b72ed36151cd21333f4106a69254935c
Eugene Ostroukhov <eostroukhov@google.com>
2023-06-29 13:21:01 -0700

    fixup: redo test

--- test/cpp/end2end/xds/xds_pick_first_end2end_test.cc
+++ test/cpp/end2end/xds/xds_pick_first_end2end_test.cc
@@ -50,7 +50,8 @@ using ::envoy::extensions::load_balancing_policies::pick_first::v3::PickFirst;
 
 class PickFirstTest : public XdsEnd2endTest {
  protected:
-  absl::variant<Status, size_t> WaitForAnyBackendHit(size_t start, size_t end) {
+  absl::variant<Status, size_t> WaitForAnyBackendHit(
+      size_t start, size_t end, size_t timeout_ms = 15000) {
     Status status;
     size_t index = 3;
     SendRpcsUntil(DEBUG_LOCATION, [&](const RpcResult& result) -> bool {
@@ -72,35 +73,6 @@ class PickFirstTest : public XdsEnd2endTest {
     }
     return index;
   }
-
-  void ConfigureCluster(size_t iteration, bool shuffle,
-                        size_t first_endpoint_index,
-                        size_t stop_endpoint_index) {
-    std::string edsService = absl::StrCat("edsservice", iteration);
-    // First 3 endpoints go to a service that will have regular pick_first LB
-    balancer_->ads_service()->SetEdsResource(BuildEdsResource(
-        EdsResourceArgs(
-            {{"locality0", CreateEndpointsForBackends(first_endpoint_index,
-                                                      stop_endpoint_index)}}),
-        edsService));
-    PickFirst pick_first;
-    pick_first.set_shuffle_address_list(shuffle);
-    auto cluster = default_cluster_;
-    cluster.clear_lb_policy();
-    cluster.clear_load_balancing_policy();
-    cluster.mutable_eds_cluster_config()->set_service_name(edsService);
-    cluster.mutable_load_balancing_policy()
-        ->add_policies()
-        ->mutable_typed_extension_config()
-        ->mutable_typed_config()
-        ->PackFrom(pick_first);
-
-    // Set ordered cluster configuration
-    balancer_->ads_service()->SetCdsResource(cluster);
-  }
-
-  grpc_core::RefCountedPtr<grpc_core::FakeResolverResponseGenerator>
-      logical_dns_cluster_resolver_response_generator_;
 };
 
 // Run both with and without load reporting, just for test coverage.
@@ -113,38 +85,33 @@ TEST_P(PickFirstTest, PickFirstConfigurationIsPropagated) {
   grpc_core::testing::ScopedExperimentalEnvVar env_var(
       "GRPC_EXPERIMENTAL_PICKFIRST_LB_CONFIG");
   CreateAndStartBackends(6);
-  SetListenerAndRouteConfiguration(balancer_.get(), default_listener_,
-                                   default_route_config_);
-  // Checking if we go back and forth between shuffled and ordered several times
-  size_t times_shuffle_detected = false;
-  constexpr size_t kExpectedTimesShuffled = 2;
-  // Shuffling has a chance of returning the same permutation. Make sure we
-  // retry it several times if needed
-  for (size_t attempt = 0;
-       attempt < 100 && times_shuffle_detected < kExpectedTimesShuffled;
-       ++attempt) {
-    // Use first 3 endpoints without shuffle
-    ConfigureCluster(attempt * 2, false, 0, 3);
-    WaitForBackendOptions opts;
-    opts.set_timeout_ms(30000);
-    WaitForBackend(DEBUG_LOCATION, 0, nullptr, opts);
-    // Confirm pick first LB is used
-    CheckRpcSendOk(DEBUG_LOCATION, 5);
-    EXPECT_EQ(5, backends_[0]->backend_service()->request_count());
-    backends_[0]->backend_service()->ResetCounters();
-    // Use endpoints 3-5 with shuffle
-    ConfigureCluster(attempt * 2 + 1, true, 3, 6);
-    auto res = WaitForAnyBackendHit(3, backends_.size());
-    ASSERT_FALSE(absl::holds_alternative<Status>(res))
-        << absl::get<Status>(res).error_message();
-    CheckRpcSendOk(DEBUG_LOCATION, 5);
-    size_t chosen_endpoint = absl::get<size_t>(res);
-    EXPECT_EQ(5,
-              backends_[chosen_endpoint]->backend_service()->request_count());
-    backends_[0]->backend_service()->ResetCounters();
-    times_shuffle_detected += chosen_endpoint != 3 ? 1 : 0;
+  // Change cluster to use pick_first with shuffle option.
+  auto cluster = default_cluster_;
+  PickFirst pick_first;
+  pick_first.set_shuffle_address_list(true);
+  cluster.mutable_load_balancing_policy()
+      ->add_policies()
+      ->mutable_typed_extension_config()
+      ->mutable_typed_config()
+      ->PackFrom(pick_first);
+  balancer_->ads_service()->SetCdsResource(cluster);
+  bool saw_different_backend = false;
+  size_t start_index = 0;
+  for (size_t i = 0; i < 100 && !saw_different_backend; ++i) {
+    // Update EDS resource.  This will send a new address list update to the LB
+    // policy.
+    balancer_->ads_service()->SetEdsResource(BuildEdsResource(
+        EdsResourceArgs({{"locality0", CreateEndpointsForBackends(
+                                           start_index, start_index + 3)}})));
+    auto result = WaitForAnyBackendHit(start_index, start_index + 3);
+    size_t backend_index = absl::get<size_t>(result);
+    ASSERT_FALSE(absl::holds_alternative<Status>(result))
+        << absl::get<Status>(result).error_message();
+    saw_different_backend = (backend_index - start_index) > 0;
+    // Toggle between backends 0-2 and 3-5
+    start_index = 3 - start_index;
   }
-  EXPECT_EQ(times_shuffle_detected, kExpectedTimesShuffled);
+  EXPECT_TRUE(saw_different_backend);
 }
 }  // namespace
 }  // namespace testing
