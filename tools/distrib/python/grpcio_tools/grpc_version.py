91a7b245b610ed6030c7e2456f80712b52dcbfcf
AJ Heller <hork@google.com>
2023-07-06 18:31:47 +0000

    regenerate projects

--- tools/distrib/python/grpcio_tools/grpc_version.py
+++ tools/distrib/python/grpcio_tools/grpc_version.py
@@ -15,4 +15,4 @@
 # AUTO-GENERATED FROM `$REPO_ROOT/templates/tools/distrib/python/grpcio_tools/grpc_version.py.template`!!!
 
 VERSION = '1.57.0.dev0'
-PROTOBUF_VERSION = '3.23.1'
+PROTOBUF_VERSION = '3.23.3'
