c7ab540dc4c2e5d5b5680a28a157ecb2d76408ff
Chris Townsend <christopher.townsend@canonical.com>
2023-07-07 10:30:13 -0400

    Revert "Correct the domain-socket client address read out from the ServerContext (#31108)"
    This reverts commit 447bd664c0fa691f859501b7ee71da48de8b64c7.

--- src/core/lib/iomgr/tcp_server_posix.cc
+++ src/core/lib/iomgr/tcp_server_posix.cc
@@ -437,7 +437,7 @@ static void on_read(void* arg, grpc_error_handle err) {
     if (grpc_is_unix_socket(&addr)) {
       memset(&addr, 0, sizeof(addr));
       addr.len = static_cast<socklen_t>(sizeof(struct sockaddr_storage));
-      if (getpeername(fd, reinterpret_cast<struct sockaddr*>(addr.addr),
+      if (getsockname(fd, reinterpret_cast<struct sockaddr*>(addr.addr),
                       &(addr.len)) < 0) {
         auto listener_addr_uri = grpc_sockaddr_to_uri(&sp->addr);
         gpr_log(
