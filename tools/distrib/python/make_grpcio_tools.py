b5c2889328ab42b617c16a6df860e519035cb7b7
AJ Heller <hork@google.com>
2023-07-13 16:36:11 -0700

    argparse fix

--- tools/distrib/python/make_grpcio_tools.py
+++ tools/distrib/python/make_grpcio_tools.py
@@ -241,7 +241,7 @@ def _delete_source_tree(target):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--keep-third-party", action=argparse.BooleanOptionalAction, default=False, help="Do not delete the temporary third_party folder"
+        "--keep-third-party", default=False, action="store_true", help="Do not delete the temporary third_party folder"
     )
     args = parser.parse_args()
     os.chdir(GRPC_ROOT)
