5fc6bc478fe2e4173a9d3496247b7bfd20fca1f6
Mark D. Roth <roth@google.com>
2023-07-14 19:49:56 +0000

    Revert "[outlier detection] remove support for ejection via raw connectivity state (#33427)"
    This reverts commit 51e54ed6363eaf5f080941fe2f6dd86cdd469cd3.

--- test/cpp/end2end/client_lb_end2end_test.cc
+++ test/cpp/end2end/client_lb_end2end_test.cc
@@ -2894,8 +2894,10 @@ TEST_F(ClientLbAddressTest, Basic) {
   // Make sure that the attributes wind up on the subchannels.
   std::vector<std::string> expected;
   for (const int port : GetServersPorts()) {
-    expected.emplace_back(absl::StrCat(ipv6_only_ ? "[::1]:" : "127.0.0.1:",
-                                       port, " args={test_key=test_value}"));
+    expected.emplace_back(absl::StrCat(
+        ipv6_only_ ? "[::1]:" : "127.0.0.1:", port,
+        " args={grpc.internal.no_subchannel.outlier_detection_disable=1, "
+        "test_key=test_value}"));
   }
   EXPECT_EQ(addresses_seen(), expected);
 }
