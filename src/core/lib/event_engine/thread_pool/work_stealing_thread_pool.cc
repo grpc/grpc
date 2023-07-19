7259ed167e84554a180ee8f7b9d3fc0040b0f822
drfloob <drfloob@users.noreply.github.com>
2023-07-19 00:53:11 +0000

    Automated change: Fix sanity tests

--- src/core/lib/event_engine/thread_pool/work_stealing_thread_pool.cc
+++ src/core/lib/event_engine/thread_pool/work_stealing_thread_pool.cc
@@ -23,7 +23,6 @@
 #include <inttypes.h>
 
 #include <atomic>
-#include <cmath>
 #include <memory>
 #include <utility>
 
@@ -39,7 +38,6 @@
 #include "src/core/lib/event_engine/trace.h"
 #include "src/core/lib/event_engine/work_queue/basic_work_queue.h"
 #include "src/core/lib/event_engine/work_queue/work_queue.h"
-#include "src/core/lib/gpr/time_precise.h"
 #include "src/core/lib/gprpp/thd.h"
 #include "src/core/lib/gprpp/time.h"
 
