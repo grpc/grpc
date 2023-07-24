bab20184e59c58b76b03418008224a7e5fc6ae85
AJ Heller <hork@google.com>
2023-07-24 16:14:54 +0000

    regenerate projects

--- src/objective-c/tests/version.h
+++ src/objective-c/tests/version.h
@@ -22,5 +22,5 @@
 // instead. This file can be regenerated from the template by running
 // `tools/buildgen/generate_projects.sh`.
 
-#define GRPC_OBJC_VERSION_STRING @"1.57.0-dev"
+#define GRPC_OBJC_VERSION_STRING @"1.58.0-dev"
 #define GRPC_C_VERSION_STRING @"34.0.0"
