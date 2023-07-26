92702354fbee08c4e605ba3fe96569c1b267535b
Nana Pang <ladynana@google.com>
2023-07-26 00:02:53 +0000

    Initial design of client transport write path

---
+++ src/core/ext/transport/chaotic_good/client_transport.cc
@@ -0,0 +1,105 @@
+// Copyright 2022 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <grpc/support/port_platform.h>
+
+#include "src/core/ext/transport/chaotic_good/client_transport.h"
+
+#include <memory>
+#include <string>
+#include <tuple>
+
+#include "absl/status/statusor.h"
+
+#include <grpc/event_engine/event_engine.h>
+#include <grpc/slice.h>
+
+#include "src/core/ext/transport/chaotic_good/frame.h"
+#include "src/core/ext/transport/chaotic_good/frame_header.h"
+#include "src/core/ext/transport/chttp2/transport/hpack_encoder.h"
+#include "src/core/lib/promise/activity.h"
+#include "src/core/lib/promise/detail/basic_join.h"
+#include "src/core/lib/promise/detail/basic_seq.h"
+#include "src/core/lib/promise/event_engine_wakeup_scheduler.h"
+#include "src/core/lib/promise/join.h"
+#include "src/core/lib/promise/loop.h"
+#include "src/core/lib/slice/slice.h"
+#include "src/core/lib/slice/slice_buffer.h"
+#include "src/core/lib/slice/slice_internal.h"
+#include "src/core/lib/transport/promise_endpoint.h"
+
+namespace grpc_core {
+namespace chaotic_good {
+
+ClientTransport::ClientTransport(const ChannelArgs& channel_args,
+                                 PromiseEndpoint&& control_endpoint,
+                                 PromiseEndpoint&& data_endpoint) {
+  auto hpack_compressor = std::make_shared<HPackCompressor>();
+  writer_ = MakeActivity(
+      Loop(Seq(
+          outgoing_frames_.Next(),
+          [hpack_compressor, &control_endpoint,
+           &data_endpoint](FrameInterface* frame) {
+            auto control_endpoint_buffer =
+                frame->Serialize(hpack_compressor.get());
+            FrameHeader frame_header =
+                FrameHeader::Parse(
+                    reinterpret_cast<const uint8_t*>(grpc_slice_to_c_string(
+                        control_endpoint_buffer.c_slice_buffer()->slices[0])))
+                    .value();
+            SliceBuffer data_endpoint_buffer;
+            // Handle data endpoint buffer based on the frame type.
+            switch (frame_header.type) {
+              case FrameType::kSettings:
+                // No data will be sent on data endpoint.
+                break;
+              case FrameType::kFragment: {
+                // TODO(ladynana): Calculate message_padding based on accumulate
+                // bytes sent on data endpoint.
+                std::string message_padding(frame_header.message_padding, '0');
+                Slice slice(grpc_slice_from_cpp_string(message_padding));
+                // Append message payload to data_endpoint_buffer.
+                data_endpoint_buffer.Append(std::move(slice));
+                auto message = std::move(
+                    static_cast<ClientFragmentFrame*>(frame)->message);
+                // Append message payload to data_endpoint_buffer.
+                message->payload()->MoveFirstNBytesIntoSliceBuffer(
+                    message->payload()->Length(), data_endpoint_buffer);
+                break;
+              }
+              case FrameType::kCancel:
+                // No data will be sent on data endpoint.
+                break;
+            }
+            return Seq(
+                Join(control_endpoint.Write(std::move(control_endpoint_buffer)),
+                     data_endpoint.Write(std::move(data_endpoint_buffer))),
+                [](std::tuple<absl::StatusOr<SliceBuffer>,
+                              absl::StatusOr<SliceBuffer>>
+                       ret) -> LoopCtl<absl::Status> {
+                  if (!(std::get<0>(ret).status().ok() ||
+                        std::get<1>(ret).status().ok())) {
+                    // TODO(ladynana): better error handling when writes failed.
+                    return absl::InternalError("Endpoint Write failed.");
+                  }
+                  return Continue();
+                });
+          })),
+      EventEngineWakeupScheduler(
+          grpc_event_engine::experimental::CreateEventEngine()),
+      [](absl::Status status) { return status; });
+}
+
+}  // namespace chaotic_good
+}  // namespace grpc_core
---
+++ src/core/ext/transport/chaotic_good/client_transport.h
@@ -0,0 +1,88 @@
+// Copyright 2022 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_CLIENT_TRANSPORT_H
+#define GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_CLIENT_TRANSPORT_H
+
+#include <grpc/support/port_platform.h>
+
+#include <stdint.h>
+
+#include <initializer_list>
+#include <memory>
+#include <utility>
+
+#include "absl/base/thread_annotations.h"
+#include "absl/status/status.h"
+#include "absl/time/clock.h"
+#include "absl/time/time.h"
+
+#include "src/core/ext/transport/chaotic_good/frame.h"
+#include "src/core/lib/channel/channel_args.h"
+#include "src/core/lib/gprpp/sync.h"
+#include "src/core/lib/promise/activity.h"
+#include "src/core/lib/promise/for_each.h"
+#include "src/core/lib/promise/mpsc.h"
+#include "src/core/lib/promise/pipe.h"
+#include "src/core/lib/promise/seq.h"
+#include "src/core/lib/transport/promise_endpoint.h"
+#include "src/core/lib/transport/transport.h"
+
+namespace grpc_core {
+namespace chaotic_good {
+
+class ClientTransport {
+ public:
+  ClientTransport(const ChannelArgs& channel_args,
+                  PromiseEndpoint&& control_endpoint_,
+                  PromiseEndpoint&& data_endpoint_);
+  auto AddStream(CallArgs call_args) {
+    // At this point, the connection is set up.
+    // Start sending data frames.
+    auto initial_frame = std::make_shared<ClientFragmentFrame>();
+    initial_frame->headers = std::move(call_args.client_initial_metadata);
+    initial_frame->end_of_stream = false;
+    {
+      MutexLock lock(&mu_);
+      initial_frame->stream_id = next_stream_id_++;
+    }
+    return ForEach(std::move(*call_args.client_to_server_messages),
+                   [this, initial_frame](MessageHandle result) {
+                     initial_frame->message = std::move(result);
+                     MpscSender<FrameInterface*> outgoing_frames =
+                         this->outgoing_frames_.MakeSender();
+                     return Seq(outgoing_frames.Send(initial_frame.get()), [] {
+                       // TODO(ladynana): remove this sleep after figure out how
+                       // to synchronize writer_ with outside activity.
+                       absl::SleepFor(absl::Seconds(5));
+                       return absl::OkStatus();
+                     });
+                   });
+  }
+
+ private:
+  // Max buffer is set to 4, so that for stream writes each time it will queue
+  // at most 2 frames.
+  MpscReceiver<FrameInterface*> outgoing_frames_ =
+      MpscReceiver<FrameInterface*>(4);
+  Mutex mu_;
+  uint32_t next_stream_id_ ABSL_GUARDED_BY(mu_) = 1;
+  ActivityPtr writer_;
+  ActivityPtr reader_;
+};
+
+}  // namespace chaotic_good
+}  // namespace grpc_core
+
+#endif  // GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_CLIENT_TRANSPORT_H
