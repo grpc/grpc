86b76f3802d36afd4bb0dfc4889d424fe4727fde
Nana Pang <ladynana@google.com>
2023-07-26 00:02:53 +0000

    Initial design of client transport write path

---
+++ test/core/transport/chaotic_good/client_transport_test.cc
@@ -0,0 +1,160 @@
+// Copyright 2023 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/core/ext/transport/chaotic_good/client_transport.h"
+
+// IWYU pragma: no_include <sys/socket.h>
+
+#include <stdio.h>
+
+#include <memory>
+
+#include "absl/functional/any_invocable.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include <grpc/event_engine/event_engine.h>
+#include <grpc/event_engine/memory_allocator.h>
+#include <grpc/event_engine/slice_buffer.h>
+#include <grpc/grpc.h>
+
+#include "src/core/lib/gprpp/ref_counted_ptr.h"
+#include "src/core/lib/promise/activity.h"
+#include "src/core/lib/promise/detail/basic_join.h"
+#include "src/core/lib/promise/join.h"
+#include "src/core/lib/promise/seq.h"
+#include "src/core/lib/resource_quota/arena.h"
+#include "src/core/lib/resource_quota/memory_quota.h"
+#include "src/core/lib/resource_quota/resource_quota.h"
+#include "src/core/lib/slice/slice.h"
+#include "src/core/lib/slice/slice_buffer.h"
+#include "test/core/promise/test_wakeup_schedulers.h"
+
+using testing::MockFunction;
+using testing::Return;
+using testing::StrictMock;
+
+namespace grpc_core {
+namespace chaotic_good {
+namespace testing {
+
+class MockEndpoint
+    : public grpc_event_engine::experimental::EventEngine::Endpoint {
+ public:
+  MOCK_METHOD(
+      bool, Read,
+      (absl::AnyInvocable<void(absl::Status)> on_read,
+       grpc_event_engine::experimental::SliceBuffer* buffer,
+       const grpc_event_engine::experimental::EventEngine::Endpoint::ReadArgs*
+           args),
+      (override));
+
+  MOCK_METHOD(
+      bool, Write,
+      (absl::AnyInvocable<void(absl::Status)> on_writable,
+       grpc_event_engine::experimental::SliceBuffer* data,
+       const grpc_event_engine::experimental::EventEngine::Endpoint::WriteArgs*
+           args),
+      (override));
+
+  MOCK_METHOD(
+      const grpc_event_engine::experimental::EventEngine::ResolvedAddress&,
+      GetPeerAddress, (), (const, override));
+  MOCK_METHOD(
+      const grpc_event_engine::experimental::EventEngine::ResolvedAddress&,
+      GetLocalAddress, (), (const, override));
+};
+
+class ClientTransportTest : public ::testing::Test {
+ public:
+  ClientTransportTest()
+      : channel_args_(ChannelArgs()),
+        control_endpoint_ptr_(new StrictMock<MockEndpoint>()),
+        data_endpoint_ptr_(new StrictMock<MockEndpoint>()),
+        memory_allocator_(
+            ResourceQuota::Default()->memory_quota()->CreateMemoryAllocator(
+                "test")),
+        control_endpoint_(*control_endpoint_ptr_),
+        data_endpoint_(*data_endpoint_ptr_),
+        control_promise_endpoint_(new PromiseEndpoint(
+            std::unique_ptr<MockEndpoint>(control_endpoint_ptr_),
+            SliceBuffer())),
+        data_promise_endpoint_(new PromiseEndpoint(
+            std::unique_ptr<MockEndpoint>(data_endpoint_ptr_), SliceBuffer())),
+        client_transport_(channel_args_, std::move(*control_promise_endpoint_),
+                          std::move(*data_promise_endpoint_)),
+        arena_(MakeScopedArena(initial_arena_size, &memory_allocator_)),
+        pipe_client_to_server_messages_(arena_.get()) {}
+
+ private:
+  const ChannelArgs channel_args_;
+  MockEndpoint* control_endpoint_ptr_;
+  MockEndpoint* data_endpoint_ptr_;
+  size_t initial_arena_size = 1024;
+
+ protected:
+  MemoryAllocator memory_allocator_;
+  MockEndpoint& control_endpoint_;
+  MockEndpoint& data_endpoint_;
+  std::unique_ptr<PromiseEndpoint> control_promise_endpoint_;
+  std::unique_ptr<PromiseEndpoint> data_promise_endpoint_;
+  ClientTransport client_transport_;
+  ScopedArenaPtr arena_;
+  Pipe<MessageHandle> pipe_client_to_server_messages_;
+};
+
+TEST_F(ClientTransportTest, AddOneStream) {
+  SliceBuffer buffer;
+  buffer.Append(Slice::FromCopiedString("test add stream."));
+  auto message = arena_->MakePooled<Message>(std::move(buffer), 0);
+  ClientMetadataHandle md;
+  auto args = CallArgs{
+      std::move(md), ClientInitialMetadataOutstandingToken::Empty(), nullptr,
+      nullptr,       &pipe_client_to_server_messages_.receiver,      nullptr};
+  StrictMock<MockFunction<void(absl::Status)>> on_done;
+  EXPECT_CALL(on_done, Call(absl::OkStatus()));
+  EXPECT_CALL(control_endpoint_, Write).WillOnce(Return(true));
+  EXPECT_CALL(data_endpoint_, Write).WillOnce(Return(true));
+  auto activity = MakeActivity(
+      Seq(  // Concurrently: send message into the pipe, and receive from the
+            // pipe.
+          Join(Seq(pipe_client_to_server_messages_.sender.Push(
+                       std::move(message)),
+                   [this] {
+                     this->pipe_client_to_server_messages_.sender.Close();
+                     return absl::OkStatus();
+                   }),
+               Seq(client_transport_.AddStream(std::move(args)),
+                   [] { return absl::OkStatus(); })),
+          // Once complete, verify successful sending and the received value.
+          []() {
+            // TODO: verify results.
+            return absl::OkStatus();
+          }),
+      InlineWakeupScheduler(),
+      [&on_done](absl::Status status) { on_done.Call(std::move(status)); });
+}
+
+}  // namespace testing
+}  // namespace chaotic_good
+}  // namespace grpc_core
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  // Must call to create default EventEngine.
+  grpc_init();
+  int ret = RUN_ALL_TESTS();
+  grpc_shutdown();
+  return ret;
+}
