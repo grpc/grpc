9aca06d38a4c914a48d2ae697af5b4b7748472ab
Alisha Nanda <alishananda@google.com>
2023-07-31 17:48:41 -0700

    Revert "[c-ares DNS resolver] Fix file descriptor use-after-close bug when c-ares writes succeed but subsequent read fails" (#33934)
    Reverts grpc/grpc#33871 due to build failures in google3.
    
    Co-authored-by: Yijie Ma <yijiem@google.com>

--- src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_wrapper.h
+++ src/core/ext/filters/client_channel/resolver/dns/c_ares/grpc_ares_wrapper.h
@@ -133,7 +133,4 @@ void grpc_cares_wrapper_address_sorting_sort(
 // Exposed in this header for C-core tests only
 extern void (*grpc_ares_test_only_inject_config)(ares_channel* channel);
 
-// Exposed in this header for C-core tests only
-extern bool g_grpc_ares_test_only_force_tcp;
-
 #endif  // GRPC_SRC_CORE_EXT_FILTERS_CLIENT_CHANNEL_RESOLVER_DNS_C_ARES_GRPC_ARES_WRAPPER_H
