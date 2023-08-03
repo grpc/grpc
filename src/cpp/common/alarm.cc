39facd848ca7411a245ae66aad97a93dcf3d053d
AJ Heller <hork@google.com>
2023-08-03 13:59:00 -0700

    use atomics instead of optionals and mutexes

--- src/cpp/common/alarm.cc
+++ src/cpp/common/alarm.cc
@@ -17,13 +17,12 @@
 
 #include <grpc/support/port_platform.h>
 
+#include <atomic>
 #include <functional>
 #include <memory>
 #include <utility>
 
-#include "absl/base/thread_annotations.h"
 #include "absl/status/status.h"
-#include "absl/types/optional.h"
 
 #include <grpc/event_engine/event_engine.h>
 #include <grpc/grpc.h>
@@ -35,7 +34,6 @@
 #include <grpcpp/impl/completion_queue_tag.h>
 
 #include "src/core/lib/event_engine/default_event_engine.h"
-#include "src/core/lib/gprpp/sync.h"
 #include "src/core/lib/gprpp/time.h"
 #include "src/core/lib/iomgr/error.h"
 #include "src/core/lib/iomgr/exec_ctx.h"
@@ -46,7 +44,7 @@ namespace grpc {
 namespace internal {
 
 namespace {
-using ::grpc_event_engine::experimental::EventEngine;
+using grpc_event_engine::experimental::EventEngine;
 }  // namespace
 
 class AlarmImpl : public grpc::internal::CompletionQueueTag {
@@ -64,45 +62,44 @@ class AlarmImpl : public grpc::internal::CompletionQueueTag {
     return true;
   }
   void Set(grpc::CompletionQueue* cq, gpr_timespec deadline, void* tag) {
-    grpc_core::MutexLock lock(&mu_);
-    GPR_ASSERT(!cq_timer_handle_.has_value() &&
-               !callback_timer_handle_.has_value());
     grpc_core::ExecCtx exec_ctx;
     GRPC_CQ_INTERNAL_REF(cq->cq(), "alarm");
     cq_ = cq->cq();
     tag_ = tag;
     GPR_ASSERT(grpc_cq_begin_op(cq_, this));
     Ref();
+    cq_armed_.store(true);
     cq_timer_handle_ = event_engine_->RunAfter(
         grpc_core::Timestamp::FromTimespecRoundUp(deadline) -
             grpc_core::ExecCtx::Get()->Now(),
-        [this] { OnCQAlarm(absl::OkStatus()); });
+        [this] {
+          cq_armed_.store(false);
+          OnCQAlarm(absl::OkStatus());
+        });
   }
   void Set(gpr_timespec deadline, std::function<void(bool)> f) {
-    grpc_core::MutexLock lock(&mu_);
-    GPR_ASSERT(!cq_timer_handle_.has_value() &&
-               !callback_timer_handle_.has_value());
     grpc_core::ExecCtx exec_ctx;
     // Don't use any CQ at all. Instead just use the timer to fire the function
     callback_ = std::move(f);
     Ref();
+    callback_armed_.store(true);
     callback_timer_handle_ = event_engine_->RunAfter(
         grpc_core::Timestamp::FromTimespecRoundUp(deadline) -
             grpc_core::ExecCtx::Get()->Now(),
-        [this] { OnCallbackAlarm(true); });
+        [this] {
+          callback_armed_.store(false);
+          OnCallbackAlarm(true);
+        });
   }
   void Cancel() {
     grpc_core::ExecCtx exec_ctx;
-    grpc_core::MutexLock lock(&mu_);
-    if (callback_timer_handle_.has_value() &&
-        event_engine_->Cancel(*callback_timer_handle_)) {
+    if (callback_armed_.load() &&
+        event_engine_->Cancel(callback_timer_handle_)) {
       event_engine_->Run([this] { OnCallbackAlarm(/*is_ok=*/false); });
-      callback_timer_handle_.reset();
-    } else if (cq_timer_handle_.has_value() &&
-               event_engine_->Cancel(*cq_timer_handle_)) {
+    }
+    if (cq_armed_.load() && event_engine_->Cancel(cq_timer_handle_)) {
       event_engine_->Run(
           [this] { OnCQAlarm(absl::CancelledError("cancelled")); });
-      cq_timer_handle_.reset();
     }
   }
   void Destroy() {
@@ -112,10 +109,6 @@ class AlarmImpl : public grpc::internal::CompletionQueueTag {
 
  private:
   void OnCQAlarm(grpc_error_handle error) {
-    {
-      grpc_core::MutexLock lock(&mu_);
-      cq_timer_handle_.reset();
-    }
     grpc_core::ApplicationCallbackExecCtx callback_exec_ctx;
     grpc_core::ExecCtx exec_ctx;
     // Preserve the cq and reset the cq_ so that the alarm
@@ -130,10 +123,6 @@ class AlarmImpl : public grpc::internal::CompletionQueueTag {
   }
 
   void OnCallbackAlarm(bool is_ok) {
-    {
-      grpc_core::MutexLock lock(&mu_);
-      callback_timer_handle_.reset();
-    }
     grpc_core::ApplicationCallbackExecCtx callback_exec_ctx;
     grpc_core::ExecCtx exec_ctx;
     callback_(is_ok);
@@ -147,11 +136,12 @@ class AlarmImpl : public grpc::internal::CompletionQueueTag {
     }
   }
 
-  grpc_core::Mutex mu_;
   std::shared_ptr<grpc_event_engine::experimental::EventEngine> event_engine_;
-  absl::optional<EventEngine::TaskHandle> cq_timer_handle_ ABSL_GUARDED_BY(mu_);
-  absl::optional<EventEngine::TaskHandle> callback_timer_handle_
-      ABSL_GUARDED_BY(mu_);
+  std::atomic<bool> cq_armed_{false};
+  EventEngine::TaskHandle cq_timer_handle_ = EventEngine::TaskHandle::kInvalid;
+  std::atomic<bool> callback_armed_{false};
+  EventEngine::TaskHandle callback_timer_handle_ =
+      EventEngine::TaskHandle::kInvalid;
   gpr_refcount refs_;
   grpc_cq_completion completion_;
   // completion queue where events about this alarm will be posted
