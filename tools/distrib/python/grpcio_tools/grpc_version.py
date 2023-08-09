9b9aeb64b20eef085f6b5f46ae1281924c78b14b
Eric Zhang <ekzhang1@gmail.com>
2023-08-09 14:17:20 +0200

    Update to correct major version of protobuf-python
    Ever since Protobuf version 21, the Python protobuf package has been on major version 4, not major version 3. So the current `PROTOBUF_VERSION` constant in `grpc_version.py` has been incorrect for the past 3 releases.

--- tools/distrib/python/grpcio_tools/grpc_version.py
+++ tools/distrib/python/grpcio_tools/grpc_version.py
@@ -15,4 +15,4 @@
 # AUTO-GENERATED FROM `$REPO_ROOT/templates/tools/distrib/python/grpcio_tools/grpc_version.py.template`!!!
 
 VERSION = '1.58.0.dev0'
-PROTOBUF_VERSION = '3.23.4'
+PROTOBUF_VERSION = '4.23.4'
