081e805a0afe1e523f0e7dc3429a5b5893153278
mohanli-ml <mohanli@google.com>
2023-08-15 22:50:44 +0000

    [pick_first] de-experiment pick first

--- src/core/ext/xds/xds_lb_policy_registry.cc
+++ src/core/ext/xds/xds_lb_policy_registry.cc
@@ -36,7 +36,6 @@
 
 #include <grpc/support/json.h>
 
-#include "src/core/ext/filters/client_channel/lb_policy/pick_first/pick_first.h"
 #include "src/core/ext/xds/xds_common_types.h"
 #include "src/core/lib/config/core_configuration.h"
 #include "src/core/lib/gprpp/time.h"
