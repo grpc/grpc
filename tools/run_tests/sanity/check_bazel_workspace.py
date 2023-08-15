be9d7fabd738c7c3b7c44277ec743b152ef5e276
Yash Tibrewal <yashkt@google.com>
2023-08-15 23:26:44 +0000

    Revert "Revert "[GSM Observability] Add bazel dependency on Google Cloud C++ OTel library" (#34069)"
    This reverts commit 462a2cae35523ad18fe28e147407a86a9eeae6b8.

--- tools/run_tests/sanity/check_bazel_workspace.py
+++ tools/run_tests/sanity/check_bazel_workspace.py
@@ -78,6 +78,7 @@ _GRPC_DEP_NAMES = [
     "com_google_googleapis",
     "com_google_libprotobuf_mutator",
     "com_github_cncf_udpa",
+    "google_cloud_cpp",
 ]
 
 _GRPC_BAZEL_ONLY_DEPS = [
@@ -104,6 +105,7 @@ _GRPC_BAZEL_ONLY_DEPS = [
     "com_envoyproxy_protoc_gen_validate",
     "com_google_googleapis",
     "com_google_libprotobuf_mutator",
+    "google_cloud_cpp",
 ]
 
 
