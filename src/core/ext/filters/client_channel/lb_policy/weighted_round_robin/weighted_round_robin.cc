8a746ad0ae929ce28af8303032f1efbdb27375ae
Mark D. Roth <roth@google.com>
2023-08-16 23:17:51 +0000

    fix WRR race condition

--- src/core/ext/filters/client_channel/lb_policy/weighted_round_robin/weighted_round_robin.cc
+++ src/core/ext/filters/client_channel/lb_policy/weighted_round_robin/weighted_round_robin.cc
@@ -603,7 +603,9 @@ void WeightedRoundRobin::Picker::BuildSchedulerAndStartTimerLocked() {
   // Start timer.
   WeakRefCountedPtr<Picker> self = WeakRef();
   timer_handle_ = wrr_->channel_control_helper()->GetEventEngine()->RunAfter(
-      config_->weight_update_period(), [self = std::move(self)]() mutable {
+      config_->weight_update_period(),
+      [self = std::move(self), work_serializer = wrr_->work_serializer()]()
+          mutable {
         ApplicationCallbackExecCtx callback_exec_ctx;
         ExecCtx exec_ctx;
         {
@@ -617,9 +619,7 @@ void WeightedRoundRobin::Picker::BuildSchedulerAndStartTimerLocked() {
           }
         }
         // Release the picker ref inside the WorkSerializer.
-        auto* self_ptr = self.get();
-        self_ptr->wrr_->work_serializer()->Run([self = std::move(self)]() {},
-                                               DEBUG_LOCATION);
+        work_serializer->Run([self = std::move(self)]() {}, DEBUG_LOCATION);
       });
 }
 
