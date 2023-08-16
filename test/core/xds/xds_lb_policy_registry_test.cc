1f9fded4c03d7b1f293edafeafc97fb9ef92bcf9
mohanli-ml <mohanli@google.com>
2023-08-16 17:32:23 +0000

    [pick_first] de-experiment pick first

--- test/core/xds/xds_lb_policy_registry_test.cc
+++ test/core/xds/xds_lb_policy_registry_test.cc
@@ -53,7 +53,6 @@
 #include "src/proto/grpc/testing/xds/v3/round_robin.pb.h"
 #include "src/proto/grpc/testing/xds/v3/typed_struct.pb.h"
 #include "src/proto/grpc/testing/xds/v3/wrr_locality.pb.h"
-#include "test/core/util/scoped_env_var.h"
 #include "test/core/util/test_config.h"
 
 namespace grpc_core {
