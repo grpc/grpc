bf46a82977da137634b2ad835a4fc1f4779ce154
Terry Wilson <tmwilson@google.com>
2023-08-16 09:06:26 -0700

    Update tools/interop_matrix/client_matrix.py
    Co-authored-by: Sergii Tkachenko <hi@sergii.org>

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -425,7 +425,6 @@ LANG_RELEASE_MATRIX = {
             ("v1.54.0", ReleaseInfo()),
             ("v1.55.1", ReleaseInfo()),
             ("v1.56.0", ReleaseInfo()),
-            ("v1.57.1", ReleaseInfo()),
             ("v1.57.2", ReleaseInfo()),
         ]
     ),
