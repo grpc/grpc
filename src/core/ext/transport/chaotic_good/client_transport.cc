09cb3d1396e85d7d161cd60feab123b326662881
Nana Pang <ladynana@google.com>
2023-08-17 20:59:50 +0000

    Simplify header parse casting.

--- src/core/ext/transport/chaotic_good/client_transport.cc
+++ src/core/ext/transport/chaotic_good/client_transport.cc
@@ -16,8 +16,6 @@
 
 #include "src/core/ext/transport/chaotic_good/client_transport.h"
 
-#include <stdlib.h>
-
 #include <memory>
 #include <string>
 #include <tuple>
@@ -67,14 +65,12 @@ ClientTransport::ClientTransport(
                 control_endpoint_write_buffer_.Append(
                     frame->Serialize(hpack_compressor_.get()));
                 if (frame->message != nullptr) {
-                  char* header_string = grpc_slice_to_c_string(
-                      control_endpoint_write_buffer_.c_slice_buffer()
-                          ->slices[0]);
                   auto frame_header =
                       FrameHeader::Parse(
-                          reinterpret_cast<const uint8_t*>(header_string))
+                          reinterpret_cast<const uint8_t*>(GRPC_SLICE_START_PTR(
+                              control_endpoint_write_buffer_.c_slice_buffer()
+                                  ->slices[0])))
                           .value();
-                  free(header_string);
                   std::string message_padding(frame_header.message_padding,
                                               '0');
                   Slice slice(grpc_slice_from_cpp_string(message_padding));
