440eef228884f8098b1d3d3fa72cfb7a06ab483e
Eugene Ostroukhov <eostroukhov@google.com>
2023-08-22 15:55:59 -0700

    [Import] Revert #34027 and #34129 (#34133)
    This reverts commit 16b67ae312df6b99b25307dbe4a18b0a461063b0.

--- test/cpp/naming/resolver_component_tests_runner_invoker.cc
+++ test/cpp/naming/resolver_component_tests_runner_invoker.cc
@@ -1,6 +1,4 @@
-//
-//
-// Copyright 2017 gRPC authors.
+// Copyright 2017 The gRPC Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,13 +11,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-//
+
+#include <grpc/support/port_platform.h>
 
 #include <signal.h>
 #include <string.h>
-#include <unistd.h>
 
+#ifndef GPR_WINDOWS
+#include <unistd.h>
+#endif  // GPR_WINDOWS
+
+#include <memory>
 #include <string>
 #include <thread>
 #include <vector>
@@ -43,6 +45,9 @@
 #include "test/core/util/test_config.h"
 #include "test/cpp/util/subprocess.h"
 #include "test/cpp/util/test_config.h"
+#ifdef GPR_WINDOWS
+#include "test/cpp/util/windows/manifest_file.h"
+#endif  // GPR_WINDOWS
 
 ABSL_FLAG(
     bool, running_under_bazel, false,
@@ -68,48 +73,22 @@ namespace grpc {
 
 namespace testing {
 
-void InvokeResolverComponentTestsRunner(
+int InvokeResolverComponentTestsRunner(
     std::string test_runner_bin_path, const std::string& test_bin_path,
     const std::string& dns_server_bin_path,
     const std::string& records_config_path,
     const std::string& dns_resolver_bin_path,
     const std::string& tcp_connect_bin_path) {
   int dns_server_port = grpc_pick_unused_port_or_die();
-
-  SubProcess* test_driver = new SubProcess(
+  auto test_driver = std::make_unique<SubProcess>(std::vector<std::string>(
       {std::move(test_runner_bin_path), "--test_bin_path=" + test_bin_path,
        "--dns_server_bin_path=" + dns_server_bin_path,
        "--records_config_path=" + records_config_path,
        "--dns_server_port=" + std::to_string(dns_server_port),
        "--dns_resolver_bin_path=" + dns_resolver_bin_path,
        "--tcp_connect_bin_path=" + tcp_connect_bin_path,
-       "--extra_args=" + absl::GetFlag(FLAGS_extra_args)});
-  gpr_mu test_driver_mu;
-  gpr_mu_init(&test_driver_mu);
-  gpr_cv test_driver_cv;
-  gpr_cv_init(&test_driver_cv);
-  int status = test_driver->Join();
-  if (WIFEXITED(status)) {
-    if (WEXITSTATUS(status)) {
-      grpc_core::Crash(absl::StrFormat(
-          "Resolver component test test-runner exited with code %d",
-          WEXITSTATUS(status)));
-    }
-  } else if (WIFSIGNALED(status)) {
-    grpc_core::Crash(absl::StrFormat(
-        "Resolver component test test-runner ended from signal %d",
-        WTERMSIG(status)));
-  } else {
-    grpc_core::Crash(absl::StrFormat(
-        "Resolver component test test-runner ended with unknown status %d",
-        status));
-  }
-  gpr_mu_lock(&test_driver_mu);
-  gpr_cv_signal(&test_driver_cv);
-  gpr_mu_unlock(&test_driver_mu);
-  delete test_driver;
-  gpr_mu_destroy(&test_driver_mu);
-  gpr_cv_destroy(&test_driver_cv);
+       "--extra_args=" + absl::GetFlag(FLAGS_extra_args)}));
+  return test_driver->Join();
 }
 
 }  // namespace testing
@@ -122,12 +101,14 @@ int main(int argc, char** argv) {
   grpc_init();
   GPR_ASSERT(!absl::GetFlag(FLAGS_test_bin_name).empty());
   std::string my_bin = argv[0];
+  int result = 0;
   if (absl::GetFlag(FLAGS_running_under_bazel)) {
     GPR_ASSERT(!absl::GetFlag(FLAGS_grpc_test_directory_relative_to_test_srcdir)
                     .empty());
     // Use bazel's TEST_SRCDIR environment variable to locate the "test data"
     // binaries.
     auto test_srcdir = grpc_core::GetEnv("TEST_SRCDIR");
+#ifndef GPR_WINDOWS
     std::string const bin_dir =
         test_srcdir.value() +
         absl::GetFlag(FLAGS_grpc_test_directory_relative_to_test_srcdir) +
@@ -135,18 +116,43 @@ int main(int argc, char** argv) {
     // Invoke bazel's executeable links to the .sh and .py scripts (don't use
     // the .sh and .py suffixes) to make
     // sure that we're using bazel's test environment.
-    grpc::testing::InvokeResolverComponentTestsRunner(
+    result = grpc::testing::InvokeResolverComponentTestsRunner(
         bin_dir + "/resolver_component_tests_runner",
         bin_dir + "/" + absl::GetFlag(FLAGS_test_bin_name),
         bin_dir + "/utils/dns_server",
         bin_dir + "/resolver_test_record_groups.yaml",
         bin_dir + "/utils/dns_resolver", bin_dir + "/utils/tcp_connect");
+#else
+    grpc::testing::ManifestFile manifest_file(
+        grpc::testing::NormalizeFilePath(test_srcdir.value()) + "\\MANIFEST");
+    grpc::testing::InvokeResolverComponentTestsRunner(
+        grpc::testing::NormalizeFilePath(
+            manifest_file.Get("com_github_grpc_grpc/test/cpp/naming/"
+                              "resolver_component_tests_runner.exe")),
+        grpc::testing::NormalizeFilePath(
+            manifest_file.Get("com_github_grpc_grpc/test/cpp/naming/" +
+                              absl::GetFlag(FLAGS_test_bin_name) + ".exe")),
+        grpc::testing::NormalizeFilePath(manifest_file.Get(
+            "com_github_grpc_grpc/test/cpp/naming/utils/dns_server.exe")),
+        grpc::testing::NormalizeFilePath(
+            manifest_file.Get("com_github_grpc_grpc/test/cpp/naming/"
+                              "resolver_test_record_groups.yaml")),
+        grpc::testing::NormalizeFilePath(manifest_file.Get(
+            "com_github_grpc_grpc/test/cpp/naming/utils/dns_resolver.exe")),
+        grpc::testing::NormalizeFilePath(manifest_file.Get(
+            "com_github_grpc_grpc/test/cpp/naming/utils/tcp_connect.exe")));
+#endif  // GPR_WINDOWS
   } else {
+#ifdef GPR_WINDOWS
+    grpc_core::Crash(
+        "Resolver component tests runner invoker does not support running "
+        "without Bazel on Windows for now.");
+#endif  // GPR_WINDOWS
     // Get the current binary's directory relative to repo root to invoke the
     // correct build config (asan/tsan/dbg, etc.).
     std::string const bin_dir = my_bin.substr(0, my_bin.rfind('/'));
     // Invoke the .sh and .py scripts directly where they are in source code.
-    grpc::testing::InvokeResolverComponentTestsRunner(
+    result = grpc::testing::InvokeResolverComponentTestsRunner(
         "test/cpp/naming/resolver_component_tests_runner.py",
         bin_dir + "/" + absl::GetFlag(FLAGS_test_bin_name),
         "test/cpp/naming/utils/dns_server.py",
@@ -155,5 +161,5 @@ int main(int argc, char** argv) {
         "test/cpp/naming/utils/tcp_connect.py");
   }
   grpc_shutdown();
-  return 0;
+  return result;
 }
