dbd64c4fd95812d1090e4c89f3580545788116b0
Mike Kruskal <mkruskal@google.com>
2023-08-25 16:34:41 -0700

    fix clang formatting

--- src/compiler/config.h
+++ src/compiler/config.h
@@ -40,8 +40,7 @@ namespace protobuf {
 namespace compiler {
 typedef GRPC_CUSTOM_CODEGENERATOR CodeGenerator;
 typedef GRPC_CUSTOM_GENERATORCONTEXT GeneratorContext;
-static inline int PluginMain(int argc, char* argv[],
-                             CodeGenerator* generator) {
+static inline int PluginMain(int argc, char* argv[], CodeGenerator* generator) {
   return GRPC_CUSTOM_PLUGINMAIN(argc, argv, generator);
 }
 static inline void ParseGeneratorParameter(
