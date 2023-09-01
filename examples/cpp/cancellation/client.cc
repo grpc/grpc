23f84be931ed71b3ca52bcdb5952fa1411224fa7
Esun Kim <veblush@google.com>
2023-09-01 15:01:13 -0700

    Added gRPC C++ cancellation example

---
+++ examples/cpp/cancellation/client.cc
@@ -0,0 +1,122 @@
+/*
+ *
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <condition_variable>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <vector>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_cat.h"
+
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/keyvaluestore.grpc.pb.h"
+#else
+#include "keyvaluestore.grpc.pb.h"
+#endif
+
+ABSL_FLAG(std::string, target, "localhost:50051", "Server address");
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using grpc::StatusCode;
+using keyvaluestore::KeyValueStore;
+using keyvaluestore::Request;
+using keyvaluestore::Response;
+
+// Requests each key in the vector and displays the key and its corresponding
+// value as a pair.
+class KeyValueStoreClient : public grpc::ClientBidiReactor<Request, Response> {
+ public:
+  KeyValueStoreClient(std::shared_ptr<Channel> channel)
+      : stub_(KeyValueStore::NewStub(channel)) {
+    stub_->async()->GetValues(&context_, this);
+    request_.set_key("Begin");
+    StartWrite(&request_);
+    StartCall();
+  }
+
+  void OnReadDone(bool ok) override {
+    if (ok) {
+      std::cout << request_.key() << " : " << response_.value() << std::endl;
+      if (++counter_ < 10) {
+        request_.set_key(absl::StrCat("Count ", counter_));
+        StartWrite(&request_);
+      } else {
+        // Cancel after sending 10 messages
+        context_.TryCancel();
+      }
+    }
+  }
+
+  void OnWriteDone(bool ok) override {
+    if (ok) {
+      StartRead(&response_);
+    }
+  }
+
+  void OnDone(const grpc::Status& status) override {
+    if (!status.ok()) {
+      if (status.error_code() == StatusCode::CANCELLED) {
+        // Eventually client will know here that call is cancelled.
+        std::cout << "RPC Cancelled!" << std::endl;
+      } else {
+        std::cout << "RPC Failed: " << status.error_code() << ": "
+                  << status.error_message() << std::endl;
+      }
+    }
+    std::unique_lock<std::mutex> l(mu_);
+    done_ = true;
+    cv_.notify_all();
+  }
+
+  void Await() {
+    std::unique_lock<std::mutex> l(mu_);
+    while (!done_) {
+      cv_.wait(l);
+    }
+  }
+
+ private:
+  std::unique_ptr<KeyValueStore::Stub> stub_;
+  size_t counter_ = 0;
+  ClientContext context_;
+  bool done_ = false;
+  Request request_;
+  Response response_;
+  std::mutex mu_;
+  std::condition_variable cv_;
+};
+
+int main(int argc, char** argv) {
+  absl::ParseCommandLine(argc, argv);
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint specified by
+  // the argument "--target=" which is the only expected argument.
+  std::string target_str = absl::GetFlag(FLAGS_target);
+  KeyValueStoreClient client(
+      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+  client.Await();
+  return 0;
+}
---
+++ examples/cpp/cancellation/server.cc
@@ -0,0 +1,113 @@
+/*
+ *
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_format.h"
+
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/keyvaluestore.grpc.pb.h"
+#else
+#include "keyvaluestore.grpc.pb.h"
+#endif
+
+ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
+
+using grpc::CallbackServerContext;
+using grpc::Server;
+using grpc::ServerBidiReactor;
+using grpc::ServerBuilder;
+using grpc::Status;
+using grpc::experimental::InterceptionHookPoints;
+using grpc::experimental::Interceptor;
+using grpc::experimental::InterceptorBatchMethods;
+using grpc::experimental::ServerInterceptorFactoryInterface;
+using grpc::experimental::ServerRpcInfo;
+using keyvaluestore::KeyValueStore;
+using keyvaluestore::Request;
+using keyvaluestore::Response;
+
+// Logic behind the server's behavior.
+class KeyValueStoreServiceImpl final : public KeyValueStore::CallbackService {
+  ServerBidiReactor<Request, Response>* GetValues(
+      CallbackServerContext* context) override {
+    class Reactor : public ServerBidiReactor<Request, Response> {
+     public:
+      explicit Reactor() { StartRead(&request_); }
+
+      void OnReadDone(bool ok) override {
+        if (!ok) {
+          // Client cancelled it
+          std::cout << "OnReadDone Cancelled!" << std::endl;
+          return Finish(grpc::Status::CANCELLED);
+        }
+        response_.set_value(absl::StrCat(request_.key(), " Ack"));
+        StartWrite(&response_);
+      }
+
+      void OnWriteDone(bool ok) override {
+        if (!ok) {
+          // Client cancelled it
+          std::cout << "OnWriteDone Cancelled!" << std::endl;
+          return Finish(grpc::Status::CANCELLED);
+        }
+        StartRead(&request_);
+      }
+
+      void OnDone() override { delete this; }
+
+     private:
+      Request request_;
+      Response response_;
+    };
+
+    return new Reactor();
+  }
+};
+
+void RunServer(uint16_t port) {
+  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
+  KeyValueStoreServiceImpl service;
+
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  absl::ParseCommandLine(argc, argv);
+  RunServer(absl::GetFlag(FLAGS_port));
+  return 0;
+}
