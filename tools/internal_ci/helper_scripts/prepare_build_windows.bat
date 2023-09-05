3efa3aafe80cbf139f4c6ada04d03650b5e252ab
Xuan Wang <xuanwn@google.com>
2023-09-05 18:32:59 +0000

    Try upgrade setuptools

--- tools/internal_ci/helper_scripts/prepare_build_windows.bat
+++ tools/internal_ci/helper_scripts/prepare_build_windows.bat
@@ -92,7 +92,7 @@ If "%PREPARE_BUILD_INSTALL_DEPS_PYTHON%" == "true" (
 )
 
 @rem Needed for uploading test results to bigquery
-python -m pip install google-api-python-client oauth2client six==1.16.0 || goto :error
+python -m pip install google-api-python-client oauth2client six==1.16.0 setuptools==59.6.0 || goto :error
 
 git submodule update --init || goto :error
 
