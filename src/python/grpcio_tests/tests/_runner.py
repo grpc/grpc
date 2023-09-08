bc32ebe7669a5ac5079b1174d8e596b6cf6b3140
Xuan Wang <xuanwn@google.com>
2023-09-08 20:28:03 +0000

    Fix run test case matcher

--- src/python/grpcio_tests/tests/_runner.py
+++ src/python/grpcio_tests/tests/_runner.py
@@ -135,8 +135,8 @@ class Runner(object):
         filtered_cases = []
         for case in _loader.iterate_suite_cases(suite):
             import sys; sys.stderr.write(f"checking_case_id_starts_with: {case.id()} startswith {testcase_filter}\n")
-            if not testcase_filter or case.id().startswith(testcase_filter):
-            # if not testcase_filter or testcase_filter in case.id():
+            # if not testcase_filter or case.id().startswith(testcase_filter):
+            if not testcase_filter or testcase_filter in case.id():
                 filtered_cases.append(case)
 
         # Ensure that every test case has no collision with any other test case in
