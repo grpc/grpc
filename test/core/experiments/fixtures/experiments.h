ed6634c4d80285f814be8c28e9ce563f844b95d0
Yijie Ma <yijiem@google.com>
2023-09-12 16:16:46 -0700

    [EventEngine] Backport "[EventEngine] Make AresResolver build compatible with older c-ares versions" to v1.58.x (#34324)
    Backport https://github.com/grpc/grpc/pull/34314 to v1.58.x.
    
    <!--
    
    If you know who should review your pull request, please assign it to
    that
    person, otherwise the pull request would get assigned randomly.
    
    If your pull request is for a specific language, please add the
    appropriate
    lang label.
    
    -->

--- test/core/experiments/fixtures/experiments.h
+++ test/core/experiments/fixtures/experiments.h
@@ -51,6 +51,7 @@
 #include <grpc/support/port_platform.h>
 
 #include <stddef.h>
+
 #include "src/core/lib/experiments/config.h"
 
 namespace grpc_core {
@@ -64,13 +65,13 @@ inline bool IsTestExperiment2Enabled() { return true; }
 #ifndef NDEBUG
 #define GRPC_EXPERIMENT_IS_INCLUDED_TEST_EXPERIMENT_3
 #endif
-inline bool IsTestExperiment3Enabled() { 
+inline bool IsTestExperiment3Enabled() {
 #ifdef NDEBUG
-return false;
+  return false;
 #else
-return true;
+  return true;
 #endif
- }
+}
 #define GRPC_EXPERIMENT_IS_INCLUDED_TEST_EXPERIMENT_4
 inline bool IsTestExperiment4Enabled() { return true; }
 
@@ -80,13 +81,13 @@ inline bool IsTestExperiment2Enabled() { return false; }
 #ifndef NDEBUG
 #define GRPC_EXPERIMENT_IS_INCLUDED_TEST_EXPERIMENT_3
 #endif
-inline bool IsTestExperiment3Enabled() { 
+inline bool IsTestExperiment3Enabled() {
 #ifdef NDEBUG
-return false;
+  return false;
 #else
-return true;
+  return true;
 #endif
- }
+}
 #define GRPC_EXPERIMENT_IS_INCLUDED_TEST_EXPERIMENT_4
 inline bool IsTestExperiment4Enabled() { return true; }
 
@@ -94,33 +95,33 @@ inline bool IsTestExperiment4Enabled() { return true; }
 #ifndef NDEBUG
 #define GRPC_EXPERIMENT_IS_INCLUDED_TEST_EXPERIMENT_1
 #endif
-inline bool IsTestExperiment1Enabled() { 
+inline bool IsTestExperiment1Enabled() {
 #ifdef NDEBUG
-return false;
+  return false;
 #else
-return true;
+  return true;
 #endif
- }
+}
 #ifndef NDEBUG
 #define GRPC_EXPERIMENT_IS_INCLUDED_TEST_EXPERIMENT_2
 #endif
-inline bool IsTestExperiment2Enabled() { 
+inline bool IsTestExperiment2Enabled() {
 #ifdef NDEBUG
-return false;
+  return false;
 #else
-return true;
+  return true;
 #endif
- }
+}
 #ifndef NDEBUG
 #define GRPC_EXPERIMENT_IS_INCLUDED_TEST_EXPERIMENT_3
 #endif
-inline bool IsTestExperiment3Enabled() { 
+inline bool IsTestExperiment3Enabled() {
 #ifdef NDEBUG
-return false;
+  return false;
 #else
-return true;
+  return true;
 #endif
- }
+}
 inline bool IsTestExperiment4Enabled() { return false; }
 #endif
 
