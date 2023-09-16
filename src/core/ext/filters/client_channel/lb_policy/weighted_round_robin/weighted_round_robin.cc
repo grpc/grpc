ccb451a87f9c80659118b83fca61f405324fb697
Mark D. Roth <roth@google.com>
2023-09-16 00:30:30 +0000

    handle SubchannelWrapper WorkSerializer experiment

--- src/core/ext/filters/client_channel/lb_policy/weighted_round_robin/weighted_round_robin.cc
+++ src/core/ext/filters/client_channel/lb_policy/weighted_round_robin/weighted_round_robin.cc
@@ -1463,8 +1463,12 @@ void WeightedRoundRobin::Picker::BuildSchedulerAndStartTimerLocked() {
             self->BuildSchedulerAndStartTimerLocked();
           }
         }
-        // Release the picker ref inside the WorkSerializer.
-        work_serializer->Run([self = std::move(self)]() {}, DEBUG_LOCATION);
+        if (!IsClientChannelSubchannelWrapperWorkSerializerOrphanEnabled()) {
+          // Release the picker ref inside the WorkSerializer.
+          work_serializer->Run([self = std::move(self)]() {}, DEBUG_LOCATION);
+          return;
+        }
+        self.reset();
       });
 }
 
