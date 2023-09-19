69b7e88ef65eadad20ed5cf36290d712ec053cc7
Richard Belleville <rbellevi@google.com>
2023-09-19 11:08:12 -0700

    Collect metadata in appnet ssa tests

--- tools/run_tests/xds_k8s_test_driver/tests/app_net_ssa_test.py
+++ tools/run_tests/xds_k8s_test_driver/tests/app_net_ssa_test.py
@@ -12,13 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-from typing import List
+from typing import List, Optional
 
 from absl import flags
 from absl.testing import absltest
 
 from framework import xds_k8s_testcase
 from framework import xds_url_map_testcase
+from framework.rpc import grpc_testing
+from framework.test_app import client_app
 from framework.test_cases import session_affinity_util
 
 logger = logging.getLogger(__name__)
@@ -35,6 +37,20 @@ _REPLICA_COUNT = 3
 
 
 class AppNetSsaTest(xds_k8s_testcase.AppNetXdsKubernetesTestCase):
+    def getClientRpcStats(
+        self,
+        test_client: _XdsTestClient,
+        num_rpcs: int,
+        *,
+        metadata_keys: Optional[tuple[str, ...]] = None,
+    ) -> grpc_testing.LoadBalancerStatsResponse:
+        """Load all metadata_keys by default."""
+        return super().getClientRpcStats(
+            test_client,
+            num_rpcs,
+            metadata_keys=metadata_keys or client_app.REQ_LB_STATS_METADATA_ALL,
+        )
+
     def test_session_affinity_policy(self):
         test_servers: List[_XdsTestServer]
 
