6d967e6b72c2cdc49e0c1ff4fd4d1bc24799c744
Eugene Ostroukhov <eostroukhov@gmail.com>
2023-09-20 09:41:58 -0700

    Revert "[test interop] Add HookService to the maintenence server (#34413)"
    This reverts commit 490f6a3ee9a9c08f7f700842a285534fbb703df6.

--- test/cpp/interop/pre_stop_hook_server_test.cc
+++ test/cpp/interop/pre_stop_hook_server_test.cc
@@ -16,7 +16,8 @@
 
 #include "test/cpp/interop/pre_stop_hook_server.h"
 
-#include <thread>
+#include <map>
+#include <memory>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -25,11 +26,7 @@
 
 #include <grpc/grpc.h>
 #include <grpcpp/grpcpp.h>
-#include <grpcpp/support/status.h>
 
-#include "src/core/lib/gprpp/sync.h"
-#include "src/proto/grpc/testing/empty.pb.h"
-#include "src/proto/grpc/testing/messages.pb.h"
 #include "src/proto/grpc/testing/test.grpc.pb.h"
 #include "test/core/util/port.h"
 #include "test/core/util/test_config.h"
@@ -47,7 +44,9 @@ struct CallInfo {
   absl::optional<Status> WaitForStatus(
       absl::Duration timeout = absl::Seconds(1)) {
     grpc_core::MutexLock lock(&mu);
-    cv.WaitWithTimeout(&mu, timeout);
+    if (!status_.has_value()) {
+      cv.WaitWithTimeout(&mu, timeout);
+    }
     return status_;
   }
 
@@ -63,22 +62,7 @@ struct CallInfo {
   absl::optional<Status> status_;
 };
 
-void ServerLoop(HookServiceImpl* service, int port, Server** server,
-                grpc_core::Mutex* mu, grpc_core::CondVar* condition) {
-  ServerBuilder builder;
-  builder.AddListeningPort(absl::StrFormat("0.0.0.0:%d", port),
-                           grpc::InsecureServerCredentials());
-  builder.RegisterService(service);
-  auto s = builder.BuildAndStart();
-  {
-    grpc_core::MutexLock lock(mu);
-    *server = s.get();
-    condition->SignalAll();
-  }
-  s->Wait();
-}
-
-TEST(StandalonePreStopHookServer, StartDoRequestStop) {
+TEST(PreStopHookServer, StartDoRequestStop) {
   int port = grpc_pick_unused_port_or_die();
   PreStopHookServerManager server;
   Status start_status = server.Start(port, 15);
@@ -89,8 +73,8 @@ TEST(StandalonePreStopHookServer, StartDoRequestStop) {
   CallInfo info;
   HookService::Stub stub(std::move(channel));
   stub.async()->Hook(&info.context, &info.request, &info.response,
-                     [&info](const Status& status) { info.SetStatus(status); });
-  ASSERT_TRUE(server.TestOnlyExpectRequests(1));
+                     [&info](Status status) { info.SetStatus(status); });
+  ASSERT_EQ(server.TestOnlyExpectRequests(1), 1);
   server.Return(StatusCode::INTERNAL, "Just a test");
   auto status = info.WaitForStatus();
   ASSERT_TRUE(status.has_value());
@@ -98,7 +82,7 @@ TEST(StandalonePreStopHookServer, StartDoRequestStop) {
   EXPECT_EQ(status->error_message(), "Just a test");
 }
 
-TEST(StandalonePreStopHookServer, StartServerWhileAlreadyRunning) {
+TEST(PreStopHookServer, StartServerWhileAlreadyRunning) {
   int port = grpc_pick_unused_port_or_die();
   PreStopHookServerManager server;
   Status status = server.Start(port, 15);
@@ -108,7 +92,7 @@ TEST(StandalonePreStopHookServer, StartServerWhileAlreadyRunning) {
       << status.error_message();
 }
 
-TEST(StandalonePreStopHookServer, StopServerWhileRequestPending) {
+TEST(PreStopHookServer, StopServerWhileRequestPending) {
   int port = grpc_pick_unused_port_or_die();
   PreStopHookServerManager server;
   Status start_status = server.Start(port, 15);
@@ -119,15 +103,15 @@ TEST(StandalonePreStopHookServer, StopServerWhileRequestPending) {
   CallInfo info;
   HookService::Stub stub(std::move(channel));
   stub.async()->Hook(&info.context, &info.request, &info.response,
-                     [&info](const Status& status) { info.SetStatus(status); });
-  ASSERT_TRUE(server.TestOnlyExpectRequests(1));
+                     [&info](Status status) { info.SetStatus(status); });
+  ASSERT_EQ(server.TestOnlyExpectRequests(1), 1);
   ASSERT_TRUE(server.Stop().ok());
   auto status = info.WaitForStatus();
   ASSERT_TRUE(status.has_value());
   EXPECT_EQ(status->error_code(), StatusCode::ABORTED);
 }
 
-TEST(StandalonePreStopHookServer, MultipleRequests) {
+TEST(PreStopHookServer, MultipleRequests) {
   int port = grpc_pick_unused_port_or_die();
   PreStopHookServerManager server;
   Status start_status = server.Start(port, 15);
@@ -139,16 +123,16 @@ TEST(StandalonePreStopHookServer, MultipleRequests) {
   CallInfo info1, info2, info3;
   server.Return(StatusCode::INTERNAL, "First");
   stub.async()->Hook(&info1.context, &info1.request, &info1.response,
-                     [&](const Status& status) { info1.SetStatus(status); });
+                     [&](Status status) { info1.SetStatus(status); });
   auto status = info1.WaitForStatus();
   ASSERT_TRUE(status.has_value());
   EXPECT_EQ(status->error_code(), StatusCode::INTERNAL);
   EXPECT_EQ(status->error_message(), "First");
   stub.async()->Hook(&info2.context, &info2.request, &info2.response,
-                     [&](const Status& status) { info2.SetStatus(status); });
-  ASSERT_TRUE(server.TestOnlyExpectRequests(1, absl::Milliseconds(500)));
+                     [&](Status status) { info2.SetStatus(status); });
+  ASSERT_EQ(server.TestOnlyExpectRequests(1, absl::Milliseconds(500)), 1);
   stub.async()->Hook(&info3.context, &info3.request, &info3.response,
-                     [&](const Status& status) { info3.SetStatus(status); });
+                     [&](Status status) { info3.SetStatus(status); });
   server.Return(StatusCode::RESOURCE_EXHAUSTED, "Second");
   server.Return(StatusCode::DEADLINE_EXCEEDED, "Third");
   status = info2.WaitForStatus();
@@ -161,14 +145,14 @@ TEST(StandalonePreStopHookServer, MultipleRequests) {
   EXPECT_EQ(status->error_message(), "Third");
 }
 
-TEST(StandalonePreStopHookServer, StopServerThatNotStarted) {
+TEST(PreStopHookServer, StopServerThatNotStarted) {
   PreStopHookServerManager server;
   Status status = server.Stop();
   EXPECT_EQ(status.error_code(), StatusCode::UNAVAILABLE)
       << status.error_message();
 }
 
-TEST(StandalonePreStopHookServer, SetStatusBeforeRequestReceived) {
+TEST(PreStopHookServer, SetStatusBeforeRequestReceived) {
   int port = grpc_pick_unused_port_or_die();
   PreStopHookServerManager server;
   Status start_status = server.Start(port, 15);
@@ -179,71 +163,12 @@ TEST(StandalonePreStopHookServer, SetStatusBeforeRequestReceived) {
   ASSERT_TRUE(channel);
   HookService::Stub stub(std::move(channel));
   CallInfo info;
-  auto status = stub.Hook(&info.context, info.request, &info.response);
-  EXPECT_EQ(status.error_code(), StatusCode::INTERNAL);
-  EXPECT_EQ(status.error_message(), "Just a test");
-}
-
-TEST(PreStopHookService, StartDoRequestStop) {
-  int port = grpc_pick_unused_port_or_die();
-  grpc_core::Mutex mu;
-  grpc_core::CondVar condition;
-  Server* server = nullptr;
-  HookServiceImpl service;
-  std::thread server_thread(ServerLoop, &service, port, &server, &mu,
-                            &condition);
-  {
-    grpc_core::MutexLock lock(&mu);
-    while (server == nullptr) {
-      condition.Wait(&mu);
-    }
-  }
-  auto channel = CreateChannel(absl::StrFormat("127.0.0.1:%d", port),
-                               InsecureChannelCredentials());
-  ASSERT_TRUE(channel);
-  CallInfo infos[3];
-  HookService::Stub stub(std::move(channel));
-  stub.async()->Hook(
-      &infos[0].context, &infos[0].request, &infos[0].response,
-      [&infos](const Status& status) { infos[0].SetStatus(status); });
-  stub.async()->Hook(
-      &infos[1].context, &infos[1].request, &infos[1].response,
-      [&infos](const Status& status) { infos[1].SetStatus(status); });
-  ASSERT_TRUE(service.TestOnlyExpectRequests(2, absl::Milliseconds(100)));
-  ClientContext ctx;
-  SetReturnStatusRequest request;
-  request.set_grpc_code_to_return(StatusCode::INTERNAL);
-  request.set_grpc_status_description("Just a test");
-  Empty response;
-  ASSERT_EQ(stub.SetReturnStatus(&ctx, request, &response).error_code(),
-            StatusCode::OK);
-  auto status = infos[0].WaitForStatus();
+  stub.async()->Hook(&info.context, &info.request, &info.response,
+                     [&info](Status status) { info.SetStatus(status); });
+  auto status = info.WaitForStatus();
   ASSERT_TRUE(status.has_value());
   EXPECT_EQ(status->error_code(), StatusCode::INTERNAL);
   EXPECT_EQ(status->error_message(), "Just a test");
-  status = infos[1].WaitForStatus();
-  ASSERT_TRUE(status.has_value());
-  EXPECT_EQ(status->error_code(), StatusCode::INTERNAL);
-  EXPECT_EQ(status->error_message(), "Just a test");
-  status = stub.Hook(&infos[2].context, infos[2].request, &infos[2].response);
-  ASSERT_TRUE(status.has_value());
-  EXPECT_EQ(status->error_code(), StatusCode::INTERNAL);
-  EXPECT_EQ(status->error_message(), "Just a test");
-  CallInfo reset_call_info;
-  ASSERT_TRUE(stub.ClearReturnStatus(&reset_call_info.context,
-                                     reset_call_info.request,
-                                     &reset_call_info.response)
-                  .ok());
-  CallInfo call_hangs;
-  stub.async()->Hook(
-      &call_hangs.context, &call_hangs.request, &call_hangs.response,
-      [&](const Status& status) { call_hangs.SetStatus(status); });
-  ASSERT_TRUE(service.TestOnlyExpectRequests(1, absl::Milliseconds(100)));
-  status = call_hangs.WaitForStatus(absl::Milliseconds(100));
-  EXPECT_FALSE(status.has_value()) << status->error_message();
-  service.Stop();
-  server->Shutdown();
-  server_thread.join();
 }
 
 }  // namespace
