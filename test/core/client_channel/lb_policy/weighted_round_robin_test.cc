b2d83b62dcbc0538df6b56a05100b96768a1bf3d
Mark D. Roth <roth@google.com>
2023-09-21 01:34:39 +0000

    fix LB unit tests

--- test/core/client_channel/lb_policy/weighted_round_robin_test.cc
+++ test/core/client_channel/lb_policy/weighted_round_robin_test.cc
@@ -850,6 +850,10 @@ TEST_F(WeightedRoundRobinTest, ZeroErrorUtilPenalty) {
 
 TEST_F(WeightedRoundRobinTest, MultipleAddressesPerEndpoint) {
   if (!IsWrrDelegateToPickFirstEnabled()) return;
+  // Can't use timer duration expectation here, because the Happy
+  // Eyeballs timer inside pick_first will use a different duration than
+  // the timer in outlier_detection.
+  SetExpectedTimerDuration(absl::nullopt);
   constexpr std::array<absl::string_view, 2> kEndpoint1Addresses = {
       "ipv4:127.0.0.1:443", "ipv4:127.0.0.1:444"};
   constexpr std::array<absl::string_view, 2> kEndpoint2Addresses = {
