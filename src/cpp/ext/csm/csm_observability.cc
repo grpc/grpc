ddc682c6c10aaccd2b97694e1ea2c52d217514f1
Yash Tibrewal <yashkt@google.com>
2023-09-22 12:03:30 -0700

    [CSM] Plumbing fixes (#34440)
    Changes -
    * CsmObservability doesn't need `SetTargetSelector`. Removed it.
    * Added missing plumbing of `ServiceMeshLabelsInjector` in
    `CsmObservability` to actually do the metadata exchange.
    
    <!--
    
    If you know who should review your pull request, please assign it to
    that
    person, otherwise the pull request would get assigned randomly.
    
    If your pull request is for a specific language, please add the
    appropriate
    lang label.
    
    -->

--- src/cpp/ext/csm/csm_observability.cc
+++ src/cpp/ext/csm/csm_observability.cc
@@ -27,7 +27,10 @@
 #include "absl/functional/any_invocable.h"
 #include "absl/status/statusor.h"
 #include "absl/types/optional.h"
+#include "google/cloud/opentelemetry/resource_detector.h"
 #include "opentelemetry/sdk/metrics/meter_provider.h"
+#include "opentelemetry/sdk/resource/resource.h"
+#include "opentelemetry/sdk/resource/resource_detector.h"
 
 #include <grpc/support/log.h>
 #include <grpcpp/ext/csm_observability.h>
@@ -35,6 +38,7 @@
 #include "src/core/ext/xds/xds_enabled_server.h"
 #include "src/core/lib/channel/channel_args.h"
 #include "src/core/lib/uri/uri_parser.h"
+#include "src/cpp/ext/csm/metadata_exchange.h"
 #include "src/cpp/ext/otel/otel_plugin.h"
 
 namespace grpc {
@@ -68,13 +72,6 @@ CsmObservabilityBuilder& CsmObservabilityBuilder::DisableAllMetrics() {
   return *this;
 }
 
-CsmObservabilityBuilder& CsmObservabilityBuilder::SetTargetSelector(
-    absl::AnyInvocable<bool(absl::string_view /*target*/) const>
-        target_selector) {
-  builder_.SetTargetSelector(std::move(target_selector));
-  return *this;
-}
-
 CsmObservabilityBuilder& CsmObservabilityBuilder::SetTargetAttributeFilter(
     absl::AnyInvocable<bool(absl::string_view /*target*/) const>
         target_attribute_filter) {
@@ -87,6 +84,11 @@ absl::StatusOr<CsmObservability> CsmObservabilityBuilder::BuildAndRegister() {
     return args.GetBool(GRPC_ARG_XDS_ENABLED_SERVER).value_or(false);
   });
   builder_.SetTargetSelector(internal::CsmChannelTargetSelector);
+  builder_.SetLabelsInjector(
+      std::make_unique<internal::ServiceMeshLabelsInjector>(
+          google::cloud::otel::MakeResourceDetector()
+              ->Detect()
+              .GetAttributes()));
   builder_.BuildAndRegisterGlobal();
   return CsmObservability();
 }
