a54872e47c1cd77cf51db02cc1ae278540dcdf15
Eugene Ostroukhov <eostroukhov@google.com>
2023-09-25 16:11:00 -0700

    Revert "[test interop] Add HookService to the maintenence server (#34413)"
    This reverts commit 490f6a3ee9a9c08f7f700842a285534fbb703df6.

--- test/cpp/interop/xds_interop_server_lib.h
+++ test/cpp/interop/xds_interop_server_lib.h
@@ -31,8 +31,7 @@ absl::optional<grpc::Status> GetStatusForRpcBehaviorMetadata(
     absl::string_view header_value, absl::string_view hostname);
 
 void RunServer(bool secure_mode, const int port, const int maintenance_port,
-               absl::string_view hostname, absl::string_view server_id,
-               const std::function<void(Server*)>& server_callback);
+               absl::string_view hostname, absl::string_view server_id);
 
 }  // namespace testing
 }  // namespace grpc
