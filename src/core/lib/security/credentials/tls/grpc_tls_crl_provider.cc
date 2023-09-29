ab7ce8a62af078ab402b35a20e4a288abb8a6e8a
Gregory Cooke <gregorycooke@google.com>
2023-09-29 18:45:48 +0000

    change import

--- src/core/lib/security/credentials/tls/grpc_tls_crl_provider.cc
+++ src/core/lib/security/credentials/tls/grpc_tls_crl_provider.cc
@@ -16,22 +16,24 @@
 //
 //
 
+#include <grpc/support/port_platform.h>
+
 #include "src/core/lib/security/credentials/tls/grpc_tls_crl_provider.h"
 
-#include <grpc/support/port_platform.h>
+#include <limits.h>
+
+#include <memory>
+#include <utility>
+#include <vector>
+
 #include <openssl/bio.h>
-#include <openssl/crypto.h>
+#include <openssl/crypto.h>  // For OPENSSL_free
 #include <openssl/pem.h>
 #include <openssl/x509.h>
-#include <limits.h>
-#include <openssl/mem.h>
-#include <memory>
-#include <vector>
-#include <utility>
 
+#include "absl/container/flat_hash_map.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
-#include "absl/container/flat_hash_map.h"
 #include "absl/strings/str_cat.h"
 
 namespace grpc_core {
