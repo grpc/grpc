06edaa0d976233ccc382ba60a0f1c7865c60b936
Esun Kim <veblush@google.com>
2023-10-06 16:24:48 -0700

    regenerate projects

--- src/ruby/lib/grpc/version.rb
+++ src/ruby/lib/grpc/version.rb
@@ -14,5 +14,5 @@
 
 # GRPC contains the General RPC module.
 module GRPC
-  VERSION = '1.59.0'
+  VERSION = '1.59.1'
 end
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.59.0'
+    VERSION = '1.59.1'
   end
 end
