bb6a6faa69ecc3291d341bc2257f4f6b15bcf4d5
Mark D. Roth <roth@google.com>
2023-10-06 15:23:33 -0700

    [SSA] support multiple addresses per endpoint (#34472)

--- src/core/ext/filters/stateful_session/stateful_session_filter.h
+++ src/core/ext/filters/stateful_session/stateful_session_filter.h
@@ -21,12 +21,15 @@
 
 #include <stddef.h>
 
+#include <utility>
+
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 
 #include "src/core/lib/channel/channel_args.h"
 #include "src/core/lib/channel/channel_fwd.h"
 #include "src/core/lib/channel/promise_based_filter.h"
+#include "src/core/lib/gprpp/ref_counted_string.h"
 #include "src/core/lib/gprpp/unique_type_name.h"
 #include "src/core/lib/promise/arena_promise.h"
 #include "src/core/lib/service_config/service_config_call_data.h"
@@ -34,20 +37,34 @@
 
 namespace grpc_core {
 
+// A call attribute to be passed to the xds_override_host LB policy.
+// The StatefulSession filter will populate the cookie's address list,
+// if set.  The xds_override_host LB policy will use that info, and then
+// set the actual address list based on the chosen endpoint.  The
+// StatefulSession filter will then use the actual address list to
+// update the cookie.
 class XdsOverrideHostAttribute
     : public ServiceConfigCallData::CallAttributeInterface {
  public:
   static UniqueTypeName TypeName();
 
-  explicit XdsOverrideHostAttribute(absl::string_view host_name)
-      : host_name_(host_name) {}
+  explicit XdsOverrideHostAttribute(absl::string_view cookie_address_list)
+      : cookie_address_list_(cookie_address_list) {}
 
-  absl::string_view host_name() const { return host_name_; }
+  absl::string_view cookie_address_list() const { return cookie_address_list_; }
+
+  absl::string_view actual_address_list() const {
+    return actual_address_list_.as_string_view();
+  }
+  void set_actual_address_list(RefCountedStringValue actual_address_list) {
+    actual_address_list_ = std::move(actual_address_list);
+  }
 
  private:
   UniqueTypeName type() const override { return TypeName(); }
 
-  absl::string_view host_name_;
+  absl::string_view cookie_address_list_;
+  RefCountedStringValue actual_address_list_;
 };
 
 // A filter to provide cookie-based stateful session affinity.
