8333f3e61afa51b5b019cc8f06f243cc710154dc
Gregory Cooke <gregorycooke@google.com>
2023-10-10 19:25:11 +0000

    grpc_tls_credentials_options gen

--- tools/codegen/core/gen_grpc_tls_credentials_options.py
+++ tools/codegen/core/gen_grpc_tls_credentials_options.py
@@ -221,6 +221,26 @@ _DATA_MEMBERS = [
         test_value_2='"crl_directory_2"',
     ),
     DataMember(
+        name="crl_provider",
+        type="std::shared_ptr<grpc_core::experimental::CrlProvider>",
+        getter_comment=(
+            "Returns the CRL Provider"
+        ),
+        setter_move_semantics=True,
+        special_comparator=(
+            "(crl_provider_ == other.crl_provider_ ||"
+            " (crl_provider_ != nullptr && other.crl_provider_"
+            " != nullptr))"
+        ),
+        test_name="DifferentCrlProvider",
+        test_value_1=(
+            'experimental::StaticCrlProvider::Create({})",'
+        ),
+        test_value_2=(
+            'experimental::StaticCrlProvider::Create({})'
+        ),
+    ),
+    DataMember(
         name="send_client_ca_list",
         type="bool",
         default_initializer="false",
