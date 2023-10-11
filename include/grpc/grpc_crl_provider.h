a82cbc25ec1527decf7644efcf4419fa862ec4d5
Gregory Cooke <gregorycooke@google.com>
2023-10-11 18:04:46 +0000

    remove map dependency

--- include/grpc/grpc_crl_provider.h
+++ include/grpc/grpc_crl_provider.h
@@ -21,11 +21,9 @@
 
 #include <grpc/support/port_platform.h>
 
-#include <map>
 #include <memory>
 #include <string>
 
-#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 
