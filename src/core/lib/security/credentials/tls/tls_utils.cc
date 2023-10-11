4b1b36ee7f81bec99907e8632922956cddf525b1
Gregory Cooke <gregorycooke@google.com>
2023-10-11 17:21:10 +0000

    clang format

--- src/core/lib/security/credentials/tls/tls_utils.cc
+++ src/core/lib/security/credentials/tls/tls_utils.cc
@@ -16,15 +16,17 @@
 //
 //
 
+#include <grpc/support/port_platform.h>
+
 #include "src/core/lib/security/credentials/tls/tls_utils.h"
 
-#include <grpc/support/port_platform.h>
 #include <stddef.h>
-#include <grpc/support/log.h>
 
 #include "absl/strings/ascii.h"
 #include "absl/strings/match.h"
 #include "absl/strings/str_cat.h"
+
+#include <grpc/support/log.h>
 
 namespace grpc_core {
 
