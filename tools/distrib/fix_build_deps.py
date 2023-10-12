7a97b35d9bb4f33186e61fcff30713b3bd3b7075
Yash Tibrewal <yashkt@google.com>
2023-10-11 17:14:59 -0700

    [CSM] Google Cloud C++ libraries now allow bazel layering checks (#34664)
    With the latest cloud C++ release - 
    1) The opentelemetry bazel target is no longer experimental.
    2) layering checks now work with bazel

--- tools/distrib/fix_build_deps.py
+++ tools/distrib/fix_build_deps.py
@@ -95,7 +95,7 @@ EXTERNAL_DEPS = {
     "absl/types/variant.h": "absl/types:variant",
     "absl/utility/utility.h": "absl/utility",
     "address_sorting/address_sorting.h": "address_sorting",
-    "google/cloud/opentelemetry/resource_detector.h": "google_cloud_cpp:experimental-opentelemetry",
+    "google/cloud/opentelemetry/resource_detector.h": "google_cloud_cpp:opentelemetry",
     "opentelemetry/common/attribute_value.h": "otel/api",
     "opentelemetry/common/key_value_iterable.h": "otel/api",
     "opentelemetry/nostd/function_ref.h": "otel/api",
