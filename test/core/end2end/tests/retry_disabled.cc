a4e5ad7af3b3584893b9a06fb9c5d313171e2ee2
Eugene Ostroukhov <eostroukhov@google.com>
2023-10-12 10:29:00 -0700

    fixup: compiler

--- test/core/end2end/tests/retry_disabled.cc
+++ test/core/end2end/tests/retry_disabled.cc
@@ -16,11 +16,8 @@
 //
 //
 
-<<<<<<< HEAD
-#include <string>
-=======
 #include <memory>
-    >>>>>>> grpc/master
+#include <string>
 
 #include "absl/types/optional.h"
 #include "gtest/gtest.h"
@@ -32,65 +29,65 @@
 #include "src/core/lib/gprpp/time.h"
 #include "test/core/end2end/end2end_tests.h"
 
-    namespace grpc_core {
-  namespace {
+namespace grpc_core {
+namespace {
 
-  // Tests that we don't retry when retries are disabled via the
-  // GRPC_ARG_ENABLE_RETRIES channel arg, even when there is retry
-  // configuration in the service config.
-  // - 1 retry allowed for ABORTED status
-  // - first attempt returns ABORTED but does not retry
-  CORE_END2END_TEST(RetryTest, RetryDisabled) {
-    InitServer(ChannelArgs());
-    InitClient(
-        ChannelArgs()
-            .Set(GRPC_ARG_ENABLE_RETRIES, false)
-            .Set(GRPC_ARG_SERVICE_CONFIG,
-                 "{\n"
-                 "  \"methodConfig\": [ {\n"
-                 "    \"name\": [\n"
-                 "      { \"service\": \"service\", \"method\": \"method\" }\n"
-                 "    ],\n"
-                 "    \"retryPolicy\": {\n"
-                 "      \"maxAttempts\": 2,\n"
-                 "      \"initialBackoff\": \"1s\",\n"
-                 "      \"maxBackoff\": \"120s\",\n"
-                 "      \"backoffMultiplier\": 1.6,\n"
-                 "      \"retryableStatusCodes\": [ \"ABORTED\" ]\n"
-                 "    }\n"
-                 "  } ]\n"
-                 "}"));
-    auto c =
-        NewClientCall("/service/method").Timeout(Duration::Seconds(5)).Create();
-    EXPECT_NE(c.GetPeer(), absl::nullopt);
-    IncomingMetadata server_initial_metadata;
-    IncomingMessage server_message;
-    IncomingStatusOnClient server_status;
-    c.NewBatch(1)
-        .SendInitialMetadata({})
-        .SendMessage("foo")
-        .RecvMessage(server_message)
-        .SendCloseFromClient()
-        .RecvInitialMetadata(server_initial_metadata)
-        .RecvStatusOnClient(server_status);
-    auto s = RequestCall(101);
-    Expect(101, true);
-    Step();
-    EXPECT_NE(s.GetPeer(), absl::nullopt);
-    EXPECT_NE(c.GetPeer(), absl::nullopt);
-    IncomingCloseOnServer client_close;
-    s.NewBatch(102)
-        .SendInitialMetadata({})
-        .SendStatusFromServer(GRPC_STATUS_ABORTED, "xyz", {})
-        .RecvCloseOnServer(client_close);
-    Expect(102, true);
-    Expect(1, true);
-    Step();
-    EXPECT_EQ(server_status.status(), GRPC_STATUS_ABORTED);
-    EXPECT_EQ(server_status.message(), "xyz");
-    EXPECT_EQ(s.method(), "/service/method");
-    EXPECT_FALSE(client_close.was_cancelled());
-  }
+// Tests that we don't retry when retries are disabled via the
+// GRPC_ARG_ENABLE_RETRIES channel arg, even when there is retry
+// configuration in the service config.
+// - 1 retry allowed for ABORTED status
+// - first attempt returns ABORTED but does not retry
+CORE_END2END_TEST(RetryTest, RetryDisabled) {
+  InitServer(ChannelArgs());
+  InitClient(
+      ChannelArgs()
+          .Set(GRPC_ARG_ENABLE_RETRIES, false)
+          .Set(GRPC_ARG_SERVICE_CONFIG,
+               "{\n"
+               "  \"methodConfig\": [ {\n"
+               "    \"name\": [\n"
+               "      { \"service\": \"service\", \"method\": \"method\" }\n"
+               "    ],\n"
+               "    \"retryPolicy\": {\n"
+               "      \"maxAttempts\": 2,\n"
+               "      \"initialBackoff\": \"1s\",\n"
+               "      \"maxBackoff\": \"120s\",\n"
+               "      \"backoffMultiplier\": 1.6,\n"
+               "      \"retryableStatusCodes\": [ \"ABORTED\" ]\n"
+               "    }\n"
+               "  } ]\n"
+               "}"));
+  auto c =
+      NewClientCall("/service/method").Timeout(Duration::Seconds(5)).Create();
+  EXPECT_NE(c.GetPeer(), absl::nullopt);
+  IncomingMetadata server_initial_metadata;
+  IncomingMessage server_message;
+  IncomingStatusOnClient server_status;
+  c.NewBatch(1)
+      .SendInitialMetadata({})
+      .SendMessage("foo")
+      .RecvMessage(server_message)
+      .SendCloseFromClient()
+      .RecvInitialMetadata(server_initial_metadata)
+      .RecvStatusOnClient(server_status);
+  auto s = RequestCall(101);
+  Expect(101, true);
+  Step();
+  EXPECT_NE(s.GetPeer(), absl::nullopt);
+  EXPECT_NE(c.GetPeer(), absl::nullopt);
+  IncomingCloseOnServer client_close;
+  s.NewBatch(102)
+      .SendInitialMetadata({})
+      .SendStatusFromServer(GRPC_STATUS_ABORTED, "xyz", {})
+      .RecvCloseOnServer(client_close);
+  Expect(102, true);
+  Expect(1, true);
+  Step();
+  EXPECT_EQ(server_status.status(), GRPC_STATUS_ABORTED);
+  EXPECT_EQ(server_status.message(), "xyz");
+  EXPECT_EQ(s.method(), "/service/method");
+  EXPECT_FALSE(client_close.was_cancelled());
+}
 
-  }  // namespace
+}  // namespace
 }  // namespace grpc_core
--- test/core/end2end/tests/retry_exceeds_buffer_size_in_initial_batch.cc
+++ test/core/end2end/tests/retry_exceeds_buffer_size_in_initial_batch.cc
@@ -16,11 +16,8 @@
 //
 //
 
-<<<<<<< HEAD
-#include <string>
-=======
 #include <memory>
-    >>>>>>> grpc/master
+#include <string>
 
 #include "absl/types/optional.h"
 #include "gtest/gtest.h"
@@ -32,66 +29,66 @@
 #include "src/core/lib/gprpp/time.h"
 #include "test/core/end2end/end2end_tests.h"
 
-    namespace grpc_core {
-  namespace {
+namespace grpc_core {
+namespace {
 
-  // Tests that we don't make any further attempts after we exceed the
-  // max buffer size.
-  // - 1 retry allowed for ABORTED status
-  // - buffer size set to 2 bytes
-  // - client sends a 3-byte message
-  // - first attempt gets ABORTED but is not retried
-  CORE_END2END_TEST(RetryTest, RetryExceedsBufferSizeInInitialBatch) {
-    InitServer(ChannelArgs());
-    InitClient(
-        ChannelArgs()
-            .Set(GRPC_ARG_SERVICE_CONFIG,
-                 "{\n"
-                 "  \"methodConfig\": [ {\n"
-                 "    \"name\": [\n"
-                 "      { \"service\": \"service\", \"method\": \"method\" }\n"
-                 "    ],\n"
-                 "    \"retryPolicy\": {\n"
-                 "      \"maxAttempts\": 2,\n"
-                 "      \"initialBackoff\": \"1s\",\n"
-                 "      \"maxBackoff\": \"120s\",\n"
-                 "      \"backoffMultiplier\": 1.6,\n"
-                 "      \"retryableStatusCodes\": [ \"ABORTED\" ]\n"
-                 "    }\n"
-                 "  } ]\n"
-                 "}")
-            .Set(GRPC_ARG_PER_RPC_RETRY_BUFFER_SIZE, 2));
-    auto c =
-        NewClientCall("/service/method").Timeout(Duration::Seconds(5)).Create();
-    EXPECT_NE(c.GetPeer(), absl::nullopt);
-    IncomingMessage server_message;
-    IncomingStatusOnClient server_status;
-    IncomingMetadata server_initial_metadata;
-    c.NewBatch(1)
-        .SendInitialMetadata({})
-        .SendMessage("foo")
-        .RecvMessage(server_message)
-        .SendCloseFromClient()
-        .RecvInitialMetadata(server_initial_metadata)
-        .RecvStatusOnClient(server_status);
-    auto s = RequestCall(101);
-    Expect(101, true);
-    Step();
-    EXPECT_NE(s.GetPeer(), absl::nullopt);
-    EXPECT_NE(c.GetPeer(), absl::nullopt);
-    IncomingCloseOnServer client_close;
-    s.NewBatch(102)
-        .SendInitialMetadata({})
-        .SendStatusFromServer(GRPC_STATUS_ABORTED, "xyz", {})
-        .RecvCloseOnServer(client_close);
-    Expect(102, true);
-    Expect(1, true);
-    Step();
-    EXPECT_EQ(server_status.status(), GRPC_STATUS_ABORTED);
-    EXPECT_EQ(server_status.message(), "xyz");
-    EXPECT_EQ(s.method(), "/service/method");
-    EXPECT_FALSE(client_close.was_cancelled());
-  }
+// Tests that we don't make any further attempts after we exceed the
+// max buffer size.
+// - 1 retry allowed for ABORTED status
+// - buffer size set to 2 bytes
+// - client sends a 3-byte message
+// - first attempt gets ABORTED but is not retried
+CORE_END2END_TEST(RetryTest, RetryExceedsBufferSizeInInitialBatch) {
+  InitServer(ChannelArgs());
+  InitClient(
+      ChannelArgs()
+          .Set(GRPC_ARG_SERVICE_CONFIG,
+               "{\n"
+               "  \"methodConfig\": [ {\n"
+               "    \"name\": [\n"
+               "      { \"service\": \"service\", \"method\": \"method\" }\n"
+               "    ],\n"
+               "    \"retryPolicy\": {\n"
+               "      \"maxAttempts\": 2,\n"
+               "      \"initialBackoff\": \"1s\",\n"
+               "      \"maxBackoff\": \"120s\",\n"
+               "      \"backoffMultiplier\": 1.6,\n"
+               "      \"retryableStatusCodes\": [ \"ABORTED\" ]\n"
+               "    }\n"
+               "  } ]\n"
+               "}")
+          .Set(GRPC_ARG_PER_RPC_RETRY_BUFFER_SIZE, 2));
+  auto c =
+      NewClientCall("/service/method").Timeout(Duration::Seconds(5)).Create();
+  EXPECT_NE(c.GetPeer(), absl::nullopt);
+  IncomingMessage server_message;
+  IncomingStatusOnClient server_status;
+  IncomingMetadata server_initial_metadata;
+  c.NewBatch(1)
+      .SendInitialMetadata({})
+      .SendMessage("foo")
+      .RecvMessage(server_message)
+      .SendCloseFromClient()
+      .RecvInitialMetadata(server_initial_metadata)
+      .RecvStatusOnClient(server_status);
+  auto s = RequestCall(101);
+  Expect(101, true);
+  Step();
+  EXPECT_NE(s.GetPeer(), absl::nullopt);
+  EXPECT_NE(c.GetPeer(), absl::nullopt);
+  IncomingCloseOnServer client_close;
+  s.NewBatch(102)
+      .SendInitialMetadata({})
+      .SendStatusFromServer(GRPC_STATUS_ABORTED, "xyz", {})
+      .RecvCloseOnServer(client_close);
+  Expect(102, true);
+  Expect(1, true);
+  Step();
+  EXPECT_EQ(server_status.status(), GRPC_STATUS_ABORTED);
+  EXPECT_EQ(server_status.message(), "xyz");
+  EXPECT_EQ(s.method(), "/service/method");
+  EXPECT_FALSE(client_close.was_cancelled());
+}
 
-  }  // namespace
+}  // namespace
 }  // namespace grpc_core
