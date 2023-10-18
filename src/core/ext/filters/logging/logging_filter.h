470714b0a95b44f9b2b5c707463dabc348de355b
Craig Tiller <ctiller@google.com>
2023-10-18 19:14:36 +0000

    fix

--- src/core/ext/filters/logging/logging_filter.h
+++ src/core/ext/filters/logging/logging_filter.h
@@ -21,8 +21,17 @@
 
 #include <grpc/support/port_platform.h>
 
+#include <string>
+#include <utility>
+
+#include "absl/status/statusor.h"
+
 #include "src/core/ext/filters/logging/logging_sink.h"
+#include "src/core/lib/channel/channel_args.h"
+#include "src/core/lib/channel/channel_fwd.h"
 #include "src/core/lib/channel/promise_based_filter.h"
+#include "src/core/lib/promise/arena_promise.h"
+#include "src/core/lib/transport/transport.h"
 
 namespace grpc_core {
 
