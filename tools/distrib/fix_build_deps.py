99275afa276e543efa92db60c8d66da29d5a0938
Esun Kim <veblush@google.com>
2023-10-20 09:41:19 -0700

    [Lint] Fixed missing deps (#34746)
    To fix some layering_check errors.

--- tools/distrib/fix_build_deps.py
+++ tools/distrib/fix_build_deps.py
@@ -45,7 +45,7 @@ EXTERNAL_DEPS = {
     "absl/base/attributes.h": "absl/base:core_headers",
     "absl/base/call_once.h": "absl/base",
     # TODO(ctiller) remove this
-    "absl/base/internal/endian.h": "absl/base",
+    "absl/base/internal/endian.h": "absl/base:endian",
     "absl/base/thread_annotations.h": "absl/base:core_headers",
     "absl/container/flat_hash_map.h": "absl/container:flat_hash_map",
     "absl/container/flat_hash_set.h": "absl/container:flat_hash_set",
