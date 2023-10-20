8fa4f77b70c4fabe274b9aa543e62139e15f676f
ctiller <ctiller@users.noreply.github.com>
2023-10-20 03:54:31 +0000

    Automated change: Fix sanity tests

--- test/core/memory_usage/xds_server.cc
+++ test/core/memory_usage/xds_server.cc
@@ -33,7 +33,6 @@
 
 #include "src/proto/grpc/testing/benchmark_service.grpc.pb.h"
 #include "src/proto/grpc/testing/messages.pb.h"
-#include "test/core/memory_usage/memstats.h"
 #include "test/core/util/test_config.h"
 
 class ServerCallbackImpl final
