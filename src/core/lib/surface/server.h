343d718875f97c502d89174ebf708129242b0973
ctiller <ctiller@users.noreply.github.com>
2023-10-24 17:53:04 +0000

    Automated change: Fix sanity tests

--- src/core/lib/surface/server.h
+++ src/core/lib/surface/server.h
@@ -22,6 +22,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <algorithm>
 #include <atomic>
 #include <functional>
 #include <list>
@@ -270,7 +271,7 @@ class Server : public InternallyRefCounted<Server>,
     static void SetRegisteredMethodOnMetadata(void* arg,
                                               ServerMetadata* metadata);
 
-    void Destroy() ABSL_EXCLUSIVE_LOCKS_REQUIRED(server_ -> mu_global_);
+    void Destroy() ABSL_EXCLUSIVE_LOCKS_REQUIRED(server_->mu_global_);
 
     static void FinishDestroy(void* arg, grpc_error_handle error);
 
