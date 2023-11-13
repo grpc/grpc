5eb3e9d505f2dd9a816be95776f8fa049c92b4f5
Mark D. Roth <roth@google.com>
2023-11-13 08:33:46 -0800

    [release] Bump version on master to 1.61.0-dev (#34923)

--- src/core/lib/surface/version.cc
+++ src/core/lib/surface/version.cc
@@ -25,4 +25,4 @@
 
 const char* grpc_version_string(void) { return "37.0.0"; }
 
-const char* grpc_g_stands_for(void) { return "gjallarhorn"; }
+const char* grpc_g_stands_for(void) { return "grand"; }
