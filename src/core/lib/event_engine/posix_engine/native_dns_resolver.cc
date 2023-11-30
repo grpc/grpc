24ded326c8b8650969efb88341a1e365cb73f4f7
yijiem <yijiem@google.com>
2023-11-29 16:56:06 -0800

    comment

--- src/core/lib/event_engine/posix_engine/native_dns_resolver.cc
+++ src/core/lib/event_engine/posix_engine/native_dns_resolver.cc
@@ -1,6 +1,4 @@
-//
-//
-// Copyright 2015 gRPC authors.
+// Copyright 2023 The gRPC Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-//
 
 #include <grpc/support/port_platform.h>
 
