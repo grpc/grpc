7eb02a9cfbbe1c91a80bd9662f6e30e9c205c9a4
markdroth <markdroth@users.noreply.github.com>
2023-12-04 23:15:38 +0000

    Automated change: Fix sanity tests

--- src/core/ext/filters/client_channel/resolver/xds/xds_config.cc
+++ src/core/ext/filters/client_channel/resolver/xds/xds_config.cc
@@ -42,12 +42,9 @@ constexpr int kMaxXdsAggregateClusterRecursionDepth = 16;
 
 std::string XdsDependencyManager::XdsConfig::ToString() const {
   std::vector<std::string> parts = {
-      "{\n  listener: {",
-      listener->ToString(),
-      "}\n  route_config: {",
-      route_config->ToString(),
-      "}\n  virtual_host: {",
-      virtual_host->ToString(),
+      "{\n  listener: {",     listener->ToString(),
+      "}\n  route_config: {", route_config->ToString(),
+      "}\n  virtual_host: {", virtual_host->ToString(),
       "}\n  clusters: {\n"};
   for (const auto& p : clusters) {
     parts.push_back(absl::StrCat("    \"", p.first, "\": "));
