a0a61a7ac5fc430fa04c60477bcb35912c368a1e
ctiller <ctiller@users.noreply.github.com>
2024-01-02 18:28:05 +0000

    Automated change: Fix sanity tests

--- src/core/ext/transport/chaotic_good/chaotic_good_transport.cc
+++ src/core/ext/transport/chaotic_good/chaotic_good_transport.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <grpc/support/port_platform.h>
+
 #include "src/core/ext/transport/chaotic_good/chaotic_good_transport.h"
 
 namespace grpc_core {}  // namespace grpc_core
