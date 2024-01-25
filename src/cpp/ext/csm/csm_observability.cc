984daf98d783b2269acfadf9503b514008ad2cc3
Yash Tibrewal <yashkt@google.com>
2024-01-25 01:54:53 -0800

    [CSM o11y] Re-experimentalize CSM OTel Plugin Option (#35660)
    We are no longer sure about this API, so re-experimentalizing it.
    
    This PR will be backported to 1.61 as well.
    
    Closes #35660
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/35660 from yashykt:ReexperimentalizeCsmPluginOption 4f114a54d9914c70d49358f7e84e7bfdab576590
    PiperOrigin-RevId: 601378856

--- src/cpp/ext/csm/csm_observability.cc
+++ src/cpp/ext/csm/csm_observability.cc
@@ -141,10 +141,9 @@ absl::StatusOr<CsmObservability> CsmObservabilityBuilder::BuildAndRegister() {
   return CsmObservability();
 }
 
-}  // namespace experimental
-
 std::unique_ptr<OpenTelemetryPluginOption> MakeCsmOpenTelemetryPluginOption() {
   return std::make_unique<grpc::internal::CsmOpenTelemetryPluginOption>();
 }
 
+}  // namespace experimental
 }  // namespace grpc
