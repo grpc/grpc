a62de2f15f197f9fd26cdb878d8a5793b30b669b
Craig Tiller <ctiller@google.com>
2024-02-01 06:20:54 +0000

    x

--- src/core/lib/transport/call_factory.h
+++ src/core/lib/transport/call_factory.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GRPC_SRC_CORE_LIB_TRANSPORT_CHANNEL_H
-#define GRPC_SRC_CORE_LIB_TRANSPORT_CHANNEL_H
+#ifndef GRPC_SRC_CORE_LIB_TRANSPORT_CALL_FACTORY_H
+#define GRPC_SRC_CORE_LIB_TRANSPORT_CALL_FACTORY_H
 
 #include <grpc/support/port_platform.h>
 
@@ -41,4 +41,4 @@ class CallFactory : public RefCounted<CallFactory> {
 
 }  // namespace grpc_core
 
-#endif  // GRPC_SRC_CORE_LIB_TRANSPORT_CHANNEL_H
+#endif  // GRPC_SRC_CORE_LIB_TRANSPORT_CALL_FACTORY_H
