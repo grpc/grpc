d3d5f7d730f13870b5811d559b293e746ad7c06d
Yash Tibrewal <yashkt@google.com>
2024-02-02 23:10:13 +0000

    Remove keepalive settings

--- examples/cpp/otel/greeter_callback_server.cc
+++ examples/cpp/otel/greeter_callback_server.cc
@@ -77,19 +77,6 @@ void RunServer(uint16_t port) {
   // Register "service" as the instance through which we'll communicate with
   // clients. In this case it corresponds to an *synchronous* service.
   builder.RegisterService(&service);
-  // Sample way of setting keepalive arguments on the server. Here, we are
-  // configuring the server to send keepalive pings at a period of 10 minutes
-  // with a timeout of 20 seconds. Additionally, pings will be sent even if
-  // there are no calls in flight on an active HTTP2 connection. When receiving
-  // pings, the server will permit pings at an interval of 10 seconds.
-  builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIME_MS,
-                             10 * 60 * 1000 /*10 min*/);
-  builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIMEOUT_MS,
-                             20 * 1000 /*20 sec*/);
-  builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS, 1);
-  builder.AddChannelArgument(
-      GRPC_ARG_HTTP2_MIN_RECV_PING_INTERVAL_WITHOUT_DATA_MS,
-      10 * 1000 /*10 sec*/);
   // Finally assemble the server.
   std::unique_ptr<Server> server(builder.BuildAndStart());
   std::cout << "Server listening on " << server_address << std::endl;
