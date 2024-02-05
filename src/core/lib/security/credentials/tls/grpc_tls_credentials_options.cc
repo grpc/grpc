eece0c034a83d98cbe8111fd74f7e98ade4e8d31
Matthew Stevenson <52979934+matthewstevenson88@users.noreply.github.com>
2024-02-05 11:04:00 -0800

    [tls] Backport of #34861 to v1.56.x. (#35795)
    Co-authored-by: Luwei Ge <lwge@google.com>

--- src/core/lib/security/credentials/tls/grpc_tls_credentials_options.cc
+++ src/core/lib/security/credentials/tls/grpc_tls_credentials_options.cc
@@ -128,3 +128,15 @@ void grpc_tls_credentials_options_set_send_client_ca_list(
   }
   options->set_send_client_ca_list(send_client_ca_list);
 }
+
+void grpc_tls_credentials_options_set_min_tls_version(
+    grpc_tls_credentials_options* options, grpc_tls_version min_tls_version) {
+  GPR_ASSERT(options != nullptr);
+  options->set_min_tls_version(min_tls_version);
+}
+
+void grpc_tls_credentials_options_set_max_tls_version(
+    grpc_tls_credentials_options* options, grpc_tls_version max_tls_version) {
+  GPR_ASSERT(options != nullptr);
+  options->set_max_tls_version(max_tls_version);
+}
