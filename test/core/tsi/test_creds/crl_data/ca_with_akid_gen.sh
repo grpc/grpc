2b087bd25fd82b6ff8d9d68e3330954577d3bdc9
Gregory Cooke <gregorycooke@google.com>
2024-02-16 22:04:33 +0000

    sanity about pushd and popd

--- test/core/tsi/test_creds/crl_data/ca_with_akid_gen.sh
+++ test/core/tsi/test_creds/crl_data/ca_with_akid_gen.sh
@@ -16,7 +16,7 @@
 rm -rf ca_with_akid/
 mkdir ca_with_akid/
 cp ca-with-akid.cnf ca_with_akid/
-pushd ca_with_akid/
+pushd ca_with_akid/ || exit
 touch index.txt
 echo 1 > ./serial
 echo 1000 > ./crlnumber
@@ -26,7 +26,7 @@ openssl req -x509 -new -newkey rsa:2048 -nodes -keyout ca_with_akid.key -out ca_
   
 openssl ca -gencrl -out crl_with_akid.crl -keyfile ca_with_akid.key -cert ca_with_akid.pem -crldays 3650  -config ca-with-akid.cnf
 
-popd
+popd || exit
 
 cp "./ca_with_akid/ca_with_akid.key" ./
 cp "./ca_with_akid/ca_with_akid.pem" ./
