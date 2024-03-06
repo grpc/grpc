a4a044244d68ad8f93f4ebcbe19d209fe58e7b8e
Gregory Cooke <gregorycooke@google.com>
2024-03-06 21:32:48 +0000

    add more gen files

--- test/core/tsi/test_creds/crl_data/evil_ca_gen.sh
+++ test/core/tsi/test_creds/crl_data/evil_ca_gen.sh
@@ -22,29 +22,13 @@ touch index.txt
 echo 1 > ./serial
 echo 1000 > ./crlnumber
 
-# Generate the 
-# openssl genrsa -out temp.rsa 2048
-# openssl pkcs8 -topk8 -in temp.rsa -out evil_ca.key -nocrypt
-# rm temp.rsa
-# openssl req -key evil_ca.key -new -out temp.csr -config evil_ca.cnf
-# openssl x509 -req -days 3650 -in temp.csr -CA "../ca.pem" -CAkey "../ca.key" -CAcreateserial -out evil_ca.pem -extfile evil_ca.cnf -extensions 'v3_req'
+# Generate the CA with the same subject as the good CA
 openssl req -x509 -new -newkey rsa:2048 -nodes -keyout evil_ca.key -out evil_ca.pem \
-  -config "../evil_ca.cnf" -days 3650 -extensions v3_req
+  -config evil_ca.cnf -days 3650 -extensions v3_req
 
 # Generate the CRL file:
 # ----------------------------------------------------------------------------
-
 openssl ca -config=evil_ca.cnf -gencrl -out evil.crl -keyfile evil_ca.key -cert evil_ca.pem -crldays 3650
 popd
 cp "./evil_ca/evil.crl" ./crls/
 rm -rf evil_ca
-
-# openssl genrsa -out evil_cert.key.rsa 2048
-# openssl pkcs8 -topk8 -in evil_cert.key.rsa -out evil_cert.key -nocrypt
-# openssl req -new -key evil_cert.key -out evil_cert.csr
-# openssl x509 -req -CA evil_ca.pem -CAkey evil_ca.key -CAcreateserial -in evil_cert.csr \
-#   -out evil_cert.pem  -days 3650
-
-# rm evil_ca.srl
-# rm evil_cert.csr
-# rm evil_cert.key.rsa
