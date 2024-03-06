4fcb264ca7ae48bd8b7bb9a9d716863d6a12b372
Gregory Cooke <gregorycooke@google.com>
2024-03-06 19:46:51 +0000

    some progress

--- test/core/tsi/crl_ssl_transport_security_test.cc
+++ test/core/tsi/crl_ssl_transport_security_test.cc
@@ -60,6 +60,10 @@ const char* kRevokedKeyPath = "test/core/tsi/test_creds/crl_data/revoked.key";
 const char* kRevokedCertPath = "test/core/tsi/test_creds/crl_data/revoked.pem";
 const char* kValidKeyPath = "test/core/tsi/test_creds/crl_data/valid.key";
 const char* kValidCertPath = "test/core/tsi/test_creds/crl_data/valid.pem";
+const char* kEvilCertKeyPath =
+    "test/core/tsi/test_creds/crl_data/evil_cert.key";
+const char* kEvilCertCertPath =
+    "test/core/tsi/test_creds/crl_data/evil_cert.pem";
 
 const char* kRevokedIntermediateKeyPath =
     "test/core/tsi/test_creds/crl_data/leaf_signed_by_intermediate.key";
@@ -68,9 +72,9 @@ const char* kRevokedIntermediateCertPath =
 const char* kRootCrlPath = "test/core/tsi/test_creds/crl_data/crls/current.crl";
 const char* kIntermediateCrlPath =
     "test/core/tsi/test_creds/crl_data/crls/intermediate.crl";
-const char* kModifiedSignature =
+const char* kModifiedSignaturePath =
     "test/core/tsi/test_creds/crl_data/crls/invalid_signature.crl";
-const char* kModifiedContent =
+const char* kModifiedContentPath =
     "test/core/tsi/test_creds/crl_data/crls/invalid_content.crl";
 
 class CrlSslTransportSecurityTest
@@ -420,6 +424,51 @@ std::string TestNameSuffix(
   if (version.param == tsi_tls_version::TSI_TLS1_2) return "TLS_1_2";
   GPR_ASSERT(version.param == tsi_tls_version::TSI_TLS1_3);
   return "TLS_1_3";
+}
+
+TEST_P(CrlSslTransportSecurityTest, CrlProviderModifiedContentCrl) {
+  std::string root_crl =
+      grpc_core::testing::GetFileContents(kModifiedContentPath);
+  std::string intermediate_crl =
+      grpc_core::testing::GetFileContents(kIntermediateCrlPath);
+
+  absl::StatusOr<std::shared_ptr<grpc_core::experimental::CrlProvider>>
+      provider = grpc_core::experimental::CreateStaticCrlProvider(
+          {root_crl, intermediate_crl});
+  ASSERT_NE(provider.status(), absl::OkStatus()) << provider.status();
+}
+
+TEST_P(CrlSslTransportSecurityTest, CrlProviderModifiedSignatureCrl) {
+  std::string root_crl =
+      grpc_core::testing::GetFileContents(kModifiedSignaturePath);
+  std::string intermediate_crl =
+      grpc_core::testing::GetFileContents(kIntermediateCrlPath);
+
+  absl::StatusOr<std::shared_ptr<grpc_core::experimental::CrlProvider>>
+      provider = grpc_core::experimental::CreateStaticCrlProvider(
+          {root_crl, intermediate_crl});
+  ASSERT_TRUE(provider.ok()) << provider.status();
+
+  auto* fixture = new SslTsiTestFixture(kValidKeyPath, kValidCertPath,
+                                        kValidKeyPath, kValidCertPath, nullptr,
+                                        *provider, false, false, false);
+  fixture->Run();
+}
+
+TEST_P(CrlSslTransportSecurityTest, CrlFromBadCa) {
+  std::string root_crl = grpc_core::testing::GetFileContents(kRootCrlPath);
+  std::string intermediate_crl =
+      grpc_core::testing::GetFileContents(kIntermediateCrlPath);
+
+  absl::StatusOr<std::shared_ptr<grpc_core::experimental::CrlProvider>>
+      provider = grpc_core::experimental::CreateStaticCrlProvider(
+          {root_crl, intermediate_crl});
+  ASSERT_TRUE(provider.ok()) << provider.status();
+
+  auto* fixture = new SslTsiTestFixture(kValidKeyPath, kValidCertPath,
+                                        kEvilCertKeyPath, kEvilCertCertPath,
+                                        nullptr, *provider, false, false, true);
+  fixture->Run();
 }
 
 // TODO(gtcooke94) Add nullptr issuer test cases - this is not simple to test
--- test/core/tsi/test_creds/crl_data/evil_ca_gen.sh
+++ test/core/tsi/test_creds/crl_data/evil_ca_gen.sh
@@ -16,3 +16,30 @@
 # Generates a CA with the same issuer name as the good CA in this directory
 openssl req -x509 -new -newkey rsa:2048 -nodes -keyout evil_ca.key -out evil_ca.pem \
   -config ca-openssl.cnf -days 3650 -extensions v3_req
+
+# Generate the CRL file:
+# ----------------------------------------------------------------------------
+rm -rf evil_ca
+mkdir evil_ca
+cp ca-openssl.cnf evil_ca/
+cp evil_ca.key evil_ca/
+cp evil_ca.pem evil_ca/
+pushd evil_ca 
+touch index.txt
+echo 1 > ./serial
+echo 1000 > ./crlnumber
+
+openssl ca -gencrl -out evil.crl -keyfile evil_ca.key -cert evil_ca.pem -crldays 3650
+popd
+cp "./evil_ca/evil.crl" ./crls/
+rm -rf evil_ca
+
+# openssl genrsa -out evil_cert.key.rsa 2048
+# openssl pkcs8 -topk8 -in evil_cert.key.rsa -out evil_cert.key -nocrypt
+# openssl req -new -key evil_cert.key -out evil_cert.csr
+# openssl x509 -req -CA evil_ca.pem -CAkey evil_ca.key -CAcreateserial -in evil_cert.csr \
+#   -out evil_cert.pem  -days 3650
+
+# rm evil_ca.srl
+# rm evil_cert.csr
+# rm evil_cert.key.rsa
