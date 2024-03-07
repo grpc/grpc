35d732a3160bcebfccf8b92b691de1042d43d3e5
Eugene Ostroukhov <eostroukhov@google.com>
2024-03-07 14:00:10 -0800

    regenerate projects

--- src/ruby/lib/grpc/version.rb
+++ src/ruby/lib/grpc/version.rb
@@ -14,5 +14,5 @@
 
 # GRPC contains the General RPC module.
 module GRPC
-  VERSION = '1.62.0'
+  VERSION = '1.62.1'
 end
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.62.0'
+    VERSION = '1.62.1'
   end
 end
