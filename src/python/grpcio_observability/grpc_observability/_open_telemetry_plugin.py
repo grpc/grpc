f935cd4a2fd91abbcfba997e768a3483421b8129
Xuan Wang <xuanwn@google.com>
2024-03-12 04:37:43 +0000

    Fix sanity

--- src/python/grpcio_observability/grpc_observability/_open_telemetry_plugin.py
+++ src/python/grpcio_observability/grpc_observability/_open_telemetry_plugin.py
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 import abc
-from typing import TYPE_CHECKING, Any, Callable, Dict, Iterable, List, Optional, Union
+from typing import Callable, Dict, Iterable, List, Optional, Union
 
 # pytype: disable=pyi-error
 import grpc
-from grpc_observability import _open_telemetry_observability
 from grpc_observability import _open_telemetry_measures
+from grpc_observability import _open_telemetry_observability
 from grpc_observability._cyobservability import MetricsName
 from grpc_observability._observability import StatsData
 from opentelemetry.metrics import Counter
@@ -29,9 +29,6 @@ from opentelemetry.metrics import MeterProvider
 GRPC_METHOD_LABEL = "grpc.method"
 GRPC_TARGET_LABEL = "grpc.target"
 GRPC_OTHER_LABEL_VALUE = "other"
-
-if TYPE_CHECKING:
-    _OpenTelemetryPlugin = Any # Forward declaration
 
 
 class OpenTelemetryLabelInjector(abc.ABC):
@@ -92,92 +89,11 @@ class OpenTelemetryPluginOption(abc.ABC):
         raise NotImplementedError()
 
 
-# pylint: disable=no-self-use
-class OpenTelemetryPlugin:
-    """Describes a Plugin for OpenTelemetry observability."""
-
-    plugin_options: Iterable[OpenTelemetryPluginOption]
-    meter_provider: Optional[MeterProvider]
-    target_attribute_filter: Callable[[str], bool]
-    generic_method_attribute_filter: Callable[[str], bool]
-    _plugin: _OpenTelemetryPlugin
-
-    def __init__(
-        self,
-        *,
-        plugin_options: Iterable[OpenTelemetryPluginOption] = [],
-        meter_provider: Optional[MeterProvider] = None,
-        target_attribute_filter: Optional[Callable[[str], bool]] = None,
-        generic_method_attribute_filter: Optional[Callable[[str], bool]] = None,
-    ):
-        """
-        Args:
-          plugin_options: An Iterable of OpenTelemetryPluginOption which will be
-        enabled for this OpenTelemetryPlugin.
-          meter_provider: A MeterProvider which will be used to collect telemetry data,
-        or None which means no metrics will be collected.
-          target_attribute_filter: Once provided, this will be called per channel to decide
-        whether to record the target attribute on client or to replace it with "other".
-        This helps reduce the cardinality on metrics in cases where many channels
-        are created with different targets in the same binary (which might happen
-        for example, if the channel target string uses IP addresses directly).
-        Return True means the original target string will be used, False means target string
-        will be replaced with "other".
-          generic_method_attribute_filter: Once provided, this will be called with a generic
-        method type to decide whether to record the method name or to replace it with
-        "other". Note that pre-registered methods will always be recorded no matter what
-        this function returns.
-        Return True means the original method name will be used, False means method name will
-        be replaced with "other".
-        """
-        self.plugin_options = plugin_options
-        self.meter_provider = meter_provider
-        if target_attribute_filter:
-            self.target_attribute_filter = target_attribute_filter
-        else:
-            self.target_attribute_filter = lambda target: True
-        if generic_method_attribute_filter:
-            self.generic_method_attribute_filter = (
-                generic_method_attribute_filter
-            )
-        else:
-            self.generic_method_attribute_filter = lambda method: False
-        self._plugin = _OpenTelemetryPlugin(self)
-
-    def register_global(self) -> None:
-        """
-        Registers a global plugin that acts on all channels and servers running on the process.
-
-        Raises:
-            RuntimeError: If a global plugin was already registered.
-        """
-        _open_telemetry_observability.start_open_telemetry_observability(
-            plugins=[self._plugin]
-        )
-
-    def deregister_global(self) -> None:
-        """
-        De-register the global plugin that acts on all channels and servers running on the process.
-
-        Raises:
-            RuntimeError: If no global plugin was registered.
-        """
-        _open_telemetry_observability.end_open_telemetry_observability()
-
-    def __enter__(self) -> None:
-        _open_telemetry_observability.start_open_telemetry_observability(
-            plugins=[self._plugin]
-        )
-
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-        _open_telemetry_observability.end_open_telemetry_observability()
-
-
 class _OpenTelemetryPlugin:
-    _plugin: OpenTelemetryPlugin
+    _plugin: "OpenTelemetryPlugin"
     _metric_to_recorder: Dict[MetricsName, Union[Counter, Histogram]]
 
-    def __init__(self, plugin: OpenTelemetryPlugin):
+    def __init__(self, plugin: "OpenTelemetryPlugin"):
         self._plugin = plugin
         self._metric_to_recorder = dict()
 
@@ -286,3 +202,84 @@ class _OpenTelemetryPlugin:
                 )
             metric_to_recorder_map[metric.cyname] = recorder
         return metric_to_recorder_map
+
+
+# pylint: disable=no-self-use
+class OpenTelemetryPlugin:
+    """Describes a Plugin for OpenTelemetry observability."""
+
+    plugin_options: Iterable[OpenTelemetryPluginOption]
+    meter_provider: Optional[MeterProvider]
+    target_attribute_filter: Callable[[str], bool]
+    generic_method_attribute_filter: Callable[[str], bool]
+    _plugin: _OpenTelemetryPlugin
+
+    def __init__(
+        self,
+        *,
+        plugin_options: Iterable[OpenTelemetryPluginOption] = [],
+        meter_provider: Optional[MeterProvider] = None,
+        target_attribute_filter: Optional[Callable[[str], bool]] = None,
+        generic_method_attribute_filter: Optional[Callable[[str], bool]] = None,
+    ):
+        """
+        Args:
+          plugin_options: An Iterable of OpenTelemetryPluginOption which will be
+        enabled for this OpenTelemetryPlugin.
+          meter_provider: A MeterProvider which will be used to collect telemetry data,
+        or None which means no metrics will be collected.
+          target_attribute_filter: Once provided, this will be called per channel to decide
+        whether to record the target attribute on client or to replace it with "other".
+        This helps reduce the cardinality on metrics in cases where many channels
+        are created with different targets in the same binary (which might happen
+        for example, if the channel target string uses IP addresses directly).
+        Return True means the original target string will be used, False means target string
+        will be replaced with "other".
+          generic_method_attribute_filter: Once provided, this will be called with a generic
+        method type to decide whether to record the method name or to replace it with
+        "other". Note that pre-registered methods will always be recorded no matter what
+        this function returns.
+        Return True means the original method name will be used, False means method name will
+        be replaced with "other".
+        """
+        self.plugin_options = plugin_options
+        self.meter_provider = meter_provider
+        if target_attribute_filter:
+            self.target_attribute_filter = target_attribute_filter
+        else:
+            self.target_attribute_filter = lambda target: True
+        if generic_method_attribute_filter:
+            self.generic_method_attribute_filter = (
+                generic_method_attribute_filter
+            )
+        else:
+            self.generic_method_attribute_filter = lambda method: False
+        self._plugin = _OpenTelemetryPlugin(self)
+
+    def register_global(self) -> None:
+        """
+        Registers a global plugin that acts on all channels and servers running on the process.
+
+        Raises:
+            RuntimeError: If a global plugin was already registered.
+        """
+        _open_telemetry_observability.start_open_telemetry_observability(
+            plugins=[self._plugin]
+        )
+
+    def deregister_global(self) -> None:
+        """
+        De-register the global plugin that acts on all channels and servers running on the process.
+
+        Raises:
+            RuntimeError: If no global plugin was registered.
+        """
+        _open_telemetry_observability.end_open_telemetry_observability()
+
+    def __enter__(self) -> None:
+        _open_telemetry_observability.start_open_telemetry_observability(
+            plugins=[self._plugin]
+        )
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        _open_telemetry_observability.end_open_telemetry_observability()
