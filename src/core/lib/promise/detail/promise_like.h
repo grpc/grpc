00b89a2ff45228e35cdc0f286d253ccbcc21763e
Ryan Prichard <rprichard@google.com>
2024-03-19 15:31:57 -0700

    Use __cpp_lib_is_invocable and _MSVC_LANG
    Match abseil's condition for "is std::invoke_result" available. Using
    __cplusplus is complicated because MSVC defaults it to an old version
    unless a special `/Zc:__cplusplus` flag is passed.

--- src/core/lib/promise/detail/promise_like.h
+++ src/core/lib/promise/detail/promise_like.h
@@ -71,7 +71,8 @@ class PromiseLike<void>;
 
 template <typename F>
 class PromiseLike<F, absl::enable_if_t<!std::is_void<
-#if __cplusplus >= 201703L
+#if (defined(__cpp_lib_is_invocable) && __cpp_lib_is_invocable >= 201703L) || \
+    (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
                          std::invoke_result_t<F>
 #else
                          typename std::result_of<F()>::type
