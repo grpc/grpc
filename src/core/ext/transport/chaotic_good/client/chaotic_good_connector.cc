3f3718750acc77312bf2a65a0794a7b53fa79d16
yijiem <yijiem@google.com>
2024-03-21 18:56:44 -0700

    fix

--- src/core/ext/transport/chaotic_good/client/chaotic_good_connector.cc
+++ src/core/ext/transport/chaotic_good/client/chaotic_good_connector.cc
@@ -33,6 +33,7 @@
 #include "src/core/ext/transport/chaotic_good/frame_header.h"
 #include "src/core/ext/transport/chaotic_good/settings_metadata.h"
 #include "src/core/lib/channel/channel_args.h"
+#include "src/core/lib/config/core_configuration.h"
 #include "src/core/lib/event_engine/channel_args_endpoint_config.h"
 #include "src/core/lib/event_engine/default_event_engine.h"
 #include "src/core/lib/event_engine/extensions/chaotic_good_extension.h"
