e40f39fa92a25f1dc9e6d0437a767865f43efa5c
AJ Heller <hork@google.com>
2024-03-25 19:23:45 +0000

    fix

--- src/core/ext/transport/chttp2/server/chttp2_server.h
+++ src/core/ext/transport/chttp2/server/chttp2_server.h
@@ -66,7 +66,8 @@ class PassiveListenerImpl final : public PassiveListener {
   // required for older gcc versions.
   friend absl::Status(::grpc_server_add_passive_listener)(
       grpc_core::Server* server, grpc_server_credentials* credentials,
-      std::shared_ptr<PassiveListenerImpl> passive_listener);
+      std::shared_ptr<grpc_core::experimental::PassiveListenerImpl>
+          passive_listener);
 
   Mutex mu_;
   // Data members will be populated when initialized.
