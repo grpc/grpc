3e0eeed4fa4dd57f10084f0461c382497d2d6f11
Tanvi Jagtap <139093547+tanvi-jagtap@users.noreply.github.com>
2024-04-10 06:00:15 -0700

    [grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging GPR_AS… (#36267)
    [grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging GPR_ASSERT
    Replacing GPR_ASSERT with absl CHECK
    
    Will not be replacing CHECK with CHECK_EQ , CHECK_NE etc because there are too many callsites. Only a few - which fit into single - line regex will be changed. This would be small in number just to reduce the load later.
    
    Replacing CHECK with CHECK_EQ , CHECK_NE etc could be done using Cider-V once these changes are submitted if we want to clean up later. Given that we have 5000+ instances of GPR_ASSERT to edit, Doing it manually is too much work for both the author and reviewer.
    
    <!--
    
    If you know who should review your pull request, please assign it to that
    person, otherwise the pull request would get assigned randomly.
    
    If your pull request is for a specific language, please add the appropriate
    lang label.
    
    -->
    
    Closes #36267
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/36267 from tanvi-jagtap:tjagtap_grpc_assert_02 3aed62610192ef9e46b2b25f215e694a4a6f6862
    PiperOrigin-RevId: 623469007

--- test/cpp/microbenchmarks/bm_chttp2_transport.cc
+++ test/cpp/microbenchmarks/bm_chttp2_transport.cc
@@ -26,6 +26,8 @@
 
 #include <benchmark/benchmark.h>
 
+#include "absl/log/check.h"
+
 #include <grpc/support/alloc.h>
 #include <grpc/support/log.h>
 #include <grpc/support/string_util.h>
@@ -64,7 +66,7 @@ class PhonyEndpoint : public grpc_endpoint {
 
   void PushInput(grpc_slice slice) {
     if (read_cb_ == nullptr) {
-      GPR_ASSERT(!have_slice_);
+      CHECK(!have_slice_);
       buffered_slice_ = slice;
       have_slice_ = true;
       return;
@@ -81,7 +83,7 @@ class PhonyEndpoint : public grpc_endpoint {
   grpc_slice buffered_slice_;
 
   void QueueRead(grpc_slice_buffer* slices, grpc_closure* cb) {
-    GPR_ASSERT(read_cb_ == nullptr);
+    CHECK_EQ(read_cb_, nullptr);
     if (have_slice_) {
       have_slice_ = false;
       grpc_slice_buffer_add(slices, buffered_slice_);
@@ -393,7 +395,7 @@ static void BM_TransportEmptyOp(benchmark::State& state) {
   gpr_event_init(stream_cancel_done);
   std::unique_ptr<TestClosure> stream_cancel_closure =
       MakeTestClosure([&](grpc_error_handle error) {
-        GPR_ASSERT(error.ok());
+        CHECK(error.ok());
         gpr_event_set(stream_cancel_done, reinterpret_cast<void*>(1));
       });
   op.on_complete = stream_cancel_closure.get();
