cd30592193c297724b18bd506767856beba70dcc
Yijie Ma <yijiem@google.com>
2024-04-11 11:01:21 -0700

    [release] Bump dev version 202404101721 (#36333)
    <!--
    
    If you know who should review your pull request, please assign it to that
    person, otherwise the pull request would get assigned randomly.
    
    If your pull request is for a specific language, please add the appropriate
    lang label.
    
    -->
    
    Closes #36333
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/36333 from yijiem:bump_dev_version_202404101721 0a7cc5b4b740688b81417ef2da8f93b4c027f3ed
    PiperOrigin-RevId: 623878150

--- src/core/lib/surface/version.cc
+++ src/core/lib/surface/version.cc
@@ -24,4 +24,4 @@
 
 const char* grpc_version_string(void) { return "40.0.0"; }
 
-const char* grpc_g_stands_for(void) { return "giggle"; }
+const char* grpc_g_stands_for(void) { return "grateful"; }
