f9eaf451402f864675549b87c637fc832a14776b
tanvi-jagtap <tjagtap@google.com>
2024-04-19 15:38:59 +0000

    [grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging GPR_ASSERT

--- test/core/security/ssl_server_fuzzer.cc
+++ test/core/security/ssl_server_fuzzer.cc
@@ -87,7 +87,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     // Create security connector
     grpc_core::RefCountedPtr<grpc_server_security_connector> sc =
         creds->create_security_connector(grpc_core::ChannelArgs());
-    CHECK_NE(sc, nullptr);
+    CHECK(sc != nullptr);
     grpc_core::Timestamp deadline =
         grpc_core::Duration::Seconds(1) + grpc_core::Timestamp::Now();
 
