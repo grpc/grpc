34b92a3cea295035aafd2528a41190e76035a28e
tanvi-jagtap <tjagtap@google.com>
2024-04-25 14:59:31 +0000

    [grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging GPR_ASSERT

--- src/cpp/client/call_credentials.cc
+++ src/cpp/client/call_credentials.cc
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#include "absl/log/check.h"
 #include "absl/strings/str_cat.h"
 
 #include <grpc/support/port_platform.h>
