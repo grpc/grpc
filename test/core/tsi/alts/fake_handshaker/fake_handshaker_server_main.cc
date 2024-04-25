60f8235ae865919836301e87c1a28d661fa7316e
Tanvi Jagtap <139093547+tanvi-jagtap@users.noreply.github.com>
2024-04-25 08:18:29 -0700

    [grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging GPR_ASSERT (#36442)
    [grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging GPR_ASSERT
    Replacing GPR_ASSERT with absl CHECK
    
    These changes have been made using string replacement and regex.
    
    Will not be replacing all instances of CHECK with CHECK_EQ , CHECK_NE etc because there are too many callsites. Only ones which are doable using very simple regex with least chance of failure will be replaced.
    
    Given that we have 5000+ instances of GPR_ASSERT to edit, Doing it manually is too much work for both the author and reviewer.
    
    <!--
    
    If you know who should review your pull request, please assign it to that
    person, otherwise the pull request would get assigned randomly.
    
    If your pull request is for a specific language, please add the appropriate
    lang label.
    
    -->
    
    Closes #36442
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/36442 from tanvi-jagtap:tjagtap_xds_tsi_address_utils 5c10771ac16de58bfaf54965b98ea7aca624ab09
    PiperOrigin-RevId: 628081981

--- test/core/tsi/alts/fake_handshaker/fake_handshaker_server_main.cc
+++ test/core/tsi/alts/fake_handshaker/fake_handshaker_server_main.cc
@@ -18,6 +18,7 @@
 #include <sstream>
 
 #include "absl/flags/flag.h"
+#include "absl/log/check.h"
 
 #include <grpc/support/log.h>
 #include <grpcpp/impl/service_type.h>
@@ -49,7 +50,7 @@ int main(int argc, char** argv) {
   grpc::testing::TestEnvironment env(&argc, argv);
   grpc::testing::InitTest(&argc, &argv, true);
 
-  GPR_ASSERT(absl::GetFlag(FLAGS_handshaker_port) != 0);
+  CHECK_NE(absl::GetFlag(FLAGS_handshaker_port), 0);
   std::ostringstream server_address;
   server_address << "[::1]:" << absl::GetFlag(FLAGS_handshaker_port);
 
