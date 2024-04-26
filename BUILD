grpc/grpc

commit 3890766adfc01b7d2884373899f083a22ee23eae

[grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging GPR_ASSERT (#36452)
[grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging GPR_ASSERT
Replacing GPR_ASSERT with absl CHECK.
These changes have been made using string replacement and regex.
Will not be replacing all instances of CHECK with CHECK_EQ , CHECK_NE etc because there are too many callsites. Only ones which are doable using very simple regex with least chance of failure will be replaced.
Given that we have 5000+ instances of GPR_ASSERT to edit, Doing it manually is too much work for both the author and reviewer.

<!--

If you know who should review your pull request, please assign it to that
person, otherwise the pull request would get assigned randomly.

If your pull request is for a specific language, please add the appropriate
lang label.

-->

Closes #36452

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/36452 from tanvi-jagtap:tjagtap_include_grpc 08e612fccdf817db8dfce268a26343a9595d0843
PiperOrigin-RevId: 628318595

BUILD
@@ -897,6 +897,7 @@ grpc_cc_library(
     name = "grpc++_public_hdrs",
     hdrs = GRPCXX_PUBLIC_HDRS,
     external_deps = [
+        "absl/log:check",
         "absl/strings:cord",
         "absl/synchronization",
         "protobuf_headers",

CMakeLists.txt
@@ -109,6 +109,7 @@ set(gRPC_ABSL_USED_TARGETS
   absl_bind_front
   absl_bits
   absl_charset
+  absl_check
   absl_city
   absl_civil_time
   absl_cleanup
@@ -169,6 +170,8 @@ set(gRPC_ABSL_USED_TARGETS
   absl_log_entry
   absl_log_globals
   absl_log_internal_append_truncated
+  absl_log_internal_check_impl
+  absl_log_internal_check_op
   absl_log_internal_conditions
   absl_log_internal_config
   absl_log_internal_fnmatch
@@ -4146,6 +4149,7 @@ target_include_directories(grpc++
 )
 target_link_libraries(grpc++
   ${_gRPC_ALLTARGETS_LIBRARIES}
+  absl::check
   grpc
   ${_gRPC_PROTOBUF_LIBRARIES}
 )
@@ -4887,6 +4891,7 @@ target_include_directories(grpc++_unsecure
 )
 target_link_libraries(grpc++_unsecure
   ${_gRPC_ALLTARGETS_LIBRARIES}
+  absl::check
   grpc_unsecure
   ${_gRPC_PROTOBUF_LIBRARIES}
 )
@@ -15350,7 +15355,6 @@ target_include_directories(grpc_cli
 
 target_link_libraries(grpc_cli
   ${_gRPC_ALLTARGETS_LIBRARIES}
-  absl::check
   grpc++
   ${_gRPC_PROTOBUF_PROTOC_LIBRARIES}
   grpc++_test_config
@@ -36971,7 +36975,7 @@ generate_pkgconfig(
   "gRPC++"
   "C++ wrapper for gRPC"
   "${gRPC_CPP_VERSION}"
-  "absl_algorithm_container absl_any_invocable absl_base absl_bind_front absl_cleanup absl_config absl_cord absl_core_headers absl_flags absl_flags_marshalling absl_flat_hash_map absl_flat_hash_set absl_function_ref absl_hash absl_inlined_vector absl_log absl_log_severity absl_memory absl_no_destructor absl_optional absl_random_bit_gen_ref absl_random_distributions absl_random_random absl_span absl_status absl_statusor absl_str_format absl_strings absl_synchronization absl_time absl_type_traits absl_utility absl_variant gpr grpc"
+  "absl_algorithm_container absl_any_invocable absl_base absl_bind_front absl_check absl_cleanup absl_config absl_cord absl_core_headers absl_flags absl_flags_marshalling absl_flat_hash_map absl_flat_hash_set absl_function_ref absl_hash absl_inlined_vector absl_log absl_log_severity absl_memory absl_no_destructor absl_optional absl_random_bit_gen_ref absl_random_distributions absl_random_random absl_span absl_status absl_statusor absl_str_format absl_strings absl_synchronization absl_time absl_type_traits absl_utility absl_variant gpr grpc"
   "libcares openssl re2 zlib"
   "-lgrpc++"
   "-laddress_sorting -lupb_textformat_lib -lupb_json_lib -lupb_message_lib -lupb_mem_lib -lupb_base_lib -lutf8_range_lib"
@@ -36982,7 +36986,7 @@ generate_pkgconfig(
   "gRPC++ unsecure"
   "C++ wrapper for gRPC without SSL"
   "${gRPC_CPP_VERSION}"
-  "absl_algorithm_container absl_any_invocable absl_base absl_bind_front absl_cleanup absl_config absl_cord absl_core_headers absl_flags absl_flags_marshalling absl_flat_hash_map absl_flat_hash_set absl_function_ref absl_hash absl_inlined_vector absl_log absl_log_severity absl_memory absl_no_destructor absl_optional absl_random_bit_gen_ref absl_random_distributions absl_random_random absl_span absl_status absl_statusor absl_str_format absl_strings absl_synchronization absl_time absl_type_traits absl_utility absl_variant gpr grpc_unsecure"
+  "absl_algorithm_container absl_any_invocable absl_base absl_bind_front absl_check absl_cleanup absl_config absl_cord absl_core_headers absl_flags absl_flags_marshalling absl_flat_hash_map absl_flat_hash_set absl_function_ref absl_hash absl_inlined_vector absl_log absl_log_severity absl_memory absl_no_destructor absl_optional absl_random_bit_gen_ref absl_random_distributions absl_random_random absl_span absl_status absl_statusor absl_str_format absl_strings absl_synchronization absl_time absl_type_traits absl_utility absl_variant gpr grpc_unsecure"
   "libcares zlib"
   "-lgrpc++_unsecure"
   "-laddress_sorting -lupb_message_lib -lupb_mem_lib -lupb_base_lib -lutf8_range_lib"

build_autogenerated.yaml
@@ -3937,6 +3937,7 @@ libs:
   - src/cpp/util/string_ref.cc
   - src/cpp/util/time_cc.cc
   deps:
+  - absl/log:check
   - grpc
   - protobuf
   baselib: true
@@ -4310,6 +4311,7 @@ libs:
   - src/cpp/util/string_ref.cc
   - src/cpp/util/time_cc.cc
   deps:
+  - absl/log:check
   - grpc_unsecure
   - protobuf
   baselib: true
@@ -10524,7 +10526,6 @@ targets:
   - test/cpp/util/proto_reflection_descriptor_database.cc
   - test/cpp/util/service_describer.cc
   deps:
-  - absl/log:check
   - grpc++
   - protoc
   - grpc++_test_config

gRPC-C++.podspec
@@ -242,6 +242,7 @@ Pod::Spec.new do |s|
     ss.dependency 'abseil/functional/bind_front', abseil_version
     ss.dependency 'abseil/functional/function_ref', abseil_version
     ss.dependency 'abseil/hash/hash', abseil_version
+    ss.dependency 'abseil/log/check', abseil_version
     ss.dependency 'abseil/log/log', abseil_version
     ss.dependency 'abseil/memory/memory', abseil_version
     ss.dependency 'abseil/meta/type_traits', abseil_version

include/grpcpp/client_context.h
@@ -38,6 +38,8 @@
 #include <memory>
 #include <string>
 
+#include "absl/log/check.h"
+
 #include <grpc/impl/compression_types.h>
 #include <grpc/impl/propagation_bits.h>
 #include <grpc/support/log.h>
@@ -246,7 +248,7 @@ class ClientContext {
   /// \return A multimap of initial metadata key-value pairs from the server.
   const std::multimap<grpc::string_ref, grpc::string_ref>&
   GetServerInitialMetadata() const {
-    GPR_ASSERT(initial_metadata_received_);
+    CHECK(initial_metadata_received_);
     return *recv_initial_metadata_.map();
   }
 

include/grpcpp/completion_queue.h
@@ -34,6 +34,8 @@
 
 #include <list>
 
+#include "absl/log/check.h"
+
 #include <grpc/grpc.h>
 #include <grpc/support/atm.h>
 #include <grpc/support/log.h>
@@ -323,7 +325,7 @@ class CompletionQueue : private grpc::internal::GrpcLibrary {
       bool ok = ev.success != 0;
       void* ignored = tag;
       if (tag->FinalizeResult(&ignored, &ok)) {
-        GPR_ASSERT(ignored == tag);
+        CHECK(ignored == tag);
         return ok;
       }
     }
@@ -344,7 +346,7 @@ class CompletionQueue : private grpc::internal::GrpcLibrary {
     bool ok = ev.success != 0;
     void* ignored = tag;
     // the tag must be swallowed if using TryPluck
-    GPR_ASSERT(!tag->FinalizeResult(&ignored, &ok));
+    CHECK(!tag->FinalizeResult(&ignored, &ok));
   }
 
   /// Performs a single polling pluck on \a tag. Calls tag->FinalizeResult if
@@ -361,7 +363,7 @@ class CompletionQueue : private grpc::internal::GrpcLibrary {
 
     bool ok = ev.success != 0;
     void* ignored = tag;
-    GPR_ASSERT(!tag->FinalizeResult(&ignored, &ok));
+    CHECK(!tag->FinalizeResult(&ignored, &ok));
   }
 
   /// Manage state of avalanching operations : completion queue tags that

include/grpcpp/impl/call_op_set.h
@@ -23,6 +23,8 @@
 #include <map>
 #include <memory>
 
+#include "absl/log/check.h"
+
 #include <grpc/grpc.h>
 #include <grpc/impl/compression_types.h>
 #include <grpc/impl/grpc_types.h>
@@ -316,7 +318,7 @@ class CallOpSendMessage {
       return;
     }
     if (msg_ != nullptr) {
-      GPR_ASSERT(serializer_(msg_).ok());
+      CHECK(serializer_(msg_).ok());
     }
     serializer_ = nullptr;
     grpc_op* op = &ops[(*nops)++];
@@ -795,7 +797,7 @@ class CallOpClientRecvStatus {
     if (recv_status_ == nullptr || hijacked_) return;
     if (static_cast<StatusCode>(status_code_) == StatusCode::OK) {
       *recv_status_ = Status();
-      GPR_DEBUG_ASSERT(debug_error_string_ == nullptr);
+      DCHECK_EQ(debug_error_string_, nullptr);
     } else {
       *recv_status_ =
           Status(static_cast<StatusCode>(status_code_),
@@ -974,7 +976,7 @@ class CallOpSet : public CallOpSetInterface,
       // WritesDone multiple times
       gpr_log(GPR_ERROR, "API misuse of type %s observed",
               grpc_call_error_to_string(err));
-      GPR_ASSERT(false);
+      CHECK(false);
     }
   }
 
@@ -984,8 +986,8 @@ class CallOpSet : public CallOpSetInterface,
     done_intercepting_ = true;
     // The following call_start_batch is internally-generated so no need for an
     // explanatory log on failure.
-    GPR_ASSERT(grpc_call_start_batch(call_.call(), nullptr, 0, core_cq_tag(),
-                                     nullptr) == GRPC_CALL_OK);
+    CHECK(grpc_call_start_batch(call_.call(), nullptr, 0, core_cq_tag(),
+                                nullptr) == GRPC_CALL_OK);
   }
 
  private:

include/grpcpp/impl/interceptor_common.h
@@ -22,6 +22,8 @@
 #include <array>
 #include <functional>
 
+#include "absl/log/check.h"
+
 #include <grpc/impl/grpc_types.h>
 #include <grpc/support/log.h>
 #include <grpcpp/impl/call.h>
@@ -56,16 +58,15 @@ class InterceptorBatchMethodsImpl
     if (call_->client_rpc_info() != nullptr) {
       return ProceedClient();
     }
-    GPR_ASSERT(call_->server_rpc_info() != nullptr);
+    CHECK_NE(call_->server_rpc_info(), nullptr);
     ProceedServer();
   }
 
   void Hijack() override {
     // Only the client can hijack when sending down initial metadata
-    GPR_ASSERT(!reverse_ && ops_ != nullptr &&
-               call_->client_rpc_info() != nullptr);
+    CHECK(!reverse_ && ops_ != nullptr && call_->client_rpc_info() != nullptr);
     // It is illegal to call Hijack twice
-    GPR_ASSERT(!ran_hijacking_interceptor_);
+    CHECK(!ran_hijacking_interceptor_);
     auto* rpc_info = call_->client_rpc_info();
     rpc_info->hijacked_ = true;
     rpc_info->hijacked_interceptor_ = current_interceptor_index_;
@@ -80,21 +81,21 @@ class InterceptorBatchMethodsImpl
   }
 
   ByteBuffer* GetSerializedSendMessage() override {
-    GPR_ASSERT(orig_send_message_ != nullptr);
+    CHECK_NE(orig_send_message_, nullptr);
     if (*orig_send_message_ != nullptr) {
-      GPR_ASSERT(serializer_(*orig_send_message_).ok());
+      CHECK(serializer_(*orig_send_message_).ok());
       *orig_send_message_ = nullptr;
     }
     return send_message_;
   }
 
   const void* GetSendMessage() override {
-    GPR_ASSERT(orig_send_message_ != nullptr);
+    CHECK_NE(orig_send_message_, nullptr);
     return *orig_send_message_;
   }
 
   void ModifySendMessage(const void* message) override {
-    GPR_ASSERT(orig_send_message_ != nullptr);
+    CHECK_NE(orig_send_message_, nullptr);
     *orig_send_message_ = message;
   }
 
@@ -129,7 +130,7 @@ class InterceptorBatchMethodsImpl
   Status* GetRecvStatus() override { return recv_status_; }
 
   void FailHijackedSendMessage() override {
-    GPR_ASSERT(hooks_[static_cast<size_t>(
+    CHECK(hooks_[static_cast<size_t>(
         experimental::InterceptionHookPoints::PRE_SEND_MESSAGE)]);
     *fail_send_message_ = true;
   }
@@ -192,7 +193,7 @@ class InterceptorBatchMethodsImpl
   }
 
   void FailHijackedRecvMessage() override {
-    GPR_ASSERT(hooks_[static_cast<size_t>(
+    CHECK(hooks_[static_cast<size_t>(
         experimental::InterceptionHookPoints::PRE_RECV_MESSAGE)]);
     *hijacked_recv_message_failed_ = true;
   }
@@ -236,7 +237,7 @@ class InterceptorBatchMethodsImpl
   // ContinueFinalizeOpsAfterInterception will be called. Note that neither of
   // them is invoked if there were no interceptors registered.
   bool RunInterceptors() {
-    GPR_ASSERT(ops_);
+    CHECK(ops_);
     auto* client_rpc_info = call_->client_rpc_info();
     if (client_rpc_info != nullptr) {
       if (client_rpc_info->interceptors_.empty()) {
@@ -261,8 +262,8 @@ class InterceptorBatchMethodsImpl
   // SyncRequest.
   bool RunInterceptors(std::function<void(void)> f) {
     // This is used only by the server for initial call request
-    GPR_ASSERT(reverse_ == true);
-    GPR_ASSERT(call_->client_rpc_info() == nullptr);
+    CHECK_EQ(reverse_, true);
+    CHECK_EQ(call_->client_rpc_info(), nullptr);
     auto* server_rpc_info = call_->server_rpc_info();
     if (server_rpc_info == nullptr || server_rpc_info->interceptors_.empty()) {
       return true;
@@ -356,7 +357,7 @@ class InterceptorBatchMethodsImpl
         return ops_->ContinueFinalizeResultAfterInterception();
       }
     }
-    GPR_ASSERT(callback_);
+    CHECK(callback_);
     callback_();
   }
 
@@ -422,112 +423,98 @@ class CancelInterceptorBatchMethods
 
   void Hijack() override {
     // Only the client can hijack when sending down initial metadata
-    GPR_ASSERT(false &&
-               "It is illegal to call Hijack on a method which has a "
-               "Cancel notification");
+    CHECK(false) << "It is illegal to call Hijack on a method which has a "
+                    "Cancel notification";
   }
 
   ByteBuffer* GetSerializedSendMessage() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetSendMessage on a method which "
-               "has a Cancel notification");
+    CHECK(false) << "It is illegal to call GetSendMessage on a method which "
+                    "has a Cancel notification";
     return nullptr;
   }
 
   bool GetSendMessageStatus() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetSendMessageStatus on a method which "
-               "has a Cancel notification");
+    CHECK(false)
+        << "It is illegal to call GetSendMessageStatus on a method which "
+           "has a Cancel notification";
     return false;
   }
 
   const void* GetSendMessage() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetOriginalSendMessage on a method which "
-               "has a Cancel notification");
+    CHECK(false)
+        << "It is illegal to call GetOriginalSendMessage on a method which "
+           "has a Cancel notification";
     return nullptr;
   }
 
   void ModifySendMessage(const void* /*message*/) override {
-    GPR_ASSERT(false &&
-               "It is illegal to call ModifySendMessage on a method which "
-               "has a Cancel notification");
+    CHECK(false) << "It is illegal to call ModifySendMessage on a method which "
+                    "has a Cancel notification";
   }
 
   std::multimap<std::string, std::string>* GetSendInitialMetadata() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetSendInitialMetadata on a "
-               "method which has a Cancel notification");
+    CHECK(false) << "It is illegal to call GetSendInitialMetadata on a "
+                    "method which has a Cancel notification";
     return nullptr;
   }
 
   Status GetSendStatus() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetSendStatus on a method which "
-               "has a Cancel notification");
+    CHECK(false) << "It is illegal to call GetSendStatus on a method which "
+                    "has a Cancel notification";
     return Status();
   }
 
   void ModifySendStatus(const Status& /*status*/) override {
-    GPR_ASSERT(false &&
-               "It is illegal to call ModifySendStatus on a method "
-               "which has a Cancel notification");
+    CHECK(false) << "It is illegal to call ModifySendStatus on a method "
+                    "which has a Cancel notification";
   }
 
   std::multimap<std::string, std::string>* GetSendTrailingMetadata() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetSendTrailingMetadata on a "
-               "method which has a Cancel notification");
+    CHECK(false) << "It is illegal to call GetSendTrailingMetadata on a "
+                    "method which has a Cancel notification";
     return nullptr;
   }
 
   void* GetRecvMessage() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetRecvMessage on a method which "
-               "has a Cancel notification");
+    CHECK(false) << "It is illegal to call GetRecvMessage on a method which "
+                    "has a Cancel notification";
     return nullptr;
   }
 
   std::multimap<grpc::string_ref, grpc::string_ref>* GetRecvInitialMetadata()
       override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetRecvInitialMetadata on a "
-               "method which has a Cancel notification");
+    CHECK(false) << "It is illegal to call GetRecvInitialMetadata on a "
+                    "method which has a Cancel notification";
     return nullptr;
   }
 
   Status* GetRecvStatus() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetRecvStatus on a method which "
-               "has a Cancel notification");
+    CHECK(false) << "It is illegal to call GetRecvStatus on a method which "
+                    "has a Cancel notification";
     return nullptr;
   }
 
   std::multimap<grpc::string_ref, grpc::string_ref>* GetRecvTrailingMetadata()
       override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetRecvTrailingMetadata on a "
-               "method which has a Cancel notification");
+    CHECK(false) << "It is illegal to call GetRecvTrailingMetadata on a "
+                    "method which has a Cancel notification";
     return nullptr;
   }
 
   std::unique_ptr<ChannelInterface> GetInterceptedChannel() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call GetInterceptedChannel on a "
-               "method which has a Cancel notification");
+    CHECK(false) << "It is illegal to call GetInterceptedChannel on a "
+                    "method which has a Cancel notification";
     return std::unique_ptr<ChannelInterface>(nullptr);
   }
 
   void FailHijackedRecvMessage() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call FailHijackedRecvMessage on a "
-               "method which has a Cancel notification");
+    CHECK(false) << "It is illegal to call FailHijackedRecvMessage on a "
+                    "method which has a Cancel notification";
   }
 
   void FailHijackedSendMessage() override {
-    GPR_ASSERT(false &&
-               "It is illegal to call FailHijackedSendMessage on a "
-               "method which has a Cancel notification");
+    CHECK(false) << "It is illegal to call FailHijackedSendMessage on a "
+                    "method which has a Cancel notification";
   }
 };
 }  // namespace internal

commit 091e13d07c06f32e95ad0bf44c921221f9adac0e

[build] fix build breakage from #36452

BUILD
@@ -927,6 +927,7 @@ grpc_cc_library(
         "src/cpp/server/secure_server_credentials.h",
     ],
     external_deps = [
+        "absl/log:check",
         "absl/strings:cord",
     ],
     language = "c++",
@@ -1231,6 +1232,7 @@ grpc_cc_library(
         "src/cpp/server/insecure_server_credentials.cc",
     ],
     external_deps = [
+        "absl/log:check",
         "absl/strings",
         "absl/synchronization",
     ],
@@ -2417,6 +2419,7 @@ grpc_cc_library(
     external_deps = [
         "absl/base:core_headers",
         "absl/functional:any_invocable",
+        "absl/log:check",
         "absl/status",
         "absl/status:statusor",
         "absl/strings",
@@ -2499,6 +2502,7 @@ grpc_cc_library(
     external_deps = [
         "absl/base:core_headers",
         "absl/functional:any_invocable",
+        "absl/log:check",
         "absl/status",
         "absl/status:statusor",
         "absl/strings",

CMakeLists.txt
@@ -5820,7 +5820,6 @@ if(_gRPC_PLATFORM_LINUX OR _gRPC_PLATFORM_MAC OR _gRPC_PLATFORM_POSIX)
 
   target_link_libraries(fd_conservation_posix_test
     ${_gRPC_ALLTARGETS_LIBRARIES}
-    absl::check
     grpc_test_util
   )
 
@@ -6134,7 +6133,6 @@ if(_gRPC_PLATFORM_LINUX OR _gRPC_PLATFORM_MAC OR _gRPC_PLATFORM_POSIX)
   target_link_libraries(address_sorting_test_unsecure
     ${_gRPC_ALLTARGETS_LIBRARIES}
     gtest
-    absl::check
     grpc++_unsecure
     grpc_test_util_unsecure
     grpc++_test_config
@@ -8339,7 +8337,6 @@ target_include_directories(buffer_list_test
 target_link_libraries(buffer_list_test
   ${_gRPC_ALLTARGETS_LIBRARIES}
   gtest
-  absl::check
   grpc_test_util
 )
 
@@ -12873,7 +12870,6 @@ target_include_directories(endpoint_pair_test
 target_link_libraries(endpoint_pair_test
   ${_gRPC_ALLTARGETS_LIBRARIES}
   gtest
-  absl::check
   grpc_test_util
 )
 
@@ -13024,7 +13020,6 @@ target_include_directories(error_test
 target_link_libraries(error_test
   ${_gRPC_ALLTARGETS_LIBRARIES}
   gtest
-  absl::check
   grpc_test_util
 )
 
@@ -23769,7 +23764,6 @@ target_include_directories(resolve_address_using_ares_resolver_test
 target_link_libraries(resolve_address_using_ares_resolver_test
   ${_gRPC_ALLTARGETS_LIBRARIES}
   gtest
-  absl::check
   grpc_test_util
   grpc++_test_config
 )
@@ -23876,7 +23870,6 @@ target_include_directories(resolve_address_using_native_resolver_test
 target_link_libraries(resolve_address_using_native_resolver_test
   ${_gRPC_ALLTARGETS_LIBRARIES}
   gtest
-  absl::check
   grpc_test_util
   grpc++_test_config
 )
@@ -26617,7 +26610,6 @@ target_include_directories(secure_endpoint_test
 target_link_libraries(secure_endpoint_test
   ${_gRPC_ALLTARGETS_LIBRARIES}
   gtest
-  absl::check
   grpc_test_util
 )
 
@@ -28466,7 +28458,6 @@ if(_gRPC_PLATFORM_LINUX OR _gRPC_PLATFORM_MAC OR _gRPC_PLATFORM_POSIX)
   target_link_libraries(socket_utils_test
     ${_gRPC_ALLTARGETS_LIBRARIES}
     gtest
-    absl::check
     grpc_test_util
   )
 
@@ -29608,7 +29599,6 @@ if(_gRPC_PLATFORM_LINUX OR _gRPC_PLATFORM_POSIX)
   target_link_libraries(tcp_posix_test
     ${_gRPC_ALLTARGETS_LIBRARIES}
     gtest
-    absl::check
     grpc_test_util
   )
 
@@ -30396,7 +30386,6 @@ target_include_directories(test_cpp_client_credentials_test
 target_link_libraries(test_cpp_client_credentials_test
   ${_gRPC_ALLTARGETS_LIBRARIES}
   gtest
-  absl::check
   grpc++
   grpc_test_util
 )
@@ -30556,7 +30545,6 @@ target_include_directories(test_cpp_server_credentials_test
 target_link_libraries(test_cpp_server_credentials_test
   ${_gRPC_ALLTARGETS_LIBRARIES}
   gtest
-  absl::check
   grpc++
   grpc_test_util
 )
@@ -30976,7 +30964,6 @@ if(_gRPC_PLATFORM_LINUX OR _gRPC_PLATFORM_MAC OR _gRPC_PLATFORM_POSIX)
   target_link_libraries(time_jump_test
     ${_gRPC_ALLTARGETS_LIBRARIES}
     gtest
-    absl::check
     grpc++
     grpc_test_util
   )
@@ -31220,7 +31207,6 @@ target_include_directories(timer_test
 target_link_libraries(timer_test
   ${_gRPC_ALLTARGETS_LIBRARIES}
   gtest
-  absl::check
   grpc++
   grpc_test_util
 )

build_autogenerated.yaml
@@ -5142,7 +5142,6 @@ targets:
   - test/core/util/slice_splitter.cc
   - test/core/util/tracer_util.cc
   deps:
-  - absl/log:check
   - grpc_test_util
   platforms:
   - linux
@@ -5313,7 +5312,6 @@ targets:
   - test/cpp/util/subprocess.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc++_unsecure
   - grpc_test_util_unsecure
   - grpc++_test_config
@@ -6278,7 +6276,6 @@ targets:
   - test/core/util/tracer_util.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc_test_util
 - name: byte_buffer_test
   gtest: true
@@ -8990,7 +8987,6 @@ targets:
   - test/core/util/tracer_util.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc_test_util
 - name: env_test
   gtest: true
@@ -9048,7 +9044,6 @@ targets:
   - test/core/util/tracer_util.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc_test_util
   uses_polling: false
 - name: error_utils_test
@@ -15316,7 +15311,6 @@ targets:
   - test/core/util/tracer_util.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc_test_util
   - grpc++_test_config
 - name: resolve_address_using_native_resolver_posix_test
@@ -15386,7 +15380,6 @@ targets:
   - test/core/util/tracer_util.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc_test_util
   - grpc++_test_config
 - name: resource_quota_end2end_stress_test
@@ -17767,7 +17760,6 @@ targets:
   - test/core/util/tracer_util.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc_test_util
 - name: security_connector_test
   gtest: true
@@ -18713,7 +18705,6 @@ targets:
   - test/core/util/tracer_util.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc_test_util
   platforms:
   - linux
@@ -19193,7 +19184,6 @@ targets:
   - test/core/util/tracer_util.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc_test_util
   platforms:
   - linux
@@ -19595,7 +19585,6 @@ targets:
   - test/cpp/util/tls_test_utils.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc++
   - grpc_test_util
 - name: test_cpp_end2end_ssl_credentials_test
@@ -19663,7 +19652,6 @@ targets:
   - test/cpp/util/tls_test_utils.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc++
   - grpc_test_util
 - name: test_cpp_util_slice_test
@@ -19795,7 +19783,6 @@ targets:
   - test/cpp/common/time_jump_test.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc++
   - grpc_test_util
   platforms:
@@ -19928,7 +19915,6 @@ targets:
   - test/cpp/common/timer_test.cc
   deps:
   - gtest
-  - absl/log:check
   - grpc++
   - grpc_test_util
 - name: tls_certificate_verifier_test