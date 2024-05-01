453c2e1a3f54e206a3003d0af7d15aecab14bdec
markdroth <18664614+markdroth@users.noreply.github.com>
2024-05-01 23:55:12 +0000

    Automated change: Fix sanity tests

--- tools/buildgen/extract_metadata_from_bazel_xml.py
+++ tools/buildgen/extract_metadata_from_bazel_xml.py
@@ -829,7 +829,8 @@ def _exclude_unwanted_cc_tests(tests: List[str]) -> List[str]:
         for test in tests
         if not test.startswith("test/cpp/ext/filters/census:")
         and not test.startswith(
-            "test/core/server:xds_channel_stack_modifier_test")
+            "test/core/server:xds_channel_stack_modifier_test"
+        )
         and not test.startswith("test/cpp/ext/gcp:")
         and not test.startswith("test/cpp/ext/filters/logging:")
         and not test.startswith("test/cpp/interop:observability_interop")
