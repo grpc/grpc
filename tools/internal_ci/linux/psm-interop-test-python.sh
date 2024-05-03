fa099d780bd7020caa3afe705b61b79781726ad0
Sergii Tkachenko <sergiitk@google.com>
2024-05-03 10:09:07 -0700

    Address the feedback: making the buildscript flow more transparent

--- tools/internal_ci/linux/psm-interop-test-python.sh
+++ tools/internal_ci/linux/psm-interop-test-python.sh
@@ -14,11 +14,12 @@
 # limitations under the License.
 set -eo pipefail
 
-# Constants
+# Input parameters to psm:: methods of the install script.
 readonly GRPC_LANGUAGE="python"
-readonly GITHUB_REPOSITORY_NAME="grpc"
-readonly TEST_DRIVER_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/${TEST_DRIVER_REPO_OWNER:-grpc}/psm-interop/${TEST_DRIVER_BRANCH:-main}/.kokoro/psm_interop_kokoro_lib.sh"
 readonly BUILD_SCRIPT_DIR="$(dirname "$0")"
+
+# Used locally.
+readonly TEST_DRIVER_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/${TEST_DRIVER_REPO_OWNER:-grpc}/psm-interop/${TEST_DRIVER_BRANCH:-main}/.kokoro/psm_interop_kokoro_lib.sh"
 
 psm::lang::source_install_lib() {
   echo "Sourcing test driver install script from: ${TEST_DRIVER_INSTALL_SCRIPT_URL}"
@@ -35,4 +36,5 @@ psm::lang::source_install_lib() {
 }
 
 psm::lang::source_install_lib
+source "${BUILD_SCRIPT_DIR}/psm-interop-build-${GRPC_LANGUAGE}.sh"
 psm::run "${PSM_TEST_SUITE}"
