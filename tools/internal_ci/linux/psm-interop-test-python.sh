6660e1fd17ed6505565e8e06bf6763fec1ecb9d6
Sergii Tkachenko <sergiitk@google.com>
2024-05-03 14:43:39 -0700

    [PSM Interop] Simplify PSM Interop Kokoro buildscripts (#36450)
    Integrates the new features of the the Kokoro PSM Interop install library introduced in grpc/psm-interop#73.
    
    Nearly all common functionality was moved from per-language/per-branch PSM Interop build scripts to [psm_interop_kokoro_lib.sh](https://github.com/grpc/psm-interop/blob/main/.kokoro/psm_interop_kokoro_lib.sh):
    1. The list of tests in the each test suite
    2. Per-test-suite flag customization
    3. `run_test` methods
    4. `build_docker_images_if_needed` methods
    5. Generic `build_test_app_docker_images` methods (simple docker build + docker push + docker tag). grpc-java is one exception, as it doesn't run docker directly, but a cloudbuild flow.
    
    Now all PSM Interop jobs share the same buildscripts by all test suites:
    1.  buildscript that invokes the test: `psm-interop-test-{language}.sh` (configured as `build_file` in the build cfg)
    2. buildscript that builds the xDS test client/server and publishes them as a Docker image: `psm-interop-build-{language}.sh` (conventional name called from `psm_interop_kokoro_lib.sh`)
    
    `psm-interop-test-{language}.sh`:
    1. Sets `GRPC_LANGUAGE`, `BUILD_SCRIPT_DIR` environment variables.
    2. Downloads the shared `psm_interop_kokoro_lib.sh` from the main branch of the psm-interop repo.
    3. Sources `psm-interop-build-{language}.sh`
    4. Calls `psm::run "${PSM_TEST_SUITE}"` (`PSM_TEST_SUITE` configured in the cfg file).
    
    `psm-interop-build-{language}.sh`:
    1. Defines `psm::lang::build_docker_images` which is called from `psm_interop_kokoro_lib.sh`.
    2. Invokes any repo-specific logic.
    3. May use `psm::build::docker_images_generic` for generic Docker build, tag, push, or provide implement its own build/publish method.
    
    References:
    - b/288578634
    - See the full list of the new features at grpc/psm-interop#73.
    - Additional fixes to the shared lib: grpc/psm-interop#78, grpc/psm-interop#79
    
    Closes #36450
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/36450 from sergiitk:psm-interop-common-prod-tests 4b977b7bd84b9cfe9caaf2a706706e6452bcf862
    PiperOrigin-RevId: 630502807

--- tools/internal_ci/linux/psm-interop-test-python.sh
+++ tools/internal_ci/linux/psm-interop-test-python.sh
@@ -1,4 +1,5 @@
-# Copyright 2023 gRPC authors.
+#!/usr/bin/env bash
+# Copyright 2024 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,17 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -eo pipefail
 
-# Config file for the internal CI (in protobuf text format)
+# Input parameters to psm:: methods of the install script.
+readonly GRPC_LANGUAGE="python"
+readonly BUILD_SCRIPT_DIR="$(dirname "$0")"
 
-# Location of the continuous shell script in repository.
-# TODO(sergiitk): delete when the job is renamed
-build_file: "grpc/tools/internal_ci/linux/psm-csm.sh"
-timeout_mins: 60
-action {
-  define_artifacts {
-    regex: "artifacts/**/*sponge_log.xml"
-    regex: "artifacts/**/*.log"
-    strip_prefix: "artifacts"
-  }
-}
+source "${BUILD_SCRIPT_DIR}/psm-interop-install-lib.sh"
+psm::lang::source_install_lib
+source "${BUILD_SCRIPT_DIR}/psm-interop-build-${GRPC_LANGUAGE}.sh"
+psm::run "${PSM_TEST_SUITE}"
