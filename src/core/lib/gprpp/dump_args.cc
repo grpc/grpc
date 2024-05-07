745ce8869a6e82dc4d9cbfb70578f44b37fb08ee
Craig Tiller <ctiller@google.com>
2024-05-07 19:21:53 +0000

    x

---
+++ src/core/lib/gprpp/dump_args.cc
@@ -0,0 +1,54 @@
+// Copyright 2024 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/core/lib/gprpp/dump_args.h"
+
+#include "absl/log/check.h"
+#include "absl/strings/ascii.h"
+#include "absl/strings/string_view.h"
+
+namespace grpc_core {
+namespace dump_args_detail {
+
+std::ostream& operator<<(std::ostream& out, const DumpArgs& args) {
+  // Parse the argument string into a vector of keys.
+  // #__VA_ARGS__ produces a stringified version of the arguments passed to the
+  // macro. It's comma separated, and we can use that to split the string into
+  // keys. Those keys might include parenthesis for e.g. argument lists, and so
+  // we need to skip commas that are inside parenthesis.
+  std::vector<absl::string_view> keys;
+  int depth = 0;
+  const char* start = args.arg_string_;
+  for (const char* p = args.arg_string_; *p; ++p) {
+    if (*p == '(') {
+      ++depth;
+    } else if (*p == ')') {
+      --depth;
+    } else if (*p == ',' && depth == 0) {
+      keys.push_back(absl::string_view(start, p - start));
+      start = p + 1;
+    }
+  }
+  keys.push_back(start);
+  CHECK_EQ(keys.size(), args.arg_dumpers_.size());
+  for (size_t i = 0; i < keys.size(); i++) {
+    if (i != 0) out << ", ";
+    out << absl::StripAsciiWhitespace(keys[i]) << " = ";
+    args.arg_dumpers_[i](out);
+  }
+  return out;
+}
+
+}  // namespace dump_args_detail
+}  // namespace grpc_core
---
+++ src/core/lib/gprpp/dump_args.h
@@ -0,0 +1,73 @@
+// Copyright 2024 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GRPC_DUMP_ARGS_H
+#define GRPC_DUMP_ARGS_H
+
+#include <ostream>
+#include <vector>
+
+#include "absl/functional/any_invocable.h"
+
+namespace grpc_core {
+namespace dump_args_detail {
+
+// Helper function... just ignore the initializer list passed into it.
+// Allows doing 'statements' via parameter pack expansion in C++11 - given
+// template <typename... Ts>:
+//  do_these_things({(foo<Ts>(), 1)});
+// will execute foo<T>() for each T in Ts.
+// In this example we also leverage the comma operator to make the resultant
+// type of each statement be a consistant int so that C++ type deduction works
+// as we'd like (note that in the expression (a, 1) in C++, the 'result' of the
+// expression is the value after the right-most ',' -- in this case 1, with a
+// executed as a side effect.
+template <typename T>
+void do_these_things(std::initializer_list<T>) {}
+
+class DumpArgs {
+ public:
+  template <typename... Args>
+  DumpArgs(const char* arg_string, const Args&... args)
+      : arg_string_(arg_string) {
+    do_these_things(
+        {AddDumper([a = &args](std::ostream& os) { os << *a; })...});
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const DumpArgs& args);
+
+ private:
+  int AddDumper(absl::AnyInvocable<void(std::ostream&) const> dumper) {
+    arg_dumpers_.push_back(std::move(dumper));
+    return 0;
+  }
+
+  const char* arg_string_;
+  std::vector<absl::AnyInvocable<void(std::ostream&) const>> arg_dumpers_;
+};
+
+}  // namespace dump_args_detail
+}  // namespace grpc_core
+
+// Helper to print a list of variables and their values.
+// Usage:
+//   int a = 1;
+//   int b = 2;
+//   LOG(INFO) << GRPC_DUMP_ARGS(a, b)
+// Output:
+//   a = 1, b = 2
+#define GRPC_DUMP_ARGS(...) \
+  grpc_core::dump_args_detail::DumpArgs(#__VA_ARGS__, __VA_ARGS__)
+
+#endif
