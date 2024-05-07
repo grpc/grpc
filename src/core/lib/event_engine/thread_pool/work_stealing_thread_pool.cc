ff3fce35fffca3ed265600b60181ffd34d4cb1db
eugeneo <287917+eugeneo@users.noreply.github.com>
2024-05-07 22:03:05 +0000

    Automated change: Fix sanity tests

--- src/core/lib/event_engine/thread_pool/work_stealing_thread_pool.cc
+++ src/core/lib/event_engine/thread_pool/work_stealing_thread_pool.cc
@@ -38,7 +38,6 @@
 #include "src/core/lib/backoff/backoff.h"
 #include "src/core/lib/event_engine/common_closures.h"
 #include "src/core/lib/event_engine/thread_local.h"
-#include "src/core/lib/event_engine/thread_pool/work_stealing_thread_pool.h"
 #include "src/core/lib/event_engine/trace.h"
 #include "src/core/lib/event_engine/work_queue/basic_work_queue.h"
 #include "src/core/lib/event_engine/work_queue/work_queue.h"
