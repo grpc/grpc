6a81083761f3234fc63fa01e4768737b232869b1
Xuan Wang <xuanwn@google.com>
2024-05-08 19:12:10 +0000

    Fix tests

--- src/python/grpcio_observability/make_grpcio_observability.py
+++ src/python/grpcio_observability/make_grpcio_observability.py
@@ -92,6 +92,7 @@ BAZEL_DEPS = os.path.join(
 # the bazel target to scrape to get list of sources for the build
 BAZEL_DEPS_QUERIES = [
     "//src/core:slice",
+    "//src/core:ref_counted_string"
 ]
 
 
