0cd07cbaaf7422d09d950649a6b2c033ede85dfb
AJ Heller <hork@google.com>
2024-05-10 16:11:50 +0000

    add check mode

--- tools/codegen/core/gen_trace_flags.py
+++ tools/codegen/core/gen_trace_flags.py
@@ -14,12 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import subprocess
 from io import StringIO
 
+import yaml
+from absl import app, flags
 from mako.lookup import TemplateLookup
 from mako.runtime import Context
 from mako.template import Template
-import yaml
+
+_DO_CHECK = flags.DEFINE_bool(
+    "check", default=False, help="Format and compare output using git diff"
+)
 
 tmpl_dir_ = TemplateLookup(directories=["tools/codegen/core/templates/"])
 
@@ -32,14 +38,22 @@ def render_source_file(tmpl_name, trace_flags):
     return buf.getvalue()
 
 
-def main():
+def main(args):
     with open("src/core/lib/debug/trace_flags.yaml") as f:
         trace_flags = yaml.safe_load(f.read())
     with open("src/core/lib/debug/trace.h", "w") as f:
         f.write(render_source_file("trace.h.mako", trace_flags))
     with open("src/core/lib/debug/trace_flags.cc", "w") as f:
         f.write(render_source_file("trace_flags.cc.mako", trace_flags))
+    if _DO_CHECK.value:
+        subprocess.run(["tools/distrib/clang_format_code.sh"], check=True)
+        diff_result = subprocess.run(["git", "diff"], check=True, capture_output=True)
+        if len(diff_result.stdout) > 0 or len(diff_result.stderr) > 0:
+            print(
+                "Trace flags need to be generated. Please run tools/codegen/core/gen_trace_flags.py"
+            )
+            return 11
 
 
 if __name__ == "__main__":
-    main()
+    app.run(main)
