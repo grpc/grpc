d9d0f8e68de1e4bb62ed97054cd38124c7b52838
AJ Heller <hork@google.com>
2024-05-13 21:43:07 +0000

    fix guards

--- src/core/lib/gprpp/glob.h
+++ src/core/lib/gprpp/glob.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GRPC_CORE_LIB_GPRPP_GLOB_H
-#define GRPC_CORE_LIB_GPRPP_GLOB_H
+#ifndef GRPC_SRC_CORE_LIB_GPRPP_GLOB_H
+#define GRPC_SRC_CORE_LIB_GPRPP_GLOB_H
 
 #include "absl/strings/string_view.h"
 
@@ -26,4 +26,4 @@ bool GlobMatch(absl::string_view name, absl::string_view pattern);
 
 }  // namespace grpc_core
 
-#endif  // GRPC_CORE_LIB_GPRPP_GLOB_H
+#endif  // GRPC_SRC_CORE_LIB_GPRPP_GLOB_H
