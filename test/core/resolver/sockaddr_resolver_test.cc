775986890d2430321b9cdfac3cf441742fc9fc97
tanvi-jagtap <tjagtap@google.com>
2024-05-23 04:58:10 +0000

    [grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging - gpr_log

--- test/core/resolver/sockaddr_resolver_test.cc
+++ test/core/resolver/sockaddr_resolver_test.cc
@@ -46,12 +46,12 @@ class ResultHandler : public grpc_core::Resolver::ResultHandler {
 
 static void test_succeeds(grpc_core::ResolverFactory* factory,
                           const char* string) {
-  gpr_log(GPR_DEBUG, "test: '%s' should be valid for '%s'", string,
-          std::string(factory->scheme()).c_str());
+  VLOG(2) << "test: '" << string << "' should be valid for '"
+          << factory->scheme() << "'";
   grpc_core::ExecCtx exec_ctx;
   absl::StatusOr<grpc_core::URI> uri = grpc_core::URI::Parse(string);
   if (!uri.ok()) {
-    gpr_log(GPR_ERROR, "%s", uri.status().ToString().c_str());
+    LOG(ERROR) << uri.status().ToString();
     ASSERT_TRUE(uri.ok());
   }
   grpc_core::ResolverArgs args;
@@ -69,12 +69,12 @@ static void test_succeeds(grpc_core::ResolverFactory* factory,
 
 static void test_fails(grpc_core::ResolverFactory* factory,
                        const char* string) {
-  gpr_log(GPR_DEBUG, "test: '%s' should be invalid for '%s'", string,
-          std::string(factory->scheme()).c_str());
+  VLOG(2) << "test: '" << string << "' should be invalid for '"
+          << factory->scheme() << "'";
   grpc_core::ExecCtx exec_ctx;
   absl::StatusOr<grpc_core::URI> uri = grpc_core::URI::Parse(string);
   if (!uri.ok()) {
-    gpr_log(GPR_ERROR, "%s", uri.status().ToString().c_str());
+    LOG(ERROR) << uri.status().ToString();
     ASSERT_TRUE(uri.ok());
   }
   grpc_core::ResolverArgs args;
