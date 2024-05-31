d26a878e83af91e7b6d828af65936d7e7babb5db
Craig Tiller <ctiller@google.com>
2024-05-31 16:20:48 -0700

    [context] Move backend metric provider to arena based context (#36781)
    Closes #36781
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/36781 from ctiller:ctx7 06f44a31d198a62b22cfe2fa2e92544f342a8d71
    PiperOrigin-RevId: 639185505

--- src/core/lib/channel/context.h
+++ src/core/lib/channel/context.h
@@ -32,10 +32,6 @@ typedef enum {
   /// Holds a pointer to ServiceConfigCallData associated with this call.
   GRPC_CONTEXT_SERVICE_CONFIG_CALL_DATA,
 
-  /// Holds a pointer to BackendMetricProvider associated with this call on
-  /// the server.
-  GRPC_CONTEXT_BACKEND_METRIC_PROVIDER,
-
   GRPC_CONTEXT_COUNT
 } grpc_context_index;
 
