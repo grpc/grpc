6bfbd3cbb02a701a905896d7323ea6530c468840
tanvi-jagtap <tjagtap@google.com>
2024-06-07 06:46:24 +0000

    [grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging - gpr_set_log_function

--- include/grpc/support/log.h
+++ include/grpc/support/log.h
@@ -85,16 +85,6 @@ typedef struct gpr_log_func_args gpr_log_func_args;
 
 typedef void (*gpr_log_func)(gpr_log_func_args* args);
 
-/**
-This was done because about 60% of gRPC files are directly using absl LOG().
-While the other 40% files use gpr_log. gpr_log will either send the logs to absl
-LOG() or if custom log function is set, that will be called. If we dont prevent
-the custom log sink from being used, we will end up in a weird state where half
-the logs go via absl and the other half go via the custom log sink. Hence
-disabling this code. This function will be deleted in a few weeks from
-everywhere. However, multiple cleanup tasks are left before gpr_set_log_function
-is deleted internally.
-*/
 GPRAPI void gpr_set_log_function(gpr_log_func func);
 
 GPRAPI void gpr_assertion_failed(const char* filename, int line,
