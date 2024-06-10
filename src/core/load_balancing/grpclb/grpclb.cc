67f6554452917650d548d6514d8301aee0900728
tanvi-jagtap <tjagtap@google.com>
2024-06-10 10:19:05 +0000

    Replace gpr_should_log with absl::MinLogLevel()

--- src/core/load_balancing/grpclb/grpclb.cc
+++ src/core/load_balancing/grpclb/grpclb.cc
@@ -74,6 +74,7 @@
 #include "absl/functional/function_ref.h"
 #include "absl/log/check.h"
 #include "absl/log/globals.h"
+#include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
