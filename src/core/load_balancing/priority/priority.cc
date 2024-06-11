44d86219028ba2432b97fbf7441d6eec415fc127
tanvi-jagtap <tjagtap@google.com>
2024-06-11 09:16:14 +0000

    Fix build

--- src/core/load_balancing/priority/priority.cc
+++ src/core/load_balancing/priority/priority.cc
@@ -318,10 +318,9 @@ void PriorityLb::ExitIdleLocked() {
   if (current_priority_ != UINT32_MAX) {
     const std::string& child_name = config_->priorities()[current_priority_];
     if (GRPC_TRACE_FLAG_ENABLED(priority_lb)) {
-      LOG(INFO) << "[priority_lb " << this
-                << "] exiting IDLE for current priority %d child "
-                << current_priority_,
-          child_name.c_str() << "";
+      gpr_log(GPR_INFO,
+              "[priority_lb %p] exiting IDLE for current priority %d child %s",
+              this, current_priority_, child_name.c_str());
     }
     children_[child_name]->ExitIdleLocked();
   }
@@ -405,9 +404,8 @@ void PriorityLb::ChoosePriorityLocked() {
     // If the child for the priority does not exist yet, create it.
     const std::string& child_name = config_->priorities()[priority];
     if (GRPC_TRACE_FLAG_ENABLED(priority_lb)) {
-      LOG(INFO) << "[priority_lb " << this << "] trying priority %u, child "
-                << priority,
-          child_name.c_str() << "";
+      gpr_log(GPR_INFO, "[priority_lb %p] trying priority %u, child %s", this,
+              priority, child_name.c_str());
     }
     auto& child = children_[child_name];
     // Create child if needed.
@@ -467,9 +465,8 @@ void PriorityLb::ChoosePriorityLocked() {
     // If the child for the priority does not exist yet, create it.
     const std::string& child_name = config_->priorities()[priority];
     if (GRPC_TRACE_FLAG_ENABLED(priority_lb)) {
-      LOG(INFO) << "[priority_lb " << this << "] trying priority %u, child "
-                << priority,
-          child_name.c_str() << "";
+      gpr_log(GPR_INFO, "[priority_lb %p] trying priority %u, child %s", this,
+              priority, child_name.c_str());
     }
     auto& child = children_[child_name];
     CHECK(child != nullptr);
