05ae475cdb30de1f00a3108eefea0e2db0e9f20a
Isidro Arias <isidroariass@hotmail.es>
2024-06-18 13:15:28 +0200

    run black formatter

--- examples/python/route_guide/route_guide_client.py
+++ examples/python/route_guide/route_guide_client.py
@@ -30,10 +30,12 @@ def make_route_note(message, latitude, longitude):
         location=route_guide_pb2.Point(latitude=latitude, longitude=longitude),
     )
 
+
 def format_point(point):
     # not delegating in point.__str__ because it is an empty string when its
     # values are zero. In addition, it puts a newline between the fields.
-    return 'latitude: %d, longitude: %d' % (point.latitude, point.longitude)
+    return "latitude: %d, longitude: %d" % (point.latitude, point.longitude)
+
 
 def guide_get_one_feature(stub, point):
     feature = stub.GetFeature(point)
@@ -42,7 +44,10 @@ def guide_get_one_feature(stub, point):
         return
 
     if feature.name:
-        print("Feature called %r at %s" % (feature.name, format_point(feature.location)))
+        print(
+            "Feature called %r at %s"
+            % (feature.name, format_point(feature.location))
+        )
     else:
         print("Found no feature at %s" % format_point(feature.location))
 
@@ -64,7 +69,10 @@ def guide_list_features(stub):
     features = stub.ListFeatures(rectangle)
 
     for feature in features:
-        print("Feature called %r at %s" % (feature.name, format_point(feature.location)))
+        print(
+            "Feature called %r at %s"
+            % (feature.name, format_point(feature.location))
+        )
 
 
 def generate_route(feature_list):
@@ -102,7 +110,8 @@ def guide_route_chat(stub):
     responses = stub.RouteChat(generate_messages())
     for response in responses:
         print(
-            "Received message %s at %s" % (response.message, format_point(response.location))
+            "Received message %s at %s"
+            % (response.message, format_point(response.location))
         )
 
 
