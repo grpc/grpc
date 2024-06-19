27e88b219bb6d3542813612310fa1fdbf783f122
tanvi-jagtap <tjagtap@google.com>
2024-06-19 12:36:18 +0000

    [grpc][Gpr_To_Absl_Logging] Migrating from gpr to absl logging - gpr_log

--- src/core/util/posix/cpu.cc
+++ src/core/util/posix/cpu.cc
@@ -39,7 +39,7 @@ static pthread_key_t thread_id_key;
 static void init_ncpus() {
   ncpus = sysconf(_SC_NPROCESSORS_CONF);
   if (ncpus < 1 || ncpus > INT32_MAX) {
-    gpr_log(GPR_ERROR, "Cannot determine number of CPUs: assuming 1");
+    LOG(ERROR) << "Cannot determine number of CPUs: assuming 1";
     ncpus = 1;
   }
 }
