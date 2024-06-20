3cab34a2f69ada52cd676594f2cb9c315782f41b
Yash Tibrewal <yashkt@google.com>
2024-06-20 09:29:18 -0700

    [log] Do not set absl min log level if user does not set GRPC_VERBOSITY (#36931) (#36957)
    Backport #36931 to 1.65.x

--- src/core/util/log.cc
+++ src/core/util/log.cc
@@ -35,10 +35,6 @@
 #include "src/core/lib/gprpp/crash.h"
 #include "src/core/util/string.h"
 
-#ifndef GPR_DEFAULT_LOG_VERBOSITY_STRING
-#define GPR_DEFAULT_LOG_VERBOSITY_STRING "ERROR"
-#endif  // !GPR_DEFAULT_LOG_VERBOSITY_STRING
-
 static constexpr gpr_atm GPR_LOG_SEVERITY_UNSET = GPR_LOG_SEVERITY_ERROR + 10;
 static constexpr gpr_atm GPR_LOG_SEVERITY_NONE = GPR_LOG_SEVERITY_ERROR + 11;
 
@@ -150,6 +146,9 @@ void gpr_to_absl_verbosity_setting_init(void) {
   } else if (absl::EqualsIgnoreCase(verbosity, "NONE")) {
     absl::SetVLogLevel("*grpc*/*", -1);
     absl::SetMinLogLevel(absl::LogSeverityAtLeast::kInfinity);
+  } else if (verbosity.empty()) {
+    // Do not set absl::MinLogLevel if verbosity has not been set. Note that the
+    // default gRPC min log severity that is printed will still be ERROR.
   } else {
     LOG(ERROR) << "Unknown log verbosity: " << verbosity;
   }
