505d78eb06f0c170fd6f7b9b7fb1728426e5b8cc
Craig Tiller <ctiller@google.com>
2024-06-27 14:23:38 -0700

    x

--- src/core/util/useful.h
+++ src/core/util/useful.h
@@ -21,10 +21,9 @@
 
 #include <grpc/support/port_platform.h>
 
-#include "absl/log/check.h"
-
 #include <cstddef>
 
+#include "absl/log/check.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/variant.h"
 
