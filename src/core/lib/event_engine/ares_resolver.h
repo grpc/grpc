b773098bb8751a17f286fef7bc7955abda71d975
alto-ruby <altorubys@gmail.com>
2024-07-10 17:44:07 -0700

    Revert "[Gpr_To_Absl_Logging] Migrating from gpr to absl logging - gpr_log (#37147)"
    This reverts commit 49f945e2bd89c2a30503440fc0855672d1f20af4.

--- src/core/lib/event_engine/ares_resolver.h
+++ src/core/lib/event_engine/ares_resolver.h
@@ -47,12 +47,11 @@
 namespace grpc_event_engine {
 namespace experimental {
 
-#define GRPC_ARES_RESOLVER_TRACE_LOG(format, ...)        \
-  do {                                                   \
-    if (GRPC_TRACE_FLAG_ENABLED(cares_resolver)) {       \
-      LOG(INFO) << "(EventEngine c-ares resolver) "      \
-                << absl::StrFormat(format, __VA_ARGS__); \
-    }                                                    \
+#define GRPC_ARES_RESOLVER_TRACE_LOG(format, ...)                              \
+  do {                                                                         \
+    if (GRPC_TRACE_FLAG_ENABLED(cares_resolver)) {                             \
+      gpr_log(GPR_INFO, "(EventEngine c-ares resolver) " format, __VA_ARGS__); \
+    }                                                                          \
   } while (0)
 
 class AresResolver : public RefCountedDNSResolverInterface {
