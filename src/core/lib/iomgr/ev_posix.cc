9187e0f1538eac80a9ef17a48b9a7d0925db568b
tanvi-jagtap <tjagtap@google.com>
2024-07-16 06:28:40 +0000

    [Gpr_To_Absl_Logging] Fixing tests

--- src/core/lib/iomgr/ev_posix.cc
+++ src/core/lib/iomgr/ev_posix.cc
@@ -104,7 +104,7 @@ static void try_engine(absl::string_view engine) {
     if (g_vtables[i] != nullptr && is(engine, g_vtables[i]->name) &&
         g_vtables[i]->check_engine_available(engine == g_vtables[i]->name)) {
       g_event_engine = g_vtables[i];
-      GRPC_TRACE_VLOG(2, polling_api)
+      GRPC_TRACE_VLOG(polling_api, 2)
           << "Using polling engine: " << g_event_engine->name;
       return;
     }
