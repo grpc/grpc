e804d20c065b0576556a309ee48187cf9cfc4ea7
Esun Kim <veblush@google.com>
2024-07-19 11:05:29 -0700

    Fix SC2199

--- tools/run_tests/helper_scripts/build_cxx.sh
+++ tools/run_tests/helper_scripts/build_cxx.sh
@@ -46,7 +46,7 @@ else
 cmake -DgRPC_BUILD_GRPCPP_OTEL_PLUGIN=ON -DgRPC_ABSL_PROVIDER=package -DgRPC_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE="${MSBUILD_CONFIG}" -DCMAKE_INSTALL_PREFIX="${INSTALL_PATH}" "$@" ../..
 fi
 
-if [[ "$@" =~ "-DgRPC_BUILD_TESTS=OFF" ]]; then
+if [[ "$*" =~ "-DgRPC_BUILD_TESTS=OFF" ]]; then
 # Just build grpc++ target when gRPC_BUILD_TESTS is OFF (This is a temporary mitigation for gcc 7. Remove this once gcc 7 is removed from the supported compilers)
 make -j"${GRPC_RUN_TESTS_JOBS}" "grpc++"
 else
