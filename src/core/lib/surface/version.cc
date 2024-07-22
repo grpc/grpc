d835f8a7108d38509c6128ac6d4e828f559e8032
AJ Heller <hork@google.com>
2024-07-22 15:58:38 -0700

    [Release] Bump version to 1.67.0-dev (on master branch) (#37279)
    Change was created by the release automation script. See go/grpc-release.
    
    Closes #37279
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/37279 from drfloob:bump_dev_version_202407222027 4e6607411e5a5f37da7d68d7226e627a28683304
    PiperOrigin-RevId: 654925894

--- src/core/lib/surface/version.cc
+++ src/core/lib/surface/version.cc
@@ -24,4 +24,4 @@
 
 const char* grpc_version_string(void) { return "43.0.0"; }
 
-const char* grpc_g_stands_for(void) { return "gladiator"; }
+const char* grpc_g_stands_for(void) { return "gesundheit"; }
