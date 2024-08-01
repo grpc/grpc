52acd88bf4d4c4942eeac843b0f11ae387e3f23a
Esun Kim <veblush@google.com>
2024-08-01 16:36:01 -0700

    regenerate projects

--- _metadata.py
+++ _metadata.py
@@ -14,4 +14,4 @@
 
 # AUTO-GENERATED FROM `$REPO_ROOT/templates/_metadata.py.template`!!!
 
-__version__ = """1.65.3"""
+__version__ = """1.65.4"""
--- src/ruby/tools/version.rb
+++ src/ruby/tools/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module Tools
-    VERSION = '1.65.3'
+    VERSION = '1.65.4'
   end
 end
