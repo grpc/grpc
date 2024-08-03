6da987080c17d5266c8eb6f4ce3a419e4f9f7268
tanvi-jagtap <tjagtap@google.com>
2024-08-03 12:58:05 +0000

    [Gpr_To_Absl_Logging]

--- src/core/ext/transport/chttp2/transport/frame_ping.cc
+++ src/core/ext/transport/chttp2/transport/frame_ping.cc
@@ -110,9 +110,10 @@ grpc_error_handle grpc_chttp2_ping_parser_parse(void* parser,
         if (t->ping_abuse_policy.ReceivedOnePing(transport_idle)) {
           grpc_chttp2_exceeded_ping_strikes(t);
         }
-      } else
+      } else {
         GRPC_TRACE_LOG(http2_ping, INFO)
             << "CLIENT[" << t << "]: received ping " << p->opaque_8bytes;
+      }
       if (t->ack_pings) {
         if (t->ping_ack_count == t->ping_ack_capacity) {
           t->ping_ack_capacity =
