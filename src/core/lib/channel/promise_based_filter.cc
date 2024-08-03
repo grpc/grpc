d08fb32f414b70a6cf7bec2ac1e3595bbc4ff1a2
tanvi-jagtap <tjagtap@google.com>
2024-08-03 13:01:27 +0000

    [Gpr_To_Absl_Logging]

--- src/core/lib/channel/promise_based_filter.cc
+++ src/core/lib/channel/promise_based_filter.cc
@@ -2378,9 +2378,10 @@ void ServerCallData::WakeInsideCombiner(Flusher* flusher) {
         GRPC_TRACE_LOG(channel, INFO)
             << LogTag() << ": WakeInsideCombiner: metadata_push done";
         send_initial_metadata_->metadata_push_.reset();
-      } else
+      } else {
         GRPC_TRACE_LOG(channel, INFO)
             << LogTag() << ": WakeInsideCombiner: metadata_push pending";
+      }
     }
   }
   if (send_message() != nullptr) {
