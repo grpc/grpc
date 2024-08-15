9c6547e9ba771a1e2e10f71e80cc81884620c957
tanvi-jagtap <tjagtap@google.com>
2024-08-15 05:22:47 +0000

    [Gpr_To_Absl_Logging] Remove logging header from src/core/lib folders

--- src/core/lib/security/security_connector/fake/fake_security_connector.cc
+++ src/core/lib/security/security_connector/fake/fake_security_connector.cc
@@ -35,7 +35,6 @@
 #include <grpc/grpc_security_constants.h>
 #include <grpc/impl/channel_arg_names.h>
 #include <grpc/support/alloc.h>
-#include <grpc/support/log.h>
 #include <grpc/support/port_platform.h>
 #include <grpc/support/string_util.h>
 
--- src/core/lib/slice/percent_encoding.cc
+++ src/core/lib/slice/percent_encoding.cc
@@ -25,7 +25,6 @@
 
 #include "absl/log/check.h"
 
-#include <grpc/support/log.h>
 #include <grpc/support/port_platform.h>
 
 #include "src/core/lib/gprpp/bitset.h"
