f9fdab56571feee205c591ffd4da62f3238f4e52
Eugene Ostroukhov <eostroukhov@google.com>
2024-08-22 09:51:33 -0700

    [ci] Update alpine in 2 more images (#37539)
    Closes #37539
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/37539 from eugeneo:alpine-update dc230dcc36a4a58755e783586455f8f7598c6e66
    PiperOrigin-RevId: 666388258

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -885,7 +885,7 @@ class PythonLanguage(object):
         elif args.compiler == "pypy3":
             return (pypy32_config,)
         elif args.compiler == "python_alpine":
-            return (python39_config,)
+            return (python310_config,)
         elif args.compiler == "all_the_cpythons":
             return (
                 python38_config,
