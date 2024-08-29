cbe809f28d953dbd2a3dc34bd3ca877fe2af524f
Mark D. Roth <roth@google.com>
2024-08-29 16:37:43 +0000

    remove include that causes build breakage for some reason

--- src/core/util/glob.cc
+++ src/core/util/glob.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "src/core/util/glob.h"
-
 #include "absl/strings/string_view.h"
 
 namespace grpc_core {
