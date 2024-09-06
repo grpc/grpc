89b16d405a4be484d3f5ba5b376fa2bed23fe4d6
Sourabh Singh <sourabhsinghs@google.com>
2024-09-06 09:48:22 +0000

    updated test config

--- tools/internal_ci/linux/grpc_distribtests_python.sh
+++ tools/internal_ci/linux/grpc_distribtests_python.sh
@@ -14,13 +14,52 @@
 # limitations under the License.
 
 set -ex
-# Enter the gRPC repo root
+
+# avoid slow finalization after the script has exited.
+source $(dirname $0)/../../../tools/internal_ci/helper_scripts/move_src_tree_and_respawn_itself_rc
+
+# change to grpc repo root
 cd $(dirname $0)/../../..
 
 source tools/internal_ci/helper_scripts/prepare_build_linux_rc
 
-export DOCKERFILE_DIR=tools/dockerfile/distribtest/python_dev_ubuntu2204_x64
-export DOCKER_RUN_SCRIPT=tools/internal_ci/linux/grpc_distribtests_python_in_docker.sh
-export GRPC_TEST_REPORT_BASE_DIR=reports
+# some distribtests use a pre-registered binfmt_misc hook
+# to automatically execute foreign binaries (such as aarch64)
+# under qemu emulator.
+source tools/internal_ci/helper_scripts/prepare_qemu_rc
 
-exec tools/run_tests/dockerize/build_and_run_docker.sh
+# configure ccache
+source tools/internal_ci/helper_scripts/prepare_ccache_rc
+
+# Build all python linux artifacts (this step actually builds all the binary wheels and source archives)
+tools/run_tests/task_runner.py -f artifact linux python ${TASK_RUNNER_EXTRA_FILTERS} -j 12 -x build_artifacts/sponge_log.xml || FAILED="true"
+
+# the next step expects to find the artifacts from the previous step in the "input_artifacts" folder.
+rm -rf input_artifacts
+mkdir -p input_artifacts
+cp -r artifacts/* input_artifacts/ || true
+
+# This step simply collects python artifacts from subdirectories of input_artifacts/ and copies them to artifacts/
+tools/run_tests/task_runner.py -f package linux python -x build_packages/sponge_log.xml || FAILED="true"
+
+# the next step expects to find the artifacts from the previous step in the "input_artifacts" folder.
+# in addition to that, preserve the contents of "artifacts" directory since we want kokoro
+# to upload its contents as job output artifacts
+rm -rf input_artifacts
+mkdir -p input_artifacts
+cp -r artifacts/* input_artifacts/ || true
+
+# Run all python linux distribtests
+# We run the distribtests even if some of the artifacts have failed to build, since that gives
+# a better signal about which distribtest are affected by the currently broken artifact builds.
+tools/run_tests/task_runner.py -f distribtest linux python ${TASK_RUNNER_EXTRA_FILTERS} -j 12 -x distribtests/sponge_log.xml || FAILED="true"
+
+# This step checks if any of the artifacts exceeds a per-file size limit.
+tools/internal_ci/helper_scripts/check_python_artifacts_size.sh
+
+tools/internal_ci/helper_scripts/store_artifacts_from_moved_src_tree.sh
+
+if [ "$FAILED" != "" ]
+then
+  exit 1
+fi
--- tools/internal_ci/linux/grpc_unittests_python.sh
+++ tools/internal_ci/linux/grpc_unittests_python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021 The gRPC Authors
+# Copyright 2024 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,65 +14,13 @@
 # limitations under the License.
 
 set -ex
-
-# avoid slow finalization after the script has exited.
-source $(dirname $0)/../../../tools/internal_ci/helper_scripts/move_src_tree_and_respawn_itself_rc
-
-# change to grpc repo root
+# Enter the gRPC repo root
 cd $(dirname $0)/../../..
 
-# source tools/internal_ci/helper_scripts/prepare_build_linux_rc
+source tools/internal_ci/helper_scripts/prepare_build_linux_rc
 
-# some distribtests use a pre-registered binfmt_misc hook
-# to automatically execute foreign binaries (such as aarch64)
-# under qemu emulator.
-# source tools/internal_ci/helper_scripts/prepare_qemu_rc
+export DOCKERFILE_DIR=tools/dockerfile/distribtest/python_dev_ubuntu2204_x64
+export DOCKER_RUN_SCRIPT=tools/internal_ci/linux/grpc_unittests_python_in_docker.sh
+export GRPC_TEST_REPORT_BASE_DIR=reports
 
-# configure ccache
-source tools/internal_ci/helper_scripts/prepare_ccache_rc
-
-# unit-tests setup starts from here
-
-function maybe_run_command () {
-  if python setup.py --help-commands | grep "$1" &>/dev/null; then
-    python setup.py "$1";
-  fi
-}
-
-BASEDIR=$(dirname "$0")
-
-PACKAGES="grpcio_channelz  grpcio_csds  grpcio_admin grpcio_health_checking  grpcio_reflection  grpcio_status  grpcio_testing grpcio_csm_observability grpcio_tests"
-
-echo $BASEDIR
-echo $PACKAGES
-
-cd "$BASEDIR";
-pip install --upgrade "cython<3.0.0rc1";
-python setup.py install;
-pushd tools/distrib/python/grpcio_tools;
-  ../make_grpcio_tools.py
-  GRPC_PYTHON_BUILD_WITH_CYTHON=1 pip install .
-popd;
-pushd src/python/grpcio_observability;
-  ./make_grpcio_observability.py
-  GRPC_PYTHON_BUILD_WITH_CYTHON=1 pip install .
-popd;
-pushd tools/distrib/python/xds_protos;
-  GRPC_PYTHON_BUILD_WITH_CYTHON=1 pip install .
-popd;
-pushd src/python;
-  for PACKAGE in ${PACKAGES}; do
-    pushd "${PACKAGE}";
-      python setup.py clean;
-      maybe_run_command preprocess
-      maybe_run_command build_package_protos
-      python -m pip install .;
-    popd;
-  done
-popd;
-pushd src/python/grpcio_tests;
-  python setup.py test_lite
-#  python setup.py test_aio
-  python setup.py test_py3_only
-popd;
-
+exec tools/run_tests/dockerize/build_and_run_docker.sh
