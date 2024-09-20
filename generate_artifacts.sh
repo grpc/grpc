b050feff4ca50bfe4e62a8c438771512814b1872
Craig Tiller <ctiller@google.com>
2024-09-20 12:57:02 -0700

    x

--- generate_artifacts.sh
+++ generate_artifacts.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set -ex
 
 cd $(dirname "$0")
 
--- tools/buildgen/generate_projects.sh
+++ tools/buildgen/generate_projects.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 
-set -e
+set -ex
 
 export TEST=${TEST:-false}
 
