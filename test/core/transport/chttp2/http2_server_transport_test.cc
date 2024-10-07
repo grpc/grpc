0c0838d2ea3fc53aeb3e7ca64c30f67b52742ca7
tanvi-jagtap <tjagtap@google.com>
2024-10-07 09:25:43 +0000

    [PH2][NewFile][ClassStructure]

--- test/core/transport/chttp2/http2_server_transport_test.cc
+++ test/core/transport/chttp2/http2_server_transport_test.cc
@@ -20,7 +20,6 @@
 
 #include <gtest/gtest.h>
 
-#include "gtest/gtest.h"
 #include "src/core/lib/event_engine/default_event_engine.h"
 #include "test/core/transport/chaotic_good/mock_promise_endpoint.h"
 #include "test/core/transport/chaotic_good/transport_test.h"
