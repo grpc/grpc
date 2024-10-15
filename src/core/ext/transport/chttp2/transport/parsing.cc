96f995b4e3dffffd30a69b5ebfe5e13a11d4a9dd
Craig Tiller <ctiller@google.com>
2024-10-15 11:16:29 -0700

    x

--- src/core/ext/transport/chttp2/transport/parsing.cc
+++ src/core/ext/transport/chttp2/transport/parsing.cc
@@ -642,7 +642,7 @@ static grpc_error_handle init_header_frame_parser(grpc_chttp2_transport* t,
                                           GRPC_HTTP2_REFUSED_STREAM, nullptr));
       grpc_chttp2_initiate_write(t, GRPC_CHTTP2_INITIATE_WRITE_RST_STREAM);
       return init_header_skip_frame_parser(t, priority_type, is_eoh);
-    } else if (IsRqFastRejectEnabled() && GPR_UNLIKELY(t->memory_owner.IsMemoryPressureHigh())) {
+    } else if (grpc_core::IsRqFastRejectEnabled() && GPR_UNLIKELY(t->memory_owner.IsMemoryPressureHigh())) {
       // We have more streams allocated than we'd like, so apply some pushback
       // by refusing this stream.
       global_stats().IncrementRqCallsRejected();
--- src/core/lib/resource_quota/memory_quota.h
+++ src/core/lib/resource_quota/memory_quota.h
@@ -530,6 +530,13 @@ class MemoryOwner final : public MemoryAllocator {
   // Is this object valid (ie has not been moved out of or reset)
   bool is_valid() const { return impl() != nullptr; }
 
+  // Return true if the controlled memory pressure is high.
+  bool IsMemoryPressureHigh() const {
+    static constexpr double kMemoryPressureHighThreshold = 0.99;
+    return GetPressureInfo().pressure_control_value >
+           kMemoryPressureHighThreshold;
+  }
+
  private:
   const GrpcMemoryAllocatorImpl* impl() const {
     return static_cast<const GrpcMemoryAllocatorImpl*>(get_internal_impl_ptr());
@@ -563,11 +570,8 @@ class MemoryQuota final
   // Resize the quota to new_size.
   void SetSize(size_t new_size) { memory_quota_->SetSize(new_size); }
 
-  // Return true if the controlled memory pressure is high.
   bool IsMemoryPressureHigh() const {
-    static constexpr double kMemoryPressureHighThreshold = 0.99;
-    return memory_quota_->GetPressureInfo().pressure_control_value >
-           kMemoryPressureHighThreshold;
+    return memory_quota_->IsMemoryPressureHigh();
   }
 
  private:
