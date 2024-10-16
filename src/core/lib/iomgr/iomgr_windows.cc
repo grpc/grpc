214c4ada0319b8b1f775e4c10ea65787787c3e8d
AJ Heller <hork@google.com>
2024-10-16 16:15:12 +0000

    fix windows

--- src/core/lib/iomgr/iomgr_windows.cc
+++ src/core/lib/iomgr/iomgr_windows.cc
@@ -107,6 +107,8 @@ void grpc_set_default_iomgr_platform() {
   grpc_set_iomgr_platform_vtable(&vtable);
 }
 
-bool grpc_iomgr_run_in_background() { return false; }
+bool grpc_iomgr_run_in_background() {
+  return grpc_core::IsEventEngineCallbackCqEnabled();
+}
 
 #endif  // GRPC_WINSOCK_SOCKET
