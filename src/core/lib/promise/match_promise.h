68ebc676ac711dbecae1a5f4929d8b1e0095992c
ctiller <10120821+ctiller@users.noreply.github.com>
2024-10-23 17:35:46 +0000

    Automated change: Fix sanity tests

--- src/core/lib/promise/match_promise.h
+++ src/core/lib/promise/match_promise.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GRPC_SRC_CORE_LIB_PROMISE_MATCH_H
-#define GRPC_SRC_CORE_LIB_PROMISE_MATCH_H
+#ifndef GRPC_SRC_CORE_LIB_PROMISE_MATCH_PROMISE_H
+#define GRPC_SRC_CORE_LIB_PROMISE_MATCH_PROMISE_H
 
 #include "absl/types/variant.h"
 #include "src/core/lib/promise/detail/promise_factory.h"
@@ -50,8 +50,9 @@ struct ConstructPromiseVariantVisitor {
   // the result into a variant type that covers ALL of the possible return types
   // given the input types listed in Ts...
   template <typename T>
-  auto operator()(T x) -> absl::variant<promise_detail::PromiseLike<
-      decltype(CallConstructorThenFactory(std::declval<Ts>()))>...> {
+  auto operator()(T x)
+      -> absl::variant<promise_detail::PromiseLike<
+          decltype(CallConstructorThenFactory(std::declval<Ts>()))>...> {
     return CallConstructorThenFactory(x);
   }
 };
@@ -101,4 +102,4 @@ auto MatchPromise(absl::variant<Ts...> value, Fs... fs) {
 
 }  // namespace grpc_core
 
-#endif  // GRPC_SRC_CORE_LIB_PROMISE_MATCH_H
+#endif  // GRPC_SRC_CORE_LIB_PROMISE_MATCH_PROMISE_H
