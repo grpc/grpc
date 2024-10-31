dcf235e96f62e8623a1b333cbfb8171e3560151c
Craig Tiller <ctiller@google.com>
2024-10-31 00:01:30 -0700

    x

--- src/core/lib/promise/try_seq.h
+++ src/core/lib/promise/try_seq.h
@@ -53,18 +53,6 @@ struct TrySeqTraitsWithSfinae {
   static R ReturnValue(T&&) {
     abort();
   }
-  template <typename F, typename Elem>
-  GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION static auto CallSeqFactory(F& f,
-                                                                  Elem&& elem,
-                                                                  T&& value)
-      -> decltype(f(std::forward<Elem>(elem), std::forward<T>(value))) {
-    auto factory = [&f, elem = std::forward<Elem>(elem),
-                    value = std::forward<T>(value)]() mutable {
-      return f(std::move(elem), std::move(value));
-    };
-    OncePromiseFactory<void, decltype(factory)> pf(std::move(factory));
-    return pf.Make();
-  }
   template <typename Result, typename RunNext>
   GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION static Poll<Result>
   CheckResultAndRunNext(T prior, RunNext run_next) {
@@ -92,12 +80,6 @@ struct TrySeqTraitsWithSfinae<absl::StatusOr<T>> {
   GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION static R ReturnValue(
       absl::StatusOr<T>&& status) {
     return FailureStatusCast<R>(status.status());
-  }
-  template <typename F, typename Elem>
-  GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION static auto CallSeqFactory(
-      F& f, Elem&& elem, absl::StatusOr<T> value)
-      -> decltype(f(std::forward<Elem>(elem), std::move(*value))) {
-    return f(std::forward<Elem>(elem), std::move(*value));
   }
   template <typename Result, typename RunNext>
   GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION static Poll<Result>
@@ -246,31 +228,13 @@ class TrySeq {
   SeqState<TrySeqTraits, P, Fs...> state_;
 };
 
-template <typename I, typename F, typename Arg>
-struct TrySeqIterTraits {
-  using Iter = I;
-  using Factory = F;
-  using Argument = Arg;
-  using IterValue = decltype(*std::declval<Iter>());
-  using StateCreated = decltype(std::declval<F>()(std::declval<IterValue>(),
-                                                  std::declval<Arg>()));
-  using State = PromiseLike<StateCreated>;
-  using Wrapped = typename State::Result;
-
-  using Traits = TrySeqTraits<Wrapped>;
-};
-
 template <typename Iter, typename Factory, typename Argument>
-struct TrySeqIterResultTraits {
-  using IterTraits = TrySeqIterTraits<Iter, Factory, Argument>;
-  using Result = BasicSeqIter<IterTraits>;
-};
+using TrySeqIter = BasicSeqIter<TrySeqTraits, Iter, Factory, Argument>;
 
 template <typename Container, typename Factory, typename Argument>
 struct TrySeqContainerResultTraits {
   using BaseResult =
-      typename TrySeqIterResultTraits<typename Container::iterator, Factory,
-                                      Argument>::Result;
+      TrySeqIter<typename Container::iterator, Factory, Argument>;
   class Result {
    public:
     Result(Container container, Factory factory, Argument argument)
@@ -281,6 +245,8 @@ struct TrySeqContainerResultTraits {
     Result& operator=(const Result&) = delete;
     Result(Result&&) = default;
     Result& operator=(Result&&) = default;
+
+    auto operator()() { return base_result_(); }
 
    private:
     Container container_;
@@ -379,14 +345,11 @@ GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION
 //   }
 //   return argument;
 template <typename Iter, typename Factory, typename Argument>
-GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION
-    typename promise_detail::TrySeqIterResultTraits<Iter, Factory,
-                                                    Argument>::Result
-    TrySeqIter(Iter begin, Iter end, Argument argument, Factory factory) {
-  using Result =
-      typename promise_detail::TrySeqIterResultTraits<Iter, Factory,
-                                                      Argument>::Result;
-  return Result(begin, end, std::move(factory), std::move(argument));
+GPR_ATTRIBUTE_ALWAYS_INLINE_FUNCTION auto TrySeqIter(Iter begin, Iter end,
+                                                     Argument argument,
+                                                     Factory factory) {
+  return promise_detail::TrySeqIter<Iter, Factory, Argument>(
+      begin, end, std::move(factory), std::move(argument));
 }
 
 template <typename Container, typename Factory, typename Argument>
