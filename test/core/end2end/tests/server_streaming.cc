1a4e9fb5a3780cd1cec5f0d05444d05e507617fe
Yash Tibrewal <yashkt@google.com>
2024-11-01 22:40:55 +0000

    Sanitize

--- test/core/end2end/tests/server_streaming.cc
+++ test/core/end2end/tests/server_streaming.cc
@@ -104,12 +104,12 @@ CORE_END2END_TEST(Http2Test, ServerStreamingEmptyStream) {
 
 CORE_END2END_TEST(Http2Test, ServerStreaming10Messages) {
   // TODO(yashykt): Remove this once b/376283636 is fixed.
-  grpc_core::ConfigVars::Overrides overrides;
+  ConfigVars::Overrides overrides;
   overrides.default_ssl_roots_file_path = CA_CERT_PATH;
   overrides.trace =
       "call,channel,client_channel,client_channel_call,client_channel_lb_call,"
       "http";
-  grpc_core::ConfigVars::SetOverrides(overrides);
+  ConfigVars::SetOverrides(overrides);
   ServerStreaming(*this, 10);
 }
 
