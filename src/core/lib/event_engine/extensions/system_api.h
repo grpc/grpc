fc0bdf2a322f69f66730632b64c85aa3274677d6
Yevgen Ostroukhov <eostroukhov@google.com>
2024-11-10 16:23:39 -0800

    fixup: Use general headers

--- src/core/lib/event_engine/extensions/system_api.h
+++ src/core/lib/event_engine/extensions/system_api.h
@@ -15,7 +15,8 @@
 #ifndef GRPC_SRC_CORE_LIB_EVENT_ENGINE_EXTENSIONS_SYSTEM_API_H
 #define GRPC_SRC_CORE_LIB_EVENT_ENGINE_EXTENSIONS_SYSTEM_API_H
 
-#include <sys/socket.h>
+#include <grpc/event_engine/event_engine.h>
+#include <grpc/support/port_platform.h>
 
 namespace grpc_event_engine {
 namespace experimental {
