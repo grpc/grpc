e5a41a7a5d2ee9023ee4177e36c831784c8f2b6b
Craig Tiller <ctiller@google.com>
2024-11-13 13:36:01 -0800

    x

--- src/core/ext/transport/chaotic_good/server_transport.cc
+++ src/core/ext/transport/chaotic_good/server_transport.cc
@@ -229,8 +229,7 @@ auto ChaoticGoodServerTransport::ReadOneFrame(
       "ReadOneFrame",
       TrySeq(
           transport->ReadFrameBytes(),
-          [this, transport = std::move(transport)](
-              IncomingFrame incoming_frame) mutable {
+          [this, transport](IncomingFrame incoming_frame) mutable {
             // CHECK_EQ(header.payload_length, payload.Length());
             return Switch(
                 incoming_frame.header().type,
