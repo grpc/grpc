7b3992eac49497123c75cbd84d9f656622864bb9
Yash Tibrewal <yashkt@google.com>
2024-11-17 22:26:15 +0000

    Move types to header for testing

--- src/core/ext/transport/chttp2/server/chttp2_server.cc
+++ src/core/ext/transport/chttp2/server/chttp2_server.cc
@@ -108,13 +108,6 @@ const char kUnixUriPrefix[] = "unix:";
 const char kUnixAbstractUriPrefix[] = "unix-abstract:";
 const char kVSockUriPrefix[] = "vsock:";
 
-struct AcceptorDeleter {
-  void operator()(grpc_tcp_server_acceptor* acceptor) const {
-    gpr_free(acceptor);
-  }
-};
-using AcceptorPtr = std::unique_ptr<grpc_tcp_server_acceptor, AcceptorDeleter>;
-
 namespace {
 Timestamp GetConnectionDeadline(const ChannelArgs& args) {
   return Timestamp::Now() +
@@ -124,6 +117,8 @@ Timestamp GetConnectionDeadline(const ChannelArgs& args) {
                  .value_or(Duration::Minutes(2)));
 }
 }  // namespace
+
+using AcceptorPtr = std::unique_ptr<grpc_tcp_server_acceptor, AcceptorDeleter>;
 
 class Chttp2ServerListener : public Server::ListenerInterface {
  public:
@@ -971,161 +966,6 @@ void Chttp2ServerListener::Orphan() {
     Unref();
   }
 }
-
-// New ChttpServerListener used if experiment "server_listener" is enabled
-class NewChttp2ServerListener : public Server::ListenerInterface {
- public:
-  // Keeps state for an individual connection. Lifetime: Internally refcounted
-  // and owned by Server::ListenerState.
-  class ActiveConnection : public LogicalConnection {
-   public:
-    // State for handshake. Lifetime: Owned by ActiveConnection and lasts while
-    // the handshake is ongoing.
-    class HandshakingState : public InternallyRefCounted<HandshakingState> {
-     public:
-      HandshakingState(RefCountedPtr<ActiveConnection> connection_ref,
-                       grpc_tcp_server* tcp_server,
-                       grpc_pollset* accepting_pollset, AcceptorPtr acceptor,
-                       const ChannelArgs& args,
-                       OrphanablePtr<grpc_endpoint> endpoint);
-
-      ~HandshakingState() override;
-
-      void Orphan() override;
-
-      void StartLocked(const ChannelArgs& args);
-
-      void ShutdownLocked(absl::Status status);
-
-     private:
-      void OnTimeoutLocked();
-      static void OnReceiveSettings(void* arg, grpc_error_handle /* error */);
-      void OnHandshakeDoneLocked(absl::StatusOr<HandshakerArgs*> result);
-
-      RefCountedPtr<ActiveConnection> const connection_;
-      grpc_tcp_server* const tcp_server_;
-      grpc_pollset* const accepting_pollset_;
-      const AcceptorPtr acceptor_;
-      grpc_pollset_set* const interested_parties_;
-      Timestamp const deadline_;
-      OrphanablePtr<grpc_endpoint> endpoint_;
-      // Following fields are protected by WorkSerializer.
-      RefCountedPtr<HandshakeManager> handshake_mgr_;
-      // State for enforcing handshake timeout on receiving HTTP/2 settings.
-      absl::optional<EventEngine::TaskHandle> timer_handle_;
-      grpc_closure on_receive_settings_;
-    };
-
-    ActiveConnection(RefCountedPtr<Server::ListenerState> listener_state,
-                     grpc_tcp_server* tcp_server,
-                     grpc_pollset* accepting_pollset, AcceptorPtr acceptor,
-                     const ChannelArgs& args, MemoryOwner memory_owner,
-                     OrphanablePtr<grpc_endpoint> endpoint);
-    void Start(const ChannelArgs& args);
-
-    void SendGoAway() override;
-    void DisconnectImmediately() override;
-
-    void Orphan() override;
-
-    // Needed to be able to grab an external weak ref in
-    // NewChttp2ServerListener::OnAccept()
-    using InternallyRefCounted<LogicalConnection>::RefAsSubclass;
-
-   private:
-    static void OnClose(void* arg, grpc_error_handle error);
-    void SendGoAwayImplLocked();
-    void DisconnectImmediatelyImplLocked();
-
-    RefCountedPtr<Server::ListenerState> const listener_state_;
-    WorkSerializer work_serializer_;
-    // Following fields are protected by WorkSerializer.
-    // Set by HandshakingState before the handshaking begins and set to a valid
-    // transport when handshaking is done successfully.
-    absl::variant<OrphanablePtr<HandshakingState>,
-                  RefCountedPtr<grpc_chttp2_transport>>
-        state_;
-    grpc_closure on_close_;
-    bool shutdown_ = false;
-  };
-
-  static grpc_error_handle Create(Server* server,
-                                  const EventEngine::ResolvedAddress& addr,
-                                  const ChannelArgs& args, int* port_num);
-
-  static grpc_error_handle CreateWithAcceptor(Server* server, const char* name,
-                                              const ChannelArgs& args);
-
-  static NewChttp2ServerListener* CreateForPassiveListener(
-      Server* server, const ChannelArgs& args,
-      std::shared_ptr<experimental::PassiveListenerImpl> passive_listener);
-
-  // Do not instantiate directly.  Use one of the factory methods above.
-  explicit NewChttp2ServerListener(
-      const ChannelArgs& args,
-      std::shared_ptr<experimental::PassiveListenerImpl> passive_listener =
-          nullptr);
-  ~NewChttp2ServerListener() override;
-
-  void AcceptConnectedEndpoint(std::unique_ptr<EventEngine::Endpoint> endpoint);
-
-  channelz::ListenSocketNode* channelz_listen_socket_node() const override {
-    return channelz_listen_socket_.get();
-  }
-
-  void SetServerListenerState(
-      RefCountedPtr<Server::ListenerState> listener_state) override {
-    listener_state_ = std::move(listener_state);
-  }
-
-  const grpc_resolved_address* resolved_address() const override {
-    return &resolved_address_;
-  }
-
-  void SetOnDestroyDone(grpc_closure* on_destroy_done) override;
-
-  void Orphan() override;
-
- private:
-  friend class experimental::PassiveListenerImpl;
-
-  // To allow access to RefCounted<> like interface.
-  friend class RefCountedPtr<NewChttp2ServerListener>;
-
-  // Should only be called once so as to start the TCP server. This should only
-  // be called by the config fetcher.
-  void Start() override;
-
-  static void OnAccept(void* arg, grpc_endpoint* tcp,
-                       grpc_pollset* accepting_pollset,
-                       grpc_tcp_server_acceptor* acceptor);
-
-  static void TcpServerShutdownComplete(void* arg, grpc_error_handle error);
-
-  static void DestroyListener(Server* /*server*/, void* arg,
-                              grpc_closure* destroy_done);
-
-  grpc_event_engine::experimental::EventEngine* event_engine() const {
-    return listener_state_->server()
-        ->channel_args()
-        .GetObject<grpc_event_engine::experimental::EventEngine>();
-  }
-
-  grpc_tcp_server* tcp_server_ = nullptr;
-  grpc_resolved_address resolved_address_;
-  RefCountedPtr<Server::ListenerState> listener_state_;
-  ChannelArgs args_;
-  Mutex mu_;
-  bool add_port_on_start_ ABSL_GUARDED_BY(mu_) = false;
-  // Signals whether the application has triggered shutdown.
-  bool shutdown_ ABSL_GUARDED_BY(mu_) = false;
-  grpc_closure tcp_server_shutdown_complete_ ABSL_GUARDED_BY(mu_);
-  grpc_closure* on_destroy_done_ ABSL_GUARDED_BY(mu_) = nullptr;
-  RefCountedPtr<channelz::ListenSocketNode> channelz_listen_socket_;
-  // TODO(yashykt): consider using absl::variant<> to minimize memory usage for
-  // disjoint cases where different fields are used.
-  std::shared_ptr<experimental::PassiveListenerImpl> passive_listener_;
-};
 
 //
 // NewChttp2ServerListener::ActiveConnection::HandshakingState
