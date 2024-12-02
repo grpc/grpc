346d04dfc94277346436c97754b51ca2e66ae513
Craig Tiller <ctiller@google.com>
2024-12-02 09:23:15 -0800

    x

--- src/core/ext/transport/chaotic_good/config.h
+++ src/core/ext/transport/chaotic_good/config.h
@@ -15,10 +15,13 @@
 #ifndef GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_CONFIG_H
 #define GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_CONFIG_H
 
-#include "pending_connection.h"
+#include <vector>
+
+#include "absl/container/flat_hash_set.h"
 #include "src/core/ext/transport/chaotic_good/chaotic_good_frame.pb.h"
 #include "src/core/ext/transport/chaotic_good/chaotic_good_transport.h"
 #include "src/core/ext/transport/chaotic_good/message_chunker.h"
+#include "src/core/ext/transport/chaotic_good/pending_connection.h"
 #include "src/core/lib/channel/channel_args.h"
 #include "src/core/lib/event_engine/extensions/tcp_trace.h"
 
@@ -39,7 +42,11 @@ namespace chaotic_good {
 // server.
 class Config {
  public:
-  explicit Config(const ChannelArgs& channel_args) {
+  explicit Config(
+      const ChannelArgs& channel_args,
+      std::initializer_list<chaotic_good_frame::Settings::Features>
+          supported_features = {chaotic_good_frame::Settings::CHUNKING})
+      : supported_features_(supported_features) {
     decode_alignment_ =
         std::max(1, channel_args.GetInt(GRPC_ARG_CHAOTIC_GOOD_ALIGNMENT)
                         .value_or(decode_alignment_));
@@ -91,6 +98,18 @@ class Config {
   absl::Status ReceiveServerIncomingSettings(
       const chaotic_good_frame::Settings& settings,
       ClientConnectionFactory& connector) {
+    absl::flat_hash_set<chaotic_good_frame::Settings::Features>
+        supported_features;
+    for (const auto feature : settings.supported_features()) {
+      if (chaotic_good_frame::Settings::Features_IsValid(feature)) {
+        const auto valid_feature =
+            static_cast<chaotic_good_frame::Settings::Features>(feature);
+        if (supported_features_.contains(valid_feature)) {
+          supported_features.insert(valid_feature);
+        }
+      }
+    }
+    supported_features_.swap(supported_features);
     for (const auto& connection_id : settings.connection_id()) {
       pending_data_endpoints_.emplace_back(connector.Connect(connection_id));
     }
@@ -99,6 +118,24 @@ class Config {
 
   absl::Status ReceiveClientIncomingSettings(
       const chaotic_good_frame::Settings& settings) {
+    absl::flat_hash_set<chaotic_good_frame::Settings::Features>
+        supported_features;
+    for (const auto feature : settings.supported_features()) {
+      if (!chaotic_good_frame::Settings::Features_IsValid(feature)) {
+        return absl::InternalError(absl::StrCat(
+            "Unsupported feature present in chaotic-good handshake: ",
+            feature));
+      }
+      const auto valid_feature =
+          static_cast<chaotic_good_frame::Settings::Features>(feature);
+      if (!supported_features_.contains(valid_feature)) {
+        return absl::InternalError(absl::StrCat(
+            "Unsupported feature present in chaotic-good handshake: ",
+            chaotic_good_frame::Settings::Features_Name(valid_feature)));
+      }
+      supported_features.insert(valid_feature);
+    }
+    supported_features_.swap(supported_features);
     if (settings.connection_id_size() != 0) {
       return absl::InternalError("Client cannot specify connection ids");
     }
@@ -146,6 +183,10 @@ class Config {
     sink.Append(config.ToString());
   }
 
+  bool supports_chunking() const {
+    return supported_features_.contains(chaotic_good_frame::Settings::CHUNKING);
+  }
+
  private:
   // Fill-in a settings frame to be sent with the results of the negotiation so
   // far. For the client this will be whatever we got from channel args; for the
@@ -163,7 +204,7 @@ class Config {
     if (settings.alignment() != 0) encode_alignment_ = settings.alignment();
     max_send_chunk_size_ =
         std::min(max_send_chunk_size_, settings.max_chunk_size());
-    if (settings.max_chunk_size() == 0) {
+    if (!supports_chunking() || settings.max_chunk_size() == 0) {
       max_recv_chunk_size_ = 0;
       max_send_chunk_size_ = 0;
     }
@@ -177,6 +218,8 @@ class Config {
   uint32_t max_recv_chunk_size_ = 1024 * 1024;
   uint32_t inline_payload_size_threshold_ = 8 * 1024;
   std::vector<PendingConnection> pending_data_endpoints_;
+  absl::flat_hash_set<chaotic_good_frame::Settings::Features>
+      supported_features_;
 };
 
 }  // namespace chaotic_good
