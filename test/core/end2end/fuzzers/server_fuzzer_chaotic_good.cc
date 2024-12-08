4d1b250e8d82db51e2328bda46167a9901e20475
Craig Tiller <ctiller@google.com>
2024-12-07 22:13:19 -0800

    x

--- test/core/end2end/fuzzers/server_fuzzer_chaotic_good.cc
+++ test/core/end2end/fuzzers/server_fuzzer_chaotic_good.cc
@@ -26,7 +26,8 @@ DEFINE_PROTO_FUZZER(const fuzzer_input::Msg& msg) {
               const grpc_core::ChannelArgs& channel_args) {
         grpc_core::ExecCtx exec_ctx;
         auto* listener = new grpc_core::chaotic_good::ChaoticGoodServerListener(
-            server, channel_args, [next = uint64_t(0)]() mutable {
+            grpc_core::Server::FromC(server), channel_args,
+            [next = uint64_t(0)]() mutable {
               return absl::StrCat(absl::Hex(next++));
             });
         auto port = listener->Bind(
