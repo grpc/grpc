a694e278e36ed9ad2b642c87c91decd3dc5dc466
AJ Heller <hork@google.com>
2024-12-13 19:02:01 +0000

    [DO NOT REVIEW] Testing removal of KeepsGrpcInitialized

--- src/core/lib/event_engine/posix_engine/posix_engine.h
+++ src/core/lib/event_engine/posix_engine/posix_engine.h
@@ -132,10 +132,7 @@ class PosixEnginePollerManager
 
 // An iomgr-based Posix EventEngine implementation.
 // All methods require an ExecCtx to already exist on the thread's stack.
-// TODO(ctiller): KeepsGrpcInitialized is an interim measure to ensure that
-// EventEngine is shut down before we shut down iomgr.
-class PosixEventEngine final : public PosixEventEngineWithFdSupport,
-                               public grpc_core::KeepsGrpcInitialized {
+class PosixEventEngine final : public PosixEventEngineWithFdSupport {
  public:
   class PosixDNSResolver : public EventEngine::DNSResolver {
    public:
