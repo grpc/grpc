902894e99d4880eff61baa0d762514a99a451001
ctiller <10120821+ctiller@users.noreply.github.com>
2024-12-14 06:09:26 +0000

    Automated change: Fix sanity tests

--- src/core/ext/transport/chaotic_good/frame_transport.h
+++ src/core/ext/transport/chaotic_good/frame_transport.h
@@ -79,4 +79,4 @@ class FrameTransport : public RefCounted<FrameTransport> {
 }  // namespace chaotic_good
 }  // namespace grpc_core
 
-#endif
+#endif  // GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_FRAME_TRANSPORT_H
