6a74651108074b4bd29e32fce86172030eb88822
Eric Anderson <ejona@google.com>
2025-01-16 16:07:33 -0800

    [interop] Add grpc-java 1.68.3 and 1.69.1 to client_matrix.py

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -457,8 +457,8 @@ LANG_RELEASE_MATRIX = {
             ("v1.65.1", ReleaseInfo()),
             ("v1.66.0", ReleaseInfo()),
             ("v1.67.1", ReleaseInfo()),
-            ("v1.68.2", ReleaseInfo()),
-            ("v1.69.0", ReleaseInfo()),
+            ("v1.68.3", ReleaseInfo()),
+            ("v1.69.1", ReleaseInfo()),
         ]
     ),
     "python": OrderedDict(
