ad7bb4afd0301606e1c1496dd3170904cf28b0cd
Craig Tiller <ctiller@google.com>
2025-01-27 13:33:24 -0800

    x

--- test/core/call/server_call_test.cc
+++ test/core/call/server_call_test.cc
@@ -37,7 +37,9 @@ class ServerCallTest : public YodelTest {
  protected:
   using YodelTest::YodelTest;
 
-  ~ServerCallTest() { grpc_metadata_array_destroy(&publish_initial_metadata_); }
+  ~ServerCallTest() override {
+    grpc_metadata_array_destroy(&publish_initial_metadata_);
+  }
 
   grpc_call* InitCall(ClientMetadataHandle client_initial_metadata) {
     CHECK_EQ(call_, nullptr);
