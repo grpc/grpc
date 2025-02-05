cbc738ee99c56babbbb1cd63bb50b59f1033208b
Esun Kim <veblush@google.com>
2025-02-05 15:45:57 -0800

    Updated shell scripts to use virtualenv properly

--- tools/codegen/core/gen_trace_flags_ci.sh
+++ tools/codegen/core/gen_trace_flags_ci.sh
@@ -17,5 +17,9 @@ set -ex
 dir=$(dirname "${0}")
 cd "${dir}/../../.."
 
+VIRTUALENV=venv_gen_trace_flags_ci
+python3 -m virtualenv $VIRTUALENV
+source $VIRTUALENV/bin/activate
+
 python3 -m pip install absl-py
 python3 ${dir}/gen_trace_flags.py --check
