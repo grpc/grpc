e3a84ed6d1cff1aa465efc86f2ef0b03f54d3c88
Craig Tiller <ctiller@google.com>
2025-02-13 11:11:15 -0800

    fix

--- test/core/end2end/tests/simple_metadata.cc
+++ test/core/end2end/tests/simple_metadata.cc
@@ -71,7 +71,7 @@ CORE_END2END_TEST(CoreEnd2endTests, SimpleMetadata) {
   EXPECT_EQ(server_status.GetTrailingMetadata("key6"), "val6");
 }
 
-TEST(Fuzzers, CoreEnd2endTests_SimpleMetadataRegression) {
+TEST(Fuzzers, CoreEnd2endTestsSimpleMetadataRegression1) {
   CoreEnd2endTests_SimpleMetadata(
     CoreTestConfigurationNamed("ChaoticGoodOneByteChunk"),
     ParseTestProto(R"pb(config_vars { trace: "promise_primitives" })pb")
