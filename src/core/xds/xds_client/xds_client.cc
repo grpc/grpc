grpc/grpc

commit 8fb4bd5e0e8fae68e8970de7bde4dea9273ecd7b

[XdsClient] fix edge case when rapidly subscribing and unsubscribing (#38698)
Closes #38698

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/38698 from markdroth:xds_client_unsubscribe_resubscribe_race db607a5cf6bff61299e40880f339b429ed0d2024
PiperOrigin-RevId: 725647636

src/core/xds/xds_client/xds_client.cc
@@ -124,7 +124,10 @@ class XdsClient::XdsChannel::AdsCall final
   // The ctor and dtor should not be used directly.
   explicit AdsCall(RefCountedPtr<RetryableCall<AdsCall>> retryable_call);
 
-  void Orphan() override;
+  // Disable thread-safety analysis because this method is called via
+  // OrphanablePtr<>, but there's no way to pass the lock annotation
+  // through there.
+  void Orphan() override ABSL_NO_THREAD_SAFETY_ANALYSIS;
 
   RetryableCall<AdsCall>* retryable_call() const {
     return retryable_call_.get();
@@ -204,7 +207,7 @@ class XdsClient::XdsChannel::AdsCall final
       // optimize by not resending the resource that we already have.
       auto& authority_state =
           ads_call->xds_client()->authority_state_map_[name_.authority];
-      ResourceState& state = authority_state.resource_map[type_][name_.key];
+      ResourceState& state = authority_state.type_map[type_][name_.key];
       if (state.HasResource()) return;
       // Start timer.
       ads_call_ = std::move(ads_call);
@@ -230,7 +233,7 @@ class XdsClient::XdsChannel::AdsCall final
         timer_handle_.reset();
         auto& authority_state =
             ads_call_->xds_client()->authority_state_map_[name_.authority];
-        ResourceState& state = authority_state.resource_map[type_][name_.key];
+        ResourceState& state = authority_state.type_map[type_][name_.key];
         // We might have received the resource after the timer fired but before
         // the callback ran.
         if (!state.HasResource()) {
@@ -446,16 +449,12 @@ void XdsClient::XdsChannel::Orphaned() ABSL_NO_THREAD_SAFETY_ANALYSIS {
 
 void XdsClient::XdsChannel::ResetBackoff() { transport_->ResetBackoff(); }
 
-XdsClient::XdsChannel::AdsCall* XdsClient::XdsChannel::ads_call() const {
-  return ads_call_->call();
-}
-
 void XdsClient::XdsChannel::SubscribeLocked(const XdsResourceType* type,
                                             const XdsResourceName& name) {
   if (ads_call_ == nullptr) {
     // Start the ADS call if this is the first request.
-    ads_call_.reset(
-        new RetryableCall<AdsCall>(WeakRef(DEBUG_LOCATION, "XdsChannel+ads")));
+    ads_call_ = MakeOrphanable<RetryableCall<AdsCall>>(
+        WeakRef(DEBUG_LOCATION, "XdsChannel+ads"));
     // Note: AdsCall's ctor will automatically subscribe to all
     // resources that the XdsClient already has watchers for, so we can
     // return here.
@@ -463,9 +462,9 @@ void XdsClient::XdsChannel::SubscribeLocked(const XdsResourceType* type,
   }
   // If the ADS call is in backoff state, we don't need to do anything now
   // because when the call is restarted it will resend all necessary requests.
-  if (ads_call() == nullptr) return;
+  if (ads_call_->call() == nullptr) return;
   // Subscribe to this resource if the ADS call is active.
-  ads_call()->SubscribeLocked(type, name, /*delay_send=*/false);
+  ads_call_->call()->SubscribeLocked(type, name, /*delay_send=*/false);
 }
 
 void XdsClient::XdsChannel::UnsubscribeLocked(const XdsResourceType* type,
@@ -497,10 +496,12 @@ bool XdsClient::XdsChannel::MaybeFallbackLocked(
        ++i) {
     authority_state.xds_channels.emplace_back(
         xds_client_->GetOrCreateXdsChannelLocked(*xds_servers[i], "fallback"));
-    for (const auto& [type, resource_map] : authority_state.resource_map) {
-      for (const auto& [key, _] : resource_map) {
-        authority_state.xds_channels.back()->SubscribeLocked(type,
-                                                             {authority, key});
+    for (const auto& [type, resource_map] : authority_state.type_map) {
+      for (const auto& [key, resource_state] : resource_map) {
+        if (resource_state.HasWatchers()) {
+          authority_state.xds_channels.back()->SubscribeLocked(
+              type, {authority, key});
+        }
       }
     }
     GRPC_TRACE_LOG(xds_client, INFO)
@@ -564,7 +565,7 @@ void XdsClient::XdsChannel::SetChannelStatusLocked(absl::Status status) {
         MaybeFallbackLocked(authority, authority_state)) {
       continue;
     }
-    for (const auto& [_, resource_map] : authority_state.resource_map) {
+    for (const auto& [_, resource_map] : authority_state.type_map) {
       for (const auto& [_, resource_state] : resource_map) {
         auto& watchers =
             resource_state.HasResource() ? watchers_cached : watchers_uncached;
@@ -723,9 +724,11 @@ XdsClient::XdsChannel::AdsCall::AdsCall(
     // Skip authorities that are not using this xDS channel. The channel can be
     // anywhere in the list.
     if (it == authority_state.xds_channels.end()) continue;
-    for (const auto& [type, resource_map] : authority_state.resource_map) {
-      for (const auto& [resource_key, _] : resource_map) {
-        SubscribeLocked(type, {authority, resource_key}, /*delay_send=*/true);
+    for (const auto& [type, resource_map] : authority_state.type_map) {
+      for (const auto& [resource_key, resource_state] : resource_map) {
+        if (resource_state.HasWatchers()) {
+          SubscribeLocked(type, {authority, resource_key}, /*delay_send=*/true);
+        }
       }
     }
   }
@@ -738,6 +741,13 @@ XdsClient::XdsChannel::AdsCall::AdsCall(
 
 void XdsClient::XdsChannel::AdsCall::Orphan() {
   state_map_.clear();
+  // We may have unsubscriptions for which we have not yet actually sent
+  // unsubscribe messages, and now we never will, so do a pass to delete
+  // any cache entries for which we've unsubscribed.
+  for (const auto& [_, type] : xds_client()->resource_types_) {
+    xds_client()->MaybeRemoveUnsubscribedCacheEntriesForTypeLocked(
+        xds_channel(), type);
+  }
   // Note that the initial ref is held by the StreamEventHandler, which
   // will be destroyed when streaming_call_ is destroyed, which may not happen
   // here, since there may be other refs held to streaming_call_ by internal
@@ -876,6 +886,8 @@ void XdsClient::XdsChannel::AdsCall::SendMessageLocked(
     buffered_requests_.insert(type);
     return;
   }
+  xds_client()->MaybeRemoveUnsubscribedCacheEntriesForTypeLocked(xds_channel(),
+                                                                 type);
   auto& state = state_map_[type];
   std::string serialized_message = CreateAdsRequest(
       type->type_url(), xds_channel()->resource_type_version_map_[type],
@@ -997,8 +1009,8 @@ void XdsClient::XdsChannel::AdsCall::ParseResource(
   }
   AuthorityState& authority_state = authority_it->second;
   // Found authority, so look up type.
-  auto type_it = authority_state.resource_map.find(context->type);
-  if (type_it == authority_state.resource_map.end()) {
+  auto type_it = authority_state.type_map.find(context->type);
+  if (type_it == authority_state.type_map.end()) {
     return;  // Skip resource -- we don't have a subscription for it.
   }
   auto& type_map = type_it->second;
@@ -1108,8 +1120,8 @@ void XdsClient::XdsChannel::AdsCall::HandleServerReportedResourceError(
   }
   AuthorityState& authority_state = authority_it->second;
   // Found authority, so look up type.
-  auto type_it = authority_state.resource_map.find(context->type);
-  if (type_it == authority_state.resource_map.end()) {
+  auto type_it = authority_state.type_map.find(context->type);
+  if (type_it == authority_state.type_map.end()) {
     return;  // Skip resource -- we don't have a subscription for it.
   }
   auto& type_map = type_it->second;
@@ -1313,8 +1325,8 @@ void XdsClient::XdsChannel::AdsCall::OnRecvMessage(absl::string_view payload) {
         }
         auto seen_authority_it = context.resources_seen.find(authority);
         // Find this resource type.
-        auto type_it = authority_state.resource_map.find(context.type);
-        if (type_it == authority_state.resource_map.end()) continue;
+        auto type_it = authority_state.type_map.find(context.type);
+        if (type_it == authority_state.type_map.end()) continue;
         // Iterate over resource ids.
         for (auto& [resource_key, resource_state] : type_it->second) {
           if (seen_authority_it == context.resources_seen.end() ||
@@ -1595,7 +1607,12 @@ void XdsClient::Orphaned() {
   MutexLock lock(&mu_);
   shutting_down_ = true;
   // Clear cache and any remaining watchers that may not have been cancelled.
-  authority_state_map_.clear();
+  // Note: We move authority_state_map_ out of the way before clearing
+  // it, because clearing the map will trigger calls to
+  // MaybeRemoveUnsubscribedCacheEntriesForTypeLocked(), which would try to
+  // modify the map while we are iterating over it.
+  auto authority_state_map = std::move(authority_state_map_);
+  authority_state_map.clear();
   invalid_watchers_.clear();
 }
 
@@ -1614,10 +1631,11 @@ RefCountedPtr<XdsClient::XdsChannel> XdsClient::GetOrCreateXdsChannelLocked(
 }
 
 bool XdsClient::HasUncachedResources(const AuthorityState& authority_state) {
-  for (const auto& [_, resource_map] : authority_state.resource_map) {
+  for (const auto& [_, resource_map] : authority_state.type_map) {
     for (const auto& [_, resource_state] : resource_map) {
-      if (resource_state.client_status() ==
-          ResourceState::ClientResourceStatus::REQUESTED) {
+      if (resource_state.HasWatchers() &&
+          resource_state.client_status() ==
+              ResourceState::ClientResourceStatus::REQUESTED) {
         return true;
       }
     }
@@ -1662,12 +1680,11 @@ void XdsClient::WatchResource(const XdsResourceType* type,
   MaybeRegisterResourceTypeLocked(type);
   AuthorityState& authority_state =
       authority_state_map_[resource_name->authority];
-  auto [it, first_watcher_for_resource] =
-      authority_state.resource_map[type].emplace(resource_name->key,
-                                                 ResourceState());
+  auto [it, created_entry] = authority_state.type_map[type].emplace(
+      resource_name->key, ResourceState());
   ResourceState& resource_state = it->second;
   resource_state.AddWatcher(watcher);
-  if (first_watcher_for_resource) {
+  if (created_entry) {
     // We try to add new channels in 2 cases:
     // - This is the first resource for this authority (i.e., the list
     //   of channels is empty).
@@ -1690,16 +1707,12 @@ void XdsClient::WatchResource(const XdsResourceType* type,
         }
       }
     }
-    for (const auto& channel : authority_state.xds_channels) {
-      channel->SubscribeLocked(type, *resource_name);
-    }
   } else {
     // If we already have a cached value for the resource, notify the new
     // watcher immediately.
     if (resource_state.HasResource()) {
       GRPC_TRACE_LOG(xds_client, INFO)
-          << "[xds_client " << this << "] returning cached listener data for "
-          << name;
+          << "[xds_client " << this << "] returning cached data for " << name;
       NotifyWatchersOnResourceChanged(resource_state.resource(), {watcher},
                                       ReadDelayHandle::NoWait());
     }
@@ -1710,6 +1723,10 @@ void XdsClient::WatchResource(const XdsResourceType* type,
       NotifyWatchersOnError(resource_state, ReadDelayHandle::NoWait(),
                             {watcher});
     }
+  }
+  // Make sure all channels are subscribing to the resource.
+  for (const auto& channel : authority_state.xds_channels) {
+    channel->SubscribeLocked(type, *resource_name);
   }
   // If the channel is not connected, report an error to the watcher.
   absl::Status channel_status = authority_state.xds_channels.back()->status();
@@ -1737,27 +1754,62 @@ void XdsClient::CancelResourceWatch(const XdsResourceType* type,
   if (authority_it == authority_state_map_.end()) return;
   AuthorityState& authority_state = authority_it->second;
   // Find type map.
-  auto type_it = authority_state.resource_map.find(type);
-  if (type_it == authority_state.resource_map.end()) return;
-  auto& type_map = type_it->second;
+  auto type_it = authority_state.type_map.find(type);
+  if (type_it == authority_state.type_map.end()) return;
+  auto& resource_map = type_it->second;
   // Find resource key.
-  auto resource_it = type_map.find(resource_name->key);
-  if (resource_it == type_map.end()) return;
+  auto resource_it = resource_map.find(resource_name->key);
+  if (resource_it == resource_map.end()) return;
   ResourceState& resource_state = resource_it->second;
   // Remove watcher.
   resource_state.RemoveWatcher(watcher);
-  // Clean up empty map entries, if any.
+  // If this was the last watcher, clean up.
   if (!resource_state.HasWatchers()) {
+    // Unsubscribe from this resource on all XdsChannels.
     for (const auto& xds_channel : authority_state.xds_channels) {
       xds_channel->UnsubscribeLocked(type, *resource_name,
                                      delay_unsubscription);
     }
-    type_map.erase(resource_it);
-    if (type_map.empty()) {
-      authority_state.resource_map.erase(type_it);
-      if (authority_state.resource_map.empty()) {
-        authority_state.xds_channels.clear();
+    // Note: We wait to remove the cache entry until we actualle send
+    // the unsubscription message on the ADS stream, so that if a watch is
+    // stopped and then started again before we send the next request
+    // for that resource type, we don't lose the cache entry without the
+    // xDS server knowing it needs to re-send it.
+    //
+    // Note: Because the cache cleanup may have been triggered by the
+    // unsubscription, it's no longer safe to access any of the
+    // iterators that we have from above.
+  }
+}
+
+void XdsClient::MaybeRemoveUnsubscribedCacheEntriesForTypeLocked(
+    XdsChannel* xds_channel, const XdsResourceType* type) {
+  for (auto authority_it = authority_state_map_.begin();
+       authority_it != authority_state_map_.end();) {
+    AuthorityState& authority_state = authority_it->second;
+    if (authority_state.xds_channels.back() == xds_channel) {
+      // Find type map.
+      auto type_it = authority_state.type_map.find(type);
+      if (type_it != authority_state.type_map.end()) {
+        auto& resource_map = type_it->second;
+        // Remove the cache entry for any resource without watchers.
+        for (auto resource_it = resource_map.begin();
+             resource_it != resource_map.end();) {
+          ResourceState& resource_state = resource_it->second;
+          if (!resource_state.HasWatchers()) {
+            resource_map.erase(resource_it++);
+          } else {
+            ++resource_it;
+          }
+        }
+        // Clean up empty entries in the map.
+        if (resource_map.empty()) authority_state.type_map.erase(type_it);
       }
+    }
+    if (authority_state.type_map.empty()) {
+      authority_state_map_.erase(authority_it++);
+    } else {
+      ++authority_it;
     }
   }
 }
@@ -1896,13 +1948,14 @@ void XdsClient::DumpClientConfig(
                   node, arena);
   // Dump each resource.
   for (const auto& [authority, authority_state] : authority_state_map_) {
-    for (const auto& [type, resource_map] : authority_state.resource_map) {
+    for (const auto& [type, resource_map] : authority_state.type_map) {
       auto it =
           string_pool
               ->emplace(absl::StrCat("type.googleapis.com/", type->type_url()))
               .first;
       upb_StringView type_url = StdStringToUpbString(*it);
       for (const auto& [resource_key, resource_state] : resource_map) {
+        if (!resource_state.HasWatchers()) continue;
         auto it2 = string_pool
                        ->emplace(ConstructFullXdsResourceName(
                            authority, type->type_url(), resource_key))
@@ -1923,12 +1976,14 @@ void XdsClient::ReportResourceCounts(
   ResourceCountLabels labels;
   for (const auto& [authority, authority_state] : authority_state_map_) {
     labels.xds_authority = authority;
-    for (const auto& [type, resource_map] : authority_state.resource_map) {
+    for (const auto& [type, resource_map] : authority_state.type_map) {
       labels.resource_type = type->type_url();
       // Count the number of entries in each state.
       std::map<absl::string_view, uint64_t> counts;
       for (const auto& [_, resource_state] : resource_map) {
-        ++counts[resource_state.CacheStateString()];
+        if (resource_state.HasWatchers()) {
+          ++counts[resource_state.CacheStateString()];
+        }
       }
       // Report the count for each state.
       for (const auto& [state, count] : counts) {

src/core/xds/xds_client/xds_client.h
@@ -198,7 +198,6 @@ class XdsClient : public DualRefCounted<XdsClient> {
     ~XdsChannel() override;
 
     XdsClient* xds_client() const { return xds_client_.get(); }
-    AdsCall* ads_call() const;
 
     void ResetBackoff();
 
@@ -358,7 +357,7 @@ class XdsClient : public DualRefCounted<XdsClient> {
   struct AuthorityState {
     std::vector<RefCountedPtr<XdsChannel>> xds_channels;
     std::map<const XdsResourceType*, std::map<XdsResourceKey, ResourceState>>
-        resource_map;
+        type_map;
   };
 
   absl::Status AppendNodeToStatus(const absl::Status& status) const;
@@ -390,6 +389,10 @@ class XdsClient : public DualRefCounted<XdsClient> {
       ABSL_EXCLUSIVE_LOCKS_REQUIRED(mu_);
 
   bool HasUncachedResources(const AuthorityState& authority_state);
+
+  void MaybeRemoveUnsubscribedCacheEntriesForTypeLocked(
+      XdsChannel* xds_channel, const XdsResourceType* type)
+      ABSL_EXCLUSIVE_LOCKS_REQUIRED(mu_);
 
   absl::StatusOr<XdsResourceName> ParseXdsResourceName(
       absl::string_view name, const XdsResourceType* type);

commit 822f9b15191840228d17dc0d305887374150150e

[XdsClient] fix ubsan failure (#38726)
This fixes a ubsan failure introduced in #38698.  In xDS fallback, when a higher-priority server comes back online, we [remove the lower-priority channels from the authority state](https://github.com/grpc/grpc/blob/686fc9dbebb2ba2c68a73fb973f1c8e8e6d89b45/src/core/xds/xds_client/xds_client.cc#L535), but unreffing the channels triggers a call to `MaybeRemoveUnsubscribedCacheEntriesForTypeLocked()`, which [attempts to access the list of channels](https://github.com/grpc/grpc/blob/686fc9dbebb2ba2c68a73fb973f1c8e8e6d89b45/src/core/xds/xds_client/xds_client.cc#L1788) while we're in the process of modifying it.

Example failure:

https://btx.cloud.google.com/invocations/69469281-f334-4a1f-91b4-3eb8905b63f4/targets/%2F%2Ftest%2Fcpp%2Fend2end%2Fxds:xds_fallback_end2end_test@experiment%3Dno_server_listener;config=79d74d08dc8cd749c211bbf112a92eee46adbf3f6203bc27099b142ea4e7aac9/log

Closes #38726

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/38726 from markdroth:xds_client_unsubscribe_resubscribe_race_fix 207c37c31f2ff190a761e70985488ba24b224911
PiperOrigin-RevId: 726322804

src/core/xds/xds_client/xds_client.cc
@@ -532,6 +532,13 @@ void XdsClient::XdsChannel::SetHealthyLocked() {
           << "[xds_client " << xds_client_.get() << "] authority " << authority
           << ": Falling forward to " << server_.server_uri();
       // Lower priority channels are no longer needed, connection is back!
+      // Note that we move the lower priority channels out of the vector
+      // before we unref them, or else
+      // MaybeRemoveUnsubscribedCacheEntriesForTypeLocked() will try to
+      // access the vector while we are modifying it.
+      std::vector<RefCountedPtr<XdsChannel>> channels_to_unref(
+          std::make_move_iterator(channel_it + 1),
+          std::make_move_iterator(channels.end()));
       channels.erase(channel_it + 1, channels.end());
     }
   }