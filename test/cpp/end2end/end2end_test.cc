ca6d25e53ba63825c1ca4453608d73b78374036c
Mark D. Roth <roth@google.com>
2025-02-25 23:29:18 +0000

    fix test

--- test/cpp/end2end/end2end_test.cc
+++ test/cpp/end2end/end2end_test.cc
@@ -1514,7 +1514,6 @@ TEST_P(End2endTest, ExpectErrorTest) {
     EXPECT_EQ(iter->code(), s.error_code());
     EXPECT_EQ(iter->error_message(), s.error_message());
     EXPECT_EQ(iter->binary_error_details(), s.error_details());
-    EXPECT_TRUE(absl::StrContains(context.debug_error_string(), "created"));
     EXPECT_TRUE(absl::StrContains(context.debug_error_string(), "status"));
     EXPECT_TRUE(absl::StrContains(context.debug_error_string(), "13"));
   }
