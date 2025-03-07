e55df295e0999860e8ad84e5b20f935a18a48fc4
Xuan Wang <xuanwn@google.com>
2025-03-07 19:21:46 +0000

    fix copy_protos

--- tools/buildgen/copy_protos.sh
+++ tools/buildgen/copy_protos.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Copy protos from proto directory to python ancillary packages directory."""
+# Copy protos from proto directory to python ancillary packages directory.
 
 set -e
 
