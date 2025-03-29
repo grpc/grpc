af3189a7e0f1d48e1a1a82fea5c8e18fda7414b5
ctiller <10120821+ctiller@users.noreply.github.com>
2025-03-29 00:15:32 +0000

    Automated change: Fix sanity tests

--- src/core/util/seq_bit_set.cc
+++ src/core/util/seq_bit_set.cc
@@ -23,7 +23,7 @@ bool SeqBitSet::IsSet(uint64_t seq) const {
   if (seq >= epoch_ + kNumFutureBitEntries * 64) {
     return far_future_bits_.count(seq) != 0;
   }
-  const size_t index = (seq - epoch_) / 64;
+  const std::size_t index = (seq - epoch_) / 64;
   const uint64_t offset = (seq - epoch_) % 64;
   return (future_bits_[index] & (1ull << offset)) != 0;
 }
@@ -33,14 +33,14 @@ bool SeqBitSet::Set(uint64_t seq) {
   if (seq >= epoch_ + kNumFutureBitEntries * 64) {
     return !far_future_bits_.insert(seq).second;
   }
-  const size_t index = (seq - epoch_) / 64;
+  const std::size_t index = (seq - epoch_) / 64;
   const uint64_t offset = (seq - epoch_) % 64;
   const bool was_set = (future_bits_[index] & (1ull << offset)) != 0;
   future_bits_[index] |= (1ull << offset);
   if (index != 0) return was_set;
   if (future_bits_[0] != std::numeric_limits<uint64_t>::max()) return was_set;
   epoch_ += 64;
-  for (size_t i = 0; i < kNumFutureBitEntries - 1; ++i) {
+  for (std::size_t i = 0; i < kNumFutureBitEntries - 1; ++i) {
     future_bits_[i] = future_bits_[i + 1];
   }
   future_bits_[kNumFutureBitEntries - 1] = 0;
