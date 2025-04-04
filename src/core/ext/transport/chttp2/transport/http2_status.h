99f27ac2cbbd8d7cd225719aba1e813dadd94839
tanvi-jagtap <tjagtap@google.com>
2025-04-04 12:34:07 +0000

    [PH2][Http2Status][Error] Error code

--- src/core/ext/transport/chttp2/transport/http2_status.h
+++ src/core/ext/transport/chttp2/transport/http2_status.h
@@ -174,6 +174,10 @@ class Http2StatusOr {
 
   Http2Status status() { return std::get<Http2Status>(status_or_); }
 
+  Http2StatusOr(Http2Status status) : status_or_(status) {}
+  Http2StatusOr(T value) : status_or_(value) {}
+
+  // TODO(tjagtap):[PH2][P1]:Implement for move
  private:
   std::variant<Http2Status, T> status_or_;
 };
