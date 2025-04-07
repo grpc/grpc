e8bc082c5a01ff547e7981048824132cc99e628d
tanvi-jagtap <tjagtap@google.com>
2025-04-07 08:30:47 +0000

    [PH2]

--- src/core/ext/transport/chttp2/transport/header_assembler.h
+++ src/core/ext/transport/chttp2/transport/header_assembler.h
@@ -20,11 +20,11 @@
 #define GRPC_SRC_CORE_EXT_TRANSPORT_CHTTP2_TRANSPORT_HEADER_ASSEMBLER_H
 
 #include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "src/core/call/message.h"
 #include "src/core/ext/transport/chttp2/transport/frame.h"
 #include "src/core/lib/slice/slice.h"
 #include "src/core/lib/slice/slice_buffer.h"
-#include "src/core/util/ref_counted_ptr.h"
 
 namespace grpc_core {
 namespace http2 {
