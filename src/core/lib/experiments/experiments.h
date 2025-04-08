b65ee5eb000308730dcc1a69f213dc7a7f2d2d0f
Adam Heller <aj@drfloob.com>
2025-04-08 12:50:03 -0700

    [EventEngine] Cleanup: EventEngine client, listener, and dns experiments are on by default on all platforms. (#39079)
    Update: it was confirmed that iOS and all other OSS platforms have been using these experiments for months, albeit via a different overlapping mechanism. This change should now be a no-op cleanup.
    
    ----
    
    Original PR claim:
    
    These experiments were not enabled by default [on iOS], and so it seems none of the experimental code paths have been used. This is effectively a full EventEngine client and DNS rollout on iOS.
    
    Closes #39079
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/39079 from drfloob:ios-ee-experiments be1d47068afc5fab9910e5a19314aeea9b1e0e50
    PiperOrigin-RevId: 745255066

--- src/core/lib/experiments/experiments.h
+++ src/core/lib/experiments/experiments.h
@@ -67,11 +67,14 @@ inline bool IsCallv3ClientAuthFilterEnabled() { return false; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_CHAOTIC_GOOD_FRAMING_LAYER
 inline bool IsChaoticGoodFramingLayerEnabled() { return true; }
 inline bool IsErrorFlattenEnabled() { return false; }
-inline bool IsEventEngineClientEnabled() { return false; }
-inline bool IsEventEngineDnsEnabled() { return false; }
+#define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_CLIENT
+inline bool IsEventEngineClientEnabled() { return true; }
+#define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_DNS
+inline bool IsEventEngineDnsEnabled() { return true; }
 inline bool IsEventEngineDnsNonClientChannelEnabled() { return false; }
 inline bool IsEventEngineForkEnabled() { return false; }
-inline bool IsEventEngineListenerEnabled() { return false; }
+#define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_LISTENER
+inline bool IsEventEngineListenerEnabled() { return true; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_CALLBACK_CQ
 inline bool IsEventEngineCallbackCqEnabled() { return true; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_FOR_ALL_OTHER_ENDPOINTS
