d7612b16c98b91f93faf50da09d509d3630c91d2
Adam Heller <aj@drfloob.com>
2025-04-08 12:11:25 -0700

    [test] Skip tests that require slower time ticks in the FuzzingEventEngine (#39166)
    Closes #39166
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/39166 from drfloob:skip-xds-tests-when-non-ee a8c782a675bd974c02e1a64eeea35da0f63587ea
    PiperOrigin-RevId: 745240556

--- test/core/event_engine/event_engine_test_utils.h
+++ test/core/event_engine/event_engine_test_utils.h
@@ -217,6 +217,11 @@ class ThreadedNoopEndpoint : public EventEngine::Endpoint {
   EventEngine::ResolvedAddress local_;
 };
 
+// We need everything EventEngine to do reasonable timer steps -- without it
+// we need to do a bunch of evil to make sure both timer systems are ticking
+// each step.
+bool IsSaneTimerEnvironment();
+
 }  // namespace experimental
 }  // namespace grpc_event_engine
 
