a8478eeb5694498aeedb3f55cf10514a4fd66f96
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2025-04-10 16:20:48 +0200

    remove forced CONFIG mode for find_package(Protobuf)

--- examples/cpp/cmake/common.cmake
+++ examples/cpp/cmake/common.cmake
@@ -100,7 +100,7 @@ else()
   # Find Protobuf installation
   # Looks for protobuf-config.cmake file installed by Protobuf's cmake installation.
   option(protobuf_MODULE_COMPATIBLE TRUE)
-  find_package(Protobuf CONFIG REQUIRED)
+  find_package(Protobuf REQUIRED)
   message(STATUS "Using protobuf ${Protobuf_VERSION}")
 
   set(_PROTOBUF_LIBPROTOBUF protobuf::libprotobuf)
