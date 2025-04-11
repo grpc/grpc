1156f87074cd76950b6c4e2ac9daf2126daf1e03
ctiller <10120821+ctiller@users.noreply.github.com>
2025-04-11 20:44:22 +0000

    Automated change: Fix sanity tests

--- src/core/ext/transport/chaotic_good/transport_context.h
+++ src/core/ext/transport/chaotic_good/transport_context.h
@@ -44,4 +44,4 @@ using TransportContextPtr = RefCountedPtr<TransportContext>;
 
 }  // namespace grpc_core::chaotic_good
 
-#endif
+#endif  // GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_TRANSPORT_CONTEXT_H
