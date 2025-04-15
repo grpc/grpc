884ca66fde5952e3cedb64703be17feabddc3383
Pawan Bhardwaj <bpawan@google.com>
2025-04-15 11:48:27 +0000

    fix

--- tools/internal_ci/macos/grpc_distribtests_python.sh
+++ tools/internal_ci/macos/grpc_distribtests_python.sh
@@ -29,8 +29,8 @@ source tools/internal_ci/helper_scripts/prepare_build_macos_rc
 python3.9 -m pip install -U 'cython<4.0.0rc1' setuptools==65.4.1 wheel --user
 python3.10 -m pip install -U 'cython<4.0.0rc1' setuptools==65.4.1 wheel --user
 python3.11 -m pip install -U 'cython<4.0.0rc1' setuptools==65.4.1 wheel --user
-python3.12 -m pip install -U 'cython<4.0.0rc1' setuptools==65.4.1 wheel --user
-python3.13 -m pip install -U 'cython<4.0.0rc1' setuptools==65.4.1 wheel --user
+python3.12 -m pip install -U 'cython<4.0.0rc1' setuptools==65.4.1 wheel --user --break-system-packages
+python3.13 -m pip install -U 'cython<4.0.0rc1' setuptools==65.4.1 wheel --user --break-system-packages
 
 # Build all python macos artifacts (this step actually builds all the binary wheels and source archives)
 tools/run_tests/task_runner.py -f artifact macos python ${TASK_RUNNER_EXTRA_FILTERS} -j 2 -x build_artifacts/sponge_log.xml || FAILED="true"
