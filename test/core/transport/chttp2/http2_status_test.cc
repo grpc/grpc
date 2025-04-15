8e44d1d855bfc8878af186db24cdf84e1ff119d4
tanvi-jagtap <tjagtap@google.com>
2025-04-15 06:40:17 +0000

    [PH2]

--- test/core/transport/chttp2/http2_status_test.cc
+++ test/core/transport/chttp2/http2_status_test.cc
@@ -18,8 +18,11 @@
 
 #include "src/core/ext/transport/chttp2/transport/http2_status.h"
 
+#include <string>
+
 #include "absl/log/check.h"
 #include "absl/log/log.h"
+#include "absl/status/status.h"
 #include "gtest/gtest.h"
 #include "src/core/ext/transport/chttp2/transport/frame.h"
 
@@ -27,23 +30,51 @@ namespace grpc_core {
 namespace http2 {
 namespace testing {
 
-// Constructor tests first create the specific Http2Status object.
-// Then check the following
+///////////////////////////////////////////////////////////////////////////////
+// Http2Status Tests
+// These tests first create the specific type of Http2Status object.
+// Then check the following:
 // 1. Http2ErrorType
 // 2. Http2ErrorCode
 // 3. message
 // 4. Absl status
-// 5. For a small subset of tests, check that extracting the wrong
-TEST(Http2StatusTest, ConstructorOkTest) {}
 
-TEST(Http2StatusTest, ConstructorHttp2ConnectionErrorTest) {}
+TEST(Http2StatusTest, OkTest) { CHECK(true); }
 
-TEST(Http2StatusTest, ConstructorHttp2StreamErrorTest) {}
+TEST(Http2StatusTest, Http2ConnectionErrorTest) { CHECK(true); }
 
-TEST(Http2StatusTest, ConstructorAbslConnectionErrorTest) {}
+TEST(Http2StatusTest, Http2StreamErrorTest) { CHECK(true); }
 
-TEST(Http2StatusTest, ConstructorAbslStreamErrorTest) {}
+TEST(Http2StatusTest, AbslConnectionErrorTest) { CHECK(true); }
 
+TEST(Http2StatusTest, AbslStreamErrorTest) { CHECK(true); }
+
+TEST(Http2StatusTest, CrashForWrongType1) {
+  // Check that extracting the wrong error type should crash.
+  CHECK(true);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// ValueOrHttp2Status Tests
+
+TEST(ValueOrHttp2Status, ValuePrimitiveDataType) { CHECK(true); }
+
+TEST(ValueOrHttp2Status, ValueHttp2DataFrame) { CHECK(true); }
+
+TEST(ValueOrHttp2Status, ValueWindowUpdateFrame) { CHECK(true); }
+
+TEST(ValueOrHttp2Status, ValueStdString) { CHECK(true); }
+
+TEST(ValueOrHttp2Status, ValueHttp2Frame) { CHECK(true); }
+
+TEST(ValueOrHttp2Status, ConnectionError) { CHECK(true); }
+
+TEST(ValueOrHttp2Status, StreamError) { CHECK(true); }
+
+TEST(ValueOrHttp2Status, CrashForWrongType2) {
+  // Check that extracting the wrong error type should crash.
+  CHECK(false);
+}
 
 }  // namespace testing
 }  // namespace http2
