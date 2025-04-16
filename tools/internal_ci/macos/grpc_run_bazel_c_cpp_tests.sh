d91a9ba22a3a603b0f0c87ddef201d7814731538
Pawan Bhardwaj <bpawan@google.com>
2025-04-16 08:22:22 +0000

    Reduce time

--- tools/internal_ci/macos/grpc_run_bazel_c_cpp_tests.sh
+++ tools/internal_ci/macos/grpc_run_bazel_c_cpp_tests.sh
@@ -49,4 +49,4 @@ bazel_c_cpp_tests/bazel_wrapper \
   --google_credentials="${KOKORO_GFILE_DIR}/GrpcTesting-d0eeee2db331.json" \
   "${BAZEL_REMOTE_CACHE_ARGS[@]}" \
   $BAZEL_FLAGS \
-  -- //test/...
+  -- //test/core/address_utils:sockaddr_utils_test
