5f11124b834f01746992a428de6bb107be34f223
Yevgen Ostroukhov <eostroukhov@google.com>
2025-04-17 10:24:47 -0700

    fixup: mac build

--- src/core/lib/event_engine/posix_engine/ev_epoll1_linux.cc
+++ src/core/lib/event_engine/posix_engine/ev_epoll1_linux.cc
@@ -537,7 +537,7 @@ Poller::WorkResult Epoll1Poller::Work(
 
 void Epoll1Poller::Kick() { grpc_core::Crash("unimplemented"); }
 
-void Epoll1Poller::AdvanceGeneration() { grpc_core::Crash("unimplemented"); }
+void Epoll1Poller::HandleForkInChild() { grpc_core::Crash("unimplemented"); }
 
 void Epoll1Poller::ResetKickState() { grpc_core::Crash("unimplemented"); }
 
