9c62e9005c64aacd265c33238ead572858564870
Craig Tiller <ctiller@google.com>
2025-04-18 22:35:16 -0700

    x

--- src/python/grpcio_observability/python_version.py
+++ src/python/grpcio_observability/python_version.py
@@ -1,20 +1,22 @@
-# Copyright 2024 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+%YAML 1.2
+--- |
+  # Copyright 2024 gRPC authors.
+  #
+  # Licensed under the Apache License, Version 2.0 (the "License");
+  # you may not use this file except in compliance with the License.
+  # You may obtain a copy of the License at
+  #
+  #     http://www.apache.org/licenses/LICENSE-2.0
+  #
+  # Unless required by applicable law or agreed to in writing, software
+  # distributed under the License is distributed on an "AS IS" BASIS,
+  # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  # See the License for the specific language governing permissions and
+  # limitations under the License.
 
-# AUTO-GENERATED FROM `$REPO_ROOT/templates/src/python/grpcio_observability/python_version.py.template`!!!
+  # AUTO-GENERATED FROM `$REPO_ROOT/templates/src/python/grpcio_observability/python_version.py.template`!!!
 
-SUPPORTED_PYTHON_VERSIONS = ['3.9', '3.10', '3.11', '3.12', '3.13']
+  SUPPORTED_PYTHON_VERSIONS = ${settings.supported_python_versions}
 
-MIN_PYTHON_VERSION = 3.9
-MAX_PYTHON_VERSION = 3.13
+  MIN_PYTHON_VERSION = ${settings.min_python_version}
+  MAX_PYTHON_VERSION = ${settings.max_python_version}
