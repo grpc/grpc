732122d50942f0b9bf51673de60ba993c91e832c
sreenithi <ssreenithi@google.com>
2025-04-23 15:11:15 +0000

    Updated Python Bazel tests to use the new config

--- tools/internal_ci/linux/grpc_python_bazel_test_fork_in_docker.sh
+++ tools/internal_ci/linux/grpc_python_bazel_test_fork_in_docker.sh
@@ -16,7 +16,7 @@
 set -ex
 
 RESULTSTORE_RESULTS_FLAG="--bazelrc=tools/remote_build/include/test_locally_with_resultstore_results.bazelrc"
-BAZEL_FLAGS="--test_output=errors"
+BAZEL_FLAGS="--test_output=errors --config=python_defines"
 
 python3 tools/run_tests/python_utils/bazel_report_helper.py --report_path python_bazel_tests_fork_support
 
