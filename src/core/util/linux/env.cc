a68f939c1be269bd1460ca8901e3d1cecbec3e0d
Pawan Bhardwaj <bpawan@google.com>
2025-04-23 07:11:10 +0000

    sanity

--- src/core/util/linux/env.cc
+++ src/core/util/linux/env.cc
@@ -37,7 +37,8 @@ namespace grpc_core {
 
 std::optional<std::string> GetEnv(const char* name) {
   char* result = nullptr;
-#if (__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 17)) && !defined(GRPC_FORCE_INSECURE_GETENV)
+#if (__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 17)) && \
+    !defined(GRPC_FORCE_INSECURE_GETENV)
   result = secure_getenv(name);
 #else
   result = getenv(name);
