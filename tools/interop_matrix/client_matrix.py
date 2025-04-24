6e2d58f7d8b13d821b5ea5043fc0bad4e68db2bf
Purnesh Dixit <purneshdixit@google.com>
2025-04-24 11:40:51 -0700

    [interop] Add v1.71.1, v1.72.0 release of grpc-go to interop matrix (#39352)
    ```
    gcloud beta container images list-tags us-docker.pkg.dev/grpc-testing/testing-images-public/grpc_interop_go1.x | grep "v1.71.1"
    ca19fe30458c  infrastructure-public-image-v1.71.1,v1.71.1                                                                    2025-04-23T17:15:00  CRITICAL=1,HIGH=1,LOW=96,MEDIUM=3
    ```
    
    ```
    gcloud beta container images list-tags us-docker.pkg.dev/grpc-testing/testing-images-public/grpc_interop_go1.x | grep "v1.72.0"
    6fba6b448704  infrastructure-public-image-v1.72.0,v1.72.0
    ```
    
    Closes #39352
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/39352 from grpc:go_release 2eadb3958c0683c5e3fac08e9c5e6431d14fe5b2
    PiperOrigin-RevId: 751073140

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -314,7 +314,8 @@ LANG_RELEASE_MATRIX = {
             ("v1.68.2", ReleaseInfo()),
             ("v1.69.4", ReleaseInfo()),
             ("v1.70.0", ReleaseInfo()),
-            ("v1.71.0", ReleaseInfo()),
+            ("v1.71.1", ReleaseInfo()),
+            ("v1.72.0", ReleaseInfo()),
         ]
     ),
     "java": OrderedDict(
