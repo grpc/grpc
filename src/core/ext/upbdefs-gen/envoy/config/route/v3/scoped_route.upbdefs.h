5e4218e83321867f4e74bd6af49749107d7c32bc
Esun Kim <veblush@google.com>
2025-04-25 14:22:12 -0700

    gen_upb_files

--- src/core/ext/upbdefs-gen/envoy/config/route/v3/scoped_route.upbdefs.h
+++ src/core/ext/upbdefs-gen/envoy/config/route/v3/scoped_route.upbdefs.h
@@ -6,13 +6,16 @@
  * regenerated.
  * NO CHECKED-IN PROTOBUF GENCODE */
 
+
+
 #ifndef ENVOY_CONFIG_ROUTE_V3_SCOPED_ROUTE_PROTO_UPB_H__UPBDEFS_H_
 #define ENVOY_CONFIG_ROUTE_V3_SCOPED_ROUTE_PROTO_UPB_H__UPBDEFS_H_
 
 #include "upb/reflection/def.h"
 #include "upb/reflection/internal/def_pool.h"
 
-#include "upb/port/def.inc" // Must be last.
+#include "upb/port/def.inc"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -33,6 +36,7 @@ UPB_INLINE const upb_MessageDef *envoy_config_route_v3_ScopedRouteConfiguration_
   _upb_DefPool_LoadDefInit(s, &envoy_config_route_v3_scoped_route_proto_upbdefinit);
   return upb_DefPool_FindMessageByName(s, "envoy.config.route.v3.ScopedRouteConfiguration.Key.Fragment");
 }
+
 
 #ifdef __cplusplus
 }  /* extern "C" */
