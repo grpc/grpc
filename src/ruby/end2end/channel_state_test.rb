d93cdb27f5d1a8477775ced3d2dae57c933d8a5b
Alexander Polcyn <apolcyn@google.com>
2025-04-27 07:51:11 +0000

    updates

--- src/ruby/end2end/channel_state_test.rb
+++ src/ruby/end2end/channel_state_test.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# Copyright 2016 gRPC authors.
+# Copyright 2015 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,38 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# make sure that the client doesn't freeze when process ended abruptly
-
 require_relative './end2end_common'
 
 def main
-  STDERR.puts 'start server'
-  server_runner = ServerRunner.new(EchoServerImpl)
-  server_port = server_runner.run
-  STDERR.puts 'start client'
-  client_controller = ClientController.new(
-    'channel_state_client.rb', server_port)
-  # sleep to allow time for the client to get into
-  # the middle of a "watch connectivity state" call
-  sleep 3
-  STDERR.puts 'kill child'
-  Process.kill('SIGTERM', client_controller.client_pid)
-
-  begin
-    Timeout.timeout(120) { Process.wait(client_controller.client_pid) }
-  rescue Timeout::Error
-    STDERR.puts "timeout wait for client pid #{client_controller.client_pid}"
-    Process.kill('SIGKILL', client_controller.client_pid)
-    Process.wait(client_controller.client_pid)
-    STDERR.puts 'killed client child'
-    raise 'Timed out waiting for client process. ' \
-           'It likely freezes when ended abruptly'
+#  parent_controller_port = ''
+#  server_port = ''
+#  OptionParser.new do |opts|
+#    opts.on('--parent_controller_port=P', String) do |p|
+#      parent_controller_port = p
+#    end
+#    opts.on('--server_port=P', String) do |p|
+#      server_port = p
+#    end
+#  end.parse!
+#  report_controller_port_to_parent(parent_controller_port, 0)
+#
+  # point to a non-existant server on IPv6 discard prefix
+  ch = GRPC::Core::Channel.new("[0100::]:80", {},
+                               :this_channel_is_insecure)
+  thr = Thread.new do
+    loop do
+      state = ch.connectivity_state
+      ch.watch_connectivity_state(state, Time.now + 360)
+    end
   end
-
-  # The interrupt in the child process should cause it to
-  # exit a non-zero status, so don't check it here.
-  # This test mainly tries to catch deadlock.
-  server_runner.stop
+  # sleep to allow time to get into the middle of a
+  # connectivity state watch operation
+  sleep 0.1
+  thr.kill
+  thr.join
 end
 
 main
