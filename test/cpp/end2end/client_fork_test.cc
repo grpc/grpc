ab090cac3c319100364b5fe474733d817527e81d
Eugene Ostroukhov <eostroukhov@google.com>
2025-05-05 15:16:48 -0700

    fixup: separate the connections in the test

--- test/cpp/end2end/client_fork_test.cc
+++ test/cpp/end2end/client_fork_test.cc
@@ -33,8 +33,11 @@ int main(int /* argc */, char** /* argv */) { return 0; }
 #include <signal.h>
 #include <unistd.h>
 
+#include <thread>
+
 #include "absl/log/log.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/substitute.h"
 #include "src/core/util/debug_location.h"
 #include "src/core/util/fork.h"
 #include "src/core/util/sync.h"
@@ -46,6 +49,76 @@ int main(int /* argc */, char** /* argv */) { return 0; }
 
 namespace grpc::testing {
 namespace {
+
+class HangWatcher {
+ public:
+  HangWatcher(absl::string_view name, absl::Duration period)
+      : name_(name), sleep_duration_(period) {
+    thread_ = std::thread(&HangWatcher::Loop, this);
+    grpc_core::MutexLock lock(&mu_);
+    while (state_ == 0) {
+      cond_.Wait(&mu_);
+    }
+    LOG(INFO).WithThreadID(getpid())
+        << "####################### " << name_ << " started";
+  }
+
+  ~HangWatcher() {
+    {
+      grpc_core::MutexLock lock(&mu_);
+      state_ = 2;
+      cond_.SignalAll();
+    }
+    thread_.join();
+    LOG(INFO).WithThreadID(getpid())
+        << "####################### " << name_ << " done";
+  }
+
+  void SetTask(absl::string_view task, grpc_core::SourceLocation location =
+                                           grpc_core::SourceLocation()) {
+    grpc_core::MutexLock lock(&mu_);
+    task_ = task;
+    location_ = location;
+  }
+
+  std::string label() const {
+    absl::MutexLock lock(&mu_);
+    return absl::StrCat(name_, "/", task_);
+  }
+
+ private:
+  void Loop() {
+    bool done = false;
+    {
+      grpc_core::MutexLock lock(&mu_);
+      state_ = 1;
+      cond_.SignalAll();
+    }
+    while (!done) {
+      absl::SleepFor(sleep_duration_);
+      {
+        grpc_core::MutexLock lock(&mu_);
+        done = state_ == 2;
+        if (!done) {
+          auto location = location_.value_or(grpc_core::SourceLocation());
+          LOG(INFO)
+                  .AtLocation(location.file(), location.line())
+                  .WithThreadID(getpid())
+              << name_ << " is still going, " << task_;
+        }
+      }
+    }
+  }
+
+  std::string name_;
+  std::string task_ ABSL_GUARDED_BY(mu_);
+  std::optional<grpc_core::SourceLocation> location_ ABSL_GUARDED_BY(mu_);
+  absl::Duration sleep_duration_;
+  std::thread thread_;
+  mutable grpc_core::Mutex mu_;
+  int state_ ABSL_GUARDED_BY(mu_) = 0;
+  grpc_core::CondVar cond_;
+};
 
 class EchoClientBidiReactor
     : public grpc::ClientBidiReactor<EchoRequest, EchoResponse> {
@@ -109,49 +182,91 @@ class ServiceImpl final : public EchoTestService::Service {
   }
 };
 
+std::shared_ptr<Channel> CreateChannel(const std::string& addr) {
+  ChannelArguments args;
+  // Using global pool makes this test flaky as first call post-fork may fail.
+  args.SetInt(GRPC_ARG_USE_LOCAL_SUBCHANNEL_POOL, 1);
+  return grpc::CreateCustomChannel(addr, InsecureChannelCredentials(), args);
+}
+
 std::pair<std::string, std::string> DoExchangeAsync(absl::string_view label,
-                                                    const std::string& addr) {
+                                                    const std::string& addr,
+                                                    HangWatcher* watcher) {
   EchoRequest request;
   EchoResponse response;
   ClientContext context;
   context.set_wait_for_ready(true);
-  std::unique_ptr<EchoTestService::Stub> stub = EchoTestService::NewStub(
-      grpc::CreateChannel(addr, InsecureChannelCredentials()));
+  if (watcher != nullptr) {
+    watcher->SetTask("starting write");
+  }
+  std::unique_ptr<EchoTestService::Stub> stub =
+      EchoTestService::NewStub(CreateChannel(addr));
   EchoClientBidiReactor reactor;
   stub->async()->BidiStream(&context, &reactor);
-  request.set_message("Hello again from child");
+  request.set_message(absl::Substitute(
+      "Hello again from [$0]",
+      watcher == nullptr ? std::to_string(getpid()) : watcher->label()));
   reactor.StartWrite(&request);
   reactor.StartRead(&response);
+  if (watcher != nullptr) {
+    watcher->SetTask("starting call");
+  }
   reactor.StartCall();
   VLOG(2) << label << " Doing the call";
+  if (watcher != nullptr) {
+    watcher->SetTask("waiting read write done");
+  }
   reactor.WaitReadWriteDone();
   reactor.StartWritesDone();
+  if (watcher != nullptr) {
+    watcher->SetTask("waiting all done");
+  }
   reactor.WaitAllDone();
   return {response.message(), request.message()};
 }
 
 std::pair<std::string, std::string> DoExchangeSync(absl::string_view label,
-                                                   const std::string& addr) {
+                                                   const std::string& addr,
+                                                   HangWatcher* watcher) {
   EchoRequest request;
   EchoResponse response;
   ClientContext context;
   context.set_wait_for_ready(true);
-  std::unique_ptr<EchoTestService::Stub> stub = EchoTestService::NewStub(
-      grpc::CreateChannel(addr, InsecureChannelCredentials()));
+  std::unique_ptr<EchoTestService::Stub> stub =
+      EchoTestService::NewStub(CreateChannel(addr));
   auto stream = stub->BidiStream(&context);
-  request.set_message("Hello again from child");
+  request.set_message(watcher == nullptr ? std::to_string(getpid())
+                                         : watcher->label());
+  if (watcher != nullptr) {
+    watcher->SetTask("write");
+  }
   stream->Write(request);
-  stream->Read(&response);
-  return {response.message(), request.message()};
+  if (watcher != nullptr) {
+    watcher->SetTask("read");
+  }
+  std::string response_message;
+  if (stream->Read(&response)) {
+    response_message = std::move(response).message();
+  } else {
+    response_message = stream->Finish().error_message();
+  }
+  return {std::move(response_message), std::move(request).message()};
 }
 
 void DoExchange(
-    absl::string_view label, const std::string& addr,
+    absl::string_view label, const std::string& addr, HangWatcher* watcher,
     grpc_core::SourceLocation location = grpc_core::SourceLocation()) {
-  const auto& [response, request] = DoExchangeAsync(label, addr);
-  EXPECT_EQ(response, request) << absl::StrCat(location);
-  const auto& [response_sync, request_sync] = DoExchangeSync(label, addr);
+  if (watcher != nullptr) {
+    watcher->SetTask("sync exchange");
+  }
+  const auto& [response_sync, request_sync] =
+      DoExchangeSync(label, addr, watcher);
   EXPECT_EQ(response_sync, request_sync) << absl::StrCat(location);
+  if (watcher != nullptr) {
+    watcher->SetTask("async exchange");
+  }
+  const auto& [response, request] = DoExchangeAsync(label, addr, watcher);
+  EXPECT_EQ(response, request) << absl::StrCat(location);
 }
 
 TEST(ClientForkTest, ClientCallsBeforeAndAfterForkSucceed) {
@@ -159,54 +274,43 @@ TEST(ClientForkTest, ClientCallsBeforeAndAfterForkSucceed) {
   int port = grpc_pick_unused_port_or_die();
   std::string addr = absl::StrCat("localhost:", port);
   pid_t server_pid = fork();
-  switch (server_pid) {
-    case -1:  // fork failed
-      GTEST_FAIL() << "failure forking";
-    case 0:  // post-fork child
-    {
-      VLOG(2) << "[" << getpid() << "] Starting server post first fork";
-      ServiceImpl impl;
-      grpc::ServerBuilder builder;
-      builder.AddListeningPort(addr, grpc::InsecureServerCredentials());
-      builder.RegisterService(&impl);
-      std::unique_ptr<Server> server(builder.BuildAndStart());
-      server->Wait();
-      return;
-    }
-    default:  // post-fork parent
-      break;
-  }
-  VLOG(2) << "[" << getpid() << "] ###########  First fork  ##########";
-  VLOG(2) << "[" << getpid() << "] ######  Client post fork 1   ######";
-  VLOG(2) << "[" << getpid() << "] ###################################";
-  // Do a round trip before we fork.
-  // NOTE: without this scope, test running with the epoll1 poller will fail.
-  DoExchange(absl::StrCat("[", getpid(), "] In first-fork parent"), addr);
-  // Fork and do round trips in the post-fork parent and child.
-  pid_t child_client_pid = fork();
-  switch (child_client_pid) {
-    case -1:  // fork failed
-      GTEST_FAIL() << "fork failed";
-    case 0:  // post-fork child
-    {
-      VLOG(2) << "[" << getpid() << "] In post-fork child";
-      DoExchange(absl::StrCat("[", getpid(), "] In post-fork child"), addr);
-      exit(0);
-    }
-    default:  // post-fork parent
-    {
-      VLOG(2) << "[" << getpid() << "] ##########  Second fork  ##########";
-      VLOG(2) << "[" << getpid() << "] ######  In post-fork parent  ######";
-      VLOG(2) << "[" << getpid() << "] ###################################";
-      DoExchange(absl::StrCat("[", getpid(), "] In post-fork parent"), addr);
-      // Wait for the post-fork child to exit; ensure it exited cleanly.
-      int child_status;
-      ASSERT_EQ(waitpid(child_client_pid, &child_status, 0), child_client_pid)
-          << "failed to get status of child client";
-      ASSERT_EQ(WEXITSTATUS(child_status), 0) << "child did not exit cleanly";
-    }
+  ASSERT_GE(server_pid, 0);
+  if (server_pid == 0) {
+    VLOG(2).WithThreadID(getpid()) << "####################### Starting server";
+    ServiceImpl impl;
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(addr, grpc::InsecureServerCredentials());
+    builder.RegisterService(&impl);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    server->Wait();
+    VLOG(2).WithThreadID(getpid()) << "####################### Server done";
+    return;
   }
 
+  // Do a round trip before we fork.
+  // NOTE: without this scope, test running with the epoll1 poller will fail.
+  DoExchange(absl::StrCat("[", getpid(), "] In first-fork parent"), addr,
+             nullptr);
+  // Fork and do round trips in the post-fork parent and child.
+  pid_t child_client_pid = fork();
+  // Spawns a thread. Not ok before fork!!!
+  HangWatcher watcher(child_client_pid == 0 ? "child" : "original",
+                      absl::Seconds(5));
+  ASSERT_GE(child_client_pid, 0) << "fork failed";
+  watcher.SetTask("message exchange");
+  DoExchange(absl::StrCat("[", getpid(), "] ", watcher.label()), addr,
+             &watcher);
+  // Nothing left for the child to do
+  if (child_client_pid == 0) {
+    return;
+  }
+  // Wait for the post-fork child to exit; ensure it exited cleanly.
+  int child_status;
+  watcher.SetTask("waiting for child shutdown");
+  ASSERT_EQ(waitpid(child_client_pid, &child_status, 0), child_client_pid)
+      << "failed to get status of child client";
+  EXPECT_EQ(WEXITSTATUS(child_status), 0) << "child did not exit cleanly";
+  watcher.SetTask("killing the server");
   kill(server_pid, SIGINT);
 }
 
