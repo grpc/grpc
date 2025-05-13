c1ec80ceaa3ef129a41184a983bc54c8bc848d64
sreenithi <ssreenithi@google.com>
2025-05-13 13:30:03 +0000

    temporary fix for copy conflicts using force flag

--- tools/run_tests/dockerize/build_and_run_docker.sh
+++ tools/run_tests/dockerize/build_and_run_docker.sh
@@ -216,7 +216,7 @@ fi
 # Copy contents of OUTPUT_DIR back under the git repo root
 if [ "${OUTPUT_DIR}" != "" ]
 then
-  cp -r "${TEMP_OUTPUT_DIR}/${OUTPUT_DIR}" "${git_root}" || DOCKER_EXIT_CODE=$?
+  cp -rf "${TEMP_OUTPUT_DIR}/${OUTPUT_DIR}" "${git_root}" || DOCKER_EXIT_CODE=$?
 fi
 
 exit $DOCKER_EXIT_CODE
