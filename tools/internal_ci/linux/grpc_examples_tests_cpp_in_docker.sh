42539707adfe4440faae593993c38f8f32288e2a
Pawan Bhardwaj <bpawan@google.com>
2025-05-14 05:55:27 +0000

    fix

--- tools/internal_ci/linux/grpc_examples_tests_cpp_in_docker.sh
+++ tools/internal_ci/linux/grpc_examples_tests_cpp_in_docker.sh
@@ -184,10 +184,10 @@ export GRPC_TRACE=http
 # Build using the define to force "getenv" instead of "secure_getenv"
 bazel build  --define GRPC_FORCE_UNSECURE_GETENV=1 //examples/cpp/helloworld:greeter_callback_client
 # Add linux capability
-sudo setcap cap_net_admin+ep ./bazel-bin/examples/cpp/helloworld/greeter_callback_client
-./bazel-bin/examples/cpp/helloworld/greeter_callback_client &> out.log
+setcap cap_net_admin+ep ./bazel-bin/examples/cpp/helloworld/greeter_callback_client
+output = $(./bazel-bin/examples/cpp/helloworld/greeter_callback_client 2>&1)
 # check if logs got enabled
-grep "gRPC Tracers:" out.log
+grep "gRPC Tracers:" $output
 return_code=$?
 if [ "$return_code" -ne 0 ]; then
     fail "Fail to read env variable with linux capability"
@@ -195,11 +195,11 @@ fi
 # Build without the define , this will use secure_getenv
 bazel build  //examples/cpp/helloworld:greeter_callback_client
 # Add linux capability
-sudo setcap cap_net_admin+ep ./bazel-bin/examples/cpp/helloworld/greeter_callback_client
-./bazel-bin/examples/cpp/helloworld/greeter_callback_client &> out.log
+setcap cap_net_admin+ep ./bazel-bin/examples/cpp/helloworld/greeter_callback_client
+output = $(./bazel-bin/examples/cpp/helloworld/greeter_callback_client 2>&1)
 # We should not see log get enabled as secure_getenv will return null in this case
-grep "gRPC Tracers:" out.log
-return_code=$?+
+grep "gRPC Tracers:" $output
+return_code=$?
 if [ "$return_code" -eq 0 ]; then
     fail "Able to read env variable with linux capability set"
 fi
