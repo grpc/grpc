f988240ecbaab6f15fcdb4db40d7e54a2a1334b8
Rishesh Agarwal <rishesh@google.com>
2025-05-14 14:58:54 +0000

    remove ruby ubuntu 20.04 version

--- tools/run_tests/artifacts/distribtest_targets.py
+++ tools/run_tests/artifacts/distribtest_targets.py
@@ -439,6 +439,7 @@ def targets():
             "linux", "x64", "debian11", use_dotnet_cli=True, presubmit=True
         ),
         CSharpDistribTest("linux", "x64", "ubuntu2204", use_dotnet_cli=True),
+        CSharpDistribTest("linux", "x64", "ubuntu2404", use_dotnet_cli=True),
         CSharpDistribTest(
             "linux", "x64", "alpine", use_dotnet_cli=True, presubmit=True
         ),
@@ -458,6 +459,7 @@ def targets():
         PythonDistribTest("linux", "x64", "arch"),
         PythonDistribTest("linux", "x64", "alpine"),
         PythonDistribTest("linux", "x64", "ubuntu2204"),
+        PythonDistribTest("linux", "x64", "ubuntu2404"),
         PythonDistribTest(
             "linux", "aarch64", "python39_buster", presubmit=True
         ),
@@ -474,6 +476,7 @@ def targets():
         PythonDistribTest("linux", "x64", "fedora40", source=True),
         PythonDistribTest("linux", "x64", "arch", source=True),
         PythonDistribTest("linux", "x64", "ubuntu2204", source=True),
+        PythonDistribTest("linux", "x64", "ubuntu2404", source=True),
         # Ruby
         RubyDistribTest(
             "linux",
@@ -506,7 +509,8 @@ def targets():
         RubyDistribTest(
             "linux", "x64", "debian11", ruby_version="ruby_3_4", presubmit=True
         ),
-        RubyDistribTest("linux", "x64", "ubuntu2204", presubmit=True),
+        RubyDistribTest("linux", "x64", "ubuntu2204"),
+        RubyDistribTest("linux", "x64", "ubuntu2404", presubmit=True),
         # PHP8
         PHP8DistribTest("linux", "x64", "debian12", presubmit=True),
         PHP8DistribTest("macos", "x64", presubmit=True),
