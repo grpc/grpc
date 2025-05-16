0f31b8434d9b409162953714189a35e972319360
Pawan Bhardwaj <bpawan@google.com>
2025-05-16 17:51:15 +0000

    sanatise

--- tools/internal_ci/linux/grpc_env_check.sh
+++ tools/internal_ci/linux/grpc_env_check.sh
@@ -1,5 +1,24 @@
+# Copyright 2025 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# For linux we use secure_getenv to read env variable
+# this return null if linux capabilites are added on the executable
+# For customer using linux capability , they need to define "--define GRPC_FORCE_UNSECURE_GETENV=1"
+# to read env variables.
 
 set +e
+set -x
 
 # install pre-requisites for gRPC C core build
 sudo apt update
@@ -18,24 +37,18 @@ pass () {
     echo "$(tput setaf 2) $1 $(tput sgr 0)"
 }
 
-# For linux we use secure_getenv to read env variable
-# this return null if linux capabilites are added on the executable
-# For customer using linux capability , they need to define "--define GRPC_FORCE_UNSECURE_GETENV=1"
-# to read env variables.
-# enable log
-
+#check presence of bazel
 tools/bazel version
 
+#enable log
+export GRPC_TRACE=http
 # Build without the define , this will use secure_getenv
 tools/bazel build  //examples/cpp/helloworld:greeter_callback_client
 # Add linux capability
 sudo setcap "cap_net_raw=ep" ./bazel-bin/examples/cpp/helloworld/greeter_callback_client
-getcap ./bazel-bin/examples/cpp/helloworld/greeter_callback_client
-export GRPC_TRACE=http
-./bazel-bin/examples/cpp/helloworld/greeter_callback_client &> wow.log
-cat wow.log
+./bazel-bin/examples/cpp/helloworld/greeter_callback_client &> out.log
 # We should not see log get enabled as secure_getenv will return null in this case
-grep "gRPC Tracers:" wow.log
+grep "gRPC Tracers:" out.log
 return_code=$?
 if [[ $return_code -eq 0 ]]; then
     fail "Able to read env variable with linux capability set"
@@ -46,10 +59,12 @@ tools/bazel build  --define GRPC_FORCE_UNSECURE_GETENV=1 //examples/cpp/hellowor
 # Add linux capability
 sudo setcap "cap_net_raw=ep" ./bazel-bin/examples/cpp/helloworld/greeter_callback_client
 ls -lrt ./bazel-bin/examples/cpp/helloworld/greeter_callback_client
-./bazel-bin/examples/cpp/helloworld/greeter_callback_client &> wow.log
-grep "gRPC Tracers:" wow.log
+./bazel-bin/examples/cpp/helloworld/greeter_callback_client &> out.log
+grep "gRPC Tracers:" out.log
 return_code=$?
 # check if logs got enabled
 if [[ ! $return_code -eq 0 ]]; then
     fail "Fail to read env variable with linux capability"
 fi
+
+echo "Test completed"
