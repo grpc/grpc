eb7184514a28fa5c1e09b9544456f4f689db988b
Tanvi Jagtap - Google LLC <139093547+tanvi-jagtap@users.noreply.github.com>
2025-05-18 22:13:47 -0700

    [PH2] Fix padding frame.cc code (#39574)
    [PH2] Fix padding frame.cc code
    
    Fixing 2 very similar checks into one check.
    
    Editing the unit tests to reflect the new error message.
    
    Closes #39574
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/39574 from tanvi-jagtap:ph2_frame_padding_weirdness 4e3befd733c11371b51f7b1183ca8bb19b52e903
    PiperOrigin-RevId: 760461620

--- src/core/ext/transport/chttp2/transport/frame.cc
+++ src/core/ext/transport/chttp2/transport/frame.cc
@@ -288,14 +288,11 @@ Http2Status StripPadding(const Http2FrameHeader& hdr, SliceBuffer& payload) {
         Http2ErrorCode::kProtocolError,
         absl::StrCat(RFC9113::kFrameParserIncorrectPadding, hdr.ToString()));
   }
+  const size_t payload_size = payload.Length();
   uint8_t padding_bytes;
   payload.MoveFirstNBytesIntoBuffer(1, &padding_bytes);
 
-  if (GPR_UNLIKELY(payload.Length() <= padding_bytes)) {
-    return Http2Status::Http2ConnectionError(
-        Http2ErrorCode::kProtocolError,
-        absl::StrCat(RFC9113::kFrameParserIncorrectPadding, hdr.ToString()));
-  } else if (GPR_UNLIKELY(hdr.length <= padding_bytes)) {
+  if (GPR_UNLIKELY(payload_size <= padding_bytes)) {
     return Http2Status::Http2ConnectionError(
         Http2ErrorCode::kProtocolError,
         absl::StrCat(RFC9113::kPaddingLengthLargerThanFrameLength,
