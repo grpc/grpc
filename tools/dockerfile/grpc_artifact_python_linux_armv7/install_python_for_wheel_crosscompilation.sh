c19a471beacf6fc3e8914dcaae7ef331986eba6a
Sergii Tkachenko <sergiitk@google.com>
2025-05-20 20:45:42 -0700

    [Python] Pin Cython to 3.1.1 (#39609)
    This solves several issues:
    1. #39588: Selecting unstable Cython releases when building gRPC Python artifacts.
    2. Using inconsistent Cython version in released platform-specific wheels within the same grpcio release.
    3. cython/cython#6878 Several published grpcio wheels were built with Cython 3.1.0 and are affected by Cython memory leak issue when using AsyncIO APIs (`grpc.aio.*`).
    
    This change will be backported to 1.73.x, 1.72.x and 1.71.x.
    
    Note: bazel Cython version wasn't upgraded because the latest cython BCR module version is still `3.0.11-1`.
    
    Closes #39609
    
    PiperOrigin-RevId: 761343405

--- tools/dockerfile/grpc_artifact_python_linux_armv7/install_python_for_wheel_crosscompilation.sh
+++ tools/dockerfile/grpc_artifact_python_linux_armv7/install_python_for_wheel_crosscompilation.sh
@@ -44,4 +44,4 @@ popd
 rm -rf "Python-${PYTHON_VERSION}"
 
 # install cython and wheel
-"${PYTHON_PREFIX}/bin/python3" -m pip install --upgrade 'cython<4.0.0rc1' wheel
+"${PYTHON_PREFIX}/bin/python3" -m pip install --upgrade 'cython==3.1.1' wheel
