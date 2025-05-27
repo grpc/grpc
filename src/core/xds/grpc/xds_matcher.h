68c1aeb50a542d5a811b17c1626dc0cc2610e037
Pawan Bhardwaj <bpawan@google.com>
2025-05-27 09:41:46 +0000

    Cleanup

--- src/core/xds/grpc/xds_matcher.h
+++ src/core/xds/grpc/xds_matcher.h
@@ -1,3 +1,17 @@
+// Copyright 2025 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef GRPC_SRC_CORE_XDS_GRPC_XDS_MATCHER_H
 #define GRPC_SRC_CORE_XDS_GRPC_XDS_MATCHER_H
 
@@ -15,52 +29,57 @@
 
 namespace matcher {
 
-int parseFunc(const xds_type_matcher_v3_Matcher* match);
+// Class For custom Match Data (Example CEL)
+class CustomMatchData {
+  public:
+    virtual ~CustomMatchData() = default;
+};
 
-using MatchDataType = std::string;
+// Type of data to match
+// std::monostate -> No data (empty)
+using MatchDataType = std::variant<std::monostate, std::string, std::shared_ptr<CustomMatchData>>;
 
-// Forward declaration
-template <class DataType>
-class MatcherList;
+// Forward declaration of Matcher Class
+template <class DataType> class Matcher;
+template <class DataType> using MatcherPtr = std::unique_ptr<Matcher<DataType>>;
 
-template<class DataType> using MatcherListPtr = std::unique_ptr<MatcherList<DataType>>;
-
-
+// Action Class definition
 class Action {
  public:
   virtual ~Action() = default;
+  virtual std::string_view typeUrl() = 0;
 };
 using ActionPtr = std::unique_ptr<Action>;
 using ActionCb = std::function<ActionPtr()>;
 
-template <class DataType>
-struct OnMatch {
+// OnMatch Class definition 
+// Contains either Action or subsequent matcher to match
+// AI : Should we create variant here ??
+template <class DataType> struct OnMatch {
   ActionCb actionCb_;
-  MatcherListPtr<DataType> matcher_;
+  MatcherPtr<DataType> matcher_;
+  // AI: CHeck usecase of keepMatching , currently unused
   bool keepMatching;
 };
 
-using MatchResult = bool;
-/*
-template <class DataType>
-struct MatchResult {
-  std::optional<bool> result;
+// Match Result to return
+// AI: Do we need to return result here ? We can return nullopt for fail case.
+template <class DataType> struct MatchResult {
+  bool result;
   std::optional<OnMatch<DataType>> onMatch;
 };
-*/
 
-template <class DataType>
-class Predicate {
+
+// Predicate
+template <class DataType> class Predicate {
  public:
   virtual ~Predicate() = default;
   virtual bool match(const DataType& data) = 0;
 };
+template <class DataType> using PredicatePtr = std::unique_ptr<Predicate<DataType>>;
 
-template <class DataType>
-using PredicatePtr = std::unique_ptr<Predicate<DataType>>;
-
-template <class DataType>
-class AndMatcher : public Predicate<DataType> {
+// AndMatcher : Return true if all the predicates return true else false
+template <class DataType> class AndMatcher : public Predicate<DataType> {
  public:
   explicit AndMatcher(std::vector<PredicatePtr<DataType>>&& matcherList)
       : matcherList_(std::move(matcherList)) {}
@@ -73,15 +92,14 @@ class AndMatcher : public Predicate<DataType> {
     }
     return true;
   }
-
  private:
   std::vector<PredicatePtr<DataType>> matcherList_;
 };
 
-template <class DataType>
-class AnyMatcher : public Predicate<DataType> {
+// OrMatcher : Return true if any of the predicates return true else false
+template <class DataType> class OrMatcher : public Predicate<DataType> {
  public:
-  explicit AnyMatcher(std::vector<PredicatePtr<DataType>>&& matcherList)
+  explicit OrMatcher(std::vector<PredicatePtr<DataType>>&& matcherList)
       : matcherList_(std::move(matcherList)) {}
   bool match(const DataType& data) {
     for (const auto& matcher : matcherList_) {
@@ -92,30 +110,32 @@ class AnyMatcher : public Predicate<DataType> {
     }
     return false;
   }
-
  private:
   std::vector<PredicatePtr<DataType>> matcherList_;
 };
 
+// Input matcher class to perform match (eg: StringMatcher ..) 
 class InputMatcher {
  public:
   virtual ~InputMatcher() = default;
   virtual bool match(const MatchDataType& input) = 0;
 };
 using InputMatcherPtr = std::unique_ptr<InputMatcher>;
-using InputMatcherFactoryCb = std::function<InputMatcherPtr()>;
 
-template <class DataType>
-class DataInput {
+// Class to extract data from the input
+// core.v3.TypedExtensionConfig
+template <class DataType> class DataInput {
  public:
   virtual ~DataInput() = default;
   // Interface to extract a string from DataType for matching.
   // Returns std::nullopt if the string is not available or not applicable.
   virtual std::optional<MatchDataType> GetInput(const DataType& data) = 0;
+  virtual std::string_view typeUrl() = 0;
 };
 template <class DataType> using DataInputPtr = std::unique_ptr<DataInput<DataType>>;
 
 
+// Matching of single predicate
 template <class DataType>
 class SinglePredicate : public Predicate<DataType> {
  public:
@@ -127,68 +147,86 @@ class SinglePredicate : public Predicate<DataType> {
       // Or handle as an error, depending on desired behavior
       return false; 
     }
-    std::optional<MatchDataType> input_str = data_input_->GetInput(data);
-    if (!input_str.has_value()) {
+    std::optional<MatchDataType> input_val = data_input_->GetInput(data);
+    if (!input_val.has_value()) {
       return false; // Or false, if not finding input means no match
     }
-    return input_matcher_->match(input_str.value());
+    return input_matcher_->match(input_val.value());
   }
 
  private:
+ // AI: do we need to have a correlation b/w data_input_ and input_matcher ?
+ // As input_matcher may not support the type returned from data_input
   DataInputPtr<DataType> data_input_;
   InputMatcherPtr input_matcher_;
 };
 
-template <class DataType>
-class MatcherList {
+// Base Matcher class
+template <class DataType> class Matcher {
+  public:
+    virtual ~Matcher() = default;
+    MatchResult<DataType> match(const DataType& data) = 0;
+};
+
+// List of Predicates
+// Return OnMatch for the corresponding Predicatematch else return onNoMatch if set 
+template <class DataType> class MatcherList : public Matcher<DataType> {
  public:
   void addMatcher(PredicatePtr<DataType> &&predicate, OnMatch<DataType> on_match) {
     fieldMatchers_.emplace_back(std::move(predicate), std::move(on_match));
   }
 
-  MatchResult match(const DataType& data) {
+  MatchResult<DataType> match(const DataType& data) override {
     for (auto& matcher_pair : fieldMatchers_) {
       const bool predicate_matched = matcher_pair.first->match(data);
       if (predicate_matched) {
-        matcher_pair.second.actionCb_();
-        if (!matcher_pair.second.keepMatching) {
-          return true;
-        }
+        return {true, matcher_pair.second};
       }
     }
-    if (onNoMatch_.has_value()) {
-      onNoMatch_->actionCb_();
-    }
-    return false;
+    // AI: check return values same as MatchResult should we just return the OnMatch (optional can indicate as false)
+    return {false, onNoMatch_};
   }
   private:
    std::vector<std::pair<PredicatePtr<DataType>, OnMatch<DataType>>> fieldMatchers_;
    std::optional<OnMatch<DataType>> onNoMatch_;
 };
 
-class HttpData : public DataInput<MatchDataType> {
-  public:
-  explicit HttpData(const std::string& data) : httpData(data) {}
-  std::optional<MatchDataType> GetInput(const std::string& data) override {
-    return httpData;
-  }
-  private:
-  std::string httpData;
-};
-
-// Concrete class for InputMatcher
-class ExactMatcher : public InputMatcher {
+// AI: will this support only string ??
+template <class DataType> class MatcherTree : public Matcher<DataType> {
 public:
-    explicit ExactMatcher(std::string exact_value) : exact_value_(std::move(exact_value)) {}
-
-    bool match(const MatchDataType& input) override {
-        return input == exact_value_;
+  MatchResult<DataType> match(const DataType& data) {
+    auto input_str = dataInput_->GetInput(data);
+    if (!input_str.has_value()) {
+      return {false, onNoMatch_};
     }
-
-private:
-    std::string exact_value_;
+    auto result = doMatch(data);
+    if (!result.has_value()) {
+      return {false, onNoMatch_};
+    }
+    if (result.value().matcher_ != nullptr) {
+      return result.value().matcher_->match(data);
+    }
+    return {true, result.value().onMatch_};
+  }
+ protected:
+  virtual std::optional<OnMatch<DataType>> doMatch(const std::string& data) = 0;
+  std::unordered_map<std::string, OnMatch<DataType>> map_;
+  DataInputPtr<DataType> dataInput_;
+  std::optional<OnMatch<DataType>> onNoMatch_;
 };
+
+template <class DataType> class ExactMatcherTree : public MatcherTree<DataType> {
+ protected:
+  std::optional<OnMatch<DataType>> doMatch(const std::string& data) override {
+    auto f = this->map_.find(data);
+    if (f != this->map_.end()) {
+      return f->second;
+    }
+    return std::nullopt;
+  }
+};
+
+// AI: Add prefix matcher for map (Based on string ?? )
 
 }  // namespace matcher
-
 #endif  // GRPC_SRC_CORE_XDS_GRPC_XDS_MATCHER_H
