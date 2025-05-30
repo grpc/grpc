98db026847c9e7b6c13345f60ab56ca738057aca
Adam Heller <aj@drfloob.com>
2025-05-30 12:41:11 -0700

    [core] Reduce overhead of backoff calculations (#39698)
    Closes #39698
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/39698 from drfloob:faster-wstp-backoff-impl 04eb8f39cfd33c2cdbd311db27e4f2034c16b59a
    PiperOrigin-RevId: 765296801

--- src/core/util/backoff.cc
+++ src/core/util/backoff.cc
@@ -22,9 +22,6 @@
 
 #include <algorithm>
 
-#include "src/core/lib/experiments/experiments.h"
-#include "src/core/util/shared_bit_gen.h"
-
 namespace grpc_core {
 
 BackOff::BackOff(const Options& options) : options_(options) { Reset(); }
@@ -36,9 +33,8 @@ Duration BackOff::NextAttemptDelay() {
     current_backoff_ *= options_.multiplier();
   }
   current_backoff_ = std::min(current_backoff_, options_.max_backoff());
-  SharedBitGen g;
   const double jitter =
-      absl::Uniform(g, 1 - options_.jitter(), 1 + options_.jitter());
+      absl::Uniform(bitgen_, 1 - options_.jitter(), 1 + options_.jitter());
   return current_backoff_ * jitter;
 }
 
