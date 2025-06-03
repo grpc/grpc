542607c984f8c52082f5b942b05b14746cf33be5
Vignesh Babu <vigneshbabu@google.com>
2025-06-03 15:52:20 -0700

    [export] [Filter Fusion] Ensure fused filters always instantiate an error interceptor latch in promise_based_filter
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/766853943](http://cl/766853943)
    
    PiperOrigin-RevId: 766853943

--- src/core/lib/channel/promise_based_filter.h
+++ src/core/lib/channel/promise_based_filter.h
@@ -196,7 +196,10 @@ struct CallHasAsyncErrorInterceptor<
 template <typename Derived>
 struct CallHasAsyncErrorInterceptor<
     Derived, std::enable_if_t<IsFusedFilter<Derived>::value>> {
-  static constexpr bool value = Derived::FusedFilterHasAsyncErrorInterceptor();
+  // Fused filter types return a promise, which resolves to a
+  // ServerMetadataHandle type. So we assume that an async error interceptor is
+  // always needed.
+  static constexpr bool value = true;
 };
 
 // Given a boolean X export a type:
