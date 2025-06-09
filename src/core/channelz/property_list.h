15e98b9338c2113bf24221c4ccb0633e5839a105
Craig Tiller <ctiller@google.com>
2025-06-09 11:48:52 -0700

    x

---
+++ src/core/channelz/property_list.h
@@ -0,0 +1,78 @@
+// Copyright 2025 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GRPC_SRC_CORE_CHANNELZ_PROPERTY_LIST_H
+#define GRPC_SRC_CORE_CHANNELZ_PROPERTY_LIST_H
+
+#include <type_traits>
+
+#include "absl/strings/string_view.h"
+#include "src/core/util/json/json.h"
+#include "src/core/util/string.h"
+#include "src/core/util/time.h"
+
+namespace grpc_core::channelz {
+
+// PropertyList contains a bag of key->value (for mostly arbitrary value types)
+// for reporting out state from channelz - the big idea is that you should be
+// able to call `PropertyList().Set("a", this->a)` and generate something that
+// channelz presenters can interpret.
+// It's currently defined in terms of JSON, but as we adopt channelz-v2 it's
+// expected we'll change this to capture protobuf directly.
+class PropertyList {
+ public:
+  PropertyList& Set(absl::string_view key, absl::string_view value) {
+    property_list_.emplace(key, Json::FromString(std::string(value)));
+    return *this;
+  }
+
+  PropertyList& Set(absl::string_view key, std::string value) {
+    property_list_.emplace(key, Json::FromString(std::move(value)));
+    return *this;
+  }
+
+  template <typename T>
+  std::enable_if_t<std::is_arithmetic_v<T>, PropertyList&> Set(
+      absl::string_view key, T value) {
+    property_list_.emplace(key, Json::FromNumber(value));
+    return *this;
+  }
+
+  PropertyList& Set(absl::string_view key, Json::Object obj) {
+    property_list_.emplace(key, Json::FromObject(std::move(obj)));
+    return *this;
+  }
+
+  PropertyList& Set(absl::string_view key, Duration dur) {
+    property_list_.emplace(key, Json::FromString(dur.ToJsonString()));
+    return *this;
+  }
+
+  PropertyList& Set(absl::string_view key, Timestamp ts) {
+    property_list_.emplace(key, Json::FromString(gpr_format_timespec(
+                                    ts.as_timespec(GPR_CLOCK_REALTIME))));
+    return *this;
+  }
+
+  // TODO(ctiller): remove soon, switch to something returning a protobuf.
+  Json::Object TakeJsonObject() { return std::move(property_list_); }
+
+ private:
+  // TODO(ctiller): switch to a protobuf representation
+  Json::Object property_list_;
+};
+
+}  // namespace grpc_core::channelz
+
+#endif
