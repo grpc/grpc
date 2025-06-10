419cbbbab91156d1344a02bd151f51e209e1d27b
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-10 13:58:16 +0530

    Upgrade pylint

---
+++ tools/distrib/pylint_code_updated.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Copyright 2017 The gRPC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+# NOTE(rbellevi): We ignore generated code.
+IGNORE_PATTERNS=--ignore-patterns='.*pb2\.py,.*pb2_grpc\.py'
+
+# change to root directory
+cd "$(dirname "$0")/../.."
+
+DIRS=(
+    'src/python/grpcio/grpc'
+)
+
+TEST_DIRS=(
+    'src/python/grpcio_tests/tests'
+    'src/python/grpcio_tests/tests_gevent'
+)
+
+VIRTUALENV=venv_python_code
+python3.11 -m venv $VIRTUALENV
+source $VIRTUALENV/bin/activate
+
+python3 -m pip install --upgrade astroid==3.3.10 \
+  pylint==3.3.7 \
+  toml==0.10.2 \
+  "isort>=4.3.0,<5.0.0"
+
+EXIT=0
+for dir in "${DIRS[@]}"; do
+  python3 -m pylint --rcfile=.pylintrc -rn "$dir" ${IGNORE_PATTERNS}  || EXIT=1
+done
+
+for dir in "${TEST_DIRS[@]}"; do
+  python3 -m pylint --rcfile=.pylintrc-tests -rn "$dir" ${IGNORE_PATTERNS} || EXIT=1
+done
+
+find examples/python \
+  -iname "*.py" \
+  -not -name "*_pb2.py" \
+  -not -name "*_pb2_grpc.py" \
+  | xargs python3 -m pylint --rcfile=.pylintrc-examples -rn ${IGNORE_PATTERNS}
+
+exit $EXIT
