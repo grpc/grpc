grpc/grpc

commit d7d6958546888bdb3303fd6d4619f4b85fd440fa

[URI] fix parsing of user_info in proxy settings (#39004)
Fixes #26548.

- fixed parsing of userinfo and host-port from URI authority component
- fixed how the user credentials and the server are parsed when retrieving the http proxy
- added tests

Closes #39004

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/39004 from edoardo-borsato:fix-26548-proxy-settings-ignored 1a9027632507f9671e19032a4ff2ff356e3e09fa
PiperOrigin-RevId: 748455016

src/core/util/uri.cc
@@ -103,6 +103,29 @@ bool IsAuthorityChar(char c) {
   return false;
 }
 
+// Returns true for any character in user_info, as defined in:
+// https://datatracker.ietf.org/doc/html/rfc3986#section-3.2.1
+bool IsUserInfoChar(char c) {
+  if (IsUnreservedChar(c)) return true;
+  if (IsSubDelimChar(c)) return true;
+  if (c == ':') return true;
+  return false;
+}
+
+// Returns true for any character in host_port, as defined in:
+// https://datatracker.ietf.org/doc/html/rfc3986#section-3.2.2
+bool IsHostPortChar(char c) {
+  if (IsUnreservedChar(c)) return true;
+  if (IsSubDelimChar(c)) return true;
+  switch (c) {
+    case ':':
+    case '[':
+    case ']':
+      return true;
+  }
+  return false;
+}
+
 // Returns true for any character in pchar, as defined in:
 // https://datatracker.ietf.org/doc/html/rfc3986#section-3.3
 bool IsPChar(char c) {
@@ -204,6 +227,13 @@ std::string URI::PercentDecode(absl::string_view str) {
   return out;
 }
 
+std::string URI::authority() const {
+  if (!user_info_.empty()) {
+    return absl::StrCat(user_info_, "@", host_port_);
+  }
+  return host_port_;
+}
+
 absl::StatusOr<URI> URI::Parse(absl::string_view uri_text) {
   absl::string_view remaining = uri_text;
   // parse scheme
@@ -225,10 +255,23 @@ absl::StatusOr<URI> URI::Parse(absl::string_view uri_text) {
   }
   remaining.remove_prefix(offset + 1);
   // parse authority
-  std::string authority;
+  std::string user_info;
+  std::string host_port;
   if (absl::ConsumePrefix(&remaining, "//")) {
     offset = remaining.find_first_of("/?#");
-    authority = PercentDecode(remaining.substr(0, offset));
+    absl::string_view encoded_authority = (remaining.substr(0, offset));
+    // parse user_info and host_port
+    absl::string_view encoded_user_info;
+    absl::string_view encoded_host_port;
+    size_t at_pos = encoded_authority.rfind('@');
+    if (at_pos == absl::string_view::npos) {
+      encoded_host_port = encoded_authority;
+    } else {
+      encoded_user_info = encoded_authority.substr(0, at_pos);
+      encoded_host_port = encoded_authority.substr(at_pos + 1);
+    }
+    user_info = PercentDecode(encoded_user_info);
+    host_port = PercentDecode(encoded_host_port);
     if (offset == remaining.npos) {
       remaining = "";
     } else {
@@ -279,26 +322,34 @@ absl::StatusOr<URI> URI::Parse(absl::string_view uri_text) {
     }
     fragment = PercentDecode(remaining);
   }
-  return URI(std::move(scheme), std::move(authority), std::move(path),
-             std::move(query_param_pairs), std::move(fragment));
+  return URI(std::move(scheme), std::move(user_info), std::move(host_port),
+             std::move(path), std::move(query_param_pairs),
+             std::move(fragment));
 }
 
-absl::StatusOr<URI> URI::Create(std::string scheme, std::string authority,
-                                std::string path,
+absl::StatusOr<URI> URI::Create(std::string scheme, std::string user_info,
+                                std::string host_port, std::string path,
                                 std::vector<QueryParam> query_parameter_pairs,
                                 std::string fragment) {
-  if (!authority.empty() && !path.empty() && path[0] != '/') {
+  if (!host_port.empty() && !path.empty() && path[0] != '/') {
     return absl::InvalidArgumentError(
-        "if authority is present, path must start with a '/'");
+        "if host_port is present, path must start with a '/'");
   }
-  return URI(std::move(scheme), std::move(authority), std::move(path),
-             std::move(query_parameter_pairs), std::move(fragment));
+  if (!user_info.empty() && host_port.empty()) {
+    return absl::InvalidArgumentError(
+        "if user_info is present, host_port must be present");
+  }
+  return URI(std::move(scheme), std::move(user_info), std::move(host_port),
+             std::move(path), std::move(query_parameter_pairs),
+             std::move(fragment));
 }
 
-URI::URI(std::string scheme, std::string authority, std::string path,
-         std::vector<QueryParam> query_parameter_pairs, std::string fragment)
+URI::URI(std::string scheme, std::string user_info, std::string host_port,
+         std::string path, std::vector<QueryParam> query_parameter_pairs,
+         std::string fragment)
     : scheme_(std::move(scheme)),
-      authority_(std::move(authority)),
+      user_info_(std::move(user_info)),
+      host_port_(std::move(host_port)),
       path_(std::move(path)),
       query_parameter_pairs_(std::move(query_parameter_pairs)),
       fragment_(std::move(fragment)) {
@@ -309,7 +360,8 @@ URI::URI(std::string scheme, std::string authority, std::string path,
 
 URI::URI(const URI& other)
     : scheme_(other.scheme_),
-      authority_(other.authority_),
+      user_info_(other.user_info_),
+      host_port_(other.host_port_),
       path_(other.path_),
       query_parameter_pairs_(other.query_parameter_pairs_),
       fragment_(other.fragment_) {
@@ -323,7 +375,8 @@ URI& URI::operator=(const URI& other) {
     return *this;
   }
   scheme_ = other.scheme_;
-  authority_ = other.authority_;
+  user_info_ = other.user_info_;
+  host_port_ = other.host_port_;
   path_ = other.path_;
   query_parameter_pairs_ = other.query_parameter_pairs_;
   fragment_ = other.fragment_;
@@ -350,9 +403,13 @@ std::string URI::ToString() const {
   std::vector<std::string> parts = {PercentEncode(scheme_, IsSchemeChar), ":"};
   // If path starts with '//' we need to encode the authority to ensure that
   // we can round-trip the URI through a parse/encode/parse loop.
-  if (!authority_.empty() || absl::StartsWith(path_, "//")) {
+  if (!host_port_.empty() || absl::StartsWith(path_, "//")) {
     parts.emplace_back("//");
-    parts.emplace_back(PercentEncode(authority_, IsAuthorityChar));
+    if (!user_info_.empty()) {
+      parts.emplace_back(PercentEncode(user_info_, IsUserInfoChar));
+      parts.emplace_back("@");
+    }
+    parts.emplace_back(PercentEncode(host_port_, IsHostPortChar));
   }
   parts.emplace_back(EncodedPathAndQueryParams());
   if (!fragment_.empty()) {

test/core/util/uri_fuzzer_test.cc
@@ -42,6 +42,10 @@ void ParseRoundTrips(std::string buffer) {
       << GRPC_DUMP_ARGS(absl::CEscape(buffer), absl::CEscape(buffer2));
   EXPECT_EQ(uri->authority(), uri2->authority())
       << GRPC_DUMP_ARGS(absl::CEscape(buffer), absl::CEscape(buffer2));
+  EXPECT_EQ(uri->user_info(), uri2->user_info())
+      << GRPC_DUMP_ARGS(absl::CEscape(buffer), absl::CEscape(buffer2));
+  EXPECT_EQ(uri->host_port(), uri2->host_port())
+      << GRPC_DUMP_ARGS(absl::CEscape(buffer), absl::CEscape(buffer2));
   EXPECT_EQ(uri->path(), uri2->path())
       << GRPC_DUMP_ARGS(absl::CEscape(buffer), absl::CEscape(buffer2));
   EXPECT_EQ(uri->query_parameter_pairs(), uri2->query_parameter_pairs())

src/core/credentials/call/external/aws_request_signer.cc
@@ -151,8 +151,9 @@ std::map<std::string, std::string> AwsRequestSigner::GetSignedRequestHeaders() {
   canonical_request_vector.emplace_back(query);
   canonical_request_vector.emplace_back("\n");
   // 4. CanonicalHeaders
+  std::string authority = url_.authority();
   if (request_headers_.empty()) {
-    request_headers_.insert({"host", url_.authority()});
+    request_headers_.insert({"host", authority});
     if (!token_.empty()) {
       request_headers_.insert({"x-amz-security-token", token_});
     }
@@ -198,7 +199,7 @@ std::map<std::string, std::string> AwsRequestSigner::GetSignedRequestHeaders() {
   string_to_sign_vector.emplace_back("\n");
   // 3. CredentialScope
   std::pair<absl::string_view, absl::string_view> host_parts =
-      absl::StrSplit(url_.authority(), absl::MaxSplits('.', 1));
+      absl::StrSplit(authority, absl::MaxSplits('.', 1));
   std::string service_name(host_parts.first);
   std::string credential_scope = absl::StrFormat(
       "%s/%s/%s/aws4_request", request_date_short, region_, service_name);

src/core/credentials/call/external/url_external_account_credentials.cc
@@ -146,8 +146,8 @@ UrlExternalAccountCredentials::RetrieveSubjectToken(
     Timestamp deadline,
     absl::AnyInvocable<void(absl::StatusOr<std::string>)> on_done) {
   auto url_for_request =
-      URI::Create(url_.scheme(), url_.authority(), url_full_path_,
-                  {} /* query params */, "" /* fragment */);
+      URI::Create(url_.scheme(), url_.user_info(), url_.host_port(),
+                  url_full_path_, {} /* query params */, "" /* fragment */);
   if (!url_for_request.ok()) {
     return MakeOrphanable<NoOpFetchBody>(
         event_engine(), std::move(on_done),

src/core/credentials/call/gcp_service_account_identity/gcp_service_account_identity_credentials.cc
@@ -180,7 +180,7 @@ GcpServiceAccountIdentityCallCredentials::StartHttpRequest(
   // channel. This would allow us to cancel an authentication query when under
   // extreme memory pressure.
   auto uri = URI::Create(
-      "http", "metadata.google.internal.",
+      "http", /*user_info=*/"", "metadata.google.internal.",
       "/computeMetadata/v1/instance/service-accounts/default/identity",
       {{"audience", audience_}}, /*fragment=*/"");
   CHECK_OK(uri);  // params are hardcoded

src/core/credentials/call/jwt/jwt_verifier.cc
@@ -756,8 +756,8 @@ static void on_openid_config_retrieved(void* user_data,
   // TODO(ctiller): Carry the resource_quota in ctx and share it with the host
   // channel. This would allow us to cancel an authentication query when under
   // extreme memory pressure.
-  uri = grpc_core::URI::Create("https", host, path, {} /* query params /*/,
-                               "" /* fragment */);
+  uri = grpc_core::URI::Create("https", /*user_info=*/"", host, path,
+                               {} /* query params /*/, "" /* fragment */);
   if (!uri.ok()) {
     goto error;
   }
@@ -886,8 +886,8 @@ static void retrieve_key_and_verify(verifier_cb_ctx* ctx) {
   // TODO(ctiller): Carry the resource_quota in ctx and share it with the host
   // channel. This would allow us to cancel an authentication query when under
   // extreme memory pressure.
-  uri = grpc_core::URI::Create("https", host, path, {} /* query params */,
-                               "" /* fragment */);
+  uri = grpc_core::URI::Create("https", /*user_info=*/"", host, path,
+                               {} /* query params */, "" /* fragment */);
   if (!uri.ok()) {
     goto error;
   }

src/core/credentials/call/oauth2/oauth2_credentials.cc
@@ -314,7 +314,8 @@ class grpc_compute_engine_token_fetcher_credentials
     // TODO(ctiller): Carry the memory quota in ctx and share it with the host
     // channel. This would allow us to cancel an authentication query when under
     // extreme memory pressure.
-    auto uri = grpc_core::URI::Create("http", GRPC_COMPUTE_ENGINE_METADATA_HOST,
+    auto uri = grpc_core::URI::Create("http", /*user_info=*/"",
+                                      GRPC_COMPUTE_ENGINE_METADATA_HOST,
                                       GRPC_COMPUTE_ENGINE_METADATA_TOKEN_PATH,
                                       {} /* query params */, "" /* fragment */);
     CHECK(uri.ok());  // params are hardcoded
@@ -372,7 +373,8 @@ grpc_google_refresh_token_credentials::StartHttpRequest(
   // TODO(ctiller): Carry the memory quota in ctx and share it with the host
   // channel. This would allow us to cancel an authentication query when under
   // extreme memory pressure.
-  auto uri = grpc_core::URI::Create("https", GRPC_GOOGLE_OAUTH2_SERVICE_HOST,
+  auto uri = grpc_core::URI::Create("https", /*user_info=*/"",
+                                    GRPC_GOOGLE_OAUTH2_SERVICE_HOST,
                                     GRPC_GOOGLE_OAUTH2_SERVICE_TOKEN_PATH,
                                     {} /* query params */, "" /* fragment */);
   CHECK(uri.ok());  // params are hardcoded

src/core/credentials/transport/google_default/google_default_credentials.cc
@@ -217,9 +217,9 @@ static int is_metadata_server_reachable() {
   detector.is_done = 0;
   detector.success = 0;
   memset(&request, 0, sizeof(grpc_http_request));
-  auto uri =
-      grpc_core::URI::Create("http", GRPC_COMPUTE_ENGINE_DETECTION_HOST, "/",
-                             {} /* query params */, "" /* fragment */);
+  auto uri = grpc_core::URI::Create("http", /*user_info=*/"",
+                                    GRPC_COMPUTE_ENGINE_DETECTION_HOST, "/",
+                                    {} /* query params */, "" /* fragment */);
   CHECK(uri.ok());  // params are hardcoded
   auto http_request = grpc_core::HttpRequest::Get(
       std::move(*uri), nullptr /* channel args */, &detector.pollent, &request,

commit 690f87c65ce199f78449c1602388e294e87cfc08

[URI] fix ToString() with user_info but no host_port (#39826)
Fixes a fuzzer failure introduced in #39004.

b/412887217

Closes #39826

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/39826 from markdroth:uri_fuzzer_fix fa16042821ae0822f1ae6519691f59db475bb151
PiperOrigin-RevId: 770171811

src/core/util/uri.cc
@@ -259,7 +259,7 @@ absl::StatusOr<URI> URI::Parse(absl::string_view uri_text) {
   std::string host_port;
   if (absl::ConsumePrefix(&remaining, "//")) {
     offset = remaining.find_first_of("/?#");
-    absl::string_view encoded_authority = (remaining.substr(0, offset));
+    absl::string_view encoded_authority = remaining.substr(0, offset);
     // parse user_info and host_port
     absl::string_view encoded_user_info;
     absl::string_view encoded_host_port;
@@ -404,7 +404,8 @@ std::string URI::ToString() const {
   std::vector<std::string> parts = {PercentEncode(scheme_, IsSchemeChar), ":"};
   // If path starts with '//' we need to encode the authority to ensure that
   // we can round-trip the URI through a parse/encode/parse loop.
-  if (!host_port_.empty() || absl::StartsWith(path_, "//")) {
+  if (!user_info_.empty() || !host_port_.empty() ||
+      absl::StartsWith(path_, "//")) {
     parts.emplace_back("//");
     if (!user_info_.empty()) {
       parts.emplace_back(PercentEncode(user_info_, IsUserInfoChar));

test/core/util/uri_fuzzer_test.cc
@@ -58,5 +58,9 @@ FUZZ_TEST(UriTest, ParseRoundTrips);
 
 TEST(UriTest, ParseRoundTripsRegression) { ParseRoundTrips("W:////\244"); }
 
+TEST(UriTest, UserInfoWithoutHostPort) {
+  ParseRoundTrips("foo://user_info@/path");
+}
+
 }  // namespace
 }  // namespace grpc_core