4261a7512dda271dd1f0c9b094b42af5e91e7246
Craig Tiller <ctiller@google.com>
2025-06-11 10:13:45 -0700

    Fix global subchannel not registering channels in some cases
    If the ref count on a channel drops to zero and we try to register a new subchannel in the same slot before the unregister completes we'll return no channel instead of registering the newly constructed channel.
    
    PiperOrigin-RevId: 770206102

--- src/core/client_channel/global_subchannel_pool.cc
+++ src/core/client_channel/global_subchannel_pool.cc
@@ -77,7 +77,10 @@ RefCountedPtr<Subchannel> GlobalSubchannelPool::RegisterSubchannel(
   SubchannelMap old_map2;
   MutexLock lock(&write_shard.mu);
   auto* existing = write_shard.map.Lookup(key);
-  if (existing != nullptr) return (*existing)->RefIfNonZero();
+  if (existing != nullptr) {
+    auto existing_ref = (*existing)->RefIfNonZero();
+    if (existing_ref != nullptr) return existing_ref;
+  }
   old_map1 = std::exchange(write_shard.map,
                            write_shard.map.Add(key, constructed->WeakRef()));
   MutexLock lock_read(&read_shard.mu);
