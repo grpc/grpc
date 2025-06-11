d150544dd3458bea71cfceb48a934ce323dcdc71
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-11 12:28:54 +0530

    create a new type

--- src/python/grpcio/grpc/_observability.py
+++ src/python/grpcio/grpc/_observability.py
@@ -36,6 +36,7 @@ _LOGGER = logging.getLogger(__name__)
 _channel = Any  # _channel.py imports this module.
 ClientCallTracerCapsule = TypeVar("ClientCallTracerCapsule")
 ServerCallTracerFactoryCapsule = TypeVar("ServerCallTracerFactoryCapsule")
+ServerCallTracerFactoryOption = Union[Tuple[Tuple[str, 'ServerCallTracerFactory']], Tuple[()]]
 
 _plugin_lock: threading.RLock = threading.RLock()
 _OBSERVABILITY_PLUGIN: Optional["ObservabilityPlugin"] = None
@@ -291,7 +292,7 @@ def maybe_record_rpc_latency(state: "_channel._RPCState") -> None:
 
 def create_server_call_tracer_factory_option(
     xds: bool,
-) -> Union[Tuple[Tuple[str, ServerCallTracerFactory]], Tuple[()]]:
+) -> ServerCallTracerFactoryOption:
     with get_plugin() as plugin:
         if plugin and plugin.stats_enabled:
             server_call_tracer_factory_address = (
