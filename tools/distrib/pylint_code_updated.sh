45d510e1ca7e34fc5ec12e42563c2b8953f7f50d
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-11 14:43:42 +0530

    revert pylint_code.sh

--- tools/distrib/pylint_code_updated.sh
+++ tools/distrib/pylint_code_updated.sh
@@ -33,8 +33,9 @@ VIRTUALENV=venv_python_code
 python3.11 -m virtualenv $VIRTUALENV
 source $VIRTUALENV/bin/activate
 
+# TODO(https://github.com/grpc/grpc/issues/23394): Update Pylint.
 python3 -m pip install --upgrade astroid==3.3.10 \
-  pylint==3.3.7 \
+  pylint==2.2.2 \
   toml==0.10.2 \
   "isort>=4.3.0,<5.0.0"
 
