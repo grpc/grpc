ff372d4129cf1dc509adf030a1f5d0c3ca88c164
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-16 13:57:57 +0000

    fix typing for 3.9

--- src/python/grpcio/grpc/_channel.py
+++ src/python/grpcio/grpc/_channel.py
@@ -15,8 +15,6 @@
 
 from __future__ import annotations
 
-from collections.abc import Iterator
-from collections.abc import Sequence
 import copy
 import functools
 import logging
@@ -24,8 +22,22 @@ import os
 import sys
 import threading
 import time
-import types
-from typing import Any, Callable, Optional, Union
+from typing import TYPE_CHECKING, Any, Callable, Optional, Union
+
+if TYPE_CHECKING:
+    import types
+    from collections.abc import Iterator
+    from collections.abc import Sequence
+    from grpc._typing import (
+        ChannelArgumentType,
+        DeserializingFunction,
+        IntegratedCallFactory,
+        MetadataType,
+        NullaryCallbackType,
+        ResponseType,
+        SerializingFunction,
+        UserTag,
+    )
 
 import grpc  # pytype: disable=pyi-error
 from grpc import _common  # pytype: disable=pyi-error
@@ -33,14 +45,6 @@ from grpc import _compression  # pytype: disable=pyi-error
 from grpc import _grpcio_metadata  # pytype: disable=pyi-error
 from grpc import _observability  # pytype: disable=pyi-error
 from grpc._cython import cygrpc
-from grpc._typing import ChannelArgumentType
-from grpc._typing import DeserializingFunction
-from grpc._typing import IntegratedCallFactory
-from grpc._typing import MetadataType
-from grpc._typing import NullaryCallbackType
-from grpc._typing import ResponseType
-from grpc._typing import SerializingFunction
-from grpc._typing import UserTag
 import grpc.experimental  # pytype: disable=pyi-error
 
 CallbackType = Callable[[grpc.ChannelConnectivity], None]
