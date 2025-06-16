aae22f3e78c76688b6899f3774eb652ff11b28ca
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-16 10:30:21 +0000

    revert pytype

--- tools/distrib/check_pytype.sh
+++ tools/distrib/check_pytype.sh
@@ -16,8 +16,9 @@
 JOBS=$(nproc) || JOBS=4
 
 VIRTUALENV=venv_check_pytype
-python3.11 -m virtualenv $VIRTUALENV
+python3.7 -m virtualenv $VIRTUALENV
 source $VIRTUALENV/bin/activate
 
-python3 -m pip install pytype==2024.10.11
+# TODO(xuanwn): update pytype version
+python3 -m pip install pytype==2019.11.27
 python3 -m pytype --keep-going -j "$JOBS" --strict-import --config "setup.cfg"
