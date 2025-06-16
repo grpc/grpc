284c2a88642159d45b234977fda1d1b0d868d653
Rishesh Agarwal <rishesh@google.com>
2025-06-16 16:24:55 +0000

    addressing review comments

--- include/grpc/grpc_posix.h
+++ include/grpc/grpc_posix.h
@@ -28,8 +28,6 @@
 
 #include <grpc/event_engine/event_engine.h>
 
-#include <memory>
-
 extern "C" {
 #endif
 
@@ -63,23 +61,6 @@ GRPCAPI void grpc_server_add_channel_from_fd(grpc_server* server, int fd,
 }
 
 namespace grpc_core::experimental {
-
-/**
- * EXPERIMENTAL API - Subject to change
- *
- * This function creates a gRPC channel using a pre-established
- * endpoint from the EventEngine. This API supports both secure and insecure
- * channel credentials.
- *
- * \param endpoint A unique pointer to an EventEngine endpoint representing
- *        an established connection.
- * \param creds The channel credentials used to secure the connection.
- * \param args Optional channel arguments to configure the channel behavior.
- */
-grpc_channel* CreateChannelFromEndpoint(
-    std::unique_ptr<grpc_event_engine::experimental::EventEngine::Endpoint>
-        endpoint,
-    grpc_channel_credentials* creds, const grpc_channel_args* args);
 
 /**
  * EXPERIMENTAL API - Subject to change
