8ce82380d116a2415d624a3c022cbaa40f023a78
sreenithi <ssreenithi@google.com>
2025-06-17 11:13:32 +0000

    add full path of initialize file instead of directory

--- setup.py
+++ setup.py
@@ -504,7 +504,7 @@ def cython_extensions_and_necessity():
                 + list(CYTHON_HELPER_C_FILES)
                 + core_c_files
                 + asm_files
-                + list(ABSL_INCLUDE)
+                + ["third_party/abseil-cpp/absl/log/initialize.cc"]
             ),
             include_dirs=list(EXTENSION_INCLUDE_DIRECTORIES),
             libraries=list(EXTENSION_LIBRARIES),
