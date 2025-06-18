fb51123a9b2e16aad9f69bd67727a9936050d6c3
Ashesh Vidyut <134911583+asheshvidyut@users.noreply.github.com>
2025-06-18 09:45:23 +0530

    Update tools/distrib/check_pytype_updated.sh
    Co-authored-by: Sergii Tkachenko <sergiitk@google.com>

--- tools/distrib/check_pytype_updated.sh
+++ tools/distrib/check_pytype_updated.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -ex
-# Copyright 2019 The gRPC Authors
+# Copyright 2025 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
