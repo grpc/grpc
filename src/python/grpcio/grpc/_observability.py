be4497e04863c50fad47bfa063950267345e50d6
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-18 19:20:06 +0530

    fix pytype on 3.7

--- src/python/grpcio/grpc/_observability.py
+++ src/python/grpcio/grpc/_observability.py
@@ -18,7 +18,7 @@ import abc
 import contextlib
 import logging
 import threading
-from typing import Any, Generator, Generic, List, Optional, TypeVar
+from typing import Any, Generator, Generic, List, Optional, TypeVar, Union
 
 from grpc._cython import cygrpc as _cygrpc
 from grpc._typing import ChannelArgumentType
@@ -283,7 +283,7 @@ def maybe_record_rpc_latency(state: "_channel._RPCState") -> None:
 
 def create_server_call_tracer_factory_option(
     xds: bool,
-) -> tuple[ChannelArgumentType, ...]:
+) -> Union[tuple[ChannelArgumentType], tuple[()]]:
     with get_plugin() as plugin:
         if plugin and plugin.stats_enabled:
             server_call_tracer_factory_address = (
