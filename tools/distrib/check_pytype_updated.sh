5765451716469096872b752ed6cfa10675b20d03
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-19 05:43:21 +0530

    address comments

--- tools/distrib/check_pytype_updated.sh
+++ tools/distrib/check_pytype_updated.sh
@@ -20,4 +20,4 @@ python3.11 -m virtualenv $VIRTUALENV
 source $VIRTUALENV/bin/activate
 
 pip install pytype==2024.10.11
-pytype -j auto --config=pytype.toml
+pytype --output=~/.cache/pytype --config=grpc-style-config.toml
