d435365e30624aa4e39cca67a8b59ce75352a33b
Craig Tiller <craig.tiller@gmail.com>
2025-06-23 15:32:44 -0700

    x

--- tools/codegen/core/gen_upb_api.sh
+++ tools/codegen/core/gen_upb_api.sh
@@ -34,7 +34,4 @@ else
 fi
 
 # generate upb files from bazel rules
-python3 tools/codegen/core/gen_upb_api_from_bazel_xml.py \
-  --upb_out=$UPB_OUTPUT_DIR \
-  --upbdefs_out=$UPBDEFS_OUTPUT_DIR \
-  --verbose
+tools/artifact_gen/gen_upb_api_from_bazel.sh --upb_out=$UPB_OUTPUT_DIR --upbdefs_out=$UPBDEFS_OUTPUT_DIR
