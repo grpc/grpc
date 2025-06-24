62c34feb6345bd08484f949c633b1a3cae600ea3
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-24 13:06:53 +0000

    add future import annotations

--- src/python/grpcio/grpc/aio/_interceptor.py
+++ src/python/grpcio/grpc/aio/_interceptor.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Interceptors implementation of gRPC Asyncio Python."""
+from __future__ import annotations
+
 from abc import ABCMeta
 from abc import abstractmethod
 import asyncio
