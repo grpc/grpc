ac1818d9e3f9d4fa5c97002500c2a7e32e53415f
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-24 10:23:56 +0000

    add future

--- src/python/grpcio/grpc/aio/_call.py
+++ src/python/grpcio/grpc/aio/_call.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Invocation-side implementation of gRPC Asyncio Python."""
+from __future__ import annotations
 
 import asyncio
 from collections.abc import AsyncIterator
