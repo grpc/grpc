c18a3a1a0d71e822548df87b77250082005de360
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-24 12:14:25 +0000

    revert back some imports

--- src/python/grpcio/grpc/aio/__init__.py
+++ src/python/grpcio/grpc/aio/__init__.py
@@ -16,8 +16,7 @@
 gRPC Async API objects may only be used on the thread on which they were
 created. AsyncIO doesn't provide thread safety for most of its APIs.
 """
-from __future__ import annotations
-
+import grpc
 from grpc._cython.cygrpc import AbortError
 from grpc._cython.cygrpc import BaseError
 from grpc._cython.cygrpc import EOF
