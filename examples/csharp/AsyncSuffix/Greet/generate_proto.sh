620c3891a90db3be418983baafddd02f4b7a8ecc
TN-GANESH <tnganesh@live.in>
2025-06-25 16:12:37 +0530

    Modified the example files
    The example files have been updated and corrected.

--- examples/csharp/AsyncSuffix/Greet/generate_proto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Generate without async suffix
-protoc --csharp_out=./Server/ --grpc_out=./Server/ --plugin=protoc-gen-grpc=grpc_csharp_plugin greet.proto
-
-# Generate with async suffix
-protoc --csharp_out=./Server/ --grpc_out=./Server/ --plugin=protoc-gen-grpc=grpc_csharp_plugin --grpc_opt=append_async_suffix=true greet.proto
-
-echo "Generated C# code with and without async suffixes"
