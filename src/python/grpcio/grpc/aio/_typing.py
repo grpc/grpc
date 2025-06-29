5aba993a2e8cdb062a484670384ee245a9ed5801
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-29 14:03:59 +0530

    removed unsed code

--- src/python/grpcio/grpc/aio/_typing.py
+++ src/python/grpcio/grpc/aio/_typing.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Common types for gRPC Async API"""
-from __future__ import annotations
-
 from typing import (
     Any,
     AsyncIterable,
