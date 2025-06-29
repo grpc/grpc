c524f8084361920ae5f44c53e50051b11e360059
Ashesh Vidyut <asheshvidyut@google.com>
2025-06-29 12:34:24 +0000

    fix import

--- src/python/grpcio/grpc/_compression.py
+++ src/python/grpcio/grpc/_compression.py
@@ -11,10 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import annotations
 
 from typing import Optional
 
-from __future__ import annotations
 import grpc
 from grpc._cython import cygrpc
 from grpc._typing import MetadataType
