4c9db9bd4154aacb97d935e056b817f6ae2a6aa1
Ashesh Vidyut <asheshvidyut@google.com>
2025-07-02 13:31:14 +0530

    add typing extension

--- setup.py
+++ setup.py
@@ -540,7 +540,9 @@ PACKAGE_DIRECTORIES = {
     "": PYTHON_STEM,
 }
 
-INSTALL_REQUIRES = ()
+INSTALL_REQUIRES = (
+    "typing-extensions>=4.0.0",
+)
 
 EXTRAS_REQUIRES = {
     "protobuf": "grpcio-tools>={version}".format(version=grpc_version.VERSION),
