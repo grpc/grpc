2b74feb930f48a339cc7c7898e2bf783ee87de95
Ashesh Vidyut <asheshvidyut@google.com>
2025-07-04 10:24:39 +0000

    revert ClientInterceptor change

--- src/python/grpcio/grpc/aio/_interceptor.py
+++ src/python/grpcio/grpc/aio/_interceptor.py
@@ -134,7 +134,7 @@ class ClientCallDetails(
     wait_for_ready: Optional[bool]
 
 
-class ClientInterceptor:
+class ClientInterceptor(metaclass=ABCMeta): # noqa: B024
     """Base class used for all Aio Client Interceptor classes"""
 
 
