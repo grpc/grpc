2a4bd06fad80c3d7fbd9ac5798f0c18c7145f715
sreenithi <ssreenithi@google.com>
2025-07-04 11:50:14 +0000

    rename variable as private

--- src/python/grpcio/grpc/_cython/cygrpc.pyx
+++ src/python/grpcio/grpc/_cython/cygrpc.pyx
@@ -76,13 +76,13 @@ include "_cygrpc/aio/call.pyx.pxi"
 include "_cygrpc/aio/channel.pyx.pxi"
 include "_cygrpc/aio/server.pyx.pxi"
 
-disable_absl_init_log = os.getenv("DISABLE_ABSL_INIT_LOG")
+_disable_absl_init_log = os.getenv("DISABLE_ABSL_INIT_LOG")
 
 #
 # initialize gRPC
 #
 cdef _initialize():
-  if not disable_absl_init_log:
+  if not _disable_absl_init_log:
     InitializeLog()
   grpc_set_ssl_roots_override_callback(
           <grpc_ssl_roots_override_callback>ssl_roots_override_callback)
