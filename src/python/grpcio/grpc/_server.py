d6a3960ad23825cb65b9163141cbed4e73907b90
Ashesh Vidyut <asheshvidyut@google.com>
2025-07-10 10:15:27 +0000

    removed suppression for EM103

--- src/python/grpcio/grpc/_server.py
+++ src/python/grpcio/grpc/_server.py
@@ -1395,9 +1395,9 @@ def _validate_generic_rpc_handlers(
     for generic_rpc_handler in generic_rpc_handlers:
         service_attribute = getattr(generic_rpc_handler, "service", None)
         if service_attribute is None:
+            error_msg = f'"{generic_rpc_handler}" must conform to grpc.GenericRpcHandler type but does not have "service" method!'
             raise AttributeError(
-                '"{}" must conform to grpc.GenericRpcHandler type but does '
-                'not have "service" method!'.format(generic_rpc_handler)
+                error_msg
             )
 
 
