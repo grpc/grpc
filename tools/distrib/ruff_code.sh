db0bf26f735c49c0e5f00de9d3682c4174232111
Ashesh Vidyut <asheshvidyut@google.com>
2025-07-12 02:26:10 +0000

    merge master

--- tools/distrib/ruff_code.sh
+++ tools/distrib/ruff_code.sh
@@ -19,6 +19,7 @@ set -ex
 cd "$(dirname "$0")/../.."
 
 DIRS=(
+<<<<<<< HEAD
    src/python/grpcio/grpc
    src/python/grpcio_channelz/grpc_channelz
    src/python/grpcio_health_checking/grpc_health
@@ -38,6 +39,25 @@ source $VIRTUALENV/bin/activate
 python3 --version
 
 pip install ruff==0.12.2
+=======
+    src/python/grpcio/grpc
+    src/python/grpcio_channelz/grpc_channelz
+    src/python/grpcio_health_checking/grpc_health
+    src/python/grpcio_reflection/grpc_reflection
+    src/python/grpcio_testing/grpc_testing
+    src/python/grpcio_status/grpc_status
+    src/python/grpcio_observability/grpc_observability
+    src/python/grpcio_csm_observability/grpc_csm_observability
+    src/python/grpcio_tests/tests
+    src/python/grpcio_tests/tests_gevent
+)
+
+VIRTUALENV=.venv_ruff
+python3.11 -m virtualenv $VIRTUALENV
+source $VIRTUALENV/bin/activate
+
+pip install ruff==0.12.1
+>>>>>>> 9eef0bb1c4 (add ruff check bash script)
 
 # Check if --fix flag is provided
 RUFF_COMMAND="ruff check"
@@ -45,4 +65,11 @@ if [[ "$1" == "--fix" ]]; then
     RUFF_COMMAND="ruff check --fix"
 fi
 
+<<<<<<< HEAD
 exec $RUFF_COMMAND "${DIRS[@]}"
+=======
+EXIT=0
+$RUFF_COMMAND "${DIRS[@]}" || EXIT=1
+
+exit $EXIT
+>>>>>>> 9eef0bb1c4 (add ruff check bash script)
