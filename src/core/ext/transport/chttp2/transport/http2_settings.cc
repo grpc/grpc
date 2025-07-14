b31499fe7368b350f57414c4c6803deee644a71c
tanvi-jagtap <tjagtap@google.com>
2025-07-14 11:23:12 +0000

    [PH2]

--- src/core/ext/transport/chttp2/transport/http2_settings.cc
+++ src/core/ext/transport/chttp2/transport/http2_settings.cc
@@ -23,7 +23,6 @@
 #include <grpc/support/port_platform.h>
 
 #include "absl/strings/str_cat.h"
-#include "src/core/ext/transport/chttp2/transport/frame.h"
 #include "src/core/ext/transport/chttp2/transport/http2_status.h"
 #include "src/core/util/useful.h"
 
