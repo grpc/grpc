3af3e0910587295af4f12865110d328c6a1f3131
Gregory Cooke <gregorycooke@google.com>
2025-07-22 05:55:49 -0700

    Automated rollback of commit 889aaeae794e3f7d8feac3f3615b388591d16dac.
    PiperOrigin-RevId: 785820361

--- src/core/credentials/transport/tls/ssl_utils.h
+++ src/core/credentials/transport/tls/ssl_utils.h
@@ -34,7 +34,6 @@
 #include "absl/status/status.h"
 #include "absl/strings/string_view.h"
 #include "src/core/credentials/transport/security_connector.h"
-#include "src/core/credentials/transport/tls/spiffe_utils.h"
 #include "src/core/lib/iomgr/error.h"
 #include "src/core/tsi/ssl/key_logging/ssl_key_logging.h"
 #include "src/core/tsi/ssl_transport_security.h"
@@ -85,8 +84,7 @@ const char** ParseAlpnStringIntoArray(absl::string_view preferred_protocols,
 
 // Initialize TSI SSL server/client handshaker factory.
 grpc_security_status grpc_ssl_tsi_client_handshaker_factory_init(
-    tsi_ssl_pem_key_cert_pair* key_cert_pair,
-    std::shared_ptr<RootCertInfo> root_cert_info,
+    tsi_ssl_pem_key_cert_pair* key_cert_pair, const char* pem_root_certs,
     bool skip_server_certificate_verification, tsi_tls_version min_tls_version,
     tsi_tls_version max_tls_version, tsi_ssl_session_cache* ssl_session_cache,
     tsi::TlsSessionKeyLoggerCache::TlsSessionKeyLogger* tls_session_key_logger,
@@ -96,7 +94,7 @@ grpc_security_status grpc_ssl_tsi_client_handshaker_factory_init(
 
 grpc_security_status grpc_ssl_tsi_server_handshaker_factory_init(
     tsi_ssl_pem_key_cert_pair* key_cert_pairs, size_t num_key_cert_pairs,
-    std::shared_ptr<RootCertInfo> root_cert_info,
+    const char* pem_root_certs,
     grpc_ssl_client_certificate_request_type client_certificate_request,
     tsi_tls_version min_tls_version, tsi_tls_version max_tls_version,
     tsi::TlsSessionKeyLoggerCache::TlsSessionKeyLogger* tls_session_key_logger,
