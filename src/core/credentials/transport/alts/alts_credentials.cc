0f1af44dea5be3b7b9440c22e638439adb6c9064
Yousuk Seung <ysseung@google.com>
2025-07-25 13:19:47 -0700

    Log error when null creds are returned
    PiperOrigin-RevId: 787218438

--- src/core/credentials/transport/alts/alts_credentials.cc
+++ src/core/credentials/transport/alts/alts_credentials.cc
@@ -25,6 +25,7 @@
 
 #include <utility>
 
+#include "absl/log/log.h"
 #include "src/core/credentials/transport/alts/alts_security_connector.h"
 #include "src/core/credentials/transport/alts/check_gcp_environment.h"
 #include "src/core/credentials/transport/alts/grpc_alts_credentials_options.h"
@@ -89,6 +90,8 @@ grpc_channel_credentials* grpc_alts_credentials_create_customized(
     const grpc_alts_credentials_options* options,
     const char* handshaker_service_url, bool enable_untrusted_alts) {
   if (!enable_untrusted_alts && !grpc_alts_is_running_on_gcp()) {
+    LOG(ERROR) << "ALTS creds ignored. Not running on GCP and untrusted ALTS "
+                  "is not enabled.";
     return nullptr;
   }
   return new grpc_alts_credentials(options, handshaker_service_url);
@@ -98,6 +101,8 @@ grpc_server_credentials* grpc_alts_server_credentials_create_customized(
     const grpc_alts_credentials_options* options,
     const char* handshaker_service_url, bool enable_untrusted_alts) {
   if (!enable_untrusted_alts && !grpc_alts_is_running_on_gcp()) {
+    LOG(ERROR) << "ALTS server creds ignored. Not running on GCP and untrusted "
+                  "ALTS is not enabled.";
     return nullptr;
   }
   return new grpc_alts_server_credentials(options, handshaker_service_url);
