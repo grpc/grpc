bb1780ce9be715452a52e909ca1cc59f2a59b184
Sergii Tkachenko <sergiitk@google.com>
2025-07-26 02:13:59 -0700

    use #!/usr/bin/env bash shebang

--- tools/distrib/check_pytype.sh
+++ tools/distrib/check_pytype.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -ex
+#!/usr/bin/env bash
 # Copyright 2025 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- tools/distrib/isort_code.sh
+++ tools/distrib/isort_code.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Copyright 2021 The gRPC Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
