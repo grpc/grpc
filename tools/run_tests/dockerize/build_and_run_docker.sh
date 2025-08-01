156f9c37a99266f5c32fc12e663629922b3db6b5
sreenithi <ssreenithi@google.com>
2025-08-01 09:23:34 +0000

    fix copy conflicts

--- tools/run_tests/dockerize/build_and_run_docker.sh
+++ tools/run_tests/dockerize/build_and_run_docker.sh
@@ -216,7 +216,9 @@ fi
 # Copy contents of OUTPUT_DIR back under the git repo root
 if [ "${OUTPUT_DIR}" != "" ]
 then
-  cp -r "${TEMP_OUTPUT_DIR}/${OUTPUT_DIR}" "${git_root}" || DOCKER_EXIT_CODE=$?
+  # ignore existing files like *tar.gz and *py3-none-any.whl files
+  cp -r --update=none "${TEMP_OUTPUT_DIR}/${OUTPUT_DIR}" \
+    "${git_root}" || DOCKER_EXIT_CODE=$?
 fi
 
 exit $DOCKER_EXIT_CODE
