993ac72b9ce6ffbf2641af21cfa3b419840f756c
tanvi-jagtap <tjagtap@google.com>
2025-08-07 09:39:56 +0000

    [PH2][Settings] Settings Timeout

--- test/core/transport/chttp2/http2_client_transport_test.cc
+++ test/core/transport/chttp2/http2_client_transport_test.cc
@@ -823,9 +823,7 @@ TEST_F(SettingsTimeoutManagerTest, NoTimeoutThreeSettings) {
                                      MockSettingsAckReceived(manager)),
              TryJoin<absl::StatusOr>(MockStartSettingsTimeout(manager),
                                      MockSettingsAckReceived(manager))),
-      [&notification](
-          absl::StatusOr<std::tuple<grpc_core::Empty, grpc_core::Empty>>
-              status) {
+      [&notification](absl::StatusOr<std::tuple<Empty, Empty>> status) {
         EXPECT_TRUE(status.ok());
         notification.Notify();
       });
@@ -849,9 +847,7 @@ TEST_F(SettingsTimeoutManagerTest, NoTimeoutThreeSettingsDelayed) {
                                      MockSettingsAckReceivedDelayed(manager)),
              TryJoin<absl::StatusOr>(MockStartSettingsTimeout(manager),
                                      MockSettingsAckReceivedDelayed(manager))),
-      [&notification](
-          absl::StatusOr<std::tuple<grpc_core::Empty, grpc_core::Empty>>
-              status) {
+      [&notification](absl::StatusOr<std::tuple<Empty, Empty>> status) {
         EXPECT_TRUE(status.ok());
         notification.Notify();
       });
@@ -901,9 +897,7 @@ TEST_F(SettingsTimeoutManagerTest, NoTimeoutThreeSettingsRareOrder) {
                                      MockStartSettingsTimeout(manager)),
              TryJoin<absl::StatusOr>(MockSettingsAckReceived(manager),
                                      MockStartSettingsTimeout(manager))),
-      [&notification](
-          absl::StatusOr<std::tuple<grpc_core::Empty, grpc_core::Empty>>
-              status) {
+      [&notification](absl::StatusOr<std::tuple<Empty, Empty>> status) {
         EXPECT_TRUE(status.ok());
         notification.Notify();
       });
@@ -927,9 +921,7 @@ TEST_F(SettingsTimeoutManagerTest, NoTimeoutThreeSettingsMixedOrder) {
                                      MockStartSettingsTimeout(manager)),
              TryJoin<absl::StatusOr>(MockStartSettingsTimeout(manager),
                                      MockSettingsAckReceived(manager))),
-      [&notification](
-          absl::StatusOr<std::tuple<grpc_core::Empty, grpc_core::Empty>>
-              status) {
+      [&notification](absl::StatusOr<std::tuple<Empty, Empty>> status) {
         EXPECT_TRUE(status.ok());
         notification.Notify();
       });
