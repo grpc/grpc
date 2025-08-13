48f9e2163a59c240acac8c6612365c12ddd66805
sreenithi <ssreenithi@google.com>
2025-08-13 12:13:41 +0000

    fix find_cmd error

--- tools/run_tests/artifacts/build_package_python.sh
+++ tools/run_tests/artifacts/build_package_python.sh
@@ -22,15 +22,18 @@ mkdir -p artifacts/
 # All the python packages have been built in the artifact phase already
 # and we only collect them here to deliver them to the distribtest phase.
 
-find "${EXTERNAL_GIT_ROOT}"/input_artifacts/ \
-    -maxdepth 1 \
-    -type d \
-    -name "${ARTIFACT_PREFIX}*" \
-    -not -name "${EXCLUDE_PATTERN}" \
-    -print0 \
-        | xargs -0 -I% find % -type f -maxdepth 1 \
-        -not -name "*.tar.gz" -not -name "*py3-none-any.whl" \
-        -exec cp -v {} ./artifacts \;
+find_cmd=(
+    find "${EXTERNAL_GIT_ROOT}/input_artifacts/"
+    -maxdepth 1
+    -type d
+    -name "${ARTIFACT_PREFIX}*"
+    -not -name "${EXCLUDE_PATTERN}"
+)
+
+"${find_cmd[@]}"-print0 \
+    | xargs -0 -I% find % -type f -maxdepth 1 \
+    -not -name "*.tar.gz" -not -name "*py3-none-any.whl" \
+    -exec cp -v {} ./artifacts \;
 
 # # Build the find command to include all files which start with ARTIFACT_PREFIX but does not match any of the EXCLUDE_PATTERNS
 # find_cmd=(
