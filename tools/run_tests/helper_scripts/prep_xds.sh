0872687f10178fe4274f348e5dc0435cafc2da9e
Sergii Tkachenko <sergiitk@google.com>
2025-08-13 21:09:12 -0700

    rollback prep_xds to non-venv mode

--- tools/run_tests/helper_scripts/prep_xds.sh
+++ tools/run_tests/helper_scripts/prep_xds.sh
@@ -19,33 +19,30 @@ set -ex
 # change to grpc repo root
 pushd "${KOKORO_ARTIFACTS_DIR}/github/grpc"
 
-# needrestart checks which daemons need to be restarted after library
-# upgrades. It's useless to us in non-interactive mode.
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq remove needrestart
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install --auto-remove python3-venv
+# Note: we don't use venv here because then per-language build files would need
+#   to source this script to have venv python be on the PATH. This would require
+#   backport this change to ~30 branches. Instead, we just install pip packages
+#   globally here. If this ever breaks, uncomment the following lines, remove
+#   sudo from pip install, and do the backports.
+#
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -qq remove needrestart
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install --auto-remove python3-venv
+# VIRTUAL_ENV="$(mktemp -d)"
+# python3 -m venv "${VIRTUAL_ENV}"
 
-VIRTUAL_ENV="$(mktemp -d)"
-python3 -m venv "${VIRTUAL_ENV}"
-
-# Activate venv without printing garbage.
-{ set +x; } 2>/dev/null
-echo "Activating Python venv"
-source "${VIRTUAL_ENV}/bin/activate"
-set -x
-
-python -VV
-pip install --upgrade pip==25.2
-# Note that these are only test driver's dependencies. gRPC version
-# shouldn't matter, as it's only used for getting the LB stats from the client.
-pip install --upgrade \
+python3 -VV
+sudo python3 -m pip install --upgrade pip==25.2
+# TODO(sergiitk): Unpin grpcio-tools when a version of xds-protos
+#   compatible with protobuf 4.X is uploaded to PyPi.
+sudo python3 -m pip install --upgrade \
     grpcio-tools==1.74.0 \
     grpcio==1.74.0 \
     xds-protos==1.74.0 \
     google-api-python-client==2.179.0 \
     google-auth-httplib2==0.2.0 \
     oauth2client==4.1.3
-pip list
+python3 -m pip list
 
 # Prepare generated Python code.
 TOOLS_DIR=tools/run_tests
@@ -53,7 +50,7 @@ PROTO_SOURCE_DIR=src/proto/grpc/testing
 PROTO_DEST_DIR=${TOOLS_DIR}/${PROTO_SOURCE_DIR}
 mkdir -p ${PROTO_DEST_DIR}
 
-python -m grpc_tools.protoc \
+python3 -m grpc_tools.protoc \
     --proto_path=. \
     --python_out=${TOOLS_DIR} \
     --grpc_python_out=${TOOLS_DIR} \
@@ -65,7 +62,7 @@ HEALTH_PROTO_SOURCE_DIR=src/proto/grpc/health/v1
 HEALTH_PROTO_DEST_DIR=${TOOLS_DIR}/${HEALTH_PROTO_SOURCE_DIR}
 mkdir -p ${HEALTH_PROTO_DEST_DIR}
 
-python -m grpc_tools.protoc \
+python3 -m grpc_tools.protoc \
     --proto_path=. \
     --python_out=${TOOLS_DIR} \
     --grpc_python_out=${TOOLS_DIR} \
