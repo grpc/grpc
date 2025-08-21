grpc/grpc

commit 5f4dec08d6d90677c832411f8646c53519574aa1

[metrics] add grpc.lb.backend_service label (#40486)
Implements [gRFC A89](https://github.com/grpc/proposal/blob/master/A89-backend-service-metric-label.md).

Closes #40486

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/40486 from markdroth:metrics_backend_service_label b7486e23402d26707329db7e7f58de32d0e3bb4e
PiperOrigin-RevId: 797493482

src/core/load_balancing/xds/cds.cc
@@ -36,6 +36,7 @@
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
+#include "src/core/client_channel/client_channel_internal.h"
 #include "src/core/config/core_configuration.h"
 #include "src/core/lib/channel/channel_args.h"
 #include "src/core/lib/debug/trace.h"
@@ -121,8 +122,45 @@ class CdsLb final : public LoadBalancingPolicy {
   void ExitIdleLocked() override;
 
  private:
+  class Picker final : public SubchannelPicker {
+   public:
+    Picker(RefCountedPtr<CdsLb> cds_lb,
+           RefCountedPtr<SubchannelPicker> child_picker)
+        : cds_lb_(std::move(cds_lb)), child_picker_(std::move(child_picker)) {}
+
+    PickResult Pick(PickArgs args) override {
+      auto* call_state =
+          static_cast<ClientChannelLbCallState*>(args.call_state);
+      auto* call_attempt_tracer = call_state->GetCallAttemptTracer();
+      if (call_attempt_tracer != nullptr) {
+        call_attempt_tracer->SetOptionalLabel(
+            ClientCallTracerInterface::CallAttemptTracer::OptionalLabelKey::
+                kBackendService,
+            cds_lb_->cluster_name_);
+      }
+      return child_picker_->Pick(args);
+    }
+
+   private:
+    RefCountedPtr<CdsLb> cds_lb_;
+    RefCountedPtr<SubchannelPicker> child_picker_;
+  };
+
   // Delegating helper to be passed to child policy.
-  using Helper = ParentOwningDelegatingChannelControlHelper<CdsLb>;
+  class Helper final
+      : public ParentOwningDelegatingChannelControlHelper<CdsLb> {
+   public:
+    using ParentOwningDelegatingChannelControlHelper::
+        ParentOwningDelegatingChannelControlHelper;
+
+    void UpdateState(grpc_connectivity_state state, const absl::Status& status,
+                     RefCountedPtr<SubchannelPicker> picker) override {
+      parent_helper()->UpdateState(
+          state, status,
+          MakeRefCounted<Picker>(parent()->RefAsSubclass<CdsLb>(),
+                                 std::move(picker)));
+    }
+  };
 
   // State used to retain child policy names for the priority policy.
   struct ChildNameState {
@@ -160,7 +198,7 @@ class CdsLb final : public LoadBalancingPolicy {
 
   void ReportTransientFailure(absl::Status status);
 
-  std::string cluster_name_;
+  RefCountedStringValue cluster_name_;
   RefCountedPtr<const XdsConfig> xds_config_;
 
   // Cluster subscription, for dynamic clusters (e.g., RLS).
@@ -225,7 +263,7 @@ std::string MakeChildPolicyName(absl::string_view cluster,
 class PriorityEndpointIterator final : public EndpointAddressesIterator {
  public:
   PriorityEndpointIterator(
-      std::string cluster_name, bool use_http_connect,
+      RefCountedStringValue cluster_name, bool use_http_connect,
       std::shared_ptr<const XdsEndpointResource> endpoints,
       std::vector<size_t /*child_number*/> priority_child_numbers)
       : cluster_name_(std::move(cluster_name)),
@@ -238,8 +276,8 @@ class PriorityEndpointIterator final : public EndpointAddressesIterator {
     const auto& priority_list = GetUpdatePriorityList(endpoints_.get());
     for (size_t priority = 0; priority < priority_list.size(); ++priority) {
       const auto& priority_entry = priority_list[priority];
-      std::string priority_child_name =
-          MakeChildPolicyName(cluster_name_, priority_child_numbers_[priority]);
+      std::string priority_child_name = MakeChildPolicyName(
+          cluster_name_.as_string_view(), priority_child_numbers_[priority]);
       for (const auto& [locality_name, locality] : priority_entry.localities) {
         std::vector<RefCountedStringValue> hierarchical_path = {
             RefCountedStringValue(priority_child_name),
@@ -264,7 +302,7 @@ class PriorityEndpointIterator final : public EndpointAddressesIterator {
   }
 
  private:
-  std::string cluster_name_;
+  RefCountedStringValue cluster_name_;
   bool use_http_connect_;
   std::shared_ptr<const XdsEndpointResource> endpoints_;
   std::vector<size_t /*child_number*/> priority_child_numbers_;
@@ -280,16 +318,16 @@ absl::Status CdsLb::UpdateLocked(UpdateArgs args) {
   CHECK(new_config != nullptr);
   // Cluster name should never change, because we should use a different
   // child name in xds_cluster_manager in that case.
-  if (cluster_name_.empty()) {
-    cluster_name_ = new_config->cluster();
+  if (cluster_name_.as_string_view().empty()) {
+    cluster_name_ = RefCountedStringValue(new_config->cluster());
   } else {
-    CHECK(cluster_name_ == new_config->cluster());
+    CHECK_EQ(cluster_name_.as_string_view(), new_config->cluster());
   }
   // Start dynamic subscription if needed.
   if (new_config->is_dynamic() && subscription_ == nullptr) {
     GRPC_TRACE_LOG(cds_lb, INFO)
         << "[cdslb " << this << "] obtaining dynamic subscription for cluster "
-        << cluster_name_;
+        << cluster_name_.as_string_view();
     auto* dependency_mgr = args.args.GetObject<XdsDependencyManager>();
     if (dependency_mgr == nullptr) {
       // Should never happen.
@@ -298,7 +336,8 @@ absl::Status CdsLb::UpdateLocked(UpdateArgs args) {
       ReportTransientFailure(status);
       return status;
     }
-    subscription_ = dependency_mgr->GetClusterSubscription(cluster_name_);
+    subscription_ =
+        dependency_mgr->GetClusterSubscription(cluster_name_.as_string_view());
   }
   // Get xDS config.
   auto new_xds_config = args.args.GetObjectRef<XdsConfig>();
@@ -309,7 +348,7 @@ absl::Status CdsLb::UpdateLocked(UpdateArgs args) {
     ReportTransientFailure(status);
     return status;
   }
-  auto it = new_xds_config->clusters.find(cluster_name_);
+  auto it = new_xds_config->clusters.find(cluster_name_.as_string_view());
   if (it == new_xds_config->clusters.end()) {
     // Cluster not present.
     if (new_config->is_dynamic()) {
@@ -318,14 +357,16 @@ absl::Status CdsLb::UpdateLocked(UpdateArgs args) {
       // got the new cluster, in which case it will still be missing.
       GRPC_TRACE_LOG(cds_lb, INFO)
           << "[cdslb " << this
-          << "] xDS config has no entry for dynamic cluster " << cluster_name_
+          << "] xDS config has no entry for dynamic cluster "
+          << cluster_name_.as_string_view()
           << ", waiting for subsequent update";
       // Stay in CONNECTING until we get an update that has the cluster.
       return absl::OkStatus();
     }
     // Not a dynamic cluster.  This should never happen.
-    absl::Status status = absl::UnavailableError(absl::StrCat(
-        "xDS config has no entry for static cluster ", cluster_name_));
+    absl::Status status = absl::UnavailableError(
+        absl::StrCat("xDS config has no entry for static cluster ",
+                     cluster_name_.as_string_view()));
     ReportTransientFailure(status);
     return status;
   }
@@ -339,7 +380,7 @@ absl::Status CdsLb::UpdateLocked(UpdateArgs args) {
   // Find old cluster, if any.
   const XdsConfig::ClusterConfig* old_cluster_config = nullptr;
   if (xds_config_ != nullptr) {
-    auto it_old = xds_config_->clusters.find(cluster_name_);
+    auto it_old = xds_config_->clusters.find(cluster_name_.as_string_view());
     if (it_old != xds_config_->clusters.end() && it_old->second.ok()) {
       old_cluster_config = &*it_old->second;
       // If nothing changed for a leaf cluster, then ignore the update.
@@ -379,7 +420,8 @@ absl::Status CdsLb::UpdateLocked(UpdateArgs args) {
         aggregate_cluster_resource = aggregate_cluster_config->cluster.get();
       }
     } else {
-      args.args = args.args.Set(kArgXdsAggregateClusterName, cluster_name_);
+      args.args = args.args.Set(kArgXdsAggregateClusterName,
+                                cluster_name_.as_string_view());
     }
   }
   // Construct child policy config and update state based on the cluster type.
@@ -408,6 +450,9 @@ absl::Status CdsLb::UpdateLocked(UpdateArgs args) {
         // Construct child policy config.
         child_policy_config_json = CreateChildPolicyConfigForLeafCluster(
             *new_cluster_config, endpoint_config, aggregate_cluster_resource);
+        // Pass backend service label to child policy.
+        args.args = args.args.Set(GRPC_ARG_BACKEND_SERVICE,
+                                  cluster_name_.as_string_view());
       },
       // Aggregate cluster.
       [&](const XdsConfig::ClusterConfig::AggregateConfig& aggregate_config) {
@@ -426,9 +471,9 @@ absl::Status CdsLb::UpdateLocked(UpdateArgs args) {
           child_policy_config_json);
   if (!child_config.ok()) {
     // Should never happen.
-    absl::Status status = absl::InternalError(
-        absl::StrCat(cluster_name_, ": error parsing child policy config: ",
-                     child_config.status().message()));
+    absl::Status status = absl::InternalError(absl::StrCat(
+        cluster_name_.as_string_view(), ": error parsing child policy config: ",
+        child_config.status().message()));
     ReportTransientFailure(status);
     return status;
   }
@@ -444,8 +489,8 @@ absl::Status CdsLb::UpdateLocked(UpdateArgs args) {
             (*child_config)->name(), std::move(lb_args));
     if (child_policy_ == nullptr) {
       // Should never happen.
-      absl::Status status = absl::UnavailableError(
-          absl::StrCat(cluster_name_, ": failed to create child policy"));
+      absl::Status status = absl::UnavailableError(absl::StrCat(
+          cluster_name_.as_string_view(), ": failed to create child policy"));
       ReportTransientFailure(status);
       return status;
     }
@@ -566,8 +611,9 @@ Json CdsLb::CreateChildPolicyConfigForLeafCluster(
       GetUpdatePriorityList(endpoint_config.endpoints.get());
   for (size_t priority = 0; priority < priority_list.size(); ++priority) {
     // Add priority entry, with the appropriate child name.
-    std::string child_name = MakeChildPolicyName(
-        cluster_name_, child_name_state_.priority_child_numbers[priority]);
+    std::string child_name =
+        MakeChildPolicyName(cluster_name_.as_string_view(),
+                            child_name_state_.priority_child_numbers[priority]);
     priority_priorities.emplace_back(Json::FromString(child_name));
     Json::Object child_config = {{"config", xds_lb_policy}};
     if (!is_logical_dns) {
@@ -586,7 +632,7 @@ Json CdsLb::CreateChildPolicyConfigForLeafCluster(
   Json xds_override_host_policy = Json::FromArray({Json::FromObject({
       {"xds_override_host_experimental",
        Json::FromObject({
-           {"clusterName", Json::FromString(cluster_name_)},
+           {"clusterName", Json::FromString(cluster_name_.c_str())},
            {"childPolicy", std::move(priority_policy)},
        })},
   })});
@@ -594,7 +640,7 @@ Json CdsLb::CreateChildPolicyConfigForLeafCluster(
   Json xds_cluster_impl_policy = Json::FromArray({Json::FromObject({
       {"xds_cluster_impl_experimental",
        Json::FromObject({
-           {"clusterName", Json::FromString(cluster_name_)},
+           {"clusterName", Json::FromString(cluster_name_.c_str())},
            {"childPolicy", std::move(xds_override_host_policy)},
        })},
   })});
@@ -689,7 +735,7 @@ Json CdsLb::CreateChildPolicyConfigForAggregateCluster(
 }
 
 void CdsLb::ResetState() {
-  cluster_name_.clear();
+  cluster_name_ = RefCountedStringValue("");
   xds_config_.reset();
   child_name_state_.Reset();
   if (child_policy_ != nullptr) {

src/core/BUILD
@@ -6569,6 +6569,7 @@ grpc_cc_library(
     ],
     deps = [
         "channel_args",
+        "client_channel_internal_header",
         "delegating_helper",
         "env",
         "grpc_lb_address_filtering",

src/core/load_balancing/weighted_round_robin/weighted_round_robin.cc
@@ -81,6 +81,8 @@ namespace {
 constexpr absl::string_view kWeightedRoundRobin = "weighted_round_robin";
 
 constexpr absl::string_view kMetricLabelLocality = "grpc.lb.locality";
+constexpr absl::string_view kMetricLabelBackendService =
+    "grpc.lb.backend_service";
 
 const auto kMetricRrFallback =
     GlobalInstrumentsRegistry::RegisterUInt64Counter(
@@ -90,7 +92,7 @@ const auto kMetricRrFallback =
         "fall back to RR behavior.",
         "{update}", false)
         .Labels(kMetricLabelTarget)
-        .OptionalLabels(kMetricLabelLocality)
+        .OptionalLabels(kMetricLabelLocality, kMetricLabelBackendService)
         .Build();
 
 const auto kMetricEndpointWeightNotYetUsable =
@@ -102,7 +104,7 @@ const auto kMetricEndpointWeightNotYetUsable =
         "period).",
         "{endpoint}", false)
         .Labels(kMetricLabelTarget)
-        .OptionalLabels(kMetricLabelLocality)
+        .OptionalLabels(kMetricLabelLocality, kMetricLabelBackendService)
         .Build();
 
 const auto kMetricEndpointWeightStale =
@@ -112,7 +114,7 @@ const auto kMetricEndpointWeightStale =
         "latest weight is older than the expiration period.",
         "{endpoint}", false)
         .Labels(kMetricLabelTarget)
-        .OptionalLabels(kMetricLabelLocality)
+        .OptionalLabels(kMetricLabelLocality, kMetricLabelBackendService)
         .Build();
 
 const auto kMetricEndpointWeights =
@@ -124,7 +126,7 @@ const auto kMetricEndpointWeights =
         "without usable weights will have weight 0.",
         "{weight}", false)
         .Labels(kMetricLabelTarget)
-        .OptionalLabels(kMetricLabelLocality)
+        .OptionalLabels(kMetricLabelLocality, kMetricLabelBackendService)
         .Build();
 
 // Config for WRR policy.
@@ -413,6 +415,7 @@ class WeightedRoundRobin final : public LoadBalancingPolicy {
       ABSL_GUARDED_BY(&endpoint_weight_map_mu_);
 
   const absl::string_view locality_name_;
+  const absl::string_view backend_service_name_;
 
   bool shutdown_ = false;
 
@@ -620,16 +623,18 @@ void WeightedRoundRobin::Picker::BuildSchedulerAndStartTimerLocked() {
         now, config_->weight_expiration_period(), config_->blackout_period(),
         &num_not_yet_usable, &num_stale);
     weights.push_back(weight);
-    stats_plugins.RecordHistogram(kMetricEndpointWeights, weight,
-                                  {wrr_->channel_control_helper()->GetTarget()},
-                                  {wrr_->locality_name_});
+    stats_plugins.RecordHistogram(
+        kMetricEndpointWeights, weight,
+        {wrr_->channel_control_helper()->GetTarget()},
+        {wrr_->locality_name_, wrr_->backend_service_name_});
   }
-  stats_plugins.AddCounter(
-      kMetricEndpointWeightNotYetUsable, num_not_yet_usable,
-      {wrr_->channel_control_helper()->GetTarget()}, {wrr_->locality_name_});
+  stats_plugins.AddCounter(kMetricEndpointWeightNotYetUsable,
+                           num_not_yet_usable,
+                           {wrr_->channel_control_helper()->GetTarget()},
+                           {wrr_->locality_name_, wrr_->backend_service_name_});
   stats_plugins.AddCounter(kMetricEndpointWeightStale, num_stale,
                            {wrr_->channel_control_helper()->GetTarget()},
-                           {wrr_->locality_name_});
+                           {wrr_->locality_name_, wrr_->backend_service_name_});
   GRPC_TRACE_LOG(weighted_round_robin_lb, INFO)
       << "[WRR " << wrr_.get() << " picker " << this
       << "] new weights: " << absl::StrJoin(weights, " ");
@@ -646,9 +651,9 @@ void WeightedRoundRobin::Picker::BuildSchedulerAndStartTimerLocked() {
     GRPC_TRACE_LOG(weighted_round_robin_lb, INFO)
         << "[WRR " << wrr_.get() << " picker " << this
         << "] no scheduler, falling back to RR";
-    stats_plugins.AddCounter(kMetricRrFallback, 1,
-                             {wrr_->channel_control_helper()->GetTarget()},
-                             {wrr_->locality_name_});
+    stats_plugins.AddCounter(
+        kMetricRrFallback, 1, {wrr_->channel_control_helper()->GetTarget()},
+        {wrr_->locality_name_, wrr_->backend_service_name_});
   }
   {
     MutexLock lock(&scheduler_mu_);
@@ -689,10 +694,12 @@ WeightedRoundRobin::WeightedRoundRobin(Args args)
     : LoadBalancingPolicy(std::move(args)),
       locality_name_(channel_args()
                          .GetString(GRPC_ARG_LB_WEIGHTED_TARGET_CHILD)
-                         .value_or("")) {
+                         .value_or("")),
+      backend_service_name_(
+          channel_args().GetString(GRPC_ARG_BACKEND_SERVICE).value_or("")) {
   GRPC_TRACE_LOG(weighted_round_robin_lb, INFO)
-      << "[WRR " << this << "] Created -- locality_name=\""
-      << std::string(locality_name_) << "\"";
+      << "[WRR " << this << "] Created -- locality_name=\"" << locality_name_
+      << "\", backend_service_name=\"" << backend_service_name_ << "\"";
 }
 
 WeightedRoundRobin::~WeightedRoundRobin() {

src/core/resolver/endpoint_addresses.h
@@ -42,6 +42,9 @@
 // Name associated with individual address, if available (e.g., DNS name).
 #define GRPC_ARG_ADDRESS_NAME "grpc.address_name"
 
+// Backend service name associated with the addresses.
+#define GRPC_ARG_BACKEND_SERVICE "grpc.internal.backend_service"
+
 namespace grpc_core {
 
 // A list of addresses for a given endpoint with an associated set of channel

src/core/telemetry/call_tracer.h
@@ -148,6 +148,7 @@ class ClientCallTracerInterface : public CallTracerAnnotationInterface {
       kXdsServiceName,       // not public
       kXdsServiceNamespace,  // not public
       kLocality,
+      kBackendService,
       kSize  // should be last
     };
 

src/cpp/ext/otel/otel_plugin.cc
@@ -683,7 +683,8 @@ OpenTelemetryPluginImpl::~OpenTelemetryPluginImpl() {
 
 namespace {
 constexpr absl::string_view kLocality = "grpc.lb.locality";
-}
+constexpr absl::string_view kBackendService = "grpc.lb.backend_service";
+}  // namespace
 
 absl::string_view OpenTelemetryPluginImpl::OptionalLabelKeyToString(
     grpc_core::ClientCallTracerInterface::CallAttemptTracer::OptionalLabelKey
@@ -692,6 +693,9 @@ absl::string_view OpenTelemetryPluginImpl::OptionalLabelKeyToString(
     case grpc_core::ClientCallTracerInterface::CallAttemptTracer::
         OptionalLabelKey::kLocality:
       return kLocality;
+    case grpc_core::ClientCallTracerInterface::CallAttemptTracer::
+        OptionalLabelKey::kBackendService:
+      return kBackendService;
     default:
       grpc_core::Crash("Illegal OptionalLabelKey index");
   }
@@ -703,6 +707,9 @@ OpenTelemetryPluginImpl::OptionalLabelStringToKey(absl::string_view key) {
   if (key == kLocality) {
     return grpc_core::ClientCallTracerInterface::CallAttemptTracer::
         OptionalLabelKey::kLocality;
+  } else if (key == kBackendService) {
+    return grpc_core::ClientCallTracerInterface::CallAttemptTracer::
+        OptionalLabelKey::kBackendService;
   }
   return std::nullopt;
 }

test/core/load_balancing/weighted_round_robin_test.cc
@@ -58,6 +58,7 @@ namespace testing {
 namespace {
 
 constexpr absl::string_view kLocalityName = "locality0";
+constexpr absl::string_view kBackendServiceName = "backend_service0";
 
 class WeightedRoundRobinTest : public LoadBalancingPolicyTest {
  protected:
@@ -108,8 +109,9 @@ class WeightedRoundRobinTest : public LoadBalancingPolicyTest {
   WeightedRoundRobinTest()
       : LoadBalancingPolicyTest(
             "weighted_round_robin",
-            ChannelArgs().Set(GRPC_ARG_LB_WEIGHTED_TARGET_CHILD,
-                              kLocalityName)) {}
+            ChannelArgs()
+                .Set(GRPC_ARG_LB_WEIGHTED_TARGET_CHILD, kLocalityName)
+                .Set(GRPC_ARG_BACKEND_SERVICE, kBackendServiceName)) {}
 
   void SetUp() override {
     LoadBalancingPolicyTest::SetUp();
@@ -1010,8 +1012,9 @@ TEST_F(WeightedRoundRobinTest, MetricDefinitionRrFallback) {
   EXPECT_EQ(descriptor->name, "grpc.lb.wrr.rr_fallback");
   EXPECT_EQ(descriptor->unit, "{update}");
   EXPECT_THAT(descriptor->label_keys, ::testing::ElementsAre("grpc.target"));
-  EXPECT_THAT(descriptor->optional_label_keys,
-              ::testing::ElementsAre("grpc.lb.locality"));
+  EXPECT_THAT(
+      descriptor->optional_label_keys,
+      ::testing::ElementsAre("grpc.lb.locality", "grpc.lb.backend_service"));
 }
 
 TEST_F(WeightedRoundRobinTest, MetricDefinitionEndpointWeightNotYetUsable) {
@@ -1027,8 +1030,9 @@ TEST_F(WeightedRoundRobinTest, MetricDefinitionEndpointWeightNotYetUsable) {
   EXPECT_EQ(descriptor->name, "grpc.lb.wrr.endpoint_weight_not_yet_usable");
   EXPECT_EQ(descriptor->unit, "{endpoint}");
   EXPECT_THAT(descriptor->label_keys, ::testing::ElementsAre("grpc.target"));
-  EXPECT_THAT(descriptor->optional_label_keys,
-              ::testing::ElementsAre("grpc.lb.locality"));
+  EXPECT_THAT(
+      descriptor->optional_label_keys,
+      ::testing::ElementsAre("grpc.lb.locality", "grpc.lb.backend_service"));
 }
 
 TEST_F(WeightedRoundRobinTest, MetricDefinitionEndpointWeightStale) {
@@ -1044,8 +1048,9 @@ TEST_F(WeightedRoundRobinTest, MetricDefinitionEndpointWeightStale) {
   EXPECT_EQ(descriptor->name, "grpc.lb.wrr.endpoint_weight_stale");
   EXPECT_EQ(descriptor->unit, "{endpoint}");
   EXPECT_THAT(descriptor->label_keys, ::testing::ElementsAre("grpc.target"));
-  EXPECT_THAT(descriptor->optional_label_keys,
-              ::testing::ElementsAre("grpc.lb.locality"));
+  EXPECT_THAT(
+      descriptor->optional_label_keys,
+      ::testing::ElementsAre("grpc.lb.locality", "grpc.lb.backend_service"));
 }
 
 TEST_F(WeightedRoundRobinTest, MetricDefinitionEndpointWeights) {
@@ -1061,8 +1066,9 @@ TEST_F(WeightedRoundRobinTest, MetricDefinitionEndpointWeights) {
   EXPECT_EQ(descriptor->name, "grpc.lb.wrr.endpoint_weights");
   EXPECT_EQ(descriptor->unit, "{weight}");
   EXPECT_THAT(descriptor->label_keys, ::testing::ElementsAre("grpc.target"));
-  EXPECT_THAT(descriptor->optional_label_keys,
-              ::testing::ElementsAre("grpc.lb.locality"));
+  EXPECT_THAT(
+      descriptor->optional_label_keys,
+      ::testing::ElementsAre("grpc.lb.locality", "grpc.lb.backend_service"));
 }
 
 TEST_F(WeightedRoundRobinTest, MetricValues) {
@@ -1083,7 +1089,8 @@ TEST_F(WeightedRoundRobinTest, MetricValues) {
           "grpc.lb.wrr.endpoint_weights")
           .value();
   const absl::string_view kLabelValues[] = {target_};
-  const absl::string_view kOptionalLabelValues[] = {kLocalityName};
+  const absl::string_view kOptionalLabelValues[] = {kLocalityName,
+                                                    kBackendServiceName};
   auto stats_plugin = std::make_shared<FakeStatsPlugin>(
       nullptr, /*use_disabled_by_default_metrics=*/true);
   stats_plugin_group_.AddStatsPlugin(stats_plugin, nullptr);

test/cpp/end2end/xds/xds_cluster_end2end_test.cc
@@ -339,7 +339,9 @@ TEST_P(CdsTest, MetricLabels) {
           ::testing::Pair(OptionalLabelKey::kXdsServiceNamespace,
                           "mynamespace"),
           ::testing::Pair(OptionalLabelKey::kLocality,
-                          LocalityNameString("locality0"))));
+                          LocalityNameString("locality0")),
+          ::testing::Pair(OptionalLabelKey::kBackendService,
+                          kDefaultClusterName)));
   // Send an RPC to backend 1.
   WaitForBackend(DEBUG_LOCATION, 1);
   // Verify that the optional labels are recorded in the call
@@ -353,7 +355,9 @@ TEST_P(CdsTest, MetricLabels) {
           ::testing::Pair(OptionalLabelKey::kXdsServiceNamespace,
                           "mynamespace"),
           ::testing::Pair(OptionalLabelKey::kLocality,
-                          LocalityNameString("locality1"))));
+                          LocalityNameString("locality1")),
+          ::testing::Pair(OptionalLabelKey::kBackendService,
+                          kDefaultClusterName)));
 }
 
 //

commit fbdb100ae70a3d2758ad1e6d80b2a7c6cf77fff4

[metrics] fix tsan race (#40517)
Fixes a TSAN race accidentally introduced in #40486.

b/440289448

Closes #40517

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/40517 from markdroth:metrics_backend_service_label_tsan_fix 7b8291e44fed0b6d414c4bfdee4fb67efabf6565
PiperOrigin-RevId: 797943490

src/core/load_balancing/xds/cds.cc
@@ -124,9 +124,9 @@ class CdsLb final : public LoadBalancingPolicy {
  private:
   class Picker final : public SubchannelPicker {
    public:
-    Picker(RefCountedPtr<CdsLb> cds_lb,
-           RefCountedPtr<SubchannelPicker> child_picker)
-        : cds_lb_(std::move(cds_lb)), child_picker_(std::move(child_picker)) {}
+    Picker(CdsLb* cds_lb, RefCountedPtr<SubchannelPicker> child_picker)
+        : cluster_name_(cds_lb->cluster_name_),
+          child_picker_(std::move(child_picker)) {}
 
     PickResult Pick(PickArgs args) override {
       auto* call_state =
@@ -136,13 +136,13 @@ class CdsLb final : public LoadBalancingPolicy {
         call_attempt_tracer->SetOptionalLabel(
             ClientCallTracerInterface::CallAttemptTracer::OptionalLabelKey::
                 kBackendService,
-            cds_lb_->cluster_name_);
+            cluster_name_);
       }
       return child_picker_->Pick(args);
     }
 
    private:
-    RefCountedPtr<CdsLb> cds_lb_;
+    RefCountedStringValue cluster_name_;
     RefCountedPtr<SubchannelPicker> child_picker_;
   };
 
@@ -156,9 +156,7 @@ class CdsLb final : public LoadBalancingPolicy {
     void UpdateState(grpc_connectivity_state state, const absl::Status& status,
                      RefCountedPtr<SubchannelPicker> picker) override {
       parent_helper()->UpdateState(
-          state, status,
-          MakeRefCounted<Picker>(parent()->RefAsSubclass<CdsLb>(),
-                                 std::move(picker)));
+          state, status, MakeRefCounted<Picker>(parent(), std::move(picker)));
     }
   };
 