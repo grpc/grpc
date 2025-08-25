fb2923abfc373d5e74475d5ed6d0ef0e10b79bf8
Mark D. Roth <roth@google.com>
2025-08-25 10:40:23 -0700

    [subchannel] make sure internal subchannel is unreffed in WorkSerializer (#40501)
    In b/435721210, there is a suspicion (not proven) that the internal subchannel is not being unreffed within the channel's `WorkSerializer`.  This PR cleans up a few edge cases that could conceivably result in the internal subchannel being unreffed from outside the `WorkSerializer`, although I have no proof that that's actually happening in practice.
    
    Specifically:
    - Change `SubchannelWrapper::Orphaned()` to explicitly clear any non-cancelled watchers (both connectivity state watchers and data watchers).
    - Change the health checking and ORCA OOB data watchers to hold a weak ref to the internal subchannel instead of a strong ref, just in case they somehow got unreffed outside of `SubchannelWrapper::Orphaned()`.
    
    Closes #40501
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/40501 from markdroth:subchannel_ref_cleanup_in_work_serializer b9163f5a6eeb9242bc3ae5f79048eac028c1e90f
    PiperOrigin-RevId: 799182096

--- src/core/client_channel/client_channel_filter.cc
+++ src/core/client_channel/client_channel_filter.cc
@@ -538,6 +538,18 @@ class ClientChannelFilter::SubchannelWrapper final
           }
         }
       }
+      // We need to make sure that the internal subchannel gets unreffed
+      // inside of the WorkSerializer, so that updates to the local
+      // subchannel pool are properly synchronized.  To that end, we
+      // drop our ref to the internal subchannel here.  We also cancel
+      // any watchers that were not properly cancelled, in case any of
+      // them are holding a ref to the internal subchannel.
+      for (const auto& [_, watcher] : watcher_map_) {
+        subchannel_->CancelConnectivityStateWatch(watcher);
+      }
+      watcher_map_.clear();
+      data_watchers_.clear();
+      subchannel_.reset();
       WeakUnref(DEBUG_LOCATION, "subchannel map cleanup");
     });
   }
@@ -549,7 +561,7 @@ class ClientChannelFilter::SubchannelWrapper final
     CHECK_EQ(watcher_wrapper, nullptr);
     watcher_wrapper = new WatcherWrapper(
         std::move(watcher),
-        RefAsSubclass<SubchannelWrapper>(DEBUG_LOCATION, "WatcherWrapper"));
+        WeakRefAsSubclass<SubchannelWrapper>(DEBUG_LOCATION, "WatcherWrapper"));
     subchannel_->WatchConnectivityState(
         RefCountedPtr<Subchannel::ConnectivityStateWatcherInterface>(
             watcher_wrapper));
@@ -609,7 +621,7 @@ class ClientChannelFilter::SubchannelWrapper final
     WatcherWrapper(
         std::unique_ptr<SubchannelInterface::ConnectivityStateWatcherInterface>
             watcher,
-        RefCountedPtr<SubchannelWrapper> parent)
+        WeakRefCountedPtr<SubchannelWrapper> parent)
         : watcher_(std::move(watcher)), parent_(std::move(parent)) {}
 
     ~WatcherWrapper() override {
@@ -682,7 +694,7 @@ class ClientChannelFilter::SubchannelWrapper final
 
     std::unique_ptr<SubchannelInterface::ConnectivityStateWatcherInterface>
         watcher_;
-    RefCountedPtr<SubchannelWrapper> parent_;
+    WeakRefCountedPtr<SubchannelWrapper> parent_;
   };
 
   // A heterogenous lookup comparator for data watchers that allows
