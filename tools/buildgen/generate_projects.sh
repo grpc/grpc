grpc/grpc

commit f0d0435993653c3f5796b9917327369614614e6c

[sanity] fix on master (#40518)
Closes #40518

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/40518 from markdroth:sanity_fix a1d6d2aacc71474f6017e1aa245b4bbcca4d39eb
PiperOrigin-RevId: 797870628

tools/buildgen/generate_projects.sh
@@ -49,11 +49,11 @@ if [[ ! -d generate_projects_virtual_environment ]]; then
       fi
       exit 1
     fi
-  fi 
+  fi
   python3 -m virtualenv generate_projects_virtual_environment
 fi
 
-generate_projects_virtual_environment/bin/pip install --upgrade --ignore-installed grpcio-tools==1.59.0
+generate_projects_virtual_environment/bin/pip install --upgrade --ignore-installed grpcio-tools==1.74.0
 generate_projects_virtual_environment/bin/python tools/distrib/python/xds_protos/build.py
 generate_projects_virtual_environment/bin/python tools/distrib/python/make_grpcio_tools.py
 generate_projects_virtual_environment/bin/python src/python/grpcio_observability/make_grpcio_observability.py

commit 4d1b072604e19f1926fc7d5741ea4a795b550d82

[Fix][CI] Allow _at_head jobs to skip xds-protos generation step (#40575)
"At head" jobs still use Python 3.8, and were broken with #40518, which bumped grpcio-tools to 1.74.0. We use grpcio-tools to generate xds-protos.

This PR introduces a temporary fix, allowing the following jobs to skip xds-protos generation:
- grpc/core/master/linux/grpc_build_abseil-cpp_at_head
- grpc/core/master/linux/grpc_build_boringssl_at_head
- grpc/core/master/linux/grpc_build_boringssl_at_head

This logic should be removed once our Ubuntu custom image is updated.

Closes #40575

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/40575 from sergiitk:fix/py/build/abseil-cpp 5315876c34609108f00c975090f99ce209116845
PiperOrigin-RevId: 800638644

tools/buildgen/generate_projects.sh
@@ -53,17 +53,25 @@ if [[ ! -d generate_projects_virtual_environment ]]; then
   python3 -m virtualenv generate_projects_virtual_environment
 fi
 
-# Keep grpcio-tools version in sync with XDS_PROTOS_GENCODE_GRPC_VERSION
-# in tools/distrib/python/xds_protos/setup.py.
-#
-# Explanation: since PR #40518, xds-protos grpc gencode contains a poison pill
-# that enforces grpcio runtime version to be equal or greater
-# to the grpcio-tools version that generated the code.
-#
-# Note: this version needs to be updated periodically to keep up with mainstream
-# python releases, as older grpcio-tools release may not support them.
-generate_projects_virtual_environment/bin/pip install --upgrade --ignore-installed grpcio-tools==1.74.0
-generate_projects_virtual_environment/bin/python tools/distrib/python/xds_protos/build.py
+# TODO(sergiitk): remove GRPC_GENERATE_PROJECTS_SKIP_XDS_PROTOS when all CIs are
+#                 are upgraded to use Python 3.9+.
+# WARNING! Do not use this variable when running the script directly. This is
+# a temporary measure to allow some other scripts scripts that rely on
+# generate_projects.sh (f.e. _at_head jobs), to skip this step while they're
+# using on older Python versions.
+if [[ "${GRPC_GENERATE_PROJECTS_SKIP_XDS_PROTOS:-0}" != "1" ]]; then
+  # Keep grpcio-tools version in sync with XDS_PROTOS_GENCODE_GRPC_VERSION
+  # in tools/distrib/python/xds_protos/setup.py.
+  #
+  # Explanation: since PR #40518, xds-protos grpc gencode contains a poison pill
+  # that enforces grpcio runtime version to be equal or greater
+  # to the grpcio-tools version that generated the code.
+  #
+  # Note: this version needs to be updated periodically to keep up with mainstream
+  # python releases, as older grpcio-tools release may not support them.
+  generate_projects_virtual_environment/bin/pip install --upgrade --ignore-installed grpcio-tools==1.74.0
+  generate_projects_virtual_environment/bin/python tools/distrib/python/xds_protos/build.py
+fi
 generate_projects_virtual_environment/bin/python tools/distrib/python/make_grpcio_tools.py
 generate_projects_virtual_environment/bin/python src/python/grpcio_observability/make_grpcio_observability.py
 

tools/internal_ci/linux/grpc_build_submodule_at_head.sh
@@ -81,6 +81,12 @@ then
   cp -r third_party/protobuf/third_party/utf8_range third_party/utf8_range/
 fi
 
+# TODO(sergiitk): remove this logic once the CI uses python3.9+.
+# Note: we set GRPC_GENERATE_PROJECTS_SKIP_XDS_PROTOS to skip xds-protos
+# generation, which shouldn't bee needed for _at_head jobs.
+# Normally, it doesn't hurt to generate them, but our CI uses python3.8,
+# which grpcio python packages dropped at v1.71.0.
+export GRPC_GENERATE_PROJECTS_SKIP_XDS_PROTOS="${GRPC_GENERATE_PROJECTS_SKIP_XDS_PROTOS:-1}"
 tools/buildgen/generate_projects.sh
 
 # commit so that changes are passed to Docker