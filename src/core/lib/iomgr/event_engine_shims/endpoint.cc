b09b9cdabe0c0e20ac11343717a7fc8edcb6fd4b
Eugene Ostroukhov <eostroukhov@google.com>
2025-09-03 16:42:50 -0700

    fixup: revert shims

--- src/core/lib/iomgr/event_engine_shims/endpoint.cc
+++ src/core/lib/iomgr/event_engine_shims/endpoint.cc
@@ -412,10 +412,9 @@ EventEngineEndpointWrapper::EventEngineEndpointWrapper(
 }  // namespace
 
 grpc_endpoint* grpc_event_engine_endpoint_create(
-    absl::StatusOr<std::unique_ptr<EventEngine::Endpoint>> ee_endpoint) {
-  DCHECK(ee_endpoint.ok()) << ee_endpoint.status();
-  DCHECK(ee_endpoint.value() != nullptr);
-  auto wrapper = new EventEngineEndpointWrapper(std::move(ee_endpoint).value());
+    std::unique_ptr<EventEngine::Endpoint> ee_endpoint) {
+  DCHECK(ee_endpoint != nullptr);
+  auto wrapper = new EventEngineEndpointWrapper(std::move(ee_endpoint));
   return wrapper->GetGrpcEndpoint();
 }
 
