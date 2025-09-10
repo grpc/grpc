c41b8e609b751e6072154c27035aa231a3cf5220
Eshita Chandwani <emchandwani@google.com>
2025-09-10 15:26:27 +0530

    [interop] Add v1.72.3 release of grpc-go to interop matrix

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -318,7 +318,7 @@ LANG_RELEASE_MATRIX = {
             ("v1.69.4", ReleaseInfo()),
             ("v1.70.0", ReleaseInfo()),
             ("v1.71.3", ReleaseInfo()),
-            ("v1.72.2", ReleaseInfo()),
+            ("v1.72.3", ReleaseInfo()),
             ("v1.73.1", ReleaseInfo()),
             ("v1.74.3", ReleaseInfo()),
             ("v1.75.1", ReleaseInfo()),
