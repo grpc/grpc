91ba487a4547db9a22bb4320813d3fa9808288b4
Siddharth Nohria <snohria@google.com>
2025-09-15 06:23:06 +0000

    Wrong preprocessor guards sanity fix

--- src/core/lib/resource_tracker/resource_tracker.h
+++ src/core/lib/resource_tracker/resource_tracker.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GRPC_SRC_CORE_LIB_RESOURCE_TRACKER_RESOURCE_TRACKER_H_
-#define GRPC_SRC_CORE_LIB_RESOURCE_TRACKER_RESOURCE_TRACKER_H_
+#ifndef GRPC_SRC_CORE_LIB_RESOURCE_TRACKER_RESOURCE_TRACKER_H
+#define GRPC_SRC_CORE_LIB_RESOURCE_TRACKER_RESOURCE_TRACKER_H
 
 #include <grpc/support/port_platform.h>
 
@@ -43,4 +43,4 @@ class ResourceTracker {
 
 }  // namespace grpc_core
 
-#endif  // GRPC_SRC_CORE_LIB_RESOURCE_TRACKER_RESOURCE_TRACKER_H_
+#endif  // GRPC_SRC_CORE_LIB_RESOURCE_TRACKER_RESOURCE_TRACKER_H
