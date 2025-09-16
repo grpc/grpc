042d7748a3fd74a136ce4e86f056b686ffc3c4a8
Akshit Patel <akshitpatel@google.com>
2025-09-16 05:58:15 +0000

    Fix

--- src/core/ext/transport/chttp2/transport/stream.h
+++ src/core/ext/transport/chttp2/transport/stream.h
@@ -1,6 +1,6 @@
 //
 //
-// Copyright 2024 gRPC authors.
+// Copyright 2025 gRPC authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
