f847e489bce5ccd89d3b9cd0040b327569d2883f
Ashesh Vidyut <asheshvidyut@google.com>
2025-09-23 06:06:45 -0700

    [Python][Part 8] - Introducing Ruff (#40183)
    ### Description
    
    ## Part 8 of Introducing Ruff
    
    * In this PR - the suppression for `PLR1714` and `PLR5501` and  has been removed on the root `ruff.toml`
    
    ## Related:
    * Next: #40184
    * Prev: #40182
    * b/423755915
    
    Closes #40183
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/40183 from asheshvidyut:feature/setup-ruff-part-8 af5f5967d96c9e82d280bd66744bcf1ae972e7ec
    PiperOrigin-RevId: 810411168

--- src/python/grpcio/grpc/beta/utilities.py
+++ src/python/grpcio/grpc/beta/utilities.py
@@ -44,15 +44,14 @@ class _ChannelReadyFuture(future.Future):
                     raise future.CancelledError()
                 elif self._matured:
                     return
+                elif until is None:
+                    self._condition.wait()
                 else:
-                    if until is None:
-                        self._condition.wait()
+                    remaining = until - time.time()
+                    if remaining < 0:
+                        raise future.TimeoutError()
                     else:
-                        remaining = until - time.time()
-                        if remaining < 0:
-                            raise future.TimeoutError()
-                        else:
-                            self._condition.wait(timeout=remaining)
+                        self._condition.wait(timeout=remaining)
 
     def _update(self, connectivity):
         with self._condition:
