fdb5aac143d1e1c54dfd565e4a301c16e19c56b5
Aananth V <aananthv@google.com>
2025-09-29 23:42:30 -0700

    [export] Improve error reporting in `check_include_style.py`.
    The script now prints the specific lines that are badly formed and shows the suggested correction for each line, including the line number.
    
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/813112590](http://cl/813112590)
    
    PiperOrigin-RevId: 813112590

--- tools/run_tests/sanity/check_include_style.py
+++ tools/run_tests/sanity/check_include_style.py
@@ -52,7 +52,15 @@ def check_include_style(directory_root):
             for regex, replace in BAD_REGEXES:
                 text = re.sub(regex, replace, text)
             if text != original:
-                bad_files.append(path)
+                bad_lines = []
+                for line_number, (original_line, text_line) in enumerate(
+                    zip(original.splitlines(), text.splitlines())
+                ):
+                    if original_line != text_line:
+                        bad_lines.append(
+                            (line_number + 1, original_line, text_line)
+                        )
+                bad_files.append((path, bad_lines))
                 if fix:
                     with open(path, "w") as f:
                         f.write(text)
@@ -68,6 +76,10 @@ all_bad_files += check_include_style(os.path.join("include", "grpc"))
 all_bad_files += check_include_style(os.path.join("include", "grpcpp"))
 
 if all_bad_files:
-    for f in all_bad_files:
-        print("%s has badly formed grpc system header files" % f)
+    for f, bad_lines in all_bad_files:
+        print("%s has badly formed grpc system header files:" % f)
+        for line_number, original_line, text_line in bad_lines:
+            print(
+                "\tline %d: %s -> %s" % (line_number, original_line, text_line)
+            )
     sys.exit(1)
