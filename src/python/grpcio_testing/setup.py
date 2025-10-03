b09280a25dfc4040495132fa86aa8068bc2bef85
sreenithi <ssreenithi@google.com>
2025-10-03 13:52:37 +0000

    add working version of grpcio_testing

--- src/python/grpcio_testing/setup.py
+++ src/python/grpcio_testing/setup.py
@@ -18,11 +18,9 @@ import sys
 
 import setuptools
 
-_PACKAGE_PATH = os.path.realpath(os.path.dirname(__file__))
-_README_PATH = os.path.join(_PACKAGE_PATH, "README.rst")
-
-# Ensure we're in the proper directory whether or not we're being used by pip.
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# Manually insert the source directory into the Python path for local module
+# imports to succeed
+sys.path.insert(0, os.path.abspath("."))
 
 # Break import style to ensure that we can find same-directory modules.
 import grpc_version
@@ -44,10 +42,6 @@ class _NoOpCommand(setuptools.Command):
         pass
 
 
-PACKAGE_DIRECTORIES = {
-    "": ".",
-}
-
 INSTALL_REQUIRES = (
     "protobuf>=6.31.1,<7.0.0",
     "grpcio>={version}".format(version=grpc_version.VERSION),
@@ -67,17 +61,8 @@ except ImportError:
         "preprocess": _NoOpCommand,
     }
 
-setuptools.setup(
-    name="grpcio-testing",
-    version=grpc_version.VERSION,
-    license="Apache License 2.0",
-    description="Testing utilities for gRPC Python",
-    long_description=open(_README_PATH, "r").read(),
-    author="The gRPC Authors",
-    author_email="grpc-io@googlegroups.com",
-    url="https://grpc.io",
-    package_dir=PACKAGE_DIRECTORIES,
-    packages=setuptools.find_packages("."),
-    install_requires=INSTALL_REQUIRES,
-    cmdclass=COMMAND_CLASS,
-)
+if __name__ == '__main__':
+    setuptools.setup(
+        install_requires=INSTALL_REQUIRES,
+        cmdclass=COMMAND_CLASS,
+    )
