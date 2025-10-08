97fe90d31c57f4668eb72e84e7fc623f84bb6ce2
Alisha Nanda <alishananda@google.com>
2025-10-08 11:46:10 -0700

    Add tcp metrics to http2 ztrace, every 100ms if ztrace is active.
    PiperOrigin-RevId: 816797846

--- src/core/ext/transport/chttp2/transport/internal.h
+++ src/core/ext/transport/chttp2/transport/internal.h
@@ -593,6 +593,7 @@ struct grpc_chttp2_transport final : public grpc_core::FilterStackTransport,
 
   std::shared_ptr<grpc_core::Http2StatsCollector> http2_stats;
   grpc_core::Http2ZTraceCollector http2_ztrace_collector;
+  grpc_core::Timestamp last_ztrace_time = grpc_core::Timestamp::InfPast();
 
   GPR_NO_UNIQUE_ADDRESS grpc_core::latent_see::Flow write_flow;
 };
