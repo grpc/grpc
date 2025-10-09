a36d2716fbf1f994ec8d6cea71aa1a79db72e615
Ashesh Vidyut <asheshvidyut@google.com>
2025-10-09 10:18:51 +0000

    add todo

--- src/python/grpcio/grpc/aio/_server.py
+++ src/python/grpcio/grpc/aio/_server.py
@@ -57,6 +57,7 @@ class Server(_base_server.Server):
                     "Interceptor must be ServerInterceptor,"
                     "the following are invalid: {invalid_interceptors}"
                 )
+                # TODO: fix the value error below
                 raise ValueError(error_msg)
         self._server = cygrpc.AioServer(
             self._loop,
