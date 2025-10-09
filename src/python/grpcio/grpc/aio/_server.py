f413765bf668c6b0589fad22af57916c0cd599b2
Ashesh Vidyut <asheshvidyut@google.com>
2025-10-09 10:21:29 +0000

    udpate todo

--- src/python/grpcio/grpc/aio/_server.py
+++ src/python/grpcio/grpc/aio/_server.py
@@ -58,6 +58,7 @@ class Server(_base_server.Server):
                     "the following are invalid: {invalid_interceptors}"
                 )
                 # TODO: fix the value error below
+                # not caught by ruff.
                 raise ValueError(error_msg)
         self._server = cygrpc.AioServer(
             self._loop,
