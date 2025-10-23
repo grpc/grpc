7ed9398ce135413756ff9edb3ac0b5bb953f1979
Sreenithi Sridharan <ssreenithi@google.com>
2025-10-23 06:12:39 -0700

    [Python] Update setuptools min version to 77.0.1  (#40931)
    This PR updates the minimum version of `setuptools` package required across different Python setup files to v77.0.1. This version contains Python 3.14 support as well as deprecates a format for defining project license in `pyproject.toml` files ([Reference](https://setuptools.pypa.io/en/stable/history.html#id71)) which is a prerequisite for #40833
    
    Closes #40931
    
    PiperOrigin-RevId: 823008815

--- tools/distrib/python/grpcio_tools/setup.py
+++ tools/distrib/python/grpcio_tools/setup.py
@@ -23,6 +23,7 @@ import subprocess
 from subprocess import PIPE
 import sys
 import sysconfig
+import tempfile
 
 import setuptools
 from setuptools import Extension
@@ -130,6 +131,20 @@ class BuildExt(build_ext.build_ext):
         return filename
 
     def build_extensions(self):
+
+        # use short temp directory to avoid linker command file errors caused by
+        # exceeding 131071 characters in Windows.
+        # TODO(ssreenithi): Remove once we have a better solution: b/454497076
+        use_short_temp = os.environ.get(
+            "GRPC_PYTHON_BUILD_USE_SHORT_TEMP_DIR_NAME", 0
+        )
+        if use_short_temp == "1":
+            if not os.path.exists("pyb"):
+                os.mkdir("pyb")
+
+            self.build_temp = tempfile.mkdtemp(dir="pyb")
+            print(f"Using temp build directory: {self.build_temp}")
+
         # This is to let UnixCompiler get either C or C++ compiler options depending on the source.
         # Note that this doesn't work for MSVCCompiler and will be handled by _spawn_patch.py.
         old_compile = self.compiler._compile
@@ -349,7 +364,7 @@ setuptools.setup(
     install_requires=[
         "protobuf>=6.31.1,<7.0.0",
         "grpcio>={version}".format(version=grpc_version.VERSION),
-        "setuptools",
+        "setuptools>=77.0.1",
     ],
     package_data=package_data(),
     cmdclass={
