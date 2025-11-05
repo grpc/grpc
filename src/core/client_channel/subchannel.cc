3a82df1adfe1bd949b3a7f3ad65211f4b90b0c52
Mark D. Roth <roth@google.com>
2025-11-05 19:42:59 +0000

    implement StreamCounts

--- src/core/client_channel/subchannel.cc
+++ src/core/client_channel/subchannel.cc
@@ -96,6 +96,71 @@ using ::grpc_event_engine::experimental::EventEngine;
 // Subchannel::ConnectivityStateWatcherInterface.
 using TransportConnectivityStateWatcher = ConnectivityStateWatcherInterface;
 
+namespace {
+
+//
+// StreamCounts
+//
+
+class StreamCounts final {
+ public:
+  // Returns true if there is quota for another RPC to start on this
+  // connection.
+  bool SetMaxConcurrentStreams(uint32_t max_concurrent_streams) {
+    uint64_t prev_stream_counts =
+        stream_counts_.load(std::memory_order_acquire);
+    do {
+      const uint32_t rpcs_in_flight = GetRpcsInFlight(prev_stream_counts);
+    } while (!stream_counts_.compare_exchange_weak(                                     prev_stream_counts,
+        MakeStreamCounts(max_concurrent_streams, rpcs_in_flight),
+        std::memory_order_acq_rel, std::memory_order_acquire));
+    return rpcs_in_flight < max_concurrent_streams;
+  }
+
+  // Returns true if the RPC can start.
+  bool StartRpc() {
+    uint64_t prev_stream_counts =
+        stream_counts_.load(std::memory_order_acquire);
+    do {
+      const uint32_t rpcs_in_flight = GetRpcsInFlight(prev_stream_counts);
+      const uint32_t max_concurrent_streams =
+          GetMaxConcurrentStreams(prev_stream_counts);
+      if (rpcs_in_flight == max_concurrent_streams) return false;
+    } while (!stream_counts_.compare_exchange_weak(
+        prev_stream_counts, prev_stream_counts + MakeStreamCounts(0, 1),
+        std::memory_order_acq_rel, std::memory_order_acquire));
+    return true;
+  }
+
+  // Returns true if there is quota for another RPC to start on this
+  // connection.
+  bool FinishRpc() {
+    prev_stream_counts = stream_counts_.fetch_sub(MakeStreamCounts(0, 1));
+    return GetRpcsInFlight(prev_stream_counts) ==
+           GetMaxConcurrentStreams(prev_stream_counts);
+  }
+
+ private:
+  // First 32 bits are the MAX_CONCURRENT_STREAMS value reported by
+  // the transport.
+  // Last 32 bits are the current number of RPCs in flight on the connection.
+  static uint64_t MakeStreamCounts(uint32_t max_concurrent_streams,
+                                   uint32_t rpcs_in_flight) {
+    return (static_cast<uint64_t>(max_concurrent_streams) << 32) +
+           static_cast<int64_t>(rpcs_in_flight);
+  }
+  static uint32_t GetMaxConcurrentStreams(uint64_t stream_counts) {
+    return static_cast<uint32_t>(stream_counts >> 32);
+  }
+  static uint32_t GetRpcsInFlight(uint64_t stream_counts) {
+    return static_cast<uint32_t>(stream_counts & 0xffffffffu);
+  }
+
+  std::atomic<uint64_t> stream_counts_{0};
+};
+
+}  // namespace
+
 //
 // Subchannel::Call
 //
