5125beaf280d38f6211327fa6f865b7b87b11a3e
Atan Bhardwaj <atanbhardwaj@google.com>
2025-11-12 19:33:10 -0800

    Refactor `proto_reflection_descriptor_database` in util to use `absl::flat_hash_map` and `absl::flat_hash_set` instead of `std::unordered_map` and `std::unordered_set` for potential performance improvements. This also involves including the necessary absl headers and updating the `BUILD` file.
    Replace `.insert()` with `.emplace()` for `missing_symbols_` in
    `proto_reflection_descriptor_database.cc.`
    
    PiperOrigin-RevId: 831644658

--- test/cpp/util/proto_reflection_descriptor_database.h
+++ test/cpp/util/proto_reflection_descriptor_database.h
@@ -22,11 +22,11 @@
 #include <grpcpp/impl/codegen/config_protobuf.h>
 
 #include <mutex>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 #include "src/proto/grpc/reflection/v1alpha/reflection.grpc.pb.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
 
 namespace grpc {
 
@@ -98,10 +98,10 @@ class ProtoReflectionDescriptorDatabase : public protobuf::DescriptorDatabase {
   std::shared_ptr<ClientStream> stream_;
   grpc::ClientContext ctx_;
   std::unique_ptr<grpc::reflection::v1alpha::ServerReflection::Stub> stub_;
-  std::unordered_set<string> known_files_;
-  std::unordered_set<string> missing_symbols_;
-  std::unordered_map<string, std::unordered_set<int>> missing_extensions_;
-  std::unordered_map<string, std::vector<int>> cached_extension_numbers_;
+  absl::flat_hash_set<std::string> known_files_;
+  absl::flat_hash_set<string> missing_symbols_;
+  absl::flat_hash_map<string, absl::flat_hash_set<int>> missing_extensions_;
+  absl::flat_hash_map<string, std::vector<int>> cached_extension_numbers_;
   std::mutex stream_mutex_;
 
   protobuf::SimpleDescriptorDatabase cached_db_;
