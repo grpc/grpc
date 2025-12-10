b7241061737f9d9bbf796b72792500907df74994
Ana Salazar <anasalazar@google.com>
2025-12-10 20:23:23 +0000

    Use correcy closing guard on new include file

--- include/grpc/grpc_private_key_offload.h
+++ include/grpc/grpc_private_key_offload.h
@@ -77,4 +77,4 @@ GRPCAPI void grpc_tls_credentials_options_set_root_certificate_provider(
     grpc_tls_credentials_options* options,
     grpc_tls_certificate_provider* provider);
 
-#endif  // GRPC_GRPC_PRIVATE_KEY_OFFLOAD_H
+#endif /* GRPC_GRPC_PRIVATE_KEY_OFFLOAD_H */
