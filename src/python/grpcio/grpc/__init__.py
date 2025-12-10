b14626cb04d31ba68593372861e7d6218a3dbd6e
Ashesh Vidyut <asheshvidyut@google.com>
2025-12-10 03:59:55 +0000

    add attributes to RpcMethodHandler

--- src/python/grpcio/grpc/__init__.py
+++ src/python/grpcio/grpc/__init__.py
@@ -40,6 +40,7 @@ from grpc._cython import cygrpc as _cygrpc
 from grpc._runtime_protos import protos
 from grpc._runtime_protos import protos_and_services
 from grpc._runtime_protos import services
+from grpc._typing import ArityAgnosticMethodHandler
 from grpc._typing import ChannelArgumentType
 from grpc._typing import ClientInterceptor
 from grpc._typing import DeserializingFunction
@@ -1425,39 +1426,47 @@ class ServicerContext(RpcContext, metaclass=abc.ABCMeta):
 
 
 class RpcMethodHandler(abc.ABC):
-    """An implementation of a single RPC method.
+    """An implementation of a single RPC method."""
 
-    Attributes:
-      request_streaming: Whether the RPC supports exactly one request message
-        or any arbitrary number of request messages.
-      response_streaming: Whether the RPC supports exactly one response message
-        or any arbitrary number of response messages.
-      request_deserializer: A callable :term:`deserializer` that accepts a byte string and
-        returns an object suitable to be passed to this object's business
-        logic, or None to indicate that this object's business logic should be
-        passed the raw request bytes.
-      response_serializer: A callable :term:`serializer` that accepts an object produced
-        by this object's business logic and returns a byte string, or None to
-        indicate that the byte strings produced by this object's business logic
-        should be transmitted on the wire as they are.
-      unary_unary: This object's application-specific business logic as a
-        callable value that takes a request value and a ServicerContext object
-        and returns a response value. Only non-None if both request_streaming
-        and response_streaming are False.
-      unary_stream: This object's application-specific business logic as a
-        callable value that takes a request value and a ServicerContext object
-        and returns an iterator of response values. Only non-None if
-        request_streaming is False and response_streaming is True.
-      stream_unary: This object's application-specific business logic as a
-        callable value that takes an iterator of request values and a
-        ServicerContext object and returns a response value. Only non-None if
-        request_streaming is True and response_streaming is False.
-      stream_stream: This object's application-specific business logic as a
-        callable value that takes an iterator of request values and a
-        ServicerContext object and returns an iterator of response values.
-        Only non-None if request_streaming and response_streaming are both
-        True.
-    """
+    @property
+    @abc.abstractmethod
+    def request_streaming(self) -> bool:
+        """Whether the RPC supports exactly one request message or any arbitrary number of request messages."""
+
+    @property
+    @abc.abstractmethod
+    def response_streaming(self) -> bool:
+        """Whether the RPC supports exactly one response message or any arbitrary number of response messages."""
+
+    @property
+    @abc.abstractmethod
+    def request_deserializer(self) -> Optional[DeserializingFunction]:
+        """A callable :term:`deserializer` that accepts a byte string and returns an object suitable to be passed to this object's business logic, or None to indicate that this object's business logic should be passed the raw request bytes."""
+
+    @property
+    @abc.abstractmethod
+    def response_serializer(self) -> Optional[SerializingFunction]:
+        """A callable :term:`serializer` that accepts an object produced by this object's business logic and returns a byte string, or None to indicate that the byte strings produced by this object's business logic should be transmitted on the wire as they are."""
+
+    @property
+    @abc.abstractmethod
+    def unary_unary(self) -> Optional[ArityAgnosticMethodHandler]:
+        """This object's application-specific business logic as a callable value that takes a request value and a ServicerContext object and returns a response value. Only non-None if both request_streaming and response_streaming are False."""
+
+    @property
+    @abc.abstractmethod
+    def unary_stream(self) -> Optional[ArityAgnosticMethodHandler]:
+        """This object's application-specific business logic as a callable value that takes a request value and a ServicerContext object and returns an iterator of response values. Only non-None if request_streaming is False and response_streaming is True."""
+
+    @property
+    @abc.abstractmethod
+    def stream_unary(self) -> Optional[ArityAgnosticMethodHandler]:
+        """This object's application-specific business logic as a callable value that takes an iterator of request values and a ServicerContext object and returns a response value. Only non-None if request_streaming is True and response_streaming is False."""
+
+    @property
+    @abc.abstractmethod
+    def stream_stream(self) -> Optional[ArityAgnosticMethodHandler]:
+        """This object's application-specific business logic as a callable value that takes an iterator of request values and a ServicerContext object and returns an iterator of response values. Only non-None if request_streaming and response_streaming are both True."""
 
 
 class HandlerCallDetails(abc.ABC):
