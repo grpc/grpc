c2a3819e8bdf89e5db92aa8b20c20e2ff8d4c9ab
Weizhe Yuan <weizheyuan@google.com>
2025-12-16 17:47:42 -0800

    Move --check_direct_dependencies=error to tools/remote_build/linux_bzlmod.bazelrc

--- tools/bazelify_tests/test/bazel_build_with_bzlmod_linux.sh
+++ tools/bazelify_tests/test/bazel_build_with_bzlmod_linux.sh
@@ -19,7 +19,6 @@ set -ex
 tools/bazel \
     --bazelrc=tools/remote_build/linux_bzlmod.bazelrc \
     build \
-    --check_direct_dependencies=error \
     -- \
     :all \
     -:grpcpp_csm_observability  # Needs google_cloud_cpp to be added to BCR
@@ -28,7 +27,6 @@ tools/bazel \
 tools/bazel \
     --bazelrc=tools/remote_build/linux_bzlmod.bazelrc \
     build \
-    --check_direct_dependencies=error \
     -- \
     //examples/cpp/... \
     -//examples/cpp/csm/...  # Needs grpcpp_csm_observability
@@ -39,7 +37,6 @@ tools/bazel \
 tools/bazel \
     --bazelrc=tools/remote_build/linux_bzlmod.bazelrc \
     test \
-    --check_direct_dependencies=error \
     -- \
     //test/core/config:all \
     //test/cpp/common:all
@@ -49,7 +46,6 @@ tools/bazel \
 tools/bazel \
     --bazelrc=tools/remote_build/linux_bzlmod.bazelrc \
     build \
-    --check_direct_dependencies=error \
     --define=//third_party:grpc_use_openssl=true \
     -- \
     :all \
