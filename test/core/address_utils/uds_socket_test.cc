e24e4d9b7c56f2a2c45540916738bd436053e114
Ganesh T N <tnganesh@live.com>
2025-12-23 13:28:49 +0530

    kokoro run updates

--- test/core/address_utils/uds_socket_test.cc
+++ test/core/address_utils/uds_socket_test.cc
@@ -1,22 +1,26 @@
-/*
- *
- * Copyright 2025 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+//
+//
+// Copyright 2025 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
 
 #include <grpc/support/port_platform.h>
+
+#ifdef GRPC_HAVE_UNIX_SOCKET
+#include <sys/un.h>
+#endif
 
 #include "absl/log/log.h"
 #include "gtest/gtest.h"
@@ -28,9 +32,6 @@
 #include "src/core/lib/iomgr/port.h"
 #include "src/core/lib/iomgr/sockaddr.h"
 #include "test/core/test_util/test_config.h"
-
-#ifdef GRPC_HAVE_UNIX_SOCKET
-#include <sys/un.h>
 
 TEST(UdsSocketTest, UnixSockaddrPopulateAndLen) {
   grpc_resolved_address resolved_addr;
