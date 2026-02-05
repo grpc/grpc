4c6ce39bc4b13c621f5bf53e728650a4c236a0dc
Tanvi Jagtap <tjagtap@google.com>
2026-02-05 09:38:30 -0800

    [export] [PH2][CPPE2E] grpclb_end2end_test
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/865987991](http://cl/865987991)
    
    PiperOrigin-RevId: 865987991

--- test/cpp/end2end/grpclb_end2end_test.cc
+++ test/cpp/end2end/grpclb_end2end_test.cc
@@ -513,6 +513,10 @@ class GrpclbEnd2endTest : public ::testing::Test {
   static void TearDownTestSuite() { grpc_shutdown(); }
 
   void SetUp() override {
+    SKIP_TEST_FOR_PH2(
+        "TODO(tjagtap): [PH2][OSS] grpclb is used by OSS customers. This "
+        "entire test suite will need to pass before PH2 can be enabled for OSS "
+        "users.");
     response_generator_ =
         grpc_core::MakeRefCounted<grpc_core::FakeResolverResponseGenerator>();
     balancer_ = CreateAndStartBalancer();
@@ -933,7 +937,6 @@ TEST_F(GrpclbEnd2endTest,
 }
 
 TEST_F(GrpclbEnd2endTest, UsePickFirstChildPolicy) {
-  SKIP_TEST_FOR_PH2("TODO(tjagtap) [PH2][P1] Fix bug");
   const size_t kNumBackends = 2;
   const size_t kNumRpcs = kNumBackends * 2;
   CreateBackends(kNumBackends);
@@ -971,7 +974,6 @@ TEST_F(GrpclbEnd2endTest, UsePickFirstChildPolicy) {
 }
 
 TEST_F(GrpclbEnd2endTest, SwapChildPolicy) {
-  SKIP_TEST_FOR_PH2("TODO(tjagtap) [PH2][P1] Fix bug");
   const size_t kNumBackends = 2;
   const size_t kNumRpcs = kNumBackends * 2;
   CreateBackends(kNumBackends);
@@ -1034,7 +1036,6 @@ TEST_F(GrpclbEnd2endTest, SameBackendListedMultipleTimes) {
 }
 
 TEST_F(GrpclbEnd2endTest, InitiallyEmptyServerlist) {
-  SKIP_TEST_FOR_PH2("TODO(tjagtap) [PH2][P1] Fix bug");
   CreateBackends(1);
   SetNextResolutionDefaultBalancer();
   // First response is an empty serverlist.  RPCs should fail.
@@ -1164,7 +1165,6 @@ TEST_F(GrpclbEnd2endTest,
 
 TEST_F(GrpclbEnd2endTest,
        FallbackAfterStartupLoseContactWithBackendsThenBalancer) {
-  SKIP_TEST_FOR_PH2("TODO(tjagtap) [PH2][P1] Fix. Flakes 10% of the time.");
   // First two backends are fallback, last two are pointed to by balancer.
   const size_t kNumBackends = 4;
   const size_t kNumFallbackBackends = 2;
@@ -1263,7 +1263,6 @@ TEST_F(GrpclbEnd2endTest, FallbackControlledByBalancerAfterFirstServerlist) {
 }
 
 TEST_F(GrpclbEnd2endTest, BackendsRestart) {
-  SKIP_TEST_FOR_PH2("TODO(tjagtap) [PH2][P1] Flaking 2 out of 100 times.");
   CreateBackends(2);
   SetNextResolutionDefaultBalancer();
   SendBalancerResponse(BuildResponseForBackends(GetBackendPorts(), {}));
@@ -1299,7 +1298,6 @@ TEST_F(GrpclbEnd2endTest, ServiceNameFromLbPolicyConfig) {
 
 TEST_F(GrpclbEnd2endTest,
        NewBalancerAddressNotUsedIfOriginalStreamDoesNotFail) {
-  SKIP_TEST_FOR_PH2("TODO(tjagtap) [PH2][P1] Fix bug");
   CreateBackends(3);
   // Default balancer sends backend 0.
   SendBalancerResponse(BuildResponseForBackends({backends_[0]->port()}, {}));
@@ -1578,7 +1576,6 @@ TEST_F(GrpclbEnd2endTest, DropAll) {
 }
 
 TEST_F(GrpclbEnd2endTest, ClientLoadReporting) {
-  SKIP_TEST_FOR_PH2("TODO(tjagtap) [PH2][P1] Fix bug");
   const size_t kNumBackends = 3;
   CreateBackends(kNumBackends);
   balancer_->service().set_client_load_reporting_interval_seconds(3);
@@ -1618,7 +1615,6 @@ TEST_F(GrpclbEnd2endTest, ClientLoadReporting) {
 }
 
 TEST_F(GrpclbEnd2endTest, LoadReportingWithBalancerRestart) {
-  SKIP_TEST_FOR_PH2("TODO(tjagtap) [PH2][P1] Fix bug");
   const size_t kNumBackends = 4;
   const size_t kNumBackendsFirstPass = 2;
   const size_t kNumBackendsSecondPass = kNumBackends - kNumBackendsFirstPass;
@@ -1676,7 +1672,6 @@ TEST_F(GrpclbEnd2endTest, LoadReportingWithBalancerRestart) {
 }
 
 TEST_F(GrpclbEnd2endTest, LoadReportingWithDrops) {
-  SKIP_TEST_FOR_PH2("TODO(tjagtap) [PH2][P1] Fix bug");
   const size_t kNumBackends = 3;
   const size_t kNumRpcsPerAddress = 3;
   const int kNumDropRateLimiting = 2;
