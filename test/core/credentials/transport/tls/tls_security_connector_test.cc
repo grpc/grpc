16b5135cd656a8c5ae5f95a22c39d6949fd2ce94
Ana Salazar <anasalazar@google.com>
2026-02-09 18:53:54 +0000

    Remove unused helper function

--- test/core/credentials/transport/tls/tls_security_connector_test.cc
+++ test/core/credentials/transport/tls/tls_security_connector_test.cc
@@ -47,15 +47,6 @@
 #define SERVER_CERT_PATH_1 "src/core/tsi/test_creds/server1.pem"
 #define SERVER_KEY_PATH_1 "src/core/tsi/test_creds/server1.key"
 
-namespace grpc_core {
-namespace {
-RefCountedPtr<grpc_tls_certificate_provider> CreateTestingCertificateProvider(
-    const std::string& root_cert_info,
-    const PemKeyCertPairList& pem_key_cert_pairs) {
-  auto provider = MakeRefCounted<InMemoryCertificateProvider>();
-  return provider;
-}
-}  // namespace
 namespace testing {
 
 constexpr const char* kRootCertName = "root_cert_name";
