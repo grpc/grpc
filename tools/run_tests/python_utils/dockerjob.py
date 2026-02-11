eaa480d7c9a5e3a4b7c0e8c55a706fedf9923905
Kannan J <kannanjgithub@google.com>
2026-02-11 17:27:39 +0000

    Add debug print stmts

--- tools/run_tests/python_utils/dockerjob.py
+++ tools/run_tests/python_utils/dockerjob.py
@@ -155,7 +155,7 @@ class DockerJob:
     """Encapsulates a job"""
 
     def __init__(self, spec):
-        print('DockerJob:__init__ called with spec ' + spec, flush=True)
+        print('DockerJob:__init__ called with spec ' + spec.shortname, flush=True)
         self._spec = spec
         self._job = jobset.Job(
             spec, newline_on_success=True, travis=True, add_env={}
