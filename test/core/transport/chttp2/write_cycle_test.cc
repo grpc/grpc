5bf1dc89683b40a4e0a8f114564398bee53ebb68
Akshit Patel <akshitpatel@google.com>
2026-02-17 03:49:09 +0000

    Fix

--- test/core/transport/chttp2/write_cycle_test.cc
+++ test/core/transport/chttp2/write_cycle_test.cc
@@ -35,7 +35,6 @@ namespace {}  // namespace
 }  // namespace http2
 }  // namespace grpc_core
 
-
 int main(int argc, char** argv) {
   grpc::testing::TestEnvironment env(&argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
