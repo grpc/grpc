ac678f5bab95b0baeaf2a92c89f2a2f3c220a3ba
ac-patel <akshitpatel@google.com>
2026-02-16 22:52:07 -0800

    [PH2][Perf] Build changes (#41642)
    Closes #41642
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/41642 from ac-patel:build1 5bf1dc89683b40a4e0a8f114564398bee53ebb68
    PiperOrigin-RevId: 871128418

---
+++ test/core/transport/chttp2/write_cycle_test.cc
@@ -0,0 +1,42 @@
+//
+//
+// Copyright 2026 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+
+#include "src/core/ext/transport/chttp2/transport/write_cycle.h"
+
+#include <string>
+#include <utility>
+
+#include "src/core/ext/transport/chttp2/transport/frame.h"
+#include "src/core/ext/transport/chttp2/transport/write_size_policy.h"
+#include "src/core/lib/slice/slice.h"
+#include "src/core/lib/slice/slice_buffer.h"
+#include "test/core/test_util/test_config.h"
+#include "gtest/gtest.h"
+#include "absl/strings/string_view.h"
+
+namespace grpc_core {
+namespace http2 {
+namespace {}  // namespace
+}  // namespace http2
+}  // namespace grpc_core
+
+int main(int argc, char** argv) {
+  grpc::testing::TestEnvironment env(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
