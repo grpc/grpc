5088435207a3d394ccd579d3189a80b71ce80823
Ashesh Vidyut <asheshvidyut@google.com>
2026-02-18 15:25:25 +0530

    revert

--- src/python/grpcio/grpc/_compression.py
+++ src/python/grpcio/grpc/_compression.py
@@ -39,7 +39,7 @@ def _compression_algorithm_to_metadata_value(
 
 def compression_algorithm_to_metadata(compression: grpc.Compression):
     return (
-        grpc._common.decode(cygrpc.GRPC_COMPRESSION_REQUEST_ALGORITHM_MD_KEY),
+        cygrpc.GRPC_COMPRESSION_REQUEST_ALGORITHM_MD_KEY,
         _compression_algorithm_to_metadata_value(compression),
     )
 
