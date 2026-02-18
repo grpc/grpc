99542dda31295f855443a07a89485f4cae5cdf81
Siddharth Nohria <snohria@google.com>
2026-02-18 07:14:25 +0000

    Fix Sanity

--- include/grpcpp/impl/generic_stub_session.h
+++ include/grpcpp/impl/generic_stub_session.h
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #ifndef GRPCPP_IMPL_GENERIC_STUB_SESSION_H
 #define GRPCPP_IMPL_GENERIC_STUB_SESSION_H
 
