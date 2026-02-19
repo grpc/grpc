0b59187b4355921526222e76a1977c48e361b3c2
Rishesh Agarwal <rishesh@google.com>
2026-02-19 11:52:48 +0000

    ext_authz_client

---
+++ src/core/ext/filters/ext_authz/ext_authz_client.cc
@@ -0,0 +1,362 @@
+//
+// Copyright 2026 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include "src/core/ext/filters/ext_authz/ext_authz_client.h"
+
+#include <grpc/event_engine/event_engine.h>
+
+#include <cstddef>
+#include <memory>
+#include <optional>
+#include <string>
+
+#include "envoy/config/core/v3/base.upb.h"
+#include "envoy/service/auth/v3/attribute_context.upb.h"
+#include "envoy/service/auth/v3/external_auth.upb.h"
+#include "envoy/type/v3/http_status.upb.h"
+#include "google/protobuf/timestamp.upb.h"
+#include "google/rpc/status.upb.h"
+#include "src/core/lib/debug/trace.h"
+#include "src/core/lib/iomgr/exec_ctx.h"
+#include "src/core/lib/transport/status_conversion.h"
+#include "src/core/util/backoff.h"
+#include "src/core/util/debug_location.h"
+#include "src/core/util/grpc_check.h"
+#include "src/core/util/orphanable.h"
+#include "src/core/util/ref_counted_ptr.h"
+#include "src/core/util/sync.h"
+#include "src/core/util/upb_utils.h"
+#include "src/core/util/xds_utils.h"
+#include "src/core/xds/xds_client/xds_transport.h"
+#include "upb/base/string_view.h"
+#include "absl/base/thread_annotations.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
+
+namespace grpc_core {
+
+using ::grpc_event_engine::experimental::EventEngine;
+
+//
+// Internal class declarations
+//
+
+//
+// ExtAuthzClient::ExtAuthzChannel
+//
+
+ExtAuthzClient::ExtAuthzChannel::ExtAuthzChannel(
+    WeakRefCountedPtr<ExtAuthzClient> ext_authz_client_,
+    std::shared_ptr<const XdsBootstrap::XdsServerTarget> server)
+    : DualRefCounted<ExtAuthzChannel>(
+          GRPC_TRACE_FLAG_ENABLED(xds_client_refcount) ? "ExtAuthzChannel"
+                                                       : nullptr),
+      ext_authz_client_(std::move(ext_authz_client_)),
+      server_(std::move(server)) {
+  GRPC_TRACE_LOG(xds_client, INFO)
+      << "[ext_authz_client " << ext_authz_client_.get()
+      << "] creating channel " << this << " for server "
+      << server_->server_uri();
+  absl::Status status;
+  transport_ =
+      ext_authz_client_->transport_factory_->GetTransport(*server_, &status);
+  GRPC_CHECK(transport_ != nullptr);
+  if (!status.ok()) {
+    LOG(ERROR) << "Error creating ExtAuthz channel to " << server_->server_uri()
+               << ": " << status;
+  }
+}
+
+ExtAuthzClient::ExtAuthzChannel::~ExtAuthzChannel() {
+  GRPC_TRACE_LOG(xds_client, INFO) << "[ext_authz_client " << ext_authz_client()
+                                   << "] destroying ExtAuthz channel " << this
+                                   << " for server " << server_->server_uri();
+  ext_authz_client_.reset(DEBUG_LOCATION, "ExtAuthzChannel");
+}
+
+// This method should only ever be called when holding the lock, but we can't
+// use a ABSL_EXCLUSIVE_LOCKS_REQUIRED annotation, because Orphan() will be
+// called from DualRefCounted::Unref(), which cannot have a lock annotation for
+// a lock in this subclass.
+void ExtAuthzClient::ExtAuthzChannel::Orphaned()
+    ABSL_NO_THREAD_SAFETY_ANALYSIS {
+  GRPC_TRACE_LOG(xds_client, INFO) << "[ext_authz_client " << ext_authz_client()
+                                   << "] orphaning ExtAuthz channel " << this
+                                   << " for server " << server_->server_uri();
+  transport_.reset();
+  // At this time, all strong refs are removed, remove from channel map to
+  // prevent subsequent subscription from trying to use this ExtAuthzChannel as
+  // it is shutting down.
+  ext_authz_client_->ext_authz_channel_map_.erase(server_->Key());
+}
+
+void ExtAuthzClient::ExtAuthzChannel::ResetBackoff() {
+  transport_->ResetBackoff();
+}
+
+absl::StatusOr<ExtAuthzClient::ExtAuthzResponse>
+ExtAuthzClient::ExtAuthzChannel::Check(const ExtAuthzRequestParams& params) {
+  std::string payload;
+  {
+    MutexLock lock(&ext_authz_client()->mu_);
+    payload = ext_authz_client()->CreateExtAuthzRequest(params);
+  }
+  const char* method = "/envoy.service.auth.v3.Authorization/Check";
+  auto call = transport_->CreateUnaryCall(method);
+  if (call == nullptr) {
+    return absl::UnavailableError("Failed to create unary call");
+  }
+  // Start the call.
+  GRPC_TRACE_LOG(xds_client, INFO)
+      << "[ext_authz_client " << ext_authz_client() << "] ext_authz server "
+      << server_->server_uri() << ": starting ext_authz call";
+  auto status = call->SendMessage(std::move(payload));
+  if (!status.ok()) {
+    return status.status();
+  }
+  MutexLock lock(&ext_authz_client()->mu_);
+  return ext_authz_client()->ParseExtAuthzResponse(*status);
+}
+
+
+//
+// ExtAuthzClient::ExtAuthzRequest
+//
+
+namespace {
+
+struct ExtAuthzApiContext {
+  ExtAuthzClient* client;
+  upb_DefPool* def_pool;
+  upb_Arena* arena;
+};
+
+std::string SerializeExtAuthzRequest(
+    const ExtAuthzApiContext& context,
+    const envoy_service_auth_v3_AttributeContext* request) {
+  size_t output_length;
+  char* output = envoy_service_auth_v3_AttributeContext_serialize(
+      request, context.arena, &output_length);
+  return std::string(output, output_length);
+}
+
+}  // namespace
+
+envoy_service_auth_v3_AttributeContext_Request* CreateRequest(
+    const ExtAuthzApiContext& context,
+    const ExtAuthzClient::ExtAuthzRequestParams& params) {
+  envoy_service_auth_v3_AttributeContext_Request* request =
+      envoy_service_auth_v3_AttributeContext_Request_new(context.arena);
+  envoy_service_auth_v3_AttributeContext_HttpRequest* http_request =
+      envoy_service_auth_v3_AttributeContext_HttpRequest_new(context.arena);
+  // set method
+  envoy_service_auth_v3_AttributeContext_HttpRequest_set_method(
+      http_request, upb_StringView_FromString("POST"));
+  // TODO(rishesh)
+  // set path
+  envoy_service_auth_v3_AttributeContext_HttpRequest_set_path(
+      http_request, upb_StringView_FromString(params.path.data()));
+  // set size
+  envoy_service_auth_v3_AttributeContext_HttpRequest_set_size(http_request, -1);
+  // set protocol
+  envoy_service_auth_v3_AttributeContext_HttpRequest_set_protocol(
+      http_request, upb_StringView_FromString("HTTP/2"));
+  // set http_request to request
+  envoy_service_auth_v3_AttributeContext_Request_set_http(request,
+                                                          http_request);
+  // set time
+  const Timestamp now = Timestamp::Now();
+  google_protobuf_Timestamp* timestamp =
+      google_protobuf_Timestamp_new(context.arena);
+  google_protobuf_Timestamp_set_nanos(
+      timestamp, now.milliseconds_after_process_epoch() * 1000000);
+  // set headers
+  auto header_map = envoy_config_core_v3_HeaderMap_new(context.arena);
+  for (auto& [key, value] : params.headers) {
+    auto* header_to_assign =
+        envoy_config_core_v3_HeaderMap_add_headers(header_map, context.arena);
+    envoy_config_core_v3_HeaderValue_set_key(
+        header_to_assign, upb_StringView_FromString(key.data()));
+    envoy_config_core_v3_HeaderValue_set_value(
+        header_to_assign, upb_StringView_FromString(value.data()));
+  }
+  envoy_service_auth_v3_AttributeContext_Request_set_time(request, timestamp);
+  return request;
+}
+
+envoy_service_auth_v3_AttributeContext_Peer* CreateSource(
+    const ExtAuthzApiContext& context) {
+  // TODO(rishesh): add logic for create source
+  return nullptr;
+}
+
+envoy_service_auth_v3_AttributeContext_Peer* CreateDestination(
+    const ExtAuthzApiContext& context) {
+  // TODO(rishesh): add logic for create destination
+  return nullptr;
+}
+
+std::string ExtAuthzClient::CreateExtAuthzRequest(
+    const ExtAuthzRequestParams& params) {
+  upb::Arena arena;
+  const ExtAuthzApiContext context = {this, def_pool_.ptr(), arena.ptr()};
+  envoy_service_auth_v3_AttributeContext* attribute_context =
+      envoy_service_auth_v3_AttributeContext_new(arena.ptr());
+
+  if (!params.is_client_call) {
+    envoy_service_auth_v3_AttributeContext_set_source(attribute_context,
+                                                      CreateSource(context));
+    envoy_service_auth_v3_AttributeContext_set_destination(
+        attribute_context, CreateDestination(context));
+  }
+  envoy_service_auth_v3_AttributeContext_set_request(
+      attribute_context, CreateRequest(context, params));
+
+  return SerializeExtAuthzRequest(context, attribute_context);
+}
+
+//
+// ExtAuthzClient::ExtAuthzResponse
+//
+
+absl::StatusOr<ExtAuthzClient::ExtAuthzResponse>
+ExtAuthzClient::ParseExtAuthzResponse(absl::string_view encoded_response) {
+  upb::Arena arena;
+  const envoy_service_auth_v3_CheckResponse* decoded_response =
+      envoy_service_auth_v3_CheckResponse_parse(
+          encoded_response.data(), encoded_response.size(), arena.ptr());
+  if (decoded_response == nullptr) {
+    return absl::UnavailableError("Can't decode response.");
+  }
+  // const ExtAuthzApiContext context = {this, def_pool_.ptr(), arena.ptr()};
+  const auto* status =
+      envoy_service_auth_v3_CheckResponse_status(decoded_response);
+  const int32_t status_code = google_rpc_Status_code(status);
+  ExtAuthzResponse result;
+  result.status_code = grpc_http2_status_to_grpc_status(status_code);
+  ValidationErrors errors;
+  if (status_code == 200) {
+    const auto* ok_resp =
+        envoy_service_auth_v3_CheckResponse_ok_response(decoded_response);
+    ExtAuthzResponse::OkResponse ok_struct;
+
+    // Headers
+    size_t size = 0;
+    const auto* const* headers =
+        envoy_service_auth_v3_OkHttpResponse_headers(ok_resp, &size);
+    for (size_t i = 0; i < size; ++i) {
+      ok_struct.headers.push_back(ParseHeaderValueOption(headers[i], &errors));
+    }
+
+    // Headers to remove
+    auto headers_remove =
+        envoy_service_auth_v3_OkHttpResponse_headers_to_remove(ok_resp, &size);
+    for (size_t i = 0; i < size; ++i) {
+      ok_struct.headers_to_remove.push_back(
+          UpbStringToStdString(headers_remove[i]));
+    }
+
+    // Response headers to add
+    const auto* const* resp_headers =
+        envoy_service_auth_v3_OkHttpResponse_response_headers_to_add(ok_resp,
+                                                                     &size);
+    for (size_t i = 0; i < size; ++i) {
+      ok_struct.response_headers_to_add.push_back(
+          ParseHeaderValueOption(resp_headers[i], &errors));
+    }
+
+    result.ok_response = std::move(ok_struct);
+  } else {
+    if (envoy_service_auth_v3_CheckResponse_has_denied_response(
+            decoded_response)) {
+      const auto* denied =
+          envoy_service_auth_v3_CheckResponse_denied_response(decoded_response);
+      ExtAuthzResponse::DeniedResponse denied_struct;
+
+      const auto* http_status =
+          envoy_service_auth_v3_DeniedHttpResponse_status(denied);
+      envoy_type_v3_HttpStatus_code(http_status);
+      denied_struct.status = grpc_http2_status_to_grpc_status(
+          envoy_type_v3_HttpStatus_code(http_status));
+
+      size_t size = 0;
+      const auto* const* headers =
+          envoy_service_auth_v3_DeniedHttpResponse_headers(denied, &size);
+      for (size_t i = 0; i < size; ++i) {
+        denied_struct.headers.push_back(
+            ParseHeaderValueOption(headers[i], &errors));
+      }
+      result.denied_response = std::move(denied_struct);
+    }
+  }
+  return result;
+}
+
+//
+// ExtAuthzClient
+//
+
+ExtAuthzClient::ExtAuthzClient(
+    std::shared_ptr<XdsBootstrap> bootstrap,
+    RefCountedPtr<XdsTransportFactory> transport_factory,
+    std::shared_ptr<grpc_event_engine::experimental::EventEngine> engine)
+    : DualRefCounted<ExtAuthzClient>(
+          GRPC_TRACE_FLAG_ENABLED(xds_client_refcount) ? "ExtAuthzClient"
+                                                       : nullptr),
+      engine_(std::move(engine)),
+      bootstrap_(std::move(bootstrap)),
+      transport_factory_(std::move(transport_factory)) {
+  GRPC_TRACE_LOG(xds_client, INFO)
+      << "[ext_authz_client " << this << "] creating ext_authz client";
+}
+
+ExtAuthzClient::~ExtAuthzClient() {
+  GRPC_TRACE_LOG(xds_client, INFO)
+      << "[ext_authz_client " << this << "] destroying ext_authz client";
+}
+
+RefCountedPtr<ExtAuthzClient::ExtAuthzChannel>
+ExtAuthzClient::GetOrCreateExtAuthzChannelLocked(
+    std::shared_ptr<const XdsBootstrap::XdsServerTarget> server,
+    const char* reason) {
+  std::string key = server->Key();
+  auto it = ext_authz_channel_map_.find(key);
+  if (it != ext_authz_channel_map_.end()) {
+    return it->second->Ref(DEBUG_LOCATION, reason);
+  }
+  // Channel not found, so create a new one.
+  auto ext_authz_channel = MakeRefCounted<ExtAuthzChannel>(
+      WeakRef(DEBUG_LOCATION, "ExtAuthzChannel"), std::move(server));
+  ext_authz_channel_map_[std::move(key)] = ext_authz_channel.get();
+  return ext_authz_channel;
+}
+
+void ExtAuthzClient::ResetBackoff() {
+  MutexLock lock(&mu_);
+  for (auto& [_, ext_authz_channel] : ext_authz_channel_map_) {
+    ext_authz_channel->ResetBackoff();
+  }
+}
+
+void ExtAuthzClient::RemoveExtAuthzChannel(const std::string& key) {
+  auto it = ext_authz_channel_map_.find(key);
+  if (it != ext_authz_channel_map_.end()) {
+    ext_authz_channel_map_.erase(key);
+  }
+}
+
+}  // namespace grpc_core
---
+++ src/core/ext/filters/ext_authz/ext_authz_client.h
@@ -0,0 +1,122 @@
+//
+// Copyright 2026 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef GRPC_SRC_CORE_EXT_FILTERS_EXT_AUTHZ_EXT_AUTHZ_CLIENT_H
+#define GRPC_SRC_CORE_EXT_FILTERS_EXT_AUTHZ_EXT_AUTHZ_CLIENT_H
+
+#include <grpc/event_engine/event_engine.h>
+#include <grpc/status.h>
+#include <grpc/support/port_platform.h>
+
+#include "src/core/util/dual_ref_counted.h"
+#include "src/core/util/sync.h"
+#include "src/core/xds/grpc/xds_common_types.h"
+#include "src/core/xds/xds_client/xds_bootstrap.h"
+#include "src/core/xds/xds_client/xds_transport.h"
+#include "upb/reflection/def.hpp"
+namespace grpc_core {
+
+class ExtAuthzClient : public DualRefCounted<ExtAuthzClient> {
+ public:
+  ExtAuthzClient(
+      std::shared_ptr<XdsBootstrap> bootstrap,
+      RefCountedPtr<XdsTransportFactory> transport_factory,
+      std::shared_ptr<grpc_event_engine::experimental::EventEngine> engine);
+  ~ExtAuthzClient() override;
+
+  // Resets connection backoff state.
+  void ResetBackoff();
+
+  XdsTransportFactory* transport_factory() const {
+    return transport_factory_.get();
+  }
+
+  grpc_event_engine::experimental::EventEngine* engine() {
+    return engine_.get();
+  }
+
+  struct ExtAuthzResponse {
+    struct OkResponse {
+      std::vector<HeaderValueOption> headers;
+      std::vector<std::string> headers_to_remove;
+      std::vector<HeaderValueOption> response_headers_to_add;
+    };
+
+    struct DeniedResponse {
+      grpc_status_code status;
+      std::vector<HeaderValueOption> headers;
+    };
+
+    grpc_status_code status_code;
+    OkResponse ok_response;
+    DeniedResponse denied_response;
+  };
+
+  struct ExtAuthzRequestParams {
+    bool is_client_call;
+    std::vector<std::pair<std::string, std::string>> headers;
+    std::string path;
+  };
+
+ private:
+  class ExtAuthzChannel final : public DualRefCounted<ExtAuthzChannel> {
+   public:
+    absl::StatusOr<ExtAuthzResponse> Check(const ExtAuthzRequestParams& params);
+
+    ExtAuthzChannel(
+        WeakRefCountedPtr<ExtAuthzClient> ext_authz_client_,
+        std::shared_ptr<const XdsBootstrap::XdsServerTarget> server);
+    ~ExtAuthzChannel() override;
+
+    ExtAuthzClient* ext_authz_client() const { return ext_authz_client_.get(); }
+    absl::string_view server_uri() const { return server_->server_uri(); }
+    void ResetBackoff();
+
+   private:
+    void Orphaned() override;
+
+    // The owning ExtAuthzClient.
+    WeakRefCountedPtr<ExtAuthzClient> ext_authz_client_;
+
+    std::shared_ptr<const XdsBootstrap::XdsServerTarget> server_;
+
+    RefCountedPtr<XdsTransportFactory::XdsTransport> transport_;
+  };
+
+  std::string CreateExtAuthzRequest(const ExtAuthzRequestParams& params)
+      ABSL_EXCLUSIVE_LOCKS_REQUIRED(&mu_);
+
+  absl::StatusOr<ExtAuthzResponse> ParseExtAuthzResponse(
+      absl::string_view encoded_response) ABSL_EXCLUSIVE_LOCKS_REQUIRED(&mu_);
+
+  void RemoveExtAuthzChannel(const std::string& key)
+      ABSL_EXCLUSIVE_LOCKS_REQUIRED(&mu_);
+  RefCountedPtr<ExtAuthzChannel> GetOrCreateExtAuthzChannelLocked(
+      std::shared_ptr<const XdsBootstrap::XdsServerTarget> server,
+      const char* reason) ABSL_EXCLUSIVE_LOCKS_REQUIRED(mu_);
+
+  std::shared_ptr<grpc_event_engine::experimental::EventEngine> engine_;
+  std::shared_ptr<XdsBootstrap> bootstrap_;  // not required
+  RefCountedPtr<XdsTransportFactory> transport_factory_;
+  Mutex mu_;
+  upb::DefPool def_pool_ ABSL_GUARDED_BY(mu_);
+  std::map<std::string /*XdsServer key*/, ExtAuthzChannel*>
+      ext_authz_channel_map_ ABSL_GUARDED_BY(mu_);
+};
+
+}  // namespace grpc_core
+
+#endif  // GRPC_SRC_CORE_EXT_FILTERS_EXT_AUTHZ_EXT_AUTHZ_CLIENT_H
