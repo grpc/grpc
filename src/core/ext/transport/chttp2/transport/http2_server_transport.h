3a7e256976113f0c442ee40c59d7b9a8f0a7e4a0
Tanvi Jagtap <tjagtap@google.com>
2026-02-25 18:33:41 -0800

    [export] [PH2][Server][Settings] Part 1 of 3 1. Copied the settings functions as-is from http2_client_transport.h . Just changed Http2ClientTransport -> Http2ServerTransport and GRPC_HTTP2_CLIENT_DLOG -> GRPC_HTTP2_SERVER_DLOG
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/875434845](http://cl/875434845)
    
    PiperOrigin-RevId: 875434845

--- src/core/ext/transport/chttp2/transport/http2_server_transport.h
+++ src/core/ext/transport/chttp2/transport/http2_server_transport.h
@@ -405,9 +405,9 @@ class Http2ServerTransport final : public ServerTransport,
   //////////////////////////////////////////////////////////////////////////////
   // Settings
 
-  // auto WaitForSettingsTimeoutOnDone();
-  // void MaybeSpawnWaitForSettingsTimeout();
-  // void EnforceLatestIncomingSettings();
+  void EnforceLatestIncomingSettings();
+  auto WaitForSettingsTimeoutOnDone();
+  void MaybeSpawnWaitForSettingsTimeout();
 
   //////////////////////////////////////////////////////////////////////////////
   // Flow Control and BDP
