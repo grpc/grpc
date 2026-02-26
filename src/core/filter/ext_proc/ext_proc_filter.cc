6c4bfaf97c12019564a8bf2b748bed0a8cfd69e4
Mark D. Roth <roth@google.com>
2026-02-26 21:22:07 +0000

    remove unused code

--- src/core/filter/ext_proc/ext_proc_filter.cc
+++ src/core/filter/ext_proc/ext_proc_filter.cc
@@ -120,129 +120,16 @@ absl::StatusOr<RefCountedPtr<ExtProcFilter>> ExtProcFilter::Create(
                                          std::move(filter_args));
 }
 
-namespace {
-
-// An adaptor to use an InterceptionChainBuilder as a FilterChainBuilder
-// when dealing with xDS HTTP filters.
-class InterceptionChainBuilderWrapper final : public FilterChainBuilder {
- public:
-  explicit InterceptionChainBuilderWrapper(InterceptionChainBuilder& builder)
-      : builder_(builder) {}
-
-  absl::StatusOr<RefCountedPtr<FilterChain>> Build() override {
-    return absl::UnimplementedError("should never be called");
-  }
-
- private:
-  void AddFilter(const FilterHandle& filter_handle,
-                 RefCountedPtr<const FilterConfig> config) override {
-    filter_handle.AddToBuilder(&builder_, std::move(config));
-  }
-
-  InterceptionChainBuilder& builder_;
-};
-
-}  // namespace
-
 ExtProcFilter::ExtProcFilter(const ChannelArgs& args,
-                                 RefCountedPtr<const Config> config,
-                                 ChannelFilter::Args filter_args)
-    : config_(std::move(config)) {
-  // Populate filter_chain_map_ from config_.
-  config_->matcher->ForEachAction([&](const XdsMatcher::Action& action) {
-    if (action.type() != ExecuteFilterAction::Type()) return;
-    const auto& execute_filter_action =
-        DownCast<const ExecuteFilterAction&>(action);
-    InterceptionChainBuilder builder(args, filter_args.blackboard());
-    InterceptionChainBuilderWrapper builder_wrapper(builder);
-    for (const auto& [filter_impl, filter_config] :
-         execute_filter_action.filter_chain()) {
-      filter_impl->AddFilter(builder_wrapper, filter_config);
-    }
-    filter_chain_map_[&execute_filter_action] =
-        builder.Build(wrapped_destination());
-  });
-}
+                             RefCountedPtr<const Config> config,
+                             ChannelFilter::Args filter_args)
+    : config_(std::move(config)) {}
 
 void ExtProcFilter::InterceptCall(
     UnstartedCallHandler unstarted_call_handler) {
   // Consume the call coming to us from the client side.
   CallHandler handler = Consume(std::move(unstarted_call_handler));
-  handler.SpawnGuarded(
-      "choose_filter_chain",
-      [self = RefAsSubclass<ExtProcFilter>(), handler]() mutable {
-        return TrySeq(
-            handler.PullClientInitialMetadata(),
-            [handler, self](ClientMetadataHandle metadata) {
-              // Use the matcher to find an action to use for this call.
-              XdsMatcher::Result actions;
-              if (!self->config_->matcher->FindMatches(
-                      RpcMatchContext(metadata.get()), actions)) {
-                return absl::UnavailableError(
-                    "no match found in ext_proc filter");
-              }
-              if (actions.size() != 1) {
-                return absl::InternalError(
-                    "ext_proc filter: matcher succeeded but did "
-                    "not return actions");
-              }
-              auto& action = actions.front();
-              // If the action is SkipFilter, then we forward the
-              // call to the next filter without sending it
-              // through any child filter chain.
-              if (action->type() == SkipFilterAction::Type()) {
-                GRPC_TRACE_LOG(channel, INFO)
-                    << "[ext_proc " << self.get()
-                    << "]: found SkipFilter, starting child call";
-                CallInitiator initiator = self->MakeChildCall(
-                    std::move(metadata), GetContext<Arena>()->Ref());
-                ForwardCall(handler, initiator);
-                return absl::OkStatus();
-              }
-              // If it's not SkipFilter, it must be ExecuteFilterAction.
-              if (action->type() != ExecuteFilterAction::Type()) {
-                return absl::InternalError(
-                    "ext_proc filter encountered unknown action type");
-              }
-              const auto& execute_filter_action =
-                  DownCast<const ExecuteFilterAction&>(*action);
-              // Determine if we're sampled.  If not, forward the
-              // call to the next filter without sending it
-              // through any child filter chain.
-              if (execute_filter_action.sample_per_million() < 1000000) {
-                uint32_t random_value =
-                    absl::Uniform<uint32_t>(SharedBitGen(), 0, 1000000);
-                bool sampled =
-                    random_value < execute_filter_action.sample_per_million();
-                if (!sampled) {
-                  GRPC_TRACE_LOG(channel, INFO)
-                      << "[ext_proc " << self.get()
-                      << "]: not sampled, starting child call";
-                  CallInitiator initiator = self->MakeChildCall(
-                      std::move(metadata), GetContext<Arena>()->Ref());
-                  ForwardCall(handler, initiator);
-                  return absl::OkStatus();
-                }
-              }
-              // Find interception chain to use.
-              auto it = self->filter_chain_map_.find(&execute_filter_action);
-              if (it == self->filter_chain_map_.end()) {
-                return absl::InternalError("no filter chain found for action");
-              }
-              GRPC_TRACE_LOG(channel, INFO)
-                  << "[ext_proc " << self.get()
-                  << "]: starting call on filter chain";
-              auto& unstarted_destination = it->second;
-              if (!unstarted_destination.ok()) {
-                return unstarted_destination.status();
-              }
-              auto [initiator, unstarted_handler] =
-                  MakeCallPair(std::move(metadata), GetContext<Arena>()->Ref());
-              (*unstarted_destination)->StartCall(std::move(unstarted_handler));
-              ForwardCall(handler, initiator);
-              return absl::OkStatus();
-            });
-      });
+  // FIXME: implement
 }
 
 }  // namespace grpc_core
