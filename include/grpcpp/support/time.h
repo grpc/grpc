26d5ff66d5e88552c3fdccb89fd9a172ad2950c1
Vignesh Babu <vigneshbabu@google.com>
2026-03-07 08:47:05 -0800

    [export] Create an absl::Time specialization for grpc::TimePoint
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/880139240](http://cl/880139240)
    
    PiperOrigin-RevId: 880139240

--- include/grpcpp/support/time.h
+++ include/grpcpp/support/time.h
@@ -19,10 +19,14 @@
 #ifndef GRPCPP_SUPPORT_TIME_H
 #define GRPCPP_SUPPORT_TIME_H
 
+#include <grpc/impl/codegen/gpr_types.h>
 #include <grpc/impl/grpc_types.h>
 #include <grpcpp/support/config.h>
 
 #include <chrono>
+
+#include "absl/time/clock.h"
+#include "absl/time/time.h"
 
 namespace grpc {
 
@@ -58,6 +62,33 @@ class TimePoint<gpr_timespec> {
   gpr_timespec time_;
 };
 
+template <>
+class TimePoint<absl::Time> {
+ public:
+  explicit TimePoint(absl::Time time) : time_(TimeToGprTimespec(time)) {}
+
+  gpr_timespec raw_time() const { return time_; }
+
+ private:
+  static gpr_timespec TimeToGprTimespec(absl::Time time) {
+    if (time == absl::InfiniteFuture()) {
+      return gpr_inf_future(GPR_CLOCK_REALTIME);
+    }
+    if (time == absl::InfinitePast()) {
+      return gpr_inf_past(GPR_CLOCK_REALTIME);
+    }
+
+    gpr_timespec spec;
+    timespec t = absl::ToTimespec(time);
+    spec.tv_sec = t.tv_sec;
+    spec.tv_nsec = static_cast<int32_t>(t.tv_nsec);
+    spec.clock_type = GPR_CLOCK_REALTIME;
+    return spec;
+  }
+
+  const gpr_timespec time_;
+};
+
 }  // namespace grpc
 
 namespace grpc {
@@ -83,6 +114,37 @@ class TimePoint<std::chrono::system_clock::time_point> {
  private:
   gpr_timespec time_;
 };
+
+// Converts gpr timespec to absl::Time
+absl::Time TimeFromGprTimespec(gpr_timespec time);
+
+// Converts absl::Time to gpr timespec with clock type set to REALTIME
+gpr_timespec GprTimeSpecFromTime(absl::Time);
+
+// Converts gpr timespec to absl::Duration. The gpr timespec clock type
+// should be TIMESPAN.
+absl::Duration DurationFromGprTimespec(gpr_timespec time);
+
+// Converts absl::Duration to gpr timespec with clock type set to TIMESPAM
+gpr_timespec GprTimeSpecFromDuration(absl::Duration);
+
+// Creates an absolute-time deadline from now + dur.
+inline gpr_timespec DeadlineFromDuration(absl::Duration dur) {
+  if (absl::time_internal::IsInfiniteDuration(dur)) {
+    if (dur > absl::ZeroDuration()) {
+      return gpr_inf_future(GPR_CLOCK_MONOTONIC);
+    } else {
+      return gpr_inf_past(GPR_CLOCK_MONOTONIC);
+    }
+  }
+
+  timespec t = absl::ToTimespec(dur);
+  gpr_timespec span;
+  span.tv_sec = t.tv_sec;
+  span.tv_nsec = static_cast<int32_t>(t.tv_nsec);
+  span.clock_type = GPR_TIMESPAN;
+  return gpr_time_add(gpr_now(GPR_CLOCK_MONOTONIC), span);
+}
 
 }  // namespace grpc
 
