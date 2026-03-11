4116c9d7c9f9e519363da073277ece1c860a2756
Mark D. Roth <roth@google.com>
2026-03-11 16:49:16 +0000

    bump to 1.80.0-pre1

--- Makefile
+++ Makefile
@@ -368,7 +368,7 @@ Q = @
 endif
 
 CORE_VERSION = 53.0.0
-CPP_VERSION = 1.79.0-pre1
+CPP_VERSION = 1.80.0-pre1
 
 CPPFLAGS_NO_ARCH += $(addprefix -I, $(INCLUDES)) $(addprefix -D, $(DEFINES))
 CPPFLAGS += $(CPPFLAGS_NO_ARCH) $(ARCH_FLAGS)
--- src/objective-c/tests/version.h
+++ src/objective-c/tests/version.h
@@ -22,5 +22,5 @@
 // instead. This file can be regenerated from the template by running
 // `tools/buildgen/generate_projects.sh`.
 
-#define GRPC_OBJC_VERSION_STRING @"1.79.0-pre1"
+#define GRPC_OBJC_VERSION_STRING @"1.80.0-pre1"
 #define GRPC_C_VERSION_STRING @"53.0.0"
