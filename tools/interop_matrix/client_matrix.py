000ac702226d3e4cd8856b8fb0cce03aa53e18bd
MV Shiva Prasad <okshiva@google.com>
2026-03-17 15:12:02 +0000

    [interop] Add grpc-java 1.80.0 to client_matrix.py

--- tools/interop_matrix/client_matrix.py
+++ tools/interop_matrix/client_matrix.py
@@ -487,6 +487,7 @@ LANG_RELEASE_MATRIX = {
             ("v1.77.0", ReleaseInfo()),
             ("v1.78.0", ReleaseInfo()),
             ("v1.79.0", ReleaseInfo()),
+            ("v1.80.0", ReleaseInfo()),
         ]
     ),
     "python": OrderedDict(
