f4c2f0048457941f45f4af70e05cfa67ea3efde3
Ashesh Vidyut <asheshvidyut@google.com>
2026-03-17 11:47:05 +0000

    fix unused import

--- src/python/grpcio/grpc/aio/_metadata.py
+++ src/python/grpcio/grpc/aio/_metadata.py
@@ -16,13 +16,9 @@ from __future__ import annotations
 
 from collections import OrderedDict
 from collections import abc
-from collections.abc import Collection
-from collections.abc import ItemsView
 from collections.abc import Iterable
 from collections.abc import Iterator
-from collections.abc import KeysView
 from collections.abc import Sequence
-from collections.abc import ValuesView
 from typing import Any, List, Optional, Tuple, Union
 
 from typing_extensions import Self
