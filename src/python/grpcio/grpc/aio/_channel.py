f25c160b187c613abb84658b83501d1ea4f0ca3c
Ashesh Vidyut <asheshvidyut@google.com>
2026-03-20 18:01:26 +0530

    add comment

--- src/python/grpcio/grpc/aio/_channel.py
+++ src/python/grpcio/grpc/aio/_channel.py
@@ -400,8 +400,10 @@ class Channel(_base_channel.Channel):
         if grace is not None and grace > 0:
             tasks_to_wait = []
             for call in calls:
+                # Unary response calls (UnaryUnary, StreamUnary) use _call_response
                 if hasattr(call, "_call_response"):
                     tasks_to_wait.append(call._call_response)
+                # Stream response calls (UnaryStream, StreamStream) use _preparation
                 elif hasattr(call, "_preparation"):
                     tasks_to_wait.append(call._preparation)
 
