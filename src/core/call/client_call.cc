grpc/grpc

commit ae915ea843f4939eae0809b3f1356d6b8388b911

[grpc_error] add experiment to flatten errors (#38874)
This experiment introduces the following changes:
- There are no more hierarchical errors; when calling `StatusAddChild()`, we now instead merge the two errors into a single `absl::Status`.  The rules for this merging are:
  - Code: If the parent's code is UNKNOWN, we use the child's code; otherwise, we use the parent's code.
  - Message: We append the child's message to the parent's.
  - Payload: We copy all payload keys from the parent.  Then we copy all payload keys from the child that aren't already present in the parent.
- The `kRpcStatus` and `kGrpcMessage` properties are no longer encoded in the payload; instead, they are reflected in the top-level code and message of the `absl::Status`.
  - Note: Because `absl::Status` does not retain the message if the status is OK, this means that gRPC will no longer retain the message sent from the server if the status is OK.  This is a behavior change, but it is consistent with our long-term desire to replace `grpc::Status` with `absl::Status`.  It is also consistent with behavior of grpc-go.  (grpc-java does retain the message in this case but exposes it only to the interceptor API, not the application API.)
- This also simplifies the logic in `grpc_error_get_status()` for determining the status.  The only special case left there is the mechanism for computing the status code from the HTTP2 error and vice-versa: when returning the status code, we convert from the HTTP2 error only if the status code is UNKNOWN, and when returning the HTTP2 error, we convert from the status code only if it is something other than UNKNOWN.  We can work toward removing that last special case in a subsequent effort.
- The legacy `grpc_error` APIs in src/core/lib/iomgr/error.h are now simple wrappers around the newer APIs in src/core/util/status_helpers.h.  There are no more special behaviors in the wrappers, so the behavior is the same, no matter which API a caller uses.

src/core/call/client_call.cc
@@ -404,12 +404,14 @@ void ClientCall::OnReceivedStatus(ServerMetadataHandle server_trailing_metadata,
   const auto status = server_trailing_metadata->get(GrpcStatusMetadata())
                           .value_or(GRPC_STATUS_UNKNOWN);
   *out_status = status;
-  Slice message_slice;
-  if (Slice* message =
-          server_trailing_metadata->get_pointer(GrpcMessageMetadata())) {
-    message_slice = message->Ref();
+  if (!IsErrorFlattenEnabled() || status != GRPC_STATUS_OK) {
+    Slice message_slice;
+    if (Slice* message =
+            server_trailing_metadata->get_pointer(GrpcMessageMetadata())) {
+      message_slice = message->Ref();
+    }
+    *out_status_details = message_slice.TakeCSlice();
   }
-  *out_status_details = message_slice.TakeCSlice();
   if (out_error_string != nullptr) {
     if (status != GRPC_STATUS_OK) {
       *out_error_string =

CMakeLists.txt
@@ -9619,6 +9619,7 @@ add_executable(cancel_callback_test
   src/core/lib/slice/percent_encoding.cc
   src/core/lib/slice/slice.cc
   src/core/lib/slice/slice_string_helpers.cc
+  src/core/lib/transport/status_conversion.cc
   src/core/util/dump_args.cc
   src/core/util/glob.cc
   src/core/util/latent_see.cc
@@ -10789,6 +10790,7 @@ add_executable(chunked_vector_test
   src/core/lib/slice/percent_encoding.cc
   src/core/lib/slice/slice.cc
   src/core/lib/slice/slice_string_helpers.cc
+  src/core/lib/transport/status_conversion.cc
   src/core/util/dump_args.cc
   src/core/util/glob.cc
   src/core/util/latent_see.cc
@@ -15008,6 +15010,7 @@ add_executable(exec_ctx_wakeup_scheduler_test
   src/core/lib/slice/percent_encoding.cc
   src/core/lib/slice/slice.cc
   src/core/lib/slice/slice_string_helpers.cc
+  src/core/lib/transport/status_conversion.cc
   src/core/util/dump_args.cc
   src/core/util/glob.cc
   src/core/util/latent_see.cc
@@ -15815,6 +15818,7 @@ add_executable(flow_control_test
   src/core/lib/slice/slice_buffer.cc
   src/core/lib/slice/slice_string_helpers.cc
   src/core/lib/transport/bdp_estimator.cc
+  src/core/lib/transport/status_conversion.cc
   src/core/util/dump_args.cc
   src/core/util/glob.cc
   src/core/util/latent_see.cc
@@ -15892,6 +15896,7 @@ add_executable(for_each_test
   src/core/lib/slice/percent_encoding.cc
   src/core/lib/slice/slice.cc
   src/core/lib/slice/slice_string_helpers.cc
+  src/core/lib/transport/status_conversion.cc
   src/core/util/dump_args.cc
   src/core/util/glob.cc
   src/core/util/latent_see.cc
@@ -19526,6 +19531,7 @@ add_executable(interceptor_list_test
   src/core/lib/slice/percent_encoding.cc
   src/core/lib/slice/slice.cc
   src/core/lib/slice/slice_string_helpers.cc
+  src/core/lib/transport/status_conversion.cc
   src/core/util/dump_args.cc
   src/core/util/glob.cc
   src/core/util/latent_see.cc
@@ -20570,6 +20576,7 @@ add_executable(map_pipe_test
   src/core/lib/slice/percent_encoding.cc
   src/core/lib/slice/slice.cc
   src/core/lib/slice/slice_string_helpers.cc
+  src/core/lib/transport/status_conversion.cc
   src/core/util/dump_args.cc
   src/core/util/glob.cc
   src/core/util/latent_see.cc
@@ -22600,6 +22607,7 @@ add_executable(periodic_update_test
   src/core/lib/slice/percent_encoding.cc
   src/core/lib/slice/slice.cc
   src/core/lib/slice/slice_string_helpers.cc
+  src/core/lib/transport/status_conversion.cc
   src/core/util/glob.cc
   src/core/util/latent_see.cc
   src/core/util/per_cpu.cc

bazel/experiments.bzl
@@ -22,6 +22,7 @@ EXPERIMENT_ENABLES = {
     "call_tracer_transport_fix": "call_tracer_transport_fix",
     "callv3_client_auth_filter": "callv3_client_auth_filter",
     "chaotic_good_framing_layer": "chaotic_good_framing_layer",
+    "error_flatten": "error_flatten",
     "event_engine_client": "event_engine_client",
     "event_engine_dns": "event_engine_dns",
     "event_engine_dns_non_client_channel": "event_engine_dns_non_client_channel",
@@ -66,10 +67,14 @@ EXPERIMENTS = {
             "core_end2end_test": [
                 "callv3_client_auth_filter",
                 "chaotic_good_framing_layer",
+                "error_flatten",
                 "event_engine_dns_non_client_channel",
                 "event_engine_fork",
                 "local_connector_secure",
                 "retry_in_callv3",
+            ],
+            "cpp_end2end_test": [
+                "error_flatten",
             ],
             "endpoint_test": [
                 "tcp_frame_size_tuning",
@@ -86,6 +91,9 @@ EXPERIMENTS = {
             "resource_quota_test": [
                 "free_large_allocator",
                 "unconstrained_max_quota_buffer_size",
+            ],
+            "xds_end2end_test": [
+                "error_flatten",
             ],
         },
         "on": {
@@ -123,10 +131,14 @@ EXPERIMENTS = {
             "core_end2end_test": [
                 "callv3_client_auth_filter",
                 "chaotic_good_framing_layer",
+                "error_flatten",
                 "event_engine_dns_non_client_channel",
                 "event_engine_fork",
                 "local_connector_secure",
                 "retry_in_callv3",
+            ],
+            "cpp_end2end_test": [
+                "error_flatten",
             ],
             "endpoint_test": [
                 "tcp_frame_size_tuning",
@@ -143,6 +155,9 @@ EXPERIMENTS = {
             "resource_quota_test": [
                 "free_large_allocator",
                 "unconstrained_max_quota_buffer_size",
+            ],
+            "xds_end2end_test": [
+                "error_flatten",
             ],
         },
         "on": {
@@ -166,10 +181,14 @@ EXPERIMENTS = {
             "core_end2end_test": [
                 "callv3_client_auth_filter",
                 "chaotic_good_framing_layer",
+                "error_flatten",
                 "event_engine_dns_non_client_channel",
                 "event_engine_fork",
                 "local_connector_secure",
                 "retry_in_callv3",
+            ],
+            "cpp_end2end_test": [
+                "error_flatten",
             ],
             "endpoint_test": [
                 "tcp_frame_size_tuning",
@@ -186,6 +205,9 @@ EXPERIMENTS = {
             "resource_quota_test": [
                 "free_large_allocator",
                 "unconstrained_max_quota_buffer_size",
+            ],
+            "xds_end2end_test": [
+                "error_flatten",
             ],
         },
         "on": {

build_autogenerated.yaml
@@ -7038,6 +7038,8 @@ targets:
   - src/core/lib/slice/slice_internal.h
   - src/core/lib/slice/slice_refcount.h
   - src/core/lib/slice/slice_string_helpers.h
+  - src/core/lib/transport/http2_errors.h
+  - src/core/lib/transport/status_conversion.h
   - src/core/util/atomic_utils.h
   - src/core/util/bitset.h
   - src/core/util/cpp_impl_of.h
@@ -7077,6 +7079,7 @@ targets:
   - src/core/lib/slice/percent_encoding.cc
   - src/core/lib/slice/slice.cc
   - src/core/lib/slice/slice_string_helpers.cc
+  - src/core/lib/transport/status_conversion.cc
   - src/core/util/dump_args.cc
   - src/core/util/glob.cc
   - src/core/util/latent_see.cc
@@ -7822,6 +7825,8 @@ targets:
   - src/core/lib/slice/slice_internal.h
   - src/core/lib/slice/slice_refcount.h
   - src/core/lib/slice/slice_string_helpers.h
+  - src/core/lib/transport/http2_errors.h
+  - src/core/lib/transport/status_conversion.h
   - src/core/util/atomic_utils.h
   - src/core/util/bitset.h
   - src/core/util/chunked_vector.h
@@ -7862,6 +7867,7 @@ targets:
   - src/core/lib/slice/percent_encoding.cc
   - src/core/lib/slice/slice.cc
   - src/core/lib/slice/slice_string_helpers.cc
+  - src/core/lib/transport/status_conversion.cc
   - src/core/util/dump_args.cc
   - src/core/util/glob.cc
   - src/core/util/latent_see.cc
@@ -10383,6 +10389,8 @@ targets:
   - src/core/lib/slice/slice_internal.h
   - src/core/lib/slice/slice_refcount.h
   - src/core/lib/slice/slice_string_helpers.h
+  - src/core/lib/transport/http2_errors.h
+  - src/core/lib/transport/status_conversion.h
   - src/core/util/atomic_utils.h
   - src/core/util/bitset.h
   - src/core/util/down_cast.h
@@ -10416,6 +10424,7 @@ targets:
   - src/core/lib/slice/percent_encoding.cc
   - src/core/lib/slice/slice.cc
   - src/core/lib/slice/slice_string_helpers.cc
+  - src/core/lib/transport/status_conversion.cc
   - src/core/util/dump_args.cc
   - src/core/util/glob.cc
   - src/core/util/latent_see.cc
@@ -11208,6 +11217,7 @@ targets:
   - src/core/lib/slice/slice_string_helpers.h
   - src/core/lib/transport/bdp_estimator.h
   - src/core/lib/transport/http2_errors.h
+  - src/core/lib/transport/status_conversion.h
   - src/core/util/atomic_utils.h
   - src/core/util/bitset.h
   - src/core/util/cpp_impl_of.h
@@ -11251,6 +11261,7 @@ targets:
   - src/core/lib/slice/slice_buffer.cc
   - src/core/lib/slice/slice_string_helpers.cc
   - src/core/lib/transport/bdp_estimator.cc
+  - src/core/lib/transport/status_conversion.cc
   - src/core/util/dump_args.cc
   - src/core/util/glob.cc
   - src/core/util/latent_see.cc
@@ -11323,6 +11334,8 @@ targets:
   - src/core/lib/slice/slice_internal.h
   - src/core/lib/slice/slice_refcount.h
   - src/core/lib/slice/slice_string_helpers.h
+  - src/core/lib/transport/http2_errors.h
+  - src/core/lib/transport/status_conversion.h
   - src/core/util/atomic_utils.h
   - src/core/util/bitset.h
   - src/core/util/cpp_impl_of.h
@@ -11363,6 +11376,7 @@ targets:
   - src/core/lib/slice/percent_encoding.cc
   - src/core/lib/slice/slice.cc
   - src/core/lib/slice/slice_string_helpers.cc
+  - src/core/lib/transport/status_conversion.cc
   - src/core/util/dump_args.cc
   - src/core/util/glob.cc
   - src/core/util/latent_see.cc
@@ -12898,6 +12912,8 @@ targets:
   - src/core/lib/slice/slice_internal.h
   - src/core/lib/slice/slice_refcount.h
   - src/core/lib/slice/slice_string_helpers.h
+  - src/core/lib/transport/http2_errors.h
+  - src/core/lib/transport/status_conversion.h
   - src/core/util/atomic_utils.h
   - src/core/util/bitset.h
   - src/core/util/cpp_impl_of.h
@@ -12938,6 +12954,7 @@ targets:
   - src/core/lib/slice/percent_encoding.cc
   - src/core/lib/slice/slice.cc
   - src/core/lib/slice/slice_string_helpers.cc
+  - src/core/lib/transport/status_conversion.cc
   - src/core/util/dump_args.cc
   - src/core/util/glob.cc
   - src/core/util/latent_see.cc
@@ -13411,6 +13428,8 @@ targets:
   - src/core/lib/slice/slice_internal.h
   - src/core/lib/slice/slice_refcount.h
   - src/core/lib/slice/slice_string_helpers.h
+  - src/core/lib/transport/http2_errors.h
+  - src/core/lib/transport/status_conversion.h
   - src/core/util/atomic_utils.h
   - src/core/util/bitset.h
   - src/core/util/cpp_impl_of.h
@@ -13451,6 +13470,7 @@ targets:
   - src/core/lib/slice/percent_encoding.cc
   - src/core/lib/slice/slice.cc
   - src/core/lib/slice/slice_string_helpers.cc
+  - src/core/lib/transport/status_conversion.cc
   - src/core/util/dump_args.cc
   - src/core/util/glob.cc
   - src/core/util/latent_see.cc
@@ -14226,6 +14246,8 @@ targets:
   - src/core/lib/slice/slice_internal.h
   - src/core/lib/slice/slice_refcount.h
   - src/core/lib/slice/slice_string_helpers.h
+  - src/core/lib/transport/http2_errors.h
+  - src/core/lib/transport/status_conversion.h
   - src/core/util/bitset.h
   - src/core/util/glob.h
   - src/core/util/latent_see.h
@@ -14253,6 +14275,7 @@ targets:
   - src/core/lib/slice/percent_encoding.cc
   - src/core/lib/slice/slice.cc
   - src/core/lib/slice/slice_string_helpers.cc
+  - src/core/lib/transport/status_conversion.cc
   - src/core/util/glob.cc
   - src/core/util/latent_see.cc
   - src/core/util/per_cpu.cc

src/core/BUILD
@@ -519,11 +519,15 @@ grpc_cc_library(
         "absl/time",
     ],
     deps = [
+        "experiments",
         "percent_encoding",
         "slice",
+        "status_conversion",
+        "time",
         "//:debug_location",
         "//:google_rpc_status_upb",
         "//:gpr",
+        "//:grpc_public_hdrs",
         "//:protobuf_any_upb",
     ],
 )
@@ -1763,6 +1767,7 @@ grpc_cc_library(
     ],
     visibility = ["@grpc:alt_grpc_base_legacy"],
     deps = [
+        "experiments",
         "gpr_spinlock",
         "slice",
         "slice_refcount",
@@ -6495,6 +6500,7 @@ grpc_cc_library(
     external_deps = ["absl/status"],
     deps = [
         "error",
+        "experiments",
         "http2_errors",
         "status_conversion",
         "status_helper",

src/core/lib/experiments/experiments.cc
@@ -38,6 +38,9 @@ const char* const additional_constraints_callv3_client_auth_filter = "{}";
 const char* const description_chaotic_good_framing_layer =
     "Enable the chaotic good framing layer.";
 const char* const additional_constraints_chaotic_good_framing_layer = "{}";
+const char* const description_error_flatten =
+    "Flatten errors to ordinary absl::Status form.";
+const char* const additional_constraints_error_flatten = "{}";
 const char* const description_event_engine_client =
     "Use EventEngine clients instead of iomgr's grpc_tcp_client";
 const char* const additional_constraints_event_engine_client = "{}";
@@ -160,6 +163,8 @@ const ExperimentMetadata g_experiment_metadata[] = {
     {"chaotic_good_framing_layer", description_chaotic_good_framing_layer,
      additional_constraints_chaotic_good_framing_layer, nullptr, 0, false,
      true},
+    {"error_flatten", description_error_flatten,
+     additional_constraints_error_flatten, nullptr, 0, false, false},
     {"event_engine_client", description_event_engine_client,
      additional_constraints_event_engine_client, nullptr, 0, false, false},
     {"event_engine_dns", description_event_engine_dns,
@@ -247,6 +252,9 @@ const char* const additional_constraints_callv3_client_auth_filter = "{}";
 const char* const description_chaotic_good_framing_layer =
     "Enable the chaotic good framing layer.";
 const char* const additional_constraints_chaotic_good_framing_layer = "{}";
+const char* const description_error_flatten =
+    "Flatten errors to ordinary absl::Status form.";
+const char* const additional_constraints_error_flatten = "{}";
 const char* const description_event_engine_client =
     "Use EventEngine clients instead of iomgr's grpc_tcp_client";
 const char* const additional_constraints_event_engine_client = "{}";
@@ -369,6 +377,8 @@ const ExperimentMetadata g_experiment_metadata[] = {
     {"chaotic_good_framing_layer", description_chaotic_good_framing_layer,
      additional_constraints_chaotic_good_framing_layer, nullptr, 0, false,
      true},
+    {"error_flatten", description_error_flatten,
+     additional_constraints_error_flatten, nullptr, 0, false, false},
     {"event_engine_client", description_event_engine_client,
      additional_constraints_event_engine_client, nullptr, 0, true, false},
     {"event_engine_dns", description_event_engine_dns,
@@ -456,6 +466,9 @@ const char* const additional_constraints_callv3_client_auth_filter = "{}";
 const char* const description_chaotic_good_framing_layer =
     "Enable the chaotic good framing layer.";
 const char* const additional_constraints_chaotic_good_framing_layer = "{}";
+const char* const description_error_flatten =
+    "Flatten errors to ordinary absl::Status form.";
+const char* const additional_constraints_error_flatten = "{}";
 const char* const description_event_engine_client =
     "Use EventEngine clients instead of iomgr's grpc_tcp_client";
 const char* const additional_constraints_event_engine_client = "{}";
@@ -578,6 +591,8 @@ const ExperimentMetadata g_experiment_metadata[] = {
     {"chaotic_good_framing_layer", description_chaotic_good_framing_layer,
      additional_constraints_chaotic_good_framing_layer, nullptr, 0, false,
      true},
+    {"error_flatten", description_error_flatten,
+     additional_constraints_error_flatten, nullptr, 0, false, false},
     {"event_engine_client", description_event_engine_client,
      additional_constraints_event_engine_client, nullptr, 0, true, false},
     {"event_engine_dns", description_event_engine_dns,

src/core/lib/experiments/experiments.h
@@ -65,6 +65,7 @@ inline bool IsCallTracerInTransportEnabled() { return true; }
 inline bool IsCallTracerTransportFixEnabled() { return true; }
 inline bool IsCallv3ClientAuthFilterEnabled() { return false; }
 inline bool IsChaoticGoodFramingLayerEnabled() { return false; }
+inline bool IsErrorFlattenEnabled() { return false; }
 inline bool IsEventEngineClientEnabled() { return false; }
 inline bool IsEventEngineDnsEnabled() { return false; }
 inline bool IsEventEngineDnsNonClientChannelEnabled() { return false; }
@@ -107,6 +108,7 @@ inline bool IsCallTracerInTransportEnabled() { return true; }
 inline bool IsCallTracerTransportFixEnabled() { return true; }
 inline bool IsCallv3ClientAuthFilterEnabled() { return false; }
 inline bool IsChaoticGoodFramingLayerEnabled() { return false; }
+inline bool IsErrorFlattenEnabled() { return false; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_CLIENT
 inline bool IsEventEngineClientEnabled() { return true; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_DNS
@@ -152,6 +154,7 @@ inline bool IsCallTracerInTransportEnabled() { return true; }
 inline bool IsCallTracerTransportFixEnabled() { return true; }
 inline bool IsCallv3ClientAuthFilterEnabled() { return false; }
 inline bool IsChaoticGoodFramingLayerEnabled() { return false; }
+inline bool IsErrorFlattenEnabled() { return false; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_CLIENT
 inline bool IsEventEngineClientEnabled() { return true; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_DNS
@@ -196,6 +199,7 @@ enum ExperimentIds {
   kExperimentIdCallTracerTransportFix,
   kExperimentIdCallv3ClientAuthFilter,
   kExperimentIdChaoticGoodFramingLayer,
+  kExperimentIdErrorFlatten,
   kExperimentIdEventEngineClient,
   kExperimentIdEventEngineDns,
   kExperimentIdEventEngineDnsNonClientChannel,
@@ -242,6 +246,10 @@ inline bool IsCallv3ClientAuthFilterEnabled() {
 #define GRPC_EXPERIMENT_IS_INCLUDED_CHAOTIC_GOOD_FRAMING_LAYER
 inline bool IsChaoticGoodFramingLayerEnabled() {
   return IsExperimentEnabled<kExperimentIdChaoticGoodFramingLayer>();
+}
+#define GRPC_EXPERIMENT_IS_INCLUDED_ERROR_FLATTEN
+inline bool IsErrorFlattenEnabled() {
+  return IsExperimentEnabled<kExperimentIdErrorFlatten>();
 }
 #define GRPC_EXPERIMENT_IS_INCLUDED_EVENT_ENGINE_CLIENT
 inline bool IsEventEngineClientEnabled() {

src/core/lib/experiments/experiments.yaml
@@ -66,6 +66,12 @@
   expiry: 2025/06/01
   owner: ctiller@google.com
   test_tags: [core_end2end_test]
+- name: error_flatten
+  description: Flatten errors to ordinary absl::Status form.
+  expiry: 2025/06/01
+  owner: roth@google.com
+  test_tags: ["core_end2end_test", "cpp_end2end_test", "xds_end2end_test"]
+  allow_in_fuzzing_config: false
 - name: event_engine_callback_cq
   description: Use EventEngine instead of the CallbackAlternativeCQ.
   expiry: 2025/05/01

commit 477942a76e3036f40a51462127a536923d49e19a

[error_flatten] set status details to empty slice for OK status in v3 stack (#41940)
It looks like this bug was introduced back in #38874 when I originally implemented the error_flatten changes.  I'm honestly surprised that nothing has caught this until now.

Closes #41940

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/41940 from markdroth:error_flatten_v3_fix adaa7b081aee895f006d3d676e5b4f94c3cd4ad9
PiperOrigin-RevId: 890647719

src/core/call/client_call.cc
@@ -442,14 +442,14 @@ void ClientCall::OnReceivedStatus(ServerMetadataHandle server_trailing_metadata,
   const auto status = server_trailing_metadata->get(GrpcStatusMetadata())
                           .value_or(GRPC_STATUS_UNKNOWN);
   *out_status = status;
+  Slice message_slice;
   if (!IsErrorFlattenEnabled() || status != GRPC_STATUS_OK) {
-    Slice message_slice;
     if (Slice* message =
             server_trailing_metadata->get_pointer(GrpcMessageMetadata())) {
       message_slice = message->Ref();
     }
-    *out_status_details = message_slice.TakeCSlice();
   }
+  *out_status_details = message_slice.TakeCSlice();
   if (out_error_string != nullptr) {
     if (status != GRPC_STATUS_OK) {
       *out_error_string =