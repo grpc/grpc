06712d68e22103e8cc089d78d8616c0800b8a9e3
Ashesh Vidyut <asheshvidyut@google.com>
2026-04-02 14:07:26 +0530

    fix issue 38538

--- tools/bazelify_tests/test/build_artifact_protoc_linux_aarch64.sh
+++ tools/bazelify_tests/test/build_artifact_protoc_linux_aarch64.sh
@@ -16,7 +16,10 @@
 set -ex
 
 # compile/link options extracted from ProtocArtifact in tools/run_tests/artifacts/artifact_targets.py
-export LDFLAGS="${LDFLAGS} -static-libgcc -static-libstdc++ -s"
+# NOTE: -Wl,-z,max-page-size=65536 is required because ARM64 Linux kernels may
+# use page sizes up to 64KB. Without this flag the protoc binary may segfault
+# (exit code 139) on such systems. See https://github.com/grpc/grpc/issues/38538.
+export LDFLAGS="${LDFLAGS} -static-libgcc -static-libstdc++ -Wl,-z,max-page-size=65536 -s"
 # set build parallelism to fit the machine configuration of bazelified tests RBE pool.
 export GRPC_PROTOC_BUILD_COMPILER_JOBS=8
 
