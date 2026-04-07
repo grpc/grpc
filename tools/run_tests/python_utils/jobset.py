7366afd4b1cbfae1d16efb9b25e7d55de1af1487
Michael Lumish <mlumish@google.com>
2026-04-07 14:21:30 -0700

    Update tools/run_tests/python_utils/jobset.py
    Co-authored-by: Sergii Tkachenko <sergiitk@google.com>

--- tools/run_tests/python_utils/jobset.py
+++ tools/run_tests/python_utils/jobset.py
@@ -309,7 +309,7 @@ class Job:
             )
             if not os.path.exists(logfile_dir):
                 os.makedirs(logfile_dir)
-            message("LOG", "Logging output to %s" % (self._spec.logfilename))
+            message("LOG", f"Logging output to {self._spec.logfilename}")
             self._logfile = open(self._spec.logfilename, "w+")
         else:
             # macOS: a series of quick os.unlink invocation might cause OS
