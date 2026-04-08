40886ff499c51f3fecf9ef0f458a417d816b68e4
Pawan Bhardwaj <bpawan@google.com>
2026-04-08 14:29:47 +0000

    fix

--- tools/internal_ci/linux/grpc_android_in_docker.sh
+++ tools/internal_ci/linux/grpc_android_in_docker.sh
@@ -47,7 +47,7 @@ gcloud firebase test android run \
     --app app/build/outputs/apk/debug/app-debug.apk \
     --test app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
     --device model=panther,version=33,locale=en,orientation=portrait \
-    --device model=oriole,version=34,locale=en,orientation=portrait \
+    --device model=shiba,version=34,locale=en,orientation=portrait \
     --device model=shiba,version=35,locale=en,orientation=portrait
 
 # Build hello world example
