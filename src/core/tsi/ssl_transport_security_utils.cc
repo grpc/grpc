7253fecdf18f1ced2243751d4b59c8928a1746ed
Gregory Cooke <gregorycooke@google.com>
2026-04-13 18:43:19 +0000

    check right var

--- src/core/tsi/ssl_transport_security_utils.cc
+++ src/core/tsi/ssl_transport_security_utils.cc
@@ -282,7 +282,7 @@ bool VerifyCrlCertIssuerNamesMatch(X509_CRL* crl, X509* cert) {
     return false;
   }
   X509_NAME* cert_issuer_name = X509_get_issuer_name(cert);
-  if (cert == nullptr) {
+  if (cert_issuer_name == nullptr) {
     return false;
   }
   X509_NAME* crl_issuer_name = X509_CRL_get_issuer(crl);
