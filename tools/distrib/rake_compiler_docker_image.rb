267ddb42fe8156b007857cd32323723eb4d0ab46
Ashesh Vidyut <asheshvidyut@google.com>
2026-04-14 20:39:18 +0530

    fix DOCKER_DEFAULT_PLATFORM

--- tools/distrib/rake_compiler_docker_image.rb
+++ tools/distrib/rake_compiler_docker_image.rb
@@ -32,5 +32,12 @@ def run_rake_compiler(platform, args)
   ENV['RCD_RUBYVM'] = 'mri'
   ENV['RCD_PLATFORM'] = platform
   ENV['RCD_IMAGE'] = docker_image_for_rake_compiler(platform)
+  
+  if platform =~ /arm64/ || platform =~ /aarch64/
+    ENV['DOCKER_DEFAULT_PLATFORM'] = 'linux/arm64'
+  else
+    ENV.delete('DOCKER_DEFAULT_PLATFORM')
+  end
+
   RakeCompilerDock.sh args
 end
