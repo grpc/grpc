8414f87b921d44fd21139f3f296d637b9fb91aa8
Weizhe Yuan <weizheyuan@google.com>
2026-04-14 23:48:14 -0700

    job=200

--- tools/internal_ci/macos/grpc_run_bazel_c_cpp_tests.sh
+++ tools/internal_ci/macos/grpc_run_bazel_c_cpp_tests.sh
@@ -46,6 +46,7 @@ python3 tools/run_tests/python_utils/bazel_report_helper.py --report_path bazel_
 bazel_c_cpp_tests/bazel_wrapper \
   --output_base=.bazel_rbe \
   --bazelrc=tools/remote_build/mac.bazelrc \
+  --jobs=200
   test \
   --google_credentials="${KOKORO_GFILE_DIR}/GrpcTesting-d0eeee2db331.json" \
   "${BAZEL_REMOTE_CACHE_ARGS[@]}" \
