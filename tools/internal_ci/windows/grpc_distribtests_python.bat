2ca04876bfb4ef198f71d5e32b0f63aeb4aef379
Ashesh Vidyut <asheshvidyut@google.com>
2026-04-16 11:01:02 +0530

    reduce parallelism

--- tools/internal_ci/windows/grpc_distribtests_python.bat
+++ tools/internal_ci/windows/grpc_distribtests_python.bat
@@ -28,12 +28,8 @@ cd /d %~dp0\..\..\..
 set PREPARE_BUILD_INSTALL_DEPS_PYTHON=true
 call tools/internal_ci/helper_scripts/prepare_build_windows.bat || exit /b 1
 
-@rem Flood the disk I/O in the background to force the log writer to block
-if not exist artifacts mkdir artifacts
-start cmd /c "for /L %%i in (1,1,100000) do (copy /Y tools\internal_ci\windows\grpc_distribtests_python.bat artifacts\temp_%%i.bat > nul)"
-
 @rem Build all python windows artifacts
-python tools/run_tests/task_runner.py -f artifact windows python %TASK_RUNNER_EXTRA_FILTERS% -j 5 --inner_jobs 4 -x build_artifacts_python/sponge_log.xml || set FAILED=true
+python tools/run_tests/task_runner.py -f artifact windows python %TASK_RUNNER_EXTRA_FILTERS% -j 2 --inner_jobs 2 -x build_artifacts_python/sponge_log.xml || set FAILED=true
 
 @rem the next step expects to find the artifacts from the previous step in the "input_artifacts" folder.
 bash -c "rm -rf input_artifacts; mkdir -p input_artifacts; cp -r artifacts/* input_artifacts/ || true"
