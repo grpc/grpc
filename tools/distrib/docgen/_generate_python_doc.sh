392ce6dc52b813efe34240c927c70a2ecebf1ced
Ashesh Vidyut <asheshvidyut@google.com>
2026-04-16 14:01:47 +0530

    init

--- tools/distrib/docgen/_generate_python_doc.sh
+++ tools/distrib/docgen/_generate_python_doc.sh
@@ -27,7 +27,7 @@ SOURCE_DIR="doc/python/sphinx"
 TARGET_DIR="doc/build"
 
 pip install -r tools/distrib/docgen/requirements.docs.lock
-tools/run_tests/run_tests.py -c opt -l python --compiler python3.9 --newline_on_success -j 8 --build_only
+tools/run_tests/run_tests.py -c opt -l python --compiler python3.10 --newline_on_success -j 8 --build_only
 # shellcheck disable=SC1091
 source py39/bin/activate
 pip install --upgrade Sphinx
