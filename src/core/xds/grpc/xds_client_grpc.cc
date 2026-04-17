ed99425f102032167412e975e90dd38b2b65d644
Pawan Bhardwaj <bpawan@google.com>
2026-04-17 06:50:14 +0000

    Dummy xds change for testing

--- src/core/xds/grpc/xds_client_grpc.cc
+++ src/core/xds/grpc/xds_client_grpc.cc
@@ -1,6 +1,8 @@
 //
 // Copyright 2022 gRPC authors.
 //
+// Dummy xds comment for CI purposes.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
