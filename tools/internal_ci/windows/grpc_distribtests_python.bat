7f991fc69839a8e5b483444a8de1320a61d231c8
Weizhe Yuan <weizheyuan@google.com>
2026-04-17 15:49:09 -0700

    bat file change

--- tools/internal_ci/windows/grpc_distribtests_python.bat
+++ tools/internal_ci/windows/grpc_distribtests_python.bat
@@ -30,7 +30,7 @@ call tools/internal_ci/helper_scripts/prepare_build_windows.bat || exit /b 1
 
 call tools/internal_ci/helper_scripts/prepare_ccache.bat || exit /b 1
 set CCACHE_LOGFILE=T:\src\github\grpc\reports\ccache.log
-set CCACHE_IGNOREOPTIONS=/Zc:preprocessor
+set "CCACHE_IGNOREOPTIONS=/Zc:preprocessor /Tc /Tp /TC /TC"
 
 @rem Build all python windows artifacts
 python tools/run_tests/task_runner.py -f artifact windows python %TASK_RUNNER_EXTRA_FILTERS% -j 3 --inner_jobs 2 -x build_artifacts_python/sponge_log.xml || set FAILED=true
