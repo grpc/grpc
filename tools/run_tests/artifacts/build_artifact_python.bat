97ed7f188c88f7fc8fe5103f19a68732a59f9194
yuanweiz <weizheyuan@google.com>
2026-04-20 15:46:15 -0700

    [Core CI] Make windows python distrib tests use ccache. (#42171)
    Previously ccache wasn't properly enabled for our python builds before because setuptools has special logic to find MSVC and disregard `CC/CXX` flags https://github.com/pypa/setuptools/blob/main/setuptools/msvc.py. There's no graceful solution other than monkeypatching (which we already do with `_spawn_patch.py`).
    
    Certain MSVC flags are not properly handled by ccache 4.8 so we need to rewrite and exclude them from hash key generation.
    
    <!--
    
    If you know who should review your pull request, please assign it to that
    person, otherwise the pull request would get assigned randomly.
    
    If your pull request is for a specific language, please add the appropriate
    lang label.
    
    -->
    
    Closes #42171
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/42171 from yuanweiz:fixit-windows-ccache 5ff00caa9db8ea95c653a34332de5a2e215ddb37
    PiperOrigin-RevId: 902859436

--- tools/run_tests/artifacts/build_artifact_python.bat
+++ tools/run_tests/artifacts/build_artifact_python.bat
@@ -27,6 +27,8 @@ python -m pip install -r requirements.txt --user
 
 @rem set GRPC_PYTHON_OVERRIDE_CYGWIN_DETECTION_FOR_27=1
 set GRPC_PYTHON_BUILD_WITH_CYTHON=1
+set CCACHE_NOHASHDIR=true
+set CCACHE_LOGFILE=T:\src\github\grpc\reports\ccache_%1_%2.log
 
 @rem Allow build_ext to build C/C++ files in parallel
 @rem by enabling a monkeypatch. It speeds up the build a lot.
