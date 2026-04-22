49a1450e1cb8b760bb005b98f79d33ccea897193
Agyei Holy <agyeiholy978@gmail.com>
2026-04-22 02:56:40 -0500

    reflection: expose default health service descriptors

--- test/cpp/end2end/proto_server_reflection_test.cc
+++ test/cpp/end2end/proto_server_reflection_test.cc
@@ -20,6 +20,7 @@
 #include <grpcpp/channel.h>
 #include <grpcpp/client_context.h>
 #include <grpcpp/create_channel.h>
+#include <grpcpp/health_check_service_interface.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/security/credentials.h>
 #include <grpcpp/security/server_credentials.h>
@@ -29,12 +30,14 @@
 #include <grpcpp/support/channel_arguments.h>
 
 #include <memory>
+#include <mutex>
 #include <vector>
 
 #include "src/proto/grpc/reflection/v1/reflection.grpc.pb.h"
 #include "src/proto/grpc/reflection/v1/reflection.pb.h"
 #include "src/proto/grpc/reflection/v1alpha/reflection.grpc.pb.h"
 #include "src/proto/grpc/reflection/v1alpha/reflection.pb.h"
+#include "src/proto/grpc/health/v1/health.pb.h"
 #include "src/proto/grpc/testing/echo.grpc.pb.h"
 #include "test/core/test_util/port.h"
 #include "test/core/test_util/test_config.h"
@@ -45,6 +48,31 @@
 
 namespace grpc {
 namespace testing {
+namespace {
+
+class ScopedDefaultHealthCheckServiceEnable final {
+ public:
+  explicit ScopedDefaultHealthCheckServiceEnable(bool enable)
+      : lock_(GetMutex()),
+        previous_value_(grpc::DefaultHealthCheckServiceEnabled()) {
+    grpc::EnableDefaultHealthCheckService(enable);
+  }
+
+  ~ScopedDefaultHealthCheckServiceEnable() {
+    grpc::EnableDefaultHealthCheckService(previous_value_);
+  }
+
+ private:
+  static std::mutex& GetMutex() {
+    static auto* mu = new std::mutex();
+    return *mu;
+  }
+
+  std::lock_guard<std::mutex> lock_;
+  bool previous_value_;
+};
+
+}  // namespace
 
 class ProtoServerReflectionTest : public ::testing::Test {
  public:
@@ -54,6 +82,15 @@ class ProtoServerReflectionTest : public ::testing::Test {
     port_ = grpc_pick_unused_port_or_die();
     ref_desc_pool_ = protobuf::DescriptorPool::generated_pool();
 
+    StartServer();
+  }
+
+  void StartServer(bool enable_default_health_service = false) {
+    std::unique_ptr<ScopedDefaultHealthCheckServiceEnable> health_check_guard;
+    if (enable_default_health_service) {
+      health_check_guard =
+          std::make_unique<ScopedDefaultHealthCheckServiceEnable>(true);
+    }
     ServerBuilder builder;
     std::string server_address = "localhost:" + to_string(port_);
     builder.AddListeningPort(server_address, InsecureServerCredentials());
@@ -218,6 +255,43 @@ TEST_F(ProtoServerReflectionTest, V1ApiInstalled) {
   ASSERT_TRUE(reader_writer->Read(&response));
   EXPECT_EQ(response.file_descriptor_response().file_descriptor_proto_size(), 1)
       << response.DebugString();
+}
+
+TEST_F(ProtoServerReflectionTest, DefaultHealthServiceDescriptorAvailable) {
+  server_->Shutdown();
+  server_.reset();
+  port_ = grpc_pick_unused_port_or_die();
+  StartServer(true);
+  ResetStub();
+
+  using Service = reflection::v1alpha::ServerReflection;
+  using Request = reflection::v1alpha::ServerReflectionRequest;
+  using Response = reflection::v1alpha::ServerReflectionResponse;
+  Service::Stub stub(channel_);
+  ClientContext context;
+  auto reader_writer = stub.ServerReflectionInfo(&context);
+  Request request;
+  request.set_list_services("*");
+  ASSERT_TRUE(reader_writer->Write(request));
+  Response response;
+  ASSERT_TRUE(reader_writer->Read(&response));
+  EXPECT_THAT(ServicesFromResponse(response),
+              ::testing::Contains("grpc.health.v1.Health"));
+
+  request = Request::default_instance();
+  request.set_file_containing_symbol("grpc.health.v1.Health");
+  ASSERT_TRUE(reader_writer->WriteLast(request, WriteOptions()));
+  response = Response::default_instance();
+  ASSERT_TRUE(reader_writer->Read(&response));
+  ASSERT_EQ(response.file_descriptor_response().file_descriptor_proto_size(), 1)
+      << response.DebugString();
+  EXPECT_TRUE(reader_writer->Finish().ok());
+
+  protobuf::FileDescriptorProto file_desc_proto;
+  ASSERT_TRUE(file_desc_proto.ParseFromString(
+      response.file_descriptor_response().file_descriptor_proto(0)));
+  EXPECT_EQ(file_desc_proto.name(),
+            grpc::health::v1::HealthCheckRequest::descriptor()->file()->name());
 }
 
 }  // namespace testing
