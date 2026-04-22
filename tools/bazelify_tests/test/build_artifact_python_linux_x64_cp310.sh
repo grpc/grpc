efaca074e9f5253afe48a42e69d2778442eeb01c
Ashesh Vidyut <asheshvidyut@google.com>
2026-04-22 16:10:44 +0530

    Revert "remove scl enable devtoolset-10"
    This reverts commit 32eb7aabd498bec92858be0c8b64236eefca9e9d.

--- tools/bazelify_tests/test/build_artifact_python_linux_x64_cp310.sh
+++ tools/bazelify_tests/test/build_artifact_python_linux_x64_cp310.sh
@@ -35,4 +35,4 @@ export ARTIFACTS_OUT=artifacts
 # the build. This differs from the main branch/legacy setup where artifact
 # builds are usually run inside specific Docker images (managed by run_tests.py)
 # that already have the correct environment enabled.
-tools/run_tests/artifacts/build_artifact_python.sh
+scl enable devtoolset-10 -- tools/run_tests/artifacts/build_artifact_python.sh
