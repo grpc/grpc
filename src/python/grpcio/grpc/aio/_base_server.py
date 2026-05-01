566667c2a42f6d5a397553bf86bddf9e2089d0d2
Ashesh Vidyut <asheshvidyut@google.com>
2026-05-01 09:29:24 +0000

    fix pyright for base_server.py

--- src/python/grpcio/grpc/aio/_base_server.py
+++ src/python/grpcio/grpc/aio/_base_server.py
@@ -214,6 +214,7 @@ class ServicerContext(Generic[RequestType, ResponseType], abc.ABC):
           Exception: An exception is always raised to signal the abortion the
             RPC to the gRPC runtime.
         """
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def set_trailing_metadata(self, trailing_metadata: MetadataType) -> None:
@@ -314,6 +315,7 @@ class ServicerContext(Generic[RequestType, ResponseType], abc.ABC):
         Returns:
           A map of strings to an iterable of bytes for each auth property.
         """
+        raise NotImplementedError()
 
     def time_remaining(self) -> float:
         """Describes the length of allowed time remaining for the RPC.
@@ -323,6 +325,7 @@ class ServicerContext(Generic[RequestType, ResponseType], abc.ABC):
           remaining for the RPC to complete before it is considered to have
           timed out, or None if no deadline was specified for the RPC.
         """
+        raise NotImplementedError()
 
     def trailing_metadata(self):
         """Access value to be used as trailing metadata upon RPC completion.
@@ -374,6 +377,7 @@ class ServicerContext(Generic[RequestType, ResponseType], abc.ABC):
         Returns:
           A bool indicates whether the RPC is cancelled or not.
         """
+        raise NotImplementedError()
 
     def done(self) -> bool:
         """Return True if the RPC is done.
@@ -385,3 +389,4 @@ class ServicerContext(Generic[RequestType, ResponseType], abc.ABC):
         Returns:
           A bool indicates if the RPC is done.
         """
+        raise NotImplementedError()
