b169e364044f51caf075e8f64992a44baeeee8e4
Vignesh Babu <vigneshbabu@google.com>
2026-05-14 19:02:01 +0000

    Define experiment to manage server backlog

--- src/core/lib/experiments/experiments.cc
+++ src/core/lib/experiments/experiments.cc
@@ -223,6 +223,9 @@ const char* const additional_constraints_secure_endpoint_read_coalescing = "{}";
 const uint8_t required_experiments_secure_endpoint_read_coalescing[] = {
     static_cast<uint8_t>(grpc_core::kExperimentIdEventEngineClient),
     static_cast<uint8_t>(grpc_core::kExperimentIdEventEngineListener)};
+const char* const description_server_backlog_management =
+    "Reject requests when server backlog exceeds limit.";
+const char* const additional_constraints_server_backlog_management = "{}";
 const char* const description_skip_clear_peer_on_cancellation =
     "If set, skips clearing of peer string on call cancellation.";
 const char* const additional_constraints_skip_clear_peer_on_cancellation = "{}";
@@ -419,6 +422,8 @@ const ExperimentMetadata g_experiment_metadata[] = {
      description_secure_endpoint_read_coalescing,
      additional_constraints_secure_endpoint_read_coalescing,
      required_experiments_secure_endpoint_read_coalescing, 2, false, true},
+    {"server_backlog_management", description_server_backlog_management,
+     additional_constraints_server_backlog_management, nullptr, 0, true, true},
     {"skip_clear_peer_on_cancellation",
      description_skip_clear_peer_on_cancellation,
      additional_constraints_skip_clear_peer_on_cancellation, nullptr, 0, false,
@@ -660,6 +665,9 @@ const char* const additional_constraints_secure_endpoint_read_coalescing = "{}";
 const uint8_t required_experiments_secure_endpoint_read_coalescing[] = {
     static_cast<uint8_t>(grpc_core::kExperimentIdEventEngineClient),
     static_cast<uint8_t>(grpc_core::kExperimentIdEventEngineListener)};
+const char* const description_server_backlog_management =
+    "Reject requests when server backlog exceeds limit.";
+const char* const additional_constraints_server_backlog_management = "{}";
 const char* const description_skip_clear_peer_on_cancellation =
     "If set, skips clearing of peer string on call cancellation.";
 const char* const additional_constraints_skip_clear_peer_on_cancellation = "{}";
@@ -856,6 +864,8 @@ const ExperimentMetadata g_experiment_metadata[] = {
      description_secure_endpoint_read_coalescing,
      additional_constraints_secure_endpoint_read_coalescing,
      required_experiments_secure_endpoint_read_coalescing, 2, false, true},
+    {"server_backlog_management", description_server_backlog_management,
+     additional_constraints_server_backlog_management, nullptr, 0, true, true},
     {"skip_clear_peer_on_cancellation",
      description_skip_clear_peer_on_cancellation,
      additional_constraints_skip_clear_peer_on_cancellation, nullptr, 0, false,
@@ -1097,6 +1107,9 @@ const char* const additional_constraints_secure_endpoint_read_coalescing = "{}";
 const uint8_t required_experiments_secure_endpoint_read_coalescing[] = {
     static_cast<uint8_t>(grpc_core::kExperimentIdEventEngineClient),
     static_cast<uint8_t>(grpc_core::kExperimentIdEventEngineListener)};
+const char* const description_server_backlog_management =
+    "Reject requests when server backlog exceeds limit.";
+const char* const additional_constraints_server_backlog_management = "{}";
 const char* const description_skip_clear_peer_on_cancellation =
     "If set, skips clearing of peer string on call cancellation.";
 const char* const additional_constraints_skip_clear_peer_on_cancellation = "{}";
@@ -1293,6 +1306,8 @@ const ExperimentMetadata g_experiment_metadata[] = {
      description_secure_endpoint_read_coalescing,
      additional_constraints_secure_endpoint_read_coalescing,
      required_experiments_secure_endpoint_read_coalescing, 2, false, true},
+    {"server_backlog_management", description_server_backlog_management,
+     additional_constraints_server_backlog_management, nullptr, 0, true, true},
     {"skip_clear_peer_on_cancellation",
      description_skip_clear_peer_on_cancellation,
      additional_constraints_skip_clear_peer_on_cancellation, nullptr, 0, false,
--- src/core/lib/experiments/experiments.h
+++ src/core/lib/experiments/experiments.h
@@ -129,6 +129,8 @@ inline bool IsScheduleCancellationOverWriteEnabled() { return false; }
 inline bool IsSecureEndpointOffloadLargeReadsEnabled() { return false; }
 inline bool IsSecureEndpointOffloadLargeWritesEnabled() { return false; }
 inline bool IsSecureEndpointReadCoalescingEnabled() { return false; }
+#define GRPC_EXPERIMENT_IS_INCLUDED_SERVER_BACKLOG_MANAGEMENT
+inline bool IsServerBacklogManagementEnabled() { return true; }
 inline bool IsSkipClearPeerOnCancellationEnabled() { return false; }
 inline bool IsSubchannelConnectionScalingEnabled() { return false; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_SUBCHANNEL_WRAPPER_CLEANUP_ON_ORPHAN
@@ -215,6 +217,8 @@ inline bool IsScheduleCancellationOverWriteEnabled() { return false; }
 inline bool IsSecureEndpointOffloadLargeReadsEnabled() { return false; }
 inline bool IsSecureEndpointOffloadLargeWritesEnabled() { return false; }
 inline bool IsSecureEndpointReadCoalescingEnabled() { return false; }
+#define GRPC_EXPERIMENT_IS_INCLUDED_SERVER_BACKLOG_MANAGEMENT
+inline bool IsServerBacklogManagementEnabled() { return true; }
 inline bool IsSkipClearPeerOnCancellationEnabled() { return false; }
 inline bool IsSubchannelConnectionScalingEnabled() { return false; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_SUBCHANNEL_WRAPPER_CLEANUP_ON_ORPHAN
@@ -301,6 +305,8 @@ inline bool IsScheduleCancellationOverWriteEnabled() { return false; }
 inline bool IsSecureEndpointOffloadLargeReadsEnabled() { return false; }
 inline bool IsSecureEndpointOffloadLargeWritesEnabled() { return false; }
 inline bool IsSecureEndpointReadCoalescingEnabled() { return false; }
+#define GRPC_EXPERIMENT_IS_INCLUDED_SERVER_BACKLOG_MANAGEMENT
+inline bool IsServerBacklogManagementEnabled() { return true; }
 inline bool IsSkipClearPeerOnCancellationEnabled() { return false; }
 inline bool IsSubchannelConnectionScalingEnabled() { return false; }
 #define GRPC_EXPERIMENT_IS_INCLUDED_SUBCHANNEL_WRAPPER_CLEANUP_ON_ORPHAN
@@ -368,6 +374,7 @@ enum ExperimentIds {
   kExperimentIdSecureEndpointOffloadLargeReads,
   kExperimentIdSecureEndpointOffloadLargeWrites,
   kExperimentIdSecureEndpointReadCoalescing,
+  kExperimentIdServerBacklogManagement,
   kExperimentIdSkipClearPeerOnCancellation,
   kExperimentIdSubchannelConnectionScaling,
   kExperimentIdSubchannelWrapperCleanupOnOrphan,
@@ -585,6 +592,10 @@ inline bool IsSecureEndpointOffloadLargeWritesEnabled() {
 #define GRPC_EXPERIMENT_IS_INCLUDED_SECURE_ENDPOINT_READ_COALESCING
 inline bool IsSecureEndpointReadCoalescingEnabled() {
   return IsExperimentEnabled<kExperimentIdSecureEndpointReadCoalescing>();
+}
+#define GRPC_EXPERIMENT_IS_INCLUDED_SERVER_BACKLOG_MANAGEMENT
+inline bool IsServerBacklogManagementEnabled() {
+  return IsExperimentEnabled<kExperimentIdServerBacklogManagement>();
 }
 #define GRPC_EXPERIMENT_IS_INCLUDED_SKIP_CLEAR_PEER_ON_CANCELLATION
 inline bool IsSkipClearPeerOnCancellationEnabled() {
