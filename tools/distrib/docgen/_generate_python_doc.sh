5a1c21df463078895e733e5df6707b28d1889076
Tomasz Janiewicz <janiewicz@google.com>
2026-05-26 13:26:11 +0000

    Move Sphinx installation from _generate_python_doc.sh to requirements.docs.txt.  Auto generated requirements.docs.lock file

--- tools/distrib/docgen/_generate_python_doc.sh
+++ tools/distrib/docgen/_generate_python_doc.sh
@@ -30,8 +30,6 @@ pip install -r tools/distrib/docgen/requirements.docs.lock
 tools/run_tests/run_tests.py -c opt -l python --compiler python3.10 --newline_on_success -j 8 --build_only
 # shellcheck disable=SC1091
 source py310/bin/activate
-pip install --upgrade Sphinx
-pip install pydata_sphinx_theme==0.16.1
 
 # Use direct sphinx-build CLI command instead of `python setup.py doc`
 sphinx-build -b html -W --keep-going "$SOURCE_DIR" "$TARGET_DIR"
