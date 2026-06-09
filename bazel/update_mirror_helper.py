42726b31bab295a895822c24d41479b3123fc93e
Weizhe Yuan <weizheyuan@google.com>
2026-06-09 16:33:13 -0700

    uploader

--- bazel/update_mirror_helper.py
+++ bazel/update_mirror_helper.py
@@ -14,25 +14,14 @@
 # limitations under the License.
 
 import base64
+import logging
 import json
 import re
 import sys
 
-# TODO(weizheyuan): Remove this corner case.
-#
-# libpfm+ uses sourceforge url which has different scheme than github.
-# in short term we can use `gsutil cp` for manual upload.
-_EXCLUDE_REPOS = ["libpfm+"]
 _MIRROR_SITE_PATTERN = "https://storage.googleapis.com/grpc-bazel-mirror"
-
-def _integrity_to_sha256(integrity: str) -> str:
-    """Convert a SRI to sha256 checksum hex string"""
-    matches = re.match("sha256-(.*)", integrity)
-    if matches is None:
-        return None
-    sha256_base64 = matches.group(1)
-    sha256_bytes = base64.b64decode(sha256_base64)
-    return sha256_bytes.hex()
+_SUPPORTED_RULE_KINDS = ["http_archive", "http_file", "python_repository", "whl_library"]
+logger = logging.getLogger(__name__)
 
 def parse_ndjson(file_path):
     repos = []
@@ -41,51 +30,47 @@ def parse_ndjson(file_path):
             # Clean up whitespace and skip empty lines
             line = line.strip()
             if not line:
-                print(f"line: {line}")
+                logger.info(f"line: {line}")
                 continue
             try:
                 # Parse the individual JSON object
                 obj = json.loads(line)
                 repos.append(obj)
             except json.JSONDecodeError as e:
-                print(f"Skipping invalid JSON on line {line_number}: {e}")
+                logger.info(f"Skipping invalid JSON on line {line_number}: {e}")
     return repos
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     if len(sys.argv) < 3:
-        print("Usage: update_mirror_helper.py <input> <output>")
+        logger.info("Usage: update_mirror_helper.py <input> <output>")
         sys.exit(1)
     input_path = sys.argv[1]
     output_path = sys.argv[2]
     repos = parse_ndjson(input_path)
     output_lines = []
     for repo in repos:
-        if repo["repoRuleName"] != "http_archive":
-            continue
-        canonical_name = repo["canonicalName"]
-        if canonical_name in _EXCLUDE_REPOS:
+        if repo["repoRuleName"] not in _SUPPORTED_RULE_KINDS:
             continue
         archive = {}
         urls = []
         archive["urls"] = urls
-        archive["canonicalName"] = canonical_name
-        archive["apparentName"] = repo["apparentName"]
-        sha256 = ""
+        archive["canonicalName"] = repo.get("canonicalName")
+        archive["apparentName"] = repo.get("apparentName")
+        archive["name"] = repo.get("name")
         for attr in repo["attribute"]:
             if attr["name"] == "url" and attr["stringValue"]:
                 urls.append(attr["stringValue"])
             elif attr["name"] == "urls":
                 urls.extend(attr.get("stringListValue", list()))
             elif attr["name"] == "sha256" and attr["stringValue"]:
-                sha256 = attr["stringValue"]
-                archive["sha256"] = sha256
-            elif attr["name"] == "integrity"and attr["stringValue"]:
-                sha256 = _integrity_to_sha256(attr["stringValue"])
-                archive["sha256"] = sha256
-        print(f"archive={json.dumps(archive, indent=4)}")
+                archive["sha256"] = attr["stringValue"]
+            elif attr["name"] == "integrity" and attr["stringValue"]:
+                archive["integrity"] = attr["stringValue"]
+        logger.debug(f"Processing archive definition: {json.dumps(archive, indent=4)}")
         for url in urls:
             if _MIRROR_SITE_PATTERN not in url:
-                output_lines.append(f"{url.removeprefix('https://')} {sha256}")
+                output_lines.append(f"{url.removeprefix('https://')}")
     output = "\n".join(output_lines)
     with open(output_path, "w") as file:
         contents = file.write(output)
