201a13cf0aeed3a6d050605bb9356a32dc5e5b62
Pawan Bhardwaj <bpawan@google.com>
2026-06-09 22:27:49 +0530

    [Release] Bump version to 1.78.2 (on v1.78.x branch) (#42611)
    Created by release automation

--- src/ruby/lib/grpc/version.rb
+++ src/ruby/lib/grpc/version.rb
@@ -14,5 +14,5 @@
 
 # GRPC contains the General RPC module.
 module GRPC
-  VERSION = '1.78.1'
+  VERSION = '1.78.2'
 end
--- src/ruby/nativedebug/version.rb
+++ src/ruby/nativedebug/version.rb
@@ -14,6 +14,6 @@
 
 module GRPC
   module NativeDebug
-    VERSION = '1.78.1'
+    VERSION = '1.78.2'
   end
 end
