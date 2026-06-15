ae2e58b86d972848d0f5b4f76f0de002e24d766c
yuanweiz <weizheyuan@google.com>
2026-06-15 15:05:03 -0700

    [CI] Remove gcc7 and gcc8 tests. (#42663)
    TODO items:
    * update `src/php/docker/README.md` and remove centos7/gcc7 related sections.
    * Understand if we want to delete `GRPC_SKIP_BUILD_TESTS` flag, which, according to comment, is only introduced because of gcc 7 related issues.
    
    Closes #42663
    
    PiperOrigin-RevId: 932687420

--- tools/run_tests/run_tests_matrix.py
+++ tools/run_tests/run_tests_matrix.py
@@ -368,7 +368,6 @@ def _create_portability_test_jobs(
 
     # portability C and C++ on x64
     for compiler in [
-        "gcc8",
         "gcc10",
         # TODO(b/283304471): Tests using OpenSSL's engine APIs were broken and removed
         "gcc10.2_openssl102",
