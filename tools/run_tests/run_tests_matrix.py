2d489a0e056bacb978d286163d6a12c6e25f4c85
yuanweiz <weizheyuan@google.com>
2026-06-15 20:48:55 -0700

    Add Clang14 docker image for portability test. (#42333)
    OSS policy states the oldest version we need to support is clang 14. This PR adds new clang14 test and we can delete clang10 tests in follow up.
    
    https://github.com/google/oss-policies-info/blob/main/foundational-cxx-support-matrix.md
    
    Ephemeral run
    - [x] [grpc/core/master/linux/grpc_portability](https://source.cloud.google.com/results/invocations/88bfcadd-8c9b-454f-9222-dcb67599ee76)
    
    Closes #42333
    
    PiperOrigin-RevId: 932834869

--- tools/run_tests/run_tests_matrix.py
+++ tools/run_tests/run_tests_matrix.py
@@ -376,6 +376,7 @@ def _create_portability_test_jobs(
         "gcc14",
         "gcc_musl",
         "clang11",
+        "clang14",
         "clang19",
     ]:
         test_jobs += _generate_jobs(
