51684b38be13e77e60fdbfea1659d79a66143913
Ashesh Vidyut <asheshvidyut@google.com>
2026-06-25 01:35:47 +0000

    black code

--- tools/run_tests/artifacts/package_targets.py
+++ tools/run_tests/artifacts/package_targets.py
@@ -184,8 +184,11 @@ class PythonPackage:
         }
 
         job_name = os.getenv("KOKORO_JOB_NAME", "")
-        if job_name == "grpc/core/master/linux/release/grpc_collect_all_packages":
-          environ["EXCLUDE_PATTERNS"] += " python_*_cp315"
+        if (
+            job_name
+            == "grpc/core/master/linux/release/grpc_collect_all_packages"
+        ):
+            environ["EXCLUDE_PATTERNS"] += " python_*_cp315"
 
         if self.platform == "any":
             # all the artifact builder configurations generate an equivalent
