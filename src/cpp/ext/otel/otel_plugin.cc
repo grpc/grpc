4b0121ad9e4964ff63bf3d11d8d632a18b9b36eb
Gregory Cooke <gregorycooke@google.com>
2026-07-01 15:25:30 +0000

    use consts

--- src/cpp/ext/otel/otel_plugin.cc
+++ src/cpp/ext/otel/otel_plugin.cc
@@ -63,8 +63,8 @@ bool IsMetricEnabledByDefault(absl::string_view) { return false; }
 bool IsOpenTelemetryLabelOptional(absl::string_view label_key) {
   // TODO(ctiller): register other optional labels here with
   // `if (label_key =="xyz") return true;` checks.
-  if (label_key == "grpc.lb.locality" ||
-      label_key == "grpc.lb.backend_service") {
+  if (label_key == grpc_core::kMetricLabelLocality ||
+      label_key == grpc_core::kMetricLabelBackendService) {
     return true;
   }
   return absl::StartsWith(label_key, "test_optional.");
