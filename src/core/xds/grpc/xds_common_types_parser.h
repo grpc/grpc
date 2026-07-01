c4e1617035fef49568fe1dd7073e9cd069366038
Mark D. Roth <roth@google.com>
2026-07-01 20:20:43 +0000

    move GrpcService parser to its own file

--- src/core/xds/grpc/xds_common_types_parser.h
+++ src/core/xds/grpc/xds_common_types_parser.h
@@ -22,7 +22,6 @@
 
 #include "envoy/config/common/mutation_rules/v3/mutation_rules.upb.h"
 #include "envoy/config/core/v3/base.upb.h"
-#include "envoy/config/core/v3/grpc_service.upb.h"
 #include "envoy/type/matcher/v3/string.upb.h"
 #include "envoy/type/v3/percent.upb.h"
 #include "google/protobuf/any.upb.h"
@@ -83,11 +82,6 @@ absl::StatusOr<Json> ParseProtobufStructToJson(
 std::optional<XdsExtension> ExtractXdsExtension(
     const XdsResourceType::DecodeContext& context,
     const google_protobuf_Any* any, ValidationErrors* errors);
-
-XdsGrpcService ParseXdsGrpcService(
-    const XdsResourceType::DecodeContext& context,
-    const envoy_config_core_v3_GrpcService* grpc_service,
-    ValidationErrors* errors);
 
 HeaderMutationRules ParseHeaderMutationRules(
     const envoy_config_common_mutation_rules_v3_HeaderMutationRules*
