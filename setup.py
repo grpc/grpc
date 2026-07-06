0ed99a3a72915140997e9f6b4a236fdc39636c02
sreenithi <ssreenithi@google.com>
2026-07-06 11:30:18 +0000

    make macro and API tag dynamic for 3.10 and 3.12

--- setup.py
+++ setup.py
@@ -496,7 +496,12 @@ if "linux" in sys.platform or "darwin" in sys.platform:
     DEFINE_MACROS += (("PyMODINIT_FUNC", pymodinit),)
     DEFINE_MACROS += (("GRPC_POSIX_FORK_ALLOW_PTHREAD_ATFORK", 1),)
 
-DEFINE_MACROS += (("Py_LIMITED_API", "0x030A0000"),)
+if sys.version_info >= (3, 12):
+    DEFINE_MACROS += (("Py_LIMITED_API", "0x030C00F0"),)
+    API_TAG = "cp312"
+else:
+    DEFINE_MACROS += (("Py_LIMITED_API", "0x030A00F0"),)
+    API_TAG = "cp310"
 
 def cython_extensions_and_necessity():
     cython_module_files = [
@@ -611,4 +616,9 @@ if __name__ == "__main__":
         extras_require=EXTRAS_REQUIRES,
         setup_requires=SETUP_REQUIRES,
         cmdclass=COMMAND_CLASS,
+        options={
+            "bdist_wheel": {
+                "py_limited_api": API_TAG
+            }
+        }
     )
