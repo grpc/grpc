e9fe0471b5f8884e41cb28c4a539973230db72ae
Ashesh Vidyut <asheshvidyut@google.com>
2026-07-09 10:22:15 +0000

    using type alias for EOFType

--- src/python/grpcio/grpc/aio/_typing.py
+++ src/python/grpcio/grpc/aio/_typing.py
@@ -20,6 +20,7 @@ from typing import (
     Iterable,
     Sequence,
     Tuple,
+    TypeAlias,
     TypeVar,
     Union,
 )
@@ -40,7 +41,7 @@ ResponseType = TypeVar("ResponseType")
 SerializingFunction = Callable[[Any], bytes]
 DeserializingFunction = Callable[[bytes], Any]
 ChannelArgumentType = Sequence[Tuple[str, Any]]
-EOFType = _EOF
+EOFType: TypeAlias = _EOF
 DoneCallbackType = Callable[[Any], None]
 RequestIterableType = Union[Iterable[Any], AsyncIterable[Any]]
 ResponseIterableType = AsyncIterable[Any]
