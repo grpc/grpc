947fa0cc574de671e80cef9dceffb9aae8d3ea7e
Ashesh Vidyut <asheshvidyut@google.com>
2026-07-13 13:36:29 +0530

    removed enable unused-import

--- src/python/grpcio/grpc/aio/_base_server.py
+++ src/python/grpcio/grpc/aio/_base_server.py
@@ -18,7 +18,6 @@ from typing import Generic, Iterable, Mapping, NoReturn, Optional, Sequence
 
 import grpc
 
-# pylint: enable=unused-import
 from ._typing import DoneCallbackType
 from ._typing import MetadataType
 from ._typing import RequestType
