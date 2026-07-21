7d7c76d21b594d6329a9eef6bc8d7098354dfc9f
Ashesh Vidyut <asheshvidyut@google.com>
2026-07-21 16:35:50 +0530

    add a comment about the file

--- private_stubs/grpc/__init__.pyi
+++ private_stubs/grpc/__init__.pyi
@@ -11,7 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# NOTE: These type stubs are private to this repository and added solely to
+# support internal type checking (e.g. Pyright). They are not public APIs.
 """gRPC's Python API type stubs."""
+
 
 import abc
 import enum
