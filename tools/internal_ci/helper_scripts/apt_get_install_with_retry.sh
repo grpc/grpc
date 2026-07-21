eea5c4e464670076a7ba2dcc1c96f8b07ff09c34
Piotr Grohman <grohman@google.com>
2026-07-21 08:31:31 +0000

    feat: introduce a retry wrapper for apt-get installations in Dockerfiles to improve reliability + pr cleanup

--- tools/internal_ci/helper_scripts/apt_get_install_with_retry.sh
+++ tools/internal_ci/helper_scripts/apt_get_install_with_retry.sh
@@ -1,23 +1,3 @@
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-FROM silkeh/clang:19-bookworm
-
-#=================
-#  Create wrapper for apt-get
-
-COPY --chmod=+x <<-"EOF" /usr/local/bin/apt-get-install-retry
 #!/usr/bin/env bash
 # Copyright 2026 The gRPC Authors
 #
@@ -68,21 +48,3 @@ for i in $(seq 1 $MAX_RETRIES); do
     sleep $(($delay**$i));
   fi
 done
-
-EOF
-
-
-# Install prerequisites for the clang-tidy script
-RUN /usr/local/bin/apt-get-install-retry  python3 jq git
-
-# otherwise clang-tidy will report missing <gtest/gtest.h> header
-RUN /usr/local/bin/apt-get-install-retry  libgtest-dev
-
-ADD clang_tidy_all_the_things.sh /
-
-# When running locally, we'll be impersonating the current user, so we need
-# to make the script runnable by everyone.
-RUN chmod a+rx /clang_tidy_all_the_things.sh
-
-CMD ["echo 'Run with tools/distrib/clang_tidy_code.sh'"]
-
