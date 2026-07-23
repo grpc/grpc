a0c8ed1e06d3a134e63109893814640d485c6ecb
Jason Aragorn Tobias Lunn <jatl@google.com>
2026-07-23 07:56:18 -0700

    [export] [proto compiler] Upgrade protoc plugins to support Protobuf Edition 2026
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/952760200](http://cl/952760200)
    
    PiperOrigin-RevId: 952760200

--- src/compiler/python_generator.h
+++ src/compiler/python_generator.h
@@ -19,6 +19,8 @@
 #ifndef GRPC_INTERNAL_COMPILER_PYTHON_GENERATOR_H
 #define GRPC_INTERNAL_COMPILER_PYTHON_GENERATOR_H
 
+#include <cstdint>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -59,12 +61,10 @@ class PythonGrpcGenerator : public grpc::protobuf::compiler::CodeGenerator {
     return grpc::protobuf::Edition::EDITION_PROTO2;
   }
   grpc::protobuf::Edition GetMaximumEdition() const override {
-    // TODO(yuanweiz): Remove when the protobuf is updated to a version
-    //      that supports edition 2024.
-#if !defined(GOOGLE_PROTOBUF_VERSION) || GOOGLE_PROTOBUF_VERSION >= 6032000
-    return grpc::protobuf::Edition::EDITION_2024;
+#if !defined(GOOGLE_PROTOBUF_VERSION) || GOOGLE_PROTOBUF_VERSION >= 6036000
+    return grpc::protobuf::Edition::EDITION_2026;
 #else
-    return grpc::protobuf::Edition::EDITION_2023;
+    return grpc::protobuf::Edition::EDITION_2024;
 #endif
   }
 #endif
