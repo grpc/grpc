0c46ba8077624cb1da3d9294536f4f541fc6e792
Jason Aragorn Tobias Lunn <jatl@google.com>
2026-07-23 07:39:20 -0700

    [export] Upgrade plugins to support edition 2026
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/952752454](http://cl/952752454)
    
    PiperOrigin-RevId: 952752454

--- src/compiler/php_plugin.cc
+++ src/compiler/php_plugin.cc
@@ -49,7 +49,7 @@ class PHPGrpcGenerator : public grpc::protobuf::compiler::CodeGenerator {
     // TODO(yuanweiz): Remove when the protobuf is updated to a version
     //      that supports edition 2024.
 #if !defined(GOOGLE_PROTOBUF_VERSION) || GOOGLE_PROTOBUF_VERSION >= 6032000
-    return grpc::protobuf::Edition::EDITION_2024;
+    return grpc::protobuf::Edition::EDITION_2026;
 #else
     return grpc::protobuf::Edition::EDITION_2023;
 #endif
