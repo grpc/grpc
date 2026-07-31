5d64f824ce341e9830678743b96908ee8ee0d980
Mark D. Roth <roth@google.com>
2026-07-31 00:30:40 +0000

    fix build

--- test/core/xds/xds_http_filters_test.cc
+++ test/core/xds/xds_http_filters_test.cc
@@ -2220,7 +2220,6 @@ TEST_F(XdsExtProcFilterTest, Accessors) {
             "envoy.extensions.filters.http.ext_proc.v3.ExternalProcessor");
   EXPECT_EQ(factory_->OverrideConfigProtoName(),
             "envoy.extensions.filters.http.ext_proc.v3.ExtProcPerRoute");
-  EXPECT_EQ(factory_->channel_filter(), &ExtProcFilter::kFilterVtable);
   EXPECT_TRUE(factory_->IsSupportedOnClients());
   EXPECT_FALSE(factory_->IsSupportedOnServers());
   EXPECT_FALSE(factory_->IsTerminalFilter());
