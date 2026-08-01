53e0838f463aafbae90d865fe135eb93b97d2712
Sergii Tkachenko <sergiitk@google.com>
2026-08-01 01:01:35 -0700

    Address PR feedback: wrap BigQuery pip install in UPLOAD_TEST_RESULTS check
    Wrap google-api-python-client and oauth2client pip installation in tools/internal_ci/linux/grpc_python_bazel_test_in_docker.sh inside an if [[ "${UPLOAD_TEST_RESULTS}" == "true" ]] conditional block so PR presubmit runs avoid unnecessary package installation overhead.

--- tools/internal_ci/linux/grpc_python_bazel_test_in_docker.sh
+++ tools/internal_ci/linux/grpc_python_bazel_test_in_docker.sh
@@ -20,7 +20,9 @@ set -ex
 
 # Needed for upload_rbe_results.py big_query_utils called by bazel_report_helper.py
 # TODO(sergiitk): move to the main bazel image, similar to bazel_arm64
-python3 -m pip install --user google-api-python-client oauth2client
+if [[ "${UPLOAD_TEST_RESULTS}" == "true" ]]; then
+  python3 -m pip install --user google-api-python-client oauth2client
+fi
 
 RESULTSTORE_RESULTS_FLAG="--bazelrc=tools/remote_build/include/test_locally_with_resultstore_results.bazelrc"
 TEST_TARGETS="//src/python/... //tools/distrib/python/grpcio_tools/... //examples/python/..."
