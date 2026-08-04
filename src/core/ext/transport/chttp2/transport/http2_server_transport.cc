b2be17a6a0dca52d58ac1d8d24471382bfda38c7
Tanvi Jagtap <tjagtap@google.com>
2026-08-04 02:49:58 -0700

    [export] [PH2][Bug] Fix Ping bug
    ----
    DO NOT SUBMIT. This PR is for testing purposes only. [cl/958906507](http://cl/958906507)
    
    PiperOrigin-RevId: 958906507

--- src/core/ext/transport/chttp2/transport/http2_server_transport.cc
+++ src/core/ext/transport/chttp2/transport/http2_server_transport.cc
@@ -1930,7 +1930,7 @@ Http2ServerTransport::Http2ServerTransport(
   TransportChannelArgs args;
   ReadChannelArgs(channel_args, args);
 
-  ping_manager_.emplace(channel_args, args.ping_timeout,
+  ping_manager_.emplace(channel_args, kIsClient, args.ping_timeout,
                         PingSystemInterfaceImpl::Make(this), event_engine_);
 
   // The keepalive loop is only spawned if the keepalive time is not infinity.
