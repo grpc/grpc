1f62095b4e5954e1b976ee404f15a5f34596f65a
Pawan Bhardwaj <bpawan@google.com>
2026-08-10 08:32:06 +0000

    add back

--- test/core/filters/filter_test_v2_test.cc
+++ test/core/filters/filter_test_v2_test.cc
@@ -250,3 +250,8 @@ TEST_F(NoOpFilterTest, CanProcessServerToClientMessage) {
 
 }  // namespace
 }  // namespace grpc_core
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
