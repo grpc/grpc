dc0fd0857ce3af92d3382e5319ea3584ed2dc0a9
Mark D. Roth <roth@google.com>
2026-08-11 19:06:43 +0000

    fix test

--- test/core/xds/xds_lb_policy_registry_test.cc
+++ test/core/xds/xds_lb_policy_registry_test.cc
@@ -160,25 +160,10 @@ TEST(ClientSideWeightedRoundRobinTest, FieldsExplicitlySet) {
             "\"blackoutPeriod\":\"2.000000000s\","
             "\"enableOobLoadReport\":true,"
             "\"errorUtilizationPenalty\":5,"
+            "\"metricNamesForComputingUtilization\":[\"cpu_usage\"],"
             "\"oobReportingPeriod\":\"1.000000000s\","
             "\"weightExpirationPeriod\":\"3.000000000s\","
             "\"weightUpdatePeriod\":\"4.000000000s\""
-            "}}");
-}
-
-TEST(ClientSideWeightedRoundRobinTest, WrrCustomMetrics) {
-  ClientSideWeightedRoundRobin wrr;
-  wrr.add_metric_names_for_computing_utilization("cpu_usage");
-  LoadBalancingPolicyProto policy;
-  policy.add_policies()
-      ->mutable_typed_extension_config()
-      ->mutable_typed_config()
-      ->PackFrom(wrr);
-  auto result = ConvertXdsPolicy(policy);
-  ASSERT_TRUE(result.ok()) << result.status();
-  EXPECT_EQ(*result,
-            "{\"weighted_round_robin\":{"
-            "\"metricNamesForComputingUtilization\":[\"cpu_usage\"]"
             "}}");
 }
 
