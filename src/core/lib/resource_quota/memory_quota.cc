64a73e6cdaccfedafc5e308b0d917d8ce7e0ee2d
Aadi Kapur <aadik@google.com>
2026-08-11 04:09:46 +0000

    Use gpr_malloc instead of adding a new line to check

--- src/core/lib/resource_quota/memory_quota.cc
+++ src/core/lib/resource_quota/memory_quota.cc
@@ -405,9 +405,7 @@ void GrpcMemoryAllocatorImpl::Replenish() {
 
 grpc_slice GrpcMemoryAllocatorImpl::MakeSlice(MemoryRequest request) {
   auto size = Reserve(request.Increase(sizeof(SliceRefCount)));
-  void* p = malloc(size);
-  GRPC_CHECK_NE(p, nullptr) << absl::StrFormat(
-      "GrpcMemoryAllocatorImpl::MakeSlice: malloc(%zu) returned nullptr", size);
+  void* p = gpr_malloc(size);
   new (p) SliceRefCount(shared_from_this(), size);
   grpc_slice slice;
   slice.refcount = static_cast<SliceRefCount*>(p);
