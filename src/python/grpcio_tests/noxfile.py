804b8297fc33de17912715c53b252b0e47650ce7
Tomasz Janiewicz <janiewicz@google.com>
2026-08-17 09:22:39 +0000

    Fix CI/CD error

--- src/python/grpcio_tests/noxfile.py
+++ src/python/grpcio_tests/noxfile.py
@@ -115,7 +115,7 @@ def build_package_protos(session: nox.Session):
     command.build_package_protos(PYTHON_STEM, args.strict_mode)
 
 
-@nox.session(venv_backend="venv", venv_params=["--system-site-packages"])
+@nox.session(venv_backend="venv", venv_params=["--system-site-packages", "--without-pip"])
 def test_lite(session: nox.Session):
     """Command to run tests without fetching or building anything."""
     session.log("Running test_lite for grpcio-tools...")
@@ -146,7 +146,7 @@ def test_lite(session: nox.Session):
     )
 
 
-@nox.session(venv_backend="venv", venv_params=["--system-site-packages"])
+@nox.session(venv_backend="venv", venv_params=["--system-site-packages", "--without-pip"])
 def test_py3_only(session: nox.Session):
     """Command to run tests for Python 3+ features.
 
@@ -180,7 +180,7 @@ def test_py3_only(session: nox.Session):
     )
 
 
-@nox.session(venv_backend="venv", venv_params=["--system-site-packages"])
+@nox.session(venv_backend="venv", venv_params=["--system-site-packages", "--without-pip"])
 def test_aio(session: nox.Session):
     """Command to run aio tests without fetching or building anything."""
 
