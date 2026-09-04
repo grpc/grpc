06eb9ddd6349600bbd43cb3cae537125083bc994
Tomasz Janiewicz <janiewicz@google.com>
2026-09-04 08:55:11 +0000

    [Python] Migrate grpcio-health-checking build commands to noxfile - Remove custom setuptools cmdclasses (preprocess, build_package_protos) and _NoOpCommand from setup.py. - Remove health_commands.py in favor of nox sessions. - Add noxfile.py with preprocess and build_package_protos sessions for proto generation and licensing.

--- src/python/grpcio_health_checking/setup.py
+++ src/python/grpcio_health_checking/setup.py
@@ -25,23 +25,6 @@ sys.path.insert(0, os.path.abspath("."))
 import grpc_version
 import python_version
 
-
-class _NoOpCommand(setuptools.Command):
-    """No-op command."""
-
-    description = ""
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        pass
-
-
 CLASSIFIERS = [
     "Development Status :: 5 - Production/Stable",
     "Programming Language :: Python",
@@ -56,31 +39,9 @@ INSTALL_REQUIRES = (
     "grpcio>={version}".format(version=grpc_version.VERSION),
 )
 
-try:
-    import health_commands as _health_commands
-
-    # we are in the build environment, otherwise the above import fails
-    SETUP_REQUIRES = (
-        "grpcio-tools=={version}".format(version=grpc_version.VERSION),
-    )
-    COMMAND_CLASS = {
-        # Run preprocess from the repository *before* doing any packaging!
-        "preprocess": _health_commands.Preprocess,
-        "build_package_protos": _health_commands.BuildPackageProtos,
-    }
-except ImportError:
-    SETUP_REQUIRES = ()
-    COMMAND_CLASS = {
-        # wire up commands to no-op not to break the external dependencies
-        "preprocess": _NoOpCommand,
-        "build_package_protos": _NoOpCommand,
-    }
-
 if __name__ == "__main__":
     setuptools.setup(
         classifiers=CLASSIFIERS,
         python_requires=f">={python_version.MIN_PYTHON_VERSION}",
         install_requires=INSTALL_REQUIRES,
-        setup_requires=SETUP_REQUIRES,
-        cmdclass=COMMAND_CLASS,
     )
