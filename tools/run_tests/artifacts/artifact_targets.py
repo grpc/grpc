2b75f47a213e29c00277f9fb0355c44a772f89a7
Weizhe Yuan <weizheyuan@google.com>
2026-09-04 10:06:20 -0700

    [CI] Disable ruby build test for x64-mingw-ucrt.

--- tools/run_tests/artifacts/artifact_targets.py
+++ tools/run_tests/artifacts/artifact_targets.py
@@ -520,7 +520,9 @@ def targets():
             PythonArtifact("windows", "x64", "Python314"),
             PythonArtifact("windows", "x64", "Python315", presubmit=True),
             RubyArtifact("linux", "x86-mingw32", presubmit=True),
-            RubyArtifact("linux", "x64-mingw-ucrt"),
+            # TODO(weizheyuan, asheshvidyut): Properly install gcc-mingw >= 10
+            # (which is required by abseil) and re-enable this test.
+            # RubyArtifact("linux", "x64-mingw-ucrt"),
             RubyArtifact("linux", "x86_64-linux-gnu", presubmit=True),
             RubyArtifact("linux", "x86_64-linux-musl", presubmit=True),
             RubyArtifact("linux", "x86-linux-gnu"),
