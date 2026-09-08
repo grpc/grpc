d9979793a2f57ddfc4ae5b975799106e29ab9a55
Weizhe Yuan <weizheyuan@google.com>
2026-09-08 16:27:25 -0700

    move from .sh to Dockerfile

--- test/distrib/cpp/run_distrib_test_cmake_aarch64_cross.sh
+++ test/distrib/cpp/run_distrib_test_cmake_aarch64_cross.sh
@@ -18,9 +18,6 @@ set -ex
 
 cd "$(dirname "$0")/../../.."
 
-# Install openssl (to use instead of boringssl)
-apt-get update && apt-get install -y libssl-dev
-
 # Use externally provided env to determine build parallelism, otherwise use default.
 GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS=${GRPC_CPP_DISTRIBTEST_BUILD_COMPILER_JOBS:-4}
 
