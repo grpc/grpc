5d6a440eb49a03f56c297a8be5cf1f055dc0c4ba
Rishesh Agarwal <rishesh@google.com>
2026-09-14 09:24:48 +0000

    [tsi] Expose TLS peer and local certificate properties in auth context

--- src/core/credentials/transport/tls/ssl_utils.cc
+++ src/core/credentials/transport/tls/ssl_utils.cc
@@ -36,6 +36,7 @@
 #include "src/core/config/config_vars.h"
 #include "src/core/credentials/transport/tls/grpc_tls_certificate_selector.h"
 #include "src/core/credentials/transport/tls/load_system_roots.h"
+#include "src/core/credentials/transport/tls/tls_utils.h"
 #include "src/core/ext/transport/chttp2/alpn/alpn.h"
 #include "src/core/lib/channel/channel_args.h"
 #include "src/core/transport/auth_context.h"
@@ -308,6 +309,15 @@ grpc_core::RefCountedPtr<grpc_auth_context> grpc_ssl_peer_to_auth_context(
       grpc_auth_context_add_property(
           ctx.get(), GRPC_SSL_NEGOTIATED_KEY_EXCHANGE_GROUP_PROPERTY_NAME,
           prop->value.data, prop->value.length);
+    } else if (strcmp(prop->name,
+                      TSI_SSL_REQUESTED_SERVER_NAME_PEER_PROPERTY) == 0) {
+      grpc_auth_context_add_property(
+          ctx.get(), GRPC_SSL_REQUESTED_SERVER_NAME_PROPERTY_NAME,
+          prop->value.data, prop->value.length);
+    } else if (strcmp(prop->name, TSI_SSL_TLS_VERSION_PEER_PROPERTY) == 0) {
+      grpc_auth_context_add_property(ctx.get(),
+                                     GRPC_SSL_TLS_VERSION_PROPERTY_NAME,
+                                     prop->value.data, prop->value.length);
     } else if (strcmp(prop->name, TSI_SECURITY_LEVEL_PEER_PROPERTY) == 0) {
       grpc_auth_context_add_property(
           ctx.get(), GRPC_TRANSPORT_SECURITY_LEVEL_PROPERTY_NAME,
@@ -330,6 +340,22 @@ grpc_core::RefCountedPtr<grpc_auth_context> grpc_ssl_peer_to_auth_context(
                                      prop->value.data, prop->value.length);
     } else if (strcmp(prop->name, TSI_X509_IP_PEER_PROPERTY) == 0) {
       grpc_auth_context_add_property(ctx.get(), GRPC_PEER_IP_PROPERTY_NAME,
+                                     prop->value.data, prop->value.length);
+    }
+    // The three properties below describe the LOCAL endpoint's certificate,
+    // not the peer's, so they must never affect
+    // peer_identity_property_name.
+    else if (strcmp(prop->name, TSI_X509_LOCAL_URI_PROPERTY) == 0) {
+      grpc_auth_context_add_property(ctx.get(),
+                                     GRPC_X509_LOCAL_URI_PROPERTY_NAME,
+                                     prop->value.data, prop->value.length);
+    } else if (strcmp(prop->name, TSI_X509_LOCAL_DNS_PROPERTY) == 0) {
+      grpc_auth_context_add_property(ctx.get(),
+                                     GRPC_X509_LOCAL_DNS_PROPERTY_NAME,
+                                     prop->value.data, prop->value.length);
+    } else if (strcmp(prop->name, TSI_X509_LOCAL_SUBJECT_PROPERTY) == 0) {
+      grpc_auth_context_add_property(ctx.get(),
+                                     GRPC_X509_LOCAL_SUBJECT_PROPERTY_NAME,
                                      prop->value.data, prop->value.length);
     }
   }
