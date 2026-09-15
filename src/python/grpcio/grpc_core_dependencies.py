ce8ee3b50d5edc0f02d92a883761fee8ce020d8e
Tanvi Jagtap <tjagtap@google.com>
2026-09-15 06:19:50 +0000

    [PH2][Trivial] Build

--- src/python/grpcio/grpc_core_dependencies.py
+++ src/python/grpcio/grpc_core_dependencies.py
@@ -179,6 +179,7 @@ CORE_SOURCE_FILES = [
     'src/core/ext/transport/chttp2/transport/ping_callbacks.cc',
     'src/core/ext/transport/chttp2/transport/ping_promise.cc',
     'src/core/ext/transport/chttp2/transport/ping_rate_policy.cc',
+    'src/core/ext/transport/chttp2/transport/reclaimer.cc',
     'src/core/ext/transport/chttp2/transport/stream_lists.cc',
     'src/core/ext/transport/chttp2/transport/transport_common.cc',
     'src/core/ext/transport/chttp2/transport/varint.cc',
