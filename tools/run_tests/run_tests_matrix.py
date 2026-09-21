c5db41a3ee8687735e5700c4578ff0816edeaa88
Weizhe Yuan <weizheyuan@google.com>
2026-09-21 11:08:25 -0700

    revert ref

--- tools/run_tests/run_tests_matrix.py
+++ tools/run_tests/run_tests_matrix.py
@@ -376,7 +376,7 @@ def _create_portability_test_jobs(
         "gcc14",
         "gcc_musl",
         "clang14",
-        "clang21",
+        "clang19",
     ]:
         test_jobs += _generate_jobs(
             languages=["c", "c++"],
