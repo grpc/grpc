a0b26d9364e42a7310b18a98ae0060cc6b050554
Rishesh Agarwal <rishesh@google.com>
2026-09-25 02:39:08 -0700

    [promise_based_filter] Report cancellation status on recv_message completion (#43494)
    Closes #43494
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/43494 from rishesh007:recv_message_cancelled_status_fix 5ebf46671fcec152fac52238bc22c4c40e56c578
    PiperOrigin-RevId: 988137579

--- src/core/lib/channel/promise_based_filter.cc
+++ src/core/lib/channel/promise_based_filter.cc
@@ -843,9 +843,15 @@ void BaseCallData::ReceiveMessage::Done(const ServerMetadata& metadata,
       state_ = State::kCancelledWhilstIdle;
       break;
     case State::kForwardedBatch:
+      if (IsRecvMessageCancelledStatusFixEnabled()) {
+        cancelled_status_ = StatusFromMetadata(metadata);
+      }
       state_ = State::kCancelledWhilstForwarding;
       break;
     case State::kForwardedBatchNoPipe:
+      if (IsRecvMessageCancelledStatusFixEnabled()) {
+        cancelled_status_ = StatusFromMetadata(metadata);
+      }
       state_ = State::kCancelledWhilstForwardingNoPipe;
       break;
     case State::kCompletedWhileBatchCompleted:
@@ -855,7 +861,11 @@ void BaseCallData::ReceiveMessage::Done(const ServerMetadata& metadata,
         // Store the cancellation status so that WakeInsideCombiner() can
         // propagate it to the application's message ready callback instead of
         // defaulting to OK.
-        completed_status_ = StatusFromMetadata(metadata);
+        if (IsRecvMessageCancelledStatusFixEnabled()) {
+          cancelled_status_ = StatusFromMetadata(metadata);
+        } else {
+          completed_status_ = StatusFromMetadata(metadata);
+        }
         // Drop the buffered message here so it cannot bypass the trailing
         // metadata; WakeInsideCombiner() just fires the completion.
         if (intercepted_slice_buffer_ != nullptr) {
@@ -913,14 +923,21 @@ void BaseCallData::ReceiveMessage::Done(const ServerMetadata& metadata,
           *intercepted_slice_buffer_ = std::nullopt;
         }
       }
+      if (IsRecvMessageCancelledStatusFixEnabled()) {
+        cancelled_status_ = StatusFromMetadata(metadata);
+      }
       state_ = State::kBatchCompletedButCancelledNoPipe;
       break;
     case State::kBatchCompletedButCancelled:
     case State::kBatchCompletedButCancelledNoPipe:
       Crash(absl::StrFormat("ILLEGAL STATE: %s", StateString(state_)));
-    case State::kCancelledWhilstIdle:
     case State::kCancelledWhilstForwarding:
     case State::kCancelledWhilstForwardingNoPipe:
+      if (IsRecvMessageCancelledStatusFixEnabled() && cancelled_status_.ok()) {
+        cancelled_status_ = StatusFromMetadata(metadata);
+      }
+      break;
+    case State::kCancelledWhilstIdle:
     case State::kCancelled:
       break;
   }
@@ -963,13 +980,21 @@ void BaseCallData::ReceiveMessage::WakeInsideCombiner(Flusher* flusher,
     case State::kBatchCompletedButCancelled:
       interceptor()->Push()->Close();
       state_ = State::kCancelled;
-      flusher->AddClosure(std::exchange(intercepted_on_complete_, nullptr),
-                          completed_status_, "recv_message");
+      flusher->AddClosure(
+          std::exchange(intercepted_on_complete_, nullptr),
+          (IsRecvMessageCancelledStatusFixEnabled() && !cancelled_status_.ok())
+              ? cancelled_status_
+              : completed_status_,
+          "recv_message");
       break;
     case State::kBatchCompletedButCancelledNoPipe:
       state_ = State::kCancelled;
-      flusher->AddClosure(std::exchange(intercepted_on_complete_, nullptr),
-                          completed_status_, "recv_message");
+      flusher->AddClosure(
+          std::exchange(intercepted_on_complete_, nullptr),
+          (IsRecvMessageCancelledStatusFixEnabled() && !cancelled_status_.ok())
+              ? cancelled_status_
+              : completed_status_,
+          "recv_message");
       break;
     case State::kCompletedWhileBatchCompleted:
       if (!IsRecvMessageFilterBypassFixEnabled()) {
